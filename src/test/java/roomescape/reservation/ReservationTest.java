1ba92a23a78da78c3bd0aa71564f5a826b124011
woowabrie <buyong.jo@woowahan.com>
2024-04-05 10:44:00 +0900

    migrate first mission
    feat: setup project
    
    [1 - 3단계 - 방탈출 예약 관리] 러쉬(이태훈) 미션 제출합니다. (#37)
    
    * docs(README.md): 구현할 기능 목록 작성
    
    * feat(RoomescapeController): 어드민 메인 페이지 응답 기능 구현
    
    * feat: 예약 관리 페이지 및 조회 기능
    
    * feat(RoomescapeController): 예약 추가 API 기능 구현
    
    * feat(RoomescapeController): 예약 취소 api 기능 구현
    
    * fix: 기능 변경에 따른 테스트 값 변경
    
    * refactor(RoomescapeController):  모든 예약 조회 api ResponseEntity 사용하도록 수정
    
    * refactor(RoomescapeController): 에러 메시지 추가
    
    * refactor(Reservation): Member class명 변경
    
    * fix(reservation-legacy.html): 경로 수정
    
    * feat(ReservationRequest): Reservation 요청 dto 생성
    
    * refactor(ReservationTest): ReservationTest 테스트 분리 및 수정
    
    * refactor(ReservationController): Controller 명 수정
    
    * refactor(AdminController): Admin Controller 분리
    
    * refactor(ReservationController): final 추가
    
    * refactor(ReservationTest): 주석제거
    
    * style: 문자열 포맷팅
    
    ---------
    
    Co-authored-by: lemone <lejinhy@gmail.com>
    
    [4 - 9단계 방탈출 예약 관리] 러쉬(이태훈) 미션 제출합니다. (#97)
    
    * feat: h2 DB 설정 추가 및 스키마 작성
    
    * test: DB 연결 테스트 추가
    
    * feat(ReservationDao): 예약 조회 DB 연결 기능 구현
    
    * feat: 예약 추가, 예약 취소 DB 연결 기능 구현
    
    * fix: js파일 https status code 수정
    
    * test(ReservationTest): ReservationTest Https Status code 수정
    
    * refactor(JdbcReservationDao): SimpleJdbcInsert를 이용해 예약 추가하도록 변경
    
    * test(JdbcReservationDaoTest): 예약 추가, 삭제 test 분리
    
    * feat(schema.sql): reservation_time 테이블 생성 sql문 추가
    
    * feat: 시간 추가 API 기능 구현
    
    * feat: 시간 조회 API 기능 구현
    
    * feat: 시간 삭제 API 기능 구현
    
    * feat: 시간 관리 페이지 API 기능 구현
    
    * feat(schema.sql): DB schema 변경
    
    * feat: 시간 테이블에 저장된 값만 선택 기능 구현
    
    * refactor(ReservationService):  ReservationService 계층 분리
    
    * refactor(ReservationTimeService):  ReservationTimeService 계층 분리
    
    * refactor(ReservationTimeDao): ReservationTimeDao interface 분리
    
    * refactor: ResponseDto 추가
    
    * chore: 패키지 분리
    
    * refactor: DAO insert 객체 반환하도록 수정
    
    * refactor: SimpleJdbcInsert jdbc 주입하도록 수정
    
    * feat(ReservationTimeDao): ReservationTime 없을 시 예외처리
    
    * fix: SimpleJdbcInsert table명 매핑
    
    * style: 문자열 포맷팅
    
    * chore: admin 패키지 분리
    
    * style: 문자열 포맷팅
    
    * fix: Response 명세서에 맞게 수정
    
    * refactor: 각 레이어 naming 수정
    
    * refactor: RequestMapping을 통해 공통된 path 처리
    
    * refactor(JdbcReservationDao): BeanPropertySqlParameterSource를 이용하도록 수정
    
    * refactor: SimpleJdbcInsert명 변경
    
    * refactor(JdbcReservationTimeDao): 단일조회시, 값이 없는 경우 예외처리
    
    * chore: 주석제거
    
    * test: dao test 수정
    
    * chore: dao test 패키지 이동
    
    * test: service test 추가
    
    * test: test용 port 지정
    
    * test: test용 port 지정
    
    * refactor: domain dto 의존성 분리
    
    * test(AdminTest): 시간 추가 페이지 테스트 추가
    
    * test(ReservationServiceTest): 예약 삭제 테스트 수정
    
    * test(JdbcReservationDaoTest): JdbcReservationDaoTest 수정
    
    * test(JdbcReservationTimeDaoTest): JdbcReservationTimeDaoTest 수정
    
    * test: 저장된 데이터 변수명 수정
    
    feat: setup project
    
    feat: delete

---
+++ src/test/java/roomescape/reservation/ReservationTest.java
@@ -0,0 +1,91 @@
+package roomescape.reservation;
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import roomescape.reservation.dto.request.ReservationRequest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class ReservationTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @DisplayName("예약 추가 API 테스트")
+    @Test
+    void createReservation() {
+        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES(?)", LocalTime.of(10, 0));
+
+        ReservationRequest reservationRequest = new ReservationRequest("브라운", LocalDate.of(2023, 8, 5), 1L);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationRequest)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201)
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+    }
+
+    @DisplayName("예약 조회 API 테스트")
+    @Test
+    void getReservations() {
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @DisplayName("예약 취소 API 테스트")
+    @Test
+    void deleteReservation() {
+        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES(?)", LocalTime.of(10, 0));
+        ReservationRequest reservationRequest = new ReservationRequest("브라운", LocalDate.of(2023, 8, 5), 1L);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationRequest)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201)
+                .body("id", is(1));
+
+        RestAssured.given().log().all()
+                .when().delete("/reservations/1")
+                .then().log().all()
+                .statusCode(204);
+
+        RestAssured.given().log().all()
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+}
