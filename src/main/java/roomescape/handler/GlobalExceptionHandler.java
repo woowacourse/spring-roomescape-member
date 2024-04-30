2da8e00145b08ad8d8d95e3e191c7df921349a50
hyun <osh123chm@gmail.com>
2024-04-30 15:44:51 +0900

    feat: 표준 예외 처리 기능 구현

--- src/main/java/roomescape/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/handler/GlobalExceptionHandler.java
@@ -12,4 +12,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity handle(MethodArgumentNotValidException exception) {
         return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity handle(IllegalArgumentException exception) {
+        return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity handle(IllegalStateException exception) {
+        return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
