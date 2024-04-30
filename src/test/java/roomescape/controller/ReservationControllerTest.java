59e8ced8b3cc8adaff3aa1e047a260c4d3227238
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-04-30 17:32:04 +0900

    style(ReservationControllerTest): 사용하지 않는 import문 제거
    Co-authored-by: leegwichan <cnddkscndgus@naver.com>

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
-import roomescape.controller.ReservationController;
 import roomescape.domain.Reservation;
 import roomescape.dto.ReservationCreateRequest;
 
