76914b8793c782d6ad74dc39125e9ca3228dabb9
DobbyMac <kimdobby@g.skku.edu>
2024-04-30 20:46:06 +0900

    build: DB 테이블 스키마 업데이트

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,6 +1,7 @@
 SET REFERENTIAL_INTEGRITY FALSE;
 TRUNCATE TABLE reservation RESTART IDENTITY;
 TRUNCATE TABLE reservation_time RESTART IDENTITY;
+TRUNCATE TABLE theme RESTART IDENTITY;
 SET REFERENTIAL_INTEGRITY TRUE;
 
 INSERT INTO reservation_time (start_at)
@@ -8,7 +9,11 @@ VALUES ('15:40'),
        ('13:40'),
        ('17:40');
 
+INSERT INTO theme (name, description, thumbnail)
+VALUES ("polla", "폴라 방탈출", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"),
+        ("dobby", "도비 방탈출", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg");
+
 INSERT INTO reservation (name, date, time_id)
-VALUES ('폴라', '2023-08-05', 1),
-       ('구구', '2023-06-05', 2);
+VALUES ('폴라', '2023-08-05', 1, 1),
+       ('구구', '2023-06-05', 2, 2);
 
