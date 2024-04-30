b6fae1316f915599eabe8f75a2bf2206d570f283
DobbyMac <kimdobby@g.skku.edu>
2024-04-30 18:43:53 +0900

    feat(Time): 예약 시간 검증 기능 구현

--- src/main/java/roomescape/time/domain/Time.java
+++ src/main/java/roomescape/time/domain/Time.java
@@ -1,14 +1,20 @@
-package roomescape.Time.domain;
+package roomescape.time.domain;
 
 import java.time.LocalTime;
 import java.util.Objects;
+import roomescape.exception.BadRequestException;
 
 public class Time {
+
+    private static final LocalTime OPEN_TIME = LocalTime.of(8, 0);
+    private static final LocalTime CLOSE_TIME = LocalTime.of(23, 0);
+
     private long id;
     private final LocalTime startAt;
 
     public Time(LocalTime startAt) {
         this(0, startAt);
+        validation();
     }
 
     public Time(long id, LocalTime startAt) {
@@ -26,6 +32,15 @@ public class Time {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public void validation() {
+        if (startAt == null) {
+            throw new BadRequestException("시간 값이 정의되지 않은 요청입니다.");
+        }
+        if (OPEN_TIME.isBefore(startAt) || CLOSE_TIME.isAfter(startAt)) {
+            throw new BadRequestException("운영 시간 외의 예약 시간 요청입니다.");
+        }
     }
 
     @Override
---
+++ src/test/java/roomescape/time/domain/TimeTest.java
@@ -0,0 +1,39 @@
+package roomescape.time.domain;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import roomescape.exception.BadRequestException;
+
+public class TimeTest {
+
+    @Test
+    @DisplayName("시간이 null일 경우 예외가 발생한다.")
+    void validation_ShouldThrowException_WhenStartAtIsNull() {
+        assertAll(() -> {
+                    Throwable nullStartAt = assertThrows(
+                            BadRequestException.class, () -> new Time(null));
+                    assertEquals("시간 값이 정의되지 않은 요청입니다.", nullStartAt.getMessage());
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("운영 시간보다 빠르거나, 끝나는 시간보다 늦을 경우 예외가 발생한다.")
+    void validation_ShouldThrowException_WhenStartAtIsNotOpeningHour() {
+        assertAll(() -> {
+                    Throwable beforeOpenTime = assertThrows(
+                            BadRequestException.class, () -> new Time(LocalTime.of(7, 59)));
+                    assertEquals("운영 시간 외의 예약 시간 요청입니다.", beforeOpenTime.getMessage());
+
+                    Throwable afterCloseTime = assertThrows(
+                            BadRequestException.class, () -> new Time(LocalTime.of(23, 01)));
+                    assertEquals("운영 시간 외의 예약 시간 요청입니다.", afterCloseTime.getMessage());
+                }
+        );
+    }
+}
