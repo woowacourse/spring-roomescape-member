aacda373314b7f5418a6cabdbe1cfcccee722679
yoonseo choi <choiyoonseo02@gmail.com>
2024-04-30 17:32:49 +0900

    test: 시간 삭제 오류 테스트 추가
    Co-Authored-By: linirini <101927543+linirini@users.noreply.github.com>

--- src/test/java/roomescape/controller/ReservationTimeControllerTest.java
+++ src/test/java/roomescape/controller/ReservationTimeControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import roomescape.service.dto.ReservationRequest;
 import roomescape.service.dto.ReservationTimeRequest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -103,5 +104,29 @@ class ReservationTimeControllerTest {
                 .when().get("/times")
                 .then().log().all()
                 .assertThat().body("size()", is(0));
+    }
+
+    @DisplayName("시간 삭제 실패 테스트 - 이미 예약이 존재하는 시간 삭제 시도 오류")
+    @Test
+    void cannotDeleteReservationTime() {
+        //given
+        long id = (int) RestAssured.given().contentType(ContentType.JSON).body(new ReservationTimeRequest("10:00"))
+                .when().post("/times")
+                .then().extract().response().jsonPath().get("id");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new ReservationRequest("브라운", "2222-04-30", id))
+                .when().post("/reservations");
+
+        //when&then
+        RestAssured.given().log().all()
+                .when().delete("/times/" + id)
+                .then().log().all()
+                .assertThat().statusCode(400).body("message", is("해당 시간에 예약이 존재해서 삭제할 수 없습니다."));
+        RestAssured.given().log().all()
+                .when().get("/times")
+                .then().log().all()
+                .assertThat().body("size()", is(1));
     }
 }
