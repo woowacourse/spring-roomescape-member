bc523f5ab7f2419fc4d30c4aefa3fd61ce66c81c
ay-eonii <naayen@naver.com>
2024-04-30 19:36:35 +0900

    fix: 예외 별 메시지 출력하도록 수정

--- src/main/java/roomescape/exception/ExceptionControllerAdvice.java
+++ src/main/java/roomescape/exception/ExceptionControllerAdvice.java
@@ -15,14 +15,13 @@ public class ExceptionControllerAdvice {
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> IllegalArgumentException() {
-        return ResponseEntity.badRequest()
-                .body("[ERROR] 잘못된 접근입니다.");
+    public ResponseEntity<String> IllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> Exception() {
-        return ResponseEntity.internalServerError()
-                .body("[ERROR] 알 수 없는 오류가 발생했습니다.");
-    }
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<String> Exception() {
+//        return ResponseEntity.internalServerError()
+//                .body("[ERROR] 알 수 없는 오류가 발생했습니다.");
+//    }
 }
