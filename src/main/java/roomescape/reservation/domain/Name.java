f6fcea21008a2e7e16316e43f2c8cd48c2a5a305
hoyeonyy <highoption@naver.com>
2024-04-30 17:17:03 +0900

    feat : Name 클래스 생성 테스트 추가

--- src/main/java/roomescape/reservation/domain/Name.java
+++ src/main/java/roomescape/reservation/domain/Name.java
@@ -10,19 +10,19 @@ public class Name {
     }
 
     private void validate(String name) {
-        validateIsBlank(name);
         validateIsNull(name);
-    }
-
-    private void validateIsBlank(String name) {
-        if (name.isBlank()) {
-            throw new IllegalArgumentException("이름에는 빈 문자열, 공백을 입력할 수 없습니다.");
-        }
+        validateIsBlank(name);
     }
 
     private void validateIsNull(String name) {
         if (name == null) {
             throw new IllegalArgumentException("값을 입력하지 않았습니다.");
+        }
+    }
+
+    private void validateIsBlank(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름에는 빈 문자열, 공백을 입력할 수 없습니다.");
         }
     }
 
