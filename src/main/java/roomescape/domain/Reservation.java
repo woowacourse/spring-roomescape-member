3b88d839e9b3fe7b7f7a49369792daf5c120e511
takoyakimchi <c4nyou@gmail.com>
2024-04-30 17:11:25 +0900

    feat : 예약을 빈 이름으로 생성하면 예외가 발생하는 기능 추가
    Co-authored-by: jimi567 <repday0609@gmail.com>

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -14,10 +14,17 @@ public class Reservation {
     }
 
     public Reservation(Long id, String name, LocalDate date, ReservationTime time) {
+        validateName(name);
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    private static void validateName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("예약자명은 1글자 이상입니다.");
+        }
     }
 
     public Long getId() {
--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -1,11 +1,14 @@
 package roomescape.domain;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ReservationTest {
 
@@ -13,9 +16,17 @@ class ReservationTest {
     @Test
     void create() {
         assertThatCode(() -> new Reservation(1L, "wiib", LocalDate.now(), new ReservationTime(1L, LocalTime.now())))
-                .doesNotThrowAnyException();
+            .doesNotThrowAnyException();
         assertThatCode(() -> new Reservation("wiib", LocalDate.now(), new ReservationTime(1L, LocalTime.now())))
-                .doesNotThrowAnyException();
+            .doesNotThrowAnyException();
 
+    }
+
+    @DisplayName("빈 이름으로 생성하면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void create_WithBlankName(String name) {
+        assertThatThrownBy(() -> new Reservation(name, LocalDate.MAX, new ReservationTime(LocalTime.MAX)))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
