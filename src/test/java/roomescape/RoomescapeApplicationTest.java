220dac0f6a21c8d0618cab80a8bd5a0c25b980e5
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-04-30 14:49:22 +0900

    test(RoomescapeApplicationTest): 요구사항 테스트 추가

---
+++ src/test/java/roomescape/RoomescapeApplicationTest.java
@@ -0,0 +1,13 @@
+package roomescape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomescapeApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
