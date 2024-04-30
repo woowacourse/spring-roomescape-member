b4c80d5f410a222067525362e39a36e971f7cd82
3juhwan <13selfesteem91@naver.com>
2024-04-30 17:24:45 +0900

    feat: 잘못된 시간 형식 검증 기능 추가

---
+++ src/main/java/roomescape/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package roomescape;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/roomescape/controller/request/ReservationTimeRequest.java
+++ src/main/java/roomescape/controller/request/ReservationTimeRequest.java
@@ -3,14 +3,25 @@ package roomescape.controller.request;
 import roomescape.domain.ReservationTime;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 import java.util.Objects;
 
-public record ReservationTimeRequest(LocalTime startAt) {
+public record ReservationTimeRequest(String startAt) {
     public ReservationTimeRequest {
         Objects.requireNonNull(startAt);
+        validateReservationTime(startAt);
     }
 
+    private void validateReservationTime(String startAt) {
+        try {
+            LocalTime.parse(startAt);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("시간은 HH:mm 형식으로 입력해야 합니다. 입력한 값: " + startAt);
+        }
+    }
+
+
     public ReservationTime toEntity() {
-        return new ReservationTime(startAt);
+        return new ReservationTime(LocalTime.parse(startAt));
     }
 }
