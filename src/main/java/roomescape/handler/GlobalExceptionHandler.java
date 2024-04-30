061bb08ebec3f3c1297c3c7b9e6a76cc1b568038
hyun <osh123chm@gmail.com>
2024-04-30 15:18:04 +0900

    feat: dto 예외 처리 기능 구현

---
+++ src/main/java/roomescape/handler/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package roomescape.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handle(MethodArgumentNotValidException exception) {
+        return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
