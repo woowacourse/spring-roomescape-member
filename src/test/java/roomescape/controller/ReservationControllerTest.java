4d62ccaded7bd68654864ac3235b4bb291756be2
eunjungL <dldmswjd322@gmail.com>
2024-04-30 16:34:53 +0900

    fix(ReservationControllerTest): 테스트 시 항상 미래의 날자를 사용하도록 수정
    - createInitReservation() 오타 수정
    
    Co-authored-by: Hyunguk Ryu <hw0603@naver.com>

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,7 +31,7 @@ class ReservationControllerTest {
     @DisplayName("예약 컨트롤러는 예약 조회 시 값을 반환한다.")
     @Test
     void readReservations() {
-        createIntiReservation();
+        createInitReservation();
 
         RestAssured.given().log().all()
                 .when().get("/reservations")
@@ -44,7 +45,7 @@ class ReservationControllerTest {
     void createReservation() {
         Map<String, Object> reservation = new HashMap<>();
         reservation.put("name", "브라운");
-        reservation.put("date", "2023-08-05");
+        reservation.put("date", LocalDate.MAX.toString());
         reservation.put("timeId", 1);
 
         RestAssured.given().log().all()
@@ -65,7 +66,7 @@ class ReservationControllerTest {
     @DisplayName("예약 컨트롤러는 id 값에 따라 예약을 삭제한다.")
     @Test
     void deleteReservation() {
-        createIntiReservation();
+        createInitReservation();
 
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
@@ -79,10 +80,10 @@ class ReservationControllerTest {
                 .body("size()", is(0));
     }
 
-    private void createIntiReservation() {
+    private void createInitReservation() {
         Map<String, Object> reservation = new HashMap<>();
         reservation.put("name", "브라운");
-        reservation.put("date", "2023-08-05");
+        reservation.put("date", LocalDate.MAX.toString());
         reservation.put("timeId", 1);
 
         RestAssured.given().log().all()
