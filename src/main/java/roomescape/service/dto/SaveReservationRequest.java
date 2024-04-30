e30a8baae591c685464f4584fe4090f77175b542
KimSangHyeok <tkdgur0906@naver.com>
2024-04-30 15:26:53 +0900

    feat: 예약자명이 빈칸인지 검증
    Co-authored-by: nak-honest <e5slnh9876@gmail.com>

--- src/main/java/roomescape/service/dto/SaveReservationRequest.java
+++ src/main/java/roomescape/service/dto/SaveReservationRequest.java
@@ -7,7 +7,19 @@ import java.time.LocalDate;
 
 public record SaveReservationRequest(String name, LocalDate date, Long timeId) {
 
+    public SaveReservationRequest {
+        validateNameBlank(name);
+    }
+
+    private void validateNameBlank(String name) {
+        if(name.isBlank()) {
+            throw new IllegalArgumentException("이름은 빈칸일 수 없습니다.");
+        }
+    }
+
     public static Reservation toEntity(SaveReservationRequest request, ReservationTime reservationTime) {
         return new Reservation(request.name(), request.date(), reservationTime);
     }
+
+
 }
