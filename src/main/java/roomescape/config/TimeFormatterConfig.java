60e2f8896a2c04d220b47678d4ffab2a1298de90
robinjoon <robin980108@naver.com>
2024-04-30 19:13:17 +0900

    feat: ISO 8601 표준에 따른 YYYY-MM-dd 포맷에 해당하지 않는 날짜가 포함된 예약 생성 요청 시 에러 구현
    Co-authored-by: zangsu <zangsu_@naver.com>

--- src/main/java/roomescape/config/TimeFormatterConfig.java
+++ src/main/java/roomescape/config/TimeFormatterConfig.java
@@ -1,6 +1,8 @@
 package roomescape.config;
 
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import java.time.format.DateTimeFormatter;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
@@ -9,11 +11,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TimeFormatterConfig {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer localTimeSerializerCustomizer() {
-        return builder -> builder.serializers(new LocalTimeSerializer(FORMATTER))
-                .deserializers(new LocalTimeDeserializer(FORMATTER));
+        return builder -> builder.serializers(new LocalTimeSerializer(TIME_FORMATTER),
+                        new LocalDateSerializer(DATE_FORMATTER))
+                .deserializers(new LocalTimeDeserializer(TIME_FORMATTER), new LocalDateDeserializer(DATE_FORMATTER));
     }
 }
