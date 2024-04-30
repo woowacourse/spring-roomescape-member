0c5681a888beac5ffb986c90c02e689ca037acc8
kelly6bf <kelly6bf@gmail.com>
2024-04-30 19:59:42 +0900

    feat: ThemeResponse 객체 정의 및 ReservationResponse에 적용

--- src/main/java/roomescape/dto/ReservationResponse.java
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -4,13 +4,20 @@ import roomescape.domain.Reservation;
 
 import java.time.LocalDate;
 
-public record ReservationResponse(Long id, String name, LocalDate date, ReservationTimeResponse time) {
+public record ReservationResponse(
+        Long id,
+        String name,
+        LocalDate date,
+        ReservationTimeResponse time,
+        ThemeResponse theme
+) {
     public static ReservationResponse from(final Reservation reservation) {
         return new ReservationResponse(
                 reservation.getId(),
                 reservation.getClientName().getValue(),
                 reservation.getDate(),
-                ReservationTimeResponse.from(reservation.getTime())
+                ReservationTimeResponse.from(reservation.getTime()),
+                ThemeResponse.from(reservation.getTheme())
         );
     }
 }
