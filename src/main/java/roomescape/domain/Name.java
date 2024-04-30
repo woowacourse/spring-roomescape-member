7f3f5f0aad963a2bd802cd9b404791e04d01cd20
donghoony <aru0504@naver.com>
2024-04-30 19:47:45 +0900

    feat: 이름 제약조건 검증
    Co-authored-by: kunsanglee <gosmdochee2@gmail.com>

--- src/main/java/roomescape/domain/Name.java
+++ src/main/java/roomescape/domain/Name.java
@@ -3,16 +3,24 @@ package roomescape.domain;
 import java.util.Objects;
 
 public class Name {
+    public static final int NAME_MAX_LENGTH = 20;
     private final String name;
 
     public Name(String name) {
         validateNonBlank(name);
+        validateLength(name);
         this.name = name;
     }
 
     private void validateNonBlank(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("예약자명은 필수 입력값 입니다.");
+        }
+    }
+
+    private void validateLength(String name) {
+        if (name != null && name.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(String.format("예약자명은 %d자 이하여야 합니다.", NAME_MAX_LENGTH));
         }
     }
 
