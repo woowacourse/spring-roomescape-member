f49cf59cb32659dcedd2787c366eec53a31c0fd7
Arachneee <jhg2819@naver.com>
2024-04-30 19:06:10 +0900

    feat: theme 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,12 @@
+CREATE TABLE theme
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    thumbnail VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE reservation_times
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
@@ -11,6 +20,8 @@ CREATE TABLE reservations
     name    VARCHAR(255) NOT NULL,
     date    VARCHAR(255) NOT NULL,
     time_id BIGINT       NOT NULL,
+    theme_id BIGINT      NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_times (id)
+    FOREIGN KEY (time_id) REFERENCES reservation_times (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
