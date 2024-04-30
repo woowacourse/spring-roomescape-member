5c9f845cc8d5c9f653766736ab1021fd19e156bc
Choo <choo000407@naver.com>
2024-04-30 15:41:06 +0900

    feat: exeption handler를 추가한다

---
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package roomescape.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<String> handleException() {
+        return new ResponseEntity<>("서버 에러입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
