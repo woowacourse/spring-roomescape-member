4a3c4bca0957522e3318b5a78e6ac0dc6ea1dba4
KimSangHyeok <tkdgur0906@naver.com>
2024-04-30 16:25:28 +0900

    test: 정상적인 이름에 대한 테스트 코드 추가
    Co-authored-by: nak-honest <e5slnh9876@gmail.com>

--- src/test/java/roomescape/service/dto/SaveReservationRequestTest.java
+++ src/test/java/roomescape/service/dto/SaveReservationRequestTest.java
@@ -1,19 +1,28 @@
 package roomescape.service.dto;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDate;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class SaveReservationRequestTest {
 
+    @Test
+    @DisplayName("이름이 정상 입력될 경우 성공한다.")
+    void checkNameBlank_Success() {
+        assertThatCode(() -> new SaveReservationRequest("capy", LocalDate.now(), 1L))
+                .doesNotThrowAnyException();
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"", " ", "  "})
     @DisplayName("이름이 빈칸인 경우 예외가 발생한다.")
-    void nameBlank(String name) {
+    void checkNameBlank_Failure(String name) {
         assertThatThrownBy(() -> new SaveReservationRequest(name, LocalDate.now(), 1L))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 빈칸일 수 없습니다.");
