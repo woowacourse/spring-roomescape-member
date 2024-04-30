84c13ce9efe1ca50bdec437bf088df094df00f04
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-04-30 16:34:39 +0900

    feat(CreateReservationTimeRequest): 시간 생성 요청 시 시간이 empty인 경우 예외처리
    Co-authored-by: seunghye218 <seunghye@student.42seoul.kr>

--- src/main/java/roomescape/reservationtime/dto/request/CreateReservationTimeRequest.java
+++ src/main/java/roomescape/reservationtime/dto/request/CreateReservationTimeRequest.java
@@ -3,7 +3,15 @@ package roomescape.reservationtime.dto.request;
 import java.time.LocalTime;
 import roomescape.reservationtime.model.ReservationTime;
 
+// TODO: 날짜 범위나, 형식에 맞지 않을 경우 HttpMessageNotReadableException 발생, 자동 400에러 응답
 public record CreateReservationTimeRequest(LocalTime startAt) {
+    public CreateReservationTimeRequest(final LocalTime startAt) {
+        if (startAt == null) {
+            throw new IllegalArgumentException("올바른 시간을 입력해주세요.");
+        }
+        this.startAt = startAt;
+    }
+
     public ReservationTime toReservationTime() {
         return new ReservationTime(
                 null,
