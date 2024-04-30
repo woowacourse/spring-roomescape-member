70b329954c1e468676de439e2998eb306c1774b1
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-04-30 16:52:56 +0900

    refactor: 코드 포맷팅
    Co-Authored-By: gmuz1c <86762272+shin-jisong@users.noreply.github.com>

--- src/main/java/roomescape/reservation/dto/ReservationRequest.java
+++ src/main/java/roomescape/reservation/dto/ReservationRequest.java
@@ -4,10 +4,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-public record ReservationRequest(@NotBlank String name,
-                                 @NotBlank
-                                 @Pattern(regexp = "^(?:(?:19|20)\\d{2})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1])$")
-                                 String date,
-                                 @NotNull
-                                 long timeId) {
+public record ReservationRequest(
+        @NotBlank
+        String name,
+
+        @NotBlank
+        @Pattern(regexp = "^(?:(?:19|20)\\d{2})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1])$")
+        String date,
+
+        @NotNull
+        long timeId
+) {
 }
--- src/main/java/roomescape/reservation/dto/ReservationTimeRequest.java
+++ src/main/java/roomescape/reservation/dto/ReservationTimeRequest.java
@@ -6,6 +6,6 @@ import jakarta.validation.constraints.Pattern;
 public record ReservationTimeRequest(
         @NotBlank
         @Pattern(regexp = "^(?:[01]\\d|2[0-4]):(?:[0-5]\\d|60)$")
-        String startAt) {
-
+        String startAt
+) {
 }
