a82bb2a2f604cec0d8ac7603119b413372f33422
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-04-30 20:25:32 +0900

    fix: 에약 테마 정보 반환
    Co-Authored-By: gmuz1c <86762272+shin-jisong@users.noreply.github.com>

--- src/main/java/roomescape/reservation/dto/ReservationResponse.java
+++ src/main/java/roomescape/reservation/dto/ReservationResponse.java
@@ -3,13 +3,19 @@ package roomescape.reservation.dto;
 import java.time.LocalDate;
 import roomescape.reservation.domain.Reservation;
 
-public record ReservationResponse(long id, String name, LocalDate date, ReservationTimeResponse time) {
+public record ReservationResponse(
+        long id,
+        String name,
+        LocalDate date,
+        ReservationTimeResponse time,
+        ThemeResponse theme) {
     public static ReservationResponse from(Reservation reservation) {
         return new ReservationResponse(
                 reservation.getId(),
                 reservation.getName(),
                 reservation.getDate(),
-                ReservationTimeResponse.from(reservation.getTime())
+                ReservationTimeResponse.from(reservation.getTime()),
+                ThemeResponse.from(reservation.getTheme())
         );
     }
 }
