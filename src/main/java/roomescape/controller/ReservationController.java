4bb34cc640d3906cf2a2f6237393e14fd3686a97
mzeong <mjeong@sookmyung.ac.kr>
2024-04-30 13:52:41 +0900

    feat: migrate mission code

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,46 @@
+package roomescape.controller;
+
+import java.net.URI;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.controller.dto.ReservationRequest;
+import roomescape.controller.dto.ReservationResponse;
+import roomescape.service.ReservationService;
+import roomescape.service.dto.ReservationOutput;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationResponse> createReservation(@RequestBody ReservationRequest request) {
+        ReservationOutput output = reservationService.createReservation(request.toInput());
+        return ResponseEntity.created(URI.create("/reservations/" + output.id()))
+                .body(ReservationResponse.toResponse(output));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReservationResponse>> getAllReservations() {
+        List<ReservationOutput> outputs = reservationService.getAllReservations();
+        return ResponseEntity.ok(ReservationResponse.toResponses(outputs));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable long id) {
+        reservationService.deleteReservation(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/roomescape/service/ReservationService.java
@@ -0,0 +1,38 @@
+package roomescape.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import roomescape.dao.ReservationDao;
+import roomescape.dao.ReservationTimeDao;
+import roomescape.domain.Reservation;
+import roomescape.domain.ReservationTime;
+import roomescape.service.dto.ReservationInput;
+import roomescape.service.dto.ReservationOutput;
+
+@Service
+public class ReservationService {
+
+    private final ReservationDao reservationDao;
+    private final ReservationTimeDao reservationTimeDao;
+
+    public ReservationService(ReservationDao reservationDao, ReservationTimeDao reservationTimeDao) {
+        this.reservationDao = reservationDao;
+        this.reservationTimeDao = reservationTimeDao;
+    }
+
+    public ReservationOutput createReservation(ReservationInput input) {
+        ReservationTime time = reservationTimeDao.find(input.timeId());
+        Reservation reservation = input.toReservation(time);
+        Reservation savedReservation = reservationDao.create(reservation);
+        return ReservationOutput.toOutput(savedReservation);
+    }
+
+    public List<ReservationOutput> getAllReservations() {
+        List<Reservation> reservations = reservationDao.getAll();
+        return ReservationOutput.toOutputs(reservations);
+    }
+
+    public void deleteReservation(long id) {
+        reservationDao.delete(id);
+    }
+}
