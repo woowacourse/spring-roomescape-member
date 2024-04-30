a6d856622a19aa38603572c96f36131144b85464
BurningFalls <sherlocky31415@gmail.com>
2024-04-30 20:31:45 +0900

    feat: 전역 예외 핸들러 구현
    Co-authored-by: YoonJuHo <dbswn990@naver.com>

---
+++ src/main/java/roomescape/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package roomescape.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.format.DateTimeParseException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = DateTimeParseException.class)
+    public ResponseEntity<String> handleDateTimeParse(DateTimeParseException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
