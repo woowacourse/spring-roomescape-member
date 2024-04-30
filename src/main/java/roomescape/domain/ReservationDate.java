47771ebce8221d9f102452c05a2e9a46a03a9890
mzeong <mjeong@sookmyung.ac.kr>
2024-04-30 15:19:10 +0900

    feat(ReservationDate): 도메인 생성 기능 구현
    - 이에 따른 테스트 코드 추가

---
+++ src/main/java/roomescape/domain/ReservationDate.java
@@ -0,0 +1,19 @@
+package roomescape.domain;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+public record ReservationDate(LocalDate date) {
+
+    public static ReservationDate from(String date) {
+        try {
+            return new ReservationDate(LocalDate.parse(date));
+        } catch (DateTimeException exception) {
+            throw new IllegalArgumentException(String.format("%s 는 유효하지 않은 값입니다.(EX: 10:00)", date));
+        }
+    }
+
+    public String asString() {
+        return date.toString();
+    }
+}
