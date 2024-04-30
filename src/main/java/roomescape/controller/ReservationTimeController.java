87951478164ce27f022af8e621b08332c062764a
yechop <channetattoo@gmail.com>
2024-04-30 14:58:25 +0900

    refactor: 시간 관리 API가 응답하도록 변경

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -6,10 +6,12 @@ import roomescape.domain.ReservationTime;
 import roomescape.dto.ReservationTimeRequest;
 import roomescape.service.ReservationTimeService;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 public class ReservationTimeController {
+
     private final ReservationTimeService reservationTimeService;
 
     public ReservationTimeController(final ReservationTimeService reservationTimeService) {
@@ -24,12 +26,12 @@ public class ReservationTimeController {
     @PostMapping("/times")
     public ResponseEntity<ReservationTime> create(@RequestBody ReservationTimeRequest reservationTimeRequest) {
         final ReservationTime savedReservationTime = reservationTimeService.save(reservationTimeRequest);
-        return ResponseEntity.ok(savedReservationTime);
+        return ResponseEntity.created(URI.create("/times/" + savedReservationTime.getId())).body(savedReservationTime);
     }
 
     @DeleteMapping("/times/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
         reservationTimeService.delete(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
