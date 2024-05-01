3c1d8f6c4b798e81faf88ac61a4e1cba2d70d891
ehtjsv2 <ehtjsv2@naver.com>
2024-05-01 18:09:17 +0900

    feat: ThemeController 추가와 삭제기능 구현

--- src/main/java/roomescape/service/ThemeAddRequest.java
+++ src/main/java/roomescape/service/ThemeAddRequest.java
@@ -11,6 +11,13 @@ public class ThemeAddRequest {
 
     }
 
+    public ThemeAddRequest(String name, String description, String thumbnail) {
+        this.name = name;
+        this.description = description;
+        this.thumbnail = thumbnail;
+    }
+
+
     public Theme toEntity() {
         return new Theme(null, name, description, thumbnail);
     }
--- src/test/java/roomescape/controller/ThemeControllerTest.java
+++ src/test/java/roomescape/controller/ThemeControllerTest.java
@@ -3,6 +3,7 @@ package roomescape.controller;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import java.time.LocalTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import roomescape.dto.ReservationTimeAddRequest;
+import roomescape.service.ThemeAddRequest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -33,13 +35,33 @@ class ThemeControllerTest {
 
     @DisplayName("전체 테마를 조회할 수 있다 (200 OK)")
     @Test
-    void should_get_reservation_time_list_in_db() {
-        ReservationTimeAddRequest reservationTimeAddRequest = new ReservationTimeAddRequest(LocalTime.of(10, 0));
-
+    void should_get_theme_list() {
         RestAssured.given().log().all()
                 .when().get("/themes")
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
+    }
+
+    @DisplayName("테마를 추가할 수 있다 (201 created)")
+    @Test
+    void should_add_theme() {
+        ThemeAddRequest themeAddRequest = new ThemeAddRequest("도도", "배고픔", "url");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(themeAddRequest)
+                .when().post("/themes")
+                .then().log().all()
+                .statusCode(201);
+    }
+
+    @DisplayName("테마를 삭제할 수 있다 (204 no content)")
+    @Test
+    void should_remove_theme() {
+        RestAssured.given().log().all()
+                .when().delete("/themes/1")
+                .then().log().all()
+                .statusCode(204);
     }
 }
