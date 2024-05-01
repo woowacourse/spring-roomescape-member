40e244b65fcbf314c9cad981617c42f65b2e50fd
donghoony <aru0504@naver.com>
2024-05-02 00:43:53 +0900

    refactor: DDL `if not exists` 추가
    Co-authored-by: kunsanglee <gosmdochee2@gmail.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-create table theme
+create table if not exists theme
 (
     id          bigint       not null auto_increment,
     name        varchar(255) not null,
@@ -7,14 +7,14 @@ create table theme
     primary key (id)
 );
 
-create table reservation_time
+create table if not exists reservation_time
 (
     id       bigint not null auto_increment,
     start_at time   not null,
     primary key (id)
 );
 
-create table reservation
+create table if not exists reservation
 (
     id       bigint       not null auto_increment,
     name     varchar(255) not null,
@@ -25,6 +25,3 @@ create table reservation
     foreign key (time_id) references reservation_time (id),
     foreign key (theme_id) references theme (id)
 );
---
--- insert into theme (name, description, thumbnail) values ('Theme 1', 'Description 1', 'thumbnail1.jpg');
--- insert into reservation_time (start_at) values ('10:00:00');
