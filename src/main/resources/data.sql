ec44866abf187b50be2418a0325f15175a93e3d6
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-01 20:11:04 +0900

    refactor(data.sql): id 값 제외

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,16 @@
-INSERT INTO theme (id, name, description, thumbnail) VALUES
-(1, '테마1', '테마1 설명 테마1 설명', 'thumbnail1.jpg'),
-(2, '테마2', '테마2 설명 테마2 설명', 'thumbnail2.jpg'),
-(3, '테마3', '테마3 설명 테마3 설명', 'thumbnail3.jpg');
+INSERT INTO theme (name, description, thumbnail) VALUES
+('테마1', '테마1 설명 테마1 설명', 'thumbnail1.jpg'),
+('테마2', '테마2 설명 테마2 설명', 'thumbnail2.jpg'),
+('테마3', '테마3 설명 테마3 설명', 'thumbnail3.jpg');
 
 
-INSERT INTO reservation_time (id, start_at) VALUES
-(1,'10:00'),
-(2,'12:00'),
-(3,'14:00');
+INSERT INTO reservation_time (start_at) VALUES
+('10:00'),
+('12:00'),
+('14:00');
 
-INSERT INTO reservation (id, name, date, time_id, theme_id) VALUES
-(1,'테드', '2024-04-27', 1, 1),
-(2,'종이', '2024-04-30', 2, 2),
-(3,'범블비', '2024-05-04', 3, 2),
-(4,'제이', '2024-05-05', 3, 2);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES
+(`'테드', '2024-04-27', 1, 1),
+(`'종이', '2024-04-30', 2, 2),
+(`'범블비', '2024-05-04', 3, 2),
+(`'제이', '2024-05-05', 3, 2);
