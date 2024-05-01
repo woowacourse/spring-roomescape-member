9167b477a6c9405a958a373ed854534397920f59
libienz <leekh10195@gmail.com>
2024-05-01 17:38:02 +0900

    feat: ThemeService 전체 테마 조회 기능 구현

---
+++ src/main/java/roomescape/service/ThemeService.java
@@ -0,0 +1,20 @@
+package roomescape.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import roomescape.domain.Theme;
+import roomescape.repository.ThemeDao;
+
+@Service
+public class ThemeService {
+
+    private final ThemeDao themeDao;
+
+    public ThemeService(ThemeDao themeDao) {
+        this.themeDao = themeDao;
+    }
+
+    public List<Theme> findAllTheme() {
+        return themeDao.findAll();
+    }
+}
