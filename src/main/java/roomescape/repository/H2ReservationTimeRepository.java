0e9d8ad0522efa933c4a2fe45dfe585c346a98d7
3juhwan <13selfesteem91@naver.com>
2024-05-01 19:55:47 +0900

    refactor: 리포지토리 rowMapper 분리, sql 분리

--- src/main/java/roomescape/repository/H2ReservationTimeRepository.java
+++ src/main/java/roomescape/repository/H2ReservationTimeRepository.java
@@ -1,21 +1,26 @@
 package roomescape.repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.ReservationTime;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class H2ReservationTimeRepository implements ReservationTimeRepository {
+    private final ReservationTimeRowMapper rowMapper;
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert jdbcInsert;
 
     public H2ReservationTimeRepository(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+        this.rowMapper = new ReservationTimeRowMapper();
         this.jdbcTemplate = jdbcTemplate;
         this.jdbcInsert = new SimpleJdbcInsert(dataSource)
                 .withTableName("reservation_time")
@@ -23,7 +28,8 @@ public class H2ReservationTimeRepository implements ReservationTimeRepository {
     }
 
     public ReservationTime save(ReservationTime reservationTime) {
-        long reservationTimeId = jdbcInsert.executeAndReturnKey(Map.of("start_at", reservationTime.getStartAt()))
+        long reservationTimeId = jdbcInsert.executeAndReturnKey(Map.of(
+                        "start_at", reservationTime.getStartAt()))
                 .longValue();
 
         return new ReservationTime(
@@ -34,24 +40,27 @@ public class H2ReservationTimeRepository implements ReservationTimeRepository {
     public List<ReservationTime> findAll() {
         String sql = "select * from reservation_time";
 
-        return jdbcTemplate.query(sql, (resultSet, rowNum) -> new ReservationTime(
-                resultSet.getLong("id"),
-                resultSet.getTime("start_at").toLocalTime()));
+        return jdbcTemplate.query(sql, rowMapper);
     }
 
-    @Override
     public Optional<ReservationTime> findById(Long id) {
         String sql = "select * from reservation_time where id = ?";
-        List<ReservationTime> reservationTimes = jdbcTemplate.query(sql, (resultSet, rowNum) -> new ReservationTime(
-                resultSet.getLong("id"),
-                resultSet.getTime("start_at").toLocalTime()), id);
+        List<ReservationTime> reservationTimes = jdbcTemplate.query(sql, rowMapper, id);
 
         return reservationTimes.stream().findFirst();
     }
 
-    @Override
     public void deleteById(long id) {
         String sql = "delete from reservation_time where id = ?";
         jdbcTemplate.update(sql, id);
+    }
+
+    static class ReservationTimeRowMapper implements RowMapper<ReservationTime> {
+        @Override
+        public ReservationTime mapRow(final ResultSet rs, final int rowNum) throws SQLException {
+            return new ReservationTime(
+                    rs.getLong("id"),
+                    rs.getTime("start_at").toLocalTime());
+        }
     }
 }
--- src/main/java/roomescape/repository/ThemeRepository.java
+++ src/main/java/roomescape/repository/ThemeRepository.java
@@ -1,61 +1,16 @@
 package roomescape.repository;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.stereotype.Repository;
 import roomescape.domain.Theme;
 
-import javax.sql.DataSource;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-@Repository
-public class ThemeRepository {
-    private final JdbcTemplate jdbcTemplate;
-    private final SimpleJdbcInsert jdbcInsert;
+public interface ThemeRepository {
+    List<Theme> findAll();
 
-    public ThemeRepository(JdbcTemplate jdbcTemplate, DataSource dataSource) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.jdbcInsert = new SimpleJdbcInsert(dataSource)
-                .withTableName("theme")
-                .usingGeneratedKeyColumns("id");
-    }
+    Optional<Theme> findById(long id);
 
-    public List<Theme> findAll() {
-        return jdbcTemplate.query("select * from theme", (rs, rowNum) -> new Theme(
-                rs.getLong("id"),
-                rs.getString("name"),
-                rs.getString("description"),
-                rs.getString("thumbnail")
-        ));
-    }
+    Theme save(Theme theme);
 
-    public Optional<Theme> findById(long id) {
-        String sql = "select * from theme where id = ?";
-
-        return Optional.ofNullable(jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new Theme(
-                rs.getLong("id"),
-                rs.getString("name"),
-                rs.getString("description"),
-                rs.getString("thumbnail")
-        ), id));
-    }
-
-    public Theme save(Theme theme) {
-        long reservationTimeId = jdbcInsert.executeAndReturnKey(Map.of(
-                        "name", theme.getName(), "description", theme.getDescription(), "thumbnail", theme.getThumbnail()))
-                .longValue();
-
-        return new Theme(
-                reservationTimeId,
-                theme.getName(),
-                theme.getDescription(),
-                theme.getThumbnail());
-    }
-
-    public void deleteById(long id) {
-        String sql = "delete from theme where id = ?";
-        jdbcTemplate.update(sql, id);
-    }
+    void deleteById(long id);
 }
