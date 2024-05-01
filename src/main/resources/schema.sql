71867786bcd416c2d56dc690a46e922d60620337
masonkimseoul <masonkimseoul@gmail.com>
2024-05-01 15:30:49 +0900

    refactor : DB 스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,16 @@ CREATE TABLE reservation_time
     id       BIGINT NOT NULL AUTO_INCREMENT,
     start_at TIME   NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE(start_at)
+    UNIQUE (start_at)
+);
+
+CREATE TABLE theme
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    thumbnail   VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation
@@ -12,7 +21,9 @@ CREATE TABLE reservation
     name    VARCHAR(15) NOT NULL,
     date    DATE        NOT NULL,
     time_id BIGINT,
+    theme_id BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    UNIQUE(date, time_id)
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    UNIQUE (date, time_id, theme_id)
 );
