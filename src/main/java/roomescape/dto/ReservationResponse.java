88297e6385b6755b343803c500a4ac26ffb9a597
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-01 18:26:37 +0900

    fix(ReservationResponse): 예약 응답에 테마 추가
    Co-authored-by: leegwichan <cnddkscndgus@naver.com>

--- src/main/java/roomescape/dto/ReservationResponse.java
+++ src/main/java/roomescape/dto/ReservationResponse.java
@@ -2,13 +2,14 @@ package roomescape.dto;
 
 import roomescape.domain.Reservation;
 
-public record ReservationResponse(Long id, String name, String date, TimeResponse time) {
+public record ReservationResponse(Long id, String name, String date, TimeResponse time, ThemeResponse theme) {
 
     public static ReservationResponse from(Reservation reservation) {
         return new ReservationResponse(
                 reservation.getId(),
                 reservation.getName(),
                 reservation.getDate(),
-                TimeResponse.from(reservation.getTime()));
+                TimeResponse.from(reservation.getTime()),
+                ThemeResponse.from(reservation.getTheme()));
     }
 }
