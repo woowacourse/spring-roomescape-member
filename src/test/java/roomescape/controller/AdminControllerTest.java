fc5560d6e95d59f91cfa7a2152199dc0f1771c5f
mzeong <mjeong@sookmyung.ac.kr>
2024-05-01 13:40:16 +0900

    feat(AdminController): admin/theme 페이지 응답 기능 구현
    - 기능 완료에 따른 요구사항 체크

---
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -0,0 +1,33 @@
+package roomescape.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class AdminControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    @DisplayName("/admin/theme 요청시 테마 관리 페이지를 응답한다.")
+    void response_theme_page() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .when().get("/admin/theme")
+                .then()
+                .statusCode(200);
+    }
+
+}
