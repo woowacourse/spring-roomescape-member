c4686fdd0447a72012702f48540a32e747e76b96
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-01 17:10:27 +0900

    feat(Theme): id를 제외한 인자 중 null이 있으면 예외를 던진다
    Co-authored-by: leegwichan <cnddkscndgus@naver.com>

--- src/main/java/roomescape/domain/Theme.java
+++ src/main/java/roomescape/domain/Theme.java
@@ -1,5 +1,7 @@
 package roomescape.domain;
 
+import java.util.Objects;
+
 public class Theme {
     private final Long id;
     private final String name;
@@ -7,17 +9,19 @@ public class Theme {
     private final String thumbnail;
 
     public Theme(Long id, String name, String description, String thumbnail) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.thumbnail = thumbnail;
+        final String errorMessage = "인자 중 null 값이 존재합니다.";
+        this.id = Objects.requireNonNull(id, errorMessage);
+        this.name = Objects.requireNonNull(name, errorMessage);
+        this.description = Objects.requireNonNull(description, errorMessage);
+        this.thumbnail = Objects.requireNonNull(thumbnail, errorMessage);
     }
 
     public Theme(String name, String description, String thumbnail) {
+        final String errorMessage = "인자 중 null 값이 존재합니다.";
         this.id = null;
-        this.name = name;
-        this.description = description;
-        this.thumbnail = thumbnail;
+        this.name = Objects.requireNonNull(name, errorMessage);
+        this.description = Objects.requireNonNull(description, errorMessage);
+        this.thumbnail = Objects.requireNonNull(thumbnail, errorMessage);
     }
 
     public Theme changeId(Long id) {
