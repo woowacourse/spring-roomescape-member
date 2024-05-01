87615ef169ee9b467c69b8107119e186e193c086
masonkimseoul <masonkimseoul@gmail.com>
2024-05-01 12:56:59 +0900

    test : 누락된 UserName 테스트 추가
    예약자명 길이, 형식 테스트

--- src/main/java/roomescape/domain/UserName.java
+++ src/main/java/roomescape/domain/UserName.java
@@ -2,7 +2,7 @@ package roomescape.domain;
 
 public class UserName {
 
-    private static final String NAME_FORMAT = "^[a-zA-Z가-힣0-9]*$";
+    private static final String NAME_FORMAT = "^[a-zA-Z가-힣ㄱ-ㅎ0-9]*$";
 
     public UserName(String userName) {
         validate(userName);
@@ -12,7 +12,7 @@ public class UserName {
     private final String userName;
 
     private void validate(String userName) {
-        if (userName.isEmpty() || userName.length() > 20) {
+        if (userName.isBlank() || userName.length() > 20) {
             throw new IllegalArgumentException("예약자명은 1글자 이상, 20글자 이하로 작성해야 합니다.");
         }
         if (!userName.matches(NAME_FORMAT)) {
