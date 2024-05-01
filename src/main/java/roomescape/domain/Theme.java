bfa43819f0ec06c51fb28f9fd464d837850a11a5
libienz <leekh10195@gmail.com>
2024-05-01 17:03:19 +0900

    feat: Theme 클래스 추가

---
+++ src/main/java/roomescape/domain/Theme.java
@@ -0,0 +1,52 @@
+package roomescape.domain;
+
+import java.util.Objects;
+
+public class Theme {
+    private final Long id;
+    private final String name;
+    private final String description;
+    private final String thumbnail;
+
+    public Theme(Long id, String name, String description, String thumbnail) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.thumbnail = thumbnail;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Theme theme = (Theme) o;
+        return Objects.equals(id, theme.id) && Objects.equals(name, theme.name)
+                && Objects.equals(description, theme.description) && Objects.equals(thumbnail,
+                theme.thumbnail);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, thumbnail);
+    }
+}
