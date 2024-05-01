690bf1d458a8cf45ab2a8940daae4c73ee899828
hoyeonyy <highoption@naver.com>
2024-05-01 16:16:33 +0900

    feat : 예외 컨트롤러 추가

---
+++ src/main/java/roomescape/exception/ExceptionApiController.java
@@ -0,0 +1,15 @@
+package roomescape.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(annotations = RestController.class)
+public class ExceptionApiController {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ExceptionInfo> IllegalArgExHandler(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest().body(new ExceptionInfo(exception.getMessage()));
+    }
+}
