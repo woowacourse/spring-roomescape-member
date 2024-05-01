58604252270ce0abd3b571dda866307b00715828
fram1998 <koust6u@pusan.ac.kr>
2024-05-01 18:52:08 +0900

    test: 테마를 추가할 수 있다

--- src/test/java/roomescape/integration/ThemeIntegrationTest.java
+++ src/test/java/roomescape/integration/ThemeIntegrationTest.java
@@ -3,9 +3,12 @@ package roomescape.integration;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
-public class ThemeIntegrationTest extends IntegrationTest {
+class ThemeIntegrationTest extends IntegrationTest {
     @Test
     void 테마_목록을_조회할_수_있다() {
         RestAssured.given().log().all()
@@ -13,5 +16,37 @@ public class ThemeIntegrationTest extends IntegrationTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(1));
+    }
+
+    @Test
+    void 테마를_추가할_수_있다() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "레벨3");
+        params.put("description", "내용이다.");
+        params.put("thumbnail", "https://naver.com");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/themes")
+                .then().log().all()
+                .statusCode(201)
+                .header("Location", "/themes/2")
+                .body("id", is(2));
+    }
+
+    @Test
+    void 필드가_빈_값이면_테마를_추가할_수_없다() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "레벨3");
+        params.put("description", null);
+        params.put("thumbnail", "https://naver.com");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/themes")
+                .then().log().all()
+                .statusCode(400);
     }
 }
