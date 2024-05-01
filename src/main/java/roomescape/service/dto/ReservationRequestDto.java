0915bd44caaa8d5e347128144a61cbdb4f593553
Seokmyung Ham <gkatjraud1@gmail.com>
2024-05-01 17:25:03 +0900

    feat(ReservationRequestDto): 잘못된 입력 검증
    - 이름이 입력되지 않은 경우
    - 날짜가 입력되지 않은 경우
    - 날짜의 형식이 잘못된 경우
    - 시간 아이디가 입력되지 않은 경우
    - 시간 아이디가 자연수가 아닌 경우
    
    Co-authored-by: hyxrxn <gpfls981220@gmail.com>

--- src/main/java/roomescape/service/dto/ReservationRequestDto.java
+++ src/main/java/roomescape/service/dto/ReservationRequestDto.java
@@ -1,6 +1,8 @@
 package roomescape.service.dto;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 import roomescape.domain.Name;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationDate;
@@ -10,12 +12,49 @@ public class ReservationRequestDto {
 
     private final String name;
     private final String date;
-    private final long timeId;
+    private final Long timeId;
 
-    public ReservationRequestDto(String name, String date, long timeId) {
+    public ReservationRequestDto(String name, String date, Long timeId) {
+        validateNameExist(name);
+        validateDateExist(date);
+        validateDateFormat(date);
+        validateTimeIdExist(timeId);
+        validateTimeIdNaturalNumber(timeId);
         this.name = name;
         this.date = date;
         this.timeId = timeId;
+    }
+
+    private void validateNameExist(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 반드시 입력되어야 합니다.");
+        }
+    }
+
+    private void validateDateExist(String date) {
+        if (date == null || date.isBlank()) {
+            throw new IllegalArgumentException("날짜는 반드시 입력되어야 합니다.");
+        }
+    }
+
+    private void validateDateFormat(String date) {
+        try {
+            LocalDate.parse(date);
+        } catch (DateTimeParseException ex) {
+            throw new IllegalArgumentException("날짜 형식이 올바르지 않습니다.");
+        }
+    }
+
+    private void validateTimeIdExist(Long timeId) {
+        if (timeId == null) {
+            throw new IllegalArgumentException("시간 아이디는 반드시 입력되어야 합니다.");
+        }
+    }
+
+    private void validateTimeIdNaturalNumber(Long timeId) {
+        if (timeId <= 0) {
+            throw new IllegalArgumentException("시간 아이디는 자연수여야 합니다.");
+        }
     }
 
     public Reservation toReservation() {
---
+++ src/test/java/roomescape/service/dto/ReservationRequestDtoTest.java
@@ -0,0 +1,68 @@
+package roomescape.service.dto;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ReservationRequestDtoTest {
+
+    private final String validName = "아토";
+    private final String validDate = "2024-12-20";
+    private final Long validTimeId = 1L;
+
+    @DisplayName("이름이 입력되지 않으면 예외가 발생한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void throw_exception_when_null_name_input(String name) {
+        assertThatThrownBy(() -> new ReservationRequestDto(name, validDate, validTimeId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 반드시 입력되어야 합니다.");
+    }
+
+    @DisplayName("날짜가 입력되지 않으면 예외가 발생한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void throw_exception_when_null_date_input(String date) {
+        assertThatThrownBy(() -> new ReservationRequestDto(validName, date, validTimeId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("날짜는 반드시 입력되어야 합니다.");
+    }
+
+    @DisplayName("잘못된 형식이 입력되면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"23", "1-12-20", "2014-11", "2015-13-01", "2016-02-30", "2019-09-31", "2022-05-00"})
+    void throw_exception_when_invalid_date_format_input(String date) {
+        assertThatThrownBy(() -> new ReservationRequestDto(validName, date, validTimeId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("날짜 형식이 올바르지 않습니다.");
+    }
+
+    @DisplayName("시간 아이디가 입력되지 않으면 예외가 발생한다.")
+    @Test
+    void throw_exception_when_null_time_id_input() {
+        assertThatThrownBy(() -> new ReservationRequestDto(validName, validDate, null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시간 아이디는 반드시 입력되어야 합니다.");
+    }
+
+    @DisplayName("시간 아이디가 자연수가 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(longs = {0, -1})
+    void throw_exception_when_not_natural_time_id_input(Long timeId) {
+        assertThatThrownBy(() -> new ReservationRequestDto(validName, validDate, timeId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시간 아이디는 자연수여야 합니다.");
+    }
+
+    @DisplayName("유효한 예약 입력 시 정상 생성된다.")
+    @Test
+    void create_success() {
+        assertThatNoException()
+                .isThrownBy(() -> new ReservationRequestDto(validName, validDate, validTimeId));
+    }
+}
