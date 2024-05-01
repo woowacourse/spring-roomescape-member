d053ba1ca970f19a053566baa203126f97488d48
ehtjsv2 <ehtjsv2@naver.com>
2024-05-01 18:09:17 +0900

    feat: ThemeController 추가와 삭제기능 구현

--- src/main/java/roomescape/controller/ThemeController.java
+++ src/main/java/roomescape/controller/ThemeController.java
@@ -1,10 +1,16 @@
 package roomescape.controller;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Theme;
+import roomescape.service.ThemeAddRequest;
 import roomescape.service.ThemeService;
 
 @RestController
@@ -18,5 +24,17 @@ public class ThemeController {
     @GetMapping("/themes")
     public ResponseEntity<List<Theme>> getThemeList() {
         return ResponseEntity.ok(themeService.findAllTheme());
+    }
+
+    @PostMapping("/themes")
+    public ResponseEntity<Theme> addTheme(@RequestBody ThemeAddRequest themeAddRequest) {
+        Theme theme = themeService.addTheme(themeAddRequest);
+        return ResponseEntity.created(URI.create("/themes" + theme.getId())).body(theme);
+    }
+
+    @DeleteMapping("/themes/{id}")
+    public ResponseEntity<Void> deleteTheme(@PathVariable("id") Long id) {
+        themeService.removeTheme(id);
+        return ResponseEntity.noContent().build();
     }
 }
