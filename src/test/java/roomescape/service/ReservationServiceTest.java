57d1f09447420cf664b608d0e615d701b9a036fb
libienz <leekh10195@gmail.com>
2024-05-01 14:04:09 +0900

    feat: 존재하지 않는 예약시각에 대한 예약 예외 구현

--- src/test/java/roomescape/service/ReservationServiceTest.java
+++ src/test/java/roomescape/service/ReservationServiceTest.java
@@ -2,8 +2,10 @@ package roomescape.service;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import roomescape.dto.ReservationAddRequest;
 
 class ReservationServiceTest {
 
@@ -16,5 +18,17 @@ class ReservationServiceTest {
         assertThatThrownBy(() -> reservationService.removeReservation(1L))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("해당 id를 가진 예약이 존재하지 않습니다.");
+    }
+
+    @DisplayName("존재하지 않는 예약시각으로 예약 시 예외가 발생합니다.")
+    @Test
+    void should_throw_IllegalArgumentException_when_reserve_non_exist_time() {
+        ReservationService reservationService = new ReservationService(new FakeReservationDao(),
+                new FakeReservationTimeDao());
+        ReservationAddRequest reservationAddRequest = new ReservationAddRequest(LocalDate.of(12, 12, 12), "dodo", 1L);
+
+        assertThatThrownBy(() -> reservationService.addReservation(reservationAddRequest))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("존재 하지 않는 예약시각으로 예약할 수 없습니다.");
     }
 }
