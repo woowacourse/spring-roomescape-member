36659111872117ec5fa7eb8b9d5074533fec47b6
unifolio0 <boho82ha@gmail.com>
2024-05-01 12:45:26 +0900

    feat: 테마 추가 요청에 대한 입력 값 검증

--- src/main/java/roomescape/controller/request/ThemeRequest.java
+++ src/main/java/roomescape/controller/request/ThemeRequest.java
@@ -1,5 +1,6 @@
 package roomescape.controller.request;
 
+import java.util.Objects;
 import roomescape.domain.Theme;
 
 public record ThemeRequest(
@@ -7,6 +8,26 @@ public record ThemeRequest(
         String description,
         String thumbnail
 ) {
+    public ThemeRequest {
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(description);
+        Objects.requireNonNull(thumbnail);
+        validateName(name);
+        validateDescription(description);
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("테마의 이름에 빈 값이 들어갈 수 없습니다.");
+        }
+    }
+
+    private void validateDescription(String description) {
+        if (description.isBlank()) {
+            throw new IllegalArgumentException("테마의 설명에 빈 값이 들어갈 수 없습니다.");
+        }
+    }
+
     public Theme toEntity() {
         return new Theme(name, description, thumbnail);
     }
