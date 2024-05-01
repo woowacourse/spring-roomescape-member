466561541f988c9dec0194ca570abad3ff6c1552
masonkimseoul <masonkimseoul@gmail.com>
2024-05-01 13:12:05 +0900

    fix : 테스트 오류 수정
    RANDOM_PORT, DirtiesContext 추가

--- src/test/java/roomescape/RoomescapeApplicationTest.java
+++ src/test/java/roomescape/RoomescapeApplicationTest.java
@@ -2,8 +2,11 @@ package roomescape;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class RoomescapeApplicationTest {
 
         @Test
