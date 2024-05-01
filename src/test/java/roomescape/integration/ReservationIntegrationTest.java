3e0c249f42a351cdd3189fce6a285f8a92cbd313
fram1998 <koust6u@pusan.ac.kr>
2024-05-01 15:21:11 +0900

    test: 지나간 날짜와 시간에 대한 예약은 추가할 수 없다

--- src/test/java/roomescape/integration/ReservationIntegrationTest.java
+++ src/test/java/roomescape/integration/ReservationIntegrationTest.java
@@ -2,15 +2,23 @@ package roomescape.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 class ReservationIntegrationTest extends IntegrationTest {
+
+    @Mock
+    Clock clock;
+
     @Test
     void 예약을_추가할_수_있다() {
         Map<String, String> params = new HashMap<>();
@@ -71,6 +79,24 @@ class ReservationIntegrationTest extends IntegrationTest {
                 .when().post("/reservations")
                 .then().log().all()
                 .statusCode(409);
+    }
+
+    @Test
+    void 지나간_날짜와_시간에_대한_예약은_추가할_수_없다() {
+        given(clock.instant()).willReturn(Instant.parse("1999-09-19T19:19:00Z"));
+        given(clock.getZone()).willReturn(ZoneId.of("KST"));
+
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "도라");
+        params.put("date", "1998-12-11");
+        params.put("timeId", "1");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400);
     }
 
     @Test
