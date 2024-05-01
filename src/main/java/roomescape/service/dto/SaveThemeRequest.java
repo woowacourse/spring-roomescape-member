8a1dbd6c418a7bbb56fd1f205e3e37f1f730f7ee
KimSangHyeok <tkdgur0906@naver.com>
2024-05-01 15:10:17 +0900

    feat: 테마 이름이 빈칸인지 검증
    Co-authored-by: nak-honest <e5slnh9876@gmail.com>

--- src/main/java/roomescape/service/dto/SaveThemeRequest.java
+++ src/main/java/roomescape/service/dto/SaveThemeRequest.java
@@ -4,6 +4,16 @@ import roomescape.domain.Theme;
 
 public record SaveThemeRequest(String name, String description, String thumbnail) {
 
+    public SaveThemeRequest {
+        validateNameBlank(name);
+    }
+
+    private void validateNameBlank(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("테마 이름은 빈칸일 수 없습니다.");
+        }
+    }
+
     public static Theme toEntity(SaveThemeRequest request) {
         return new Theme(request.name(), request.description(), request.thumbnail());
     }
---
+++ src/test/java/roomescape/service/dto/SaveThemeRequestTest.java
@@ -0,0 +1,28 @@
+package roomescape.service.dto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class SaveThemeRequestTest {
+
+    @Test
+    @DisplayName("테마 이름이 정상 입력될 경우 성공한다.")
+    void checkThemeNameBlank_Success() {
+        assertThatCode(() -> new SaveThemeRequest("capy", "description", "thumbnail"))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  "})
+    @DisplayName("테마 이름이 빈칸인 경우 예외가 발생한다.")
+    void checkThemeNameBlank_Failure(String name) {
+        assertThatThrownBy(() -> new SaveThemeRequest(name, "description", "thumbnail"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("테마 이름은 빈칸일 수 없습니다.");
+    }
+}
