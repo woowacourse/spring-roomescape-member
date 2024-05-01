bf57772a5a2ecfaaff0575b918b8430f1c116390
takoyakimchi <c4nyou@gmail.com>
2024-05-01 22:17:57 +0900

    test : 테마 리포지토리 테스트 데이터베이스 격리
    Co-authored-by: jimi567 <repday0609@gmail.com>

--- src/test/java/roomescape/repository/JdbcThemeRepositoryTest.java
+++ src/test/java/roomescape/repository/JdbcThemeRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import roomescape.domain.Theme;
@@ -12,6 +13,7 @@ import roomescape.domain.ThemeRepository;
 
 @SpringBootTest
 @Transactional
+@AutoConfigureTestDatabase
 class JdbcThemeRepositoryTest {
 
     @Autowired
