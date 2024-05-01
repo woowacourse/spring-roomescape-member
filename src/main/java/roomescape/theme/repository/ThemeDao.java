ca0228dc41b83589eea5c43fcf188224ec2f5357
Zeus6768 <akaps6768@gmail.com>
2024-05-01 18:00:47 +0900

    feat: 방탈출 테마 조회, 추가, 삭제
    Co-authored-by: tackyu <tack970913@gmail.com>

---
+++ src/main/java/roomescape/theme/repository/ThemeDao.java
@@ -0,0 +1,52 @@
+package roomescape.theme.repository;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import roomescape.theme.domain.Theme;
+
+@Repository
+public class ThemeDao {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    private final RowMapper<Theme> themeRowMapper = (resultSet, __) -> new Theme(
+            resultSet.getLong("id"),
+            resultSet.getString("name"),
+            resultSet.getString("description"),
+            resultSet.getString("thumbnail")
+    );
+
+    public ThemeDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("theme")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    public List<Theme> findAll() {
+        return jdbcTemplate.query("SELECT * FROM THEME", themeRowMapper);
+    }
+
+    public Theme findById(Long id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM THEME WHERE ID = ?", themeRowMapper, id);
+    }
+
+    public Theme save(Theme theme) {
+        SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(theme);
+        long id = simpleJdbcInsert.executeAndReturnKey(parameterSource).longValue();
+        return findById(id);
+    }
+
+    public void delete(long themeID) {
+        String query = "DELETE FROM THEME WHERE ID = ?";
+        jdbcTemplate.update(query, themeID);
+    }
+}
