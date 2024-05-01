6a45b7d20f2a6b4348aeb31c0f4d78467f8f8fe9
Seokmyung Ham <gkatjraud1@gmail.com>
2024-05-01 22:20:07 +0900

    feat(ThemeRequestDto): 잘못된 입력 시 예외 발생
    - 이름이 입력되지 않은 경우
    - 설명이 입력되지 않은 경우
    - 썸네일이 입력되지 않은 경우
    
    Co-authored-by: hyxrxn <gpfls981220@gmail.com>

---
+++ src/main/java/roomescape/service/dto/ThemeRequestDto.java
@@ -0,0 +1,35 @@
+package roomescape.service.dto;
+
+public class ThemeRequestDto {
+
+    private final String name;
+    private final String description;
+    private final String thumbnail;
+
+    public ThemeRequestDto(String name, String description, String thumbnail) {
+        validateNameExist(name);
+        validateDescriptionExist(description);
+        validateThumbnailExist(thumbnail);
+        this.name = name;
+        this.description = description;
+        this.thumbnail = thumbnail;
+    }
+
+    private void validateNameExist(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 반드시 입력되어야 합니다.");
+        }
+    }
+
+    private void validateDescriptionExist(String description) {
+        if (description == null || description.isBlank()) {
+            throw new IllegalArgumentException("설명은 반드시 입력되어야 합니다.");
+        }
+    }
+
+    private void validateThumbnailExist(String thumbnail) {
+        if (thumbnail == null || thumbnail.isBlank()) {
+            throw new IllegalArgumentException("썸네일은 반드시 입력되어야 합니다.");
+        }
+    }
+}
