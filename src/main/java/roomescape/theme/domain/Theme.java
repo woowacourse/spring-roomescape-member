b5970f7db8faad815835f81cdd28aa6cc750b11b
DobbyMac <kimdobby@g.skku.edu>
2024-05-01 16:34:39 +0900

    feat(Theme): 테마 기능 데이터 전송 객체 및 도메인 객체 구현

---
+++ src/main/java/roomescape/theme/domain/Theme.java
@@ -0,0 +1,44 @@
+package roomescape.theme.domain;
+
+public class Theme {
+    private long id;
+    private final String name;
+    private final String description;
+    private final String thumbnail;
+
+    public Theme(long id, String name, String description, String thumbnail) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.thumbnail = thumbnail;
+    }
+
+    public Theme(String name, String description, String thumbnail) {
+        this(0, name, description, thumbnail);
+    }
+
+    public Theme(long id) {
+        this(id, null, null, null);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+}
