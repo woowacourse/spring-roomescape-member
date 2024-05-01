7b80885a2abc15f6c53a7c3c82702bef932b8356
Zeus6768 <akaps6768@gmail.com>
2024-05-01 17:23:06 +0900

    feat: db 스키마에 테마 추가
    Co-authored-by: tackyu <tack970913@gmail.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE reservation IF EXISTS;
+DROP TABLE theme IF EXISTS;
+DROP TABLE reservation_time IF EXISTS;
+
 CREATE TABLE reservation_time
 (
     id       BIGINT NOT NULL AUTO_INCREMENT,
@@ -5,12 +9,23 @@ CREATE TABLE reservation_time
     PRIMARY KEY (id)
 );
 
+CREATE TABLE theme
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    thumbnail   VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE reservation
 (
-    id      BIGINT      NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(10) NOT NULL,
-    date    DATE        NOT NULL,
-    time_id BIGINT,
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL,
+    date     VARCHAR(255) NOT NULL,
+    time_id  BIGINT,
+    theme_id BIGINT,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
