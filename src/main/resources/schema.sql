4a3b192a06b596dd3fb51f1305e2ca6d015f7d1c
Zeus6768 <akaps6768@gmail.com>
2024-05-01 18:56:25 +0900

    refactor: 스키마 타입 변경
    Co-authored-by: tackyu <tack970913@gmail.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE reservation_time
 CREATE TABLE theme
 (
     id          BIGINT       NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
+    name        VARCHAR(10)  NOT NULL,
     description VARCHAR(255) NOT NULL,
     thumbnail   VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
@@ -20,9 +20,9 @@ CREATE TABLE theme
 
 CREATE TABLE reservation
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    name     VARCHAR(255) NOT NULL,
-    date     VARCHAR(255) NOT NULL,
+    id       BIGINT      NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(10) NOT NULL,
+    date     DATE        NOT NULL,
     time_id  BIGINT,
     theme_id BIGINT,
     PRIMARY KEY (id),
