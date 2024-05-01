03776a205d4c9a75cab95d87f3d16dbe976abdac
hjk0761 <hjk0761@naver.com>
2024-05-01 17:29:27 +0900

    feat: 테마 Service 기능 구현
    Co-authored-by: pricelees <priceelees@gmail.com>

---
+++ src/main/java/roomescape/service/ReservationThemeService.java
@@ -0,0 +1,36 @@
+package roomescape.service;
+
+import org.springframework.stereotype.Service;
+import roomescape.dao.ReservationDao;
+import roomescape.dao.ReservationThemeDao;
+import roomescape.domain.ReservationTheme;
+import roomescape.dto.ReservationThemeRequestDto;
+
+import java.util.List;
+
+@Service
+public class ReservationThemeService {
+
+    private final ReservationDao reservationDao;
+    private final ReservationThemeDao reservationThemeDao;
+
+    public ReservationThemeService(ReservationDao reservationDao, ReservationThemeDao reservationThemeDao) {
+        this.reservationDao = reservationDao;
+        this.reservationThemeDao = reservationThemeDao;
+    }
+    public List<ReservationTheme> getAllThemes() {
+        return reservationThemeDao.findAll();
+    }
+
+    public ReservationTheme insertTheme(ReservationThemeRequestDto reservationThemeRequestDto) {
+        Long id = reservationThemeDao.insert(reservationThemeRequestDto.name(), reservationThemeRequestDto.description(), reservationThemeRequestDto.thumbnail());
+        return new ReservationTheme(id, reservationThemeRequestDto.name(), reservationThemeRequestDto.description(), reservationThemeRequestDto.thumbnail());
+    }
+
+    public void deleteTheme(Long id) {
+        if (reservationDao.countByThemeId(id) != 0) {
+            throw new IllegalStateException();
+        }
+        reservationThemeDao.deleteById(id);
+    }
+}
---
+++ src/test/java/roomescape/service/ReservationThemeServiceTest.java
@@ -0,0 +1,57 @@
+package roomescape.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import roomescape.domain.ReservationTheme;
+import roomescape.dto.ReservationThemeRequestDto;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReservationThemeServiceTest {
+
+    @Autowired
+    private ReservationThemeService reservationThemeService;
+
+    @Test
+    void getAllThemesTest() {
+        List<ReservationTheme> reservationThemes = reservationThemeService.getAllThemes();
+
+        assertThat(reservationThemes.size()).isEqualTo(1);
+    }
+
+    @Test
+    void insertThemeTest() {
+        ReservationThemeRequestDto reservationThemeRequestDto = new ReservationThemeRequestDto(
+                "레벨2 탈출", "우테코 레벨2 탈출", "https://hi.com"
+        );
+        ReservationTheme reservationTheme = reservationThemeService.insertTheme(reservationThemeRequestDto);
+
+        assertThat(reservationTheme.getName()).isEqualTo("레벨2 탈출");
+        assertThat(reservationTheme.getDescription()).isEqualTo("우테코 레벨2 탈출");
+        assertThat(reservationTheme.getThumbnail()).isEqualTo("https://hi.com");
+    }
+
+    @Test
+    void deleteThemeTest() {
+        ReservationThemeRequestDto reservationThemeRequestDto = new ReservationThemeRequestDto(
+                "레벨2 탈출", "우테코 레벨2 탈출", "https://hi.com"
+        );
+        ReservationTheme reservationTheme = reservationThemeService.insertTheme(reservationThemeRequestDto);
+
+        int sizeBeforeDelete = reservationThemeService.getAllThemes().size();
+        assertThatCode(() -> reservationThemeService.deleteTheme(reservationTheme.getId())).doesNotThrowAnyException();
+        assertThat(reservationThemeService.getAllThemes().size()).isEqualTo(sizeBeforeDelete - 1);
+    }
+
+    @Test
+    void deleteInvalidThemeIdTest() {
+        assertThatThrownBy(() -> reservationThemeService.deleteTheme(1L))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
