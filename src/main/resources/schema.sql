455b3a27757180fc9fcb796fecf2025a5f32ff05
kelly6bf <kelly6bf@gmail.com>
2024-05-01 22:27:37 +0900

    config: schema.sql 현재를 기점으로 시간 설정 쿼리 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -32,9 +32,19 @@ CREATE TABLE reservation
 
 INSERT INTO theme(name, description, thumbnail) VALUES ('테바와 비밀친구', '나랑.. 비밀친구할래..?', '테바 사진 링크');
 INSERT INTO theme(name, description, thumbnail) VALUES ('켈리의 댄스교실', '켈켈켈켈켈', '켈리 사진 링크');
+INSERT INTO theme(name, description, thumbnail) VALUES ('우테코 탈출하기', '우테코를... 탈출..하자..!', '우테코 사진 링크');
 
-INSERT INTO reservation_time(start_at) VALUES ('10:10');
+INSERT INTO reservation_time(start_at) VALUES ('09:30');
 INSERT INTO reservation_time(start_at) VALUES ('11:30');
+INSERT INTO reservation_time(start_at) VALUES ('13:30');
+INSERT INTO reservation_time(start_at) VALUES ('15:30');
 
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('켈리', '2024-07-03', 1, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('오리', '2024-07-03', 1, 2);
+INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('켈리', CAST(TIMESTAMPADD(DAY, -3, NOW()) AS DATE), 1, 1);
+INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('오리', CAST(TIMESTAMPADD(DAY, -4, NOW()) AS DATE), 1, 2);
+INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('치킨보이', CAST(TIMESTAMPADD(DAY, -2, NOW()) AS DATE), 4, 1);
+INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('치킨걸', CAST(TIMESTAMPADD(DAY, -6, NOW()) AS DATE), 3, 1);
+
+INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('켈리', CAST(TIMESTAMPADD(DAY, 3, NOW()) AS DATE), 1, 1);
+INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('오리', CAST(TIMESTAMPADD(DAY, 4, NOW()) AS DATE), 1, 2);
+INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('치킨보이', CAST(TIMESTAMPADD(DAY, 2, NOW()) AS DATE), 4, 1);
+INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('치킨걸', CAST(TIMESTAMPADD(DAY, 6, NOW()) AS DATE), 3, 1);
