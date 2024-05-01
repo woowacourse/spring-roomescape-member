4ba711999239236ae9eaa5dfec07aaa6cc2066cc
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-01 14:56:43 +0900

    test: 테마 서비스 성공 테스트
    Co-Authored-By: linirini <101927543+linirini@users.noreply.github.com>

---
+++ src/test/java/roomescape/service/ThemeServiceTest.java
@@ -0,0 +1,71 @@
+package roomescape.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import roomescape.service.dto.ThemeRequest;
+import roomescape.service.dto.ThemeResponse;
+
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+class ThemeServiceTest {
+
+    @Autowired
+    private ThemeService themeService;
+
+    @AfterEach
+    void init() {
+        for (ThemeResponse themeResponse : themeService.findAll()) {
+            themeService.deleteById(themeResponse.id());
+        }
+    }
+
+    @DisplayName("테마를 생성한다.")
+    @Test
+    void create() {
+        //given
+        ThemeRequest themeRequest = new ThemeRequest("레벨2 탈출", "우테코 레벨2를 탈출하는 내용입니다.",
+                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg");
+
+        //when
+        ThemeResponse themeResponse = themeService.create(themeRequest);
+
+        //then
+        assertThat(themeResponse.id()).isNotZero();
+    }
+
+    @DisplayName("모든 테마를 조회한다.")
+    @Test
+    void findAll() {
+        //given
+        ThemeRequest themeRequest = new ThemeRequest("레벨2 탈출", "우테코 레벨2를 탈출하는 내용입니다.",
+                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg");
+        themeService.create(themeRequest);
+
+        //when
+        List<ThemeResponse> responses = themeService.findAll();
+
+        //then
+        assertThat(responses).hasSize(1);
+    }
+
+    @DisplayName("테마를 삭제한다.")
+    @Test
+    void deleteById() {
+        //given
+        ThemeRequest themeRequest = new ThemeRequest("레벨2 탈출", "우테코 레벨2를 탈출하는 내용입니다.",
+                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg");
+        ThemeResponse target = themeService.create(themeRequest);
+
+        //when
+        themeService.deleteById(target.id());
+
+        //then
+        assertThat(themeService.findAll()).hasSize(0);
+    }
+}
