0d9a15057f1a62b07d22c0dc1206dd3adc1baa9f
Nam Gi Beom <rlqja0523@naver.com>
2024-05-01 16:16:34 +0900

    feat: request에 validation 추가
    Co-authored-by: JiHyeonL <lejinhy@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

--- src/main/java/roomescape/dto/reservationtime/ReservationTimeRequest.java
+++ src/main/java/roomescape/dto/reservationtime/ReservationTimeRequest.java
@@ -1,31 +1,11 @@
 package roomescape.dto.reservationtime;
 
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
-import java.util.Objects;
-import java.util.regex.Pattern;
-
 import roomescape.application.dto.ReservationTimeCreationRequest;
 
-public record ReservationTimeRequest(String startAt) {
-    private static final Pattern FORMAT = Pattern.compile("^\\d{2}:\\d{2}$");
-
-    public ReservationTimeRequest {
-        if (!FORMAT.matcher(startAt).matches()) {
-            throw new IllegalArgumentException("시작 시간은 HH:mm 형식이어야 합니다.");
-        }
-
-        try {
-            LocalTime.parse(startAt);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("시간이 올바르지 않습니다.");
-        }
-
-        Objects.requireNonNull(startAt, "시작 시간은 필수입니다.");
-    }
-
-
-
+public record ReservationTimeRequest(@NotNull(message = "시작 시간은 필수입니다.") LocalTime startAt) {
     public ReservationTimeCreationRequest toReservationCreationRequest() {
-        return new ReservationTimeCreationRequest(LocalTime.parse(startAt));
+        return new ReservationTimeCreationRequest(startAt);
     }
 }
