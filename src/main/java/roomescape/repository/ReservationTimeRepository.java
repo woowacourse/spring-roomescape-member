cc62f4b565f06565c0a496dd5d212c4e03daf851
zangsu <zangsu_@naver.com>
2024-05-01 22:10:35 +0900

    refactor : 해결된 TODO 삭제

--- src/main/java/roomescape/repository/ReservationTimeRepository.java
+++ src/main/java/roomescape/repository/ReservationTimeRepository.java
@@ -8,7 +8,6 @@ import roomescape.domain.ReservationTime;
 public interface ReservationTimeRepository {
     ReservationTime save(ReservationTime reservationTime);
 
-    //Todo 메서드명 고민
     boolean existsByStartAt(LocalTime startAt);
 
     Optional<ReservationTime> findById(long id);
