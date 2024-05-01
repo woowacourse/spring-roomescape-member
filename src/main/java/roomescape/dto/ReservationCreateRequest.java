dace2fe4917a729b578d2a70ba4f39d4eca2dafc
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-01 15:51:55 +0900

    refactor(ReservationCreateRequest): id 타입을 long -> Long로 변환
    Co-authored-by: leegwichan <cnddkscndgus@naver.com>

--- src/main/java/roomescape/dto/ReservationCreateRequest.java
+++ src/main/java/roomescape/dto/ReservationCreateRequest.java
@@ -3,7 +3,7 @@ package roomescape.dto;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 
-public record ReservationCreateRequest(String name, String date, long timeId) {
+public record ReservationCreateRequest(String name, String date, Long timeId) {
     public Reservation createReservation(ReservationTime time) {
         return new Reservation(name, date, time);
     }
