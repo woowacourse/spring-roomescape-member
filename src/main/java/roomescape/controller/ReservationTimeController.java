53592c13922d28a596674e496e0290235819190a
Nam Gi Beom <rlqja0523@naver.com>
2024-05-01 16:18:29 +0900

    refactor: @Valid 어노테이션으로 검증 변경
    Co-authored-by: JiHyeonL <lejinhy@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -1,8 +1,8 @@
 package roomescape.controller;
 
+import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import roomescape.application.ReservationTimeService;
 import roomescape.domain.time.ReservationTime;
 import roomescape.dto.reservationtime.ReservationTimeRequest;
@@ -27,7 +26,8 @@ public class ReservationTimeController {
     }
 
     @PostMapping
-    public ResponseEntity<ReservationTimeResponse> createReservationTime(@RequestBody ReservationTimeRequest request) {
+    public ResponseEntity<ReservationTimeResponse> createReservationTime(
+            @Valid @RequestBody ReservationTimeRequest request) {
         ReservationTime time = reservationTimeService.register(request.toReservationCreationRequest());
         ReservationTimeResponse response = ReservationTimeResponse.from(time);
         URI location = URI.create("/times/" + response.id());
--- src/test/java/roomescape/controller/ReservationTimeControllerTest.java
+++ src/test/java/roomescape/controller/ReservationTimeControllerTest.java
@@ -9,6 +9,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.List;
 import java.util.stream.IntStream;
+import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +30,7 @@ class ReservationTimeControllerTest extends ControllerTest {
     void 예약_시간을_생성한다() throws Exception {
         ReservationTime reservationTime = ReservationFixture.reservationTime();
         when(reservationTimeService.register(any())).thenReturn(reservationTime);
-        ReservationTimeRequest request = new ReservationTimeRequest(reservationTime.getStartAt().toString());
+        ReservationTimeRequest request = new ReservationTimeRequest(reservationTime.getStartAt());
         String content = objectMapper.writeValueAsString(request);
 
         ResultActions result = SimpleMockMvc.post(mockMvc, "/times", content);
@@ -77,17 +78,23 @@ class ReservationTimeControllerTest extends ControllerTest {
 
         ResultActions result = SimpleMockMvc.post(mockMvc, "/times", content);
 
-        result.andExpect(status().isBadRequest())
+        result.andExpectAll(
+                        status().isBadRequest(),
+                        jsonPath("$.fieldErrors[0].field").value("startAt"),
+                        jsonPath("$.fieldErrors[0].rejectedValue").value(IsNull.nullValue()),
+                        jsonPath("$.fieldErrors[0].reason").value("시작 시간은 필수입니다.")
+                )
                 .andDo(print());
     }
 
     @Test
     void 예약_시간이_포맷에_맞지_않을_경우_Bad_Request_상태를_반환한다() throws Exception {
-        String content = "{\"startAt\":1112}";
+        String content = "{\"startAt\":\"1112\"}";
 
         ResultActions result = SimpleMockMvc.post(mockMvc, "/times", content);
 
         result.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("잘못된 날짜 혹은 시간입니다."))
                 .andDo(print());
     }
 
@@ -98,6 +105,7 @@ class ReservationTimeControllerTest extends ControllerTest {
         ResultActions result = SimpleMockMvc.post(mockMvc, "/times", content);
 
         result.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("잘못된 날짜 혹은 시간입니다."))
                 .andDo(print());
     }
 }
