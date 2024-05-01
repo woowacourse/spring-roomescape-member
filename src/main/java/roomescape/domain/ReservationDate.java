ee1f92831d17341dc23a916a5c013726b10567b6
libienz <leekh10195@gmail.com>
2024-05-01 13:44:51 +0900

    feat: ReservationDate 과거 날짜는 검증 구현

--- src/main/java/roomescape/domain/ReservationDate.java
+++ src/main/java/roomescape/domain/ReservationDate.java
@@ -7,13 +7,24 @@ public class ReservationDate {
     private final LocalDate date;
 
     public ReservationDate(LocalDate date) {
-        validateNonNull(date);
+        validate(date);
         this.date = date;
     }
 
+    private void validate(LocalDate date) {
+        validateNonNull(date);
+        validateNonPastDate(date);
+    }
+    
     private void validateNonNull(LocalDate date) {
         if (date == null) {
             throw new NullPointerException("날짜는 null일 수 없습니다");
+        }
+    }
+
+    private void validateNonPastDate(LocalDate date) {
+        if (date.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException(date + ": 예약 날짜는 현재 보다 이전일 수 없습니다");
         }
     }
 }
--- src/test/java/roomescape/domain/ReservationDateTest.java
+++ src/test/java/roomescape/domain/ReservationDateTest.java
@@ -2,6 +2,7 @@ package roomescape.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +14,14 @@ class ReservationDateTest {
         assertThatThrownBy(() -> new ReservationDate(null))
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage("날짜는 null일 수 없습니다");
+    }
+
+    @DisplayName("date가 현재 날짜 보다 이전 날짜이면 ReservationDate생성 시 예외가 발생한다")
+    @Test
+    void should_throw_IllegalArgument_when_date_is_past() {
+        LocalDate pastDate = LocalDate.now().minusDays(1);
+        assertThatThrownBy(() -> new ReservationDate(pastDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(pastDate + ": 예약 날짜는 현재 보다 이전일 수 없습니다");
     }
 }
