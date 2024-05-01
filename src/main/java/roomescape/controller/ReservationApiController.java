d9cb606683f1e8892e21f65b9c7782a50f4ccb86
Seokmyung Ham <gkatjraud1@gmail.com>
2024-05-01 20:50:50 +0900

    feat(ReservationApiController): ExceptionHandler 추가
    Co-authored-by: hyxrxn <gpfls981220@gmail.com>

--- src/main/java/roomescape/controller/ReservationApiController.java
+++ src/main/java/roomescape/controller/ReservationApiController.java
@@ -2,7 +2,9 @@ package roomescape.controller;
 
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.exception.ErrorResult;
 import roomescape.service.ReservationService;
 import roomescape.service.dto.ReservationRequestDto;
 import roomescape.service.dto.ReservationResponseDto;
@@ -39,5 +42,10 @@ public class ReservationApiController {
     @DeleteMapping("/{id}")
     public void deleteReservation(@PathVariable long id) {
         reservationService.deleteReservation(id);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(new ErrorResult(ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
