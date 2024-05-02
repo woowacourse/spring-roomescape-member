9a3940bdd8a50fbb279f7adb123ac9df4162ad58
leetaehoon <xogns1514@gmail.com>
2024-05-02 15:52:40 +0900

    refactor: test 코드 중복 제거

--- src/test/java/roomescape/reservation/ReservationTest.java
+++ src/test/java/roomescape/reservation/ReservationTest.java
@@ -30,15 +30,20 @@ class ReservationTest {
         RestAssured.port = port;
     }
 
+    @BeforeEach
+    void insert() {
+        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES(?)", LocalTime.of(10, 0));
+        jdbcTemplate.update("INSERT INTO theme (name, description, thumbnail) VALUES(?, ?, ?)", "hi", "happy", "abcd.html");
+        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id, theme_id) VALUES(?, ?, ?, ?)",
+                "rush", LocalDate.of(2030, 12, 12), 1, 1);
+    }
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @DisplayName("예약 추가 API 테스트")
     @Test
     void createReservation() {
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES(?)", LocalTime.of(10, 0));
-        jdbcTemplate.update("INSERT INTO theme (name, description, thumbnail) VALUES(?, ?, ?)", "happy", "hi", "abcd.html");
-
         ReservationRequest reservationRequest = new ReservationRequest("브라운", LocalDate.of(2999, 8, 5), 1L, 1L);
 
         RestAssured.given().log().all()
@@ -47,13 +52,13 @@ class ReservationTest {
                 .when().post("/reservations")
                 .then().log().all()
                 .statusCode(201)
-                .body("id", is(1));
+                .body("id", is(2));
 
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(1));
+                .body("size()", is(2));
     }
 
     @DisplayName("예약 조회 API 테스트")
@@ -63,25 +68,12 @@ class ReservationTest {
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(0));
+                .body("size()", is(1));
     }
 
     @DisplayName("예약 취소 API 테스트")
     @Test
     void deleteReservation() {
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES(?)", LocalTime.of(10, 0));
-        jdbcTemplate.update("INSERT INTO theme (name, description, thumbnail) VALUES(?, ?, ?)", "happy", "hi", "abcd.html");
-
-        ReservationRequest reservationRequest = new ReservationRequest("브라운", LocalDate.of(2999, 8, 5), 1L, 1L);
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(reservationRequest)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(201)
-                .body("id", is(1));
-
         RestAssured.given().log().all()
                 .when().delete("/reservations/1")
                 .then().log().all()
@@ -97,12 +89,11 @@ class ReservationTest {
     @DisplayName("올바르지 않은 예약자명 형식으로 입력시 예외처리")
     @Test
     void invalidNameFormat() {
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES(?)", LocalTime.of(10, 0));
-
         Map<String, String> reservationRequest = new HashMap<>();
         reservationRequest.put("name", "12345678900");
         reservationRequest.put("date", "2025-12-12");
         reservationRequest.put("timeId", "1");
+        reservationRequest.put("themeId", "1");
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -115,12 +106,11 @@ class ReservationTest {
     @DisplayName("올바르지 않은 날짜 형식으로 입력시 예외처리")
     @Test
     void invalidDateFormat() {
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES(?)", LocalTime.of(10, 0));
-
         Map<String, String> reservationRequest = new HashMap<>();
         reservationRequest.put("name", "1234567890");
         reservationRequest.put("date", "2025-aa-bb");
         reservationRequest.put("timeId", "1");
+        reservationRequest.put("themeId", "1");
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -133,12 +123,11 @@ class ReservationTest {
     @DisplayName("올바르지 않은 예약자명 형식으로 입력시 예외처리")
     @Test
     void invalidTimeIdFormat() {
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES(?)", LocalTime.of(10, 0));
-
         Map<String, String> reservationRequest = new HashMap<>();
         reservationRequest.put("name", "12345678900");
         reservationRequest.put("date", "2030-12-12");
         reservationRequest.put("timeId", "a");
+        reservationRequest.put("themeId", "1");
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -151,12 +140,11 @@ class ReservationTest {
     @DisplayName("지나간 시점에 대한 예약시 예외처리")
     @Test
     void pastTimeSlotReservation() {
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES(?)", LocalTime.of(10, 0));
-
         Map<String, String> reservationRequest = new HashMap<>();
         reservationRequest.put("name", "1234567890");
         reservationRequest.put("date", "1999-12-12");
         reservationRequest.put("timeId", "1");
+        reservationRequest.put("themeId", "1");
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -166,17 +154,14 @@ class ReservationTest {
                 .statusCode(400);
     }
 
-    @DisplayName("동일한 시간에 중복 예약시 예외 처리")
+    @DisplayName("동일한 날짜와 시간에 중복 예약시 예외 처리")
     @Test
     void duplicateReservation() {
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES(?)", LocalTime.of(10, 0));
-        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id) VALUES(?, ?, ?)",
-                "rush", LocalDate.of(2030, 12, 12), 1);
-
         Map<String, String> reservationRequest = new HashMap<>();
         reservationRequest.put("name", "1234567890");
         reservationRequest.put("date", "2030-12-12");
         reservationRequest.put("timeId", "1");
+        reservationRequest.put("themeId", "1");
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
