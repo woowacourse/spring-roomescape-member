e0da0415948617885dc95b0c08467cea75044044
hangillee <skfcb10@naver.com>
2024-05-02 16:16:35 +0900

    refactor: 테스트 DisplayName 추가

--- src/test/java/roomescape/web/controller/UserControllerTest.java
+++ src/test/java/roomescape/web/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package roomescape.web.controller;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -12,7 +13,6 @@ import org.springframework.test.context.TestPropertySource;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @TestPropertySource(properties = {"spring.config.location = classpath:application-test.yml"})
 class UserControllerTest {
-
     @LocalServerPort
     private int port;
 
@@ -22,10 +22,11 @@ class UserControllerTest {
     }
 
     @Test
+    @DisplayName("예약 페이지로 이동한다.")
     void moveToReservationPage() {
         RestAssured.given().log().all()
-            .when().get("/reservation")
-            .then().log().all()
-            .statusCode(200);
+                .when().get("/reservation")
+                .then().log().all()
+                .statusCode(200);
     }
 }
