2d15d33a904767ac6cf3597f0d90186f6e362b3e
jhon3242 <rldotnr2@gmail.com>
2024-05-02 17:00:35 +0900

    refactor: 테마 객체 동등성 판단을 id와 이름으로 한정

--- src/main/java/roomescape/domain/Theme.java
+++ src/main/java/roomescape/domain/Theme.java
@@ -37,11 +37,11 @@ public class Theme {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Theme theme = (Theme) o;
-        return Objects.equals(id, theme.id) && Objects.equals(name, theme.name) && Objects.equals(description, theme.description) && Objects.equals(thumbnail, theme.thumbnail);
+        return Objects.equals(id, theme.id) && Objects.equals(name, theme.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, description, thumbnail);
+        return Objects.hash(id, name);
     }
 }
