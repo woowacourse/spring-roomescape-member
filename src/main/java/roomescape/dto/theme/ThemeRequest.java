9f6e9afe6710caaac97c6ebab8422d448f70269d
Nam Gi Beom <rlqja0523@naver.com>
2024-05-02 17:15:12 +0900

    refactor: @Size 지정
    Co-authored-by: JiHyeonL <lejinhy@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

--- src/main/java/roomescape/dto/theme/ThemeRequest.java
+++ src/main/java/roomescape/dto/theme/ThemeRequest.java
@@ -6,7 +6,7 @@ import roomescape.application.dto.ThemeCreationRequest;
 
 public record ThemeRequest(
         @NotBlank(message = "테마명은 필수입니다.")
-        @Size()
+        @Size(min = 3, max = 20, message = "테마명은 3글자 이상 20글자 이하여야 합니다.")
         String name,
 
         @NotBlank(message = "테마 설명은 필수입니다.")
