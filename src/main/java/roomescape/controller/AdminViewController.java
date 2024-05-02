93cb710b7bc2a42c2c773537f2564b03378f13eb
ehtjsv2 <ehtjsv2@naver.com>
2024-05-02 15:22:45 +0900

    feat: welcomePage 변경

--- src/main/java/roomescape/controller/AdminViewController.java
+++ src/main/java/roomescape/controller/AdminViewController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminViewController {
 
-    @GetMapping(path = {"/", "/admin"})
-    public String wellComePage() {
+    @GetMapping( "/admin")
+    public String adminMainPage() {
         return "admin/index";
     }
 
