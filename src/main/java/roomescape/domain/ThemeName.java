295c64f22b508effa82012457748ef5463899262
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-02 14:34:58 +0900

    refactor(ThemeName): 테마 이름 길이 제한 변경

--- src/main/java/roomescape/domain/ThemeName.java
+++ src/main/java/roomescape/domain/ThemeName.java
@@ -14,8 +14,8 @@ public class ThemeName {
     }
 
     private void validateName(String name) {
-        if (name.isEmpty() || name.length() > 10) {
-            throw new IllegalArgumentException("[ERROR] 테마이름은 1~10자만 가능합니다.");
+        if (name.isEmpty() || name.length() > 20) {
+            throw new IllegalArgumentException("[ERROR] 테마이름은 1~20자만 가능합니다.");
         }
     }
 }
