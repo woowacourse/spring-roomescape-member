ea4db560da5f58b50124fcc30dea63e63745f5d7
Seokmyung Ham <gkatjraud1@gmail.com>
2024-05-02 17:03:21 +0900

    feat: 샘플 데이터 생성
    Co-authored-by: hyxrxn <gpfls981220@gmail.com>

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO reservation_time
+values (1, '12:00'),
+       (2, '16:00');
+
+INSERT INTO theme
+values (1, '테마1', '테마1입니다', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       (2, '테마2', '테마2입니다', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       (3, '테마3', '테마3입니다', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+
+INSERT INTO reservation
+values (1, '아토', '2024-05-01', 1, 2),
+       (2, '안돌', '2024-05-01', 2, 2),
+       (3, '재즈', '2024-04-30', 1, 2),
+       (4, '러너덕', '2024-05-01', 1, 1),
+       (5, '로키', '2024-05-01', 2, 1),
+       (6, '폴라', '2024-05-01', 1, 3);
