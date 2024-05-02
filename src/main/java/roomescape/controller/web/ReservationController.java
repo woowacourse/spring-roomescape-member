99be1c14d783d6a537a42590b912f2c095190e7e
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-02 11:15:12 +0900

    feat: 선택가능한 시간 반환 기능 구현

--- src/main/java/roomescape/controller/web/ReservationController.java
+++ src/main/java/roomescape/controller/web/ReservationController.java
@@ -1,17 +1,14 @@
 package roomescape.controller.web;
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import roomescape.dto.ReservationRequest;
 import roomescape.dto.ReservationResponse;
+import roomescape.dto.SelectableTimeResponse;
 import roomescape.service.ReservationService;
 
 @Controller
@@ -39,5 +36,12 @@ public class ReservationController {
     public ResponseEntity<Void> deleteReservation(@PathVariable("id") long id) {
         reservationService.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/times")
+    public ResponseEntity<List<SelectableTimeResponse>> findSelectableTimes(@RequestParam(name="date") LocalDate date,
+                                                                            @RequestParam(name="themeId") long themeId) {
+        List<SelectableTimeResponse> selectableTimeResponses = reservationService.findSelectableTime(date, themeId);
+        return ResponseEntity.ok(selectableTimeResponses);
     }
 }
