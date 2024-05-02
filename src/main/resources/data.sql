478963ce44d40b3a58abac73a3f37bbd47082031
J-I-H-O <jeongjiho0731@gmail.com>
2024-05-02 09:20:00 +0900

    chore: 더미 데이터 삽입 SQL 작성
    Co-authored-by: PgmJun <chltmdwns96@gmail.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마1', '재밌는 테마입니다',
+        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마2', '재밌는 테마입니다',
+        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마3', '재밌는 테마입니다',
+        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마4', '재밌는 테마입니다',
+        'https://www.google.co.kr/url?sa=i&url=http%3A%2F%2Fwww.code-k.co.kr%2Fsub%2Fcode_sub03.html%3FR_JIJEM%3DS1&psig=AOvVaw20fNjL28MSMMiR0Nb57Eh-&ust=1714695060162000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOiO2oLX7YUDFQAAAAAdAAAAABAE');
+
+INSERT INTO reservation_time (start_at)
+VALUES ('08:00');
+INSERT INTO reservation_time (start_at)
+VALUES ('10:00');
+INSERT INTO reservation_time (start_at)
+VALUES ('13:00');
+INSERT INTO reservation_time (start_at)
+VALUES ('21:00');
+INSERT INTO reservation_time (start_at)
+VALUES ('23:00');
