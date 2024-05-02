352014b44ea08164254725ec61008b64dc5a3841
hoyeonyy <highoption@naver.com>
2024-05-02 13:19:33 +0900

    refactor : 에러 정보 레코드로 변경

--- src/main/java/roomescape/exception/ExceptionInfo.java
+++ src/main/java/roomescape/exception/ExceptionInfo.java
@@ -1,13 +1,4 @@
 package roomescape.exception;
 
-public class ExceptionInfo {
-    private final String message;
-
-    public ExceptionInfo(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+public record ExceptionInfo(String message) {
 }
