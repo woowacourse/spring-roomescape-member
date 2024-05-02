7095cfe737d407affe9541e0a258fcd6edcd7744
Choo <choo000407@naver.com>
2024-05-02 16:34:23 +0900

    test: nested 사용해 관심사 별 분리

--- src/test/java/roomescape/integration/ReservationIntegrationTest.java
+++ src/test/java/roomescape/integration/ReservationIntegrationTest.java
@@ -7,116 +7,120 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ReservationIntegrationTest extends IntegrationTest {
-    @Test
-    void 예약_목록을_조회할_수_있다() {
-        RestAssured.given().log().all()
-                .when().get("/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
+    @Nested
+    @DisplayName("예약 목록 조회 API")
+    class FindAllReservations {
+        @Test
+        void 예약_목록을_조회할_수_있다() {
+            RestAssured.given().log().all()
+                    .when().get("/reservations")
+                    .then().log().all()
+                    .statusCode(200)
+                    .body("size()", is(1));
+        }
     }
 
-    @Test
-    void 예약을_추가할_수_있다() {
-        Map<String, String> params = new HashMap<>(); // TODO: nested 써서 중복 코드 제거하기
-        params.put("name", "브라운");
-        params.put("date", "2023-08-06");
-        params.put("timeId", "1");
-        params.put("themeId", "1");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(201)
-                .header("Location", "/reservations/2")
-                .body("id", is(2));
-    }
-
-    @Test
-    void 시간이_빈_값이면_예약을_추가할_수_없다() {
+    @Nested
+    @DisplayName("예약 추가 API")
+    class SaveReservation {
         Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-08-06");
-        params.put("timeId", null);
-        params.put("themeId", "1");
 
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(400);
+        @BeforeEach
+        void setUp() {
+            params.put("name", "브라운");
+            params.put("themeId", "1");
+            params.put("timeId", "1");
+        }
+
+        @Test
+        void 예약을_추가할_수_있다() {
+            params.put("date", "2023-08-06");
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(201)
+                    .header("Location", "/reservations/2")
+                    .body("id", is(2));
+        }
+
+        @Test
+        void 필드가_빈_값이면_예약을_추가할_수_없다() {
+            params.put("date", null);
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(400);
+        }
+
+        @Test
+        void 날짜의_형식이_다르면_예약을_추가할_수_없다() {
+            params.put("date", "2023-13-05");
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(400);
+        }
+
+        @Test
+        void 시간대와_테마가_똑같은_중복된_예약은_추가할_수_없다() {
+            params.put("date", "2024-08-05");
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(409);
+        }
+
+        @Test
+        void 지나간_날짜와_시간에_대한_예약은_추가할_수_없다() {
+            params.put("date", "1998-12-11");
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/reservations")
+                    .then().log().all()
+                    .statusCode(400);
+        }
     }
 
-    @Test
-    void 날짜의_형식이_다르면_예약을_추가할_수_없다() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("date", "2023-13-05");
-        params.put("timeId", "1");
-        params.put("themeId", "1");
+    @Nested
+    @DisplayName("예약 삭제 API")
+    class DeleteReservation {
+        @Test
+        void 예약을_삭제할_수_있다() {
+            RestAssured.given().log().all()
+                    .when().delete("/reservations/1")
+                    .then().log().all()
+                    .statusCode(204);
 
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(400);
-    }
+            Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
+            assertThat(countAfterDelete).isZero();
+        }
 
-    @Test
-    void 시간대와_테마가_똑같은_중복된_예약은_추가할_수_없다() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "도라");
-        params.put("date", "2024-08-05");
-        params.put("timeId", "1");
-        params.put("themeId", "1");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(409);
-    }
-
-    @Test
-    void 지나간_날짜와_시간에_대한_예약은_추가할_수_없다() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "도라");
-        params.put("date", "1998-12-11");
-        params.put("timeId", "1");
-        params.put("themeId", "1");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(400);
-    }
-
-    @Test
-    void 예약을_삭제할_수_있다() {
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
-                .then().log().all()
-                .statusCode(204);
-
-        Integer countAfterDelete = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
-        assertThat(countAfterDelete).isZero();
-    }
-
-    @Test
-    void 존재하지_않는_예약은_삭제할_수_없다() {
-        RestAssured.given().log().all()
-                .when().delete("/reservations/10")
-                .then().log().all()
-                .statusCode(404);
+        @Test
+        void 존재하지_않는_예약은_삭제할_수_없다() {
+            RestAssured.given().log().all()
+                    .when().delete("/reservations/10")
+                    .then().log().all()
+                    .statusCode(404);
+        }
     }
 }
--- src/test/java/roomescape/integration/ThemeIntegrationTest.java
+++ src/test/java/roomescape/integration/ThemeIntegrationTest.java
@@ -9,90 +9,108 @@ import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ThemeIntegrationTest extends IntegrationTest {
-    @Test
-    void 테마_목록을_조회할_수_있다() {
-        RestAssured.given().log().all()
-                .when().get("/themes")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
+    @Nested
+    @DisplayName("테마 목록 조회 API")
+    class FindAllTheme {
+        @Test
+        void 테마_목록을_조회할_수_있다() {
+            RestAssured.given().log().all()
+                    .when().get("/themes")
+                    .then().log().all()
+                    .statusCode(200)
+                    .body("size()", is(1));
+        }
     }
 
-    @Test
-    void 최근_일주일동안_예약_건수_많은_순서대로_10개_테마를_인기_테마로_조회할_수_있다() {
-        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id, theme_id) VALUES (?, ?, ?, ?)",
-                "브라운", "1999-09-18", "1", "1");
+    @Nested
+    @DisplayName("인기 테마 목록 조회 API")
+    class FindAllPopularTheme {
+        @Test
+        void 최근_일주일동안_예약_건수_많은_순서대로_10개_테마를_인기_테마로_조회할_수_있다() {
+            jdbcTemplate.update("INSERT INTO reservation (name, date, time_id, theme_id) VALUES (?, ?, ?, ?)",
+                    "브라운", "1999-09-18", "1", "1");
 
-        RestAssured.given().log().all()
-                .when().get("/themes/ranking")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(1));
+            RestAssured.given().log().all()
+                    .when().get("/themes/ranking")
+                    .then().log().all()
+                    .statusCode(200)
+                    .body("size()", is(1));
 
-        List<Map<String, Object>> response = RestAssured.get("/themes/ranking")
-                .as(new TypeRef<>() {
-                });
-        assertThat(response.get(0)).containsEntry("id", 1);
+            List<Map<String, Object>> response = RestAssured.get("/themes/ranking")
+                    .as(new TypeRef<>() {
+                    });
+            assertThat(response.get(0)).containsEntry("id", 1);
+        }
     }
 
-    @Test
-    void 테마를_추가할_수_있다() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "레벨3");
-        params.put("description", "내용이다.");
-        params.put("thumbnail", "https://naver.com");
+    @Nested
+    @DisplayName("테마 추가 API")
+    class SaveTheme {
+        @Test
+        void 테마를_추가할_수_있다() {
+            Map<String, String> params = new HashMap<>();
+            params.put("name", "레벨3");
+            params.put("description", "내용이다.");
+            params.put("thumbnail", "https://naver.com");
 
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/themes")
-                .then().log().all()
-                .statusCode(201)
-                .header("Location", "/themes/2")
-                .body("id", is(2));
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/themes")
+                    .then().log().all()
+                    .statusCode(201)
+                    .header("Location", "/themes/2")
+                    .body("id", is(2));
+        }
+
+        @Test
+        void 필드가_빈_값이면_테마를_추가할_수_없다() {
+            Map<String, String> params = new HashMap<>();
+            params.put("name", "레벨3");
+            params.put("description", null);
+            params.put("thumbnail", "https://naver.com");
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(params)
+                    .when().post("/themes")
+                    .then().log().all()
+                    .statusCode(400);
+        }
     }
 
-    @Test
-    void 필드가_빈_값이면_테마를_추가할_수_없다() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "레벨3");
-        params.put("description", null);
-        params.put("thumbnail", "https://naver.com");
+    @Nested
+    @DisplayName("테마 삭제 API")
+    class DeleteTheme {
+        @Test
+        void 테마를_삭제할_수_있다() {
+            jdbcTemplate.update("DELETE FROM reservation WHERE id = ?", 1);
 
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/themes")
-                .then().log().all()
-                .statusCode(400);
-    }
+            RestAssured.given().log().all()
+                    .when().delete("/themes/1")
+                    .then().log().all()
+                    .statusCode(204);
+        }
 
-    @Test
-    void 테마를_삭제할_수_있다() {
-        jdbcTemplate.update("DELETE FROM reservation WHERE id = ?", 1);
+        @Test
+        void 존재하지_않는_테마는_삭제할_수_없다() {
+            RestAssured.given().log().all()
+                    .when().delete("/themes/13")
+                    .then().log().all()
+                    .statusCode(404);
+        }
 
-        RestAssured.given().log().all()
-                .when().delete("/themes/1")
-                .then().log().all()
-                .statusCode(204);
-    }
-
-    @Test
-    void 존재하지_않는_테마는_삭제할_수_없다() {
-        RestAssured.given().log().all()
-                .when().delete("/themes/13")
-                .then().log().all()
-                .statusCode(404);
-    }
-
-    @Test
-    void 예약이_존재하는_테마는_삭제할_수_없다() {
-        RestAssured.given().log().all()
-                .when().delete("/themes/1")
-                .then().log().all()
-                .statusCode(400);
+        @Test
+        void 예약이_존재하는_테마는_삭제할_수_없다() {
+            RestAssured.given().log().all()
+                    .when().delete("/themes/1")
+                    .then().log().all()
+                    .statusCode(400);
+        }
     }
 }
