3bbb87221622d685208773188b39fee02193ea67
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-02 16:54:31 +0900

    refactor(CreateReservationRequest): 불필요한 주석 제거 및 레코드의 현재 생성자를 컴팩트 생성자로 변경
    Co-authored-by: seunghye218 <seunghye@student.42seoul.kr>

--- src/main/java/roomescape/reservation/dto/request/CreateReservationRequest.java
+++ src/main/java/roomescape/reservation/dto/request/CreateReservationRequest.java
@@ -6,15 +6,10 @@ import roomescape.reservationtime.model.ReservationTime;
 import roomescape.theme.model.Theme;
 
 public record CreateReservationRequest(LocalDate date, String name, Long timeId, Long themeId) {
-    public CreateReservationRequest(final LocalDate date, final String name, final Long timeId, final Long themeId) {
+    public CreateReservationRequest {
         if (date == null || name.isBlank() || timeId == null) {
             throw new IllegalArgumentException("올바른 예약이 아닙니다.");
         }
-        // 날짜 현재보다 이후인지
-        this.date = date;
-        this.name = name;
-        this.timeId = timeId;
-        this.themeId = themeId;
     }
 
     public Reservation toReservation(final ReservationTime reservationTime, final Theme theme) {
