76b61b15cffd13a1c486fad5917ad6ce3c6b8f9b
takoyakimchi <c4nyou@gmail.com>
2024-05-02 15:12:12 +0900

    refactor : 예외 메시지 변경
    - 테마, 시간을 삭제할 때 예약이 이미 존재하는 경우 발생하는 예외 메시지 변경
    
    Co-authored-by: jimi567 <repday0609@gmail.com>

--- src/main/java/roomescape/exception/ReservationExistsException.java
+++ src/main/java/roomescape/exception/ReservationExistsException.java
@@ -5,6 +5,6 @@ import org.springframework.dao.DataAccessException;
 public class ReservationExistsException extends DataAccessException {
 
     public ReservationExistsException() {
-        super("해당 시간을 사용하는 예약이 존재합니다.");
+        super("해당 데이터를 사용하는 예약이 존재합니다.");
     }
 }
