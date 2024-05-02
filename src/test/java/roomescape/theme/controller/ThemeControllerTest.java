7658243b04e00d3bcb8e160571dcbed71c6375c5
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-02 14:54:45 +0900

    test(ThemeControllerTest): 테마 컨트롤러 테스트 추가
    Co-authored-by: seunghye218 <seunghye@student.42seoul.kr>

---
+++ src/test/java/roomescape/theme/controller/ThemeControllerTest.java
@@ -0,0 +1,86 @@
+package roomescape.theme.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import roomescape.theme.dto.request.CreateThemeRequest;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+class ThemeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void createReservationTime() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/themes")
+                        .content(objectMapper.writeValueAsString(new CreateThemeRequest("마크", "도망갔다.", "https://abc.com")))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(header().stringValues("Location", "/themes/11"));
+    }
+
+    @Test
+    void getReservationTimes() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/themes")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].name").value("공포"))
+                .andExpect(jsonPath("$[0].description").value("무서워"))
+                .andExpect(jsonPath("$[0].thumbnail").value("https://sherlock-holmes.co.kr/attach/theme/16956118601.jpg"))
+
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].name").value("액션"))
+                .andExpect(jsonPath("$[1].description").value("신나"))
+                .andExpect(jsonPath("$[1].thumbnail").value("https://sherlock-holmes.co.kr/attach/theme/17000394031.jpg"))
+
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getReservationTime() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/themes/popular")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].name").value("공포"))
+                .andExpect(jsonPath("$[0].description").value("무서워"))
+                .andExpect(jsonPath("$[0].thumbnail").value("https://sherlock-holmes.co.kr/attach/theme/16956118601.jpg"))
+
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].name").value("액션"))
+                .andExpect(jsonPath("$[1].description").value("신나"))
+                .andExpect(jsonPath("$[1].thumbnail").value("https://sherlock-holmes.co.kr/attach/theme/17000394031.jpg"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteReservationTime() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/themes/10"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void deleteReservationTime_isConflict() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/themes/3"))
+                .andExpect(status().isConflict());
+    }
+}
