2f9d82a895d3427ca53f066c52aa7e60d71282d9
tsulocalize <tsulocalize@gmail.com>
2024-05-02 17:58:40 +0900

    충돌 해결

--- src/main/resources/static/js/ranking.js
+++ src/main/resources/static/js/ranking.js
@@ -1,15 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
+<<<<<<< HEAD
     requestRead('/themes/ranking') // 인기 테마 목록 조회 API endpoint
+=======
+    /*
+    TODO: [3단계] 인기 테마 - 인기 테마 목록 조회 API 호출
+    */
+    requestRead('/') // 인기 테마 목록 조회 API endpoint
+>>>>>>> 30a2fc7 (feat: setup project)
         .then(render)
         .catch(error => console.error('Error fetching times:', error));
 });
 
 function render(data) {
     const container = document.getElementById('theme-ranking');
+<<<<<<< HEAD
     data.forEach(theme => {
         const name = theme.name;
         const thumbnail = theme.thumbnail;
         const description = theme.description;
+=======
+
+    /*
+    TODO: [3단계] 인기 테마 - 인기 테마 목록 조회 API 호출 후 렌더링
+          response 명세에 맞춰 name, thumbnail, description 값 설정
+    */
+    data.forEach(theme => {
+        const name = '';
+        const thumbnail = '';
+        const description = '';
+>>>>>>> 30a2fc7 (feat: setup project)
 
         const htmlContent = `
             <img class="mr-3 img-thumbnail" src="${thumbnail}" alt="${name}">
