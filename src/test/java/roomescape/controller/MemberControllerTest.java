80df5e091636c7acd07ddd63912e2a6e5b4e4d3c
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-02 09:53:15 +0900

    feat: 사용자 예약 페이지 컨트롤러 구현

---
+++ src/test/java/roomescape/controller/MemberControllerTest.java
@@ -0,0 +1,35 @@
+package roomescape.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class MemberControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void init(){
+        RestAssured.port = port;
+    }
+
+    @DisplayName("사용자 예약 Page 접근 성공 테스트")
+    @Test
+    void responseReservationPage() {
+        Response response = RestAssured.given().log().all()
+                .when().get("/reservation")
+                .then().log().all().extract().response();
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+}
