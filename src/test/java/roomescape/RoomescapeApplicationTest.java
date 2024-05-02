6c5af9a1951014225179cf0a670de33e79ccee09
woojong <dwax1324@gmail.com>
2024-05-02 16:01:24 +0900

    test(RoomescapeApplicationTest): SpringBootTest의 인자 추가

--- src/test/java/roomescape/RoomescapeApplicationTest.java
+++ src/test/java/roomescape/RoomescapeApplicationTest.java
@@ -3,7 +3,7 @@ package roomescape;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RoomescapeApplicationTest.class)
 class RoomescapeApplicationTest {
 
         @Test
