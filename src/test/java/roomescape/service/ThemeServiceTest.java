c0416ff93ee77ccd26c24db684e28227d584cf1e
juha <khabh@naver.com>
2024-05-02 15:28:29 +0900

    test: ThemeService 기능 테스트

--- src/test/java/roomescape/service/ThemeServiceTest.java
+++ src/test/java/roomescape/service/ThemeServiceTest.java
@@ -3,19 +3,19 @@ package roomescape.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.jdbc.Sql;
 import roomescape.dto.ThemeResponse;
 import roomescape.dto.ThemeSaveRequest;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
-@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@Sql(scripts = {"/test.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class ThemeServiceTest {
 
     @Autowired
@@ -24,49 +24,56 @@ class ThemeServiceTest {
     @DisplayName("테마 저장")
     @Test
     void save() {
-        // given
         final ThemeSaveRequest themeSaveRequest = new ThemeSaveRequest("감자", "설명", "섬네일");
-        // when
         final ThemeResponse themeResponse = themeService.saveTheme(themeSaveRequest);
-
-        // then
-        assertThat(themeResponse).isEqualTo(new ThemeResponse(1L, "감자", "설명", "섬네일"));
+        assertThat(themeResponse).isEqualTo(new ThemeResponse(14L, "감자", "설명", "섬네일"));
     }
 
     @DisplayName("테마 조회")
     @Test
     void getThemes() {
-        // given
-        final ThemeSaveRequest themeSaveRequest = new ThemeSaveRequest("감자", "설명", "섬네일");
-        final ThemeResponse themeResponse = themeService.saveTheme(themeSaveRequest);
-
-        // when
         final List<ThemeResponse> themeResponses = themeService.getThemes();
-
-        // then
-        assertThat(themeResponses).hasSize(1)
-                .containsExactly(new ThemeResponse(1L, "감자", "설명", "섬네일"));
+        assertThat(themeResponses).hasSize(13);
     }
 
     @DisplayName("테마 삭제")
     @Test
     void deleteTheme() {
-        // given
-        final ThemeSaveRequest themeSaveRequest = new ThemeSaveRequest("감자", "설명", "섬네일");
-        final ThemeResponse themeResponse = themeService.saveTheme(themeSaveRequest);
-
-        // when
-        themeService.deleteTheme(themeResponse.id());
-
-        // then
-        assertThat(themeService.getThemes()).hasSize(0);
+        themeService.deleteTheme(13L);
+        assertThat(themeService.getThemes()).hasSize(12);
     }
 
     @DisplayName("존재하지 않는 테마 삭제")
     @Test
     void deleteNonExistTheme() {
+        assertThatThrownBy(() -> themeService.deleteTheme(14L))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("예약이 존재하는 테마 삭제")
+    @Test
+    void deleteReservationExistTheme() {
         assertThatThrownBy(() -> themeService.deleteTheme(1L))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("인기 테마 조회")
+    @Test
+    void getPopularThemes() {
+        final List<ThemeResponse> themeResponses = themeService.getPopularThemes(LocalDate.parse("2024-05-01"));
+        assertThat(themeResponses)
+                .hasSize(10)
+                .containsExactly(
+                        new ThemeResponse(1L, "이름1", "설명1", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"),
+                        new ThemeResponse(2L, "이름2", "설명2", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"),
+                        new ThemeResponse(3L, "이름3", "설명3", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"),
+                        new ThemeResponse(4L, "이름4", "설명4", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"),
+                        new ThemeResponse(5L, "이름5", "설명5", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"),
+                        new ThemeResponse(6L, "이름6", "설명6", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"),
+                        new ThemeResponse(7L, "이름7", "설명7", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"),
+                        new ThemeResponse(8L, "이름8", "설명8", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"),
+                        new ThemeResponse(9L, "이름9", "설명9", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"),
+                        new ThemeResponse(10L, "이름10", "설명10", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg")
+                );
+    }
 }
