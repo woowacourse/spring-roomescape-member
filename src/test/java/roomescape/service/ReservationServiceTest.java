e31468996e3528306f3f85add28b82591f8274b4
juha <khabh@naver.com>
2024-05-02 15:54:55 +0900

    test: ReservationService 기능 테스트

--- src/test/java/roomescape/service/ReservationServiceTest.java
+++ src/test/java/roomescape/service/ReservationServiceTest.java
@@ -4,27 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.jdbc.Sql;
 import roomescape.dto.ReservationResponse;
 import roomescape.dto.ReservationSaveRequest;
-import roomescape.model.Reservation;
-import roomescape.model.ReservationTime;
-import roomescape.model.Theme;
 import roomescape.repository.ReservationRepository;
-import roomescape.repository.ReservationTimeRepository;
-import roomescape.repository.ThemeRepository;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
-@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@Sql(scripts = {"/test.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class ReservationServiceTest {
 
     @Autowired
@@ -33,38 +25,34 @@ class ReservationServiceTest {
     @Autowired
     private ReservationRepository reservationRepository;
 
-    @Autowired
-    private ReservationTimeRepository reservationTimeRepository;
-
-    @Autowired
-    private ThemeRepository themeRepository;
-
-    @BeforeEach
-    void init() {
-        final ReservationTime time = reservationTimeRepository.save(new ReservationTime(LocalTime.parse("10:00")));
-        final Theme theme = themeRepository.save(new Theme("이름", "설명", "썸네일"));
-        reservationRepository.save(new Reservation("감자", LocalDate.parse("2025-05-13"), time, theme));
-    }
-
     @DisplayName("예약 목록 조회")
     @Test
     void getReservations() {
         final List<ReservationResponse> reservationResponses = reservationService.getReservations();
-        assertThat(reservationResponses.size()).isEqualTo(1);
+        assertThat(reservationResponses.size()).isEqualTo(13);
     }
 
     @DisplayName("예약 저장")
     @Test
     void saveReservation() {
         final ReservationSaveRequest reservationSaveRequest = new ReservationSaveRequest("고구마",  LocalDate.parse("2025-11-11"), 1L, 1L);
-        reservationService.saveReservation(reservationSaveRequest);
-        assertThat(reservationRepository.findById(2L)).isPresent();
+        final ReservationResponse reservationResponse = reservationService.saveReservation(reservationSaveRequest);
+        assertThat(reservationRepository.findById(reservationResponse.id())).isPresent();
     }
 
     @DisplayName("존재하지 않는 예약 시간으로 예약 저장")
     @Test
     void timeForSaveReservationNotFound() {
-        final ReservationSaveRequest reservationSaveRequest = new ReservationSaveRequest("고구마", LocalDate.parse("2025-11-11"), 2L, 1L);
+        final ReservationSaveRequest reservationSaveRequest = new ReservationSaveRequest("고구마", LocalDate.parse("2025-11-11"), 200L, 1L);
+        assertThatThrownBy(() -> {
+            reservationService.saveReservation(reservationSaveRequest);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("존재하지 않는 테마로 예약 저장")
+    @Test
+    void timeForSaveThemeNotFound() {
+        final ReservationSaveRequest reservationSaveRequest = new ReservationSaveRequest("고구마", LocalDate.parse("2025-11-11"), 200L, 1L);
         assertThatThrownBy(() -> {
             reservationService.saveReservation(reservationSaveRequest);
         }).isInstanceOf(IllegalArgumentException.class);
@@ -74,14 +62,14 @@ class ReservationServiceTest {
     @Test
     void deleteReservation() {
         reservationService.deleteReservation(1L);
-        assertThat(reservationService.getReservations().size()).isEqualTo(0);
+        assertThat(reservationRepository.findById(1L)).isEmpty();
     }
 
     @DisplayName("존재하지 않는 예약 삭제")
     @Test
     void deleteReservationNotFound() {
         assertThatThrownBy(() -> {
-            reservationService.deleteReservation(2L);
+            reservationService.deleteReservation(200L);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -90,14 +78,6 @@ class ReservationServiceTest {
     void saveReservationWithGoneTime() {
         assertThatThrownBy(() -> {
             reservationService.saveReservation(new ReservationSaveRequest("백호", LocalDate.parse("2023-11-11"), 1L, 1L));
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("중복된 예약 저장")
-    @Test
-    void saveDuplicatedReservation() {
-        assertThatThrownBy(() -> {
-            reservationService.saveReservation(new ReservationSaveRequest("호롤로", LocalDate.parse("2025-05-13"), 1L, 1L));
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
