5624674eb3686dae9abe0406ed3b5f852b1afe72
DobbyMac <kimdobby@g.skku.edu>
2024-05-02 17:44:08 +0900

    feat(ThemeService): 인기 테마 기능 조회 기능 구현

--- src/main/java/roomescape/theme/service/ThemeService.java
+++ src/main/java/roomescape/theme/service/ThemeService.java
@@ -1,19 +1,28 @@
 package roomescape.theme.service;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import roomescape.reservation.dao.ReservationDao;
 import roomescape.theme.dao.ThemeDao;
 import roomescape.theme.domain.Theme;
+import roomescape.theme.dto.ThemeRankResponse;
 import roomescape.theme.dto.ThemeRequest;
 import roomescape.theme.dto.ThemeResponse;
 
 @Service
 public class ThemeService {
 
-    private final ThemeDao themeDao;
+    public static final int NUMEBER_OF_ONE_WEEK = 7;
+    public static final int NUMBER_OF_ONE_DAY = 1;
+    public static final int TOP_THEMES_LIMIT = 10;
 
-    public ThemeService(ThemeDao themeDao) {
+    private final ThemeDao themeDao;
+    private final ReservationDao reservationDao;
+
+    public ThemeService(ThemeDao themeDao, ReservationDao reservationDao) {
         this.themeDao = themeDao;
+        this.reservationDao = reservationDao;
     }
 
     public ThemeResponse addTheme(ThemeRequest themeRequest) {
@@ -30,6 +39,20 @@ public class ThemeService {
         List<Theme> themes = themeDao.findAllThemes();
         return themes.stream()
                 .map(ThemeResponse::fromTheme)
+                .toList();
+    }
+
+    public List<ThemeRankResponse> findRankedThemes() {
+        LocalDate today = LocalDate.now()
+                .minusDays(NUMBER_OF_ONE_DAY);
+        LocalDate beforeOneWeek = today.minusDays(NUMEBER_OF_ONE_WEEK);
+
+        List<Theme> rankedThemes = reservationDao.findThemeByDateOrderByThemeIdCount(beforeOneWeek, today);
+        System.out.println(rankedThemes);
+        return rankedThemes.stream()
+                .sequential()
+                .limit(TOP_THEMES_LIMIT)
+                .map(ThemeRankResponse::fromTheme)
                 .toList();
     }
 }
