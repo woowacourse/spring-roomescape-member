79cdb09528aede0700f842fc37e0a2d8354376a4
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-02 13:58:18 +0900

    refactor(ReservationService): 메서드명 및 순서 변경

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -34,7 +34,7 @@ public class ReservationService {
         ReservationTime reservationTime = findReservationTime(reservationRequest);
         Theme theme = findTheme(reservationRequest);
         validateReservationNotDuplicate(reservationRequest);
-        validateUnpassedDate(reservationRequest.date(),reservationTime.getStartAt());
+        validateUnPassedDate(reservationRequest.date(),reservationTime.getStartAt());
         Reservation reservationToSave = reservationRequest.toEntity(reservationTime, theme);
         return reservationRepository.save(reservationToSave);
     }
@@ -57,18 +57,6 @@ public class ReservationService {
         reservationRepository.delete(id);
     }
 
-    private void validateUnpassedDate(LocalDate date, LocalTime time) {
-        if (date.isBefore(LocalDate.now()) && time.isBefore(LocalTime.now())) {
-            throw new IllegalArgumentException("[ERROR] 지나간 날짜와 시간에 대한 예약 생성은 불가능합니다. : " + date + " " + time);
-        }
-    }
-
-    public void validateIdExist(Long id) {
-        if (!reservationRepository.existId(id)) {
-            throw new IllegalArgumentException("[ERROR] id가 존재하지 않습니다 : " + id);
-        }
-    }
-
     private ReservationTime findReservationTime(ReservationRequest reservationRequest) {
         Long timeId = reservationRequest.timeId();
         if (!timeRepository.existId(timeId)) {
@@ -86,8 +74,25 @@ public class ReservationService {
     }
 
     private void validateReservationNotDuplicate(ReservationRequest reservationRequest) {
-        if (reservationRepository.existDateAndTimeId(reservationRequest.date(), reservationRequest.timeId())) {
-            throw new IllegalArgumentException("[ERROR] 이미 해당 시간에 예약이 존재합니다.");
+        if (reservationRepository.existDateTimeAndTheme(
+                reservationRequest.date(),
+                reservationRequest.timeId(),
+                reservationRequest.themeId())
+        ) {
+            throw new IllegalArgumentException("[ERROR] 이미 해당 시간에 동일한 테마의 예약이 존재합니다.");
+        }
+    }
+
+    private void validateUnPassedDate(LocalDate date, LocalTime time) {
+        if ((date.isBefore(LocalDate.now()) && time.isBefore(LocalTime.now())) ||
+                date.isEqual(LocalDate.now()) && time.isBefore(LocalTime.now())) {
+            throw new IllegalArgumentException("[ERROR] 지나간 날짜와 시간에 대한 예약 생성은 불가능합니다. : " + date + " " + time);
+        }
+    }
+
+    public void validateIdExist(Long id) {
+        if (!reservationRepository.existId(id)) {
+            throw new IllegalArgumentException("[ERROR] id가 존재하지 않습니다 : " + id);
         }
     }
 }
