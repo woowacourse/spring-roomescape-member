39dbed56e368b831f94c7e24a2c863cafaa50436
eunjungL <dldmswjd322@gmail.com>
2024-05-03 17:12:26 +0900

    style: UserReservationController 띄어쓰기 수정

--- src/main/java/roomescape/controller/UserReservationController.java
+++ src/main/java/roomescape/controller/UserReservationController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping("/reservation")
 public class UserReservationController {
+
     @GetMapping
     public String readUserReservation() {
         return "reservation";
