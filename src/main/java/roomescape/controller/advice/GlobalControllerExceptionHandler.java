06513241d2b59d284862314f3ef14f67f5e0cbdc
Arachneee <jhg2819@naver.com>
2024-05-03 22:27:09 +0900

    refactor: 날짜 형식 예외 메시지 수정

--- src/main/java/roomescape/controller/advice/GlobalControllerExceptionHandler.java
+++ src/main/java/roomescape/controller/advice/GlobalControllerExceptionHandler.java
@@ -1,9 +1,9 @@
 package roomescape.controller.advice;
 
+import java.time.format.DateTimeParseException;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -16,15 +16,17 @@ public class GlobalControllerExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Order(1)
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> handleHttpMessageNotReadableException(final HttpMessageNotReadableException e) {
         logger.error(e.getMessage(), e);
-        final String input = e.getMessage().split("\"")[1];
-        return ResponseEntity.badRequest().body(input + "의 형식이 잘못되었습니다.");
+        final Throwable rootCause = e.getRootCause();
+        if (rootCause instanceof DateTimeParseException dateTimeParseException) {
+            final String parsedString = dateTimeParseException.getParsedString();
+            return ResponseEntity.badRequest().body(parsedString + " 의 형식이 잘못됐습니다.");
+        }
+        return ResponseEntity.badRequest().body("json의 형식이 잘못됐습니다.");
     }
 
-    @Order(2)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
         logger.error(e.getMessage(),e);
@@ -36,14 +38,12 @@ public class GlobalControllerExceptionHandler {
         return ResponseEntity.badRequest().body(errorMessage);
     }
 
-    @Order(3)
     @ExceptionHandler(ReservationBusinessException.class)
     public ResponseEntity<String> handleIllegalArgument(final ReservationBusinessException e) {
         logger.error(e.getMessage(), e);
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
-    @Order(4)
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(final Exception e) {
         logger.error(e.getMessage(), e);
