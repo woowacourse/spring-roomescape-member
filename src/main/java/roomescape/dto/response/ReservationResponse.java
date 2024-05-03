7ec977d4bd9352b49eb3034f5ed721d32c4501ea
devhoya97 <diajbh123@gmail.com>
2024-05-03 15:56:30 +0900

    fix: js에서 사용하는 변수명과 DTO 필드명 매칭

--- src/main/java/roomescape/dto/response/ReservationResponse.java
+++ src/main/java/roomescape/dto/response/ReservationResponse.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 import roomescape.domain.Reservation;
 import roomescape.domain.Theme;
 
-public record ReservationResponse(Long id, String name, String date, ReservationTimeResponse timeResponse, ThemeResponse themeResponse) {
+public record ReservationResponse(Long id, String name, String date, ReservationTimeResponse time, ThemeResponse theme) {
 
     public static ReservationResponse from(Reservation reservation) {
         return new ReservationResponse(
