2b5ac7030b5192d00cd94785b845e2abfd0c5d39
hyeonjilee <skylar12200@gmail.com>
2024-05-03 20:58:20 +0900

    feat: 테마 도메인 빈값 검증 추가

--- src/main/java/roomescape/domain/Theme.java
+++ src/main/java/roomescape/domain/Theme.java
@@ -9,10 +9,31 @@ public class Theme {
     private final String thumbnail;
 
     public Theme(Long id, String name, String description, String thumbnail) {
+        validateName(name);
+        validateDescription(description);
+        validateThumbnail(thumbnail);
         this.id = id;
         this.name = name;
         this.description = description;
         this.thumbnail = thumbnail;
+    }
+
+    private void validateName(final String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 테마 이름은 비어있을 수 없습니다.");
+        }
+    }
+
+    private void validateDescription(final String description) {
+        if (description.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 테마 설명은 비어있을 수 없습니다.");
+        }
+    }
+
+    private void validateThumbnail(final String thumbnail) {
+        if (thumbnail.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 테마 썸네일은 비어있을 수 없습니다.");
+        }
     }
 
     public Long getId() {
