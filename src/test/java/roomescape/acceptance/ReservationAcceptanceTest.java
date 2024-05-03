0489a207a17793192055e37a999f9ed75318aad4
unifolio0 <boho82ha@gmail.com>
2024-05-03 15:29:11 +0900

    refactor: Sql 어노테이션 클래스 상단으로 이동

--- src/test/java/roomescape/acceptance/ReservationAcceptanceTest.java
+++ src/test/java/roomescape/acceptance/ReservationAcceptanceTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 import org.springframework.test.context.jdbc.Sql;
 
-    @Sql("/init-for-reservation.sql")
+@Sql("/init-for-reservation.sql")
 public class ReservationAcceptanceTest extends BasicAcceptanceTest {
     @TestFactory
     @DisplayName("예약을 추가한다")
@@ -26,7 +26,6 @@ public class ReservationAcceptanceTest extends BasicAcceptanceTest {
     }
 
     @TestFactory
-    @Sql("/init-for-reservation.sql")
     @DisplayName("과거 시간에 대한 예약을 하면, 예외가 발생한다")
     Stream<DynamicTest> pastTimeReservationTest() {
         LocalDate yesterday = LocalDate.now().minusDays(1);
@@ -37,7 +36,6 @@ public class ReservationAcceptanceTest extends BasicAcceptanceTest {
     }
 
     @TestFactory
-    @Sql("/init-for-reservation.sql")
     @DisplayName("이미 예약된 테마와 시간을 예약을 하면, 예외가 발생한다")
     Stream<DynamicTest> duplicateReservationTest() {
         LocalDate tomorrow = LocalDate.now().plusDays(1);
@@ -67,3 +65,4 @@ public class ReservationAcceptanceTest extends BasicAcceptanceTest {
         return 0L;
     }
 }
+
