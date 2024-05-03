ba345d3f36ca0e8d325d2d224e04a8407125f8ee
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-05-03 20:46:44 +0900

    refactor: 불필요한 필드 제거

--- src/main/java/roomescape/exception/NotFoundException.java
+++ src/main/java/roomescape/exception/NotFoundException.java
@@ -1,15 +1,8 @@
 package roomescape.exception;
 
 public class NotFoundException extends RuntimeException {
-    private final String message;
 
     public NotFoundException(String message) {
         super(message);
-        this.message = message;
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
     }
 }
