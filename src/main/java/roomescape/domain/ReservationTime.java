609215a88bc063d5f1cdaf57821608ffd1588c48
donghoony <aru0504@naver.com>
2024-05-03 17:12:45 +0900

    refactor: 주생성자 순서 상단 배치

--- src/main/java/roomescape/domain/ReservationTime.java
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -7,16 +7,16 @@ public class ReservationTime {
     private final Long id;
     private final LocalTime startAt;
 
-    public ReservationTime(LocalTime startAt) {
-        this(null, startAt);
-    }
-
     public ReservationTime(Long id, LocalTime startAt) {
         if (startAt == null) {
             throw new IllegalArgumentException("예약 시간은 필수 입력값 입니다.");
         }
         this.id = id;
         this.startAt = startAt;
+    }
+
+    public ReservationTime(LocalTime startAt) {
+        this(null, startAt);
     }
 
     public Long getId() {
