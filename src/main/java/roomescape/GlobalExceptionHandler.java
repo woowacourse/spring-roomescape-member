53656f79093bf02c8bd366dbad50ece6c499b1a9
unifolio0 <boho82ha@gmail.com>
2024-05-03 15:52:45 +0900

    refactor: 기타 예외 발생 시 500 상태코드 반환

--- src/main/java/roomescape/GlobalExceptionHandler.java
+++ src/main/java/roomescape/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package roomescape;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,7 +9,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler
+    @Order(1)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    @Order(2)
+    public ResponseEntity<String> handelException(Exception e) {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
