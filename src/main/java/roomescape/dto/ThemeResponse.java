11bd2a9e2986c20f7c0b1562452bd754f09e86b3
libienz <leekh10195@gmail.com>
2024-05-03 19:46:40 +0900

    feat: ThemeResponse dto 구현

---
+++ src/main/java/roomescape/dto/ThemeResponse.java
@@ -0,0 +1,9 @@
+package roomescape.dto;
+
+import roomescape.domain.Theme;
+
+public record ThemeResponse(Long id, String name, String description, String thumbnail) {
+    public ThemeResponse(Theme theme) {
+        this(theme.getId(), theme.getName(), theme.getDescription(), theme.getThumbnail());
+    }
+}
