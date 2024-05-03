191708171811d7f99ecd492f0aa9260294ed443d
yoonjuho <dbswn990@naver.com>
2024-05-03 15:27:40 +0900

    refactor: 응답 형식 명확하게 정의

--- src/main/java/roomescape/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/handler/GlobalExceptionHandler.java
@@ -4,9 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.exception.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = DuplicateReservationException.class)
