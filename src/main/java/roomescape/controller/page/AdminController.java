a308a777a508e63812b6edad0ce2633725d57c3a
3juhwan <13selfesteem91@naver.com>
2024-05-03 14:04:52 +0900

    refactor: 어드민 페이지 컨트롤러에 @RequestMapping 삭제

--- src/main/java/roomescape/controller/page/AdminController.java
+++ src/main/java/roomescape/controller/page/AdminController.java
@@ -2,27 +2,25 @@ package roomescape.controller.page;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
 public class AdminController {
-    @GetMapping
+    @GetMapping("/admin")
     public String home() {
         return "admin/index";
     }
 
-    @GetMapping("/reservation")
+    @GetMapping("/admin/reservation")
     public String reservation() {
         return "admin/reservation-new";
     }
 
-    @GetMapping("/time")
+    @GetMapping("/admin/time")
     public String time() {
         return "admin/time";
     }
 
-    @GetMapping("/theme")
+    @GetMapping("/admin/theme")
     public String theme() {
         return "admin/theme";
     }
