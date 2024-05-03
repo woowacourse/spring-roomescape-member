874f95bd7f57d4b29ac7dc5f97f54f098c1afcf7
hangillee <skfcb10@naver.com>
2024-05-03 17:57:58 +0900

    feat: IllegalArgumentException 이외의 예외에 대한 핸들러 메소드 구현

--- src/main/java/roomescape/web/controller/ControllerExceptionHandler.java
+++ src/main/java/roomescape/web/controller/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package roomescape.web.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,5 +11,11 @@ public class ControllerExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<String> handleReservationTimeException(final IllegalArgumentException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ProblemDetail> handleRuntimeException(final RuntimeException exception) {
+        return ResponseEntity.internalServerError()
+                .body(ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage()));
     }
 }
