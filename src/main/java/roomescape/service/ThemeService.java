8b0d02227f4a061b4aec436fa7b8981dc5d80477
hyxrxn <gpfls981220@gmail.com>
2024-05-04 22:00:31 +0900

    refactor(ThemeService): 가독성을 위해 개행 위치 수정

--- src/main/java/roomescape/service/ThemeService.java
+++ src/main/java/roomescape/service/ThemeService.java
@@ -28,8 +28,10 @@ public class ThemeService {
     }
 
     public List<ThemeResponseDto> findTopBookedThemes() {
-        List<Theme> topBookedThemes = themeRepository.findTopBookedThemes(LocalDate.now().minusDays(7),
-                LocalDate.now().minusDays(1), 10);
+        List<Theme> topBookedThemes = themeRepository.findTopBookedThemes(
+                LocalDate.now().minusDays(7),
+                LocalDate.now().minusDays(1),
+                10);
 
         return topBookedThemes.stream()
                 .map(ThemeResponseDto::new)
