f24142f70920b6e3293c44ee2964ef38cdb844f1
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-04 20:37:37 +0900

    refactor(ReservationService): 메서드명 변경

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -39,15 +39,17 @@ public class ReservationService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 테마가 존재하지 않습니다."));
         Reservation reservation = dto.createReservation(time, theme);
 
-        validate(reservation);
+        validateReservationTime(reservation);
+
         Reservation createdReservation = reservationDao.createReservation(reservation);
         return ReservationResponse.from(createdReservation);
     }
 
-    private void validate(Reservation reservation) {
+    private void validateReservationTime(Reservation reservation) {
         if (reservation.isBefore(LocalDateTime.now())) {
             throw new IllegalArgumentException("예약은 현재 시간 이후여야 합니다.");
         }
+
         if (reservationDao.isExistReservationByDateAndTimeIdAndThemeId(
                 reservation.date(), reservation.findTimeId(), reservation.findThemeId())) {
             throw new IllegalArgumentException("해당 시간대 해당 테마 예약은 이미 존재합니다.");
