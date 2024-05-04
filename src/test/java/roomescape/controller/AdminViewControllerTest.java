67bef0e93da634893a221c6ffdf7b76272cef2f8
libienz <leekh10195@gmail.com>
2024-05-04 21:16:00 +0900

    test: 테마 페이지 응답 테스트 추가 및 테스트 디스플레이 수정

--- src/test/java/roomescape/controller/AdminViewControllerTest.java
+++ src/test/java/roomescape/controller/AdminViewControllerTest.java
@@ -8,38 +8,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class AdminViewControllerTest {
 
-    @DisplayName("/admin get 요청 시 응답할 수 있다")
+    @DisplayName("어드민 메인 페이지 요청 테스트 - 200 OK")
     @Test
-    void should_response_200_when_request_admin_page() {
+    void should_response_200_when_requested_admin_page() {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
                 .statusCode(200);
     }
 
-    @DisplayName("웰컴페이지 요청 시 응답할 수 있다")
+    @DisplayName("어드민 예약 페이지 요청 테스트 - 200 OK")
     @Test
-    void should_response_200_when_request_welcome_page() {
-        RestAssured.given().log().all()
-                .when().get("/")
-                .then().log().all()
-                .statusCode(200);
-    }
-
-    @DisplayName("/admin/reservation get 요청 시 응답할 수 있다")
-    @Test
-    void should_response_200_when_request_reservation_page() {
+    void should_response_200_when_requested_reservation_page() {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
     }
 
-    @DisplayName("/admin/time get 요청 시 응답할 수 있다")
+    @DisplayName("어드민 시간 관리 페이지 요청 테스트 - 200 OK")
     @Test
-    void should_response_200_when_request_time_page() {
+    void should_response_200_when_requested_time_page() {
         RestAssured.given().log().all()
                 .when().get("/admin/time")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @DisplayName("어드민 테마 관리 페이지 요청 테스트 - 200 OK")
+    @Test
+    void should_response_200_when_requested_theme_page() {
+        RestAssured.given().log().all()
+                .when().get("/admin/theme")
                 .then().log().all()
                 .statusCode(200);
     }
