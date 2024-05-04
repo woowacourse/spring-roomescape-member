bd82b56d3d21e47dec6fedcf8c1241ae9600d605
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-05 01:41:37 +0900

    chore: 스키마 수정

--- src/test/resources/popular_themes_data.sql
+++ src/test/resources/popular_themes_data.sql
@@ -14,8 +14,7 @@ ALTER TABLE theme
     ALTER COLUMN id RESTART;
 
 INSERT INTO reservation_time (start_at)
-VALUES ('12:00'),
-       ('11:00');
+VALUES ('12:00');
 
 INSERT INTO theme (name, description, thumbnail)
 VALUES ('레벨1 탈출',
@@ -23,8 +22,13 @@ VALUES ('레벨1 탈출',
         'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
        ('레벨2 탈출',
         '우테코 레벨2를 탈출하는 내용입니다.',
+        'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       ('레벨3 탈출',
+        '우테코 레벨2를 탈출하는 내용입니다.',
         'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg')
 ;
 
 INSERT INTO reservation (name, date, time_id, theme_id)
-VALUES ('릴리', '2222-05-04', 1, 1);
+VALUES ('릴리', '2024-04-30', 1, 1),
+       ('리니', '2024-05-04', 1, 1),
+       ('엘라', '2222-04-03', 1, 2);
