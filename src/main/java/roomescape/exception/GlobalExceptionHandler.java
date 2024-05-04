dd2b3a1220ab8b45811dd96cccf0e1cce731e145
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-05-04 18:57:51 +0900

    refactor(GlobalExceptionHandler): 어노테이션의 value옵션 삭제

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -10,22 +10,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = NotFoundException.class)
+    @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleNotFoundException(NotFoundException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(value = BadRequestException.class)
+    @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<String> handleBadRequestException(BadRequestException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(value = DuplicatedException.class)
+    @ExceptionHandler(DuplicatedException.class)
     public ResponseEntity<String> handleDuplicatedException(DuplicatedException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(value = DateTimeParseException.class)
+    @ExceptionHandler(DateTimeParseException.class)
     public ResponseEntity<String> handleDateTimeParseException(DateTimeParseException exception) {
         return new ResponseEntity<>("[ERROR] 날짜와 시간 형식이 맞지 않습니다.", HttpStatus.BAD_REQUEST);
     }
