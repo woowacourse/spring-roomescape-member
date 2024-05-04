15239648d54c85968ffbef162c930c6315410e08
unifolio0 <boho82ha@gmail.com>
2024-05-04 19:09:21 +0900

    test: ThemeRepository 단위 테스트 작성

---
+++ src/test/java/roomescape/repository/H2ThemeRepositoryTest.java
@@ -0,0 +1,52 @@
+package roomescape.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+import roomescape.BasicAcceptanceTest;
+import roomescape.TestFixtures;
+import roomescape.domain.Theme;
+
+@Sql("/setTheme.sql")
+class H2ThemeRepositoryTest extends BasicAcceptanceTest {
+    @Autowired
+    private ThemeRepository themeRepository;
+
+    @DisplayName("DB에 저장된 전체 테마를 반환한다")
+    @Test
+    void findAll() {
+        List<Theme> expectedThemes = List.of(TestFixtures.THEME_1, TestFixtures.THEME_2);
+
+        assertThat(themeRepository.findAll()).isEqualTo(expectedThemes);
+    }
+
+    @DisplayName("해당 id의 테마를 반환한다")
+    @Test
+    void findById() {
+        assertThat(themeRepository.findById(1L).orElseThrow()).isEqualTo(TestFixtures.THEME_1);
+    }
+
+    @DisplayName("테마를 DB에 저장한다")
+    @Test
+    void save() {
+        themeRepository.save(TestFixtures.THEME_3);
+
+        List<Theme> expectedThemes = List.of(TestFixtures.THEME_1, TestFixtures.THEME_2, TestFixtures.THEME_3);
+
+        assertThat(themeRepository.findAll()).isEqualTo(expectedThemes);
+    }
+
+    @DisplayName("해당 id의 테마를 삭제한다")
+    @Test
+    void deleteById() {
+        themeRepository.deleteById(1L);
+
+        List<Theme> expectedThemes = List.of(TestFixtures.THEME_2);
+
+        assertThat(themeRepository.findAll()).isEqualTo(expectedThemes);
+    }
+}
