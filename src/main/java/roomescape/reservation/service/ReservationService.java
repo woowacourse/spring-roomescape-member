afb6895966def01c9865386bb06b173313bad7f0
DobbyMac <kimdobby@g.skku.edu>
2024-05-05 04:41:53 +0900

    feat(ReservationService): 중복 예약 검증 기능 구현

--- src/main/java/roomescape/reservation/service/ReservationService.java
+++ src/main/java/roomescape/reservation/service/ReservationService.java
@@ -3,6 +3,7 @@ package roomescape.reservation.service;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import roomescape.exception.ConflictException;
 import roomescape.reservation.dao.ReservationDao;
 import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.dto.ReservationRequest;
@@ -18,17 +19,22 @@ public class ReservationService {
     private final TimeDao timeDao;
 
     public ReservationService(ReservationDao reservationDao, TimeDao timeDao) {
-        this.reservationDao = reservationDao;
-        this.timeDao = timeDao;
+        this.reservationDao = reservationDao; this.timeDao = timeDao;
     }
 
     public ReservationResponse addReservation(ReservationRequest reservationRequest) {
         Reservation reservation = reservationRequest.toReservation();
-        Time time = timeDao.findById(reservation.getReservationTime()
-                .getId());
-        reservation.setTime(time);
+        Time time = timeDao.findById(reservation.getReservationTimeId());
+        validateDuplicateReservation(reservationRequest, time); reservation.setTime(time);
         Reservation savedReservation = reservationDao.save(reservation);
         return ReservationResponse.fromReservation(savedReservation);
+    }
+
+    private void validateDuplicateReservation(ReservationRequest reservationRequest, Time time) {
+        List<Time> bookedTimes = getBookedTimesOfThemeAtDate(reservationRequest.themeId(), reservationRequest.date());
+        if (isTimeBooked(time, bookedTimes)) {
+            throw new ConflictException("해당 시간에 예약이 존재합니다.");
+        }
     }
 
     public List<ReservationResponse> findReservations() {
@@ -41,12 +47,16 @@ public class ReservationService {
 
     public List<ReservationTimeAvailabilityResponse> findTimeAvailability(long themeId, LocalDate date) {
         List<Time> allTimes = timeDao.findAllReservationTimesInOrder();
-        List<Reservation> reservations = reservationDao.findAllByThemeIdAndDate(themeId, date);
-        List<Time> bookedTimes = extractReservationTimes(reservations);
+        List<Time> bookedTimes = getBookedTimesOfThemeAtDate(themeId, date);
 
         return allTimes.stream()
                 .map(time -> ReservationTimeAvailabilityResponse.fromTime(time, isTimeBooked(time, bookedTimes)))
                 .toList();
+    }
+
+    private List<Time> getBookedTimesOfThemeAtDate(long themeId, LocalDate date) {
+        List<Reservation> reservationsOfThemeInDate = reservationDao.findAllByThemeIdAndDate(themeId, date);
+        return extractReservationTimes(reservationsOfThemeInDate);
     }
 
     private List<Time> extractReservationTimes(List<Reservation> reservations) {
