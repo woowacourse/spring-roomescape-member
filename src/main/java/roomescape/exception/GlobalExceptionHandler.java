a27c883a47b9a359300d7ecfa0738c826bf883b1
libienz <leekh10195@gmail.com>
2024-05-04 20:31:34 +0900

    refactor: 전역 예외 처리기 예외 response 구현 및 로그 구체화 개선

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package roomescape.exception;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,20 +16,27 @@ public class GlobalExceptionHandler {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler
-    public ResponseEntity<String> handle(MethodArgumentNotValidException e) {
-        log.info(e.getMessage());
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+        String errorMessage = fieldErrors.stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining());
+
+        log.info("Bad Request: {}", errorMessage, e);
+        return ResponseEntity.badRequest().body(new ErrorResponse(errorMessage));
     }
 
     @ExceptionHandler
-    public ResponseEntity<String> handle(IllegalRequestException e) {
-        log.info(e.getMessage());
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handle(IllegalRequestException e) {
+        log.info("Bad Request: {}", e.getMessage(), e);
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleInternalServerError(Exception e) {
-        log.error(e.getMessage());
-        return ResponseEntity.internalServerError().body("서버 관리자에게 문의하세요");
+    public ResponseEntity<ErrorResponse> handleInternalServerError(Exception e) {
+        log.error("Internal Server Error 발생: {}", e.getMessage(), e);
+        return ResponseEntity.internalServerError().body(new ErrorResponse("서버 관리자에게 문의하세요"));
     }
 }
