a2baf1f5635c6ff1085c3fb6e453237d698ac7d4
HoeSeong123 <ghltjd3@gmail.com>
2024-05-04 20:48:13 +0900

    refactor : 의미 있는 숫자 상수화

--- src/main/java/roomescape/domain/UserName.java
+++ src/main/java/roomescape/domain/UserName.java
@@ -2,6 +2,7 @@ package roomescape.domain;
 
 public class UserName {
 
+    private static final int MAX_NAME_LENGTH = 20;
     private static final String NAME_FORMAT = "^[a-zA-Z가-힣ㄱ-ㅎ0-9]*$";
 
     private final String userName;
@@ -12,8 +13,8 @@ public class UserName {
     }
 
     private void validate(String userName) {
-        if (userName.isBlank() || userName.length() > 20) {
-            throw new IllegalArgumentException("예약자명은 1글자 이상, 20글자 이하로 작성해야 합니다.");
+        if (userName.isBlank() || userName.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("예약자명은 1글자 이상, " + MAX_NAME_LENGTH + "글자 이하로 작성해야 합니다.");
         }
         if (!userName.matches(NAME_FORMAT)) {
             throw new IllegalArgumentException("예약자명은 한글, 영문, 숫자만 허용합니다.");
