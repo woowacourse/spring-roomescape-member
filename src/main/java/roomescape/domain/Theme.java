4f6b06db9a54553e4a74d49419d7bc5ae56cca57
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-05-04 11:26:17 +0900

    refactor(domain): 각 필드의 validation을 위해 값 객체 도입

--- src/main/java/roomescape/domain/Theme.java
+++ src/main/java/roomescape/domain/Theme.java
@@ -5,27 +5,25 @@ import java.util.Objects;
 public class Theme {
     private final Long id;
     private final ThemeName name;
-    private final String description;
-    private final String thumbnail;
+    private final ThemeDescription description;
+    private final ThemeThumbnail thumbnail;
 
     public Theme(Long id, String name, String description, String thumbnail) {
-        final String errorMessage = "인자 중 null 값이 존재합니다.";
-        this.id = Objects.requireNonNull(id, errorMessage);
+        this.id = Objects.requireNonNull(id, "인자 중 null 값이 존재합니다.");
         this.name = new ThemeName(name);
-        this.description = Objects.requireNonNull(description, errorMessage);
-        this.thumbnail = Objects.requireNonNull(thumbnail, errorMessage);
+        this.description = new ThemeDescription(description);
+        this.thumbnail = new ThemeThumbnail(thumbnail);
     }
 
     public Theme(String name, String description, String thumbnail) {
-        final String errorMessage = "인자 중 null 값이 존재합니다.";
         this.id = null;
         this.name = new ThemeName(name);
-        this.description = Objects.requireNonNull(description, errorMessage);
-        this.thumbnail = Objects.requireNonNull(thumbnail, errorMessage);
+        this.description = new ThemeDescription(description);
+        this.thumbnail = new ThemeThumbnail(thumbnail);
     }
 
     public Theme changeId(Long id) {
-        return new Theme(id, this.name.value(), this.description, this.thumbnail);
+        return new Theme(id, this.name.value(), this.description.value(), this.thumbnail.value());
     }
 
     public Long getId() {
@@ -37,10 +35,10 @@ public class Theme {
     }
 
     public String getDescription() {
-        return description;
+        return description.value();
     }
 
     public String getThumbnail() {
-        return thumbnail;
+        return thumbnail.value();
     }
 }
---
+++ src/test/java/roomescape/domain/ThemeThumbnailTest.java
@@ -0,0 +1,24 @@
+package roomescape.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ThemeThumbnailTest {
+    @DisplayName("테마 설명이 null일 경우 예외를 던진다.")
+    @Test
+    void validateTest_whenValueIsNull() {
+        assertThatThrownBy(() -> new ThemeThumbnail(null))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("인자 중 null 값이 존재합니다.");
+    }
+
+    @DisplayName("테마 설명이 \"\"일 경우 예외를 던진다.")
+    @Test
+    void validateTest_whenValueIsEmpty() {
+        assertThatThrownBy(() -> new ThemeThumbnail(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("테마 썸네일은 한글자 이상이어야 합니다.");
+    }
+}
