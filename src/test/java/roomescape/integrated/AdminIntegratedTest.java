2d60aeb15f2a4641df5592bcfa6d8d8d9ba2042c
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-05-04 14:43:23 +0900

    style(AdminIntegratedTest): 가독성 증대를 위해, 줄바꿈 문자 추가

--- src/test/java/roomescape/integrated/AdminIntegratedTest.java
+++ src/test/java/roomescape/integrated/AdminIntegratedTest.java
@@ -20,24 +20,36 @@ class AdminIntegratedTest {
     @DisplayName("관리자 페이지를 열 수 있다.")
     @Test
     void loadAdminPage() {
-        RestAssured.given().log().all().when().get("/admin").then().log().all().statusCode(200);
+        RestAssured.given().log().all()
+                .when().get("/admin")
+                .then().log().all()
+                .statusCode(200);
     }
 
     @DisplayName("예약 페이지를 열 수 있다.")
     @Test
     void loadReservationPage() {
-        RestAssured.given().log().all().when().get("/admin/reservation").then().log().all().statusCode(200);
+        RestAssured.given().log().all()
+                .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200);
     }
 
     @DisplayName("시간 관리 페이지를 열 수 있다.")
     @Test
     void loadTimePage() {
-        RestAssured.given().log().all().when().get("/admin/time").then().log().all().statusCode(200);
+        RestAssured.given().log().all()
+                .when().get("/admin/time")
+                .then().log().all()
+                .statusCode(200);
     }
 
     @DisplayName("테마 관리 페이지를 열 수 있다.")
     @Test
     void loadThemePage() {
-        RestAssured.given().log().all().when().get("/admin/theme").then().log().all().statusCode(200);
+        RestAssured.given().log().all()
+                .when().get("/admin/theme")
+                .then().log().all()
+                .statusCode(200);
     }
 }
