992f0a01221ab6463b383c706b89088fa25589d5
HoeSeong123 <ghltjd3@gmail.com>
2024-05-04 21:14:49 +0900

    refactor : 정규식 사용 방법 변경

--- src/main/java/roomescape/domain/UserName.java
+++ src/main/java/roomescape/domain/UserName.java
@@ -1,9 +1,11 @@
 package roomescape.domain;
 
+import java.util.regex.Pattern;
+
 public class UserName {
 
     private static final int MAX_NAME_LENGTH = 20;
-    private static final String NAME_FORMAT = "^[a-zA-Z가-힣ㄱ-ㅎ0-9]*$";
+    private static final Pattern NAME_FORMAT = Pattern.compile("^[a-zA-Z가-힣ㄱ-ㅎ0-9]*$");
 
     private final String userName;
 
@@ -16,7 +18,7 @@ public class UserName {
         if (userName.isBlank() || userName.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("예약자명은 1글자 이상, " + MAX_NAME_LENGTH + "글자 이하로 작성해야 합니다.");
         }
-        if (!userName.matches(NAME_FORMAT)) {
+        if (!NAME_FORMAT.matcher(userName).matches()) {
             throw new IllegalArgumentException("예약자명은 한글, 영문, 숫자만 허용합니다.");
         }
     }
