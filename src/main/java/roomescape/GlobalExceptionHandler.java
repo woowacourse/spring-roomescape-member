a1d899b216ce784126eb2be0597cecc07f82f5a2
unifolio0 <boho82ha@gmail.com>
2024-05-04 19:15:08 +0900

    refactor: Order 어노테이션 삭제

--- src/main/java/roomescape/GlobalExceptionHandler.java
+++ src/main/java/roomescape/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package roomescape;
 
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,13 +8,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler
-    @Order(1)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
-    @Order(2)
     public ResponseEntity<String> handelException(Exception e) {
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
