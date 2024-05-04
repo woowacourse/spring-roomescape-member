9d61e999e64e3b8293dd01cf4d5c4d1b6994b5b1
hjk0761 <hjk0761@naver.com>
2024-05-05 01:40:10 +0900

    feat: HttpMessageNotReadableException handler 구현

--- src/main/java/roomescape/controller/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package roomescape.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,6 +15,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = IllegalStateException.class)
     public ResponseEntity<String> handleIllegalStateException(IllegalStateException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
