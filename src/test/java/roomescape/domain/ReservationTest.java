eba033831d1b4abda3e1b6550b0da885e663a018
Gyeongho Yang <chchch1213@naver.com>
2024-05-05 01:42:44 +0900

    test: 예약 도메인 생성 및 검증에 대한 테스트 추가

--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -3,11 +3,11 @@ package roomescape.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import roomescape.domain.exception.InvalidDateException;
 import roomescape.domain.exception.InvalidNameException;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,20 +20,8 @@ class ReservationTest {
     void assignId() {
         // given
         final Long id = 2L;
-        final Reservation reservation = new Reservation(
-                null,
-                new ReserveName("Seyang"),
-                LocalDate.now(),
-                new ReservationTime(1L, LocalTime.of(10, 0)),
-                new Theme(1L, null, null, null)
-        );
-        final Reservation expected = new Reservation(
-                id,
-                new ReserveName("Seyang"),
-                LocalDate.now(),
-                new ReservationTime(1L, LocalTime.of(10, 0)),
-                new Theme(1L, null, null, null)
-        );
+        final Reservation reservation = new Reservation(null, "Seyang", "2024-05-05", null, null);
+        final Reservation expected = new Reservation(id, "Seyang", "2024-05-05", null, null);
 
         // when
         Reservation actual = reservation.assignId(id);
@@ -47,20 +35,8 @@ class ReservationTest {
     void assignTime() {
         // given
         ReservationTime reservationTime = new ReservationTime(1L, LocalTime.of(10, 0));
-        Reservation reservation = new Reservation(
-                2L,
-                new ReserveName("Seyang"),
-                LocalDate.of(2024, 4, 24),
-                new ReservationTime(1L, LocalTime.MIN),
-                new Theme(1L, null, null, null)
-        );
-        Reservation expected = new Reservation(
-                2L,
-                new ReserveName("Seyang"),
-                LocalDate.of(2024, 4, 24),
-                reservationTime,
-                new Theme(1L, null, null, null)
-        );
+        Reservation reservation = new Reservation(2L, new ReserveName("Seyang"), null, null, null);
+        Reservation expected = new Reservation(2L, new ReserveName("Seyang"), null, reservationTime, null);
 
         // when
         Reservation actual = reservation.assignTime(reservationTime);
@@ -72,37 +48,23 @@ class ReservationTest {
     @Test
     @DisplayName("빈 이름이 입력 될 경우 예외가 발생한다.")
     void validateEmptyName() {
-        assertThatThrownBy(() -> new Reservation(
-                2L,
-                "",
-                "2024-04-24",
-                new ReservationTime(1L, LocalTime.of(10, 0)),
-                new Theme(1L, null, null, null)
-        )).isInstanceOf(InvalidNameException.class);
+        assertThatThrownBy(() -> new Reservation(null, "", "2024-04-24", null, null))
+                .isInstanceOf(InvalidNameException.class);
     }
 
     @Test
     @DisplayName("빈 날짜가 입력 될 경우 예외가 발생한다.")
     void validateEmptyDate() {
-        assertThatThrownBy(() -> new Reservation(
-                2L,
-                "seyang",
-                "",
-                new ReservationTime(1L, LocalTime.of(10, 0)),
-                new Theme(1L, null, null, null)
-        )).isInstanceOf(InvalidDateException.class);
+        assertThatThrownBy(() -> new Reservation(null, "Seyang", "", null, null))
+                .isInstanceOf(InvalidDateException.class);
     }
 
     @ParameterizedTest
     @DisplayName("유효하지 않은 날짜인경우 예외가 발생한다.")
+    @NullAndEmptySource
     @ValueSource(strings = {"202020-12-13", "1-13-4", "2024-14-15"})
     void validateFormat(String date) {
-        // given
-        final ReservationTime time = new ReservationTime(1L, LocalTime.parse("13:00"));
-        final Theme theme = new Theme(1L, "spring", "Escape from spring cold", "Spring thumb");
-
-        // when & then
-        assertThatThrownBy(() -> new Reservation(1L, "name", date, time, theme))
+        assertThatThrownBy(() -> new Reservation(null, "Seyang", date, null, null))
                 .isInstanceOf(InvalidDateException.class);
     }
 }
