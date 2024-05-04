617fe67c5e1631a8702b2745e27a5e5cb1d1eff8
Hyeon0208 <dmsgktn0208@gmail.com>
2024-05-04 12:54:41 +0900

    feat : 예약 시간 null 검증 로직 추가

--- src/main/java/roomescape/reservation/domain/ReservationTime.java
+++ src/main/java/roomescape/reservation/domain/ReservationTime.java
@@ -4,16 +4,23 @@ import java.time.LocalTime;
 
 public class ReservationTime {
 
-    private final LocalTime startAt;
     private Long id;
+    private final LocalTime startAt;
 
     public ReservationTime(LocalTime startAt) {
+        validateIsNull(startAt);
         this.startAt = startAt;
     }
 
     public ReservationTime(Long id, LocalTime startAt) {
         this.id = id;
         this.startAt = startAt;
+    }
+
+    private void validateIsNull(LocalTime startAt) {
+        if (startAt == null) {
+            throw new IllegalArgumentException("값을 입력하지 않았습니다.");
+        }
     }
 
     public Long getId() {
