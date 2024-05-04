8f941b26e46c15da86b2be6d27669331ef27dca7
reddevilmidzy <midzy0228@gmail.com>
2024-05-05 02:22:45 +0900

    feat: Exception에 대한 예외 처리 구현

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -6,10 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(annotations = RestController.class)
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
@@ -35,5 +34,12 @@ public class GlobalExceptionHandler {
         logger.error(e.getMessage(), e);
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(final Exception e) {
+        logger.error(e.getMessage(), e);
+        return ResponseEntity.internalServerError()
+                .body(new ErrorResponse("예기치 않은 오류가 발생했습니다."));
     }
 }
