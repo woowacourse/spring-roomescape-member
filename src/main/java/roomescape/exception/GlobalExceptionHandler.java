ecc868d9a597400de8500292393d7b356c8ddb6a
hyxrxn <gpfls981220@gmail.com>
2024-05-04 20:31:10 +0900

    refactor(GlobalExceptionHandler): 예외 타입 명시

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -9,17 +9,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResult> handleIllegalArgumentException(IllegalArgumentException ex) {
         return new ResponseEntity<>(new ErrorResult(ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResult> handleException(HttpMessageNotReadableException ex) {
         return new ResponseEntity<>(new ErrorResult(ex.getMostSpecificCause().getMessage()), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResult> handleException(Exception ex) {
         return new ResponseEntity<>(new ErrorResult("죄송합니다. 서버에서 문제가 발생하여 요청을 처리할 수 없습니다."),
                 HttpStatus.INTERNAL_SERVER_ERROR);
