e571005cf2f2cad0d6f3c95fe78e2231bd295425
reddevilmidzy <midzy0228@gmail.com>
2024-05-04 20:12:55 +0900

    feat: 예외시 로그 출력

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package roomescape.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(RoomescapeException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidNameException(final RoomescapeException e) {
+    public ResponseEntity<ErrorResponse> handleRoomescapeException(final RoomescapeException e) {
+        logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse(e.getMessage()));
     }
