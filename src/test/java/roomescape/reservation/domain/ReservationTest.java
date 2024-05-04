ecd567c8cabec97d93c8fe48115fe45cb02b70c8
DobbyMac <kimdobby@g.skku.edu>
2024-05-05 00:11:46 +0900

    test(ReservationTest): 운영 시간 적용에 따른 테스트 코드 수정

--- src/test/java/roomescape/reservation/domain/ReservationTest.java
+++ src/test/java/roomescape/reservation/domain/ReservationTest.java
@@ -18,52 +18,46 @@ class ReservationTest {
     @Test
     @DisplayName("전달된 id와 같은 값의 id인지 확인.")
     void hasSameId() {
-        Reservation reservation = new Reservation(1L, "폴라", LocalDate.now(), new Time(LocalTime.now()),
+        Reservation reservation = new Reservation(1L, "폴라", LocalDate.MAX, new Time(LocalTime.of(12, 0)),
                                                   new Theme("polla", "폴라 방탈출", "thumbnail"));
 
-        assertAll(() -> Assertions.assertThat(reservation.hasSameId(2L))
-                .isFalse(), () -> Assertions.assertThat(reservation.hasSameId(1L))
-                .isTrue());
+        assertAll(() -> Assertions.assertThat(reservation.hasSameId(2L)).isFalse(),
+                  () -> Assertions.assertThat(reservation.hasSameId(1L)).isTrue());
     }
 
     @Test
     @DisplayName("이름이 Null 혹은 빈값인 경우 에러를 발생한다.")
     void validation_ShouldThrowException_WhenNameIsNull() {
         assertAll(() -> {
-                      Throwable nameIsEmpty = assertThrows(BadRequestException.class,
-                                                           () -> new Reservation(" ", LocalDate.now(), 1L, 1L));
-                      assertEquals("null 혹은 빈칸으로 이루어진 이름으로 예약을 시도하였습니다.", nameIsEmpty.getMessage());
+            Throwable nameIsEmpty = assertThrows(BadRequestException.class,
+                                                 () -> new Reservation(" ", LocalDate.now(), 1L, 1L));
+            assertEquals("null 혹은 빈칸으로 이루어진 이름으로 예약을 시도하였습니다.", nameIsEmpty.getMessage());
 
-                      Throwable nameIsNull = assertThrows(BadRequestException.class,
-                                                          () -> new Reservation(null, LocalDate.now(), 1L, 1L));
-                      assertEquals("null 혹은 빈칸으로 이루어진 이름으로 예약을 시도하였습니다.", nameIsNull.getMessage());
-                  }
-        );
+            Throwable nameIsNull = assertThrows(BadRequestException.class,
+                                                () -> new Reservation(null, LocalDate.now(), 1L, 1L));
+            assertEquals("null 혹은 빈칸으로 이루어진 이름으로 예약을 시도하였습니다.", nameIsNull.getMessage());
+        });
     }
 
     @Test
     @DisplayName("과거의 날짜를 예약하려고 시도하는 경우 에러를 발생한다.")
     void validation_ShouldThrowException_WhenReservationDateIsPast() {
         assertAll(() -> {
-                      Throwable pastDateReservation = assertThrows(BadRequestException.class, () -> new Reservation("pollari",
-                                                                                                                    LocalDate.now()
-                                                                                                                            .minusDays(
-                                                                                                                                    1),
-                                                                                                                    1L, 1L));
-                      assertEquals("지난 날짜의 예약을 시도하였습니다.", pastDateReservation.getMessage());
-                  }
-        );
+            Throwable pastDateReservation = assertThrows(BadRequestException.class,
+                                                         () -> new Reservation("pollari", LocalDate.now().minusDays(1),
+                                                                               1L, 1L));
+            assertEquals("지난 날짜의 예약을 시도하였습니다.", pastDateReservation.getMessage());
+        });
     }
 
     @Test
     @DisplayName("이름에 특수문자가 들어가는 경우 에러를 발생한다.")
     void validation_ShouldThrowException_WhenNameContainsSymbol() {
         assertAll(() -> {
-                      Throwable pastDateReservation = assertThrows(BadRequestException.class,
-                                                                   () -> new Reservation("@특수문자", LocalDate.now(), 1L, 1L));
-                      assertEquals("특수문자가 포함된 이름으로 예약을 시도하였습니다.", pastDateReservation.getMessage());
-                  }
-        );
+            Throwable pastDateReservation = assertThrows(BadRequestException.class,
+                                                         () -> new Reservation("@특수문자", LocalDate.now(), 1L, 1L));
+            assertEquals("특수문자가 포함된 이름으로 예약을 시도하였습니다.", pastDateReservation.getMessage());
+        });
     }
 
 }
