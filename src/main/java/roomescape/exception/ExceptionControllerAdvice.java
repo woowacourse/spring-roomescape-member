c6886ac9eb8211c9b4d491dfda7ee4da0293cfa5
ay-eonii <naayen@naver.com>
2024-05-05 21:16:45 +0900

    feat: 처리하지 않은 예외 핸들러

--- src/main/java/roomescape/exception/ExceptionControllerAdvice.java
+++ src/main/java/roomescape/exception/ExceptionControllerAdvice.java
@@ -4,25 +4,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.NoSuchElementException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice {
-    private final static Logger LOGGER = Logger.getGlobal();
 
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<String> NoSuchElementException() {
-        return ResponseEntity.notFound().build();
-    }
+    private final static Logger LOGGER = Logger.getGlobal();
 
     @ExceptionHandler(RoomEscapeException.class)
     public ResponseEntity<String> roomEscapeException(RoomEscapeException e) {
-        StackTraceElement stackTraceElement = e.getStackTrace()[0];
-        LOGGER.log(Level.WARNING, "message = {0}, class = {1}, method = {2}",
-                new Object[]{e.getMessage(), stackTraceElement.getClassName(), stackTraceElement.getMethodName()}
+        StackTraceElement stackTraceElement = e.getStackTrace()[1];
+        LOGGER.log(Level.WARNING, "class = {0}, method = {1}, message = {2}",
+                new Object[]{stackTraceElement.getClassName(), stackTraceElement.getMethodName(), e.getMessage()}
         );
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> internalServerException(Exception e) {
+        LOGGER.log(Level.WARNING, "알 수 없는 오류가 발생했습니다.", e);
+        return ResponseEntity.internalServerError().body("[ERROR] 알 수 없는 오류가 발생했습니다.");
     }
 }
