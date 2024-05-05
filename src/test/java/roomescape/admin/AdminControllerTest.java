feee4fd8f10e4a89b6116ccfb484a3258fe8fa58
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-05-05 17:28:03 +0900

    test: AdminController 테스트 추가

--- src/test/java/roomescape/admin/AdminControllerTest.java
+++ src/test/java/roomescape/admin/AdminControllerTest.java
@@ -1,5 +1,6 @@
 package roomescape.admin;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
@@ -15,7 +16,8 @@ class AdminControllerTest extends ControllerTest {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body(containsString("방탈출 어드민"));
     }
 
     @DisplayName("관리자 예약 페이지 조회에 성공한다.")
@@ -24,12 +26,33 @@ class AdminControllerTest extends ControllerTest {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body(containsString("방탈출 예약 페이지"));
 
         RestAssured.given().log().all()
                 .when().get("/reservations")
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(0));
+    }
+
+    @DisplayName("관리자 시간 페이지 조회에 성공한다.")
+    @Test
+    void adminTimePage() {
+        RestAssured.given().log().all()
+                .when().get("/admin/time")
+                .then().log().all()
+                .statusCode(200)
+                .body(containsString("시간 관리 페이지"));
+    }
+
+    @DisplayName("관리자 테마 페이지 조회에 성공한다.")
+    @Test
+    void adminThemePage() {
+        RestAssured.given().log().all()
+                .when().get("/admin/theme")
+                .then().log().all()
+                .statusCode(200)
+                .body(containsString("테마 관리 페이지"));
     }
 }
