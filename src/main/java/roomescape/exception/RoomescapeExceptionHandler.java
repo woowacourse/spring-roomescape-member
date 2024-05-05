f285ec4ed83c3b0118a7854f41d070501dafc742
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-05-06 01:42:49 +0900

    feat : 확장성 고려 위한 Exception ErrorResponse 로 반환
    [관련 피드백 링크](https://github.com/woowacourse/spring-roomescape-member/pull/47#discussion_r1588657540)

--- src/main/java/roomescape/exception/RoomescapeExceptionHandler.java
+++ src/main/java/roomescape/exception/RoomescapeExceptionHandler.java
@@ -7,31 +7,52 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class RoomescapeExceptionHandler {
+    private static class ErrorResponse {
+        private static final boolean ERROR_STATUS = false;
+        private final String message;
+
+        public ErrorResponse(Exception e) {
+            this.message = e.getMessage();
+        }
+
+        public boolean isStatus() {
+            return ERROR_STATUS;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
 
     @ExceptionHandler(value = IllegalArgumentException.class)
-    public ResponseEntity<String> handleArgumentException(final IllegalArgumentException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleArgumentException(final IllegalArgumentException exception) {
+        return ResponseEntity.badRequest()
+                             .body(new ErrorResponse(exception));
     }
 
     @ExceptionHandler(value = PastTimeReservationException.class)
-    public ResponseEntity<String> handlePastTimeReservationException(final PastTimeReservationException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handlePastTimeReservationException(final PastTimeReservationException exception) {
+        return ResponseEntity.badRequest()
+                             .body(new ErrorResponse(exception));
     }
 
 
     @ExceptionHandler(value = ExistReservationException.class)
-    public ResponseEntity<String> handleExistReservationException(
-            final ExistReservationException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+    public ResponseEntity<ErrorResponse> handleExistReservationException(final ExistReservationException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                             .body(new ErrorResponse(exception));
     }
 
     @ExceptionHandler(value = AlreadyExistsException.class)
-    public ResponseEntity<String> handleAlreadyExistsException(final AlreadyExistsException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+    public ResponseEntity<ErrorResponse> handleAlreadyExistsException(final AlreadyExistsException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                             .body(new ErrorResponse(exception));
+
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = NotExistException.class)
-    public ResponseEntity<String> handleNotExistException(final NotExistException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    @ExceptionHandler(value = NotExistException.class)
+    public ResponseEntity<ErrorResponse> handleNotExistException(final NotExistException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                             .body(new ErrorResponse(exception));
     }
 }
