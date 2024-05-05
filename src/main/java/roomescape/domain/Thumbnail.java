5ceb8654ce922bb76a15c15bd4ecc338077fb660
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-05-06 01:13:40 +0900

    refactor : 변수명 가독성 있게 변경

--- src/main/java/roomescape/domain/Thumbnail.java
+++ src/main/java/roomescape/domain/Thumbnail.java
@@ -6,9 +6,9 @@ import java.util.regex.Pattern;
 public record Thumbnail(String value) {
 
     private static final List<String> ALLOW_EXTENSIONS = List.of("jpg", "jpeg", "png", "heic");
-    public static final String ALLOW_EXTENSIONS_PATTERN = String.join("|", ALLOW_EXTENSIONS);
-    private static final Pattern PATTERN = Pattern.compile(
-            String.format("^\\S+.(?i)(%s)$", ALLOW_EXTENSIONS_PATTERN));
+    public static final String ALLOW_EXTENSIONS_FORMAT = String.join("|", ALLOW_EXTENSIONS);
+    private static final Pattern ALLOW_EXTENSIONS_PATTERN = Pattern.compile(
+            String.format("^\\S+.(?i)(%s)$", ALLOW_EXTENSIONS_FORMAT));
 
 
     public Thumbnail {
@@ -22,14 +22,14 @@ public record Thumbnail(String value) {
 
     private void validateNull(final String url) {
         if (url.isBlank()) {
-            throw new IllegalArgumentException("");
+            throw new IllegalArgumentException("경로는 공백일 수 없습니다!");
         }
     }
 
     private void validateExt(final String url) {
-        if (!PATTERN.matcher(url)
-                    .matches()) {
-            throw new IllegalArgumentException(String.format("%s 확장자만 가능합니다.", ALLOW_EXTENSIONS_PATTERN));
+        if (!ALLOW_EXTENSIONS_PATTERN.matcher(url.toLowerCase())
+                                     .matches()) {
+            throw new IllegalArgumentException(String.format("%s 확장자만 가능합니다.", ALLOW_EXTENSIONS_FORMAT));
         }
     }
 
