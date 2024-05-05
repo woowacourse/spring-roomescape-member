d443f428be20df82085f927170f8e2768641d8a8
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-05 16:19:00 +0900

    feat: 스프링 내부 예외 처리를 위한 ResponseEntityExceptionHandler상속

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -3,11 +3,12 @@ package roomescape.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 // TODO 예상치 못한 예외 처리 고민
 //  TODO IllegalStateException, RuntimeException 과 같은 도메인 오류 처리
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = IllegalArgumentException.class)
     public ResponseEntity<String> handle(final IllegalArgumentException exception) {
         return ResponseEntity.badRequest()
