859952a6a67ae717459817ee9bfe79b5bb922c7d
Eugene Jang <kyunellroll@gmail.com>
2024-05-05 17:38:36 +0900

    feat(RoomEscapeException): RoomEscape 커스텀 Exception 생성

---
+++ src/main/java/roomescape/exception/model/RoomEscapeException.java
@@ -0,0 +1,17 @@
+package roomescape.exception.model;
+
+import org.springframework.http.HttpStatus;
+
+public class RoomEscapeException extends RuntimeException {
+
+    private final ExceptionCode exceptionCode;
+
+    public RoomEscapeException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return exceptionCode.getHttpStatus();
+    }
+}
