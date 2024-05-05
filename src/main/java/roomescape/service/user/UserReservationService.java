1e2b7a1025e75ef3ff13cae7b26226944ee5f300
ehtjsv2 <ehtjsv2@naver.com>
2024-05-05 16:12:54 +0900

    refactor: findBookableTimes() 코드 단순화

--- src/main/java/roomescape/service/user/UserReservationService.java
+++ src/main/java/roomescape/service/user/UserReservationService.java
@@ -1,6 +1,5 @@
 package roomescape.service.user;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import roomescape.domain.ReservationTime;
@@ -24,17 +23,14 @@ public class UserReservationService {
         List<ReservationTime> bookedTimes = reservationDao.findByDateAndTheme(bookableTimesRequest.date(),
                 bookableTimesRequest.themeId());
         List<ReservationTime> allTimes = reservationTimeDao.findAll();
-        List<BookableTimeResponse> bookableTimeResponses = new ArrayList<>();
-        for (ReservationTime allTime : allTimes) {
-            if (!bookedTimes.contains(allTime)) {
-                bookableTimeResponses.add(new BookableTimeResponse(allTime.getStartAt(), allTime.getId(), false));
-            }
-        }
-        List<BookableTimeResponse> bookedTimeResponse = bookedTimes.stream()
-                .map((reservationTime -> new BookableTimeResponse(reservationTime.getStartAt(), reservationTime.getId(),
-                        true)))
+        return allTimes.stream()
+                .map(time -> {
+                    return new BookableTimeResponse(time.getStartAt(), time.getId(), isBookedTime(bookedTimes, time));
+                })
                 .toList();
-        bookableTimeResponses.addAll(bookedTimeResponse);
-        return bookableTimeResponses;
+    }
+
+    private boolean isBookedTime(List<ReservationTime> bookedTimes, ReservationTime time) {
+        return bookedTimes.contains(time);
     }
 }
