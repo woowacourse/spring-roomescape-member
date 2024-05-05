880f4abb9235fadff57e9677c2a29fcbc5abf2bf
Seokmyung Ham <gkatjraud1@gmail.com>
2024-05-05 23:32:46 +0900

    feat: 시간에 대하여 오름차순으로 정렬하도록 변경

--- src/main/java/roomescape/service/dto/AvailableTimeResponseDtos.java
+++ src/main/java/roomescape/service/dto/AvailableTimeResponseDtos.java
@@ -1,5 +1,6 @@
 package roomescape.service.dto;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -13,13 +14,23 @@ public class AvailableTimeResponseDtos {
 
     public AvailableTimeResponseDtos(ReservationTimeStatus timeStatus) {
         availableTimeResponses = new ArrayList<>();
+        addAvailableTimeResponses(timeStatus);
+        sortAscendingByStartAt();
+    }
 
+    private void addAvailableTimeResponses(ReservationTimeStatus timeStatus) {
         Map<ReservationTime, ReservationStatus> timesStatus = timeStatus.getTimesStatus();
         for (Map.Entry<ReservationTime, ReservationStatus> entry : timesStatus.entrySet()) {
-            ReservationTime reservationTime = entry.getKey();
-            ReservationStatus isBooked = entry.getValue();
-            availableTimeResponses.add(new AvailableTimeResponseDto(reservationTime, isBooked.getStatus()));
+            availableTimeResponses.add(new AvailableTimeResponseDto(entry.getKey(), entry.getValue().getStatus()));
         }
+    }
+
+    private void sortAscendingByStartAt() {
+        availableTimeResponses.sort((o1, o2) -> {
+            LocalTime startAt1 = LocalTime.parse(o1.getTimeResponseDto().getStartAt());
+            LocalTime startAt2 = LocalTime.parse(o2.getTimeResponseDto().getStartAt());
+            return startAt1.compareTo(startAt2);
+        });
     }
 
     public List<AvailableTimeResponseDto> getAvailableTimeResponses() {
