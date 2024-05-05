12168f699afd0291f29a636518b7d162d34664e4
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-06 00:34:42 +0900

    refactor(JdbcReservationTimeRepositoryTest): @SpringBootTest를 @JdbcTest로 변경 및 Fixture 제거

--- src/test/java/roomescape/reservationtime/repository/JdbcReservationTimeRepositoryTest.java
+++ src/test/java/roomescape/reservationtime/repository/JdbcReservationTimeRepositoryTest.java
@@ -7,53 +7,78 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
 import roomescape.reservationtime.model.ReservationTime;
-import roomescape.util.DummyDataFixture;
+import roomescape.util.ReservationTimeFixture;
 
-@SpringBootTest
-@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
-class JdbcReservationTimeRepositoryTest extends DummyDataFixture {
+@ActiveProfiles("test")
+@JdbcTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class JdbcReservationTimeRepositoryTest {
+
+    private final JdbcReservationTimeRepository jdbcReservationTimeRepository;
 
     @Autowired
-    private JdbcReservationTimeRepository jdbcReservationTimeRepository;
+    JdbcReservationTimeRepositoryTest(final JdbcTemplate jdbcTemplate, final DataSource dataSource) {
+        this.jdbcReservationTimeRepository = new JdbcReservationTimeRepository(jdbcTemplate, dataSource);
+    }
 
     @Test
     @DisplayName("ReservationTime 저장한 후 저장한 row의 id값을 반환한다.")
     void save() {
         ReservationTime newReservationTime = new ReservationTime(null, LocalTime.of(10, 00));
-        assertThat(jdbcReservationTimeRepository.save(newReservationTime)).isEqualTo(new ReservationTime(7L, LocalTime.of(10, 00)));
+        assertThat(jdbcReservationTimeRepository.save(newReservationTime)).isEqualTo(
+                new ReservationTime(1L, LocalTime.of(10, 00)));
     }
 
     @Test
     @DisplayName("ReservationTime 테이블의 있는 모든 데이터를 조회한다.")
     void findAll() {
-        List<ReservationTime> preparedReservationTimes = super.getPreparedReservationTimes();
-        assertThat(jdbcReservationTimeRepository.findAll()).isEqualTo(preparedReservationTimes);
+        List<ReservationTime> savedTimes = ReservationTimeFixture.get(2).stream()
+                .map(jdbcReservationTimeRepository::save)
+                .toList();
+        assertThat(jdbcReservationTimeRepository.findAll()).isEqualTo(savedTimes);
     }
 
     @Test
     @DisplayName("ReservationTime 테이블의 주어진 id와 동일한 데이터를 조회한다.")
     void findById() {
-        ReservationTime reservationTime = super.getReservationTimeById(1L);
-        assertThat(jdbcReservationTimeRepository.findById(1L)).isEqualTo(Optional.of(reservationTime));
+        ReservationTime savedTime = jdbcReservationTimeRepository.save(ReservationTimeFixture.getOne());
+
+        assertThat(jdbcReservationTimeRepository.findById(savedTime.getId())).isEqualTo(Optional.of(savedTime));
     }
 
     @Test
     @DisplayName("해당 예약 시간 id값과 일치하는 예약이 존재하는 경우 참을 반환한다.")
     void existsById() {
-        assertTrue(jdbcReservationTimeRepository.existsById(1L));
+        ReservationTime savedTime = jdbcReservationTimeRepository.save(ReservationTimeFixture.getOne());
+        assertTrue(jdbcReservationTimeRepository.existsById(savedTime.getId()));
     }
 
     @Test
     @DisplayName("해당 예약 시간 id값과 일치하는 예약이 존재하지 않은 경우 거짓을 반환한다.")
     void existsById_WhenNotExist() {
         assertFalse(jdbcReservationTimeRepository.existsById(7L));
+    }
+
+    @Test
+    @DisplayName("해당 예약 시간 id값과 일치하는 예약이 존재하는 경우 참을 반환한다.")
+    void existsByStartAt() {
+        ReservationTime savedTime = jdbcReservationTimeRepository.save(ReservationTimeFixture.getOne());
+        assertTrue(jdbcReservationTimeRepository.existsByStartAt(savedTime.getTime()));
+    }
+
+    @Test
+    @DisplayName("해당 예약 시간 id값과 일치하는 예약이 존재하지 않은 경우 거짓을 반환한다.")
+    void existsByStartAt_WhenNotExist() {
+        assertFalse(jdbcReservationTimeRepository.existsByStartAt(LocalTime.parse("00:01")));
     }
 
     @Test
