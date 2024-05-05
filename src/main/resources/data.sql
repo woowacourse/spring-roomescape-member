0e393e489fb9738d1cdbd8bc7b9fcc8f449a12d9
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-05-05 15:47:05 +0900

    feat: 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,30 +1,61 @@
+-- 테마 추가
 INSERT INTO theme(name, description, thumbnail)
-VALUES ('테마1', '설명1', 'https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/08/urbanbrush-20220801083851022216.jpg');
+VALUES ('추리', '추리 테마입니다.', 'https://image.yes24.com/goods/73161943/L');
 INSERT INTO theme(name, description, thumbnail)
-VALUES ('테마2', '설명2', 'https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/12/urbanbrush-20221209202526239031.jpg');
+VALUES ('아날로그식', '아날로그식 테마입니다.', 'https://image.yes24.com/goods/62087889/L');
 INSERT INTO theme(name, description, thumbnail)
-VALUES ('테마3', '설명3', 'https://previews.123rf.com/images/yusakp/yusakp1405/yusakp140500013/28097632-%EA%B7%80%EC%97%AC%EC%9A%B4-%EA%BF%80%EB%B2%8C%EC%9D%98-%EB%B2%A1%ED%84%B0-%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8-%EB%A0%88%EC%9D%B4-%EC%85%98.jpg');
+VALUES ('스테이지형', '스테이지형 테마입니다.', 'https://image.yes24.com/goods/125101417/L');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('협동형', '협동형 테마입니다.', 'https://image.yes24.com/momo/TopCate432/MidCate002/43115671.jpg');
 
-INSERT INTO reservation_time(start_at)
-VALUES ('11:59');
-INSERT INTO reservation_time(start_at)
-VALUES ('17:12');
-INSERT INTO reservation_time(start_at)
-VALUES ('11:11');
 
+-- 예약 시간 추가
+INSERT INTO reservation_time(start_at)
+VALUES ('10:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('11:30');
+INSERT INTO reservation_time(start_at)
+VALUES ('13:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('15:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('17:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('18:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('19:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('20:00');
+
+
+-- 예약 추가
 INSERT INTO reservation(date, time_id, theme_id)
-VALUES (DATEADD('DAY', 1, CURRENT_DATE()), 1, 1);
+VALUES (DATEADD('DAY', -2, CURRENT_DATE()), 1, 1);
 INSERT INTO reservation(date, time_id, theme_id)
 VALUES (DATEADD('YEAR', 1, CURRENT_DATE()), 2, 2);
 INSERT INTO reservation(date, time_id, theme_id)
-VALUES (DATEADD('DAY', 4, CURRENT_DATE()), 3, 2);
+VALUES (DATEADD('DAY', -4, CURRENT_DATE()), 3, 3);
+INSERT INTO reservation(date, time_id, theme_id)
+VALUES (DATEADD('DAY', 4, CURRENT_DATE()), 3, 4);
 
+
+-- 회원 추가
 INSERT INTO member(name)
-VALUES ('choco');
+VALUES ('초코칩');
+INSERT INTO member(name)
+VALUES ('이든');
+INSERT INTO member(name)
+VALUES ('클로버');
 
+
+-- 예약 목록 추가
 INSERT INTO reservation_list(member_id, reservation_id)
 VALUES (1, 1);
 INSERT INTO reservation_list(member_id, reservation_id)
 VALUES (1, 3);
 INSERT INTO reservation_list(member_id, reservation_id)
 VALUES (1, 2);
+INSERT INTO reservation_list(member_id, reservation_id)
+VALUES (2, 3);
+INSERT INTO reservation_list(member_id, reservation_id)
+VALUES (3, 4);
