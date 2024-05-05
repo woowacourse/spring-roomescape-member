1f6c1e8682ec7b74fc1c44bdec32059510492694
linirini <2001yerin@naver.com>
2024-05-05 17:28:20 +0900

    refactor: 필드명 변경 및 테스트용 데이터 파일 생성

--- src/main/java/roomescape/service/dto/AvailableReservationTimeResponse.java
+++ src/main/java/roomescape/service/dto/AvailableReservationTimeResponse.java
@@ -7,5 +7,5 @@ import java.time.LocalTime;
 public record AvailableReservationTimeResponse(
         long id,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm") LocalTime startAt,
-        boolean isBooked) {
+        boolean alreadyBooked) {
 }
