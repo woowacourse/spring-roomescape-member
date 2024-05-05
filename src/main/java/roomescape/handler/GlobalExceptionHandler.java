4172edcbd395f56c7833c25acaecbd0f8a0bd6a0
Eugene Jang <kyunellroll@gmail.com>
2024-05-06 01:31:34 +0900

    feat(GlobalExceptionHandler): 메서드 타입 불일치에 발생하는 error 추가

--- src/main/java/roomescape/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/handler/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package roomescape.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import roomescape.exception.model.RoomEscapeException;
 
 @ControllerAdvice
@@ -13,5 +15,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(exception.getHttpStatus())
                 .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleMethodArgumentTypeMismatchException() {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("타입이 일치하지 않습니다.");
     }
 }
