753a773b52ef314882a8e38571deb0207fb4c785
pricelees <priceelees@gmail.com>
2024-05-05 14:28:13 +0900

    rename: UserReservationController 클래스명을 UserPageController 로 수정

--- src/main/java/roomescape/controller/UserPageController.java
+++ src/main/java/roomescape/controller/UserPageController.java
@@ -4,15 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UserReservationController {
-
-    @GetMapping("/reservation")
-    public String reservation() {
-        return "reservation";
-    }
+public class UserPageController {
 
     @GetMapping
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
     }
 }
--- src/test/java/roomescape/controller/UserPageControllerTest.java
+++ src/test/java/roomescape/controller/UserPageControllerTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class UserReservationControllerTest {
+public class UserPageControllerTest {
 
     @LocalServerPort
     int port;
