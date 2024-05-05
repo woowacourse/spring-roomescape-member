e12815ecf1b41da12a3ab6296a679cd13501b2ec
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-05 21:46:31 +0900

    refactor(truncate.sql): sql 정렬 수정

--- src/test/resources/truncate.sql
+++ src/test/resources/truncate.sql
@@ -1,12 +1,7 @@
-DELETE
-FROM reservation;
-DELETE
-FROM reservation_time;
-DELETE
-FROM theme;
-ALTER TABLE reservation
-    ALTER COLUMN id RESTART;
-ALTER TABLE reservation_time
-    ALTER COLUMN id RESTART;
-ALTER TABLE theme
-    ALTER COLUMN id RESTART;
+DELETE FROM reservation;
+DELETE FROM reservation_time;
+DELETE FROM theme;
+
+ALTER TABLE reservation ALTER COLUMN id RESTART;
+ALTER TABLE reservation_time ALTER COLUMN id RESTART;
+ALTER TABLE theme ALTER COLUMN id RESTART;
