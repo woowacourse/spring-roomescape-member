8f519d07dc2821667e25051f4805e2c8b62b1c5b
le2sky <leehaneul990623@gmail.com>
2024-05-05 23:51:27 +0900

    refactor: IntegrationTestSupport 내부 불필요한 어노테이션 제거

--- src/test/java/roomescape/support/IntegrationTestSupport.java
+++ src/test/java/roomescape/support/IntegrationTestSupport.java
@@ -1,16 +1,12 @@
 package roomescape.support;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.jdbc.JdbcTestUtils;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-@Transactional
-@AutoConfigureTestDatabase
 @Sql(scripts = "/reset_test_data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public abstract class IntegrationTestSupport {
 
