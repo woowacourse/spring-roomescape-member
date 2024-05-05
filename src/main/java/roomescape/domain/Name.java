d6f2fb72977fb7a519ce05be8a6deaa418268931
unifolio0 <boho82ha@gmail.com>
2024-05-05 19:06:27 +0900

    refactor: 불필요한 static 삭제

--- src/main/java/roomescape/domain/Name.java
+++ src/main/java/roomescape/domain/Name.java
@@ -10,7 +10,7 @@ public record Name(String value) {
         validateLength(value);
     }
 
-    public static void validateLength(String value) {
+    public void validateLength(String value) {
         Objects.requireNonNull(value);
         if (value.length() < NAME_LENGTH_MIN || NAME_LENGTH_MAX < value.length()) {
             throw new IllegalArgumentException(String.format("사용자 이름은 %d자 이상 %d자 이하여야 합니다. (현재 입력한 이름 길이: %d자)",
