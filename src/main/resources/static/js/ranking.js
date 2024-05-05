15ec7a8ecff2f6d7fc6b952dc45f5b8f27f7969f
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-05 21:25:40 +0900

    refactor(js): 필요없는 todo 제거

--- src/main/resources/static/js/ranking.js
+++ src/main/resources/static/js/ranking.js
@@ -1,8 +1,6 @@
+const POPULAR_ENDPOINT = '/themes/popular';
 document.addEventListener('DOMContentLoaded', () => {
-    /*
-    TODO: [3단계] 인기 테마 - 인기 테마 목록 조회 API 호출
-    */
-    requestRead('/themes/popular') // 인기 테마 목록 조회 API endpoint
+    requestRead(POPULAR_ENDPOINT) // 인기 테마 목록 조회 API endpoint
         .then(render)
         .catch(error => console.error('Error fetching times:', error));
 });
