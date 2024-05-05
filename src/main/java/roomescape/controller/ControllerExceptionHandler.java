a3e25b8a7fdcc42dbbd01b65181e93b6b3cd41dc
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-05 21:34:58 +0900

    feat(ControllerExceptionHandler): 예상치 못한 예외를 처리하는 핸들러 구현

--- src/main/java/roomescape/controller/ControllerExceptionHandler.java
+++ src/main/java/roomescape/controller/ControllerExceptionHandler.java
@@ -11,11 +11,18 @@ import roomescape.domain.exception.IllegalNullArgumentException;
 @ControllerAdvice
 public class ControllerExceptionHandler {
     @ExceptionHandler({IllegalArgumentException.class, IllegalNullArgumentException.class})
-    public ResponseEntity<ProblemDetail> handleException(RuntimeException e) {
+    public ResponseEntity<ProblemDetail> bedRequestHandleException(RuntimeException e) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
-
         return ResponseEntity
                 .badRequest()
+                .body(problemDetail);
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<ProblemDetail> systemHandleException(Exception e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        return ResponseEntity
+                .internalServerError()
                 .body(problemDetail);
     }
 }
