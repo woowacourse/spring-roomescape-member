2c1aff785f018771c4ff22ac94b722a377833b61
Arachneee <jhg2819@naver.com>
2024-05-06 16:32:45 +0900

    refactor: 에러 객체 추가

--- src/main/java/roomescape/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/advice/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package roomescape.controller.advice;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -16,31 +18,49 @@ public class GlobalExceptionHandler {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> handleHttpMessageNotReadableException(final HttpMessageNotReadableException e) {
+    public ResponseEntity<ProblemDetail> handleHttpMessageNotReadableException(
+            final HttpMessageNotReadableException e) {
         logger.error(e.getMessage(), e);
-        return ResponseEntity.badRequest().body("잘못된 형식의 Request Body 입니다.");
+
+        return ResponseEntity.badRequest()
+                .body(ProblemDetail.forStatusAndDetail(
+                        HttpStatus.BAD_REQUEST,
+                        "잘못된 형식의 Request Body 입니다."
+                ));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
-        logger.error(e.getMessage(),e);
+    public ResponseEntity<ProblemDetail> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        logger.error(e.getMessage(), e);
 
         final String errorMessage = e.getFieldErrors().stream()
                 .map(error -> error.getField() + "는 " + error.getDefaultMessage())
                 .collect(Collectors.joining(", "));
 
-        return ResponseEntity.badRequest().body(errorMessage);
+        return ResponseEntity.badRequest()
+                .body(ProblemDetail.forStatusAndDetail(
+                        HttpStatus.BAD_REQUEST,
+                        errorMessage
+                ));
     }
 
     @ExceptionHandler(ReservationBusinessException.class)
-    public ResponseEntity<String> handleIllegalArgument(final ReservationBusinessException e) {
+    public ResponseEntity<ProblemDetail> handleIllegalArgument(final ReservationBusinessException e) {
         logger.error(e.getMessage(), e);
-        return ResponseEntity.badRequest().body(e.getMessage());
+        return ResponseEntity.badRequest()
+                .body(ProblemDetail.forStatusAndDetail(
+                        HttpStatus.BAD_REQUEST,
+                        e.getMessage()
+                ));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(final Exception e) {
+    public ResponseEntity<ProblemDetail> handleException(final Exception e) {
         logger.error(e.getMessage(), e);
-        return ResponseEntity.internalServerError().body("서버 오류입니다.");
+        return ResponseEntity.internalServerError()
+                .body(ProblemDetail.forStatusAndDetail(
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                        "서버 오류입니다."
+                ));
     }
 }
