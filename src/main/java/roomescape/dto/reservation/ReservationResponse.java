e46558c504c6e7cfb8730f6c33e912725abcfbf9
pricelees <priceelees@gmail.com>
2024-05-06 16:01:18 +0900

    refactor: ReservationResponse 필드 타입 수정
    - ReservationTime -> TimeResponse
    - ReservationTheme -> ThemeResponse

--- src/main/java/roomescape/dto/reservation/ReservationResponse.java
+++ src/main/java/roomescape/dto/reservation/ReservationResponse.java
@@ -1,13 +1,13 @@
 package roomescape.dto.reservation;
 
 import roomescape.domain.Reservation;
-import roomescape.domain.ReservationTheme;
-import roomescape.domain.ReservationTime;
+import roomescape.dto.theme.ThemeResponse;
+import roomescape.dto.time.TimeResponse;
 
-public record ReservationResponse(Long id, String name, String date, ReservationTime time, ReservationTheme theme) {
+public record ReservationResponse(Long id, String name, String date, TimeResponse time, ThemeResponse theme) {
 
     public ReservationResponse(Reservation reservation) {
-        this(reservation.getId(), reservation.getName(), reservation.getDate(), reservation.getTime(),
-                reservation.getTheme());
+        this(reservation.getId(), reservation.getName(), reservation.getDate(), new TimeResponse(reservation.getTime()),
+                new ThemeResponse(reservation.getTheme()));
     }
 }
