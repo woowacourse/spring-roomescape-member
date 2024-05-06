c412edcb590748a0d623e1ded11c298a234a272c
slimsha2dy <chy2581@naver.com>
2024-05-07 03:34:03 +0900

    feat: RestExceptionHandler에서 NoSuchElementException를 처리하는 기능 추가

--- src/main/java/roomescape/controller/exception/RestExceptionHandler.java
+++ src/main/java/roomescape/controller/exception/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package roomescape.controller.exception;
 
+import java.util.NoSuchElementException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +10,14 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<CustomErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        CustomErrorResponse errorResponse = new CustomErrorResponse(e.getMessage());
+
+        return ResponseEntity.badRequest()
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<CustomErrorResponse> handleNoSuchElementException(NoSuchElementException e) {
         CustomErrorResponse errorResponse = new CustomErrorResponse(e.getMessage());
 
         return ResponseEntity.badRequest()
