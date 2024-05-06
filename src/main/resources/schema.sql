a900183546f25a84bf8a7572dbde72bf6709c674
SCY <helenason@naver.com>
2024-05-07 01:17:05 +0900

    refactor(sql): 더미데이터 생성 로직 파일 분리

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,14 +20,8 @@ CREATE TABLE reservation
     name VARCHAR(255) NOT NULL,
     date VARCHAR(255) NOT NULL,
     time_id BIGINT,
-    theme_id BIGINT,                                        -- 컬럼 추가
+    theme_id BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)            -- 외래키 추가
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
-
-INSERT INTO reservation_time (start_at) VALUES ('10:00');
-INSERT INTO reservation_time (start_at) VALUES ('11:00');
-
-INSERT INTO theme (name, description, thumbnail) VALUES ('에버', '공포', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
-INSERT INTO theme (name, description, thumbnail) VALUES ('배키', '스릴러', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
