bc66106b16118539600cf8d5ba38ae403e8ddafd
kelly6bf <kelly6bf@gmail.com>
2024-05-06 15:23:09 +0900

    config: 스키마 데이터 크기 및 타입 정정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,24 +5,24 @@ drop table if exists reservation_time CASCADE;
 CREATE TABLE theme
 (
     id          BIGINT       NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    thumbnail   VARCHAR(255) NOT NULL,
+    name        VARCHAR(20)  NOT NULL,
+    description VARCHAR(80)  NOT NULL,
+    thumbnail   VARCHAR(700) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation_time
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL,
+    id       BIGINT NOT NULL AUTO_INCREMENT,
+    start_at TIME   NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    name     VARCHAR(255) NOT NULL,
-    date     VARCHAR(255) NOT NULL,
+    id       BIGINT     NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(5) NOT NULL,
+    date     DATE       NOT NULL,
     time_id  BIGINT, -- 컬럼 수정
     theme_id BIGINT, -- 컬럼 수정
     PRIMARY KEY (id),
