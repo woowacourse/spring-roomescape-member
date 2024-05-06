4a858af14a0d313eebc8cccc8fd41dac7ff50aa2
Zeus6768 <akaps6768@gmail.com>
2024-05-06 22:17:20 +0900

    feat: 테마 이름 예외 처리

---
+++ src/test/java/roomescape/domain/ThemeTest.java
@@ -0,0 +1,21 @@
+package roomescape.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ThemeTest {
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {" "})
+    @DisplayName("테마 이름에 공백 또는 빈 문자열이 입력되면 예외가 발생한다.")
+    void nameException(String name) {
+        assertThatThrownBy(() -> new Theme(0, name, "description", "thumbnail"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Theme name cannot be null or blank");
+    }
+}
