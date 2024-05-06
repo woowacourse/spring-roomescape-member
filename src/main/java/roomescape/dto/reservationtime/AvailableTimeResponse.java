11a686a335a8ddb26002856cdc3cfde7966dbc6b
Nam Gi Beom <rlqja0523@naver.com>
2024-05-07 00:27:06 +0900

    refactor: JsonFormat 적용

--- src/main/java/roomescape/dto/reservationtime/AvailableTimeResponse.java
+++ src/main/java/roomescape/dto/reservationtime/AvailableTimeResponse.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 import roomescape.domain.time.ReservationTime;
 
-public record AvailableTimeResponse(long id, @JsonFormat(pattern = "hh:mm") LocalTime startAt, boolean alreadyBooked) {
+public record AvailableTimeResponse(long id, @JsonFormat(pattern = "HH:mm") LocalTime startAt, boolean alreadyBooked) {
     public static AvailableTimeResponse from(ReservationTime reservationTime, boolean alreadyBooked) {
         return new AvailableTimeResponse(reservationTime.getId(), reservationTime.getStartAt(), alreadyBooked);
     }
--- src/main/java/roomescape/dto/reservationtime/ReservationTimeResponse.java
+++ src/main/java/roomescape/dto/reservationtime/ReservationTimeResponse.java
@@ -1,16 +1,10 @@
 package roomescape.dto.reservationtime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import roomescape.domain.time.ReservationTime;
 
-public record ReservationTimeResponse(long id, String startAt) {
-    private static final DateTimeFormatter DEFAULT_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
-
-    public ReservationTimeResponse(long id, LocalTime time) {
-        this(id, time.format(DEFAULT_TIME_FORMATTER));
-    }
-
+public record ReservationTimeResponse(long id, @JsonFormat(pattern = "HH:mm") LocalTime startAt) {
     public static ReservationTimeResponse from(ReservationTime time) {
         return new ReservationTimeResponse(time.getId(), time.getStartAt());
     }
