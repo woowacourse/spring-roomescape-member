04bb1cdce9936fd079b7ad0ab68aeb5cda09c092
Nam Gi Beom <rlqja0523@naver.com>
2024-05-06 17:15:18 +0900

    feat: 테마 설명, 썸네일 url 길이 검증 추가

--- src/main/java/roomescape/domain/theme/Theme.java
+++ src/main/java/roomescape/domain/theme/Theme.java
@@ -1,12 +1,17 @@
 package roomescape.domain.theme;
 
 public class Theme {
+    private static final int MAX_DESCRIPTION_LENGTH = 255;
+    private static final int MAX_THUMBNAIL_LENGTH = 255;
+
     private final Long id;
     private final ThemeName name;
     private final String description;
     private final String thumbnail;
 
     public Theme(Long id, String name, String description, String thumbnail) {
+        validateDescriptionLength(description);
+        validateThumbnailLength(thumbnail);
         this.id = id;
         this.name = new ThemeName(name);
         this.description = description;
@@ -15,6 +20,18 @@ public class Theme {
 
     public Theme(String name, String description, String thumbnail) {
         this(null, name, description, thumbnail);
+    }
+
+    private void validateDescriptionLength(String description) {
+        if (description.length() > MAX_DESCRIPTION_LENGTH) {
+            throw new IllegalArgumentException(String.format("테마 설명은 %d자 이하여야 합니다.", MAX_DESCRIPTION_LENGTH));
+        }
+    }
+
+    private void validateThumbnailLength(String thumbnail) {
+        if (thumbnail.length() > MAX_THUMBNAIL_LENGTH) {
+            throw new IllegalArgumentException(String.format("썸네일 URL은 %d자 이하여야 합니다.", MAX_THUMBNAIL_LENGTH));
+        }
     }
 
     public Long getId() {
