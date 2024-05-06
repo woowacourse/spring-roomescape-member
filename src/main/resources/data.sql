8d00569c7fbcca04b2bc02fc9edfe8e254c6bf4f
lemone <lejinhy@gmail.com>
2024-05-06 21:11:36 +0900

    feat: 애플리케이션 초기값 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO theme
+VALUES (1L, '테마1', '설명1', 'https://idsncdn.iwinv.biz/news/data/20220919/p1065578397270038_168_thum.jpg');
+
+INSERT INTO reservation_time
+VALUES (1L, '12:00');
+
+INSERT INTO reservation
+VALUES (1L, '레모네', '2024-05-30', 1L, 1L);
+
