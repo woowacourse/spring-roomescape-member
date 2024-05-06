c9862da5bdc34927e3282389f0366f245decc99f
haiseong <wjdgotjd9908@gmail.com>
2024-05-06 19:41:15 +0900

    feat: member crd 기능 구현

---
+++ src/main/java/roomescape/dto/request/MemberRequest.java
@@ -0,0 +1,10 @@
+package roomescape.dto.request;
+
+import roomescape.domain.Member;
+
+public record MemberRequest(String name, String email, String password) {
+
+    public Member toMember() {
+        return new Member(name, email, password);
+    }
+}
