afb3b02eafc1c2047a62f992808adb76641b13de
eunjungL <dldmswjd322@gmail.com>
2024-05-06 22:24:05 +0900

    test: 어드민/사용자 예약 컨트롤러 body까지 검증하게 추가

--- src/test/java/roomescape/controller/AdminControllerTest.java
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import static org.hamcrest.Matchers.equalTo;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DisplayName("어드민 컨트롤러")
 public class AdminControllerTest {
@@ -25,7 +27,8 @@ public class AdminControllerTest {
         RestAssured.given().log().all()
                 .when().get("/admin")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("html.body.div.h2", equalTo("방탈출 어드민"));
     }
 
     @DisplayName("어드민 컨트롤러는 /admin/reservation으로 GET 요청이 들어오면 예약 목록을 페이지를 반환한다.")
@@ -34,7 +37,8 @@ public class AdminControllerTest {
         RestAssured.given().log().all()
                 .when().get("/admin/reservation")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("html.body.div.h2", equalTo("방탈출 예약 페이지"));
     }
 
     @DisplayName("어드민 컨트롤러는 /admin/time으로 GET 요청이 들어오면 시간 목록을 페이지를 반환한다.")
@@ -43,7 +47,8 @@ public class AdminControllerTest {
         RestAssured.given().log().all()
                 .when().get("/admin/time")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("html.body.div.h2", equalTo("시간 관리 페이지"));
     }
 
     @DisplayName("어드민 컨트롤러는 /admin/theme로 GET 요청이 들어오면 테마 목록 페이지를 반환한다.")
@@ -52,6 +57,7 @@ public class AdminControllerTest {
         RestAssured.given().log().all()
                 .when().get("/admin/theme")
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("html.body.div.h2", equalTo("테마 관리 페이지"));
     }
 }
