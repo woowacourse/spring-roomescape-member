27ad377d60fdbb552d7da0ba9201c0401da8fadb
Nam Gi Beom <rlqja0523@naver.com>
2024-05-06 17:14:56 +0900

    chore: name 길이 스키마 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS theme
 (
     id          BIGINT       NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
+    name        VARCHAR(20)  NOT NULL,
     description VARCHAR(255) NOT NULL,
     thumbnail   VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
@@ -17,11 +17,11 @@ CREATE TABLE IF NOT EXISTS reservation_time
 
 CREATE TABLE IF NOT EXISTS reservation
 (
-    id               BIGINT       NOT NULL AUTO_INCREMENT,
-    name             VARCHAR(255) NOT NULL,
-    reservation_date DATE         NOT NULL,
-    time_id          BIGINT       NOT NULL,
-    theme_id         BIGINT       NOT NULL,
+    id               BIGINT      NOT NULL AUTO_INCREMENT,
+    name             VARCHAR(10) NOT NULL,
+    reservation_date DATE        NOT NULL,
+    time_id          BIGINT      NOT NULL,
+    theme_id         BIGINT      NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
