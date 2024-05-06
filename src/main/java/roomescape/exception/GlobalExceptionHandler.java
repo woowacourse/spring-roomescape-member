b619d2f6560e97b527a065f7a118c55fed007555
SCY <helenason@naver.com>
2024-05-07 01:11:18 +0900

    fix(GlobalExceptionHandler): 모든 런타임 예외 처리

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -16,12 +16,17 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(value = {BadRequestException.class, DuplicatedException.class})
-    public ResponseEntity<String> handleBadRequestException(RuntimeException exception) {
+    public ResponseEntity<String> handleBadRequestAndDuplicatedException(RuntimeException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = DateTimeParseException.class)
     public ResponseEntity<String> handleDateTimeParseException(DateTimeParseException exception) {
         return new ResponseEntity<>("[ERROR] 유효하지 않은 요청입니다.", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
