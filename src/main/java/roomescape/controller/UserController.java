7309985c5e572cfac87464742b4a28a3a1824cdf
Seokmyung Ham <gkatjraud1@gmail.com>
2024-05-06 14:46:30 +0900

    refactor: 유저 페이지 호출 핸들러를 UserController로 이동

--- src/main/java/roomescape/controller/UserController.java
+++ src/main/java/roomescape/controller/UserController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class UserController {
 
     @GetMapping("/")
     public String getIndexPage() {
         return "index";
+    }
+
+    @GetMapping("/reservation")
+    public String getReservationPage() {
+        return "reservation";
     }
 }
