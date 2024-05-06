182baa7a332ff4d3caef8ea794b6fa103878388a
seunghye218 <seunghye@student.42seoul.kr>
2024-05-07 00:45:08 +0900

    refactor(Controller): 뷰 컨트롤러 통합

--- src/main/java/roomescape/page/PageController.java
+++ src/main/java/roomescape/page/PageController.java
@@ -2,10 +2,19 @@ package roomescape.page;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class AdminController {
+public class PageController {
+
+    @GetMapping("/")
+    public String getPopularThemePage() {
+        return "index";
+    }
+
+    @GetMapping("/reservation")
+    public String getUserReservationPage() {
+        return "reservation";
+    }
 
     @GetMapping("/admin")
     public String getMainPage() {
