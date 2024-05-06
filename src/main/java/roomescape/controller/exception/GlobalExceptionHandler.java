a5c07d39a0a783b0ebbf33d598b3190c1ad464f7
Minjoo <m0522inju@gmail.com>
2024-05-06 17:38:14 +0900

    refactor: exceptionHandler 패키지 이동

--- src/main/java/roomescape/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/exception/GlobalExceptionHandler.java
@@ -1,8 +1,9 @@
-package roomescape.exception;
+package roomescape.controller.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import roomescape.exception.InvalidValueException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
