aeee323d3cd1040354c6b3655c9b29993276c176
reddevilmidzy <midzy0228@gmail.com>
2024-05-06 20:34:28 +0900

    test: 불필요한 @Sql 애노테이션 제거

--- src/test/java/roomescape/exception/GlobalExceptionHandlerTest.java
+++ src/test/java/roomescape/exception/GlobalExceptionHandlerTest.java
@@ -6,14 +6,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Map;
 
-@Sql(scripts = {"/data.sql"},
-        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(scripts = {"/drop.sql", "/schema.sql"},
-        executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GlobalExceptionHandlerTest {
 
