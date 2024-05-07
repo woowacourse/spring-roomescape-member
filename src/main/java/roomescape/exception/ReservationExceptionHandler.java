6ee5c1c5372c79280c2443447027a660895abc17
SungKyum Kim <sungkyum1@naver.com>
2024-05-07 13:13:16 +0900

    refactor: 에러 핸들러 통합

--- src/main/java/roomescape/exception/ReservationExceptionHandler.java
+++ src/main/java/roomescape/exception/ReservationExceptionHandler.java
@@ -4,18 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.NoSuchElementException;
-
 @RestControllerAdvice
 public class ReservationExceptionHandler {
 
     @ExceptionHandler
-    protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
-    }
-
-    @ExceptionHandler
-    protected ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e) {
+    protected ResponseEntity<ErrorResponse> handleDefaultException(RuntimeException e) {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 }
