272a414b527904b1cd96414f5606f38172286257
PgmJun <chltmdwns96@gmail.com>
2024-05-07 12:34:44 +0900

    refactor: 복잡성 제거를 위해 ApiResponse isSuccess 필드 제거

--- src/main/java/roomescape/global/dto/response/ApiResponse.java
+++ src/main/java/roomescape/global/dto/response/ApiResponse.java
@@ -3,21 +3,20 @@ package roomescape.global.dto.response;
 import roomescape.global.exception.error.ErrorType;
 
 public record ApiResponse<T>(
-        boolean isSuccess,
         String message,
         T data
 ) {
     private static final String SUCCESS_MESSAGE = "요청이 성공적으로 수행되었습니다.";
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(true, SUCCESS_MESSAGE, data);
+        return new ApiResponse<>(SUCCESS_MESSAGE, data);
     }
 
     public static <T> ApiResponse<T> success() {
-        return new ApiResponse<>(true, SUCCESS_MESSAGE, null);
+        return new ApiResponse<>(SUCCESS_MESSAGE, null);
     }
 
     public static <T> ApiResponse<T> fail(final ErrorType errorType) {
-        return new ApiResponse<>(false, errorType.getDescription(), null);
+        return new ApiResponse<>(errorType.getDescription(), null);
     }
 }
