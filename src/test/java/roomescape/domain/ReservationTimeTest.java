5e2f10d55fc938b3c23d3a0ff9e2d22a40450683
3juhwan <13selfesteem91@naver.com>
2024-05-07 20:52:07 +0900

    test: ReservationTime 도메인 테스트

---
+++ src/test/java/roomescape/domain/ReservationTimeTest.java
@@ -0,0 +1,17 @@
+package roomescape.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+class ReservationTimeTest {
+    @DisplayName("예약 시간을 생성한다")
+    @Test
+    void when_createReservationTime_then_created() {
+        assertThatCode(() -> new ReservationTime(LocalTime.of(12, 12, 12)))
+                .doesNotThrowAnyException();
+    }
+}
