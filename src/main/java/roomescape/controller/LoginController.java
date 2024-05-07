a8b4bb5d77108e47be2c1bd7ca1ce5ae7cc28758
reddevilmidzy <midzy0228@gmail.com>
2024-05-07 16:11:24 +0900

    feat: 인증 정보 조회 기능 구현

--- src/main/java/roomescape/controller/LoginController.java
+++ src/main/java/roomescape/controller/LoginController.java
@@ -1,8 +1,10 @@
 package roomescape.controller;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +24,22 @@ public class LoginController {
         response.setHeader("Keep-Alive", "timeout=600");
         return ResponseEntity.ok()
                 .build();
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<MemberResponse> check(final HttpServletRequest request) {
+        final MemberResponse member = getMember(request);
+        return ResponseEntity.ok(member);
+    }
+
+    private MemberResponse getMember(final HttpServletRequest request) {
+        //TODO Cannot read the array length because "<local3>" is null 뭔진 몰겠지만 이런 예외 발생
+        final Cookie[] cookies = request.getCookies();
+        for (final Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                return new MemberResponse(cookie.getValue());
+            }
+        }
+        return null; //TODO 예외 던지기
     }
 }
