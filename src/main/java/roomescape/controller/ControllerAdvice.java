fd34d80d92834786fefef5a513401a82b0132573
takoyakimchi <c4nyou@gmail.com>
2024-05-07 15:02:44 +0900

    feat: RuntimeException 글로벌 예외처리 로직 추가

--- src/main/java/roomescape/controller/ControllerAdvice.java
+++ src/main/java/roomescape/controller/ControllerAdvice.java
@@ -8,6 +8,12 @@ import roomescape.controller.dto.ErrorMessageResponse;
 @RestControllerAdvice
 public class ControllerAdvice {
 
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorMessageResponse> handleRuntimeException() {
+        ErrorMessageResponse errorMessageResponse = new ErrorMessageResponse("서버 내부 에러가 발생하였습니다.");
+        return ResponseEntity.internalServerError().body(errorMessageResponse);
+    }
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorMessageResponse> handleIllegalArgumentException(IllegalArgumentException e) {
         ErrorMessageResponse response = new ErrorMessageResponse(e.getMessage());
