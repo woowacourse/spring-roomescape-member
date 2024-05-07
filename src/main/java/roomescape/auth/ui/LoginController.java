b0dbcf6f202955a2973b26f7c8076148785f9457
Arachneee <jhg2819@naver.com>
2024-05-07 20:26:19 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/roomescape/auth/ui/LoginController.java
+++ src/main/java/roomescape/auth/ui/LoginController.java
@@ -3,20 +3,22 @@ package roomescape.auth.ui;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import roomescape.auth.application.AuthService;
 import roomescape.auth.dto.MemberResponse;
 import roomescape.auth.dto.TokenRequest;
 import roomescape.auth.infrastructure.AuthorizationExtractor;
 
-@RequestMapping("/login")
 @Controller
 public class LoginController {
+
+    public static final String TOKEN_NAME = "token";
+
     private final AuthService authService;
     private final AuthorizationExtractor authorizationExtractor;
 
@@ -25,26 +27,37 @@ public class LoginController {
         this.authorizationExtractor = new AuthorizationExtractor();
     }
 
-
-    @GetMapping
+    @GetMapping("/login")
     public String page() {
         return "login";
     }
 
-    @PostMapping
+    @PostMapping("/login")
     public void login(@RequestBody TokenRequest tokenRequest, HttpServletResponse response) {
         String accessToken = authService.createToken(tokenRequest);
 
-        Cookie cookie = new Cookie("token", accessToken);
+        Cookie cookie = new Cookie(TOKEN_NAME, accessToken);
         cookie.setHttpOnly(true);
         cookie.setPath("/");
         response.addCookie(cookie);
     }
 
-    @GetMapping("/check")
+    @GetMapping("/login/check")
     public ResponseEntity<MemberResponse> findMyInfo(HttpServletRequest request) {
         String token = authorizationExtractor.extract(request);
         MemberResponse member = authService.findMemberByToken(token);
         return ResponseEntity.ok().body(member);
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+
+        Arrays.stream(cookies)
+                .filter(cookie -> TOKEN_NAME.equals(cookie.getName()))
+                .forEach(cookie -> {
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                });
     }
 }
