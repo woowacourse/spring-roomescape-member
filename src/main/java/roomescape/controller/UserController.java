a01f6908802ca4bc72672fc3f7536391a0529ee0
reddevilmidzy <midzy0228@gmail.com>
2024-05-07 16:22:46 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/roomescape/controller/UserController.java
+++ src/main/java/roomescape/controller/UserController.java
@@ -1,7 +1,10 @@
 package roomescape.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -18,6 +21,15 @@ public class UserController {
 
     @GetMapping("/login")
     public String showLoginPage() {
+        return "/login";
+    }
+
+    //TODO 여기에 있는게 맞을 지 고민해보자.
+    @PostMapping("/logout")
+    public String logout(final HttpServletResponse response) {
+        Cookie cookie = new Cookie("token", null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
         return "/login";
     }
 }
