bcb9b51969d7d0014e6af75274617aaafe28e370
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-07 17:20:32 +0900

    fix(GlobalExceptionHandler): MethodArgumentNotValidException 핸들링 시 여러개의 예외인 경우 첫 메시지만 응답하는 것을 전체 메시지로 변경

--- src/main/java/roomescape/common/GlobalExceptionHandler.java
+++ src/main/java/roomescape/common/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package roomescape.common;
 
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,23 +12,33 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final String EXCEPTION_PREFIX = "[ERROR] ";
+
     @ExceptionHandler
     public ResponseEntity<String> catchValidationException(MethodArgumentNotValidException ex) {
-        return ResponseEntity.badRequest().body(ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage() );
+        String exceptionMessages = EXCEPTION_PREFIX + ex.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining("\n" + EXCEPTION_PREFIX));
+
+        System.out.println(exceptionMessages);
+        return ResponseEntity.badRequest().body(exceptionMessages);
     }
 
     @ExceptionHandler
     public ResponseEntity<String> catchBadRequestException(IllegalArgumentException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+        System.out.println(EXCEPTION_PREFIX + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(EXCEPTION_PREFIX + ex.getMessage());
     }
 
     @ExceptionHandler
     public ResponseEntity<String> catchConflictException(IllegalStateException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+        System.out.println(EXCEPTION_PREFIX + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(EXCEPTION_PREFIX + ex.getMessage());
     }
 
     @ExceptionHandler
     public ResponseEntity<String> catchNotFoundException(NoSuchElementException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        System.out.println(EXCEPTION_PREFIX + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(EXCEPTION_PREFIX + ex.getMessage());
     }
 }
