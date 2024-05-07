ba7fec2d1a027210965c3f9f4f0ac7d91f2e7769
kelly6bf <kelly6bf@gmail.com>
2024-05-07 11:18:46 +0900

    feat: 로그인 페이지 라우팅 등록

--- src/main/java/roomescape/controller/UserWebController.java
+++ src/main/java/roomescape/controller/UserWebController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserWebController {
 
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "/login";
+    }
+
     @GetMapping
     public String getPopularThemePage() {
         return "/index";
