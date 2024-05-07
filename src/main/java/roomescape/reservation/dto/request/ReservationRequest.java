6c525e630a836d497ae8bdfa953c50a74cf390d0
leetaehoon <xogns1514@gmail.com>
2024-05-07 14:10:45 +0900

    feat: 잘못된 형식의 날짜/시간에 대한 예외 처리 추가

--- src/main/java/roomescape/reservation/dto/request/ReservationRequest.java
+++ src/main/java/roomescape/reservation/dto/request/ReservationRequest.java
@@ -1,11 +1,14 @@
 package roomescape.reservation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.domain.ReservationTime;
 import roomescape.reservation.domain.Theme;
 
-public record ReservationRequest(String name, LocalDate date, Long timeId, Long themeId) {
+public record ReservationRequest(String name,
+                                 @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+                                 LocalDate date, Long timeId, Long themeId) {
 
     public Reservation toEntity(ReservationTime reservationTime, Theme theme) {
         return new Reservation(null, name, date, reservationTime, theme);
