9cdea118fdc9d180ba4d5d92a8b535a20efafbe0
takoyakimchi <c4nyou@gmail.com>
2024-05-07 21:49:12 +0900

    test: ReservationTest 가독성 개선

--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -8,28 +8,31 @@ import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ReservationTest {
 
-    @DisplayName("생성 테스트")
-    @Test
-    void create() {
-        assertThatCode(() -> new Reservation(1L, "wiib", LocalDate.now(), new ReservationTime(1L, LocalTime.now()),
-            new Theme("방탈출", "방탈출하는 게임", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg")))
-            .doesNotThrowAnyException();
-        assertThatCode(() -> new Reservation("wiib", LocalDate.now(), new ReservationTime(1L, LocalTime.now()),
-            new Theme("방탈출", "방탈출하는 게임", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg")))
-            .doesNotThrowAnyException();
+    private final String validName = "sudal";
+    private final LocalDate validDate = LocalDate.parse("2060-01-01");
+    private final ReservationTime validReservationTime = new ReservationTime(LocalTime.parse("10:00"));
+    private final Theme validTheme = new Theme("방탈출", "방탈출하는 게임", "https://");
 
+    @DisplayName("성공: 올바른 값 입력하여 생성")
+    @Test
+    void construct() {
+        assertThatCode(
+            () -> new Reservation(validName, validDate, validReservationTime, validTheme)
+        ).doesNotThrowAnyException();
     }
 
-    @DisplayName("빈 이름으로 생성하면 예외가 발생한다.")
+    @DisplayName("실패: 올바르지 않은 예약자명")
     @ParameterizedTest
-    @ValueSource(strings = {"", " "})
-    void create_WithBlankName(String name) {
-        assertThatThrownBy(() -> new Reservation(name, LocalDate.MAX, new ReservationTime(LocalTime.MAX),
-            new Theme("방탈출", "방탈출하는 게임", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg")))
-            .isInstanceOf(IllegalArgumentException.class);
+    @ValueSource(strings = {" "})
+    @NullAndEmptySource
+    void construct_IllegalName(String invalidName) {
+        assertThatThrownBy(
+            () -> new Reservation(invalidName, validDate, validReservationTime, validTheme)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
