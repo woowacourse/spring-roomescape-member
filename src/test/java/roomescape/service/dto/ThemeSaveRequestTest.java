30a65d2b004f37a963ab4309fb5dca28a4c955a4
nhlee98 <e5slnh9876@gmail.com>
2024-05-07 20:41:00 +0900

    test: 실패한 테스트를 성공 시키도록 수정

--- src/test/java/roomescape/service/dto/ThemeSaveRequestTest.java
+++ src/test/java/roomescape/service/dto/ThemeSaveRequestTest.java
@@ -1,29 +1,72 @@
 package roomescape.service.dto;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import roomescape.exception.IllegalUserRequestException;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ThemeSaveRequestTest {
+
+    private static ValidatorFactory factory;
+    private static Validator validator;
+
+    @BeforeAll
+    public static void init() {
+        factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    @DisplayName("이름이 정상 입력될 경우 성공한다.")
+    void checkNameBlank_Success() {
+        // given
+        ReservationSaveRequest request = new ReservationSaveRequest("naknak", LocalDate.now(), 1L, 1L);
+
+        // when
+        Set<ConstraintViolation<ReservationSaveRequest>> violations = validator.validate(request);
+
+        // then
+        assertThat(violations).isEmpty();
+    }
 
     @Test
     @DisplayName("테마 이름이 정상 입력될 경우 성공한다.")
     void checkThemeNameBlank_Success() {
-        assertThatCode(() -> new ThemeSaveRequest("capy", "description", "thumbnail"))
-                .doesNotThrowAnyException();
+        // given
+        ThemeSaveRequest request = new ThemeSaveRequest("naknak", "description", "thumbnail");
+
+        // when
+        Set<ConstraintViolation<ThemeSaveRequest>> violations = validator.validate(request);
+
+        // then
+        assertThat(violations).isEmpty();
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"", " ", "  "})
+    @NullAndEmptySource
     @DisplayName("테마 이름이 빈칸인 경우 예외가 발생한다.")
     void checkThemeNameBlank_Failure(String name) {
-        assertThatThrownBy(() -> new ThemeSaveRequest(name, "description", "thumbnail"))
-                .isInstanceOf(IllegalUserRequestException.class)
-                .hasMessage("테마 이름은 빈칸일 수 없습니다.");
+        // given
+        ThemeSaveRequest request = new ThemeSaveRequest(name, "description", "thumbnail");
+
+        // when
+        Set<ConstraintViolation<ThemeSaveRequest>> violations = validator.validate(request);
+        Optional<String> exceptionMessage = violations.stream()
+                .map(ConstraintViolation::getMessage)
+                .findFirst();
+
+        // then
+        assertThat(exceptionMessage).contains("테마 제목은 빈칸일 수 없습니다.");
     }
 }
