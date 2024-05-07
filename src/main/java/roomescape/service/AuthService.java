96c73c053022fe9c016c5c73aa0efc1db88fb49c
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-07 17:13:43 +0900

    refactor(user-scripts.js): 클라이언트 코드 수정

--- src/main/java/roomescape/service/AuthService.java
+++ src/main/java/roomescape/service/AuthService.java
@@ -1,11 +1,13 @@
 package roomescape.service;
 
+import org.springframework.stereotype.Service;
 import roomescape.domain.user.User;
 import roomescape.dto.LoginRequest;
 import roomescape.dto.TokenResponse;
 import roomescape.dto.UserPayload;
 import roomescape.infrastructure.JwtProvider;
 
+@Service
 public class AuthService {
 
     private final JwtProvider jwtProvider;
@@ -15,7 +17,8 @@ public class AuthService {
     }
 
     public TokenResponse login(LoginRequest request) {
-        User loginUser = authenticateUser(request);
+//        User loginUser = authenticateUser(request);
+        User loginUser = new User(1L, "kim", "eee", "21313");
         UserPayload loginUserPayload = UserPayload.from(loginUser);
         String token = jwtProvider.createToken(loginUserPayload);
         return new TokenResponse(token);
