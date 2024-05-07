27000c2d15cd32d28acd0e83753e6d4dbd5e6919
pricelees <priceelees@gmail.com>
2024-05-07 16:23:40 +0900

    refactor: URL 접속 테스트를 ParameterizedTest 로 수정

--- src/test/java/roomescape/controller/UserPageControllerTest.java
+++ src/test/java/roomescape/controller/UserPageControllerTest.java
@@ -4,7 +4,8 @@ package roomescape.controller;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -19,20 +20,12 @@ public class UserPageControllerTest {
         RestAssured.port = port;
     }
 
-    @DisplayName("유저 예약 페이지 요청 시 200으로 응답한다.")
-    @Test
-    void welcomePageTest() {
+    @DisplayName("유저 페이지들에 대한 요청시 200으로 응답한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "/reservation"})
+    void userPage(String url) {
         RestAssured.given().log().all()
-                .when().get("/reservation")
-                .then().log().all()
-                .statusCode(200);
-    }
-
-    @DisplayName("인기 테마 페이지 요청 시 200으로 응답한다.")
-    @Test
-    void adminPageTest() {
-        RestAssured.given().log().all()
-                .when().get("/")
+                .when().get(url)
                 .then().log().all()
                 .statusCode(200);
     }
