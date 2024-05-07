4e526b059336a4111265a7fde1a0b2449a0430c7
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-07 14:13:19 +0900

    refactor: Thumbnail 객체 분리

--- src/main/java/roomescape/domain/Theme.java
+++ src/main/java/roomescape/domain/Theme.java
@@ -1,22 +1,14 @@
 package roomescape.domain;
 
-import roomescape.exception.InvalidReservationException;
-
 public class Theme {
     private static final long NO_ID = 0;
-    private static final int MINIMUM_THUMBNAIL_LENGTH = 1;
-    private static final int MAXIMUM_THUMBNAIL_LENGTH = 250;
-    private static final String INVALID_THUMBNAIL_LENGTH = String.format("썸네일 URL은 %d자 이상, %d자 이하여야 합니다.",
-            MINIMUM_THUMBNAIL_LENGTH,
-            MAXIMUM_THUMBNAIL_LENGTH);
 
     private final long id;
     private final ThemeName name;
     private final Description description;
-    private final String thumbnail;
+    private final Thumbnail thumbnail;
 
-    public Theme(long id, ThemeName name, Description description, String thumbnail) {
-        validateThumbnail(thumbnail);
+    public Theme(long id, ThemeName name, Description description, Thumbnail thumbnail) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -24,7 +16,7 @@ public class Theme {
     }
 
     public Theme(final String name, final String description, final String thumbnail) {
-        this(NO_ID, new ThemeName(name), new Description(description), thumbnail);
+        this(NO_ID, new ThemeName(name), new Description(description), new Thumbnail(thumbnail));
     }
 
     public Theme(long id, Theme theme) {
@@ -32,13 +24,7 @@ public class Theme {
     }
 
     public Theme(long id, String name, String description, String thumbnail) {
-        this(id, new ThemeName(name), new Description(description), thumbnail);
-    }
-
-    private void validateThumbnail(final String thumbnail) {
-        if (thumbnail.isEmpty() || thumbnail.length() > MAXIMUM_THUMBNAIL_LENGTH) {
-            throw new InvalidReservationException(INVALID_THUMBNAIL_LENGTH);
-        }
+        this(id, new ThemeName(name), new Description(description), new Thumbnail(thumbnail));
     }
 
     public long getId() {
@@ -54,6 +40,6 @@ public class Theme {
     }
 
     public String getThumbnail() {
-        return thumbnail;
+        return thumbnail.getValue();
     }
 }
