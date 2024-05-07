a7032cf929ba46afb17cccceb48216f82b26c6a8
fromitive <fromitive@gmail.com>
2024-05-08 06:11:05 +0900

    refactor: 도메인 객체가 반환되지 않도록 변경

--- src/main/java/roomescape/domain/dto/ReservationResponse.java
+++ src/main/java/roomescape/domain/dto/ReservationResponse.java
@@ -1,19 +1,17 @@
 package roomescape.domain.dto;
 
 import roomescape.domain.Reservation;
-import roomescape.domain.Theme;
-import roomescape.domain.TimeSlot;
 
 import java.time.LocalDate;
 
-public record ReservationResponse(Long id, String name, LocalDate date, TimeSlot time, Theme theme) {
+public record ReservationResponse(Long id, String name, LocalDate date, TimeSlotResponse time, ThemeResponse theme) {
     public static ReservationResponse from(final Reservation reservation) {
         return new ReservationResponse(
                 reservation.getId(),
                 reservation.getName(),
                 reservation.getDate(),
-                reservation.getTime(),
-                reservation.getTheme()
+                TimeSlotResponse.from(reservation.getTime()),
+                ThemeResponse.from(reservation.getTheme())
         );
     }
 }
