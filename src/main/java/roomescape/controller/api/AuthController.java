69cc128d6fd63ef5e942fa54b615d38fd9b87ba4
김민수 MINSOO KIM <alstn113@gmail.com>
2024-05-07 14:25:38 +0900

    feat: 토큰 쿠키 생성 구현

--- src/main/java/roomescape/controller/api/AuthController.java
+++ src/main/java/roomescape/controller/api/AuthController.java
@@ -1,7 +1,10 @@
 package roomescape.controller.api;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import java.util.Arrays;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +15,9 @@ import roomescape.service.AuthService;
 @RestController
 public class AuthController {
 
+    private static final String TOKEN = "token";
+    private static final int TOKEN_MAX_AGE = 60 * 60; // 1 hour
+
     private final AuthService authService;
 
     public AuthController(AuthService authService) {
@@ -21,14 +27,20 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<Void> login(
             @RequestBody @Valid LoginRequest loginRequest,
-            HttpServletRequest request
+            HttpServletResponse response
     ) {
         String token = authService.createToken(loginRequest);
-        setTokenCookie(token, request);
+        setTokenCookie(response, token);
 
         return ResponseEntity.ok().build();
     }
 
-    private void setTokenCookie(String token, HttpServletRequest request) {
+    private void setTokenCookie(HttpServletResponse response, String token) {
+        Cookie cookie = new Cookie(TOKEN, token);
+        cookie.setMaxAge(TOKEN_MAX_AGE);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+
+        response.addCookie(cookie);
     }
 }
