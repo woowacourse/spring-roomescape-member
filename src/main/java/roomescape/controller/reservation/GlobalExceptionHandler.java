62870060b249bb29bab354e736e66a0991a3ff54
devhoya97 <diajbh123@gmail.com>
2024-05-07 14:49:29 +0900

    feat: 서버에서 예기치 않은 오류가 발생했을 때 처리 기능 구현

--- src/main/java/roomescape/controller/reservation/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/reservation/GlobalExceptionHandler.java
@@ -13,13 +13,19 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ClientException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleIllegalArgumentException(ClientException e) {
+    public String handleClientException(ClientException e) {
         return e.getMessage();
     }
 
     @ExceptionHandler(value = DateTimeParseException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleException() {
+    public String handleDateTimeParseException() {
         return "올바르지 않은 날짜 / 시간 형식입니다.";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException() {
+        return "서버에서 예기치 않은 오류가 발생했습니다.";
     }
 }
