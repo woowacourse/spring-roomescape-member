9bc1f0f8cc5822a19bbe99434fe1ee6a339a209d
김민수 MINSOO KIM <alstn113@gmail.com>
2024-05-07 11:07:48 +0900

    feat: 인증 의존성 추가

--- src/main/java/roomescape/controller/api/MemberController.java
+++ src/main/java/roomescape/controller/api/MemberController.java
@@ -1,12 +1,7 @@
 package roomescape.controller.api;
 
-import java.util.List;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.domain.member.Member;
-import roomescape.dto.response.MemberResponse;
 import roomescape.service.MemberService;
 
 @RestController
