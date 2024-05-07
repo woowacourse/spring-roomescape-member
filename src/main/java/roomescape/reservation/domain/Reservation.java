4196b5917b67bedf34264da02c5f681ff9c2a0dc
tackyu <tack970913@gmail.com>
2024-05-07 21:31:53 +0900

    style: 불필요 주석 제거

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -10,9 +10,7 @@ public record Reservation(long id, String name, LocalDate date, ReservationTime 
     public Reservation(long id, String name, LocalDate date, ReservationTime time, Theme theme) {
         this.id = id;
         this.name = validateName(name);
-        //LocalDateTime dateTime = validateDateTime(date, time.startAt());
-        //this.date = dateTime.toLocalDate();
-        this.date=date;
+        this.date = date;
         this.time = time;
         this.theme = theme;
     }
@@ -27,11 +25,4 @@ public record Reservation(long id, String name, LocalDate date, ReservationTime 
         return name;
     }
 
-//    private LocalDateTime validateDateTime(LocalDate date, LocalTime time) {
-//        LocalDateTime dateTime = LocalDateTime.of(date, time);
-//        if (dateTime.isBefore(LocalDateTime.now())) {
-//            throw new IllegalArgumentException("Cannot create a reservation for a past date and time.");
-//        }
-//        return dateTime;
-//    }
 }
