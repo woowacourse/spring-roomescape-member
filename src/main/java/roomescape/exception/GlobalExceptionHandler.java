8f71a8bf109c63fbd0d77e512df4d833de6b416a
Choo <choo000407@naver.com>
2024-05-07 21:20:43 +0900

    refactor: path, query parameter null 값 예외 처리

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -3,8 +3,10 @@ package roomescape.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,7 +17,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = HttpMessageConversionException.class)
     public ResponseEntity<String> handleJsonParsingException() {
-        return new ResponseEntity<>("유효하지 않은 필드가 존재합니다.", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("요청 body에 유효하지 않은 필드가 존재합니다.", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {NoResourceFoundException.class, MissingServletRequestParameterException.class})
+    public ResponseEntity<String> handleRequestException() {
+        return new ResponseEntity<>("요청 경로에 필요한 변수가 제공되지 않았습니다.", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = Exception.class)
