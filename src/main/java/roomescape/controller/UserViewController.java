aa948412b829c54ea5aa2e4209c542165ca61b37
nhlee98 <e5slnh9876@gmail.com>
2024-05-07 17:26:33 +0900

    style: 클래스 이름 오타 수정

--- src/main/java/roomescape/controller/UserViewController.java
+++ src/main/java/roomescape/controller/UserViewController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UserViewContorller {
+public class UserViewController {
     @GetMapping("/reservation")
     public String reservationUserPage() {
         return "reservation";
