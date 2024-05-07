fb23123f3aa6ece84c47cd35daf3d953093543ff
ikjo39 <chi4321@naver.com>
2024-05-07 16:35:45 +0900

    fix: 사용자 도메인 내 id 필드 추가

--- src/main/java/roomescape/domain/member/Member.java
+++ src/main/java/roomescape/domain/member/Member.java
@@ -2,14 +2,20 @@ package roomescape.domain.member;
 
 public class Member {
 
+    private final Long id;
     private final MemberName name;
     private final MemberEmail email;
     private final MemberPassword password;
 
-    public Member(MemberName name, MemberEmail email, MemberPassword password) {
+    public Member(Long id, MemberName name, MemberEmail email, MemberPassword password) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public MemberName getName() {
