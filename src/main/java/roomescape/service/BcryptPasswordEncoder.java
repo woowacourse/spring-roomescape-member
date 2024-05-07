b74e979d232b04372adb1051d3a3c171d63352a1
김민수 MINSOO KIM <alstn113@gmail.com>
2024-05-07 11:24:49 +0900

    feat: 패스워드 암호화 설정 작성

---
+++ src/main/java/roomescape/service/BcryptPasswordEncoder.java
@@ -0,0 +1,16 @@
+package roomescape.service;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class BcryptPasswordEncoder implements PasswordEncoder {
+
+    @Override
+    public String encode(String rawPassword) {
+        return BCrypt.hashpw(rawPassword, BCrypt.gensalt());
+    }
+
+    @Override
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return BCrypt.checkpw(rawPassword, encodedPassword);
+    }
+}
