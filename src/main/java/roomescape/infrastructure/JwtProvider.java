badd997917781f5c4085adbbe388a01408212348
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-07 17:01:59 +0900

    feat(JwtProvider): 토큰 생성 클래스 추가

---
+++ src/main/java/roomescape/infrastructure/JwtProvider.java
@@ -0,0 +1,21 @@
+package roomescape.infrastructure;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import roomescape.dto.UserPayload;
+
+@Component
+public class JwtProvider {
+
+    private final String secretKey = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E";
+
+    public String createToken(UserPayload userPayload) {
+        return Jwts.builder()
+                .setSubject(userPayload.id())
+                .claim("name", userPayload.name())
+                .claim("email", userPayload.email())
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+}
