85988dcbfd6855f36dad0368f3c67452e4881724
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-05-07 11:17:36 +0900

    feat(GlobalExceptionHandler): 예상치 못한 에러 처리
    - 기존에 만든 형식과 통일성있게 사용

--- src/main/java/roomescape/controller/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import roomescape.dto.ErrorResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     private static final String NULL_POINTER_EXCEPTION_ERROR_MESSAGE = "인자 중 null 값이 존재합니다.";
+    private static final String UNEXPECTED_EXCEPTION_ERROR_MESSAGE = "예상치 못한 예외가 발생했습니다. 관리자에게 문의하세요.";
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
@@ -19,5 +20,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleNullPointerException(NullPointerException e) {
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(NULL_POINTER_EXCEPTION_ERROR_MESSAGE));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleUnexpectedException(RuntimeException e) {
+        return ResponseEntity.internalServerError()
+                .body(new ErrorResponse(UNEXPECTED_EXCEPTION_ERROR_MESSAGE));
     }
 }
