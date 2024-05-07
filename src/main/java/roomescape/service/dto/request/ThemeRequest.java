64356116f2b76683f3851b59866ae24607db7b41
3juhwan <13selfesteem91@naver.com>
2024-05-07 15:28:21 +0900

    fix: DTO에 과한 검증 책임 삭제

--- src/main/java/roomescape/service/dto/request/ThemeRequest.java
+++ src/main/java/roomescape/service/dto/request/ThemeRequest.java
@@ -1,12 +1,11 @@
 package roomescape.service.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import roomescape.domain.Theme;
 
 public record ThemeRequest(
-        @NotBlank @Size(max = 20) String name,
-        @NotBlank @Size(max = 1000) String description,
+        @NotBlank String name,
+        @NotBlank String description,
         String thumbnail
 ) {
     public Theme toDomain() {
