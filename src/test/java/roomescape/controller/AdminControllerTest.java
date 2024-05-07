0d4f2f7a2fc49471ef0c790d75ccd7f0fcb9fe07
takoyakimchi <c4nyou@gmail.com>
2024-05-07 10:30:37 +0900

    refactor: 웹 컨트롤러 테스트에서 mock 제거

--- src/test/java/roomescape/controller/AdminControllerTest.java
+++ src/test/java/roomescape/controller/AdminControllerTest.java
@@ -1,38 +1,46 @@
 package roomescape.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@WebMvcTest(AdminController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class AdminControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
-
-    @DisplayName("/admin 페이지를 요청할 수 있다.")
+    @DisplayName("/admin 페이지 응답 -> 200")
     @Test
-    void getIndexPage() throws Exception {
-        mvc.perform(get("/admin"))
-            .andExpect(status().isOk());
+    void getAdminPage() {
+        RestAssured.given().log().all()
+            .when().get("/admin")
+            .then().log().all()
+            .statusCode(200);
     }
 
-    @DisplayName("/admin/reservation 페이지를 요청할 수 있다.")
+    @DisplayName("/admin/reservation 페이지 응답 -> 200")
     @Test
-    void getReservationPage() throws Exception {
-        mvc.perform(get("/admin/reservation"))
-            .andExpect(status().isOk());
+    void getReservationPage() {
+        RestAssured.given().log().all()
+            .when().get("/admin/reservation")
+            .then().log().all()
+            .statusCode(200);
     }
 
-    @DisplayName("/admin/time 페이지를 요청할 수 있다.")
+    @DisplayName("/admin/time 페이지 응답 -> 200")
     @Test
-    void getReservationTimePage() throws Exception {
-        mvc.perform(get("/admin/time"))
-            .andExpect(status().isOk());
+    void getReservationTimePage() {
+        RestAssured.given().log().all()
+            .when().get("/admin/time")
+            .then().log().all()
+            .statusCode(200);
+    }
+
+    @DisplayName("/admin/theme 페이지 응답 -> 200")
+    @Test
+    void getThemePage() {
+        RestAssured.given().log().all()
+            .when().get("/admin/theme")
+            .then().log().all()
+            .statusCode(200);
     }
 }
