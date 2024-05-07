e443ab91d6e916245f36bd523b6b093dce3cd441
김민수 MINSOO KIM <alstn113@gmail.com>
2024-05-07 10:32:54 +0900

    feat: controller 생성

---
+++ src/main/java/roomescape/controller/api/MemberController.java
@@ -0,0 +1,21 @@
+package roomescape.controller.api;
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.member.Member;
+import roomescape.dto.response.MemberResponse;
+import roomescape.service.MemberService;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
---
+++ src/main/java/roomescape/repository/JdbcMemberService.java
@@ -0,0 +1,8 @@
+package roomescape.repository;
+
+import org.springframework.stereotype.Repository;
+import roomescape.domain.member.MemberRepository;
+
+@Repository
+public class JdbcMemberService implements MemberRepository {
+}
