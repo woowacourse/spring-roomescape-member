0ea9985184a0ab26ff27e81f7debdbce20e4ecb4
juha <khabh@naver.com>
2024-05-07 17:35:27 +0900

    feat: 토큰 생성 기능 구현

---
+++ src/main/java/roomescape/auth/JwtTokenProvider.java
@@ -0,0 +1,22 @@
+package roomescape.auth;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import roomescape.model.Member;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+
+    public String createToken(final Member member) {
+        return Jwts.builder()
+                .setSubject(member.getId().toString())
+                .claim("name", member.getName())
+                .claim("role", member.getRole())
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+}
