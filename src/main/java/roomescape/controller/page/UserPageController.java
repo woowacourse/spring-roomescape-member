885d57aa0e4e637c72299e79f874dc39bd151f8c
MingyeomKim <mingyum119@naver.com>
2024-05-08 21:06:23 +0900

    fix: 로그인 화면이 뜨지 않는 오류 현상 제거

--- src/main/java/roomescape/controller/page/UserPageController.java
+++ src/main/java/roomescape/controller/page/UserPageController.java
@@ -1,7 +1,9 @@
 package roomescape.controller.page;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class UserPageController {
     @GetMapping("/")
     public String getHomePage() {
