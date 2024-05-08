8869a8683a4279b1826a6f3e920b7b749e26ec78
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-08 22:47:21 +0900

    feat(JwtTokenProvider): 토큰 생성

---
+++ src/main/java/roomescape/infrastructure/JwtTokenProvider.java
@@ -0,0 +1,28 @@
+package roomescape.infrastructure;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("/application-secret.properties")
+public class JwtTokenProvider {
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+    @Value("${security.jwt.token.expire-length}")
+    private long expireLength;
+
+    public String createToken(String email) {
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + expireLength);
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+}
