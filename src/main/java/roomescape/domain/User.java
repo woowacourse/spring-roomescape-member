63d74157ee215b31fabc11c556a4c5e02ea0f027
fram1998 <koust6u@pusan.ac.kr>
2024-05-08 16:30:18 +0900

    feat: 사용자 도메인을 추가 구현

---
+++ src/main/java/roomescape/domain/User.java
@@ -0,0 +1,13 @@
+package roomescape.domain;
+
+public class User {
+    private final String name;
+    private final String email;
+    private final String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
