d061124f06f8a696531f1511468b660a953faf9f
unifolio0 <boho82ha@gmail.com>
2024-05-08 17:34:59 +0900

    refactor: 메소드 이동

--- src/main/java/roomescape/controller/MemberController.java
+++ src/main/java/roomescape/controller/MemberController.java
@@ -1,19 +1,24 @@
 package roomescape.controller;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.controller.request.UserLoginRequest;
 import roomescape.controller.request.UserSignUpRequest;
 import roomescape.controller.response.MemberResponse;
+import roomescape.controller.response.UserResponse;
 import roomescape.service.MemberService;
 
 @RestController
-@RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
 
@@ -21,16 +26,58 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping
+    @GetMapping("/members")
     public ResponseEntity<List<MemberResponse>> findAll() {
         List<MemberResponse> memberResponses = memberService.findAll();
 
         return ResponseEntity.ok(memberResponses);
     }
 
-    @PostMapping
+    @PostMapping("/members")
     public ResponseEntity<Void> save(@RequestBody UserSignUpRequest userSignUpRequest) {
         memberService.save(userSignUpRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> userLogin(HttpServletResponse response, @RequestBody UserLoginRequest userLoginRequest) {
+        String accessToken = memberService.createToken(userLoginRequest);
+        Cookie cookie = new Cookie("token", accessToken);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/login/check")
+    public ResponseEntity<UserResponse> checkLogin(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        String token = extractTokenFromCookie(cookies);
+        Claims claims = Jwts.parser()
+                .setSigningKey("secret")
+                .parseClaimsJws(token)
+                .getBody();
+        String email = String.valueOf(claims.get("email"));
+        UserResponse userResponse = memberService.findByEmail(email);
+        return ResponseEntity.ok(userResponse);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> userLogout(HttpServletResponse response) {
+        Cookie cookie = new Cookie("token", null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return ResponseEntity.ok().build();
+    }
+
+    private String extractTokenFromCookie(Cookie[] cookies) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                return cookie.getValue();
+            }
+        }
+
+        return "";
     }
 }
