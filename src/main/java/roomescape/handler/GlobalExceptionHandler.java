d04660011b3d08f4ccda6af52b29851faea4375b
DobbyMac <kimdobby@g.skku.edu>
2024-05-08 14:17:35 +0900

    refactor(GlobalExceptionHandler): 메서드 순서 배치 순서 변경 및 콘솔 에러 로그 출력

--- src/main/java/roomescape/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/handler/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package roomescape.handler;
 
 import java.sql.SQLException;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +13,15 @@ import roomescape.exception.ConflictException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class.getName());
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception exception) {
+        logger.error(exception.getMessage());
+        return ResponseEntity.internalServerError()
+                .body("문제가 발생했습니다.");
+    }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<String> handleBadRequestException(BadRequestException exception) {
@@ -25,18 +35,6 @@ public class GlobalExceptionHandler {
                 .body(exception.getMessage());
     }
 
-    @ExceptionHandler(SQLException.class)
-    public ResponseEntity<String> handleInternalDataBaseException() {
-        return ResponseEntity.internalServerError()
-                .body("데이터 저장 중 오류가 발생하였습니다.");
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException() {
-        return ResponseEntity.internalServerError()
-                .body("오류가 발생했습니다.");
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         return ResponseEntity.badRequest()
@@ -44,6 +42,13 @@ public class GlobalExceptionHandler {
                         .getAllErrors()
                         .get(0)
                         .getDefaultMessage());
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<String> handleInternalDataBaseException(SQLException exception) {
+        logger.error(exception.getMessage());
+        return ResponseEntity.internalServerError()
+                .body("데이터 저장 중 문제가 발생하였습니다.");
     }
 
 }
