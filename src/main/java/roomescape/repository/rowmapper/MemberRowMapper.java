40bbe1fb6e93c1863606f8d8144bcf6e4b9bd3bb
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-08 22:33:39 +0900

    refactor: SQLException 예외 전환

--- src/main/java/roomescape/repository/rowmapper/MemberRowMapper.java
+++ src/main/java/roomescape/repository/rowmapper/MemberRowMapper.java
@@ -10,12 +10,16 @@ import java.sql.SQLException;
 @Component
 public class MemberRowMapper implements RowMapper<Member> {
     @Override
-    public Member mapRow(ResultSet resultSet, int rowNum) throws SQLException {
-        return new Member(
-                resultSet.getLong("id"),
-                resultSet.getString("name"),
-                resultSet.getString("user_email"),
-                resultSet.getString("user_password")
-        );
+    public Member mapRow(ResultSet resultSet, int rowNum) {
+        try {
+            return new Member(
+                    resultSet.getLong("id"),
+                    resultSet.getString("user_name"),
+                    resultSet.getString("user_email"),
+                    resultSet.getString("user_password")
+            );
+        } catch (SQLException exception) {
+            throw new RuntimeException("[ERROR] member 테이블 접근 오류", exception);
+        }
     }
 }
