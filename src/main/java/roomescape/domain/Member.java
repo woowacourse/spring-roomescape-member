aec00b0cf54a23f2152fd30be2a0c78b01330cfa
hyeonjilee <skylar12200@gmail.com>
2024-05-08 09:04:54 +0900

    feat: 사용자 도메인 추가

---
+++ src/main/java/roomescape/domain/Member.java
@@ -0,0 +1,26 @@
+package roomescape.domain;
+
+public class Member {
+
+    private final String name;
+    private final String email;
+    private final String password;
+
+    public Member(final String name, final String email, final String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
