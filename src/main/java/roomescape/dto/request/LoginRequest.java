ed03638e667be4d6e94c3e96e313832c091284e3
MingyeomKim <mingyum119@naver.com>
2024-05-08 21:09:37 +0900

    feat: 이메일과 비밀번호의 유효성을 검증한다

--- src/main/java/roomescape/dto/request/LoginRequest.java
+++ src/main/java/roomescape/dto/request/LoginRequest.java
@@ -1,7 +1,14 @@
 package roomescape.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequest(
+        @NotBlank
+        @Email
         String email,
+        
+        @NotBlank
         String password
 ) {
 }
