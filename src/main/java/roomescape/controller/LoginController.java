35f53dd2cde8a28a6c2b0b4f1d4d48a18a8fee1f
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-08 15:41:38 +0900

    feat(LoginController): 로그인 사용자 확인 요청 처리 기능 추가

--- src/main/java/roomescape/controller/LoginController.java
+++ src/main/java/roomescape/controller/LoginController.java
@@ -1,13 +1,17 @@
 package roomescape.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.dto.LoginCheckResponse;
 import roomescape.dto.LoginRequest;
-import roomescape.dto.TokenResponse;
+import roomescape.dto.TokenDto;
 import roomescape.service.AuthService;
 
 @RestController
@@ -21,7 +25,7 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<Void> login(@RequestBody LoginRequest request) {
-        TokenResponse loginToken = authService.login(request);
+        TokenDto loginToken = authService.login(request);
 
         ResponseCookie cookie = ResponseCookie
                 .from("token", loginToken.accessToken())
@@ -33,5 +37,24 @@ public class LoginController {
                 .ok()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
                 .build();
+    }
+
+    @GetMapping("/login/check")
+    public ResponseEntity<LoginCheckResponse> checkLogin(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        String token = extractTokenFromCookie(cookies);
+
+        LoginCheckResponse loginCheckResponse = authService.findUserNameByToken(new TokenDto(token));
+        return ResponseEntity.ok(loginCheckResponse);
+    }
+
+    private String extractTokenFromCookie(Cookie[] cookies) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                return cookie.getValue();
+            }
+        }
+
+        return "";
     }
 }
