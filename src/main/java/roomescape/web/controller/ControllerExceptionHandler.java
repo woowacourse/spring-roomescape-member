ae7458cd0a6eb8959a26d4e2e0f8a6f715beb8c4
eun-byeol <joeunbyeol98@gmail.com>
2024-05-08 20:47:44 +0900

    feat: DB 접근 에러 처리 로직 추가

--- src/main/java/roomescape/web/controller/ControllerExceptionHandler.java
+++ src/main/java/roomescape/web/controller/ControllerExceptionHandler.java
@@ -1,28 +1,29 @@
 package roomescape.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import roomescape.web.exception.BadRequestException;
-import roomescape.web.exception.NotFoundException;
+import roomescape.web.exception.CustomException;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<String> handleBadRequestException(final BadRequestException exception) {
-        return new ResponseEntity<>(exception.getMessage(), exception.getStatus());
-    }
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleNotFoundException(final NotFoundException exception) {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<String> handleCustomException(final CustomException exception) {
+        log.error("[Custom Exception]", exception);
         return new ResponseEntity<>(exception.getMessage(), exception.getStatus());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleMethodArgumentNotValidException(
             final MethodArgumentNotValidException exception) {
+        log.error("[Method Argument Not Valid Exception]", exception);
         String message = exception.getBindingResult()
                 .getAllErrors()
                 .get(0)
@@ -31,12 +32,20 @@ public class ControllerExceptionHandler {
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException() {
-        return new ResponseEntity<>("잘못된 파라미터 전달입니다.", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleIllegalArgumentException(final IllegalArgumentException exception) {
+        log.error("[Illegal Argument Exception]", exception);
+        return new ResponseEntity<>("잘못된 파라미터 요청입니다.", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<String> handleDataAccessException(final DataAccessException exception) {
+        log.error("[Data Access Exception]", exception);
+        return new ResponseEntity<>("잘못된 DB 접근입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleServerException() {
-        return new ResponseEntity<>("해결되지 않은 서버 에러입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<String> handleServerException(final Exception exception) {
+        log.error("[Exception]", exception);
+        return new ResponseEntity<>("서버 에러입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
