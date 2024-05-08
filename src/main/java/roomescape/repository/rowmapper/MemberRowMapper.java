b6a8288975a37a67d195102eff3007713316dc11
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-08 21:56:14 +0900

    feat: memberRowMapper 구현

---
+++ src/main/java/roomescape/repository/rowmapper/MemberRowMapper.java
@@ -0,0 +1,21 @@
+package roomescape.repository.rowmapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import roomescape.domain.member.Member;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class MemberRowMapper implements RowMapper<Member> {
+    @Override
+    public Member mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        return new Member(
+                resultSet.getLong("id"),
+                resultSet.getString("name"),
+                resultSet.getString("user_email"),
+                resultSet.getString("user_password")
+        );
+    }
+}
