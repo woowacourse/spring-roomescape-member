213b0a28ea835fd76023fb4aea6ae94c28254ee4
fram1998 <koust6u@pusan.ac.kr>
2024-05-08 15:22:10 +0900

    feat: 예외 핸들링 예외 정보 로깅 기능 구현

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -9,26 +9,35 @@ import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final String ERROR_PREFIX = "exception occur : {}";
+
     @ExceptionHandler(value = CustomException.class)
     public ResponseEntity<String> handleCustomException(CustomException exception) {
+        log.error(ERROR_PREFIX, exception.getMessage());
         return new ResponseEntity<>(exception.getMessage(), exception.getStatus());
     }
 
     @ExceptionHandler(value = BindException.class)
     public ResponseEntity<String> handleValidationException(BindException exception) {
+        log.error(ERROR_PREFIX, exception.getMessage());
         return new ResponseEntity<>(exception.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
                 BAD_REQUEST);
     }
 
     @ExceptionHandler(value = HttpMessageConversionException.class)
-    public ResponseEntity<String> handleJsonParsingException() {
+    public ResponseEntity<String> handleJsonParsingException(Exception exception) {
+        log.error(ERROR_PREFIX, exception.getMessage());
         return new ResponseEntity<>("유효하지 않은 필드가 존재합니다.", BAD_REQUEST);
     }
 
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<String> handleException() {
+    public ResponseEntity<String> handleException(Exception exception) {
+        log.error(ERROR_PREFIX, exception.getMessage());
         return new ResponseEntity<>("서버 에러입니다.", INTERNAL_SERVER_ERROR);
     }
 }
