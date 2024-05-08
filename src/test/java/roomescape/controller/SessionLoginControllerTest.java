067e6efa85c2e1c9012b4be8171b35efb7a91d33
reddevilmidzy <midzy0228@gmail.com>
2024-05-08 17:41:20 +0900

    feat: html에서 js 파일 로딩 변경

--- src/test/java/roomescape/controller/SessionLoginControllerTest.java
+++ src/test/java/roomescape/controller/SessionLoginControllerTest.java
@@ -26,13 +26,14 @@ class SessionLoginControllerTest {
     void login() {
         final Map<String, String> params = Map.of("password", "0000", "email", "redddy@gmail.com");
 
+        //TODO cookie 수정
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
                 .when().post("/login")
                 .then().log().all()
                 .statusCode(200)
-                .cookie("token", "ë \u0088ë\u0094\u0094")
+                .cookie("token", "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNzE1MTU2OTE1LCJleHAiOjE3MTUxNjA1MTV9._1KYAthr4G8r-joWTgdnQ6udk8xeZQ3uea5PI7vLiL4")
                 .header("Keep-Alive", "timeout=60");
     }
 }
