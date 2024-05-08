9a206f26c4520603b32d033a83ebb4caa77e7744
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-05-08 16:14:38 +0900

    feat(TokenProvider) : 토큰 생성,조회 기능 구현
    - 이에 따른 테스트 코드 추가

---
+++ src/main/java/roomescape/service/util/TokenProvider.java
@@ -0,0 +1,50 @@
+package roomescape.service.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import roomescape.domain.user.Member;
+
+import java.util.Date;
+
+@Component
+public class TokenProvider {
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+    @Value("${security.jwt.token.expire-length}")
+    private long validityInMilliseconds;
+
+    public static class TokenResponse {
+        private final long id;
+
+        private TokenResponse(final String value) {
+            this.id = Long.valueOf(value);
+        }
+
+        public long getId() {
+            return id;
+        }
+    }
+
+    public String generateToken(final Member member) {
+        final Date now = new Date();
+        final Date validity = new Date(now.getTime() + validityInMilliseconds);
+        return Jwts.builder()
+                .setSubject(member.getId()
+                        .toString())
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .claim("name", member.getName())
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public TokenResponse decodeToken(final String token) {
+        return new TokenResponse(Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject());
+    }
+}
