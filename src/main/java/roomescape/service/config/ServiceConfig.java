906660ab11fe224eb8876b6706252f499a274a1b
le2sky <leehaneul990623@gmail.com>
2024-05-08 17:40:06 +0900

    refactor: 불필요한 설정 제거

--- src/main/java/roomescape/service/config/ServiceConfig.java
+++ src/main/java/roomescape/service/config/ServiceConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import roomescape.domain.ReservationFactory;
 
-@Configuration(proxyBeanMethods = false)
+@Configuration
 class ServiceConfig {
 
     @Bean
