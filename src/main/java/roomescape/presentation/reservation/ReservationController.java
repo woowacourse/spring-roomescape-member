a2d6f064c4f0afdcaf870c11600a13e1a4b9850a
donghoony <aru0504@naver.com>
2024-05-08 21:42:21 +0900

    feat: 어드민을 통한 예약 생성

--- src/main/java/roomescape/presentation/reservation/ReservationController.java
+++ src/main/java/roomescape/presentation/reservation/ReservationController.java
@@ -9,15 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.application.member.LoginMemberId;
 import roomescape.application.reservation.ReservationService;
+import roomescape.application.reservation.dto.request.AdminReservationRequest;
 import roomescape.application.reservation.dto.request.ReservationRequest;
 import roomescape.application.reservation.dto.response.ReservationResponse;
 
 @RestController
-@RequestMapping("/reservations")
 public class ReservationController {
     private final ReservationService service;
 
@@ -25,13 +24,13 @@ public class ReservationController {
         this.service = service;
     }
 
-    @GetMapping
+    @GetMapping("/reservations")
     public ResponseEntity<List<ReservationResponse>> findAll() {
         List<ReservationResponse> responses = service.findAll();
         return ResponseEntity.ok(responses);
     }
 
-    @PostMapping
+    @PostMapping("/reservations")
     public ResponseEntity<ReservationResponse> create(@LoginMemberId long memberId,
                                                       @RequestBody @Valid ReservationRequest request) {
         ReservationResponse response = service.create(memberId, request);
@@ -39,7 +38,19 @@ public class ReservationController {
         return ResponseEntity.created(location).body(response);
     }
 
-    @DeleteMapping("/{id}")
+    @PostMapping("/admin/reservations")
+    public ResponseEntity<ReservationResponse> createByAdmin(@RequestBody @Valid AdminReservationRequest request) {
+        ReservationRequest reservationRequest = new ReservationRequest(
+                request.date(),
+                request.timeId(),
+                request.themeId()
+        );
+        ReservationResponse response = service.create(request.memberId(), reservationRequest);
+        URI location = URI.create("/reservations/" + response.id());
+        return ResponseEntity.created(location).body(response);
+    }
+
+    @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> delete(@PathVariable long id) {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
