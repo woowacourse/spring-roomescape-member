9bb7a6c9d1bd15f36a86d1d8e138f4a64c45b23b
yoonjuho <dbswn990@naver.com>
2024-05-08 23:01:37 +0900

    feat: 사용자 도메인 생성

---
+++ src/main/java/roomescape/auth/domain/Users.java
@@ -0,0 +1,65 @@
+package roomescape.auth.domain;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+import roomescape.exception.RoomEscapeException;
+import roomescape.exception.message.ExceptionMessage;
+
+public class Users {
+    private static final Pattern EMAIL_FORMAT = Pattern.compile("^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$");
+    private final Long id;
+    private final String name;
+    private final String email;
+    private final String password;
+
+    public Users(final Long id, final String name, final String email, final String password) {
+        validateInvalidName(name);
+        validateInvalidEmail(email);
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    private void validateInvalidName(final String name) {
+        if (Objects.isNull(name) || name.isEmpty()) {
+            throw new RoomEscapeException(ExceptionMessage.INVALID_USER_NAME);
+        }
+    }
+
+    private void validateInvalidEmail(final String email) {
+        if (!EMAIL_FORMAT.matcher(email).matches()) {
+            throw new RoomEscapeException(ExceptionMessage.INVALID_EMAIL);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return Objects.equals(id, users.id) && Objects.equals(name, users.name) && Objects.equals(email, users.email) && Objects.equals(password, users.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password);
+    }
+}
---
+++ src/test/java/roomescape/auth/domain/UsersTest.java
@@ -0,0 +1,32 @@
+package roomescape.auth.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import roomescape.exception.RoomEscapeException;
+import roomescape.exception.message.ExceptionMessage;
+
+class UsersTest {
+
+    @DisplayName("이름이 null이거나 공백인 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void validateInvalidName(String name) {
+        assertThatThrownBy(() -> new Users(1L, name, "email@email.com", "1234"))
+                .isInstanceOf(RoomEscapeException.class)
+                .hasMessage(ExceptionMessage.INVALID_USER_NAME.getMessage());
+    }
+
+    @DisplayName("이메일 형식에 맞지 않는 경우 예외가 발생한다.")
+    @Test
+    void validateInvalidEmail() {
+        assertThatThrownBy(() -> new Users(1L, "hotea", "email.com", "1234"))
+                .isInstanceOf(RoomEscapeException.class)
+                .hasMessage(ExceptionMessage.INVALID_EMAIL.getMessage());
+    }
+
+}
