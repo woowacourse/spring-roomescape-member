5c4a49df92466afb8d7129f0dedeb98d7b52c7fd
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-08 22:38:51 +0900

    refactor(schema.sql): reservation 테이블 member_id 외래키 설정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -25,10 +25,11 @@ CREATE TABLE reservation_time
 CREATE TABLE reservation
 (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
     date DATE NOT NULL,
     time_id BIGINT,
     theme_id BIGINT,
+    member_id BIGINT,
     FOREIGN KEY (time_id) REFERENCES reservation_time(id),
-    FOREIGN KEY (theme_id) REFERENCES theme(id)
+    FOREIGN KEY (theme_id) REFERENCES theme(id),
+    FOREIGN KEY (member_id) REFERENCES member(id)
 );
