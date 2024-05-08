ada2b58f1267fde89ca0fcbfe069ced0c0b1e30e
Zeus6768 <akaps6768@gmail.com>
2024-05-08 17:48:48 +0900

    refactor: ViewController 클래스 삭제 및 ViewController 설정 추가

---
+++ src/main/java/roomescape/config/WebConfiguration.java
@@ -0,0 +1,20 @@
+package roomescape.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/reservation").setViewName("reservation");
+        registry.addViewController("/admin").setViewName("admin/index");
+        registry.addViewController("/admin/reservation").setViewName("admin/reservation-new");
+        registry.addViewController("/admin/time").setViewName("admin/time");
+        registry.addViewController("/admin/theme").setViewName("admin/theme");
+    }
+}
