0ecf5f52e3b31a38ee7bf19b83f1e3e43d154dc8
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-08 21:53:49 +0900

    feat: CookieGenerator 구현

--- src/main/java/roomescape/auth/CookieGenerator.java
+++ src/main/java/roomescape/auth/CookieGenerator.java
@@ -2,23 +2,30 @@ package roomescape.auth;
 
 import jakarta.servlet.http.Cookie;
 import org.springframework.stereotype.Component;
-import roomescape.domain.member.Member;
+
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
 
 @Component
 public class CookieGenerator {
     private static final boolean HTTP_ONLY_OPTION = true;
     private static final String ALLOWED_PATH = "/";
+    private static final String TOKEN_NAME = "token";
 
-    private final JwtTokenProvider tokenProvider;
-
-    public CookieGenerator(JwtTokenProvider tokenProvider) {
-        this.tokenProvider = tokenProvider;
-    }
-
-    public Cookie generate(Member member) {
-        Cookie cookie = new Cookie("token", tokenProvider.generateToken(member));
+    public Cookie generate(Token token) {
+        Cookie cookie = new Cookie(TOKEN_NAME, token.getToken());
         cookie.setHttpOnly(HTTP_ONLY_OPTION);
         cookie.setPath(ALLOWED_PATH);
         return cookie;
+    }
+
+    public Token getToken(Cookie[] cookies) {
+        return new Token(
+                Stream.of(cookies)
+                        .filter(cookie -> cookie.getName().equals(TOKEN_NAME))
+                        .findAny()
+                        .orElseThrow(() -> new NoSuchElementException("[ERROR] 토큰을 찾을 수 없습니다."))
+                        .getValue()
+        );
     }
 }
