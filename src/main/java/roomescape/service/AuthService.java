dc74faa4fdc73e3072bac20d05cb08802dc07b9a
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-08 22:49:48 +0900

    feat(AuthService): 토큰 생성

---
+++ src/main/java/roomescape/service/AuthService.java
@@ -0,0 +1,24 @@
+package roomescape.service;
+
+import org.springframework.stereotype.Service;
+import roomescape.infrastructure.JwtTokenProvider;
+import roomescape.service.dto.TokenRequest;
+import roomescape.service.dto.TokenResponse;
+
+@Service
+public class AuthService {
+    private static final String EMAIL = "admin@email.com";
+    private static final String PASSWORD = "password";
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthService(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    public TokenResponse createToken(TokenRequest tokenRequest) {
+        String accessToken = jwtTokenProvider.createToken(tokenRequest.email());
+        return new TokenResponse(accessToken);
+    }
+
+}
