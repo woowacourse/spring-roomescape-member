2f9994e947c7c63e6cca9119e98425055f79b58e
yoonjuho <dbswn990@naver.com>
2024-05-08 16:53:05 +0900

    refactor: 예외 처리 응답 형식 변경

--- src/main/java/roomescape/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/handler/GlobalExceptionHandler.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 import jakarta.validation.ConstraintViolationException;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,25 +16,26 @@ import roomescape.exception.RoomEscapeException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = RoomEscapeException.class)
-    public ProblemDetail handleRoomEscapeException(final RoomEscapeException ex) {
-        return ProblemDetail.forStatusAndDetail(ex.getStatus(), ex.getMessage());
+    public ErrorResponse handleRoomEscapeException(final RoomEscapeException ex) {
+        return ErrorResponse.builder(ex, ex.getStatus(), ex.getMessage()).build();
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, Objects.requireNonNull(ex.getBindingResult()
-                                                                                                 .getFieldErrors()
-                                                                                                 .get(0)
-                                                                                                 .getDefaultMessage()));
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ErrorResponse.builder(ex, HttpStatus.BAD_REQUEST, Objects.requireNonNull(ex.getBindingResult()
+                                                                                          .getFieldErrors()
+                                                                                          .get(0)
+                                                                                          .getDefaultMessage()))
+                            .build();
     }
 
     @ExceptionHandler(value = ConstraintViolationException.class)
-    public ProblemDetail handleConstraintViolationException(ConstraintViolationException ex) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    public ErrorResponse handleConstraintViolationException(ConstraintViolationException ex) {
+        return ErrorResponse.builder(ex, HttpStatus.BAD_REQUEST, ex.getMessage()).build();
     }
 
     @ExceptionHandler(value = RuntimeException.class)
-    public ProblemDetail handleRuntimeException(RuntimeException ex) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    public ErrorResponse handleRuntimeException(RuntimeException ex) {
+        return ErrorResponse.builder(ex, HttpStatus.BAD_REQUEST, ex.getMessage()).build();
     }
 }
