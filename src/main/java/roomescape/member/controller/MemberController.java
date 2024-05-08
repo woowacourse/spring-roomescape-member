f05cc4a1e7b6791389bc278f5f6c6959423bb0cc
BurningFalls <sherlocky31415@gmail.com>
2024-05-08 19:56:31 +0900

    refactor: user -> member로 변경

--- src/main/java/roomescape/member/controller/MemberController.java
+++ src/main/java/roomescape/member/controller/MemberController.java
@@ -1,11 +1,11 @@
-package roomescape.user.controller;
+package roomescape.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UserController {
-    
+public class MemberController {
+
     @GetMapping("/reservation")
     public String reservation() {
         return "/reservation";
