334f0fc92e40cc45db75a1acb2403f3fe920b963
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-08 22:21:01 +0900

    feat: LoginMemberArgumentResolver 구현 및 사용을 위한 설정

---
+++ src/main/java/roomescape/controller/argumentresolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,46 @@
+package roomescape.controller.argumentresolver;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import roomescape.domain.member.Member;
+import roomescape.dto.TokenDto;
+import roomescape.service.AuthService;
+
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final AuthService authService;
+
+    public LoginMemberArgumentResolver(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(Member.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        Cookie[] cookies = request.getCookies();
+        String token = extractTokenFromCookie(cookies);
+        return authService.findUserByToken(new TokenDto(token));
+    }
+
+    private String extractTokenFromCookie(Cookie[] cookies) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                return cookie.getValue();
+            }
+        }
+
+        return "";
+    }
+}
