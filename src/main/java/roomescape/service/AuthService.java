3866734601130a46073de8323e3e39dc4099da53
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-08 15:39:44 +0900

    feat(AuthService): 로그인 사용자 검증 기능 추가 및 기존 기능 리팩터링

--- src/main/java/roomescape/service/AuthService.java
+++ src/main/java/roomescape/service/AuthService.java
@@ -1,26 +1,59 @@
 package roomescape.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import roomescape.domain.user.User;
+import roomescape.domain.member.Member;
+import roomescape.dto.LoginCheckResponse;
 import roomescape.dto.LoginRequest;
-import roomescape.dto.TokenResponse;
-import roomescape.dto.UserPayload;
+import roomescape.dto.MemberPayload;
+import roomescape.dto.TokenDto;
+import roomescape.exception.ClientErrorExceptionWithData;
 import roomescape.infrastructure.JwtProvider;
 
 @Service
 public class AuthService {
 
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
 
-    public AuthService(JwtProvider jwtProvider) {
+    public AuthService(MemberService memberService, PasswordEncoder passwordEncoder, JwtProvider jwtProvider) {
+        this.memberService = memberService;
+        this.passwordEncoder = passwordEncoder;
         this.jwtProvider = jwtProvider;
     }
 
-    public TokenResponse login(LoginRequest request) {
-//        User loginUser = authenticateUser(request);
-        User loginUser = new User(1L, "kim", "eee", "21313");
-        UserPayload loginUserPayload = UserPayload.from(loginUser);
-        String token = jwtProvider.createToken(loginUserPayload);
-        return new TokenResponse(token);
+    public TokenDto login(LoginRequest request) {
+        Member loginMember = authenticateUser(request);
+        MemberPayload loginMemberPayload = MemberPayload.from(loginMember);
+        String token = jwtProvider.createToken(loginMemberPayload);
+        return new TokenDto(token);
+    }
+
+    public LoginCheckResponse findUserNameByToken(TokenDto tokenDto) {
+        Long userId = extractUserIdByToken(tokenDto.accessToken());
+        Member member = memberService.getMemberById(userId);
+        return new LoginCheckResponse(member.getName());
+    }
+
+    private Member authenticateUser(LoginRequest request) {
+        Member member = memberService.getMemberByEmail(request.email());
+        validatePassword(request, member);
+        return member;
+    }
+
+    private void validatePassword(LoginRequest request, Member memberToLogin) {
+        if (!passwordEncoder.matches(request.password(), memberToLogin.getEncodedPassword())) {
+            throw new ClientErrorExceptionWithData("[ERROR] 잘못된 비밀번호 입니다.");
+        }
+    }
+
+    private Long extractUserIdByToken(String accessToken) {
+        String userId = jwtProvider.getSubject(accessToken);
+        try {
+            return Long.valueOf(userId);
+        } catch (NumberFormatException e) {
+            throw new ClientErrorExceptionWithData("[ERROR] 유효한 토큰이 아닙니다.", userId);
+        }
     }
 }
