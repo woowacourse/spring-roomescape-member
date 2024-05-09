9596b476c34ee52807a471d1eaa2c91de487a4fa
kelly6bf <kelly6bf@gmail.com>
2024-05-09 21:08:17 +0900

    chore: 패키지 이동

--- src/main/java/roomescape/config/JwtConfig.java
+++ src/main/java/roomescape/config/JwtConfig.java
@@ -1,9 +1,10 @@
-package roomescape.auth.token;
+package roomescape.config;
 
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import roomescape.auth.token.TokenProvider;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
--- src/main/java/roomescape/util/CookieUtil.java
+++ src/main/java/roomescape/util/CookieUtil.java
@@ -1,4 +1,4 @@
-package roomescape.auth.util;
+package roomescape.util;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
