1e22bafa62a38f4f9da5441826345271d2d47e2e
zangsu <zangsu_@naver.com>
2024-05-09 10:58:25 +0900

    refactor: RequestMapping  대신 URL을 모두 명시하도록 변경
     - API 검색에 용이
     - 한 Controller 안의 모든 URL 이 동일하게 시작하지 않을 수 있음
       - 오히려 동일하게 시작하도록 제약을 걸어두는 것이 자연스럽지 않다고 생각

--- src/main/java/roomescape/controller/LoginController.java
+++ src/main/java/roomescape/controller/LoginController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import roomescape.domain.LoginUser;
 import roomescape.dto.LoginRequest;
 import roomescape.dto.LoginResponse;
