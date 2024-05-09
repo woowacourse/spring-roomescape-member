614224d260ef9fafffa351393a95a68ffe5af274
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-09 22:38:13 +0900

    feat(WebMvcConfig): resolver 등록

---
+++ src/main/java/roomescape/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package roomescape.config;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.resolver.LoginMemberArgumentResolver;
+import roomescape.service.AuthService;
+import roomescape.service.LoginMemberService;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final AuthService authService;
+    private final LoginMemberService loginMemberService;
+
+    @Autowired
+    public WebMvcConfig(AuthService authService, LoginMemberService loginMemberService) {
+        this.authService = authService;
+        this.loginMemberService = loginMemberService;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver(authService, loginMemberService));
+    }
+}
