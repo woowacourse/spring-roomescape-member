c78057e097de92c473c6be1788b6e9fde17483d3
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-09 19:43:27 +0900

    feat: webconfig 파일 구현

---
+++ src/main/java/roomescape/config/WebMvcConfiguration.java
@@ -0,0 +1,24 @@
+package roomescape.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.infrastructure.MemberArgumentResolver;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+
+    private final MemberArgumentResolver memberArgumentResolver;
+
+    public WebMvcConfiguration(MemberArgumentResolver memberArgumentResolver) {
+        this.memberArgumentResolver = memberArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberArgumentResolver);
+    }
+}
