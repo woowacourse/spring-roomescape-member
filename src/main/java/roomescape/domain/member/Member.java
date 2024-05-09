5a0df5eac18af44ee8e420d26a209e6fb6e6a0b2
3juhwan <13selfesteem91@naver.com>
2024-05-09 17:34:33 +0900

    feat: 멤버 도메인 추가

---
+++ src/main/java/roomescape/domain/member/Member.java
@@ -0,0 +1,31 @@
+package roomescape.domain.member;
+
+public class Member {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public Member(final Long id, final String name, final String email, final String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
