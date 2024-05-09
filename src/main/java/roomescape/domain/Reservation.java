ab3d0e076894be7590cd67ad5c608d905e3395f5
Zeus6768 <akaps6768@gmail.com>
2024-05-09 15:45:02 +0900

    style: 스타일 일치

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -7,7 +7,7 @@ public record Reservation(long id, String name, LocalDate date, ReservationTime 
     public Reservation(long id, String name, LocalDate date, ReservationTime time, Theme theme) {
         this.id = id;
         this.name = validateName(name);
-        this.date=date;
+        this.date = date;
         this.time = time;
         this.theme = theme;
     }
