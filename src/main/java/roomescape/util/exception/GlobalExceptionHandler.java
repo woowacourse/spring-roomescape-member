dd2ca04324eb7c1e88777a9ac6825b47448a2eb5
MingyeomKim <mingyum119@naver.com>
2024-05-09 21:07:32 +0900

    feat: 에러 로그 확인을 위한 로깅 기능 추가

--- src/main/java/roomescape/util/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/util/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 import java.util.NoSuchElementException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,20 +14,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(value = HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handle(final HttpMessageNotReadableException exception) {
+        logger.error(exception.getMessage(), exception);
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(BAD_REQUEST, "[ERROR] 올바른 형식의 필드를 입력해주세요."));
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handle(final MethodArgumentNotValidException exception) {
+        logger.error(exception.getMessage(), exception);
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(BAD_REQUEST, "[ERROR] 올바른 형식의 필드를 입력해주세요."));
     }
 
     @ExceptionHandler(value = NoSuchElementException.class)
     public ResponseEntity<ErrorResponse> handle(final NoSuchElementException exception) {
+        logger.error(exception.getMessage(), exception);
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(BAD_REQUEST, "[ERROR] 요청된 자원이 존재하지 않습니다."));
     }
@@ -38,6 +45,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> handle(final Exception exception) {
+        logger.error(exception.getMessage(), exception);
         return ResponseEntity.internalServerError()
                 .body(new ErrorResponse(INTERNAL_SERVER_ERROR, "[ERROR] 서버 내부 오류입니다."));
     }
