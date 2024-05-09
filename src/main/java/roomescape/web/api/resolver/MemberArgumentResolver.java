0ece32dfbefd44001273c0c71a53293a37783a23
3juhwan <13selfesteem91@naver.com>
2024-05-10 00:36:15 +0900

    feat: 토큰을 핸들러 파라미터를 변환하는 기능 추가

---
+++ src/main/java/roomescape/web/api/resolver/MemberArgumentResolver.java
@@ -0,0 +1,42 @@
+package roomescape.web.api.resolver;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import roomescape.domain.auth.TokenProvider;
+import roomescape.service.MemberService;
+
+@Component
+public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private final MemberService memberService;
+    private final TokenProvider tokenProvider;
+
+    public MemberArgumentResolver(MemberService memberService, TokenProvider tokenProvider) {
+        this.memberService = memberService;
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Auth.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        Cookie[] cookies = request.getCookies();
+        String token = tokenProvider.extractToken(cookies);
+        String email = tokenProvider.getPayload(token);
+        try {
+            return memberService.findByEmail(email);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("인증 정보가 유효하지 않습니다.");
+        }
+    }
+}
