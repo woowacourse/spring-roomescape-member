b877a3887600032b54b5e8dbd2f65273132328ef
leetaehoon <xogns1514@gmail.com>
2024-05-09 12:53:51 +0900

    refactor(TokenProvider): TokenProvider Component로 변경

--- src/main/java/roomescape/infrastructure/TokenProvider.java
+++ src/main/java/roomescape/infrastructure/TokenProvider.java
@@ -1,44 +1,43 @@
-package roomescape.service;
+package roomescape.infrastructure;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import roomescape.domain.User;
 import roomescape.handler.exception.CustomException;
 import roomescape.handler.exception.ExceptionCode;
 
-@Service
-public class TokenService {
+@Component
+public class TokenProvider {
 
     private static final String RANDOM_VALUE = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
-    public static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(RANDOM_VALUE.getBytes());
+    private static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(RANDOM_VALUE.getBytes());
+    private static final String AUTHENTICATION_PAYLOAD = "name";
 
     public String generateTokenOf(User user) {
         return Jwts.builder()
                 .subject(user.getId().toString())
-                .claim("name", user.getName())
+                .claim(AUTHENTICATION_PAYLOAD, user.getName())
                 .claim("role", user.getRole())
                 .signWith(SECRET_KEY)
                 .compact();
     }
 
-    public String parseName(String authenticationInfo) {
-        String name = parsePayload(authenticationInfo).get("name", String.class);
-
-        if (name == null) {
+    public String parseAuthenticationInfo(String token) {
+        String authenticationInfo = parsePayload(token).get(AUTHENTICATION_PAYLOAD, String.class);
+        if (authenticationInfo == null) {
             throw new CustomException(ExceptionCode.NO_AUTHENTICATION_INFO);
         }
-
-        return name;
+        return authenticationInfo;
     }
 
-    private Claims parsePayload(String authenticationInfo) {
+    private Claims parsePayload(String token) {
         return Jwts.parser()
                 .verifyWith(SECRET_KEY)
                 .build()
-                .parseSignedClaims(authenticationInfo)
+                .parseSignedClaims(token)
                 .getPayload();
     }
 }
