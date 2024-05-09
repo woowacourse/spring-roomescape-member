d083cdae35c3a0d6219c9e1ff911e778ff8ebf4a
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-09 14:19:28 +0900

    feat(TokenLoginController): /check 응답 페이지 구현

--- src/main/java/roomescape/controller/TokenLoginController.java
+++ src/main/java/roomescape/controller/TokenLoginController.java
@@ -1,12 +1,17 @@
 package roomescape.controller;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.service.AuthService;
+import roomescape.service.LoginMemberService;
+import roomescape.service.dto.LoginMemberNameResponse;
 import roomescape.service.dto.TokenRequest;
 import roomescape.service.dto.TokenResponse;
 
@@ -14,15 +19,26 @@ import roomescape.service.dto.TokenResponse;
 @RequestMapping("/login")
 public class TokenLoginController {
     private final AuthService authService;
+    private final LoginMemberService loginMemberService;
 
-    public TokenLoginController(AuthService authService) {
+    @Autowired
+    public TokenLoginController(AuthService authService, LoginMemberService loginMemberService) {
         this.authService = authService;
+        this.loginMemberService = loginMemberService;
     }
 
     @PostMapping
     public void tokenLogin(@RequestBody TokenRequest tokenRequest, HttpServletResponse response) {
+        loginMemberService.validateLogin(tokenRequest);
         TokenResponse tokenResponse = authService.createToken(tokenRequest);
         Cookie cookie = authService.createCookieByToken(tokenResponse.token());
         response.addCookie(cookie);
+    }
+
+    @GetMapping("/check")
+    public LoginMemberNameResponse findUser(HttpServletRequest request) {
+        TokenResponse tokenResponse = authService.extractTokenByCookies(request);
+        String email = authService.extractEmailByToken(tokenResponse);
+        return loginMemberService.findNameByEmail(email);
     }
 }
