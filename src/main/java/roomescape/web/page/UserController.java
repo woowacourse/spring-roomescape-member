2f43d8bca19b31b7d73c5b118b5e0df8a05e9f4c
3juhwan <13selfesteem91@naver.com>
2024-05-09 17:10:37 +0900

    feat: 사용자 페이지 추가

--- src/main/java/roomescape/web/page/UserController.java
+++ src/main/java/roomescape/web/page/UserController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
     @GetMapping("/reservation")
     public String reservation() {
         return "reservation";
