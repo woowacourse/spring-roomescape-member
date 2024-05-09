c1a7fdec7b1d0ce22e707698f6c80dc09ac4603a
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-05-09 16:45:49 +0900

    fix: 이름 안보이는 문제 해결

--- src/main/java/roomescape/auth/domain/AuthInfo.java
+++ src/main/java/roomescape/auth/domain/AuthInfo.java
@@ -5,21 +5,27 @@ import roomescape.member.domain.Role;
 
 public class AuthInfo {
     private final Long id;
+    private final String name;
     private final String email;
     private final Role role;
 
-    public AuthInfo(long id, String email, Role role) {
+    public AuthInfo(Long id, String name, String email, Role role) {
         this.id = id;
+        this.name = name;
         this.email = email;
         this.role = role;
     }
 
     public static AuthInfo of(Member member) {
-        return new AuthInfo(member.getId(), member.getEmail(), member.getRole());
+        return new AuthInfo(member.getId(), member.getName(), member.getEmail(), member.getRole());
     }
 
     public Long getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getEmail() {
