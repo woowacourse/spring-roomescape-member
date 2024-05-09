dec52d2a1e579ed6f121f72b9b3b9b6cf5876150
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-05-09 13:36:32 +0900

    feat : 회원 인증 RestController 구현

---
+++ src/main/java/roomescape/controller/MemberAuthController.java
@@ -0,0 +1,71 @@
+package roomescape.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Optional;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.controller.request.MemberSignUpRequest;
+import roomescape.controller.request.TokenWebRequest;
+import roomescape.controller.response.MemberWebResponse;
+import roomescape.service.MemberAuthService;
+import roomescape.service.request.MemberAppRequest;
+import roomescape.service.request.TokenAppRequest;
+import roomescape.service.response.MemberAppResponse;
+
+@RestController
+public class MemberAuthController {
+
+    private final MemberAuthService memberAuthService;
+
+    public MemberAuthController(MemberAuthService memberAuthService) {
+        this.memberAuthService = memberAuthService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@Valid @RequestBody TokenWebRequest request,
+                                      HttpServletResponse response) {
+        String token = memberAuthService.createToken(new TokenAppRequest(request.email(), request.password()));
+        Cookie cookie = new Cookie("token", token);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/login/check")
+    public ResponseEntity<MemberWebResponse> findMember(HttpServletRequest request) {
+        if (request.getCookies() == null) {
+            throw new IllegalArgumentException("쿠키가 없습니다. 다시 로그인 해주세요.");
+        }
+        String token = extractTokenFromCookie(request.getCookies());
+        MemberAppResponse appResponse = memberAuthService.findMemberByToken(token);
+        MemberWebResponse response = new MemberWebResponse(appResponse.name());
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping("/members")
+    public ResponseEntity<MemberWebResponse> save(@Valid @RequestBody MemberSignUpRequest request) {
+        MemberAppResponse appResponse = memberAuthService.signUp(
+            new MemberAppRequest(request.name(), request.email(), request.password()));
+
+        MemberWebResponse response = new MemberWebResponse(appResponse.name());
+        return ResponseEntity.created(URI.create("/member" + appResponse.id())).body(response);
+    }
+
+    private String extractTokenFromCookie(Cookie[] cookies) {
+        Optional<String> token = Arrays.stream(cookies)
+            .filter(cookie -> cookie.getName().equals("token"))
+            .map(Cookie::getValue)
+            .findFirst();
+
+        return token.orElseThrow(() -> new IllegalArgumentException("토큰이 존재하지 않습니다."));
+    }
+}
