a6b6fe63f2e5f812dfef62e3fca9877202497923
PgmJun <chltmdwns96@gmail.com>
2024-05-09 21:44:58 +0900

    test: AuthService 테스트 작성

---
+++ src/test/java/roomescape/auth/service/AuthServiceTest.java
@@ -0,0 +1,67 @@
+package roomescape.auth.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import roomescape.auth.dto.LoginRequest;
+import roomescape.auth.dto.LoginResponse;
+import roomescape.global.auth.jwt.JwtHandler;
+import roomescape.global.exception.model.NotFoundException;
+import roomescape.member.dao.MemberDao;
+import roomescape.member.domain.Member;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@JdbcTest
+@Import({AuthService.class, MemberDao.class, JwtHandler.class})
+class AuthServiceTest {
+
+    @Autowired
+    private AuthService authService;
+    @Autowired
+    private MemberDao memberDao;
+    @Autowired
+    private JwtHandler jwtHandler;
+
+    @Test
+    @DisplayName("존재하는 회원의 email, password로 로그인하면 memberId, accessToken을 Response 한다.")
+    void loginSuccess() {
+        // given
+        Member member = memberDao.insert(new Member("이름", "test@test.com", "12341234"));
+
+        // when
+        LoginResponse response = authService.login(new LoginRequest(member.getEmail(), member.getPassword()));
+
+        // then
+        assertAll(
+                () -> Assertions.assertThat(response.memberId()).isEqualTo(member.getId()),
+                () -> Assertions.assertThat(response.accessToken()).isNotNull()
+        );
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 회원 email 또는 password로 로그인하면 예외를 발생한다.")
+    void loginFailByNotExistMemberInfo() {
+        // given
+        String notExistEmail = "invalid@test.com";
+        String notExistPassword = "invalid1234";
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> authService.login(new LoginRequest(notExistEmail, notExistPassword)))
+                .isInstanceOf(NotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 회원의 memberId로 로그인 여부를 체크하면 예외를 발생한다.")
+    void checkLoginFailByNotExistMemberInfo() {
+        // given
+        Long notExistMemberId = 1L;
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> authService.checkLogin(notExistMemberId))
+                .isInstanceOf(NotFoundException.class);
+    }
+}
