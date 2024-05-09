6f9cb4a80e8ddd950a1cf35f22be8e7fcd8aa580
unifolio0 <boho82ha@gmail.com>
2024-05-09 16:23:49 +0900

    test: admin/search 테스트 작성

--- src/test/java/roomescape/acceptance/ReservationAcceptanceTest.java
+++ src/test/java/roomescape/acceptance/ReservationAcceptanceTest.java
@@ -1,8 +1,12 @@
 package roomescape.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
@@ -33,6 +37,21 @@ class ReservationAcceptanceTest extends BasicAcceptanceTest {
                 dynamicTest("관리자 페이지에서 예약을 추가한다", () -> ReservationCRD.postAdminReservation(adminToken, tomorrow.toString(), 2L, 2L, 2L, 201)),
                 dynamicTest("관리자 페이지에서 예약을 추가한다", () -> ReservationCRD.postAdminReservation(adminToken, tomorrow.toString(), 3L, 3L, 3L, 201)),
                 dynamicTest("모든 예약을 조회한다 (총 3개)", () -> ReservationCRD.getReservations(200, 3))
+        );
+    }
+
+    @TestFactory
+    @DisplayName("관리자 페이지에서 조건에 맞는 예약을 검색한다")
+    Stream<DynamicTest> adminSearchReservation() {
+        LocalDate today = LocalDate.now();
+        LocalDate yesterday = today.minusDays(1);
+        LocalDate tomorrow = today.plusDays(1);
+
+        return Stream.of(
+                dynamicTest("관리자 페이지에서 예약을 추가한다", () -> ReservationCRD.postAdminReservation(adminToken, tomorrow.toString(), 1L, 1L, 1L, 201)),
+                dynamicTest("관리자 페이지에서 예약을 추가한다", () -> ReservationCRD.postAdminReservation(adminToken, tomorrow.toString(), 1L, 2L, 1L, 201)),
+                dynamicTest("관리자 페이지에서 예약을 추가한다", () -> ReservationCRD.postAdminReservation(adminToken, tomorrow.toString(), 3L, 3L, 3L, 201)),
+                dynamicTest("날짜는 어제부터 내일까지, member_id는 1, theme_id는 1인 예약을 검색한다 (총 2개)", () -> adminSearch(adminToken, 1L, 1L, yesterday.toString(), tomorrow.toString(), 200, 2))
         );
     }
 
@@ -87,5 +106,18 @@ class ReservationAcceptanceTest extends BasicAcceptanceTest {
                 dynamicTest("예약을 삭제한다", () -> ReservationCRD.deleteReservation(reservationId.longValue(), 204)),
                 dynamicTest("모든 예약을 조회한다 (총 1개)", () -> ReservationCRD.getReservations(200, 1))
         );
+    }
+
+    private void adminSearch(String token, Long themeId, Long memberId, String dateFrom, String dateTo, int expectedHttpCode, int expectedReservationResponsesSize) {
+        Response response = RestAssured.given().log().all()
+                .cookies("token", token)
+                .when().get("/admin/search?themeId=" + themeId + "&memberId=" + memberId + "&dateFrom=" + dateFrom + "&dateTo=" + dateTo)
+                .then().log().all()
+                .statusCode(expectedHttpCode)
+                .extract().response();
+
+        List<?> reservationResponses = response.as(List.class);
+
+        assertThat(reservationResponses).hasSize(expectedReservationResponsesSize);
     }
 }
