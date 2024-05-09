4b4be4233ac510576e1bfc699128aabe0b19ab88
seunghye218 <seunghye@student.42seoul.kr>
2024-05-09 17:22:36 +0900

    feat: 사용자 도메인 추가

---
+++ src/main/java/roomescape/member/model/Member.java
@@ -0,0 +1,51 @@
+package roomescape.member.model;
+
+import java.util.Objects;
+
+public class Member {
+
+    private final Long id;
+    private final String name;
+    private final String email;
+    private final String password;
+
+    public Member(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
