8d972f0f4dd523ccfced56fcf71a83b1d5c31d4e
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-09 12:48:32 +0900

    feat: 멤버를 찾는 로직 추가

--- src/main/java/roomescape/service/serviceimpl/MemberService.java
+++ src/main/java/roomescape/service/serviceimpl/MemberService.java
@@ -1,6 +1,8 @@
 package roomescape.service.serviceimpl;
 
 import org.springframework.stereotype.Service;
+import roomescape.domain.member.Member;
+import roomescape.dto.response.MemberResponse;
 import roomescape.repository.MemberDao;
 
 @Service
@@ -9,5 +11,17 @@ public class MemberService {
 
     public MemberService(MemberDao memberDao) {
         this.memberDao = memberDao;
+    }
+
+    public MemberResponse findMemberById(Long id) {
+        return new MemberResponse(findMember(id));
+    }
+
+    private Member findMember(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("[ERROR] 멤버 id는 null일 수 없습니다.");
+        }
+        return memberDao.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 멤버를 찾을 수 없습니다."));
     }
 }
