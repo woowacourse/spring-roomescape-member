c9e943528128b9ccdd4d9ff6da382135ec2260ae
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-09 14:14:23 +0900

    feat(JwtTokenExtractor): extractEmailByToken 구현

--- src/main/java/roomescape/infrastructure/JwtTokenExtractor.java
+++ src/main/java/roomescape/infrastructure/JwtTokenExtractor.java
@@ -1,10 +1,17 @@
 package roomescape.infrastructure;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.Cookie;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("classpath:application-secret.properties")
 public class JwtTokenExtractor {
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey; // 여기 위치 괜찮은지? provider 에도 있음
 
     public String extractByCookies(Cookie[] cookies) {
         for (Cookie cookie : cookies) {
@@ -15,4 +22,11 @@ public class JwtTokenExtractor {
         return ""; //토큰이 없으면 빈 문자열을 리턴해도 괜찮은가?
     }
 
+    public String extractEmailByToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
 }
