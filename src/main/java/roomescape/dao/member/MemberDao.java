953bda92442b2ffb9ea49a4a8816cd2b10bde68b
MingyeomKim <mingyum119@naver.com>
2024-05-09 11:05:32 +0900

    fix: 테이블 명칭 변경에 따른 SQL 문 수정

--- src/main/java/roomescape/dao/member/MemberDao.java
+++ src/main/java/roomescape/dao/member/MemberDao.java
@@ -19,7 +19,7 @@ public class MemberDao {
     }
 
     public Optional<Member> findByEmail(final String email) {
-        String sql = "SELECT * FROM user WHERE email = ?";
+        String sql = "SELECT * FROM member WHERE email = ?";
         try {
             return Optional.of(jdbcTemplate.queryForObject(sql, userRowMapper, email));
         } catch (final EmptyResultDataAccessException exception) {
@@ -29,7 +29,7 @@ public class MemberDao {
 
     // TODO: 여기서 USER 를 매개변수로 바로 반환하는 게 맞나? SimpleJdbcInsert 사용하면 좋을 듯
     public Member save(final Member member) {
-        String sql = "INSERT INTO user VALUES (?, ?, ?)";
+        String sql = "INSERT INTO member VALUES (?, ?, ?)";
         jdbcTemplate.update(sql, member.getNameValue(), member.getEmail(), member.getPassword());
         return member;
     }
