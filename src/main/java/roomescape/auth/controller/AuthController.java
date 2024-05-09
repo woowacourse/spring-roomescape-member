cdcc78bcefb13f430a519becd37772fec8baef35
eunjungL <dldmswjd322@gmail.com>
2024-05-09 16:40:46 +0900

    refactor: AccessToken 쿠키로 변환 로직 메서드 분리

--- src/main/java/roomescape/auth/controller/AuthController.java
+++ src/main/java/roomescape/auth/controller/AuthController.java
@@ -5,18 +5,27 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import roomescape.auth.service.AuthService;
 import roomescape.auth.dto.LoginCheckResponse;
 import roomescape.auth.dto.LoginRequest;
 import roomescape.auth.dto.LoginResponse;
+import roomescape.auth.service.AuthService;
 
 @Controller
 public class AuthController {
+
+    private static final long EXPIRED_PERIOD = (long) 1000 * 60 * 60; // 30일
 
     private final AuthService authService;
 
     public AuthController(AuthService authService) {
         this.authService = authService;
+    }
+
+    private String createTokenCookie(String value, long maxAge) {
+        return ResponseCookie.from("token", value)
+                .maxAge(maxAge)
+                .build()
+                .toString();
     }
 
     @GetMapping("/login")
@@ -28,9 +37,10 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<Void> login(@RequestBody LoginRequest request) {
         LoginResponse data = authService.login(request);
+        String cookie = createTokenCookie(data.accessToken(), EXPIRED_PERIOD);
 
         return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, "token=" + data.accessToken())
+                .header(HttpHeaders.SET_COOKIE, cookie)
                 .build();
     }
 
@@ -43,12 +53,15 @@ public class AuthController {
     @ResponseBody
     @PostMapping("/logout")
     public ResponseEntity<Void> logout() {
-        ResponseCookie cookie = ResponseCookie.from("token", "")
-                .maxAge(0)
-                .build();
+        String cookie = createTokenCookie("", 0);
 
         return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .header(HttpHeaders.SET_COOKIE, cookie)
                 .build();
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
     }
 }
