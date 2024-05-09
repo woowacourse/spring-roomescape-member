9af63e3f9e7b48e12fa2d83e362ea1d78ea32f1d
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-09 14:21:03 +0900

    feat(LoginMember): id 필드 및 getter 추가

--- src/main/java/roomescape/domain/LoginMember.java
+++ src/main/java/roomescape/domain/LoginMember.java
@@ -1,13 +1,31 @@
 package roomescape.domain;
 
 public class LoginMember {
+    private final long id;
     private final String name;
     private final String email;
     private final String password;
 
-    public LoginMember(String name, String email, String password) {
+    public LoginMember(long id, String name, String email, String password) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
