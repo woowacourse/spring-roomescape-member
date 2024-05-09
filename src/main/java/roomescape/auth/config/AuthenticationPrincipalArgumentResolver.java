7b3b942fc5b9db13b776b630935ae744e08ebe18
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-09 17:34:37 +0900

    feat(AuthenticationPrincipalArgumentResolver): 회원 정보에 대한 객체를 컨트롤러에 주입하는 HandlerMethodArgumentResolver 구현

---
+++ src/main/java/roomescape/auth/config/AuthenticationPrincipalArgumentResolver.java
@@ -0,0 +1,41 @@
+package roomescape.auth.config;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import roomescape.auth.TokenProvider;
+
+public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final TokenProvider tokenProvider;
+
+    public AuthenticationPrincipalArgumentResolver(final TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public boolean supportsParameter(final MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthenticationPrincipal.class);
+    }
+
+    @Override
+    public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer,
+                                  final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) webRequest.getNativeRequest();
+        String token = extractTokenFromCookie(httpServletRequest.getCookies());
+        return tokenProvider.extractAuthInfo(token);
+    }
+
+    private String extractTokenFromCookie(Cookie[] cookies) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+}
