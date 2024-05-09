9150e29984fd277f655587f343e059302ab1a85e
unifolio0 <boho82ha@gmail.com>
2024-05-09 12:52:20 +0900

    refactor: userToken 얻는 로직 추출

--- src/test/java/roomescape/TestFixtures.java
+++ src/test/java/roomescape/TestFixtures.java
@@ -1,17 +1,10 @@
-/*
 package roomescape;
 
-import java.time.LocalTime;
 import java.util.List;
-import roomescape.controller.request.ReservationTimeRequest;
-import roomescape.controller.request.ThemeRequest;
-import roomescape.controller.response.ReservationTimeResponse;
 import roomescape.controller.response.ThemeResponse;
-import roomescape.domain.ReservationTime;
-import roomescape.domain.Theme;
 
 public class TestFixtures {
-    public static final ReservationTime TIME_1 = new ReservationTime(1L, LocalTime.of(10, 0));
+    /*public static final ReservationTime TIME_1 = new ReservationTime(1L, LocalTime.of(10, 0));
     public static final ReservationTime TIME_2 = new ReservationTime(2L, LocalTime.of(11, 0));
     public static final ReservationTime TIME_3 = new ReservationTime(3L, LocalTime.of(12, 0));
     public static final ReservationTimeRequest TIME_REQUEST_2 = new ReservationTimeRequest("11:00");
@@ -30,7 +23,7 @@ public class TestFixtures {
     );
     public static final ThemeRequest THEME_REQUEST_3 = new ThemeRequest(
             "name3", "description3", "thumbnail3"
-    );
+    );*/
     public static final ThemeResponse THEME_RESPONSE_1 = new ThemeResponse(
             1L, "name1", "description1", "thumbnail1"
     );
@@ -76,8 +69,7 @@ public class TestFixtures {
             THEME_RESPONSE_1, THEME_RESPONSE_2, THEME_RESPONSE_3, THEME_RESPONSE_11, THEME_RESPONSE_4, THEME_RESPONSE_5,
             THEME_RESPONSE_6, THEME_RESPONSE_7, THEME_RESPONSE_8, THEME_RESPONSE_9
     );
-    */
-/*public static final Reservation RESERVATION_1 = new Reservation(
+    /*public static final Reservation RESERVATION_1 = new Reservation(
             1L, new Name("first"), LocalDate.now().plusDays(5), TIME_1, THEME_1
     );
     public static final Reservation RESERVATION_2 = new Reservation(
@@ -100,15 +92,16 @@ public class TestFixtures {
     );
     public static final Reservation PAST_RESERVATION_3 = new Reservation(
             5L, new Name("fifth"), LocalDate.now().minusDays(6), TIME_3, THEME_2
-    );*//*
+    );
 
-    */
-/*public static final ReservationRequest PAST_RESERVATION_REQUEST = new ReservationRequest("past", LocalDate.now().minusYears(1).toString(), 2L, 3L);
-    public static final ReservationRequest RESERVATION_REQUEST_2 = new ReservationRequest("second", LocalDate.now().plusDays(6).toString(), 2L, 2L);
-    public static final ReservationRequest RESERVATION_REQUEST_3 = new ReservationRequest("third", LocalDate.now().plusDays(7).toString(), 2L, 3L);
+    public static final ReservationRequest PAST_RESERVATION_REQUEST = new ReservationRequest("past",
+            LocalDate.now().minusYears(1).toString(), 2L, 3L);
+    public static final ReservationRequest RESERVATION_REQUEST_2 = new ReservationRequest("second",
+            LocalDate.now().plusDays(6).toString(), 2L, 2L);
+    public static final ReservationRequest RESERVATION_REQUEST_3 = new ReservationRequest("third",
+            LocalDate.now().plusDays(7).toString(), 2L, 3L);
     public static final ReservationResponse RESERVATION_RESPONSE_1 = ReservationResponse.from(RESERVATION_1);
     public static final ReservationResponse RESERVATION_RESPONSE_2 = ReservationResponse.from(RESERVATION_2);
-    public static final ReservationResponse RESERVATION_RESPONSE_3 = ReservationResponse.from(RESERVATION_3);*//*
+    public static final ReservationResponse RESERVATION_RESPONSE_3 = ReservationResponse.from(RESERVATION_3);*/
 
 }
-*/
--- src/test/java/roomescape/acceptance/ReservationAcceptanceTest.java
+++ src/test/java/roomescape/acceptance/ReservationAcceptanceTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 import java.time.LocalDate;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
@@ -14,19 +14,22 @@ import roomescape.BasicAcceptanceTest;
 
 @Sql("/init-for-reservation.sql")
 class ReservationAcceptanceTest extends BasicAcceptanceTest {
+    private String userToken;
+
+    @BeforeEach
+    void SetUp() {
+        userToken = LoginUtil.login("email1", "qq1");
+    }
+
     @TestFactory
     @DisplayName("3개의 예약을 추가한다")
     Stream<DynamicTest> reservationPostAndGetTest() {
         LocalDate tomorrow = LocalDate.now().plusDays(1);
-        AtomicReference<String> token = new AtomicReference<>();
 
         return Stream.of(
-                dynamicTest("로그인을 한다", () -> {
-                    token.set(LoginUtil.login("email1", "qq1"));
-                }),
-                dynamicTest("예약을 추가한다", () -> ReservationCRD.postUserReservation(token.get(), tomorrow.toString(), 1L, 1L, 201)),
-                dynamicTest("예약을 추가한다", () -> ReservationCRD.postUserReservation(token.get(), tomorrow.toString(), 2L, 2L, 201)),
-                dynamicTest("예약을 추가한다", () -> ReservationCRD.postUserReservation(token.get(), tomorrow.toString(), 3L, 3L, 201)),
+                dynamicTest("예약을 추가한다", () -> ReservationCRD.postUserReservation(userToken, tomorrow.toString(), 1L, 1L, 201)),
+                dynamicTest("예약을 추가한다", () -> ReservationCRD.postUserReservation(userToken, tomorrow.toString(), 2L, 2L, 201)),
+                dynamicTest("예약을 추가한다", () -> ReservationCRD.postUserReservation(userToken, tomorrow.toString(), 3L, 3L, 201)),
                 dynamicTest("모든 예약을 조회한다 (총 3개)", () -> ReservationCRD.getReservationTimes(200, 3))
         );
     }
@@ -35,13 +38,9 @@ class ReservationAcceptanceTest extends BasicAcceptanceTest {
     @DisplayName("과거 시간에 대한 예약을 하면, 예외가 발생한다")
     Stream<DynamicTest> pastTimeReservationTest() {
         LocalDate yesterday = LocalDate.now().minusDays(1);
-        AtomicReference<String> token = new AtomicReference<>();
 
         return Stream.of(
-                dynamicTest("로그인을 한다", () -> {
-                    token.set(LoginUtil.login("email1", "qq1"));
-                }),
-                dynamicTest("과거 시간에 대한 예약을 추가한다", () -> ReservationCRD.postUserReservation(token.get(), yesterday.toString(), 1L, 1L, 400))
+                dynamicTest("과거 시간에 대한 예약을 추가한다", () -> ReservationCRD.postUserReservation(userToken, yesterday.toString(), 1L, 1L, 400))
         );
     }
 
@@ -49,15 +48,11 @@ class ReservationAcceptanceTest extends BasicAcceptanceTest {
     @DisplayName("이미 예약된 테마와 시간을 예약을 하면, 예외가 발생한다")
     Stream<DynamicTest> duplicateReservationTest() {
         LocalDate tomorrow = LocalDate.now().plusDays(1);
-        AtomicReference<String> token = new AtomicReference<>();
 
         return Stream.of(
-                dynamicTest("로그인을 한다", () -> {
-                    token.set(LoginUtil.login("email1", "qq1"));
-                }),
-                dynamicTest("예약을 추가한다", () -> ReservationCRD.postUserReservation(token.get(), tomorrow.toString(), 1L, 1L, 201)),
-                dynamicTest("동일한 예약을 추가한다", () -> ReservationCRD.postUserReservation(token.get(), tomorrow.toString(), 1L, 1L, 400)),
-                dynamicTest("다른 테마를 예약한다", () -> ReservationCRD.postUserReservation(token.get(), tomorrow.toString(), 1L, 2L, 201))
+                dynamicTest("예약을 추가한다", () -> ReservationCRD.postUserReservation(userToken, tomorrow.toString(), 1L, 1L, 201)),
+                dynamicTest("동일한 예약을 추가한다", () -> ReservationCRD.postUserReservation(userToken, tomorrow.toString(), 1L, 1L, 400)),
+                dynamicTest("다른 테마를 예약한다", () -> ReservationCRD.postUserReservation(userToken, tomorrow.toString(), 1L, 2L, 201))
         );
     }
 
@@ -66,21 +61,16 @@ class ReservationAcceptanceTest extends BasicAcceptanceTest {
     Stream<DynamicTest> reservationPostAndDeleteTest() {
         LocalDate tomorrow = LocalDate.now().plusDays(1);
         AtomicLong reservationId = new AtomicLong();
-        AtomicReference<String> token = new AtomicReference<>();
 
         return Stream.of(
-                dynamicTest("로그인을 한다", () -> {
-                    token.set(LoginUtil.login("email1", "qq1"));
-                }),
                 dynamicTest("예약을 추가한다", () -> {
-                    Long id = ReservationCRD.postUserReservation(token.get(), tomorrow.toString(), 1L, 1L, 201);
+                    Long id = ReservationCRD.postUserReservation(userToken, tomorrow.toString(), 1L, 1L, 201);
                     reservationId.set(id);
                 }),
-                dynamicTest("예약을 추가한다", () -> ReservationCRD.postUserReservation(token.get(), tomorrow.toString(), 2L, 2L, 201)),
+                dynamicTest("예약을 추가한다", () -> ReservationCRD.postUserReservation(userToken, tomorrow.toString(), 2L, 2L, 201)),
                 dynamicTest("모든 예약을 조회한다 (총 2개)", () -> ReservationCRD.getReservationTimes(200, 2)),
                 dynamicTest("예약을 삭제한다", () -> ReservationCRD.deleteReservationTime(reservationId.longValue(), 204)),
                 dynamicTest("모든 예약을 조회한다 (총 1개)", () -> ReservationCRD.getReservationTimes(200, 1))
         );
     }
 }
-
