73caed41e5cd5bade8a17592aefb8b5b85200723
ehtjsv2 <ehtjsv2@naver.com>
2024-05-09 19:53:47 +0900

    feat: 예약 시 이름을 직접 받지 않고 쿠키를 활용하는 기능 구현

--- src/main/java/roomescape/domain/reservation/controller/AdminReservationController.java
+++ src/main/java/roomescape/domain/reservation/controller/AdminReservationController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.login.controller.AuthenticationPrincipal;
+import roomescape.domain.login.domain.Member;
 import roomescape.domain.reservation.domain.Reservation;
 import roomescape.domain.reservation.dto.ReservationAddRequest;
 import roomescape.domain.reservation.service.AdminReservationService;
@@ -28,7 +30,10 @@ public class AdminReservationController {
     }
 
     @PostMapping("/reservations")
-    public ResponseEntity<Reservation> addReservation(@RequestBody ReservationAddRequest reservationAddRequest) {
+    public ResponseEntity<Reservation> addReservation(@RequestBody ReservationAddRequest reservationAddRequest,
+                                                      @AuthenticationPrincipal Member member) {
+        reservationAddRequest = new ReservationAddRequest(reservationAddRequest.date(), reservationAddRequest.timeId(),
+                reservationAddRequest.themeId(), member.getId());
         Reservation reservation = adminReservationService.addReservation(reservationAddRequest);
         return ResponseEntity.created(URI.create("/reservation/" + reservation.getId())).body(reservation);
     }
