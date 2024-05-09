f748f7cf4129563d6e527976cc24652e78c0731f
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-05-09 15:55:41 +0900

    feat(User): 사용자 도메인 추가

---
+++ src/main/java/roomescape/model/User.java
@@ -0,0 +1,25 @@
+package roomescape.model;
+
+public class User {
+    private final String name;
+    private final String email;
+    private final String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
