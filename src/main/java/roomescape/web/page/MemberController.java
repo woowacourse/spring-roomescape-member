6f6f7f34f0e2767c8e592eed589b8466dfab402c
3juhwan <13selfesteem91@naver.com>
2024-05-09 17:11:42 +0900

    feat: UserController -> MemberController 수정

--- src/main/java/roomescape/web/page/MemberController.java
+++ src/main/java/roomescape/web/page/MemberController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UserController {
+public class MemberController {
     @GetMapping("/login")
     public String login() {
         return "login";
