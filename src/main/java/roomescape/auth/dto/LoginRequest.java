0df2f7be6bbc2e1bddcd6ff18148f203e342ec2e
kelly6bf <kelly6bf@gmail.com>
2024-05-09 14:45:59 +0900

    feat: AuthService 로그인 기능 구현

---
+++ src/main/java/roomescape/auth/dto/LoginRequest.java
@@ -0,0 +1,4 @@
+package roomescape.auth.dto;
+
+public record LoginRequest(String email, String password) {
+}
---
+++ src/main/java/roomescape/auth/service/AuthService.java
@@ -0,0 +1,43 @@
+package roomescape.auth.service;
+
+import org.springframework.stereotype.Service;
+import roomescape.auth.dto.LoginRequest;
+import roomescape.auth.token.AuthenticationToken;
+import roomescape.auth.token.TokenProvider;
+import roomescape.member.encoder.PasswordEncoder;
+import roomescape.member.model.Member;
+import roomescape.member.repository.MemberRepository;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class AuthService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+
+    public AuthService(
+            final MemberRepository memberRepository,
+            final PasswordEncoder passwordEncoder,
+            final TokenProvider tokenProvider
+    ) {
+        this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenProvider = tokenProvider;
+    }
+
+    public AuthenticationToken login(final LoginRequest request) {
+        final Member member = memberRepository.findByEmail(request.email())
+                .orElseThrow(() -> new NoSuchElementException("해당 이메일 정보와 일치하는 회원 정보가 없습니다."));
+        checkCredential(member, request.password());
+
+        return tokenProvider.createToken(member.getEmail(), member.getRole());
+    }
+
+    private void checkCredential(final Member member, final String password) {
+        if (!passwordEncoder.matches(password, member.getPassword().value())) {
+            throw new IllegalArgumentException("일치하지 않는 비밀번호입니다.");
+        }
+    }
+}
