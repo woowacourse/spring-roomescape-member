956783acf8b7632bdef4e64df203353f5661cb1b
pricelees <priceelees@gmail.com>
2024-05-09 11:25:40 +0900

    refactor: 예약 Request에서의 시간, 테마 필드에 @NotNull 추가

--- src/main/java/roomescape/dto/reservation/ReservationRequest.java
+++ src/main/java/roomescape/dto/reservation/ReservationRequest.java
@@ -5,9 +5,9 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record ReservationRequest(
-        @NotEmpty(message = "이름을 입력해주세요.") String name,
-        @NotNull(message = "날짜를 입력해주세요.") LocalDate date,
-        Long timeId,
-        Long themeId
+        @NotEmpty(message = "이름이 입력되지 않았습니다.") String name,
+        @NotNull(message = "날짜가 입력되지 않았습니다.") LocalDate date,
+        @NotNull(message = "시간이 입력되지 않았습니다.") Long timeId,
+        @NotNull(message = "테마가 입력되지 않았습니다.") Long themeId
 ) {
 }
