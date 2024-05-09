3dbe9d360f1a9e7323b0f6606df5fb2a29777ac8
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-09 16:23:33 +0900

    feat: reservation 스키마에 memberId 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -21,17 +21,19 @@ CREATE TABLE reservation
     date VARCHAR(255) NOT NULL,
     time_id BIGINT NOT NULL,
     theme_id BIGINT NOT NULL,
+    member_id BIGINT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    FOREIGN KEY (member_id) REFERENCES theme (id)
 );
 
 CREATE TABLE member
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
-    user_name VARCHAR(255) NOT NULL,
-    user_email VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     role ENUM('USER', 'ADMIN') NOT NULL,
     PRIMARY KEY (id)
 )
