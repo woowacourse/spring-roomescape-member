23667c311ff70ff26073ba9e5d62034a0cfb3d62
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-05-09 17:40:39 +0900

    fix(User): 사용자 도메인이 아이디 값을 가지도록 수정

--- src/main/java/roomescape/model/User.java
+++ src/main/java/roomescape/model/User.java
@@ -1,14 +1,26 @@
 package roomescape.model;
 
-public class User {
-    private final String name;
-    private final String email;
-    private final String password;
+import java.util.Objects;
 
-    public User(String name, String email, String password) {
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    private User() {
+    }
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
@@ -21,5 +33,34 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        User user = (User) object;
+        return Objects.equals(getId(), user.getId()) && Objects.equals(getName(), user.getName())
+                && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getPassword(),
+                user.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getEmail(), getPassword());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
