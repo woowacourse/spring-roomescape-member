4c14e8afef3c68c4cf33590f3c9ff2e99c8c5515
libienz <leekh10195@gmail.com>
2024-05-09 17:18:05 +0900

    feat: MemberService 구현

---
+++ src/main/java/roomescape/member/service/MemberService.java
@@ -0,0 +1,21 @@
+package roomescape.member.service;
+
+import org.springframework.stereotype.Service;
+import roomescape.exception.NoSuchRecordException;
+import roomescape.member.domain.Member;
+import roomescape.member.domain.MemberRepository;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Member findById(Long id) {
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new NoSuchRecordException("id: " + id + " 해당하는 회원을 찾을 수 없습니다"));
+    }
+}
