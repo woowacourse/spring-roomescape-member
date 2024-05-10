340ae8ef82007c78af8a34ec901e33e34c4440d1
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-10 17:59:40 +0900

    feat(ReservationFilterArgumentResolver): ReservationFilter 생성 ArgumentResolver 추가

--- src/main/java/roomescape/config/WebConfig.java
+++ src/main/java/roomescape/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.controller.argumentresolver.LoginMemberArgumentResolver;
+import roomescape.controller.argumentresolver.ReservationFilterArgumentResolver;
 import roomescape.controller.interceptor.AdminAuthorizationInterceptor;
 import roomescape.controller.interceptor.UserAuthorizationInterceptor;
 
@@ -15,13 +16,16 @@ public class WebConfig implements WebMvcConfigurer {
     private final AdminAuthorizationInterceptor adminAuthorizationInterceptor;
     private final UserAuthorizationInterceptor userAuthorizationInterceptor;
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final ReservationFilterArgumentResolver reservationFilterArgumentResolver;
 
     public WebConfig(AdminAuthorizationInterceptor adminAuthorizationInterceptor,
                      UserAuthorizationInterceptor userAuthorizationInterceptor,
-                     LoginMemberArgumentResolver loginMemberArgumentResolver) {
+                     LoginMemberArgumentResolver loginMemberArgumentResolver,
+                     ReservationFilterArgumentResolver reservationFilterArgumentResolver) {
         this.adminAuthorizationInterceptor = adminAuthorizationInterceptor;
         this.userAuthorizationInterceptor = userAuthorizationInterceptor;
         this.loginMemberArgumentResolver = loginMemberArgumentResolver;
+        this.reservationFilterArgumentResolver = reservationFilterArgumentResolver;
     }
 
     @Override
@@ -33,5 +37,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+        resolvers.add(reservationFilterArgumentResolver);
     }
 }
