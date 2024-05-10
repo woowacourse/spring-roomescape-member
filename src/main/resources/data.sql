4f7abda0efaf5559433eb3da8c21fd156dfbaea1
Nam Gi Beom <rlqja0523@naver.com>
2024-05-10 20:14:02 +0900

    chore: 비밀번호 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO users(id, name, email, password, role)
-VALUES (1, '프린', 'prin@gmail.com', '1q2w3e34r!@', 'ADMIN'),
-       (2, '레모네', 'remone@gmail.com', '1q2w3e34r!@', 'USER'),
-       (3, '오리', 'duck@gmail.com', '1q2w3e34r!@', 'USER');
+VALUES (1, '프린', 'prin@gmail.com', '1q2w3e4r!@', 'ADMIN'),
+       (2, '레모네', 'remone@gmail.com', '1q2w3e4r!@', 'USER'),
+       (3, '오리', 'duck@gmail.com', '1q2w3e4r!@', 'USER');
