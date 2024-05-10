f85233601f5f5d8d407ba2e9c1d55ffc4fc8760e
nayonsoso <panda0329@naver.com>
2024-05-10 22:33:10 +0900

    test: 테스트 데이터 초기화 쿼리 추가

---
+++ src/test/resources/truncate.sql
@@ -0,0 +1,13 @@
+SET referential_integrity false;
+
+TRUNCATE TABLE reservation;
+TRUNCATE TABLE reservation_time;
+TRUNCATE TABLE theme;
+TRUNCATE TABLE member;
+
+ALTER TABLE reservation ALTER COLUMN id RESTART WITH 1;
+ALTER TABLE reservation_time ALTER COLUMN id RESTART WITH 1;
+ALTER TABLE theme ALTER COLUMN id RESTART WITH 1;
+ALTER TABLE member ALTER COLUMN id RESTART WITH 1;
+
+SET referential_integrity true;
