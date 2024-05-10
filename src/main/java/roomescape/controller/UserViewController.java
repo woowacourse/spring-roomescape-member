2be6a6b6984a25144d9a439437ce305a8d8ea040
lemone <lejinhy@gmail.com>
2024-05-10 14:12:32 +0900

    feat: login 페이지 추가

--- src/main/java/roomescape/controller/UserViewController.java
+++ src/main/java/roomescape/controller/UserViewController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UserController {
+public class UserViewController {
     @GetMapping
     public String popularTheme() {
         return "index";
@@ -13,5 +13,10 @@ public class UserController {
     @GetMapping("/reservation")
     public String reservation() {
         return "reservation";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
