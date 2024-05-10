273492e233af9aa2cdaf6f718bd714ba437653b7
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-10 12:14:34 +0900

    refactor(LoginController): 변경된 로직에 맞게 수정 및 메서드 분리

--- src/main/java/roomescape/controller/LoginController.java
+++ src/main/java/roomescape/controller/LoginController.java
@@ -10,37 +10,41 @@ import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.login.LoginCheckResponse;
 import roomescape.dto.login.LoginMember;
 import roomescape.dto.login.LoginRequest;
+import roomescape.dto.member.MemberResponse;
 import roomescape.dto.token.TokenDto;
 import roomescape.service.AuthService;
+import roomescape.service.MemberService;
 
 @RestController
 public class LoginController {
 
     private final AuthService authService;
+    private final MemberService memberService;
 
-    public LoginController(AuthService authService) {
+    public LoginController(AuthService authService, MemberService memberService) {
         this.authService = authService;
+        this.memberService = memberService;
     }
 
     @PostMapping("/login")
     public ResponseEntity<Void> login(@RequestBody LoginRequest request) {
         TokenDto loginToken = authService.login(request);
-
-        ResponseCookie cookie = ResponseCookie
-                .from("token", loginToken.accessToken())
-                .path("/")
-                .httpOnly(true)
-                .build();
-
-        return ResponseEntity
-                .ok()
-                .header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .build();
+        ResponseCookie cookie = createResponseCookie(loginToken);
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).build();
     }
 
     @GetMapping("/login/check")
     public ResponseEntity<LoginCheckResponse> checkLogin(LoginMember loginMember) {
-        LoginCheckResponse response = new LoginCheckResponse(loginMember.name());
+        MemberResponse memberResponse = memberService.getMemberById(loginMember.id());
+        LoginCheckResponse response = new LoginCheckResponse(memberResponse.name());
         return ResponseEntity.ok(response);
+    }
+
+    private ResponseCookie createResponseCookie(TokenDto loginToken) {
+        return ResponseCookie
+                .from("token", loginToken.accessToken())
+                .path("/")
+                .httpOnly(true)
+                .build();
     }
 }
