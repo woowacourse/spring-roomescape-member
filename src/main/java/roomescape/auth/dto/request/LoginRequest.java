85ddb4fa4e9b59f2826bfb810d2b01a83e6d343e
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-10 13:42:43 +0900

    fix(LoginRequest): 로그인 요청 객체 검증 시 잘못 작성된 예외 메시지를 변경

--- src/main/java/roomescape/auth/dto/request/LoginRequest.java
+++ src/main/java/roomescape/auth/dto/request/LoginRequest.java
@@ -1,6 +1,7 @@
 package roomescape.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record LoginRequest(
@@ -8,6 +9,6 @@ public record LoginRequest(
         @NotNull(message = "이메일은 공백 문자가 불가능합니다.")
         String email,
 
-        @NotNull(message = "이메일은 공백 문자가 불가능합니다.")
+        @NotBlank(message = "비밀번호는 공백 문자가 불가능합니다.")
         String password) {
 }
