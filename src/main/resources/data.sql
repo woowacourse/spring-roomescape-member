5cdf905e2873253806a51c80162b469ca6012ad0
Nam Gi Beom <rlqja0523@naver.com>
2024-05-10 17:19:14 +0900

    chore: data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users(id, name, email, password)
+VALUES (1, '프린', 'prin@gmail.com', '1q2w3e34r!@'),
+       (2, '레모네', 'remone@gmail.com', '1q2w3e34r!@'),
+       (3, '오리', 'duck@gmail.com', '1q2w3e34r!@');
