0d3587e54b667b4c56f696781a601bdf60f7b1ab
SCY <helenason@naver.com>
2024-05-10 19:18:47 +0900

    refactor(sql): 멤버 더미 데이터 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,8 @@ INSERT INTO reservation_time (start_at) VALUES ('11:00');
 INSERT INTO theme (name, description, thumbnail) VALUES ('에버', '공포', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 INSERT INTO theme (name, description, thumbnail) VALUES ('배키', '스릴러', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
-INSERT INTO member (name, email, password, role) VALUES ('에버', 'treeboss@gmail.com', '123', 'USER');
-INSERT INTO member (name, email, password, role) VALUES ('우테코', 'wtc@gmail.com', '123', 'ADMIN');
+INSERT INTO member (name, email, password, role) VALUES ('에버', 'treeboss@gmail.com', 'treeboss123!', 'USER');
+INSERT INTO member (name, email, password, role) VALUES ('관리자', 'admin@gmail.com', 'admin123!', 'ADMIN');
 
 INSERT INTO reservation (date, time_id, theme_id, member_id) VALUES ('2024-05-08', 1, 1, 1);
 INSERT INTO reservation (date, time_id, theme_id, member_id) VALUES ('2024-05-08', 2, 2, 2);
