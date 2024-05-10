4692c6883a3909e79dc8087b621a16db87bdcef1
HoeSeong123 <ghltjd3@gmail.com>
2024-05-10 13:53:33 +0900

    style : 코드 스타일 정리

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -1,3 +1,9 @@
-DELETE FROM reservation
-DELETE FROM reservation_time
-DELETE FROM theme
+DELETE
+FROM reservation;
+DELETE
+FROM reservation_time;
+DELETE
+FROM theme;
+
+INSERT INTO users (name, email, password)
+VALUES ('유저', 'admin@email.com', 'password');
