8e5394f860f5504dfecb2b894900d60fb449c606
BurningFalls <sherlocky31415@gmail.com>
2024-05-11 01:55:53 +0900

    refacor: 미사용 메서드 member create 삭제

--- src/main/java/roomescape/member/repository/MemberRepository.java
+++ src/main/java/roomescape/member/repository/MemberRepository.java
@@ -2,13 +2,9 @@ package roomescape.member.repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import roomescape.member.domain.Member;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 @Repository
@@ -22,23 +18,9 @@ public class MemberRepository {
                     resultSet.getString("role"));
 
     private final JdbcTemplate jdbcTemplate;
-    private final SimpleJdbcInsert simpleJdbcInsert;
 
-    public MemberRepository(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+    public MemberRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
-        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
-                .withTableName("MEMBER")
-                .usingGeneratedKeyColumns("id");
-    }
-
-    public Long create(Member member) {
-        SqlParameterSource params = new MapSqlParameterSource()
-                .addValue("name", member.getName())
-                .addValue("email", member.getEmail())
-                .addValue("password", member.getPassword())
-                .addValue("role", member.getRole());
-
-        return simpleJdbcInsert.executeAndReturnKey(params).longValue();
     }
 
     public List<Member> readAll() {
@@ -47,7 +29,7 @@ public class MemberRepository {
         return jdbcTemplate.query(sql, ROW_MAPPER);
     }
 
-    public Member read(String email) {
+    public Member readByEmail(String email) {
         String sql = "SELECT * FROM member WHERE email = ?";
 
         return jdbcTemplate.queryForObject(sql, ROW_MAPPER, email);
