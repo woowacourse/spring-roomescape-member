581b2c7c1913160ab9ae60ae6959c1fe7109f9eb
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-10 21:12:26 +0900

    feat: 이메일 VO 검증로직 추가

--- src/main/java/roomescape/domain/member/Email.java
+++ src/main/java/roomescape/domain/member/Email.java
@@ -1,10 +1,23 @@
 package roomescape.domain.member;
 
+import java.util.regex.Pattern;
+
 public class Email {
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "'^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$'"
+    );
+
     private final String email;
 
     public Email(String email) {
+        validateEmail(email);
         this.email = email;
+    }
+
+    private void validateEmail(String email) {
+        if (email.isBlank() || !EMAIL_PATTERN.matcher(email).matches()) {
+            throw new IllegalArgumentException("올바른 형식의 이메일이 아닙니다.");
+        }
     }
 
     public String getEmail() {
