946d30687c65b3946482e8b49ed205a09ef6c4dd
BurningFalls <sherlocky31415@gmail.com>
2024-05-10 21:48:30 +0900

    test: sample admin token으로 AdminPageController 테스트 구현

--- src/test/java/roomescape/admin/AdminPageControllerTest.java
+++ src/test/java/roomescape/admin/AdminPageControllerTest.java
@@ -3,9 +3,12 @@ package roomescape.admin;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.auth.provider.JwtTokenProvider;
+import roomescape.member.domain.Member;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -14,47 +17,48 @@ public class AdminPageControllerTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    private String adminToken;
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+        adminToken = jwtTokenProvider.createToken(
+                new Member(1L, "한태웅", "taewoong@example.com", "123", "ADMIN"));
+    }
+
+    private void testAdminPage(String path) {
+        RestAssured.given()
+                .cookie("token", adminToken)
+                .log()
+                .all()
+                .when()
+                .get("/admin" + path)
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
     }
 
     @Test
     void admin() {
-        RestAssured.given()
-                .log()
-                .all()
-                .when()
-                .get("/admin")
-                .then()
-                .log()
-                .all()
-                .statusCode(200);
+        testAdminPage("");
     }
 
     @Test
     void reservation() {
-        RestAssured.given()
-                .log()
-                .all()
-                .when()
-                .get("/admin/reservation")
-                .then()
-                .log()
-                .all()
-                .statusCode(200);
+        testAdminPage("/reservation");
     }
 
     @Test
     void time() {
-        RestAssured.given()
-                .log()
-                .all()
-                .when()
-                .get("/admin/time")
-                .then()
-                .log()
-                .all()
-                .statusCode(200);
+        testAdminPage("/time");
+    }
+
+    @Test
+    void theme() {
+        testAdminPage("/theme");
     }
 }
