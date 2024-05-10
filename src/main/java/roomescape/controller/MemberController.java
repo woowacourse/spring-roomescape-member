c37a257f2500c01da79537454cfb932a0680508a
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-10 12:16:30 +0900

    feat(MemberController): 로그아웃 기능 구현

--- src/main/java/roomescape/controller/MemberController.java
+++ src/main/java/roomescape/controller/MemberController.java
@@ -1,5 +1,6 @@
 package roomescape.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import roomescape.domain.exception.AuthFailException;
 import roomescape.dto.request.MemberCreateRequest;
 import roomescape.dto.response.MemberResponse;
 import roomescape.dto.response.TokenResponse;
@@ -38,5 +40,21 @@ public class MemberController {
     public ResponseEntity<MemberResponse> checkLogin(HttpServletRequest request) {
         MemberResponse response = service.findMember(request.getCookies());
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            request.logout();
+        } catch (ServletException e) {
+            throw new AuthFailException("로그아웃이 실패하였습니다.");
+        }
+
+        Cookie cookie = new Cookie("token", null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/roomescape/domain/exception/AuthFailException.java
+++ src/main/java/roomescape/domain/exception/AuthFailException.java
@@ -4,4 +4,8 @@ public class AuthFailException extends RuntimeException {
     public AuthFailException() {
         super("로그인 정보가 잘못되었습니다.");
     }
+
+    public AuthFailException(String s) {
+        super(s);
+    }
 }
