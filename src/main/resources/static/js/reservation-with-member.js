65b6d4365a0fadbc8ffa8f152be5bd99ec6d6635
3juhwan <13selfesteem91@naver.com>
2024-05-10 17:06:30 +0900

    fix: 응답 형식에 맞게 클라이언트 코드 수정

--- src/main/resources/static/js/reservation-with-member.js
+++ src/main/resources/static/js/reservation-with-member.js
@@ -24,18 +24,20 @@ function render(data) {
   const tableBody = document.getElementById('table-body');
   tableBody.innerHTML = '';
 
-  data.forEach(item => {
+  console.log(data);
+
+  data.reservations.forEach(item => {
     const row = tableBody.insertRow();
 
+    row.insertCell(0).textContent = item.id;
     /*
-    TODO: [5단계] 예약 생성 기능 변경 - 관리자
-          예약 목록 조회 API 응답에 맞게 적용
-    */
-    row.insertCell(0).textContent = item.id;              // 예약 id
-    row.insertCell(1).textContent = item.member.name;     // 사용자 name
-    row.insertCell(2).textContent = item.theme.name;      // 테마 name
-    row.insertCell(3).textContent = item.date;            // date
-    row.insertCell(4).textContent = item.time.startAt;    // 예약 시간 startAt
+    TODO: 이후 DB 스키마를 변경하면 수정해야 한다.
+    row.insertCell(1).textContent = item.member.name;
+     */
+    row.insertCell(1).textContent = item.name;
+    row.insertCell(2).textContent = item.theme.name;
+    row.insertCell(3).textContent = item.date;
+    row.insertCell(4).textContent = item.time.startAt;
 
     const actionCell = row.insertCell(row.cells.length);
     actionCell.appendChild(createActionButton('삭제', 'btn-danger', deleteRow));
@@ -45,7 +47,7 @@ function render(data) {
 function fetchTimes() {
   requestRead(TIME_API_ENDPOINT)
       .then(data => {
-        timesOptions.push(...data);
+        timesOptions.push(...data.times);
       })
       .catch(error => console.error('Error fetching time:', error));
 }
@@ -53,7 +55,7 @@ function fetchTimes() {
 function fetchThemes() {
   requestRead(THEME_API_ENDPOINT)
       .then(data => {
-        themesOptions.push(...data);
+        themesOptions.push(...data.themes);
         populateSelect('theme', themesOptions, 'name');
       })
       .catch(error => console.error('Error fetching theme:', error));
