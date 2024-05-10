83173dccb7b7f455d89a6aba6c08ea84907ee7e9
KimSangHyeok <tkdgur0906@naver.com>
2024-05-10 14:05:31 +0900

    feat: Role이 ADMIN인 멤버만 /admin에 접근할 수 있도록 제한 추가

---
+++ src/main/java/roomescape/auth/AuthHandlerInterceptor.java
@@ -0,0 +1,42 @@
+package roomescape.auth;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import roomescape.domain.Member;
+import roomescape.domain.Role;
+import roomescape.service.auth.AuthService;
+
+@Component
+public class AuthHandlerInterceptor implements HandlerInterceptor {
+
+    private final AuthService authService;
+
+    public AuthHandlerInterceptor(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+        Cookie[] cookies = request.getCookies();
+        String token = extractTokenFromCookie(cookies);
+        Member member = authService.findMemberByToken(token);
+        if (member == null || !member.getRole().equals(Role.ADMIN)) {
+            return false;
+        }
+        return true;
+    }
+
+    private String extractTokenFromCookie(Cookie[] cookies) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                return cookie.getValue();
+            }
+        }
+        return "";
+    }
+}
--- src/main/java/roomescape/config/WebConfig.java
+++ src/main/java/roomescape/config/WebConfig.java
@@ -2,7 +2,9 @@ package roomescape.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.auth.AuthHandlerInterceptor;
 import roomescape.auth.AuthenticatedMemberArgumentResolver;
 
 import java.util.List;
@@ -11,13 +13,21 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthenticatedMemberArgumentResolver authenticatedMemberArgumentResolver;
+    private final AuthHandlerInterceptor authHandlerInterceptor;
 
-    public WebConfig(AuthenticatedMemberArgumentResolver authenticatedMemberArgumentResolver) {
+    public WebConfig(AuthenticatedMemberArgumentResolver authenticatedMemberArgumentResolver,
+                     AuthHandlerInterceptor authHandlerInterceptor) {
         this.authenticatedMemberArgumentResolver = authenticatedMemberArgumentResolver;
+        this.authHandlerInterceptor = authHandlerInterceptor;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authenticatedMemberArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authHandlerInterceptor).addPathPatterns("/admin/**");
     }
 }
