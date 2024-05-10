aa8e76718c2ff7e3646ef3de989dabdab06df997
Choo <choo000407@naver.com>
2024-05-10 15:40:43 +0900

    feat: 로그인 폼 페이지를 제공한다

--- src/main/java/roomescape/controller/UserPageController.java
+++ src/main/java/roomescape/controller/UserPageController.java
@@ -9,4 +9,9 @@ public class UserPageController {
     public String getReservationPage() {
         return "reservation";
     }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
 }
