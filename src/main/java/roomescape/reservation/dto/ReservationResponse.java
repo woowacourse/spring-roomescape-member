25d93a8622728d8190bf89c899d2967439f56586
kelly6bf <kelly6bf@gmail.com>
2024-05-10 14:22:47 +0900

    refactor: ReservationResponse 수정

--- src/main/java/roomescape/reservation/dto/ReservationResponse.java
+++ src/main/java/roomescape/reservation/dto/ReservationResponse.java
@@ -15,7 +15,7 @@ public record ReservationResponse(
     public static ReservationResponse from(final Reservation reservation) {
         return new ReservationResponse(
                 reservation.getId(),
-                new MemberResponse(reservation.getClientName().value()),
+                new MemberResponse(reservation.getMember().getName().value()),
                 reservation.getDate().value(),
                 ReservationTimeResponse.from(reservation.getTime()),
                 ThemeResponse.from(reservation.getTheme())
