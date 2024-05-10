b0cf863f5f2921132b76ac1befea16cae9d469ce
tsulocalize <tsulocalizing@gmail.com>
2024-05-11 02:19:17 +0900

    move: Interceptor, ArgumentResolver 패키지 이동

--- src/main/java/roomescape/config/WebMvcConfig.java
+++ src/main/java/roomescape/config/WebMvcConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import roomescape.member.controller.CheckAdminInterceptor;
-import roomescape.member.controller.MemberArgumentResolver;
 import roomescape.member.service.AuthService;
 import java.util.List;
 
