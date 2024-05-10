ea3a65d885c4055fe95e28fb76cb7fb5212517b4
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-10 23:21:01 +0900

    feat: Role 도메인 추가

--- src/main/java/roomescape/domain/LoginMember.java
+++ src/main/java/roomescape/domain/LoginMember.java
@@ -4,12 +4,12 @@ public class LoginMember {
     private static final long NO_ID = 0;
 
     private final long id;
-    private final String name;
+    private final MemberName name;
     private final String email;
     private final String password;
-    private final String role; //Role 객체로 빼기
+    private final Role role;
 
-    public LoginMember(long id, String name, String email, String password, String role) {
+    public LoginMember(long id, MemberName name, String email, String password, Role role) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -18,19 +18,19 @@ public class LoginMember {
     }
 
     public LoginMember(String name, String email, String password, String role) {
-        this(NO_ID, name, email, password, role);
+        this(NO_ID, new MemberName(name), email, password, new Role(role));
     }
 
     public LoginMember(long id, String name, String email) {
-        this(id, name, email, "", "");
+        this(id, new MemberName(name), email, "", new Role(""));
+    }
+
+    public String getName() {
+        return name.getValue();
     }
 
     public long getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public String getEmail() {
@@ -42,6 +42,6 @@ public class LoginMember {
     }
 
     public String getRole() {
-        return role;
+        return role.getValue();
     }
 }
