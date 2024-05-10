8d2d97fb5a8e954ba6e770ffaad10739087a897f
김민수 MINSOO KIM <alstn113@gmail.com>
2024-05-11 00:11:17 +0900

    test: member.sql 데이터 간소화

--- src/test/resources/member.sql
+++ src/test/resources/member.sql
@@ -1,7 +1,5 @@
 INSERT INTO member (email, password, name, role)
 VALUES ('admin@gmail.com', '$2a$10$MbGFqyn/u4wfggRK7HAqDeC1y9s1mESgmXV3b7e7GZT5u1JkIT.gm', '어드민',
         'ADMIN'), -- password: abc123
-       ('alstn1@gmail.com', '1234', '구름1', 'USER'),
-       ('alstn2@gmail.com', '1234', '구름2', 'USER'),
-       ('alstn3@gmail.com', '1234', '구름3', 'USER'),
-       ('alstn4@gmail.com', '1234', '구름4', 'USER');
+       ('user@gmail.com', '$2a$10$MbGFqyn/u4wfggRK7HAqDeC1y9s1mESgmXV3b7e7GZT5u1JkIT.gm', '유저',
+        'USER');  -- password: abc123
