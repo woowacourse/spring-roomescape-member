bf2e3b1d51083cb7cdcf785bd71798d54d709728
jhon3242 <rldotnr2@gmail.com>
2024-05-10 19:37:57 +0900

    refactor: 예외 컨트롤러 패키지 재배치

--- src/main/java/roomescape/controller/api/GlobalExceptionRestHandler.java
+++ src/main/java/roomescape/controller/api/GlobalExceptionRestHandler.java
@@ -1,4 +1,4 @@
-package roomescape.controller;
+package roomescape.controller.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
--- src/main/java/roomescape/controller/web/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/web/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package roomescape.controller;
+package roomescape.controller.web;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
