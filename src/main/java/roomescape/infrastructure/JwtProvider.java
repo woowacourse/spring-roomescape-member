cf3c5cc3d34a3c98fad97df11c99578e5d47e42e
Nam Gi Beom <rlqja0523@naver.com>
2024-05-10 20:13:34 +0900

    feat: 클레임 파싱 추가 및 시크릿키 변경

--- src/main/java/roomescape/infrastructure/JwtProvider.java
+++ src/main/java/roomescape/infrastructure/JwtProvider.java
@@ -1,6 +1,10 @@
 package roomescape.infrastructure;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import java.util.Date;
 import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,12 +14,12 @@ import roomescape.domain.user.Role;
 @Component
 public class JwtProvider {
     private final SecretKey key;
+    private final long expirationMilliseconds;
 
-    @Value("${jwt.expiration}")
-    private long expirationMilliseconds;
-
-    public JwtProvider() {
-        this.key = Jwts.SIG.HS256.key().build();
+    public JwtProvider(@Value("${jwt.secret}") String secret, @Value("${jwt.expiration}") long expirationMilliseconds) {
+        byte[] decoded = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(decoded);
+        this.expirationMilliseconds = expirationMilliseconds;
     }
 
     public String createToken(long id, Role role) {
@@ -30,13 +34,21 @@ public class JwtProvider {
                 .compact();
     }
 
-//    public long parseToken(String token) {
-//        String subject = Jwts.parser()
-//                .verifyWith(key)
-//                .build()
-//                .parseSignedClaims(token)
-//                .getPayload()
-//                .getSubject();
-//        return Long.parseLong(subject);
-//    }
+    public long parseToken(String token) {
+        Claims claims;
+        try {
+            claims = Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        } catch (JwtException e) {
+            throw new IllegalArgumentException("잘못된 토큰입니다.");
+        }
+        String subject = claims.getSubject();
+        if (claims.getExpiration().before(new Date())) {
+            throw new IllegalArgumentException("토큰이 만료되었습니다.");
+        }
+        return Long.parseLong(subject);
+    }
 }
