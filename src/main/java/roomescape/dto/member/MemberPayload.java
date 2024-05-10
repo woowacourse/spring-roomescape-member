d8a79e83cd79030e524c740e11d1be825b04c6c8
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-10 16:01:53 +0900

    refactor(MemberPayload): role 추가

--- src/main/java/roomescape/dto/member/MemberPayload.java
+++ src/main/java/roomescape/dto/member/MemberPayload.java
@@ -1,18 +1,21 @@
 package roomescape.dto.member;
 
 import roomescape.domain.member.Member;
+import roomescape.domain.member.Role;
 
 public record MemberPayload(
         String id,
         String name,
-        String email
+        String email,
+        Role role
 ) {
 
     public static MemberPayload from(Member member) {
         return new MemberPayload(
                 member.getId().toString(),
                 member.getName(),
-                member.getEmail()
+                member.getEmail(),
+                member.getRole()
         );
     }
 }
