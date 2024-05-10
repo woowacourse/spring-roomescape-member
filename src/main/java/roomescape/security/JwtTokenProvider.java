f352e6e122d3645461c2107253a98558582f404f
김민수 MINSOO KIM <alstn113@gmail.com>
2024-05-10 14:42:40 +0900

    refactor: 에러 메세지 변경

--- src/main/java/roomescape/security/JwtTokenProvider.java
+++ src/main/java/roomescape/security/JwtTokenProvider.java
@@ -52,11 +52,11 @@ public class JwtTokenProvider {
 
             return claimsJws.getPayload();
         } catch (ExpiredJwtException e) {
-            throw new IllegalArgumentException("Expired token");
+            throw new IllegalArgumentException("만료된 토큰입니다.");
         } catch (UnsupportedJwtException e) {
-            throw new IllegalArgumentException("Unsupported token");
+            throw new IllegalArgumentException("지원하지 않는 토큰입니다.");
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("Invalid token");
+            throw new IllegalArgumentException("유효하지 않은 토큰입니다.");
         }
     }
 
