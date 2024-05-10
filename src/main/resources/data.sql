015585f093b51d45e7adbeb75728124732a2b362
김민수 MINSOO KIM <alstn113@gmail.com>
2024-05-10 20:19:03 +0900

    chore: user 초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,6 +17,8 @@ VALUES ('09:00'),
 INSERT INTO member (email, password, name, role)
 VALUES ('admin@gmail.com', '$2a$10$MbGFqyn/u4wfggRK7HAqDeC1y9s1mESgmXV3b7e7GZT5u1JkIT.gm', '어드민',
         'ADMIN'), -- password: abc123
+       ('user@gmail.com', '$2a$10$MbGFqyn/u4wfggRK7HAqDeC1y9s1mESgmXV3b7e7GZT5u1JkIT.gm', '유저',
+        'USER'),  -- password: abc123
        ('example1@gmail.com', '1234', '구름1', 'USER'),
        ('example2@gmail.com', '1234', '구름2', 'USER'),
        ('example3@gmail.com', '1234', '구름3', 'USER'),
