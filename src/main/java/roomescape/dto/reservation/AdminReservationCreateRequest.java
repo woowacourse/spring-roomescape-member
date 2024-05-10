af79c21ea6fa3ca1e892a8d5997b1971e1b5b459
Minjoo <m0522inju@gmail.com>
2024-05-10 20:30:49 +0900

    feat: 새로운 요청을 받기 위한 AdminReservationCreateRequest, MemberReservationCreateRequest 생성

--- src/main/java/roomescape/dto/reservation/AdminReservationCreateRequest.java
+++ src/main/java/roomescape/dto/reservation/AdminReservationCreateRequest.java
@@ -3,19 +3,19 @@ package roomescape.dto.reservation;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import roomescape.domain.member.Member;
 import roomescape.domain.reservation.Reservation;
 import roomescape.domain.reservation.ReservationDate;
-import roomescape.domain.reservation.ReservationName;
 import roomescape.domain.reservationtime.ReservationTime;
 import roomescape.domain.theme.Theme;
 
-public record ReservationCreateRequest(
-        @NotBlank(message = "예약자 이름을 입력해주세요.")
-        String name,
-
+public record AdminReservationCreateRequest(
         @NotBlank
         @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "yyyy-MM-dd 형식이 아닙니다.")
         String date,
+
+        @NotNull(message = "회원 ID를 입력해주세요.")
+        Long memberId,
 
         @NotNull(message = "예약 시간 ID를 입력해주세요.")
         Long timeId,
@@ -23,15 +23,15 @@ public record ReservationCreateRequest(
         @NotNull(message = "테마 ID를 입력해주세요.")
         Long themeId) {
 
-    public static ReservationCreateRequest of(String name, String date, Long timeId, Long themeId) {
-        return new ReservationCreateRequest(name, date, timeId, themeId);
+    public static AdminReservationCreateRequest of(String date, Long memberId, Long timeId, Long themeId) {
+        return new AdminReservationCreateRequest(date, memberId, timeId, themeId);
     }
 
-    public Reservation toDomain(ReservationTime reservationTime, Theme theme) {
+    public Reservation toDomain(Member member, ReservationTime reservationTime, Theme theme) {
         return new Reservation(
                 null,
-                new ReservationName(name),
                 ReservationDate.from(date),
+                member,
                 reservationTime,
                 theme
         );
