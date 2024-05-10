54e817d47a7166ab026e30faa0e2e24d2aa70337
Choo <choo000407@naver.com>
2024-05-10 20:00:09 +0900

    test: 로그인한 사용자 정보를 조회할 수 있다

--- src/test/java/roomescape/integration/AuthIntegrationTest.java
+++ src/test/java/roomescape/integration/AuthIntegrationTest.java
@@ -4,30 +4,22 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import roomescape.service.dto.LoginRequest;
 
-public class AuthIntegrationTest extends IntegrationTest {
+class AuthIntegrationTest extends IntegrationTest {
     @Nested
     @DisplayName("로그인 API")
     class Login {
         @Test
         void 이메일과_비밀번호로_로그인할_수_있다() {
-            String cookie = RestAssured.given().log().all()
+            RestAssured.given().log().all()
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
                     .body(new LoginRequest("admin@email.com", "password"))
                     .when().post("/login")
                     .then().log().all()
-                    .statusCode(200)
-                    .extract().header("Set-Cookie").split(";")[0];
-//            MemberResponse member = RestAssured.given().log().all()
-//                    .header("Cookie", cookie)
-//                    .accept(MediaType.APPLICATION_JSON_VALUE)
-//                    .when().get("/login/check")
-//                    .then().log().all()
-//                    .statusCode(HttpStatus.OK.value()).extract().as(MemberResponse.class);
-//
-//            assertThat(member.getEmail()).isEqualTo("admin@email.com");
+                    .statusCode(200);
         }
 
         @Test
@@ -38,6 +30,29 @@ public class AuthIntegrationTest extends IntegrationTest {
                     .when().post("/login")
                     .then().log().all()
                     .statusCode(401);
+        }
+    }
+
+    @Nested
+    @DisplayName("인증 정보 조회 API")
+    class LoginCheck {
+        @Test
+        void 로그인한_사용자_정보를_조회할_수_있다() {
+            String cookie = RestAssured.given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(new LoginRequest("admin@email.com", "password"))
+                    .when().post("/login")
+                    .then().log().all()
+                    .extract().header("Set-Cookie").split(";")[0];
+
+            LoginCheckResponse response = RestAssured.given().log().all()
+                    .header("Cookie", cookie)
+                    .accept(MediaType.APPLICATION_JSON_VALUE)
+                    .when().get("/login/check")
+                    .then().log().all()
+                    .statusCode(HttpStatus.OK.value()).extract().as(LoginCheckResponse.class);
+
+            assertThat(response.getName()).isEqualTo("어드민");
         }
     }
 }
