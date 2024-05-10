5a18fcc8200755d5dfe95c67f7b05773f8869fcf
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-10 21:57:30 +0900

    refactor: reservationResponse 반환값을 dto로 구성하도록 변경

--- src/main/java/roomescape/dto/response/ReservationResponse.java
+++ src/main/java/roomescape/dto/response/ReservationResponse.java
@@ -1,27 +1,24 @@
 package roomescape.dto.response;
 
-import roomescape.domain.member.Member;
 import roomescape.domain.reservation.Reservation;
-import roomescape.domain.reservation.ReservationTime;
-import roomescape.domain.reservation.Theme;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public record ReservationResponse(
         long id,
-        Theme theme,
-        Member member,
+        ThemeResponse theme,
+        MemberResponse member,
         LocalDate date,
-        ReservationTime time
+        ReservationTimeResponse time
 ) {
     public ReservationResponse(final Reservation reservation) {
         this(
                 reservation.getId(),
-                reservation.getTheme(),
-                reservation.getMember(),
+                new ThemeResponse(reservation.getTheme()),
+                new MemberResponse(reservation.getMember()),
                 reservation.getDate(),
-                reservation.getTime()
+                new ReservationTimeResponse(reservation.getTime())
         );
     }
 
