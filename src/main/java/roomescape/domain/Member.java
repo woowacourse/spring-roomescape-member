bb23b0b73c9052b986ba82aad1715b10b6cb7a2d
Choo <choo000407@naver.com>
2024-05-10 15:38:31 +0900

    feat: 사용자 도메인을 추가한다

---
+++ src/main/java/roomescape/domain/Member.java
@@ -0,0 +1,31 @@
+package roomescape.domain;
+
+public class Member {
+    private Long id;
+    private String name;
+    private String email; // TODO: 이메일 객체로 추출
+    private String password;
+
+    public Member(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
