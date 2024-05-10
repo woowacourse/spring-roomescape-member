6b5351c058197de46550d121479e8fb2a01ab244
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-05-10 11:25:07 +0900

    feat : MemberAuthService 기능 구현
    - findAll
    - findMemberById

--- src/main/java/roomescape/service/MemberAuthService.java
+++ src/main/java/roomescape/service/MemberAuthService.java
@@ -1,9 +1,11 @@
 package roomescape.service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import org.springframework.stereotype.Service;
 import roomescape.domain.Member;
 import roomescape.domain.MemberEmail;
+import roomescape.domain.MemberName;
 import roomescape.domain.MemberPassword;
 import roomescape.domain.MemberRepository;
 import roomescape.exception.AuthorizationException;
@@ -28,11 +30,14 @@ public class MemberAuthService {
             throw new EmailAlreadyExistsException();
         }
 
-        Member newMember = new Member(request.name(), new MemberEmail(request.email()),
-            new MemberPassword(request.password()));
+        Member newMember = new Member(
+            new MemberName(request.name()),
+            new MemberEmail(request.email()),
+            new MemberPassword(request.password())
+        );
 
         Member savedMember = memberRepository.save(newMember);
-        return new MemberAppResponse(savedMember.getId(), savedMember.getName());
+        return new MemberAppResponse(savedMember.getId(), savedMember.getName().getValue());
     }
 
     public String createExpireToken(String token) {
@@ -51,12 +56,23 @@ public class MemberAuthService {
         String payload = jwtProvider.getPayload(token);
 
         return memberRepository.findByEmail(payload)
-            .map(member -> new MemberAppResponse(member.getId(), member.getName()))
+            .map(member -> new MemberAppResponse(member.getId(), member.getName().getValue()))
+            .orElseThrow(() -> new NoSuchElementException("회원 정보를 찾지 못했습니다. 다시 로그인 해주세요."));
+    }
+
+    public MemberAppResponse findMemberById(Long id) {
+        return memberRepository.findById(id)
+            .map(member -> new MemberAppResponse(member.getId(), member.getName().getValue()))
             .orElseThrow(() -> new NoSuchElementException("회원 정보를 찾지 못했습니다."));
     }
 
+    public List<MemberAppResponse> findAll() {
+        return memberRepository.findAll().stream()
+            .map(member -> new MemberAppResponse(member.getId(), member.getName().getValue()))
+            .toList();
+    }
+
     private boolean isExistsMember(String email, String password) {
-        return memberRepository.findByEmailAndPassword(email,
-            password).isPresent();
+        return memberRepository.findByEmailAndPassword(email, password).isPresent();
     }
 }
