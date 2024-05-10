5340caf127f75811954feb77dac2b51de79306e1
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-10 22:17:14 +0900

    feat: RoleInterceptor 구현

--- src/main/java/roomescape/config/WebMvcConfig.java
+++ src/main/java/roomescape/config/WebMvcConfig.java
@@ -4,7 +4,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.interceptor.RoleInterceptor;
 import roomescape.resolver.LoginMemberArgumentResolver;
 import roomescape.service.AuthService;
 import roomescape.service.LoginMemberService;
@@ -23,5 +25,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver(authService, loginMemberService));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RoleInterceptor(authService, loginMemberService))
+                .addPathPatterns("/admin/**");
     }
 }
---
+++ src/main/java/roomescape/interceptor/RoleInterceptor.java
@@ -0,0 +1,36 @@
+package roomescape.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import roomescape.service.AuthService;
+import roomescape.service.LoginMemberService;
+import roomescape.service.dto.LoginMemberResponse;
+import roomescape.service.dto.TokenResponse;
+
+@Component
+public class RoleInterceptor implements HandlerInterceptor {
+    private final AuthService authService;
+    private final LoginMemberService loginMemberService;
+
+    @Autowired
+    public RoleInterceptor(AuthService authService, LoginMemberService loginMemberService) {
+        this.authService = authService;
+        this.loginMemberService = loginMemberService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        TokenResponse tokenResponse = authService.extractTokenByCookies(request);
+        String email = authService.extractEmailByToken(tokenResponse);
+        LoginMemberResponse loginMemberResponse = loginMemberService.findByEmail(email);
+        if (loginMemberResponse == null || !loginMemberResponse.role().equals("ADMIN")) {
+            response.setStatus(401);
+            return false;
+        }
+        return true;
+    }
+}
