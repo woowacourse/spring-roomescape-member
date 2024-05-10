59b189dfdc1e337a9a48480ac9b461ffddb37b6e
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-10 21:12:48 +0900

    feat: 비밀번호 VO 추가

--- src/main/java/roomescape/domain/member/Password.java
+++ src/main/java/roomescape/domain/member/Password.java
@@ -1,10 +1,20 @@
 package roomescape.domain.member;
 
 public class Password {
+    private static final int MIN_LENTH = 5;
+    private static final int MAX_LENTH = 10;
+
     private final String password;
 
     public Password(String password) {
+        validatePassword(password);
         this.password = password;
+    }
+
+    private void validatePassword(String password) {
+        if (password.isBlank() || password.length() < MIN_LENTH || password.length() > MAX_LENTH) {
+            throw new IllegalArgumentException("Password는 " + MIN_LENTH + "자 이상 " + MAX_LENTH + "자 이하여야 합니다.");
+        }
     }
 
     public String getPassword() {
