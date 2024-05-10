8a8f376945f711e3c41891aea7bfe1d0e90c967d
kelly6bf <kelly6bf@gmail.com>
2024-05-10 16:00:44 +0900

    remove: 불필요한 애노테이션 제거

--- src/main/java/roomescape/resolver/AuthenticatedMemberArgumentResolver.java
+++ src/main/java/roomescape/resolver/AuthenticatedMemberArgumentResolver.java
@@ -2,7 +2,6 @@ package roomescape.resolver;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -14,7 +13,6 @@ import roomescape.util.CookieUtil;
 
 import java.util.Objects;
 
-@Component
 public class AuthenticatedMemberArgumentResolver implements HandlerMethodArgumentResolver {
 
     private final AuthService authService;
