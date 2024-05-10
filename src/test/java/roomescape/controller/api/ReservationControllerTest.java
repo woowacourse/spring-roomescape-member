95e643406628b2998ef3d147ec2e47d9a2068675
juha <khabh@naver.com>
2024-05-10 12:12:42 +0900

    test: ReservationController 테스트에 로그인 토큰 적용

--- src/test/java/roomescape/controller/api/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/api/ReservationControllerTest.java
@@ -9,12 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
-import roomescape.model.Reservation;
-import roomescape.model.ReservationTime;
-import roomescape.model.Theme;
+import roomescape.dto.TokenRequest;
+import roomescape.model.*;
+import roomescape.repository.MemberRepository;
 import roomescape.repository.ReservationRepository;
 import roomescape.repository.ReservationTimeRepository;
 import roomescape.repository.ThemeRepository;
+import roomescape.service.AuthService;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -41,6 +42,12 @@ class ReservationControllerTest {
     @Autowired
     private ThemeRepository themeRepository;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private AuthService authService;
+
     @BeforeEach
     void initPort() {
         RestAssured.port = port;
@@ -49,10 +56,11 @@ class ReservationControllerTest {
     @DisplayName("예약 목록 조회")
     @Test
     void getReservations() {
+        final Member member = memberRepository.save(new Member("감자", Role.USER, "111@aaa.com", "abc1234"));
         final ReservationTime reservationTime = reservationTimeRepository.save(new ReservationTime(LocalTime.parse("09:00")));
         final Theme theme = themeRepository.save(new Theme("이름1", "설명1", "썸네일1"));
-        reservationRepository.save(new Reservation("이름1", LocalDate.now().plusMonths(2), reservationTime, theme));
-        reservationRepository.save(new Reservation("이름2", LocalDate.now().plusMonths(1), reservationTime, theme));
+        reservationRepository.save(new Reservation(member, LocalDate.now().plusMonths(2), reservationTime, theme));
+        reservationRepository.save(new Reservation(member, LocalDate.now().plusMonths(1), reservationTime, theme));
 
         RestAssured.given().log().all()
                 .when().get("/reservations")
@@ -61,22 +69,24 @@ class ReservationControllerTest {
                 .body("size()", is(2));
     }
 
-    @DisplayName("예약 추가 및 삭제")
+    @DisplayName("사용자 예약 추가 및 삭제")
     @TestFactory
-    Stream<DynamicTest> saveAndDeleteReservation() {
+    Stream<DynamicTest> userSaveAndDeleteReservation() {
+        memberRepository.save(new Member("감자", Role.USER, "111@aaa.com", "abc1234"));
+        final String token = authService.createToken(new TokenRequest("111@aaa.com", "abc1234")).accessToken();
         final ReservationTime reservationTime = reservationTimeRepository.save(new ReservationTime(LocalTime.parse("09:00")));
         final Theme theme = themeRepository.save(new Theme("이름1", "설명1", "썸네일1"));
 
         return Stream.of(
                 dynamicTest("예약을 추가한다", () -> {
                     final Map<String, Object> params = Map.of(
-                            "name", "브라운",
                             "date", LocalDate.now().plusMonths(1),
                             "timeId", reservationTime.getId(),
                             "themeId", theme.getId());
 
                     RestAssured.given().log().all()
                             .contentType(ContentType.JSON)
+                            .cookie("token", token)
                             .body(params)
                             .when().post("/reservations")
                             .then().log().all()
@@ -92,30 +102,60 @@ class ReservationControllerTest {
         );
     }
 
-    @DisplayName("예약자 이름이 비어 있는 예약 추가 시 BadRequest 반환")
+    @DisplayName("관리자 권한으로 예약 추가")
     @Test
-    void blankReservationName() {
+    void adminSaveReservation() {
+        memberRepository.save(new Member("고구마", Role.ADMIN, "111@aaa.com", "abc1234"));
+        final Member member = memberRepository.save(new Member("감자", Role.USER, "222@aaa.com", "abc1234"));
+        final String token = authService.createToken(new TokenRequest("111@aaa.com", "abc1234")).accessToken();
         final ReservationTime reservationTime = reservationTimeRepository.save(new ReservationTime(LocalTime.parse("09:00")));
         final Theme theme = themeRepository.save(new Theme("이름1", "설명1", "썸네일1"));
 
         final Map<String, Object> params = Map.of(
-                "name", "",
+                "memberId", member.getId(),
                 "date", LocalDate.now().plusMonths(1),
                 "timeId", reservationTime.getId(),
                 "themeId", theme.getId());
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
+                .cookie("token", token)
                 .body(params)
-                .when().post("/reservations")
+                .when().post("/admin/reservations")
                 .then().log().all()
-                .statusCode(400)
-                .body("name", equalTo("예약자 이름이 비어 있습니다."));
+                .statusCode(201)
+                .header("Location", "/reservations/1");
+    }
+
+    @DisplayName("일반 유저가 관리자용 예약 추가 호출 시 Forbidden 반환")
+    @Test
+    void forbiddenAdminSaveReservation() {
+        memberRepository.save(new Member("고구마", Role.ADMIN, "111@aaa.com", "abc1234"));
+        final Member member = memberRepository.save(new Member("감자", Role.USER, "222@aaa.com", "abc1234"));
+        final String token = authService.createToken(new TokenRequest("222@aaa.com", "abc1234")).accessToken();
+        final ReservationTime reservationTime = reservationTimeRepository.save(new ReservationTime(LocalTime.parse("09:00")));
+        final Theme theme = themeRepository.save(new Theme("이름1", "설명1", "썸네일1"));
+
+        final Map<String, Object> params = Map.of(
+                "memberId", member.getId(),
+                "date", LocalDate.now().plusMonths(1),
+                "timeId", reservationTime.getId(),
+                "themeId", theme.getId());
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie("token", token)
+                .body(params)
+                .when().post("/admin/reservations")
+                .then().log().all()
+                .statusCode(403);
     }
 
     @DisplayName("예약 날짜가 비어 있는 예약 추가 시 BadRequest 반환")
     @Test
     void blankReservationDate() {
+        memberRepository.save(new Member("감자", Role.USER, "111@aaa.com", "abc1234"));
+        final String token = authService.createToken(new TokenRequest("111@aaa.com", "abc1234")).accessToken();
         final ReservationTime reservationTime = reservationTimeRepository.save(new ReservationTime(LocalTime.parse("09:00")));
         final Theme theme = themeRepository.save(new Theme("이름1", "설명1", "썸네일1"));
 
@@ -127,6 +167,7 @@ class ReservationControllerTest {
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
+                .cookie("token", token)
                 .body(params)
                 .when().post("/reservations")
                 .then().log().all()
@@ -137,6 +178,8 @@ class ReservationControllerTest {
     @DisplayName("예약 테마 아아디가 비어 있는 예약 추가 시 BadRequest 반환")
     @Test
     void blankReservationThemeId() {
+        memberRepository.save(new Member("감자", Role.USER, "111@aaa.com", "abc1234"));
+        final String token = authService.createToken(new TokenRequest("111@aaa.com", "abc1234")).accessToken();
         final ReservationTime reservationTime = reservationTimeRepository.save(new ReservationTime(LocalTime.parse("09:00")));
 
         final Map<String, Object> params = Map.of(
@@ -147,6 +190,7 @@ class ReservationControllerTest {
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
+                .cookie("token", token)
                 .body(params)
                 .when().post("/reservations")
                 .then().log().all()
@@ -157,6 +201,8 @@ class ReservationControllerTest {
     @DisplayName("예약 시간 아아디가 비어 있는 예약 추가 시 BadRequest 반환")
     @Test
     void blankReservationTimeId() {
+        memberRepository.save(new Member("감자", Role.USER, "111@aaa.com", "abc1234"));
+        final String token = authService.createToken(new TokenRequest("111@aaa.com", "abc1234")).accessToken();
         final Theme theme = themeRepository.save(new Theme("이름1", "설명1", "썸네일1"));
 
         final Map<String, Object> params = Map.of(
@@ -167,6 +213,7 @@ class ReservationControllerTest {
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
+                .cookie("token", token)
                 .body(params)
                 .when().post("/reservations")
                 .then().log().all()
@@ -178,6 +225,8 @@ class ReservationControllerTest {
     @ParameterizedTest
     @ValueSource(strings = {"2099.22.11", "2022", "abc"})
     void invalidDateFormat(final String date) {
+        memberRepository.save(new Member("감자", Role.USER, "111@aaa.com", "abc1234"));
+        final String token = authService.createToken(new TokenRequest("111@aaa.com", "abc1234")).accessToken();
         final ReservationTime reservationTime = reservationTimeRepository.save(new ReservationTime(LocalTime.parse("09:00")));
         final Theme theme = themeRepository.save(new Theme("이름1", "설명1", "썸네일1"));
         final Map<String, Object> params = Map.of(
@@ -188,6 +237,7 @@ class ReservationControllerTest {
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
+                .cookie("token", token)
                 .body(params)
                 .when().post("/reservations")
                 .then().log().all()
