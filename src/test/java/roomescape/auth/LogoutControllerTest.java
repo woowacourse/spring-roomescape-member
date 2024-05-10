9765d14b9413ea026a817cb820572a5065f03107
BurningFalls <sherlocky31415@gmail.com>
2024-05-11 00:47:40 +0900

    test: LogoutController 테스트 구현

---
+++ src/test/java/roomescape/auth/LogoutControllerTest.java
@@ -0,0 +1,37 @@
+package roomescape.auth;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class LogoutControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    @DisplayName("로그아웃 하면, 토큰 값이 사라진다.")
+    void logoutTest() {
+        RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/logout")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .log().all()
+                .cookie("token", "");
+    }
+}
