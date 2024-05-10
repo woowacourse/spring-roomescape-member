d86b9b12c341ddf50a5123685a566e1cdc5dd5b5
kelly6bf <kelly6bf@gmail.com>
2024-05-11 03:46:48 +0900

    remove: AuthenticationToken 불필요한 메서드 제거

--- src/main/java/roomescape/auth/token/AuthenticationToken.java
+++ src/main/java/roomescape/auth/token/AuthenticationToken.java
@@ -15,15 +15,6 @@ public class AuthenticationToken {
         this.value = value;
     }
 
-    public boolean validate() {
-        try {
-            getClaims();
-            return true;
-        } catch (final Exception e) {
-            return false;
-        }
-    }
-
     public Claims getClaims() {
         return Jwts.parser()
                 .verifyWith(key)
