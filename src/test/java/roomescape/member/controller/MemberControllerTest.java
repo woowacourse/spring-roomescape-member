7c83db1bdde5a74fe4582058b5948df27032e4ed
BurningFalls <sherlocky31415@gmail.com>
2024-05-11 01:31:49 +0900

    test: MemberController 테스트 구현

---
+++ src/test/java/roomescape/member/controller/MemberControllerTest.java
@@ -0,0 +1,38 @@
+package roomescape.member.controller;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class MemberControllerTest {
+    private static final int MEMBER_SIZE = 5;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    void findAllTest() {
+        RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/members")
+                .then()
+                .log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body("size()", is(MEMBER_SIZE));
+    }
+}
