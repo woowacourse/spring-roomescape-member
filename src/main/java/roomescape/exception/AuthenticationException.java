9f81319e2176323d7e3214dffc6b0fb3075e7d72
JUHA <84626225+khabh@users.noreply.github.com>
2024-05-11 16:37:51 +0900

    refactor: 사용되지 않는 메시지 삭제

--- src/main/java/roomescape/exception/AuthenticationException.java
+++ src/main/java/roomescape/exception/AuthenticationException.java
@@ -2,8 +2,6 @@ package roomescape.exception;
 
 public class AuthenticationException extends RuntimeException {
 
-    private static final String MESSAGE = "권한이 인증되지 않았습니다.";
-
     public AuthenticationException(final String message) {
         super(message);
     }
