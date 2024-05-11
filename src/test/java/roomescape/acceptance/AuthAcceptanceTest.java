2fe202b95da22ef00576133c4a2d5fce8e856b1a
donghoony <aru0504@naver.com>
2024-05-11 18:23:14 +0900

    refactor: 로그인 인수 테스트 시, 사용자 시나리오 입장에서 검증

--- src/test/java/roomescape/acceptance/AuthAcceptanceTest.java
+++ src/test/java/roomescape/acceptance/AuthAcceptanceTest.java
@@ -1,23 +1,13 @@
 package roomescape.acceptance;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.Jwts;
 import io.restassured.RestAssured;
-import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import roomescape.application.member.dto.request.MemberLoginRequest;
 import roomescape.application.member.dto.request.MemberRegisterRequest;
 import roomescape.application.member.dto.response.MemberResponse;
 
 class AuthAcceptanceTest extends AcceptanceTest {
-
-    @Value("${jwt.secret}")
-    private String secret;
 
     @Test
     @DisplayName("사용자가 로그인한다.")
@@ -28,19 +18,11 @@ class AuthAcceptanceTest extends AcceptanceTest {
                 .as(MemberResponse.class)
                 .id();
 
-        String token = RestAssured.given().log().all()
+        RestAssured.given().log().all()
                 .contentType("application/json")
                 .body(new MemberLoginRequest("aru@test.com", "12341234"))
                 .when().post("/login")
                 .then().log().all()
-                .statusCode(204)
-                .extract()
-                .cookie("token");
-
-        JwtParser build = Jwts.parserBuilder()
-                .setSigningKey(secret.getBytes(StandardCharsets.UTF_8))
-                .build();
-        Claims claims = build.parseClaimsJws(token).getBody();
-        assertThat(claims.get("memberId", Long.class)).isEqualTo(memberId);
+                .statusCode(204);
     }
 }
