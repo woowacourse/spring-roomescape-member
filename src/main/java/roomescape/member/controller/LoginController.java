30e20adb21674926fa47efca18cb8fe432f13cdc
tackyu <tack970913@gmail.com>
2024-05-12 04:15:47 +0900

    feat: LoginController 추가

---
+++ src/main/java/roomescape/member/controller/LoginController.java
@@ -0,0 +1,52 @@
+package roomescape.member.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import roomescape.member.AuthService;
+import roomescape.member.request.LoginRequest;
+import roomescape.member.response.MemberResponse;
+
+@RequestMapping("/login")
+@RestController
+public class LoginController {
+
+    private final AuthService authService;
+
+    public LoginController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> login(@RequestBody LoginRequest request, HttpServletResponse response) {
+        String token = authService.createToken(request);
+        Cookie cookie = new Cookie("token", token);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<MemberResponse> checkLogin(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        String token = extractTokenFromCookie(cookies);
+        MemberResponse response = authService.findMemberNameByToken(token);
+        return ResponseEntity.ok().body(response);
+    }
+
+    private String extractTokenFromCookie(Cookie[] cookies) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                return cookie.getValue();
+            }
+        }
+        return "";
+    }
+
+}
