7fdf6c76cb18938ce9c995dc55a1a1058a4edf01
tackyu <tack970913@gmail.com>
2024-05-12 04:03:29 +0900

    feat: TokenProvider 추가

---
+++ src/main/java/roomescape/member/JwtTokenProvider.java
@@ -0,0 +1,24 @@
+package roomescape.member;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import roomescape.member.domain.Member;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+
+    public String createToken(Member member) {
+        return Jwts.builder()
+                .setSubject(String.valueOf(member.id()))
+                .claim("email", member.email())
+                .claim("name", member.name())
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .compact();
+    }
+}
