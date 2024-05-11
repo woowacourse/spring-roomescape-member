736ca51c7403bf3cf5bff2be2328817030004f10
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-05-12 06:46:34 +0900

    refactor: 패키지 분리

--- src/test/java/roomescape/domain/reservation/ReservationTest.java
+++ src/test/java/roomescape/domain/reservation/ReservationTest.java
@@ -1,4 +1,4 @@
-package roomescape.domain;
+package roomescape.domain.reservation;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
--- src/test/java/roomescape/domain/theme/ThemeAggregationPeriodTest.java
+++ src/test/java/roomescape/domain/theme/ThemeAggregationPeriodTest.java
@@ -1,4 +1,4 @@
-package roomescape.domain;
+package roomescape.domain.theme;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
