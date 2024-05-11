c7d984028227c8a2ebfa76801d4677ab0813860a
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-11 17:26:32 +0900

    feat(Member): Member 생성 시 검증 추가

--- src/main/java/roomescape/member/domain/Member.java
+++ src/main/java/roomescape/member/domain/Member.java
@@ -10,11 +10,40 @@ public class Member {
     private final String password;
 
     public Member(final Long id, final String name, final MemberRole memberRole, final String email, final String password) {
+        validateNameIsNull(name);
+        validateRoleIsNull(memberRole);
+        validateEmailIsNull(email);
+        validatePasswordIsNull(password);
+
         this.id = id;
         this.name = name;
         this.memberRole = memberRole;
         this.email = email;
         this.password = password;
+    }
+
+    private void validateNameIsNull(final String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("회원 생성 시 이름은 필수입니다.");
+        }
+    }
+
+    private void validateRoleIsNull(final MemberRole memberRole) {
+        if (memberRole == null) {
+            throw new IllegalArgumentException("회원 생성 시 회원 권한 지정은 필수입니다.");
+        }
+    }
+
+    private void validateEmailIsNull(final String email) {
+        if (email == null) {
+            throw new IllegalArgumentException("회원 생성 시 이메일 필수입니다.");
+        }
+    }
+
+    private void validatePasswordIsNull(final String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("회원 생성 시 비밀번호는 필수입니다.");
+        }
     }
 
     public static Member of(final Long id, final Member member) {
