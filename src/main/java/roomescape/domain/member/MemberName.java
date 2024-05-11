1a5ea09bf185b11857f9acbd2ce69b9537d43697
3juhwan <13selfesteem91@naver.com>
2024-05-12 02:29:36 +0900

    feat: 사용자 이름 도메인 정책 추가

--- src/main/java/roomescape/domain/member/MemberName.java
+++ src/main/java/roomescape/domain/member/MemberName.java
@@ -1,4 +1,24 @@
 package roomescape.domain.member;
 
 public record MemberName(String value) {
+    private static final int NAME_LENGTH_MIN = 2;
+    private static final int NAME_LENGTH_MAX = 10;
+
+    public MemberName {
+        validateNotNull(value);
+        validateNameLength(value);
+    }
+
+    private void validateNotNull(String value) {
+        if (value == null) {
+            throw new IllegalArgumentException("이름은 null이 될 수 없습니다.");
+        }
+    }
+
+    private void validateNameLength(String value) {
+        if (value.length() < NAME_LENGTH_MIN || NAME_LENGTH_MAX < value.length()) {
+            throw new IllegalArgumentException(String.format("사용자 이름은 %d자 이상 %d자 이하여야 합니다. (현재 입력한 이름 길이: %d자)",
+                    NAME_LENGTH_MIN, NAME_LENGTH_MAX, value.length()));
+        }
+    }
 }
