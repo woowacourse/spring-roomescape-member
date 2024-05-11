f9f57df363871efc59eee01d5cb846b90074bd71
Nam Gi Beom <rlqja0523@naver.com>
2024-05-12 00:39:48 +0900

    chore: member sql 추가

---
+++ src/test/resources/member.sql
@@ -0,0 +1,4 @@
+INSERT INTO member(id, name, email, password, role)
+VALUES (1, '프린', 'prin@gmail.com', '1q2w3e4r!@', 'ADMIN'),
+       (2, '레모네', 'remone@gmail.com', '1q2w3e4r!@', 'MEMBER'),
+       (3, '오리', 'duck@gmail.com', '1q2w3e4r!@', 'MEMBER');
