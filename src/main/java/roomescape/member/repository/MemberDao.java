374a99498594228b67bbd0c635a837512905a120
tackyu <tack970913@gmail.com>
2024-05-12 04:14:31 +0900

    feat: MemberDao 추가

---
+++ src/main/java/roomescape/member/repository/MemberDao.java
@@ -0,0 +1,41 @@
+package roomescape.member.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import roomescape.member.domain.Member;
+
+@Repository
+public class MemberDao {
+    private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<Member> memberRowMapper = (resultSet, __) -> new Member(
+            resultSet.getLong("id"),
+            resultSet.getString("name"),
+            resultSet.getString("email"),
+            resultSet.getString("password")
+    );
+
+    public MemberDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public boolean isMember(String email, String password) {
+        String query = "SELECT COUNT(*) FROM MEMBER WHERE EMAIL = ? AND PASSWORD = ?";
+        Integer count = jdbcTemplate.queryForObject(query, Integer.class, email, password);
+        return count != null && count == 1;
+    }
+
+    public Member findMemberByEmail(String email) {
+        String query = "SELECT " +
+                "ID, NAME, EMAIL, PASSWORD " +
+                "FROM MEMBER " +
+                "WHERE email = ?";
+        return jdbcTemplate.queryForObject(query, memberRowMapper, email);
+    }
+
+    public String findNameById(Long memberId) {
+        String query = "SELECT NAME FROM MEMBER WHERE ID = ?";
+        return jdbcTemplate.queryForObject(query, String.class, memberId);
+    }
+}
