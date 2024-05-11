3e8bff2c86527b6333d8fbaa3e7c853635471b98
Choo <choo000407@naver.com>
2024-05-11 23:08:28 +0900

    fix: 클라이언트 코드를 명세에 맞게 수정

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -38,11 +38,6 @@ function renderTheme(themes) {
     themes.forEach(theme => {
         const name = theme.name
         const themeId = theme.id
-        /*
-        [3단계] 사용자 예약 - 테마 목록 조회 API 호출 후 렌더링
-              response 명세에 맞춰 createSlot 함수 호출 시 값 설정
-              createSlot('theme', theme name, theme id) 형태로 호출
-        */
         themeSlots.appendChild(createSlot('theme', name, themeId));
     });
 }
@@ -87,10 +82,6 @@ function checkDateAndTheme() {
 }
 
 function fetchAvailableTimes(date, themeId) {
-    /*
-    [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출
-          요청 포맷에 맞게 설정
-    */
     const queryString = new URLSearchParams({'date': date, 'theme-id': themeId});
     fetch('/times/available?' + queryString, { // 예약 가능 시간 조회 API endpoint
         method: 'GET',
@@ -117,10 +108,6 @@ function renderAvailableTimes(times) {
         return;
     }
     times.forEach(time => {
-        /*
-        [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출 후 렌더링
-              response 명세에 맞춰 createSlot 함수 호출 시 값 설정
-        */
         const startAt = time.startAt;
         const timeId = time.id;
         const alreadyBooked = time.alreadyBooked;
@@ -154,20 +141,12 @@ function onReservationButtonClick() {
     const selectedDate = document.getElementById("datepicker").value;
     const selectedThemeId = document.querySelector('.theme-slot.active')?.getAttribute('data-theme-id');
     const selectedTimeId = document.querySelector('.time-slot.active')?.getAttribute('data-time-id');
-    const name = document.getElementById('user-name').value;
 
     if (selectedDate && selectedThemeId && selectedTimeId) {
-
-        /*
-              [3단계] 사용자 예약 - 예약 요청 API 호출
-              TODO: [5단계] 예약 생성 기능 변경 - 사용자
-                    request 명세에 맞게 설정
-        */
         const reservationData = {
             date: selectedDate,
             themeId: selectedThemeId,
-            timeId: selectedTimeId,
-            name: name
+            timeId: selectedTimeId
         };
 
         fetch('/reservations', {
