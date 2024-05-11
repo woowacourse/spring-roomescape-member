0dfa85f152ae59ba91fdace21a7361233dc2a45b
pricelees <priceelees@gmail.com>
2024-05-12 00:58:40 +0900

    feat: 쿠키를 이용하여 멤버 객체를 생성하는 ArgumentResolver 구현

--- src/main/java/roomescape/auth/controller/AuthController.java
+++ src/main/java/roomescape/auth/controller/AuthController.java
@@ -1,55 +1,38 @@
 package roomescape.auth.controller;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import java.util.Arrays;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import roomescape.auth.dto.LoginRequest;
 import roomescape.auth.dto.TokenResponse;
+import roomescape.auth.infrastructure.TokenCookieManager;
+import roomescape.auth.resolver.LoginMember;
 import roomescape.auth.service.AuthService;
 import roomescape.member.dto.MemberResponse;
 
 @Controller
-@RequestMapping("/login")
-public class LoginController {
+public class AuthController {
 
     private final AuthService authService;
 
-    public LoginController(AuthService authService) {
+    public AuthController(AuthService authService) {
         this.authService = authService;
     }
 
-    @PostMapping
+    @PostMapping("/login")
     public ResponseEntity<Void> login(@RequestBody @Valid LoginRequest loginRequest, HttpServletResponse response) {
         TokenResponse tokenResponse = authService.createToken(loginRequest);
-
-        Cookie cookie = new Cookie("token", tokenResponse.accessToken());
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        response.addCookie(cookie);
+        TokenCookieManager.setTokenCookie(tokenResponse.accessToken(), response);
 
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/check")
-    public ResponseEntity<MemberResponse> findInfo(HttpServletRequest request) {
-        String token = getToken(request);
-        MemberResponse member = authService.findMemberByToken(token);
+    @GetMapping("/login/check")
+    public ResponseEntity<MemberResponse> findInfo(@LoginMember MemberResponse member) {
         return ResponseEntity.ok().body(member);
-    }
-
-    private String getToken(HttpServletRequest request) {
-        return Arrays.stream(request.getCookies())
-                .filter(cookie -> "token".equals(cookie.getName()))
-                .map(Cookie::getValue)
-                .findFirst()
-                .orElse(null);
     }
 }
---
+++ src/main/java/roomescape/auth/resolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,35 @@
+package roomescape.auth.resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import roomescape.auth.infrastructure.TokenCookieManager;
+import roomescape.auth.service.AuthService;
+
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final AuthService authService;
+
+    public LoginMemberArgumentResolver(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginMember.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String token = TokenCookieManager.getToken(request);
+
+        return authService.findMemberByToken(token);
+    }
+}
