2d08378eaade3b9431b4e48a6dbe17e1a519cb71
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-11 11:40:50 +0900

    refactor(CreateReservationResponse): dto에 이름만이 아닌, CreateMemberOfReservationResponse를 담아 전달
    - 불필요한 TODO 제거

--- src/main/java/roomescape/reservation/dto/response/CreateReservationResponse.java
+++ src/main/java/roomescape/reservation/dto/response/CreateReservationResponse.java
@@ -1,13 +1,14 @@
 package roomescape.reservation.dto.response;
 
 import java.time.LocalDate;
+import roomescape.member.dto.response.CreateMemberOfReservationResponse;
 import roomescape.reservation.model.Reservation;
 
-public record CreateReservationResponse(Long id, String name, LocalDate date, CreateTimeOfReservationsResponse time, CreateThemeOfReservationResponse theme) {
+public record CreateReservationResponse(Long id, CreateMemberOfReservationResponse name, LocalDate date, CreateTimeOfReservationsResponse time, CreateThemeOfReservationResponse theme) {
     public static CreateReservationResponse from(final Reservation reservation) {
         return new CreateReservationResponse(
                 reservation.getId(),
-                reservation.getMember().getName(), // TODO: CreateTimeOfReservationsResponse 처럼 변경
+                CreateMemberOfReservationResponse.from(reservation.getMember()),
                 reservation.getDate(),
                 CreateTimeOfReservationsResponse.from(reservation.getReservationTime()),
                 CreateThemeOfReservationResponse.from(reservation.getTheme())
