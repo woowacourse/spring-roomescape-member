6c0e374dc4df9910c44455d60951835c2e3e6282
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-12 01:03:32 +0900

    refactor: 부적적한 토큰값 검증 기능 구현

--- src/main/java/roomescape/infrastructure/auth/Token.java
+++ src/main/java/roomescape/infrastructure/auth/Token.java
@@ -1,11 +1,17 @@
 package roomescape.infrastructure.auth;
 
-// TODO null check, empty check등 조금 더 의미부여하기
 public class Token {
     private final String token;
 
     public Token(String token) {
+        validateToken(token);
         this.token = token;
+    }
+
+    private void validateToken(String token) {
+        if (token == null || token.isEmpty()) {
+            throw new IllegalArgumentException("부적절한 토큰 값입니다.");
+        }
     }
 
     public String getToken() {
