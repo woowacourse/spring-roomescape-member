e70e58869434a683ebe99bf6e5c9a933e5ad5e5d
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-11 09:09:55 +0900

    test(MemberDao): 사용자를 추가하고 읽을 수 있다

---
+++ src/test/java/roomescape/dao/MemberDaoTest.java
@@ -0,0 +1,59 @@
+package roomescape.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+import roomescape.domain.Member;
+
+@JdbcTest
+@Sql(scripts = "/truncate.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+class MemberDaoTest {
+    private final JdbcTemplate jdbcTemplate;
+    private final MemberDao memberDao;
+
+    @Autowired
+    MemberDaoTest(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.memberDao = new MemberDao(jdbcTemplate);
+    }
+
+    @DisplayName("id를 통해 사용자를 찾을 수 있다.")
+    @Test
+    void readMemberById() {
+        jdbcTemplate.update("INSERT INTO member(name, email, password) VALUES ('켬미', 'aaa@naver.com', '1111')");
+
+        Member member = memberDao.readMemberById(1L)
+                .orElseThrow(() -> new IllegalArgumentException("해당 id에 멤버는 존재하지 않습니다."));
+
+        Member expected = new Member(1L, "켬미", "aaa@naver.com", "1111");
+        assertThat(member).isEqualTo(expected);
+    }
+
+    @DisplayName("이메일과 비밀번호를 통해 사용자를 찾을 수 있다.")
+    @Test
+    void readMemberByEmailAndPassword() {
+        jdbcTemplate.update("INSERT INTO member(name, email, password) VALUES ('켬미', 'aaa@naver.com', '1111')");
+
+        Member member = memberDao.readMemberByEmailAndPassword("aaa@naver.com", "1111")
+                .orElseThrow(() -> new IllegalArgumentException("해당 이메일과 비밀번호인 멤버는 존재하지 않습니다."));
+
+        Member expected = new Member(1L, "켬미", "aaa@naver.com", "1111");
+        assertThat(member).isEqualTo(expected);
+    }
+
+    @DisplayName("사용자를 추가할 수 있다.")
+    @Test
+    void createMember() {
+        memberDao.createMember(new Member("켬미", "aaa@naver.com", "1111"));
+        Integer count = jdbcTemplate.queryForObject("SELECT count(1) from member", Integer.class);
+
+        assertThat(count).isEqualTo(1);
+    }
+}
