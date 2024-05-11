ceda1dcee84c882d3952eb7dc19f127f6d3ee2ce
3juhwan <13selfesteem91@naver.com>
2024-05-12 02:17:00 +0900

    feat: 메일 주소 도메인 정책 추가

--- src/main/java/roomescape/domain/member/Email.java
+++ src/main/java/roomescape/domain/member/Email.java
@@ -1,4 +1,27 @@
 package roomescape.domain.member;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public record Email(String value) {
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
+
+    public Email {
+        validateNotNull(value);
+        validateEmailFormat(value);
+    }
+
+    private void validateNotNull(String value) {
+        if (value == null) {
+            throw new IllegalArgumentException("메일 주소는 null이 될 수 없습니다.");
+        }
+    }
+
+    private void validateEmailFormat(String value) {
+        Matcher matcher = EMAIL_PATTERN.matcher(value);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(
+                    String.format("메일 주소 형식이 올바르지 않습니다. (현재 입력한 메일 주소: %s)", value));
+        }
+    }
 }
