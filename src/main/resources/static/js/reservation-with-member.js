5f2827d9ce7de9d237b3dbfe643a12de2953b5fa
le2sky <leehaneul990623@gmail.com>
2024-05-11 15:04:05 +0900

    fix: 클라이언트 예약 쿼리 요청 코드 수정

--- src/main/resources/static/js/reservation-with-member.js
+++ src/main/resources/static/js/reservation-with-member.js
@@ -192,11 +192,12 @@ function applyFilter(event) {
   const dateFrom = document.getElementById('date-from').value;
   const dateTo = document.getElementById('date-to').value;
 
-  /*
-  TODO: [6단계] 예약 검색 - 조건에 따른 예약 조회 API 호출
-        요청 포맷에 맞게 설정
-  */
-  fetch('/', { // 예약 검색 API 호출
+  const themeIdParam = themeId === "" ? themeId : `&themeId=${themeId}`
+  const memberIdParam = memberId === "" ? memberId : `&memberId=${memberId}`
+  const dateFromParam = dateFrom === "" ? dateFrom : `&fromDate=${dateFrom}`
+  const dateToParam = dateTo === "" ? dateTo : `&endDate=${dateTo}`
+
+  fetch(`/reservations?${themeIdParam + memberIdParam + dateFromParam + dateToParam}`, { // 예약 검색 API 호출
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
