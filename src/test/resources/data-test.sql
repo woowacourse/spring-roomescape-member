eba652e45bda6736ac3120bd34dd8c3984510b99
Eugene Jang <kyunellroll@gmail.com>
2024-05-12 02:58:53 +0900

    build(schema): 요구사항 추가에 따른 스키마 변경

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -10,16 +10,17 @@ VALUES ('15:40'),
        ('13:40'),
        ('17:40');
 
+INSERT INTO member (name, email, password)
+VALUES ('어드민', 'polla@gmail.com', 'pollari99'),
+       ('어드민', 'polla@naver.com', 'pollari999');
+
+
 INSERT INTO theme (name, description, thumbnail)
 VALUES ('polla', '폴라 방탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
        ('dobby', '도비 방탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
        ('pobi', '포비 방탈출', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
-INSERT INTO reservation (name, date, time_id, theme_id)
-VALUES ('폴라', '2024-04-30', 1, 1),
-       ('구구', '2025-05-01', 2, 1);
-
-INSERT INTO member (name, email, password)
-VALUES ('어드민', 'polla@gmail.com', 'pollari99');
-
+INSERT INTO reservation (date, time_id, theme_id, member_id)
+VALUES ('2024-04-30', 1, 1, 1),
+       ('2025-05-01', 2, 1, 2);
 
