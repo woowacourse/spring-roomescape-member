bf7289ac4beae84801c4db2a0e0d3396d7d63a2b
Choo <choo000407@naver.com>
2024-05-11 23:33:28 +0900

    test: 사용자 목록을 조회할 수 있다

---
+++ src/test/java/roomescape/integration/MemberControllerTest.java
@@ -0,0 +1,23 @@
+package roomescape.integration;
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class MemberControllerTest extends IntegrationTest {
+    @Nested
+    @DisplayName("사용자 목록 조회 API")
+    class FindAllMember {
+        @Test
+        void 사용자_목록을_조회할_수_있다() {
+            RestAssured.given().log().all()
+                    .when().get("/members")
+                    .then().log().all()
+                    .statusCode(200)
+                    .body("size()", is(1));
+        }
+    }
+}
