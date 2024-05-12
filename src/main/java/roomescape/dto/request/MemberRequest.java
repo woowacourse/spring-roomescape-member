0dc3563bd9faf4bb29a1e9539acdba9921e4733c
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-13 03:33:31 +0900

    refactor : 불필요한 메서드 삭제

--- src/main/java/roomescape/dto/request/MemberRequest.java
+++ src/main/java/roomescape/dto/request/MemberRequest.java
@@ -14,8 +14,4 @@ public record MemberRequest(
     public MemberRequest(Member member) {
         this(member.getId(), member.getName(), member.getEmail(), member.getPassword(), member.getRole());
     }
-
-    public Member toEntity() {
-        return new Member(id, name, email, password, role);
-    }
 }
