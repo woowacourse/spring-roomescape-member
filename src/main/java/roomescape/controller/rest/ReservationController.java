6e61c82dc25d1f2732f208489fff5ac7fffd8419
Zeus6768 <akaps6768@gmail.com>
2024-05-13 04:06:23 +0900

    refactor: 예약 생성 로직 변경

--- src/main/java/roomescape/controller/rest/ReservationController.java
+++ src/main/java/roomescape/controller/rest/ReservationController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import roomescape.controller.resolver.AccessToken;
 import roomescape.controller.rest.request.ReservationRequest;
+import roomescape.domain.Member;
 import roomescape.domain.Reservation;
 import roomescape.service.ReservationService;
 
@@ -37,9 +39,12 @@ public class ReservationController {
         return ResponseEntity.ok(reservation);
     }
 
-    @PostMapping("/reservations")
-    public ResponseEntity<Reservation> createByMember(@RequestBody ReservationRequest request) {
-        Reservation createdReservation = reservationService.createByMember(request);
+    @PostMapping("/reservations/member")
+    public ResponseEntity<Reservation> createByMember(
+            @AccessToken Member member,
+            @RequestBody ReservationRequest request
+    ) {
+        Reservation createdReservation = reservationService.createByMember(member, request);
         return ResponseEntity.created(URI.create("/reservations/" + createdReservation.id())).body(createdReservation);
     }
 
--- src/main/java/roomescape/controller/rest/request/ReservationRequest.java
+++ src/main/java/roomescape/controller/rest/request/ReservationRequest.java
@@ -2,5 +2,5 @@ package roomescape.controller.rest.request;
 
 import java.time.LocalDate;
 
-public record ReservationRequest(LocalDate date, long memberId, long timeId, long themeId) {
+public record ReservationRequest(LocalDate date, Long memberId, Long timeId, Long themeId) {
 }
