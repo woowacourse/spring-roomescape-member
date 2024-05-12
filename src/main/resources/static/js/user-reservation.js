dbe3cc8b7effb06a7e68f0662d72f96f8e4d57ad
takoyakimchi <c4nyou@gmail.com>
2024-05-12 14:43:34 +0900

    chore: 불필요한 TODO 제거

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -144,12 +144,6 @@ function onReservationButtonClick() {
       '.time-slot.active')?.getAttribute('data-time-id');
 
   if (selectedDate && selectedThemeId && selectedTimeId) {
-
-    /*
-    TODO:
-          [5단계] 예약 생성 기능 변경 - 사용자
-          request 명세에 맞게 설정
-    */
     const reservationData = {
       date: selectedDate,
       themeId: selectedThemeId,
