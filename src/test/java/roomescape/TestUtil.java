b37b542d7ee60dd48e2d50de09b32295c1982eeb
pricelees <priceelees@gmail.com>
2024-05-13 00:03:15 +0900

    refactor: TestUtil 로그인 정보 상수화

--- src/test/java/roomescape/TestUtil.java
+++ src/test/java/roomescape/TestUtil.java
@@ -1,16 +1,19 @@
 package roomescape;
 
 import io.restassured.RestAssured;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import roomescape.dto.auth.LoginRequest;
 
 @Sql(scripts = {"/test_schema.sql", "/test_admin_member.sql"})
 public class TestUtil {
 
+    private static final LoginRequest admin = new LoginRequest("admin@email.com", "admin_password");
+
     public static String getAdminUserToken() {
         return RestAssured.given().log().all()
-                .contentType("application/json")
-                .body(new LoginRequest("admin@email.com", "admin_password"))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(admin)
                 .when().post("/login")
                 .then().log().all()
                 .extract().cookie("token");
