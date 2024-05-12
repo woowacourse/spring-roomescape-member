8f87a7dabb7ed8459611437f9e7464a4e9d582e4
eunjungL <dldmswjd322@gmail.com>
2024-05-12 16:14:55 +0900

    test: MemberTest 모든 필드 검증으로 변경

--- src/test/java/roomescape/member/domain/MemberTest.java
+++ src/test/java/roomescape/member/domain/MemberTest.java
@@ -1,10 +1,9 @@
 package roomescape.member.domain;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("사용자")
 class MemberTest {
@@ -14,7 +13,15 @@ class MemberTest {
     @ParameterizedTest
     void validateNullOrEmptyField(String blank) {
         // when & then
-        assertThatThrownBy(() -> new Member(blank, "email", "password"))
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        softAssertions.assertThatThrownBy(() -> new Member(blank, "email@gmail.com", "password"))
                 .isInstanceOf(IllegalArgumentException.class);
+        softAssertions.assertThatThrownBy(() -> new Member("name", blank, "password"))
+                .isInstanceOf(IllegalArgumentException.class);
+        softAssertions.assertThatThrownBy(() -> new Member("name", "email@gmail.com", blank))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        softAssertions.assertAll();
     }
 }
