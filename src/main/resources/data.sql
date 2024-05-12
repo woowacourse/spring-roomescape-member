64d253dbda0471c88860d297428013683593f827
pricelees <priceelees@gmail.com>
2024-05-12 11:44:03 +0900

    refactor: member 테이블에 role 추가 및 초기 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,9 +12,10 @@ INSERT INTO theme(name, description, thumbnail) VALUES('test3', 'desc3', 'https:
 INSERT INTO theme(name, description, thumbnail) VALUES('test4', 'desc4', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
 -- 테스트용 유저 정보 등록
-INSERT INTO member(name, email, password) VALUES ('test_member1', 'test1@email.com', 'test_password1');
-INSERT INTO member(name, email, password) VALUES ('test_member2', 'test2@email.com', 'test_password2');
-INSERT INTO member(name, email, password) VALUES ('test_member3', 'test3@email.com', 'test_password3');
+INSERT INTO member(name, email, password, role) VALUES ('test_member1', 'test1@email.com', 'test_password1', 'user');
+INSERT INTO member(name, email, password, role) VALUES ('test_member2', 'test2@email.com', 'test_password2', 'user');
+INSERT INTO member(name, email, password, role) VALUES ('test_member3', 'test3@email.com', 'test_password3', 'user');
+INSERT INTO member(name, email, password, role) VALUES ('admin_member', 'admin@email.com', 'admin_password', 'admin');
 
 -- 가장 많이 예약된 테마가 3 -> 2 -> 1 -> 4 순으로 되도록 설정
 -- 1번 테마
