decb696f88bd5ee9dd36d59326de20ff542be97a
leetaehoon <xogns1514@gmail.com>
2024-05-12 17:36:02 +0900

    refactor(Role): enum type과 같은 value 제거

--- src/main/java/roomescape/domain/Role.java
+++ src/main/java/roomescape/domain/Role.java
@@ -4,23 +4,14 @@ import java.util.Arrays;
 
 public enum Role {
 
-    ADMIN("ADMIN"),
-    USER("USER");
-
-    private final String name;
-
-    Role(String name) {
-        this.name = name;
-    }
+    ADMIN,
+    USER;
 
     public static Role of(String roleName) {
         return Arrays.stream(values())
-                .filter(role -> role.name.equalsIgnoreCase(roleName))
+                .filter(role -> role.name().equalsIgnoreCase(roleName))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("역할을 찾을 수 없습니다."));
     }
 
-    public String getName() {
-        return name;
-    }
 }
