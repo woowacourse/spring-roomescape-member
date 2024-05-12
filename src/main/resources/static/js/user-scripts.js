c073e28c8c682ce709ddb0b17cc0af96044ac8dc
takoyakimchi <c4nyou@gmail.com>
2024-05-12 16:02:45 +0900

    feat: 관리자가 로그인 시 이름에 (관리자) 표시

--- src/main/resources/static/js/user-scripts.js
+++ src/main/resources/static/js/user-scripts.js
@@ -32,7 +32,11 @@ function updateUIBasedOnLogin() {
       })
       .then(data => {
         // 응답에서 사용자 이름을 추출하여 UI 업데이트
-        document.getElementById('profile-name').textContent = data.name; // 프로필 이름 설정
+        let profileName = data.name;
+        if (data.role === 'ADMIN') {
+          profileName += '(관리자)';
+        }
+        document.getElementById('profile-name').textContent = profileName; // 프로필 이름 설정
         document.querySelector('.nav-item.dropdown').style.display = 'block'; // 드롭다운 메뉴 표시
         document.querySelector('.nav-item a[href="/login"]').parentElement.style.display = 'none'; // 로그인 버튼 숨김
 
