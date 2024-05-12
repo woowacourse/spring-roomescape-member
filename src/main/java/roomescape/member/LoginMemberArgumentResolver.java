c44f2fd8f0f7a7ded248707018c835ff241b10c4
tackyu <tack970913@gmail.com>
2024-05-12 17:13:09 +0900

    feat: HandlerMethodArgumentResolver 구현

---
+++ src/main/java/roomescape/member/LoginMemberArgumentResolver.java
@@ -0,0 +1,37 @@
+package roomescape.member;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import roomescape.member.domain.Member;
+import roomescape.member.service.MemberService;
+
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private AuthService authService;
+    private MemberService memberService;
+
+    public LoginMemberArgumentResolver(AuthService authService, MemberService memberService) {
+        this.authService = authService;
+        this.memberService = memberService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(Member.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        Cookie[] cookies = request.getCookies();
+        String token = authService.extractTokenFromCookie(cookies);
+        long id = authService.findMemberIdByToken(token);
+        return memberService.findMemberById(id);
+    }
+}
