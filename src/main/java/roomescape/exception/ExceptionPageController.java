e180115017e01dbe9cf7678f0953a777f2e3e210
Hyeon0208 <dmsgktn0208@gmail.com>
2024-05-12 16:24:23 +0900

    feat : 401 응답의 에러 페이지 반환 핸들러 추가

--- src/main/java/roomescape/exception/ExceptionPageController.java
+++ src/main/java/roomescape/exception/ExceptionPageController.java
@@ -1,16 +1,25 @@
 package roomescape.exception;
 
-import org.springframework.http.HttpStatus;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ExceptionPageController {
 
+    @ExceptionHandler(NoAdminPrivilegeException.class)
+    public String noAdminPrivilegeExHandler(HttpServletResponse response) {
+        int statusCode = NoAdminPrivilegeException.STATUS_CODE;
+        response.setStatus(statusCode);
+
+        return "error/" + statusCode;
+    }
+
     @ExceptionHandler(UnauthenticatedUserException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public String IllegalArgExHandler() {
-        return "error/403";
+    public String unauthenticatedUserExHandler(HttpServletResponse response) {
+        int statusCode = UnauthenticatedUserException.STATUS_CODE;
+        response.setStatus(statusCode);
+
+        return "error/" + statusCode;
     }
 }
