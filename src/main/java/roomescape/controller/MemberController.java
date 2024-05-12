3401501586ba477ac61c9f4dc6103d16325a0785
leetaehoon <xogns1514@gmail.com>
2024-05-12 15:15:16 +0900

    refactor(MemberController): 회원 생성 API 응답코드 변경

--- src/main/java/roomescape/controller/MemberController.java
+++ src/main/java/roomescape/controller/MemberController.java
@@ -1,5 +1,6 @@
 package roomescape.controller;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 import roomescape.service.MemberService;
 import roomescape.service.dto.request.MemberRequest;
 import roomescape.service.dto.response.MemberResponse;
@@ -23,7 +25,14 @@ public class MemberController {
 
     @PostMapping
     public ResponseEntity<MemberResponse> createMember(@RequestBody MemberRequest memberRequest) {
-        return ResponseEntity.ok(memberService.createMember(memberRequest));
+        MemberResponse member = memberService.createMember(memberRequest);
+        URI location = UriComponentsBuilder.newInstance()
+                .path("/members/{id}")
+                .buildAndExpand(member.id())
+                .toUri();
+
+        return ResponseEntity.created(location)
+                .body(member);
     }
 
     @GetMapping
