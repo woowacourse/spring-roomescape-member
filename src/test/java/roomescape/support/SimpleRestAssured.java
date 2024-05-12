b91ce2a3569522c20265d4e32f243b24de1aa7fe
Nam Gi Beom <rlqja0523@naver.com>
2024-05-12 23:48:16 +0900

    feat: token을 쿠키에 추가하는 RestAssured 추가

--- src/test/java/roomescape/support/SimpleRestAssured.java
+++ src/test/java/roomescape/support/SimpleRestAssured.java
@@ -13,14 +13,37 @@ public class SimpleRestAssured {
                 .then().log().all();
     }
 
+    public static ValidatableResponse post(String path, Object body, String token) {
+        return RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie("token", token)
+                .body(body)
+                .when().post(path)
+                .then().log().all();
+    }
+
     public static ValidatableResponse get(String path) {
         return RestAssured.given().log().all()
                 .when().get(path)
                 .then().log().all();
     }
 
+    public static ValidatableResponse get(String path, String token) {
+        return RestAssured.given().log().all()
+                .cookie("token", token)
+                .when().get(path)
+                .then().log().all();
+    }
+
     public static ValidatableResponse delete(String path) {
         return RestAssured.given().log().all()
+                .when().delete(path)
+                .then().log().all();
+    }
+
+    public static ValidatableResponse delete(String path, String token) {
+        return RestAssured.given().log().all()
+                .cookie("token", token)
                 .when().delete(path)
                 .then().log().all();
     }
