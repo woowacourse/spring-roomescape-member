aec1e819830a312e36122eb98aa6ef9479e995a7
libienz <leekh10195@gmail.com>
2024-05-12 14:24:44 +0900

    feat: 로그아웃 구현

--- src/main/java/roomescape/auth/presentation/AuthController.java
+++ src/main/java/roomescape/auth/presentation/AuthController.java
@@ -1,6 +1,7 @@
 package roomescape.auth.presentation;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.WebUtils;
 import roomescape.auth.domain.Token;
 import roomescape.auth.dto.LoginCheckResponse;
 import roomescape.auth.dto.LoginRequest;
@@ -41,5 +43,17 @@ public class AuthController {
     @GetMapping("/login/check")
     public ResponseEntity<LoginCheckResponse> checkLogin(@CookieValue(value = "token") String token) {
         return ResponseEntity.ok().body(authService.checkLogin(token));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response) {
+        Cookie tokenCookie = WebUtils.getCookie(request, "token");
+        if (tokenCookie != null) {
+            tokenCookie.setValue(null);
+            tokenCookie.setPath("/");
+            tokenCookie.setMaxAge(0);
+            response.addCookie(tokenCookie);
+        }
+        return ResponseEntity.ok().build();
     }
 }
