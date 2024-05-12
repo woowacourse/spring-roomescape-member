4445f9a13a15851fd7d409ad38a989e5f8c2342b
Eugene Jang <kyunellroll@gmail.com>
2024-05-12 19:01:18 +0900

    feat(ReservationExceptionCode): 예외 코드 추가

--- src/main/java/roomescape/reservation/exception/ReservationExceptionCode.java
+++ src/main/java/roomescape/reservation/exception/ReservationExceptionCode.java
@@ -6,7 +6,10 @@ import roomescape.global.exception.model.ExceptionCode;
 public enum ReservationExceptionCode implements ExceptionCode {
 
     RESERVATION_TIME_IS_PAST_EXCEPTION(HttpStatus.BAD_REQUEST, "지난 시간의 테마를 선택했습니다."),
-    RESERVATION_DATE_IS_PAST_EXCEPTION(HttpStatus.BAD_REQUEST, "지난 날짜의 예약을 시도하였습니다.");
+    RESERVATION_DATE_IS_PAST_EXCEPTION(HttpStatus.BAD_REQUEST, "지난 날짜의 예약을 시도하였습니다."),
+    THEME_INFO_IS_NULL_EXCEPTION(HttpStatus.BAD_REQUEST, "필터링할 테마 정보가 존재하지 않습니다."),
+    MEMBER_INFO_IS_NULL_EXCEPTION(HttpStatus.BAD_REQUEST, "필터링할 유저 정보가 존재하지 않습니다."),
+    DATE_IS_NULL_EXCEPTION(HttpStatus.BAD_REQUEST, "필터링할 날짜 정보가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
