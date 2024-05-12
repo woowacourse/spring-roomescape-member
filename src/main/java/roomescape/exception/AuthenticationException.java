2158311d596325e0becdc7da91ad874585469398
Zeus6768 <akaps6768@gmail.com>
2024-05-13 01:31:02 +0900

    refactor: 예외 생성자 추가

--- src/main/java/roomescape/exception/AuthenticationException.java
+++ src/main/java/roomescape/exception/AuthenticationException.java
@@ -2,6 +2,9 @@ package roomescape.exception;
 
 public class AuthenticationException extends RuntimeException {
 
+    public AuthenticationException() {
+    }
+
     public AuthenticationException(String message) {
         super(message);
     }
