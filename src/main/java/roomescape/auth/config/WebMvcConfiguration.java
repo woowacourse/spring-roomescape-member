0083c72c28a06fb83ce6bbcce0b90643c009baab
pricelees <priceelees@gmail.com>
2024-05-12 13:53:06 +0900

    feat: 로그인 및 어드민 인터셉터 추가

--- src/main/java/roomescape/auth/config/WebMvcConfiguration.java
+++ src/main/java/roomescape/auth/config/WebMvcConfiguration.java
@@ -3,20 +3,39 @@ package roomescape.auth.config;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.auth.interceptor.CheckAdminInterceptor;
+import roomescape.auth.interceptor.CheckLoginInterceptor;
 import roomescape.auth.resolver.LoginMemberArgumentResolver;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final CheckLoginInterceptor checkLoginInterceptor;
+    private final CheckAdminInterceptor checkAdminInterceptor;
 
-    public WebMvcConfiguration(LoginMemberArgumentResolver loginMemberArgumentResolver) {
+    public WebMvcConfiguration(LoginMemberArgumentResolver loginMemberArgumentResolver,
+                               CheckLoginInterceptor checkLoginInterceptor,
+                               CheckAdminInterceptor checkAdminInterceptor) {
         this.loginMemberArgumentResolver = loginMemberArgumentResolver;
+        this.checkLoginInterceptor = checkLoginInterceptor;
+        this.checkAdminInterceptor = checkAdminInterceptor;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(checkLoginInterceptor)
+                .addPathPatterns("/reservation")
+                .addPathPatterns("/admin/**");
+
+        registry.addInterceptor(checkAdminInterceptor)
+                .addPathPatterns("/admin/**");
     }
 }
