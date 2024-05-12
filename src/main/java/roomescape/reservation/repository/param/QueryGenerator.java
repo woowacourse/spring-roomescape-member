093a16bd0e5488fe8dce97431da82804485751e8
kelly6bf <kelly6bf@gmail.com>
2024-05-12 16:22:57 +0900

    refactor: QueryGenerator SearchParam 변환 로직을 별도 메서드로 분리

--- src/main/java/roomescape/reservation/repository/param/QueryGenerator.java
+++ src/main/java/roomescape/reservation/repository/param/QueryGenerator.java
@@ -7,6 +7,11 @@ import java.util.List;
 
 public class QueryGenerator {
     public static String generateQueryWithSearchReservationsParams(final SearchReservationsParams params, final String baseQuery) {
+        final List<SearchParam> searchParams = convertSearchParams(params);
+        return generateQueryWithSearchParams(searchParams, baseQuery);
+    }
+
+    private static List<SearchParam> convertSearchParams(final SearchReservationsParams params) {
         boolean isFirst = true;
         List<SearchParam> searchParams = new ArrayList<>();
 
@@ -21,7 +26,7 @@ public class QueryGenerator {
         }
 
         searchParams.add(new SearchDateParam(isFirst,"date", params.from(), params.to()));
-        return generateQueryWithSearchParams(searchParams, baseQuery);
+        return searchParams;
     }
 
     private static String generateQueryWithSearchParams(final List<SearchParam> searchParams, final String baseQuery) {
