4ace6bdcee70fa5ea21cfbb6b2a80dbefd82984c
libienz <leekh10195@gmail.com>
2024-05-12 14:17:52 +0900

    fix: 어드민 멤버 조회 시 화면 렌더링 되지 않는 오류 수정

--- src/main/java/roomescape/reservation/dto/ReservationResponse.java
+++ src/main/java/roomescape/reservation/dto/ReservationResponse.java
@@ -1,12 +1,17 @@
 package roomescape.reservation.dto;
 
 import java.time.LocalDate;
+import roomescape.member.dto.MemberResponse;
 import roomescape.reservation.domain.Reservation;
+import roomescape.theme.dto.ThemeResponse;
+import roomescape.time.dto.ReservationTimeResponse;
 
-public record ReservationResponse(Long id, String name, LocalDate date, Long timeId, Long themeId) {
+public record ReservationResponse(Long id, MemberResponse member, LocalDate date, ReservationTimeResponse time,
+                                  ThemeResponse theme) {
 
     public ReservationResponse(Reservation reservation) {
-        this(reservation.getId(), reservation.getName(), reservation.getDate(), reservation.getTimeId(),
-                reservation.getThemeId());
+        this(reservation.getId(), new MemberResponse(reservation.getMember()), reservation.getDate(),
+                new ReservationTimeResponse(reservation.getTime()),
+                new ThemeResponse(reservation.getTheme()));
     }
 }
--- src/test/java/roomescape/reservation/presentation/ReservationControllerTest.java
+++ src/test/java/roomescape/reservation/presentation/ReservationControllerTest.java
@@ -3,12 +3,9 @@ package roomescape.reservation.presentation;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static roomescape.time.fixture.DateTimeFixture.DAY_AFTER_TOMORROW;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Collections;
@@ -18,13 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockCookie;
 import org.springframework.test.web.servlet.MockMvc;
 import roomescape.global.AdminHandlerInterceptor;
 import roomescape.global.AuthenticatedMemberArgumentResolver;
-import roomescape.member.fixture.MemberFixture;
-import roomescape.reservation.dto.MemberReservationAddRequest;
-import roomescape.reservation.dto.ReservationResponse;
 import roomescape.reservation.service.ReservationService;
 
 @WebMvcTest(ReservationController.class)
@@ -54,24 +47,6 @@ class ReservationControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$").isArray());
-    }
-
-    @DisplayName("예약 추가 요청을 처리할 수 있다")
-    @Test
-    void should_handle_save_reservation_request_when_requested() throws Exception {
-        MemberReservationAddRequest memberReservationAddRequest = new MemberReservationAddRequest(DAY_AFTER_TOMORROW,
-                1L, 1L);
-        ReservationResponse mockResponse = new ReservationResponse(1L, "썬", DAY_AFTER_TOMORROW, 1L, 1L);
-
-        when(reservationService.saveMemberReservation(MemberFixture.MEMBER_ID_1, memberReservationAddRequest))
-                .thenReturn(mockResponse);
-
-        mockMvc.perform(post("/reservations")
-                        .cookie(new MockCookie("token", "mock value"))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(memberReservationAddRequest)))
-                .andExpect(status().isCreated())
-                .andExpect(header().string("Location", "/reservations/" + mockResponse.id()));
     }
 
     @DisplayName("예약 삭제 요청을 처리할 수 있다")
