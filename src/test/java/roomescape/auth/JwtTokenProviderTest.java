c510d3d29d6d6059fb85d5f9fdacc7fedecf3322
gyungchan Jo <namejgc@naver.com>
2024-05-12 18:54:33 +0900

    test: 토큰 생성 및 분석 테스트 구현

---
+++ src/test/java/roomescape/auth/JwtTokenProviderTest.java
@@ -0,0 +1,35 @@
+package roomescape.auth;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class JwtTokenProviderTest {
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+    @Test
+    @DisplayName("토큰을 생성할 수 있다.")
+    void createToken() {
+        final String token = jwtTokenProvider.createToken("email@email.com");
+
+        assertThat(token).isNotNull();
+    }
+
+    @Test
+    @DisplayName("토큰을 분석할 수 있다.")
+    void getPayload() {
+        final String token = jwtTokenProvider.createToken("email@email.com");
+
+        final String result = jwtTokenProvider.getPayload(token);
+
+        assertThat(result).isEqualTo("email@email.com");
+    }
+}
