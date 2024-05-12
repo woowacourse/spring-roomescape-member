7c3a2a65831ba0b799e366be1274a2b8cc3d9ec0
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-13 03:59:52 +0900

    test: ReservationControllerIntegrationTest e2e test 추가

--- src/test/java/roomescape/infrastructure/auth/TokenManagerTest.java
+++ src/test/java/roomescape/infrastructure/auth/TokenManagerTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import roomescape.domain.member.Member;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class TokenManagerTest {
 
     @Autowired
