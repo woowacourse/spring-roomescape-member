515d5d2ae3ae2ec3ad61b42847301852d8f6aece
Zeus6768 <akaps6768@gmail.com>
2024-05-13 03:12:27 +0900

    refactor: member 도메인 분리에 따른 스키마 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
+INSERT INTO member (email, password, name)
+VALUES ('admin@woowa.com', 'admin', '어드민'),
+       ('email@email.com', 'password', 'Email'),
+       ('zeus@woowa.com', 'qwerty', 'Zeus')
+;
+
 INSERT INTO reservation_time (start_at)
 VALUES ('11:00:00'),
        ('12:00:00'),
@@ -14,16 +20,10 @@ VALUES ('Theme1', 'Description for Theme1', 'https://i.pinimg.com/236x/6e/bc/46/
        ('Theme5', 'Description for Theme5', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg')
 ;
 
-INSERT INTO reservation (name, date, time_id, theme_id)
-VALUES ('User1', '2024-05-01', 1, 1),
-       ('User2', '2024-05-02', 1, 4),
-       ('User3', '2024-05-03', 2, 4),
-       ('User4', '2024-05-04', 3, 4),
-       ('User5', '2024-05-05', 3, 3)
-;
-
-INSERT INTO member (email, password, name)
-VALUES ('admin@woowa.com', 'admin', '어드민'),
-       ('email@email.com', 'password', 'Email'),
-       ('zeus@woowa.com', 'qwerty', 'Zeus')
+INSERT INTO reservation (date, member_id, time_id, theme_id)
+VALUES ('2024-05-01', 1, 1, 1),
+       ('2024-05-02', 1, 1, 4),
+       ('2024-05-03', 1, 2, 4),
+       ('2024-05-04', 1, 3, 4),
+       ('2024-05-05', 1, 3, 3)
 ;
