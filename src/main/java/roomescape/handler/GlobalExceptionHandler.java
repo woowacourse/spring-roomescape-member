3f941c928cde7eea35b155c476b39ae1995780f1
leetaehoon <xogns1514@gmail.com>
2024-05-12 19:09:58 +0900

    feat: MethodArgumentNotValidException handler 추가

--- src/main/java/roomescape/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package roomescape.handler;
 
 import java.time.format.DateTimeParseException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import roomescape.handler.exception.CustomException;
@@ -31,5 +32,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleInvalidNumber(NumberFormatException exception) {
         return ResponseEntity.badRequest()
                 .body("잘못된 숫자 형식입니다");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        String errorMessage = exception.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity.badRequest()
+                .body(errorMessage);
     }
 }
