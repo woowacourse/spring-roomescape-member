26c512ae6e7af3334399d6315f0a0693a68ca926
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-05-12 16:59:51 +0900

    style(MemberDaoTest): 누락된 DisplayName 추가

--- src/test/java/roomescape/member/dao/MemberDaoTest.java
+++ src/test/java/roomescape/member/dao/MemberDaoTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
@@ -29,6 +30,7 @@ class MemberDaoTest {
         memberDao = new MemberDao(jdbcTemplate);
     }
 
+    @DisplayName("모든 멤버를 읽을 수 있다.")
     @Test
     void findMembersTest() {
         List<Member> actual = memberDao.findMembers();
