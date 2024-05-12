d6e3eef5f592fd8afad4f18877841bc0ce436a0a
linirini <2001yerin@naver.com>
2024-05-12 18:46:09 +0900

    feat: 설명 길이 검증 추가

--- src/main/java/roomescape/reservation/domain/Description.java
+++ src/main/java/roomescape/reservation/domain/Description.java
@@ -1,12 +1,24 @@
 package roomescape.reservation.domain;
 
+import roomescape.exception.InvalidReservationException;
+
 import java.util.Objects;
 
 public class Description {
+    private static final int MAXIMUM_DESCRIPTION_LENGTH = 100;
+    private static final String INVALID_DESCRIPTION_LENGTH = String.format("설명은 %d자를 초과할 수 없습니다.", MAXIMUM_DESCRIPTION_LENGTH);
+
     private final String value;
 
     public Description(String value) {
+        validate(value);
         this.value = value;
+    }
+
+    private void validate(String value) {
+        if(value.length()> MAXIMUM_DESCRIPTION_LENGTH){
+            throw new InvalidReservationException(INVALID_DESCRIPTION_LENGTH);
+        }
     }
 
     @Override
