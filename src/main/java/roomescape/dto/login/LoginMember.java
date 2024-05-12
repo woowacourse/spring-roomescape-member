0ea77db4648bdba02a46e576eba38d240c13b6c9
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-12 13:24:56 +0900

    refactor(LoginMember): token의 사용자 정보와 동일하게 필드 구성

--- src/main/java/roomescape/dto/login/LoginMember.java
+++ src/main/java/roomescape/dto/login/LoginMember.java
@@ -1,12 +1,21 @@
 package roomescape.dto.login;
 
 import roomescape.domain.member.Member;
+import roomescape.domain.member.Role;
 
 public record LoginMember(
-        Long id
+        Long id,
+        String name,
+        String email,
+        Role role
 ) {
 
     public static LoginMember from(Member member) {
-        return new LoginMember(member.getId());
+        return new LoginMember(
+                member.getId(),
+                member.getName(),
+                member.getEmail(),
+                member.getRole()
+        );
     }
 }
