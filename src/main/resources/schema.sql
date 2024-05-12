a48c741c1d60cb2938d24a4a745cb7433855e333
ikjo39 <chi4321@naver.com>
2024-05-12 13:34:05 +0900

    chore: 스키마 내 회원 email UNIQUE 속성 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE member
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
-    email    VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role     VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
