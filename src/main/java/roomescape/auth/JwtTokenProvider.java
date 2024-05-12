3a5b3571555d2308c41d886d51a969ba83355347
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-05-12 18:49:20 +0900

    refactor: 토큰 생성 및 만료 시간 계산 로직에 불변 객체 활용

--- src/main/java/roomescape/auth/JwtTokenProvider.java
+++ src/main/java/roomescape/auth/JwtTokenProvider.java
@@ -5,7 +5,9 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import roomescape.domain.member.Member;
-import java.util.Date;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Component
 public class JwtTokenProvider {
@@ -20,8 +22,8 @@ public class JwtTokenProvider {
     }
 
     public String createToken(final Member member) {
-        final Date now = new Date();
-        final Date validity = new Date(now.getTime() + validityInMilliseconds);
+        final Timestamp now = Timestamp.valueOf(LocalDateTime.now());
+        final Timestamp validity = Timestamp.valueOf(LocalDateTime.now().plusSeconds(validityInMilliseconds / 1000));
 
         return Jwts.builder()
                 .claim("name", member.getName())
