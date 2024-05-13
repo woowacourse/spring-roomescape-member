3e89b9d584702ebc1a4b93b7ad207f047a6099c4
fram1998 <koust6u@pusan.ac.kr>
2024-05-13 22:45:37 +0900

    refactor: presentation dto 검증 에노테이션 일관적이게 수정

--- src/main/java/roomescape/web/dto/request/member/LoginRequest.java
+++ src/main/java/roomescape/web/dto/request/member/LoginRequest.java
@@ -1,10 +1,10 @@
 package roomescape.web.dto.request.member;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 
 public record LoginRequest(
-        @Pattern(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", message = "이메일 또는 비밀번호를 형식에 맞춰 입력해주세요.") String email,
+        @Email(message = "이메일 또는 비밀번호를 형식에 맞춰 입력해주세요.") String email,
         @NotBlank(message = "이메일 또는 비밀번호를 형식에 맞춰 입력해주세요.") String password) {
 
 }
