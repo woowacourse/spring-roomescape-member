a47c9b78f944919e9751d4c5f8d445f4f3e497d1
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-13 22:26:33 +0900

    refactor: 인스턴스 생성 제거 및 빈으로 주입받아 사용

--- src/main/java/roomescape/config/WebMvcConfig.java
+++ src/main/java/roomescape/config/WebMvcConfig.java
@@ -1,35 +1,31 @@
 package roomescape.config;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.interceptor.RoleInterceptor;
 import roomescape.resolver.LoginMemberArgumentResolver;
-import roomescape.service.AuthService;
-import roomescape.service.LoginMemberService;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    private final AuthService authService;
-    private final LoginMemberService loginMemberService;
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final RoleInterceptor roleInterceptor;
 
-    @Autowired
-    public WebMvcConfig(AuthService authService, LoginMemberService loginMemberService) {
-        this.authService = authService;
-        this.loginMemberService = loginMemberService;
+    public WebMvcConfig(LoginMemberArgumentResolver loginMemberArgumentResolver, RoleInterceptor roleInterceptor) {
+        this.loginMemberArgumentResolver = loginMemberArgumentResolver;
+        this.roleInterceptor = roleInterceptor;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new LoginMemberArgumentResolver(authService, loginMemberService));
+        resolvers.add(loginMemberArgumentResolver);
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new RoleInterceptor(authService, loginMemberService))
+        registry.addInterceptor(roleInterceptor)
                 .addPathPatterns("/admin/**");
     }
 }
