847875d35d251595225d4b67928ac2e45fb6b052
MingyeomKim <mingyum119@naver.com>
2024-05-13 19:05:55 +0900

    feat: 이메일 형식의 검증을 도메인에서 수행

--- src/main/java/roomescape/member/domain/Email.java
+++ src/main/java/roomescape/member/domain/Email.java
@@ -1,7 +1,13 @@
 package roomescape.member.domain;
 
+import java.util.regex.Pattern;
+
 public class Email {
     private static final int MAX_EMAIL_LENGTH = 20;
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$"
+    );
+
     private final String value;
 
     public Email(final String value) {
@@ -13,6 +19,13 @@ public class Email {
         if (value.length() > MAX_EMAIL_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 이메일은 " + MAX_EMAIL_LENGTH + "자 이하로 입력해주세요.");
         }
+        if (isInvalidEmail(value)) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 이메일 입력입니다.");
+        }
+    }
+
+    private boolean isInvalidEmail(final String value) {
+        return !EMAIL_PATTERN.matcher(value).matches();
     }
 
     public String getValue() {
