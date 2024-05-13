eba7f6292b9d2d3c54ee8993f9a00022d6454bad
devhoya97 <diajbh123@gmail.com>
2024-05-13 17:18:50 +0900

    refactor: 로그아웃 로직 간소화

--- src/main/java/roomescape/login/controller/LogoutController.java
+++ src/main/java/roomescape/login/controller/LogoutController.java
@@ -1,9 +1,7 @@
 package roomescape.login.controller;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,23 +14,15 @@ public class LogoutController {
     private static final String TOKEN = "token";
 
     @PostMapping
-    public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(TOKEN)) {
-                    cookie.setValue("");
-                    cookie.setPath("/");
-                    cookie.setMaxAge(0);
-                    response.addCookie(cookie);
-                    break;
-                }
-            }
-        }
+    public ResponseEntity<Void> logout() {
+        ResponseCookie responseCookie = ResponseCookie.from(TOKEN, "")
+                .httpOnly(true)
+                .maxAge(0)
+                .path("/")
+                .build();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
-
-        return ResponseEntity.ok().headers(headers).build();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
+                .build();
     }
 }
