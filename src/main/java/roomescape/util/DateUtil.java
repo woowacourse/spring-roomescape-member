432de088ef97a77fe752fe2380071ad6de31c181
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-13 17:42:38 +0900

    style(DateUtil): 메서드 순서 변경

--- src/main/java/roomescape/util/DateUtil.java
+++ src/main/java/roomescape/util/DateUtil.java
@@ -12,19 +12,20 @@ public class DateUtil {
     public static final LocalDate A_WEEK_AGO = LocalDate.now().minusDays(7);
     public static final LocalTime CURRENT_TIME = LocalTime.now();
 
-    public static boolean isPastDateTime(LocalDate date, LocalTime time) {
-        boolean isPastDate = date.isBefore(TODAY);
-        boolean isPastTime = date.isEqual(TODAY) && time.isBefore(CURRENT_TIME);
-        return isPastDate || isPastTime;
-    }
-
     public static Date getCurrentTime() {
         return new Date();
     }
+
     public static Date getAfterTenMinutes() {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
         calendar.add(Calendar.MINUTE, 10);
         return calendar.getTime();
+    }
+
+    public static boolean isPastDateTime(LocalDate date, LocalTime time) {
+        boolean isPastDate = date.isBefore(TODAY);
+        boolean isPastTime = date.isEqual(TODAY) && time.isBefore(CURRENT_TIME);
+        return isPastDate || isPastTime;
     }
 }
