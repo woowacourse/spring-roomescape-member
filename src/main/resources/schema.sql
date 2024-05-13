12d15326c87fc194138d246154f9d0a356b3cb57
KimSangHyeok <tkdgur0906@naver.com>
2024-05-13 15:29:49 +0900

    refactor: db role 스키마 크기 varchar(255)로 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -33,6 +33,6 @@ CREATE TABLE member
     name     VARCHAR(255) NOT NULL,
     email    VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role     VARCHAR(5),
+    role     VARCHAR(255),
     PRIMARY KEY (id)
 );
