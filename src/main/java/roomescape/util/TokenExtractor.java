05e2f8a8d76562792d03081ac8ce3a6d2fb05fdc
devhoya97 <diajbh123@gmail.com>
2024-05-13 19:22:47 +0900

    refactor: 스트림을 활용해 가독성 향상

--- src/main/java/roomescape/util/TokenExtractor.java
+++ src/main/java/roomescape/util/TokenExtractor.java
@@ -1,6 +1,7 @@
 package roomescape.util;
 
 import jakarta.servlet.http.Cookie;
+import java.util.Arrays;
 import java.util.Objects;
 import javax.naming.AuthenticationException;
 
@@ -9,15 +10,18 @@ public class TokenExtractor {
     private TokenExtractor() {
     }
 
-    // TODO: depth 줄이기
     public static String extractTokenFromCookie(Cookie[] cookies) throws AuthenticationException {
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (Objects.equals(cookie.getName(), "token")) {
-                    return cookie.getValue();
-                }
-            }
+        validateNull(cookies);
+        return Arrays.stream(cookies)
+                .filter(cookie -> Objects.equals(cookie.getName(), "token"))
+                .findFirst()
+                .orElseThrow(() -> new AuthenticationException("접근 권한 확인을 위한 쿠키가 없습니다."))
+                .getValue();
+    }
+
+    private static void validateNull(Cookie[] cookies) throws AuthenticationException {
+        if (cookies == null) {
+            throw new AuthenticationException("접근 권한 확인을 위한 쿠키가 없습니다.");
         }
-        throw new AuthenticationException("접근 권한 확인을 위한 쿠키가 없습니다.");
     }
 }
