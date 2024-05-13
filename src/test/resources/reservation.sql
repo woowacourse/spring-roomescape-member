78942687191c851adacec0034ca8787c2e5aa921
lemone <lejinhy@gmail.com>
2024-05-13 17:11:11 +0900

    refactor: 예약 테이블 name -> member_id 변경에 따른 sql문 변경

--- src/test/resources/reservation.sql
+++ src/test/resources/reservation.sql
@@ -13,24 +13,27 @@ VALUES (2L, '오오1', '설명22', '썸네일2');
 INSERT INTO theme
 VALUES (3L, '굿굿1', '설명33', '썸네일3');
 
+INSERT INTO member
+VALUES (1L, '프린', 'prin@wooteco.com', 'prin1234');
+
 -- 테마 1, 테마 3, 테마 2 순으로 예약이 많이 되었다.
 INSERT INTO reservation
-VALUES (1L, '프린', '2024-05-01', 1L, 1L);
+VALUES (1L, '2024-05-01', 1L, 1L, 1L);
 INSERT INTO reservation
-VALUES (2L, '프린', '2024-05-01', 2L, 1L);
+VALUES (2L, '2024-05-01', 1L, 2L, 1L);
 INSERT INTO reservation
-VALUES (3L, '프린', '2024-05-02', 1L, 1L);
+VALUES (3L, '2024-05-02', 1L, 1L, 1L);
 INSERT INTO reservation
-VALUES (4L, '프린', '2024-05-02', 2L, 1L);
+VALUES (4L, '2024-05-02', 1L, 2L, 1L);
 INSERT INTO reservation
-VALUES (5L, '프린', '2024-05-02', 3L, 1L);
+VALUES (5L, '2024-05-02', 1L, 3L, 1L);
 INSERT INTO reservation
-VALUES (6L, '프린', '2024-05-03', 1L, 2L);
+VALUES (6L, '2024-05-03', 1L, 1L, 2L);
 INSERT INTO reservation
-VALUES (7L, '프린', '2024-05-03', 2L, 2L);
+VALUES (7L, '2024-05-03', 1L, 2L, 2L);
 INSERT INTO reservation
-VALUES (8L, '프린', '2024-05-03', 3L, 3L);
+VALUES (8L, '2024-05-03', 1L, 3L, 3L);
 INSERT INTO reservation
-VALUES (9L, '프린', '2024-05-04', 1L, 3L);
+VALUES (9L, '2024-05-04', 1L, 1L, 3L);
 INSERT INTO reservation
-VALUES (10L, '프린', '2024-05-04', 2L, 3L);
+VALUES (10L, '2024-05-04', 1L, 2L, 3L);
