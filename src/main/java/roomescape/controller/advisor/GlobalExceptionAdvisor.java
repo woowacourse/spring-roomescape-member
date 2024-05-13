17b6351bfaba5c61c15c953f6daabfc2cfa78b39
woojong <dwax1324@gmail.com>
2024-05-13 22:12:10 +0900

    refactor: 의도치 않은 예외 Exception 핸들러가 처리

--- src/main/java/roomescape/controller/advisor/GlobalExceptionAdvisor.java
+++ src/main/java/roomescape/controller/advisor/GlobalExceptionAdvisor.java
@@ -1,8 +1,6 @@
 package roomescape.controller.advisor;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,28 +8,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionAdvisor {
     private static final String PREFIX = "[ERROR] ";
 
-    @ExceptionHandler(value = HttpMessageNotReadableException.class)
-    private ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        return ResponseEntity.badRequest().body(PREFIX + e.getMessage());
-    }
-
     @ExceptionHandler(value = IllegalArgumentException.class)
     private ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(PREFIX + e.getMessage());
     }
 
-    @ExceptionHandler(value = NullPointerException.class)
-    private ResponseEntity<String> handleNullPointerException(NullPointerException e) {
-        return ResponseEntity.badRequest().body(PREFIX + e.getMessage());
-    }
-
-    @ExceptionHandler(value = MissingServletRequestParameterException.class)
-    private ResponseEntity<String> handleNullPointerException(MissingServletRequestParameterException e) {
-        return ResponseEntity.badRequest().body(PREFIX + "쿼리를 확인해주세요: " + e.getMessage());
-    }
-
     @ExceptionHandler(value = Exception.class)
     private ResponseEntity<String> handleException(Exception e) {
-        return ResponseEntity.internalServerError().body(PREFIX + "예상치 못한 예외가 발생했습니다. : " + e);
+        return ResponseEntity.internalServerError().body(PREFIX + "예상치 못한 예외가 발생했습니다. + e");
     }
 }
