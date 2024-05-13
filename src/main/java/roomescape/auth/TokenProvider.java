2a671951e899a9ea312bab73b6af67a16c73ac39
Nam Gi Beom <rlqja0523@naver.com>
2024-05-13 17:02:35 +0900

    refactor: TokenProvider 추상화

--- src/main/java/roomescape/auth/TokenProvider.java
+++ src/main/java/roomescape/auth/TokenProvider.java
@@ -1,51 +1,7 @@
 package roomescape.auth;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import java.util.Date;
-import javax.crypto.SecretKey;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import roomescape.auth.exception.AuthorizationException;
+public interface TokenProvider {
+    String createToken(String subject);
 
-@Component
-public class TokenProvider {
-    private final SecretKey key;
-    private final long expirationMilliseconds;
-
-    public TokenProvider(@Value("${jwt.secret}") String secret,
-                         @Value("${jwt.expiration}") long expirationMilliseconds) {
-        byte[] decoded = Decoders.BASE64.decode(secret);
-        this.key = Keys.hmacShaKeyFor(decoded);
-        this.expirationMilliseconds = expirationMilliseconds;
-    }
-
-    public String createToken(String subject) {
-        Date expiration = new Date(System.currentTimeMillis() + expirationMilliseconds);
-        return Jwts.builder()
-                .subject(subject)
-                .expiration(expiration)
-                .signWith(key)
-                .compact();
-    }
-
-    public String extractSubject(String token) {
-        try {
-            return Jwts.parser()
-                    .verifyWith(key)
-                    .build()
-                    .parseSignedClaims(token)
-                    .getPayload()
-                    .getSubject();
-        } catch (ExpiredJwtException e) {
-            throw new AuthorizationException("만료된 토큰입니다.");
-        } catch (IllegalArgumentException e) {
-            throw new AuthorizationException("토큰이 비어있습니다.");
-        } catch (JwtException e) {
-            throw new AuthorizationException("올바르지 않은 토큰입니다.");
-        }
-    }
+    String extractSubject(String token);
 }
--- src/test/java/roomescape/infrastructure/TokenProviderTest.java
+++ src/test/java/roomescape/infrastructure/TokenProviderTest.java
@@ -1,4 +1,4 @@
-package roomescape.auth;
+package roomescape.infrastructure;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -6,16 +6,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import roomescape.auth.TokenProvider;
 import roomescape.auth.exception.AuthorizationException;
 
-class TokenProviderTest {
+class JwtProviderTest {
     private static final String SECRET = "secretsecretsecretsecretsecretsecretsecretsecret";
 
-    private TokenProvider tokenProvider = new TokenProvider(SECRET, 10000L);
+    private TokenProvider tokenProvider = new JwtProvider(SECRET, 10000L);
 
     @Test
     void 토큰이_만료되면_예외가_발생한다() {
-        tokenProvider = new TokenProvider(SECRET, 0L);
+        tokenProvider = new JwtProvider(SECRET, 0L);
         String token = tokenProvider.createToken("1");
 
         assertThatThrownBy(() -> tokenProvider.extractSubject(token))
@@ -34,7 +35,7 @@ class TokenProviderTest {
     @Test
     void 올바르지_않은_토큰이면_예외가_발생한다() {
         String invalidSecret = "invalidinvalidinvalidinvalidinvalidinvalidinvalidinvalid";
-        TokenProvider invalidTokenProvider = new TokenProvider(invalidSecret, 10000L);
+        TokenProvider invalidTokenProvider = new JwtProvider(invalidSecret, 10000L);
         String invalidToken = invalidTokenProvider.createToken("1");
 
         assertThatThrownBy(() -> tokenProvider.extractSubject(invalidToken))
