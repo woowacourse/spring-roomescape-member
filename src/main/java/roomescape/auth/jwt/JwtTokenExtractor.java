45013a6aafa359cbeba8682041714348f9541a1d
MingyeomKim <mingyum119@naver.com>
2024-05-13 14:38:21 +0900

    fix: 불필요한 개행 제거

--- src/main/java/roomescape/auth/jwt/JwtTokenExtractor.java
+++ src/main/java/roomescape/auth/jwt/JwtTokenExtractor.java
@@ -12,8 +12,7 @@ public final class JwtTokenExtractor {
         if (cookies == null || cookies.length == 0) {
             throw new SecurityException("[ERROR] 쿠키에 토큰 정보를 입력해주세요.");
         }
-        return Arrays.asList(cookies)
-                .stream()
+        return Arrays.asList(cookies).stream()
                 .filter(cookie -> cookie.getName().equals("token"))
                 .findAny()
                 .orElseThrow(() -> new SecurityException("[ERROR] 쿠키에 토큰 정보를 입력해주세요."))
