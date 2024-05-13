b2823411fee6c3a276db625c533cf04147b28404
lemone <lejinhy@gmail.com>
2024-05-13 17:13:06 +0900

    feat: 토큰이 올바르지 않을 경우 예외처리 추가

--- src/main/java/roomescape/infrastructure/JwtTokenProvider.java
+++ src/main/java/roomescape/infrastructure/JwtTokenProvider.java
@@ -3,30 +3,46 @@ package roomescape.infrastructure;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
 import java.util.Date;
 import org.springframework.stereotype.Component;
+import roomescape.domain.member.Member;
+import roomescape.ui.AuthorizationException;
 
 @Component
 public class JwtTokenProvider {
-    private static final String TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwibmFtZSI6ImFkbWluIiwicm9sZSI6IkFETUlOIn0.cwnHsltFeEtOzMHs2Q5-ItawgvBZ140OyWecppNlLoI";
+    private static final String SECRET_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwibmFtZSI6ImFkbWluIiwicm9sZSI6IkFETUlOIn0.cwnHsltFeEtOzMHs2Q5-ItawgvBZ140OyWecppNlLoI";
     private static final long VALIDITY_IN_MILLISECONDS = 3600000;
 
 
-    public String createToken(String payload) {
-        Claims claims = Jwts.claims().setSubject(payload);
+    public String createToken(Member member) {
         Date now = new Date(System.currentTimeMillis());
         Date validity = new Date(System.currentTimeMillis() + VALIDITY_IN_MILLISECONDS);
 
         return Jwts.builder()
-                .setClaims(claims)
+                .claim("id", member.getId())
+                .claim("name", member.getMemberName().getValue())
+                .claim("email", member.getEmail())
                 .setIssuedAt(now)
                 .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, TOKEN.getBytes())
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
 
-    public String getPayload(String token) {
-        return Jwts.parser().setSigningKey(TOKEN.getBytes()).parseClaimsJws(token).getBody()
-                .getSubject();
+    public String getPayload(String token, String key) {
+        return parseClaims(token)
+                .get(key)
+                .toString();
+    }
+
+    public Claims parseClaims(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (SignatureException exception) {
+            throw new AuthorizationException("토큰이 올바르지 않습니다.");
+        }
     }
 }
