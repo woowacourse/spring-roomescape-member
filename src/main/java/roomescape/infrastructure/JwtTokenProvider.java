1bb5631d81b89dc5ff07f74a6510cecafc85eefc
lemone <lejinhy@gmail.com>
2024-05-13 10:16:54 +0900

    refactor: 시크릿키, expire-length 설정 삭제

--- src/main/java/roomescape/infrastructure/JwtTokenProvider.java
+++ src/main/java/roomescape/infrastructure/JwtTokenProvider.java
@@ -4,30 +4,29 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenProvider {
-    @Value("${security.jwt.token.secret-key}")
-    private String secretKey;
-    @Value("${security.jwt.token.expire-length}")
-    private long validityInMilliseconds;
+    private static final String TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwibmFtZSI6ImFkbWluIiwicm9sZSI6IkFETUlOIn0.cwnHsltFeEtOzMHs2Q5-ItawgvBZ140OyWecppNlLoI";
+    private static final long VALIDITY_IN_MILLISECONDS = 3600000;
+
 
     public String createToken(String payload) {
         Claims claims = Jwts.claims().setSubject(payload);
-        Date now = new Date();
-        Date validity = new Date(now.getTime() + validityInMilliseconds);
+        Date now = new Date(System.currentTimeMillis());
+        Date validity = new Date(System.currentTimeMillis() + VALIDITY_IN_MILLISECONDS);
 
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(SignatureAlgorithm.HS256, TOKEN.getBytes())
                 .compact();
     }
 
     public String getPayload(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser().setSigningKey(TOKEN.getBytes()).parseClaimsJws(token).getBody()
+                .getSubject();
     }
 }
