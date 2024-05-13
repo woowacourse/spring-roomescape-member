393076e477625b59c46a43c2d45c2899a882c307
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-13 17:15:21 +0900

    refactor(AdminPageApiTest): 인증 및 권한에 따른 페이지 접속 테스트 추가

--- src/test/java/roomescape/api/AdminPageApiTest.java
+++ src/test/java/roomescape/api/AdminPageApiTest.java
@@ -1,56 +1,71 @@
 package roomescape.api;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static roomescape.LoginTestSetting.getCookieByLogin;
 
+import io.restassured.http.Cookie;
+import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.jdbc.Sql;
 
+@Sql("/page-test-data.sql")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class AdminPageApiTest {
+
+    private static final String ADMIN_EMAIL = "admin@email.com";
+    private static final String ADMIN_PASSWORD = "123456";
+
+    private static final String USER_EMAIL = "test@email.com";
+    private static final String USER_PASSWORD = "123456";
 
     @LocalServerPort
     int port;
 
-    @Test
-    void 메인_페이지_이동() {
-        given().log().all()
-                .port(port)
-                .when().get("/admin")
-                .then().log().all()
-                .statusCode(200);
+    static List<String> provideUrls() {
+        return List.of("/admin", "/admin/reservation", "/admin/time", "admin/theme");
     }
 
-    @Test
-    void 예약_페이지_이동() {
-        given().log().all()
+    @ParameterizedTest
+    @MethodSource("provideUrls")
+    void 로그인이_되어있지_않은_경우_어드민_페이지_진입시_로그인_페이지로_이동(String url) {
+        given().redirects().follow(false).log().all()
                 .port(port)
-                .when().get("/admin/reservation")
+                .when().get(url)
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(302)
+                .header("Location", containsString("/login"));
     }
 
-    @Test
-    void 예약_시간_페이지_이동() {
+    @ParameterizedTest
+    @MethodSource("provideUrls")
+    void 로그인_계정의_권한이_관리자가_아닐때_어드민_페이지_진입시_권한없음_응답(String url) {
+        Cookie cookieByLogin = getCookieByLogin(port, USER_EMAIL, USER_PASSWORD);
+
         given().log().all()
                 .port(port)
-                .when().get("/admin/time")
+                .cookie(cookieByLogin)
+                .when().get(url)
                 .then().log().all()
-                .statusCode(200);
+                .statusCode(401);
     }
 
-    @Test
-    void 테마_페이지_이동() {
+    @ParameterizedTest
+    @MethodSource("provideUrls")
+    void 로그인_계정의_권한이_관리자이면_어드민_페이지_정상_진입(String url) {
+        Cookie cookieByLogin = getCookieByLogin(port, ADMIN_EMAIL, ADMIN_PASSWORD);
+
         given().log().all()
                 .port(port)
-                .when().get("/admin/theme")
+                .cookie(cookieByLogin)
+                .when().get(url)
                 .then().log().all()
                 .statusCode(200);
     }
