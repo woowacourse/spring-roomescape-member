e9cbe4cd03d03f956e17ee90f5ed947954230273
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-05-13 12:58:08 +0900

    refactor: 토큰 추출 로직 변경

--- src/main/java/roomescape/util/CheckAdminInterceptor.java
+++ src/main/java/roomescape/util/CheckAdminInterceptor.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Arrays;
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -22,18 +23,22 @@ public class CheckAdminInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String token = Arrays.stream(request.getCookies())
-                .filter(cookie -> cookie.getName().equals(JwtTokenProvider.TOKEN))
-                .findFirst()
-                .map(Cookie::getValue)
-                .orElseThrow(() -> new IllegalArgumentException("토큰이 존재하지 않습니다."));
-
-        LoginMember loginMember = (LoginMember) memberService.findLoginMemberByToken(token);
+        String token = extractToken(request);
+        LoginMember loginMember = memberService.findLoginMemberByToken(token);
         if (!loginMember.role().equals(Role.ADMIN)) {
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
             return false;
         }
         return true;
+    }
 
+    private String extractToken(HttpServletRequest request) {
+        return Arrays.stream(Optional.ofNullable(request.getCookies())
+                .orElseThrow(() -> new IllegalArgumentException("쿠키가 존재하지 않습니다.")))
+                .sequential()
+                .filter(cookie -> cookie.getName().equals(JwtTokenProvider.TOKEN))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(() -> new IllegalArgumentException("토큰이 존재하지 않습니다."));
     }
 }
