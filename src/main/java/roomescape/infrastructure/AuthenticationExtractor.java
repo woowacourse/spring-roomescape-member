c708636fb74e5bad06d2ebcdefcf1aabd4576a63
Arachneee <jhg2819@naver.com>
2024-05-13 16:19:23 +0900

    fix: 인증/ 인가 버그 수정

--- src/main/java/roomescape/infrastructure/AuthenticationExtractor.java
+++ src/main/java/roomescape/infrastructure/AuthenticationExtractor.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import org.springframework.stereotype.Component;
 import roomescape.exception.AuthenticationException;
-import roomescape.exception.AuthorizationException;
 
 @Component
 public class AuthenticationExtractor {
@@ -19,7 +18,7 @@ public class AuthenticationExtractor {
         return Arrays.stream(cookies)
                 .filter(cookie -> cookieName.equals(cookie.getName()))
                 .findFirst()
-                .orElseThrow(AuthorizationException::new)
+                .orElseThrow(AuthenticationException::new)
                 .getValue();
     }
 }
