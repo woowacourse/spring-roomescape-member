aaec694e999feb54cc7fb5339210832315d957f0
lemone <lejinhy@gmail.com>
2024-05-13 20:36:41 +0900

    refactor: 태스트 스키마 수정

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -27,8 +27,8 @@ CREATE TABLE IF NOT EXISTS reservation_time
 CREATE TABLE IF NOT EXISTS reservation
 (
     id               BIGINT       NOT NULL AUTO_INCREMENT,
-    name             VARCHAR(255) NOT NULL,
     reservation_date DATE         NOT NULL,
+    member_id        BIGINT       NOT NULL,
     time_id          BIGINT       NOT NULL,
     theme_id         BIGINT       NOT NULL,
     PRIMARY KEY (id),
