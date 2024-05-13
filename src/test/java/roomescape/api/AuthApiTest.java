08f664d733eb487aa0ebfd177ae608e30bedfa41
KIMGYUTAE <kiimwf0819@gmail.com>
2024-05-13 17:17:53 +0900

    refactor(AuthApiTest): 변경 사항에 의한 테스크 코드 수정

--- src/test/java/roomescape/api/AuthApiTest.java
+++ src/test/java/roomescape/api/AuthApiTest.java
@@ -1,80 +1,72 @@
 package roomescape.api;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static roomescape.LoginTestSetting.getCookieByLogin;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Cookie;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseCookie;
 import org.springframework.test.context.jdbc.Sql;
 import roomescape.dto.login.LoginCheckResponse;
 import roomescape.dto.login.LoginRequest;
+import roomescape.infrastructure.JwtProvider;
 
 @Sql("/member-test-data.sql")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class AuthApiTest {
 
-    private static final String NAME = "ted";
+    private static final String NAME = "테드";
     private static final String EMAIL = "test@email.com";
     private static final String PASSWORD = "123456";
+
+    @Autowired
+    JwtProvider jwtProvider;
 
     @LocalServerPort
     int port;
 
     @Test
     void 토근_로그인() {
-        RestAssured
+        String token = RestAssured
                 .given().log().all()
                 .port(port)
                 .body(new LoginRequest(EMAIL, PASSWORD))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/login")
                 .then().log().all()
-                .assertThat().cookie("token");
+                .extract().cookie("token");
+
+        Long userId = Long.parseLong(jwtProvider.getSubject(token));
+
+        assertAll(
+                () -> assertThat(jwtProvider.isValidateToken(token)).isTrue(),
+                () -> assertThat(userId).isEqualTo(1)
+        );
     }
 
     @Test
     void 로그인_사용자_조회() {
-        String accessToken = getAccessTokenByLogin();
-        ResponseCookie cookie = createCookieByAccessToken(accessToken);
+        Cookie cookieByLogin = getCookieByLogin(port, EMAIL, PASSWORD);
 
         LoginCheckResponse response = RestAssured
                 .given().log().all()
                 .port(port)
-                .cookie(cookie.toString())
-                .auth().oauth2(accessToken)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .cookie(cookieByLogin)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().get("/login/check")
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value()).extract().as(LoginCheckResponse.class);
 
         assertThat(response.name()).isEqualTo(NAME);
-    }
-
-    private String getAccessTokenByLogin() {
-        return RestAssured
-                .given().log().all()
-                .port(port)
-                .body(new LoginRequest(EMAIL, PASSWORD))
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/login")
-                .then().log().all()
-                .extract().cookie("token");
-    }
-
-    private ResponseCookie createCookieByAccessToken(String accessToken) {
-        return ResponseCookie
-                .from("token", accessToken)
-                .path("/")
-                .httpOnly(true)
-                .build();
     }
 }
