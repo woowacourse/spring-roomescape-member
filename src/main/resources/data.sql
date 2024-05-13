949aff862938990be64b80ba9935eefdd07c6ffc
lemone <lejinhy@gmail.com>
2024-05-13 17:05:55 +0900

    chore: 예약 테이블의 이름을 member_id로 수정 후 data.sql 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO theme (name, description, thumbnail)
-VALUES ('테마1', '설명1', 'https://idsncdn.iwinv.biz/news/data/20220919/p1065578397270038_168_thum.jpg');
+INSERT INTO theme (id, name, description, thumbnail)
+VALUES (1L, '테마1', '설명1', 'https://idsncdn.iwinv.biz/news/data/20220919/p1065578397270038_168_thum.jpg');
 
-INSERT INTO reservation_time (start_at)
-VALUES ('12:00');
-INSERT INTO reservation_time (start_at)
-VALUES ('14:00');
+INSERT INTO reservation_time (id, start_at)
+VALUES (1L, '12:00');
+INSERT INTO reservation_time (id, start_at)
+VALUES (2L, '14:00');
 
-INSERT INTO reservation (name, reservation_date, time_id, theme_id)
-VALUES ('레모네', '2024-05-30', 1L, 1L);
+INSERT INTO member (id, name, email, password)
+VALUES (1L, '레모네', 'lemone@wooteco.com', 'lemone1234');
 
-INSERT INTO member (name, email, password)
-VALUES ('레모네', 'lemone@wooteco.com', 'lemone1234');
+INSERT INTO reservation (id, reservation_date, member_id, time_id, theme_id)
+VALUES (1L, '2024-05-30', 1L, 1L, 1L);
