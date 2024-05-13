14c427e8e048d247c75ebfb19c95556442d9a233
Zeus6768 <akaps6768@gmail.com>
2024-05-13 17:05:55 +0900

    refactor: 요구사항에 맞도록 테스트 데이터 수정

--- src/test/java/roomescape/controller/view/AdminViewControllerTest.java
+++ src/test/java/roomescape/controller/view/AdminViewControllerTest.java
@@ -4,18 +4,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Cookie;
+import roomescape.util.JwtProvider;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestInstance(Lifecycle.PER_METHOD)
 class AdminViewControllerTest {
 
+    @Autowired
+    JwtProvider jwtProvider;
+
     @Test
     @DisplayName("방탈출 관리 홈페이지를 매핑한다.")
     void index() {
+        String token = jwtProvider.createToken("admin@woowa.com");
+        Cookie cookie = new Cookie.Builder("token", token).build();
         RestAssured.given().log().all()
+                .cookie(cookie)
                 .when().get("/admin")
                 .then().log().all()
                 .statusCode(200);
@@ -24,7 +33,10 @@ class AdminViewControllerTest {
     @Test
     @DisplayName("방탈출 예약 관리 페이지를 매핑한다.")
     void reservation() {
+        String token = jwtProvider.createToken("admin@woowa.com");
+        Cookie cookie = new Cookie.Builder("token", token).build();
         RestAssured.given().log().all()
+                .cookie(cookie)
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
@@ -33,7 +45,10 @@ class AdminViewControllerTest {
     @Test
     @DisplayName("방탈출 테마 관리 페이지를 매핑한다.")
     void theme() {
+        String token = jwtProvider.createToken("admin@woowa.com");
+        Cookie cookie = new Cookie.Builder("token", token).build();
         RestAssured.given().log().all()
+                .cookie(cookie)
                 .when().get("/admin/theme")
                 .then().log().all()
                 .statusCode(200);
