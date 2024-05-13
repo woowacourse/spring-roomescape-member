51921058f30e0fb896f52df9430a9f74cab3a5c9
Eugene Jang <kyunellroll@gmail.com>
2024-05-13 16:45:41 +0900

    feat(MemberRoleInterceptor): Auth를 구해오는 메서드 분리 및 추가

--- src/main/java/roomescape/global/interceptor/MemberRoleInterceptor.java
+++ src/main/java/roomescape/global/interceptor/MemberRoleInterceptor.java
@@ -29,14 +29,15 @@ public class MemberRoleInterceptor implements HandlerInterceptor {
         if (!(handler instanceof HandlerMethod handlerMethod)) {
             return true;
         }
-        Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
 
+        Auth auth = getAuth(handlerMethod);
         if (auth == null) {
             return true;
         }
 
         String token = CookieProvider.getCookieValue("token", request.getCookies());
         long memberId = Long.parseLong(tokenProvider.resolveToken(token));
+
         if (!isSameRole(auth, memberId)) {
             throw new RoomEscapeException(MemberExceptionCode.MEMBER_ROLE_UN_AUTHORIZED_EXCEPTION);
         }
@@ -46,5 +47,18 @@ public class MemberRoleInterceptor implements HandlerInterceptor {
     private boolean isSameRole(Auth auth, long memberId) {
         MemberRole[] permittedRole = auth.role();
         return memberService.findMemberRole(memberId).hasSameRoleFrom(permittedRole);
+    }
+
+    private Auth getAuth(HandlerMethod handlerMethod) {
+        Auth classAuth = handlerMethod.getBeanType().getAnnotation(Auth.class);
+        Auth methodAuth = handlerMethod.getMethodAnnotation(Auth.class);
+
+        if (classAuth != null) {
+            return classAuth;
+        }
+        if (methodAuth != null) {
+            return methodAuth;
+        }
+        return null;
     }
 }
