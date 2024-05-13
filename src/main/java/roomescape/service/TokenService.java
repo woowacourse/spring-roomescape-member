cee0a0ea8ac80e1f7f1bbfc43548e9b46f65fe4b
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-05-13 18:14:01 +0900

    refactor(TokenService): 인덴트 줄이기

--- src/main/java/roomescape/service/TokenService.java
+++ src/main/java/roomescape/service/TokenService.java
@@ -1,10 +1,13 @@
 package roomescape.service;
 
+import java.util.Arrays;
+
 import jakarta.servlet.http.Cookie;
 
 import org.springframework.stereotype.Service;
 
 import roomescape.domain.Role;
+import roomescape.domain.exception.AuthFailException;
 import roomescape.dto.MemberModel;
 import roomescape.dto.response.TokenResponse;
 import roomescape.infrastructure.JwtTokenProvider;
@@ -34,11 +37,10 @@ public class TokenService {
     }
 
     private String extractTokenFromCookie(Cookie[] cookies) {
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")) {
-                return cookie.getValue();
-            }
-        }
-        return "";
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals("token"))
+                .findAny()
+                .map(Cookie::getValue)
+                .orElseThrow(() -> new AuthFailException("로그인 정보가 없습니다."));
     }
 }
