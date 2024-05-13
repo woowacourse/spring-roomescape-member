b15fcb86fa84ab31c4ca7fe4031704c423297144
3juhwan <13selfesteem91@naver.com>
2024-05-14 01:09:51 +0900

    refactor: admin 예약 로직을 별도의 클래스로 분리

--- src/main/java/roomescape/web/api/ReservationAdminController.java
+++ src/main/java/roomescape/web/api/ReservationAdminController.java
@@ -2,37 +2,31 @@ package roomescape.web.api;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.service.ReservationService;
 import roomescape.service.dto.request.ReservationRequest;
 import roomescape.service.dto.response.ReservationResponse;
+import roomescape.web.api.dto.ReservationAdminRequest;
 import roomescape.web.api.dto.ReservationListResponse;
-import roomescape.web.api.dto.ReservationMemberRequest;
-import roomescape.web.api.resolver.Auth;
-import roomescape.web.api.resolver.Principal;
 
 import java.net.URI;
 import java.util.List;
 
 @RestController
-public class ReservationController {
+public class ReservationAdminController {
     private final ReservationService reservationService;
 
-    public ReservationController(ReservationService reservationService) {
+    public ReservationAdminController(ReservationService reservationService) {
         this.reservationService = reservationService;
     }
 
-    @PostMapping("/reservations")
-    public ResponseEntity<ReservationResponse> save(
-            @RequestBody @Valid ReservationMemberRequest request,
-            @Auth Principal principal
-    ) {
-        ReservationRequest reservationRequest = new ReservationRequest(principal.id(), request.date(),
+    @PostMapping("/admin/reservations")
+    public ResponseEntity<ReservationResponse> save(@RequestBody @Valid ReservationAdminRequest request) {
+        ReservationRequest reservationRequest = new ReservationRequest(request.memberId(), request.date(),
                 request.timeId(), request.themeId());
 
         ReservationResponse reservationResponse = reservationService.save(reservationRequest);
@@ -41,17 +35,16 @@ public class ReservationController {
                 .body(reservationResponse);
     }
 
-    @GetMapping("/reservations")
-    public ResponseEntity<ReservationListResponse> findAll() {
-        List<ReservationResponse> reservationResponses = reservationService.findAll();
+    @GetMapping("/admin/reservations")
+    public ResponseEntity<ReservationListResponse> findByCondition(
+            @RequestParam(value = "memberId") Long memberId,
+            @RequestParam(value = "themeId") Long themeId,
+            @RequestParam(value = "dateFrom") String dateFrom,
+            @RequestParam(value = "dateTo") String dateTo
+    ) {
+        List<ReservationResponse> reservationResponses = reservationService.findByCondition(memberId, themeId, dateFrom,
+                dateTo);
 
         return ResponseEntity.ok(new ReservationListResponse(reservationResponses));
-    }
-
-    @DeleteMapping("/reservations/{id}")
-    public ResponseEntity<Void> delete(@PathVariable(name = "id") Long id) {
-        reservationService.deleteById(id);
-
-        return ResponseEntity.noContent().build();
     }
 }
