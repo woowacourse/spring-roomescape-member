3766a39444a9e3e4d652e81ed845be9a4b1ddb08
lemone <lejinhy@gmail.com>
2024-05-16 00:44:19 +0900

    feat(MemberController): 등록된 모든 멤버를 반환하는 api 추가

--- src/main/java/roomescape/dto/member/MemberResponse.java
+++ src/main/java/roomescape/dto/member/MemberResponse.java
@@ -1,5 +1,14 @@
 package roomescape.dto.member;
 
-public record MemberResponse(String name) {
+import roomescape.domain.member.Member;
 
+public record MemberResponse(long id, String name, String email, String password) {
+    public static MemberResponse from(Member member) {
+        return new MemberResponse(
+                member.getId(),
+                member.getName().getValue(),
+                member.getEmail(),
+                member.getPassword()
+        );
+    }
 }
