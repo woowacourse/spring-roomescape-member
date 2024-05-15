121a37d100acb5e8cafa7c746231706b9a6328ed
ehtjsv2 <ehtjsv2@naver.com>
2024-05-15 14:54:53 +0900

    test: 잘못된 role 이름 예외 테스트 추가

--- src/test/java/roomescape/domain/member/domain/RoleTest.java
+++ src/test/java/roomescape/domain/member/domain/RoleTest.java
@@ -1,9 +1,11 @@
 package roomescape.domain.member.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import roomescape.global.exception.EscapeApplicationException;
 
 class RoleTest {
 
@@ -15,5 +17,13 @@ class RoleTest {
         Role actualRole = Role.convertToRole("admin");
 
         assertThat(actualRole).isEqualTo(expectedRole);
+    }
+
+    @DisplayName("잘못된 역할의 이름으로 역할객체를 찾는 경우 예외가 발생한다.")
+    @Test
+    void should_throw_exception_when_convert_with_wrong_role_name() {
+        assertThatThrownBy(() -> Role.convertToRole("wrongRole"))
+                .isInstanceOf(EscapeApplicationException.class)
+                .hasMessage("존재하지 않는 역할입니다.");
     }
 }
