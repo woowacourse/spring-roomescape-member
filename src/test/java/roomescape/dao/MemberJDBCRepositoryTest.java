81ae728e18948f860f01dbdbc22529e80f36c4af
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-15 17:06:35 +0900

    test: MemberJDBCRepositoryTest

---
+++ src/test/java/roomescape/dao/MemberJDBCRepositoryTest.java
@@ -0,0 +1,47 @@
+package roomescape.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import roomescape.domain.Member;
+
+@JdbcTest
+@Sql(scripts = "/test_data.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+class MemberJDBCRepositoryTest {
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository = new MemberJDBCRepository(jdbcTemplate);
+    }
+
+    @DisplayName("이메일로 회원을 조회한다.")
+    @Test
+    void findMemberByEmailTest() {
+        //when
+        Member member = memberRepository.findByEmail("admin@email.com").get();
+
+        //then
+        assertThat(member.getId()).isEqualTo(1);
+    }
+
+    @DisplayName("id로 회원을 조회한다.")
+    @Test
+    void findMemberByIdTest() {
+        //when
+        Member member = memberRepository.findById(1).get();
+
+        //then
+        assertThat(member.getId()).isEqualTo(1);
+    }
+}
