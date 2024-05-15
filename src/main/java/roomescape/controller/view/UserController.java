345ff93a39af9593c380766e964693369f6314cd
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-15 16:48:51 +0900

    refactor: 불필요한 path문 삭제

--- src/main/java/roomescape/controller/view/UserController.java
+++ src/main/java/roomescape/controller/view/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping
 public class UserController {
     @GetMapping("reservation")
     public String getReservationPage() {
