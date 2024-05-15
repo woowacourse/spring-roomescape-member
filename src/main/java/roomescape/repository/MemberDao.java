33e50efc148494e9f177d0c5f9cc5b6a9f1c21ad
coli-geonwoo <jameskkwoo1021@gmail.com>
2024-05-15 17:49:29 +0900

    style: sql문 lineBreak 추가

--- src/main/java/roomescape/repository/MemberDao.java
+++ src/main/java/roomescape/repository/MemberDao.java
@@ -2,11 +2,9 @@ package roomescape.repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.member.Member;
 
-import javax.sql.DataSource;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,30 +14,29 @@ public class MemberDao {
     private static final String KEY_COLUMN_NAME = "id";
 
     private final JdbcTemplate jdbcTemplate;
-    private final SimpleJdbcInsert simpleJdbcInsert;
     private final RowMapper<Member> memberRowMapper;
 
     public MemberDao(
             final JdbcTemplate jdbcTemplate,
-            final DataSource source,
             final RowMapper<Member> memberRowMapper
     ) {
         this.jdbcTemplate = jdbcTemplate;
-        this.simpleJdbcInsert = new SimpleJdbcInsert(source)
-                .withTableName(TABLE_NAME)
-                .usingGeneratedKeyColumns(KEY_COLUMN_NAME);
         this.memberRowMapper = memberRowMapper;
     }
 
     public Optional<Member> findByEmailAndPassword(final String email, final String password) {
-        String sql = "SELECT id as member_id, * FROM " + TABLE_NAME + " WHERE email = ? " + " AND password= ?";
+        String sql = "SELECT id as member_id, * " +
+                "FROM " + TABLE_NAME +
+                " WHERE email = ? AND password= ?";
         return jdbcTemplate.query(sql, memberRowMapper, email, password)
                 .stream()
                 .findAny();
     }
 
     public Optional<Member> findById(long memberId) {
-        String sql = "SELECT id as member_id, * FROM " + TABLE_NAME + " WHERE id = ?";
+        String sql = "SELECT id as member_id, * " +
+                "FROM " + TABLE_NAME +
+                " WHERE id = ?";
         return jdbcTemplate.query(sql, memberRowMapper, memberId)
                 .stream()
                 .findAny();
