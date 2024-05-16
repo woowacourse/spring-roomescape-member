16bbf16475d0925159b049582a572c01b576a2b1
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-16 13:02:44 +0900

    refactor: extractByCookies depth 1로 수정

--- src/main/java/roomescape/infrastructure/JwtTokenExtractor.java
+++ src/main/java/roomescape/infrastructure/JwtTokenExtractor.java
@@ -3,6 +3,7 @@ package roomescape.infrastructure;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.Cookie;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
@@ -16,12 +17,11 @@ public class JwtTokenExtractor implements TokenExtractor {
     private String secretKey;
 
     public String extractByCookies(Cookie[] cookies) {
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals(COOKIE_NAME)) {
-                return cookie.getValue();
-            }
-        }
-        throw new IllegalArgumentException("토큰이 존재하지 않습니다.");
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals(COOKIE_NAME))
+                .map(Cookie::getValue)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("토큰이 존재하지 않습니다."));
     }
 
     public String extractMemberIdByToken(String token) {
