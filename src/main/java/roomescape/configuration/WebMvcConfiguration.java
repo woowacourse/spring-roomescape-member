2f1ea6fc0d0594b629601399baff64547e9b2406
masonkimseoul <masonkimseoul@gmail.com>
2024-05-16 10:58:03 +0900

    refactor : "/members"에 Auth 모듈 적용

--- src/main/java/roomescape/configuration/WebMvcConfiguration.java
+++ src/main/java/roomescape/configuration/WebMvcConfiguration.java
@@ -27,6 +27,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new CheckMemberRoleInterceptor(authService, authorizationExtractor))
-                .addPathPatterns("/admin/**");
+                .addPathPatterns("/admin/**")
+                .addPathPatterns("/members/**");
     }
 }
