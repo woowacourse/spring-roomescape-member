a7d2e8492e02f7ac5ebffc4a5f10c3fdcf7f39dc
HoeSeong123 <ghltjd3@gmail.com>
2024-05-18 16:13:55 +0900

    refactor : AdminController 분리

---
+++ src/main/java/roomescape/controller/AdminController.java
@@ -0,0 +1,46 @@
+package roomescape.controller;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.dto.request.ReservationAdminCreateRequest;
+import roomescape.dto.response.ReservationResponse;
+import roomescape.service.ReservationService;
+
+@RestController
+public class AdminController {
+
+    private final ReservationService reservationService;
+
+    public AdminController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<ReservationResponse> createAdminReservation(
+            @RequestBody ReservationAdminCreateRequest reservationAdminCreateRequest) {
+        ReservationResponse reservationResponse = reservationService.createAdminReservation(
+                reservationAdminCreateRequest);
+        return ResponseEntity.created(URI.create("/reservations/" + reservationResponse.id()))
+                .body(reservationResponse);
+    }
+
+    @GetMapping("/reservation/search")
+    public ResponseEntity<List<ReservationResponse>> showSearchedReservations(
+            @RequestParam Long themeId,
+            @RequestParam Long memberId,
+            @RequestParam LocalDate dateFrom,
+            @RequestParam LocalDate dateTo) {
+        List<ReservationResponse> searchedReservations =
+                reservationService.findSearchedReservations(themeId, memberId, dateFrom, dateTo);
+
+        return ResponseEntity.ok().body(searchedReservations);
+    }
+
+}
--- src/main/java/roomescape/controller/AdminPageController.java
+++ src/main/java/roomescape/controller/AdminPageController.java
@@ -1,28 +1,12 @@
 package roomescape.controller;
 
-import java.net.URI;
-import java.time.LocalDate;
-import java.util.List;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import roomescape.dto.request.ReservationAdminCreateRequest;
-import roomescape.dto.response.ReservationResponse;
-import roomescape.service.ReservationService;
 
 @Controller
 @RequestMapping("admin")
 public class AdminPageController {
-
-    private final ReservationService reservationService;
-
-    public AdminPageController(ReservationService reservationService) {
-        this.reservationService = reservationService;
-    }
 
     @GetMapping
     public String showAdminIndexPage() {
@@ -32,27 +16,6 @@ public class AdminPageController {
     @GetMapping("/reservation")
     public String showReservationPage() {
         return "admin/reservation-new";
-    }
-
-    @PostMapping("/reservations")
-    public ResponseEntity<ReservationResponse> createAdminReservation(
-            @RequestBody ReservationAdminCreateRequest reservationAdminCreateRequest) {
-        ReservationResponse reservationResponse = reservationService.createAdminReservation(
-                reservationAdminCreateRequest);
-        return ResponseEntity.created(URI.create("/reservations/" + reservationResponse.id()))
-                .body(reservationResponse);
-    }
-
-    @GetMapping("/reservation/search")
-    public ResponseEntity<List<ReservationResponse>> showSearchedReservations(
-            @RequestParam Long themeId,
-            @RequestParam Long memberId,
-            @RequestParam LocalDate dateFrom,
-            @RequestParam LocalDate dateTo) {
-        List<ReservationResponse> searchedReservations =
-                reservationService.findSearchedReservations(themeId, memberId, dateFrom, dateTo);
-
-        return ResponseEntity.ok().body(searchedReservations);
     }
 
     @GetMapping("/time")
