412bee0f1cb4708370ec282e109d980569439a24
Zeus6768 <akaps6768@gmail.com>
2024-05-21 17:18:44 +0900

    test: 시간 관리 페이지 매핑

--- src/test/java/roomescape/controller/view/AdminViewControllerTest.java
+++ src/test/java/roomescape/controller/view/AdminViewControllerTest.java
@@ -3,14 +3,12 @@ package roomescape.controller.view;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Cookie;
 import roomescape.BaseTest;
 import roomescape.util.TokenProvider;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class AdminViewControllerTest extends BaseTest {
 
     @Autowired
@@ -36,6 +34,18 @@ class AdminViewControllerTest extends BaseTest {
         RestAssured.given().log().all()
                 .cookie(cookie)
                 .when().get("/admin/reservation")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("방탈출 시간 관리 페이지를 매핑한다.")
+    void time() {
+        String token = jwtProvider.create("admin@woowa.com");
+        Cookie cookie = new Cookie.Builder("token", token).build();
+        RestAssured.given().log().all()
+                .cookie(cookie)
+                .when().get("/admin/time")
                 .then().log().all()
                 .statusCode(200);
     }
