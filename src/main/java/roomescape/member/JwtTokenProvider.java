36f6d3bbb697a8cbeb326a362283968437e90752
tackyu <tack970913@gmail.com>
2024-05-22 01:44:45 +0900

    feat: 토큰 유효기간 설정 및 검증

--- src/main/java/roomescape/member/JwtTokenProvider.java
+++ src/main/java/roomescape/member/JwtTokenProvider.java
@@ -1,6 +1,8 @@
 package roomescape.member;
 
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,32 @@ import roomescape.member.domain.Member;
 public class JwtTokenProvider {
     @Value("${security.jwt.token.secret-key}")
     private String secretKey;
+    @Value("${security.jwt.token.expire-length}")
+    private long validityInMilliseconds;
 
     public String createToken(Member member) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
         return Jwts.builder()
                 .setSubject(String.valueOf(member.id()))
                 .claim("email", member.email())
                 .claim("name", member.name())
                 .claim("isAdmin", String.valueOf(member.isAdmin()))
+                .setExpiration(validity)
                 .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
                 .compact();
+    }
+
+    public boolean isExpiredToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                    .parseClaimsJws(token);
+
+            return claims.getBody().getExpiration().before(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            return true;
+        }
     }
 }
--- src/main/java/roomescape/member/config/WebMvcConfiguration.java
+++ src/main/java/roomescape/member/config/WebMvcConfiguration.java
@@ -8,20 +8,26 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import roomescape.member.AuthService;
+import roomescape.member.JwtTokenProvider;
 import roomescape.member.service.MemberService;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
     private final AuthService authService;
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public WebMvcConfiguration(AuthService authService, MemberService memberService) {
+    public WebMvcConfiguration(AuthService authService, MemberService memberService, JwtTokenProvider jwtTokenProvider) {
         this.authService = authService;
         this.memberService = memberService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CheckExpiredTokenInterceptor(jwtTokenProvider, authService))
+                .addPathPatterns("/admin/**");
+
         registry.addInterceptor(new CheckRoleInterceptor(authService, memberService))
                 .addPathPatterns("/admin/**");
     }
