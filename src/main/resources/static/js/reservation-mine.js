38b69424e37d267e555f2efed2de148d94b637ce
solarsalt <solar@woowahan.com>
2024-05-23 21:19:37 +0900

    feat:[2단계] 내 예약 수정 클라이언트 코드
    - 내 예약 목록 조회 시, 결제 정보 포함

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -46,7 +46,14 @@ function render(data) {
       };
       cancelCell.appendChild(cancelButton);
     } else { // 예약 완료 상태일 때
+      /*
+    TODO: [미션4 - 2단계] 내 예약 목록 조회 시,
+     예약 완료 상태일 때 결제 정보를 함께 보여주기
+     결제 정보 필드명은 자신의 response 에 맞게 변경하기
+     */
       row.insertCell(4).textContent = '';
+      row.insertCell(5).textContent = item.paymentKey;
+      row.insertCell(6).textContent = item.amount;
     }
   });
 }
