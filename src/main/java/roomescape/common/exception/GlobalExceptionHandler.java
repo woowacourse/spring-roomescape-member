3bbf00799ec503a09ddf5088c10ea0a597dee08b
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-04-29 19:49:03 +0900

    feat: 전역 예외 핸들링 추가 (IAE, ISE)

--- src/main/java/roomescape/common/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/common/exception/GlobalExceptionHandler.java
@@ -10,7 +10,25 @@ import java.time.format.DateTimeParseException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = DateTimeParseException.class)
-    public ResponseEntity<Void> handleException() {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<String> handleDateTimeParseException() {
+        return ResponseEntity.badRequest().body("잘못된 날짜/시간 형식입니다.");
+    }
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        String message = e.getMessage();
+        if (message == null) {
+            message = "잘못된 입력입니다.";
+        }
+        return ResponseEntity.badRequest().body(message);
+    }
+
+    @ExceptionHandler(value = IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalStateException(IllegalStateException e) {
+        String message = e.getMessage();
+        if (message == null) {
+            message = "실행할 수 없는 상태입니다.";
+        }
+        return ResponseEntity.badRequest().body(message);
     }
 }
