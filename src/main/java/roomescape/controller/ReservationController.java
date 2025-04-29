cf58d0c79997d5d4f6a5f52d88bde740a1697c55
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-04-29 17:54:12 +0900

    refactor: 예약 관련 API 응답 상태코드 변경
    - 저장: 200 -> 201
    - 삭제: 200 -> 204

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,11 +1,13 @@
 package roomescape.controller;
 
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.ReservationRequestDto;
 import roomescape.dto.ReservationResponseDto;
@@ -26,12 +28,14 @@ public class ReservationController {
     }
 
     @PostMapping("/reservations")
+    @ResponseStatus(HttpStatus.CREATED)
     public ReservationResponseDto createReservation(
         @RequestBody ReservationRequestDto reservationRequestDto) {
         return reservationService.saveReservation(reservationRequestDto);
     }
 
     @DeleteMapping("/reservations/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteReservation(@PathVariable(name = "id") Long id) {
         reservationService.deleteReservation(id);
     }
