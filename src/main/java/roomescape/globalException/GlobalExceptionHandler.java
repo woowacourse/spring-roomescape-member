6e151320a0d742dedb30975d77e04f0beb4563b2
KimDaehyeon <daehyeon3351@gmail.com>
2025-04-29 15:40:15 +0900

    feat: 역직렬화 시 발생 예외 핸들러 추가

--- src/main/java/roomescape/globalException/GlobalExceptionHandler.java
+++ src/main/java/roomescape/globalException/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package roomescape.globalException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +16,16 @@ public class GlobalExceptionHandler {
             .body(new ErrorResponse(
                 String.valueOf(e.getStatusValue()),
                 e.getMessage()
+            ));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(
+                String.valueOf(HttpStatus.BAD_REQUEST),
+                "잘못된 형식의 요청입니다."
             ));
     }
 }
