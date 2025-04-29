1c34d99d0b5914a5f7af109e5cedfb8e140c1637
RIWON <121172065+awrion3@users.noreply.github.com>
2025-04-29 16:19:06 +0900

    feat: 방탈출 예약 내역 추가 API 구현

--- src/main/java/roomescape/reservation/controller/ReservationApiController.java
+++ src/main/java/roomescape/reservation/controller/ReservationApiController.java
@@ -1,6 +1,7 @@
 package roomescape.reservation.controller;
 
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,7 +25,7 @@ public class ReservationApiController {
 
     @PostMapping
     public ResponseEntity<ReservationResponse> add(@RequestBody ReservationRequest reservationRequest) {
-        return ResponseEntity.ok(reservationService.add(reservationRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(reservationService.add(reservationRequest));
     }
 
     @GetMapping
