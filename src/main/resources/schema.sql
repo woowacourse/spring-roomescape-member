6f8e29fa13e94dadbe39bdbc152b455b1acc3055
DongchannN <ldc991104@gmail.com>
2025-04-29 15:34:24 +0900

    docs: 기능 요구사항 작성

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS RESERVATION, RESERVATION_TIME;
+
+CREATE TABLE RESERVATION_TIME
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE RESERVATION
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    date varchar(255) NOT NULL,
+    time_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
+);
