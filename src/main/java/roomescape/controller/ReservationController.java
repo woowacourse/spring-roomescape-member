0e26cfbc766b85017e99a6c35324356aa8db92ae
Jin409 <sualng123@gmail.com>
2025-04-29 15:41:32 +0900

    fix: 기존 기능 정상 작동하도록 수정

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,6 +1,7 @@
 package roomescape.controller;
 
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.ReservationRequestDto;
 import roomescape.dto.ReservationResponseDto;
-import roomescape.model.Reservation;
 import roomescape.service.ReservationService;
 
 @RestController
@@ -31,13 +32,13 @@ public class ReservationController {
     }
 
     @PostMapping
-    public ResponseEntity<ReservationResponseDto> addReservation(
+    @ResponseStatus(HttpStatus.CREATED)
+    public ReservationResponseDto addReservation(
             @RequestBody ReservationRequestDto reservationRequestDto) {
         try {
-            ReservationResponseDto reservationResponseDto = reservationService.saveReservation(reservationRequestDto);
-            return ResponseEntity.ok(reservationResponseDto);
+            return reservationService.saveReservation(reservationRequestDto);
         } catch (NullPointerException | IllegalArgumentException e) {
-            return ResponseEntity.badRequest().build();
+            return null;
         }
     }
 
--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -1,6 +1,7 @@
 package roomescape.controller;
 
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.ReservationTimeRequestDto;
 import roomescape.dto.ReservationTimeResponseDto;
@@ -30,11 +32,10 @@ public class ReservationTimeController {
     }
 
     @PostMapping
-    public ResponseEntity<ReservationTimeResponseDto> addTime(
+    @ResponseStatus(HttpStatus.CREATED)
+    public ReservationTimeResponseDto addTime(
             @RequestBody ReservationTimeRequestDto reservationTimeRequestDto) {
-        ReservationTimeResponseDto reservationTimeResponseDto = reservationTimeService.saveTime(
-                reservationTimeRequestDto);
-        return ResponseEntity.ok(reservationTimeResponseDto);
+        return reservationTimeService.saveTime(reservationTimeRequestDto);
     }
 
     @DeleteMapping("/{id}")
