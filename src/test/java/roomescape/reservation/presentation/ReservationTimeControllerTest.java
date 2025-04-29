71a8d35b9682c6e8e8fcf76f07c0be124679640a
sonjh919 <kingjh1125@gmail.com>
2025-04-29 20:10:48 +0900

    test: 예약 시간 검증 테스트 추가

--- src/test/java/roomescape/reservation/presentation/ReservationTimeControllerTest.java
+++ src/test/java/roomescape/reservation/presentation/ReservationTimeControllerTest.java
@@ -31,6 +31,46 @@ public class ReservationTimeControllerTest {
     }
 
     @Test
+    @DisplayName("시작 시간은 LocalTime 형식을 만족시켜야 한다.")
+    void createTimeExceptionTest() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10-00");
+
+        // when-then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(400);
+    }
+
+    @Test
+    @DisplayName("중복된 시간 추가는 불가능하다.")
+    void createTimeDuplicateExceptionTest() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10:00");
+
+        // when
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(201);
+
+        // then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(400);
+    }
+
+    @Test
     @DisplayName("시간 조회 테스트")
     void getTimeTest() {
         // given
@@ -77,6 +117,39 @@ public class ReservationTimeControllerTest {
                 .when().delete("/times/1")
                 .then().log().all()
                 .statusCode(204);
+    }
+
+    @Test
+    @DisplayName("예약이 이미 존재하는 시간은 삭제할 수 없다.")
+    void deleteTimeExceptionTest() {
+        // given
+        Map<String, String> reservationTimeParams = new HashMap<>();
+        reservationTimeParams.put("startAt", "10:00");
+
+        Map<String, String> reservationParams = new HashMap<>();
+        reservationParams.put("name", "브라운");
+        reservationParams.put("date", "2025-08-05");
+        reservationParams.put("timeId", "1");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationTimeParams)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(201);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationParams)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201);
+
+        // when-then
+        RestAssured.given().log().all()
+                .when().delete("/times/1")
+                .then().log().all()
+                .statusCode(400);
     }
 
 }
