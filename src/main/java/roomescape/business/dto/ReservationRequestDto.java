f0d4676708fa70e6b564cf3eab4f21bb2b53fee2
Yehyeok Bang <qkddpgur318@gmail.com>
2025-04-29 20:14:29 +0900

    feat: 요청 본문 유효성 검증 기능 구현
    - 요청 본문 필드가 비어있는지 확인합니다.
    
    Co-authored-by: ppparkta <pppppark2@gmail.com>

--- src/main/java/roomescape/business/dto/ReservationRequestDto.java
+++ src/main/java/roomescape/business/dto/ReservationRequestDto.java
@@ -1,10 +1,12 @@
 package roomescape.business.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record ReservationRequestDto(
-        String name,
-        LocalDate date,
-        Long timeId
+        @NotEmpty String name,
+        @NotNull LocalDate date,
+        @NotNull Long timeId
 ) {
 }
