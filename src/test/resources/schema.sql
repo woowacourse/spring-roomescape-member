eee14175044fccf78ab3bad23660d3c16fc2c186
bowook <mbosx316@naver.com>
2025-04-29 20:50:41 +0900

    chore : 테마 테이블 스키마 추가

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -2,16 +2,27 @@ CREATE TABLE IF NOT EXISTS reservation_times
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     start_at VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (start_at)
+);
+
+CREATE TABLE IF NOT EXISTS theme
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    thumbnail   VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS reservations
 (
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255) NOT NULL,
-    date    VARCHAR(255) NOT NULL,
-    time_id BIGINT       NOT NULL,
-
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL,
+    date     VARCHAR(255) NOT NULL,
+    time_id  BIGINT       NOT NULL,
+    theme_id BIGINT       NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_times (id)
+    FOREIGN KEY (time_id) REFERENCES reservation_times (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
