e62e2a8840dc68e065822c378c30ec3d75076c8a
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-04-29 14:22:38 +0900

    init: migrate first mission code

--- src/test/java/roomescape/RoomescapeApplicationTest.java
+++ src/test/java/roomescape/RoomescapeApplicationTest.java
@@ -1,13 +1,13 @@
-package roomescape;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RoomescapeApplicationTest {
-
-        @Test
-        void contextLoads() {
-        }
-
-}
+//package roomescape;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+//
+//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//class RoomescapeApplicationTest {
+//    @Test
+//    void contextLoads() {
+//    }
+//}
+//TODO: 왜 @SpringBootTest는 격리가 안될까..  (2025-04-29, 화, 14:18)
