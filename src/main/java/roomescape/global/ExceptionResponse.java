b343b6b9a05a710ee61ee74af0328adbb7546d8d
taek2222 <oot309@daum.net>
2025-04-29 20:34:05 +0900

    feat: 유효하지 않은 요청 값 에러 핸들링 추가

---
+++ src/main/java/roomescape/global/ExceptionResponse.java
@@ -0,0 +1,6 @@
+package roomescape.global;
+
+public record ExceptionResponse(
+        String message
+) {
+}
---
+++ src/main/java/roomescape/global/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package roomescape.global;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ExceptionResponse handleHttpMessageNotReadableException() {
+        return new ExceptionResponse("잘못된 요청 형식입니다.");
+    }
+}
