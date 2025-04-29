690852a21a09e7b3be684b32ff5e3975e9c88db2
DEV_WOONG <ywcsuwon@gmail.com>
2025-04-29 19:37:11 +0900

    feat: 이름 길이, 숫자 포함, 과거 날짜, 예약가능 날짜에 대한 검증 추가

--- src/main/java/roomescape/entity/Reservation.java
+++ src/main/java/roomescape/entity/Reservation.java
@@ -1,6 +1,7 @@
 package roomescape.entity;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Reservation {
 
@@ -10,20 +11,31 @@ public class Reservation {
     private final ReservationTime time;
 
     private Reservation(Long id, String name, LocalDate date, ReservationTime time) {
-        validateTime(time);
+        if (name.length() > 10) {
+            throw new IllegalArgumentException("이름은 10자를 넘어갈 수 없습니다.");
+        }
+        for (char c : name.toCharArray()) {
+            if (Character.isDigit(c)) {
+                throw new IllegalArgumentException("이름은 숫자를 포함할 수 없습니다.");
+            }
+        }
+        if (time == null) {
+            throw new IllegalArgumentException("존재하지 않는 시간입니다.");
+        }
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
     }
 
-    private void validateTime(ReservationTime time) {
-        if (time == null) {
-            throw new IllegalArgumentException("존재하지 않는 시간입니다.");
-        }
-    }
-
     public static Reservation beforeSave(final String name, final LocalDate date, final ReservationTime time) {
+        if (date.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("지나간 날짜는 예약할 수 없습니다.");
+        }
+        long minusDays = Period.between(date, LocalDate.now()).getDays();
+        if (minusDays > 7) {
+            throw new IllegalArgumentException("예약은 1주일 전부터 가능합니다.");
+        }
         return new Reservation(null, name, date, time);
     }
 
