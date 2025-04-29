010a58974d1e1b5137c6a6aaa334390f1d23c657
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-04-29 20:40:35 +0900

    RED) test(controller) : ReservationController 필드값 null 예외사항 테스트

---
+++ src/test/java/roomescape/ReservationControllerTest.java
@@ -0,0 +1,46 @@
+package roomescape;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReservationControllerTest {
+    void Test_ReservationTime_Post() {
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10:00");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(201);
+    }
+
+    @Test
+    @DisplayName("필드값 null 검증")
+    void test1() {
+        Test_ReservationTime_Post();
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", null);
+        params.put("date", "2025-08-05");
+        params.put("timeId", 1);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(400);
+    }
+
+
+}
