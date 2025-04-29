bfe7dd622113ea9de10426724cb926cf8b52b0f1
ChoiDongjun <adong0808@naver.com>
2025-04-29 15:14:13 +0900

    feat: 유효하지 않은 입력 시 400 에러 처리

---
+++ src/main/java/roomescape/controller/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package roomescape.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiFailResponse> handleException(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest().body(new ApiFailResponse("요청 형식이 올바르지 않습니다."));
+    }
+}
--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -1,19 +1,14 @@
 package roomescape.controller;
 
-import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import roomescape.controller.request.CreateReservationTimeRequest;
 import roomescape.controller.response.ReservationTimeResponse;
 import roomescape.service.ReservationTimeService;
 import roomescape.service.result.ReservationTimeResult;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/times")
