8737b9424376a79a6a1b195979cefc6088e41f23
GiSeok Kang <ime.rokz@gmail.com>
2025-04-29 20:32:35 +0900

    feat: 예약자명, 날짜, 시간 검증 및 예외 처리

--- src/main/java/roomescape/reservationTime/dto/ReservationTimeRequest.java
+++ src/main/java/roomescape/reservationTime/dto/ReservationTimeRequest.java
@@ -3,4 +3,10 @@ package roomescape.reservationTime.dto;
 import java.time.LocalTime;
 
 public record ReservationTimeRequest(LocalTime startAt) {
+    public ReservationTimeRequest {
+        if (startAt == null) {
+            throw new IllegalArgumentException("시간은 null 일 수 없습니다.");
+        }
+
+    }
 }
