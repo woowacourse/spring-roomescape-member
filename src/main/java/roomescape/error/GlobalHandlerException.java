9d2176d3425d08ced5cf2c7a1ef1c2fe19aeb528
ryan-dia <cjfdnjs158@naver.com>
2025-04-29 20:46:48 +0900

    feat: 사용자가 잘못된 요청을 했을 때 예외 응답 구현
    Co-authored-by: joon6093 <joon6093@naver.com>

---
+++ src/main/java/roomescape/error/GlobalHandlerException.java
@@ -0,0 +1,24 @@
+package roomescape.error;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalHandlerException extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({
+            ReservationException.class,
+            NullPointerException.class,
+    })
+    public ResponseEntity<Object> handleReservationException(final Exception ex, final WebRequest request) {
+        final ProblemDetail body = super.createProblemDetail(ex, HttpStatus.BAD_REQUEST, ex.getMessage(), null,
+                null, request);
+        return super.handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
