b7a0c7895ec99f685fe20f63a01c634ff019b481
Jin409 <sualng123@gmail.com>
2025-04-29 16:05:19 +0900

    feat: 예약 생성 시 예약자명 / 날짜 / 시간에 유효하지 않은 값이 입력 되었을 때 예외 처리 하도록 구현

--- src/main/java/roomescape/dto/ReservationRequestDto.java
+++ src/main/java/roomescape/dto/ReservationRequestDto.java
@@ -1,19 +1,25 @@
 package roomescape.dto;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import roomescape.model.Reservation;
 import roomescape.model.ReservationTime;
 
 public record ReservationRequestDto(
         String name,
-        LocalDate date,
+        String rawDate,
         Long timeId
 ) {
     public Reservation convertToReservation() {
-        return new Reservation(
-                this.name,
-                this.date,
-                new ReservationTime(timeId)
-        );
+        try {
+            LocalDate date = LocalDate.parse(rawDate);
+            return new Reservation(
+                    this.name,
+                    date,
+                    new ReservationTime(timeId)
+            );
+        } catch (DateTimeParseException e) {
+            throw new IllegalStateException("날짜형식이 잘못되었습니다");
+        }
     }
 }
