728a2f45654268113b398286d3478c5e4a91c248
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-04-29 20:17:39 +0900

    RED) test(controller) : ReservationTimeController 예외 테스트

---
+++ src/test/java/roomescape/ReservationTimeControllerTest.java
@@ -0,0 +1,46 @@
+package roomescape;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReservationTimeControllerTest {
+    @Test
+    @DisplayName("시작 시간 형식 검증")
+    void test1() {
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "미친형식");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(400);
+    }
+
+    @Test
+    @DisplayName("시작 시간 null 검증")
+    void test2() {
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", null);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(400)
+                .body(containsString("Cannot invoke \"java.time.LocalTime.toString()\" because \"startAt\" is null"));
+    }
+}
