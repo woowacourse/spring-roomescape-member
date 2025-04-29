5305478f0f775c39d1b65ef8313f016818ccd977
DEV_WOONG <ywcsuwon@gmail.com>
2025-04-29 15:10:37 +0900

    feat: 예약 추가 최소/최대 시간 검증 추가

--- src/test/java/roomescape/ReservationTimeTest.java
+++ src/test/java/roomescape/ReservationTimeTest.java
@@ -1,24 +1,37 @@
 package roomescape;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import roomescape.entity.ReservationTime;
 
 class ReservationTimeTest {
 
-    @Test
-    @DisplayName("예약 가능한 시간이 아닐 때 예약하면 예외가 발생한다.")
-    void whenInvalidTimeReservationThrowException() {
+    @ParameterizedTest
+    @CsvSource({"10:00", "23:00"})
+    @DisplayName("시간으로 생성할 수 있다.")
+    void canCreateWithTime(String timeStrValue) {
         //given
         //when
         //then
-        assertThatThrownBy(() -> new ReservationTime(LocalTime.of(7, 0)))
+        assertThatCode(() -> new ReservationTime(LocalTime.parse(timeStrValue)))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"09:59", "23:01"})
+    @DisplayName("예약 가능한 시간이 아닐 때 예약하면 예외가 발생한다.")
+    void whenInvalidTimeReservationThrowException(String timeStrValue) {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> new ReservationTime(LocalTime.parse(timeStrValue)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("예약할 수 없는 시간입니다.");
     }
-
-
 }
