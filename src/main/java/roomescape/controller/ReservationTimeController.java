12aa548ebb2af4d17167d8e4117bace3a367162c
yesjuhee <juhee0924@g.skku.edu>
2025-04-29 15:58:38 +0900

    feat: `HH:mm` 형식으로 시간을 입력해야한다

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -2,7 +2,10 @@ package roomescape.controller;
 
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,5 +43,10 @@ public class ReservationTimeController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteReservationTime(@PathVariable("id") final long id) {
         reservationTimeService.deleteReservationTime(id);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    private ResponseEntity<String> handleJsonParseError(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest().body("요청 형식이 올바르지 않습니다.");
     }
 }
