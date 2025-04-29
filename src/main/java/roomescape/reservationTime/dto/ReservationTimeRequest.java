7fa6cac5b26e45b98b6e291800a45870387ced23
RIWON <121172065+awrion3@users.noreply.github.com>
2025-04-29 17:48:35 +0900

    feat: 예약 시간 추가 시 dto 검증 로직 추가

--- src/main/java/roomescape/reservationTime/dto/ReservationTimeRequest.java
+++ src/main/java/roomescape/reservationTime/dto/ReservationTimeRequest.java
@@ -1,6 +1,12 @@
 package roomescape.reservationTime.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 
-public record ReservationTimeRequest(LocalTime startAt) {
+public record ReservationTimeRequest(@JsonFormat(pattern = "HH:mm") LocalTime startAt) {
+    public ReservationTimeRequest {
+        if(startAt == null) {
+            throw new IllegalArgumentException("[ERROR] 시간을 입력해주세요.");
+        }
+    }
 }
