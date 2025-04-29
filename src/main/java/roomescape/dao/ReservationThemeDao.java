02003ebb88ff79fb19e71fab81d33bc9eae05401
minSsan <msp1125@hanyang.ac.kr>
2025-04-29 20:37:50 +0900

    feat: ThemeDao 구현

---
+++ src/main/java/roomescape/dao/ReservationThemeDao.java
@@ -0,0 +1,18 @@
+package roomescape.dao;
+
+import roomescape.entity.ReservationThemeEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReservationThemeDao {
+    ReservationThemeEntity save(ReservationThemeEntity entity);
+
+    List<ReservationThemeEntity> findAll();
+
+    boolean deleteById(Long id);
+
+    Optional<ReservationThemeEntity> findById(Long id);
+
+    Optional<ReservationThemeEntity> findByName(String name);
+}
---
+++ src/main/java/roomescape/entity/ReservationThemeEntity.java
@@ -0,0 +1,35 @@
+package roomescape.entity;
+
+public class ReservationThemeEntity {
+
+    private final Long id;
+
+    private String name;
+
+    private String description;
+
+    private String thumbnail;
+
+    public ReservationThemeEntity(Long id, String name, String description, String thumbnail) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.thumbnail = thumbnail;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+}
