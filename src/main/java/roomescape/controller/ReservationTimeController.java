45e0c3c620d3bb4e22682e2ccd2c31dabf146471
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-04-29 17:41:16 +0900

    refactor: 예약 시간 관련 API 응답 상태코드 변경
    - 저장: 200 -> 201
    - 삭제: 200 -> 204

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -1,11 +1,13 @@
 package roomescape.controller;
 
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.ReservationTimeRequestDto;
 import roomescape.dto.ReservationTimeResponseDto;
@@ -26,12 +28,14 @@ public class ReservationTimeController {
     }
 
     @PostMapping("/times")
+    @ResponseStatus(HttpStatus.CREATED)
     public ReservationTimeResponseDto saveReservationTime(
         @RequestBody ReservationTimeRequestDto reservationTimeRequestDto) {
         return reservationTimeService.saveReservationTime(reservationTimeRequestDto);
     }
 
     @DeleteMapping("/times/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteReservationTime(@PathVariable(name = "id") Long id) {
         reservationTimeService.deleteReservationTime(id);
     }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -36,4 +36,28 @@ public class MissionStepTest {
             .body("size()",
                 is(0)); // 아직 생성 요청이 없으니 Controller에서 임의로 넣어준 Reservation 갯수 만큼 검증하거나 0개임을 확인하세요.
     }
+
+    @Test
+    void 칠단계() {
+        Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10:00");
+
+        RestAssured.given().log().all()
+            .contentType(ContentType.JSON)
+            .body(params)
+            .when().post("/times")
+            .then().log().all()
+            .statusCode(201);
+
+        RestAssured.given().log().all()
+            .when().get("/times")
+            .then().log().all()
+            .statusCode(200)
+            .body("size()", is(1));
+
+        RestAssured.given().log().all()
+            .when().delete("/times/1")
+            .then().log().all()
+            .statusCode(204);
+    }
 }
