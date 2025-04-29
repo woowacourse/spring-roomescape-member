2c2cfb3dd800ebe75ffd9ea6fcdf18f0b9c433dd
junhaa <2171326@hansung.ac.kr>
2025-04-29 15:56:26 +0900

    feat: 시간 생성 시 시작 시간에 유효하지 않은 값 입력 시 예외를 반환하는 기능 구현

---
+++ src/main/java/roomescape/reservation/controller/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package roomescape.reservation.controller;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleMappingException(IllegalArgumentException exception){
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<String> handleInvalidFormatException(InvalidFormatException ex){
+        return ResponseEntity.badRequest().body("유효하지 않은 형식입니다.");
+    }
+}
