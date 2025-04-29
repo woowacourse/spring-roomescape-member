c7ee64804f519558e7f3993bdd63b315908df805
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-04-29 19:45:07 +0900

    feat(controller) : ReservationTimeController 예외처리
    - startAt이 null 일 경우 예외처리

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,11 @@ public class ReservationTimeController {
 
     public ReservationTimeController(ReservationTimeService reservationTimeService) {
         this.reservationTimeService = reservationTimeService;
+    }
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNullStartAtException(NullPointerException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+
     }
 
     @PostMapping()
