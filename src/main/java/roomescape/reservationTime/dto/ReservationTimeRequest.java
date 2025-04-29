ff59d33d6ee327d22df431c48bceac283553f535
RIWON <121172065+awrion3@users.noreply.github.com>
2025-04-29 17:56:28 +0900

    style: ToDo 주석 추가

--- src/main/java/roomescape/reservationTime/dto/ReservationTimeRequest.java
+++ src/main/java/roomescape/reservationTime/dto/ReservationTimeRequest.java
@@ -5,6 +5,7 @@ import java.time.LocalTime;
 
 public record ReservationTimeRequest(@JsonFormat(pattern = "HH:mm") LocalTime startAt) {
     public ReservationTimeRequest {
+        // TODO : "" -> null로 바뀌어서 처리됨
         if(startAt == null) {
             throw new IllegalArgumentException("[ERROR] 시간을 입력해주세요.");
         }
