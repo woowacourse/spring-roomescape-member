10685ba715086473f75f5c27de52cff311e45df4
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-04-29 19:58:43 +0900

    feat: ReservationTimeRequestDto 검증 로직 추가

---
+++ src/test/java/roomescape/dto/ReservationTimeRequestDtoTest.java
@@ -0,0 +1,27 @@
+package roomescape.dto;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ReservationTimeRequestDtoTest {
+
+    @DisplayName("잘못된 날짜 형식이 들어온다면, 예외가 발생해야 한다")
+    @Test
+    void invalid_time_format_then_throw_exception() {
+        assertThatThrownBy(() -> new ReservationTimeRequestDto(
+            "25:30"
+        ));
+    }
+
+    @DisplayName("시간이 들어온다면, 성공한다.")
+    @Test
+    void success_time() {
+        assertThatCode(() -> new ReservationTimeRequestDto(
+            "13:13"
+        ));
+    }
+
+}
