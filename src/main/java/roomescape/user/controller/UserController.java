fdbeee266c0010652058d4242526b6d24d6687f6
threepebbles <threepebbles@naver.com>
2025-04-30 20:23:41 +0900

    feat : 사용자 예약 화면 응답 기능 추가

---
+++ src/main/java/roomescape/user/controller/UserController.java
@@ -0,0 +1,13 @@
+package roomescape.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "/reservation";
+    }
+}
