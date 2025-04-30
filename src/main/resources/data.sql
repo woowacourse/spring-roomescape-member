31e10d7c727b7e0c58eb3f004829446c23561155
DongchannN <ldc991104@gmail.com>
2025-04-30 11:13:42 +0900

    feat: 테마 테이블을 추가하여 스키마에 반영하기

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,15 @@ VALUES ('13:40'),
        ('15:40')
 ;
 
-INSERT INTO RESERVATION(name, date, time_id)
-VALUES ('브라운', '2023-03-03', '1'),
-       ('솔라', '2023-03-03', '2'),
-       ('네오', '2023-03-05', '3')
+INSERT INTO THEME(name, description, thumbnail)
+VALUES ('첫번째', '일번방', '썸네일1'),
+       ('두번째', '이번방', '썸네일2'),
+       ('세번째', '삼번방', '썸네일3')
+;
+
+
+INSERT INTO RESERVATION(name, date, time_id, theme_id)
+VALUES ('브라운', '2023-03-03', '1', '1'),
+       ('솔라', '2023-03-03', '2', '2'),
+       ('네오', '2023-03-05', '3', '3')
 ;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS RESERVATION, RESERVATION_TIME;
+DROP TABLE IF EXISTS RESERVATION, THEME, RESERVATION_TIME;
 
 CREATE TABLE RESERVATION_TIME
 (
@@ -7,12 +7,23 @@ CREATE TABLE RESERVATION_TIME
     PRIMARY KEY (id)
 );
 
-CREATE TABLE RESERVATION
+CREATE TABLE theme
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    thumbnail VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE reservation
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    date varchar(255) NOT NULL,
+    date VARCHAR(255) NOT NULL,
     time_id BIGINT,
+    theme_id BIGINT,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
