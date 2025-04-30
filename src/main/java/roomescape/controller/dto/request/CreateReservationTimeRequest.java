fd48b34562facd5cf080febc2a0e0c8933f410e2
RaZel713 <razel7130@gmail.com>
2025-04-30 10:45:21 +0900

    refactor: 미사용 메서드 삭제

--- src/main/java/roomescape/controller/dto/request/CreateReservationTimeRequest.java
+++ src/main/java/roomescape/controller/dto/request/CreateReservationTimeRequest.java
@@ -1,11 +1,6 @@
 package roomescape.controller.dto.request;
 
 import java.time.LocalTime;
-import roomescape.domain.ReservationTime;
 
 public record CreateReservationTimeRequest(LocalTime startAt) {
-
-    public ReservationTime toReservationTime() {
-        return new ReservationTime(startAt);
-    }
 }
