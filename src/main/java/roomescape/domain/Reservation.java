88f51983d6d0c0a5bfd1c6a65e3af8cfa84f0266
JO-eusan <goodsan0330@gmail.com>
2025-04-30 14:02:29 +0900

    fix: 관리자 과거 시간 예약 추가 허용

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,8 +1,6 @@
 package roomescape.domain;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import roomescape.exception.NotCorrectDateTimeException;
 
 public class Reservation {
 
@@ -13,21 +11,11 @@ public class Reservation {
     private final Theme theme;
 
     public Reservation(Long id, String name, LocalDate date, ReservationTime time, Theme theme) {
-        validateDateTime(date, time);
-
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
         this.theme = theme;
-    }
-
-    private void validateDateTime(LocalDate date, ReservationTime time) {
-        LocalDateTime now = LocalDateTime.now();
-        if (date.isBefore(now.toLocalDate()) ||
-            (date.isEqual(now.toLocalDate()) && time.isBefore(now.toLocalTime()))) {
-            throw new NotCorrectDateTimeException("지나간 날짜와 시간에 대한 예약 생성은 불가능하다.");
-        }
     }
 
     public Long getId() {
