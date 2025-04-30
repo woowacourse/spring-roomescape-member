0779d7b2bac613e52486632ef48fb251caf81f68
Jin409 <sualng123@gmail.com>
2025-04-30 18:26:49 +0900

    feat: CustomException 구현 및 케이스 별 예외 추가

--- src/test/java/roomescape/dto/ReservationRequestDtoTest.java
+++ src/test/java/roomescape/dto/ReservationRequestDtoTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import roomescape.dto.reservation.ReservationRequestDto;
 import roomescape.model.ReservationTime;
 import roomescape.model.Theme;
 
