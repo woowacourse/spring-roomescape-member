7fcaec3dd0dc77aec6a4253a879663de615643ef
jhan0121 <jhan0121@gmail.com>
2025-04-30 16:07:08 +0900

    test: 임시 데이터 값 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,4 @@ VALUES ('레벨2 탈출',
         '우테코 레벨2를 탈출하는 내용입니다.',
         'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 INSERT INTO reservation(name, date, time_id, theme_id)
-VALUES ('test', '2025-01-01', 1, 1);
+VALUES ('test', '2025-07-01', 1, 1);
