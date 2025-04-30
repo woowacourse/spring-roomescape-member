e7788c9dc190a9f277d68943b8ae1d7d60bd843a
jhan0121 <jhan0121@gmail.com>
2025-04-30 16:06:38 +0900

    refactor: controller 네이밍 변경

--- src/main/java/roomescape/controller/ui/AdminPageController.java
+++ src/main/java/roomescape/controller/ui/AdminPageController.java
@@ -1,4 +1,4 @@
-package roomescape.controller.view;
+package roomescape.controller.ui;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/admin")
 @Controller
-public class AdminController {
+public class AdminPageController {
 
     @GetMapping
     public String getAdminPage() {
