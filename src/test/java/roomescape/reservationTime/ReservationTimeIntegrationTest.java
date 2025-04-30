b7ab881f549165db0de61bc462b365c7f918bc8a
GiSeok Kang <ime.rokz@gmail.com>
2025-04-30 11:42:14 +0900

    test: 예약 시간 검증 테스트 구현

--- src/test/java/roomescape/reservationTime/ReservationTimeIntegrationTest.java
+++ src/test/java/roomescape/reservationTime/ReservationTimeIntegrationTest.java
@@ -4,12 +4,17 @@ import static org.hamcrest.CoreMatchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import java.util.HashMap;
 import java.util.Map;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.common.exceptionHandler.dto.ExceptionResponse;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -42,5 +47,48 @@ public class ReservationTimeIntegrationTest {
                 .when().delete("/times/1")
                 .then().log().all()
                 .statusCode(204);
+    }
+
+    @DisplayName("시간이 null인 상태로 생성 요청 시 400 응답을 준다.")
+    @Test
+    void when_given_null_time() {
+        Map<String, Object> reservationTime = new HashMap<>();
+        reservationTime.put("startAt", null);
+
+        ExceptionResponse expected = new ExceptionResponse(400, "[ERROR] 요청 입력이 잘못되었습니다.", "/times");
+
+        Response response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationTime)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(400)
+                .extract()
+                .response();
+
+        ExceptionResponse actual = response.as(ExceptionResponse.class);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("잘못된 시간으로 생성 요청 시 400 응답을 준다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "ab", "123", "2시5분", "2023:12:03", "10-39"})
+    void when_given_wrong_time(final String time) {
+        Map<String, Object> reservationTime = new HashMap<>();
+        reservationTime.put("startAt", time);
+
+        ExceptionResponse expected = new ExceptionResponse(400, "[ERROR] 요청 시간 형식이 맞지 않습니다.", "/times");
+
+        Response response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservationTime)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(400)
+                .extract()
+                .response();
+
+        ExceptionResponse actual = response.as(ExceptionResponse.class);
+        Assertions.assertThat(actual).isEqualTo(expected);
     }
 }
