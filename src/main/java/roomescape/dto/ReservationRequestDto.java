3e54512f0932fc2d1f4fee3b995ab3a71be6602d
Jin409 <sualng123@gmail.com>
2025-04-30 17:56:38 +0900

    feat: 도메인 유효성 검사 로직 추가

--- src/main/java/roomescape/dto/ReservationRequestDto.java
+++ src/main/java/roomescape/dto/ReservationRequestDto.java
@@ -12,23 +12,23 @@ public record ReservationRequestDto(
         Long timeId,
         Long themeId
 ) {
-//    public ReservationRequestDto {
-//        if (name == null || name.isBlank()) {
-//            throw new IllegalArgumentException("예약자명은 null이거나 공백일 수 없습니다");
-//        }
-//
-//        if (date == null || date.isBlank()) {
-//            throw new IllegalArgumentException("예약 날짜는 null이거나 공백일 수 없습니다");
-//        }
-//
-//        if (timeId == null) {
-//            throw new IllegalArgumentException("예약 시각은 null 일 수 없습니다.");
-//        }
-//
-//        if (themeId == null) {
-//            throw new IllegalArgumentException("테마는 null 일 수 없습니다.");
-//        }
-//    }
+    public ReservationRequestDto {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("예약자명은 null이거나 공백일 수 없습니다");
+        }
+
+        if (date == null || date.isBlank()) {
+            throw new IllegalArgumentException("예약 날짜는 null이거나 공백일 수 없습니다");
+        }
+
+        if (timeId == null) {
+            throw new IllegalArgumentException("예약 시각은 null 일 수 없습니다.");
+        }
+
+        if (themeId == null) {
+            throw new IllegalArgumentException("테마는 null 일 수 없습니다.");
+        }
+    }
 
     public Reservation convertToReservation(ReservationTime reservationTime, Theme theme) {
         try {
