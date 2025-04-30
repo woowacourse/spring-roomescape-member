fdd3f50946d7268a16edb4876c40f6fc50de741e
juno <wnsgh732@gmail.com>
2025-04-30 20:51:52 +0900

    feat: 테스트용 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,13 @@ INSERT INTO reservation_time(start_at) values ('16:00');
 INSERT INTO reservation_time(start_at) values ('18:00');
 INSERT INTO reservation_time(start_at) values ('20:00');
 INSERT INTO theme(name, description, thumbnail) values('탈출', '레벨2 탈출', 'asdf');
+INSERT INTO theme(name, description, thumbnail) values('탈출1', '레벨2 탈출', 'asdf');
+INSERT INTO theme(name, description, thumbnail) values('탈출2', '레벨2 탈출', 'asdf');
+INSERT INTO theme(name, description, thumbnail) values('탈출3', '레벨2 탈출', 'asdf');
+INSERT INTO reservation(name, date, time_id, theme_id) values ('메이', '2025-04-24', 1, 1);
+INSERT INTO reservation(name, date, time_id, theme_id) values ('메이', '2025-04-25', 1, 2);
+INSERT INTO reservation(name, date, time_id, theme_id) values ('메이', '2025-04-26', 1, 3);
+INSERT INTO reservation(name, date, time_id, theme_id) values ('메이', '2025-04-27', 1, 4);
+INSERT INTO reservation(name, date, time_id, theme_id) values ('메이', '2025-04-28', 1, 4);
+INSERT INTO reservation(name, date, time_id, theme_id) values ('메이', '2025-04-26', 2, 2);
+
