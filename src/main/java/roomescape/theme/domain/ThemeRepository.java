b4ffad895bcc7a001223320f341d63af980983be
GiSeok Kang <ime.rokz@gmail.com>
2025-04-30 16:21:55 +0900

    refactor: repository 인터페이스 추상화

--- src/main/java/roomescape/theme/domain/ThemeRepository.java
+++ src/main/java/roomescape/theme/domain/ThemeRepository.java
@@ -1,17 +1,14 @@
 package roomescape.theme.domain;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface ThemeRepository {
 
     Long save(Theme theme);
 
-    List<Theme> findPopularThemes(LocalDate start, LocalDate end, int popularCount);
-
     List<Theme> findAll();
 
-    boolean deleteById(Long id);
+    int deleteById(Long id);
 
-    Theme findById(Long id);
+    Theme findById(final Long id);
 }
