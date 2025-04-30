75d6b0e57371491fff276938d5088e2e5a5121ba
황재웅 <wodnd0131@naver.com>
2025-04-30 17:00:12 +0900

    test: 테마 추가 및 삭제 API 검증

--- src/test/java/roomescape/service/ThemeServiceTest.java
+++ src/test/java/roomescape/service/ThemeServiceTest.java
@@ -1,14 +1,17 @@
 package roomescape.service;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import roomescape.dto.ThemeResponse;
-import roomescape.fake.ThemeFakeRepository;
-import roomescape.repository.ThemeRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import roomescape.dto.ThemeRequest;
+import roomescape.dto.ThemeResponse;
+import roomescape.exceptions.EntityNotFoundException;
+import roomescape.fake.ThemeFakeRepository;
+import roomescape.repository.ThemeRepository;
 
 class ThemeServiceTest {
 
@@ -24,5 +27,24 @@ class ThemeServiceTest {
         // then
         assertThat(themeResponses.size()).isEqualTo(1);
         assertThat(themeResponses.getFirst().name()).isEqualTo("레벨2 탈출");
+    }
+
+    @Test
+    @DisplayName("엔티티를 저장한 후, DTO로 반환한다.")
+    void test_postReservationTime() {
+        //given
+        String given = "테스트";
+        ThemeRequest request = new ThemeRequest(given, "설명", "썸네일");
+        //when
+        ThemeResponse actual = themeService.postTheme(request);
+        //then
+        AssertionsForClassTypes.assertThat(actual.name()).isEqualTo(given);
+    }
+
+    @Test
+    @DisplayName("저장소에 없는 값을 삭제하려할 경우, 예외가 발생한다.")
+    void test_deleteReservationTime() {
+        assertThatThrownBy(() -> themeService.deleteTheme(999L))
+                .isInstanceOf(EntityNotFoundException.class);
     }
 }
