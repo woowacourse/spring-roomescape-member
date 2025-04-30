43bb513641e16bc83927ce1b3b587565010823d4
taek2222 <oot309@daum.net>
2025-04-30 14:55:15 +0900

    test: 미래 예약 시간 반환 테스트 작성

--- src/test/java/roomescape/domain_entity/ReservationTimeTest.java
+++ src/test/java/roomescape/domain_entity/ReservationTimeTest.java
@@ -3,10 +3,21 @@ package roomescape.domain_entity;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalTime;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ReservationTimeTest {
+
+    static Stream<Arguments> isFutureReservationTimeReturnsFalse() {
+        return Stream.of(
+                Arguments.of(LocalTime.now()),
+                Arguments.of(LocalTime.now().plusMinutes(1))
+        );
+    }
 
     @Test
     @DisplayName("지난 예약 시간인 경우 TRUE 반환한다.")
@@ -20,5 +31,19 @@ class ReservationTimeTest {
 
         // then
         assertThat(result).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("미래 예약 시간인 경우 FALSE 반환한다.")
+    void isFutureReservationTimeReturnsFalse(LocalTime time) {
+        // given
+        ReservationTime reservationTime = new ReservationTime(new Id(1L), time);
+
+        // when
+        boolean result = reservationTime.isPastTime();
+
+        // then
+        assertThat(result).isFalse();
     }
 }
