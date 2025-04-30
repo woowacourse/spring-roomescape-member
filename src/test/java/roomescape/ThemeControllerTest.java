2ac7bbed887ee22e6652322443ed456b0dd4fab8
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-04-30 16:55:14 +0900

    RED) test(controller) : Theme 생성 요청 테스트 작성

---
+++ src/test/java/roomescape/ThemeControllerTest.java
@@ -0,0 +1,31 @@
+package roomescape;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ThemeControllerTest {
+
+    @Test
+    @DisplayName("테마 생성 요청")
+    void test1() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Ddyong");
+        params.put("description", "살인마가 쫓아오는 느낌");
+        params.put("thumbnail", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/themes")
+                .then().log().all()
+                .statusCode(201);
+    }
+}
