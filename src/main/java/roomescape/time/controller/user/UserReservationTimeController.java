29d11bfeb391be5a782b82bc7d31b7dea7df859c
minSsan <msp1125@hanyang.ac.kr>
2025-04-30 17:30:25 +0900

    feat: GET available-times API 연결

---
+++ src/main/java/roomescape/time/controller/user/UserReservationTimeController.java
@@ -0,0 +1,27 @@
+package roomescape.time.controller.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.time.dto.AvailableReservationTimeResponse;
+import roomescape.time.service.ReservationTimeService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+public class UserReservationTimeController {
+    private final ReservationTimeService timeService;
+
+    public UserReservationTimeController(ReservationTimeService timeService) {
+        this.timeService = timeService;
+    }
+
+    @GetMapping("/available-times")
+    public List<AvailableReservationTimeResponse> getAvailableTimes(
+            @RequestParam("date") LocalDate date,
+            @RequestParam("themeId")Long themeId
+    ) {
+        return timeService.getAvailableTimes(date, themeId);
+    }
+}
