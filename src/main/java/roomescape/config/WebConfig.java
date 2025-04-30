dd4d3ab63d5ee858b3b283f825715ae0562925f3
BWY <quantumjump10@gmail.com>
2025-04-30 15:54:32 +0900

    feat: 테마 관리 페이지 추가

--- src/main/java/roomescape/config/WebConfig.java
+++ src/main/java/roomescape/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/admin").setViewName("/admin/index");
-        registry.addViewController("/admin/reservation").setViewName("/admin/reservation");
+        registry.addViewController("/admin/reservation").setViewName("/admin/reservation-new");
         registry.addViewController("/admin/time").setViewName("/admin/time");
+        registry.addViewController("/admin/theme").setViewName("/admin/theme");
     }
 }
