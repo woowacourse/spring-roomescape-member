b9937acb128a6601480a12d00d16fe951be2fef7
Minuring <mw9505@gmail.com>
2025-04-30 14:58:05 +0900

    feat: 테마 DB 테이블 추가
    Co-authored-by: yeonnhuu <yeonnhuu@gmail.com>

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,31 @@
-CREATE TABLE reservation_time
+DROP TABLE IF EXISTS RESERVATION;
+DROP TABLE IF EXISTS THEME;
+DROP TABLE IF EXISTS RESERVATION_TIME;
+
+CREATE TABLE IF NOT EXISTS reservation_time
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation
+CREATE TABLE IF NOT EXISTS theme
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    thumbnail VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS reservation
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     date VARCHAR(255) NOT NULL,
     time_id BIGINT,
+    theme_id BIGINT,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
