da195f3420efda5c5fa2367e534d06317cf12b57
sonjh919 <kingjh1125@gmail.com>
2025-04-30 14:47:17 +0900

    test: 사용하지 않는 테스트 삭제

--- src/test/java/roomescape/RoomescapeApplicationTest.java
+++ src/test/java/roomescape/RoomescapeApplicationTest.java
@@ -1,13 +1,10 @@
 package roomescape;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RoomescapeApplicationTest {
-
-        @Test
-        void contextLoads() {
-        }
 
 }
