1e5538c4747fc17d63c5b713b9df09826428d08c
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-04-30 16:20:15 +0900

    refactor: 테마 API가 구체적인 상태코드를 반환하도록 수정

--- src/main/java/roomescape/controller/ThemeController.java
+++ src/main/java/roomescape/controller/ThemeController.java
@@ -1,11 +1,13 @@
 package roomescape.controller;
 
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.ThemeRequestDto;
 import roomescape.dto.ThemeResponseDto;
@@ -26,11 +28,13 @@ public class ThemeController {
     }
 
     @PostMapping("/themes")
+    @ResponseStatus(HttpStatus.CREATED)
     public ThemeResponseDto saveTheme(@RequestBody ThemeRequestDto request) {
         return themeService.saveTheme(request);
     }
 
     @DeleteMapping("/themes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTheme(@PathVariable Long id) {
         themeService.deleteTheme(id);
     }
