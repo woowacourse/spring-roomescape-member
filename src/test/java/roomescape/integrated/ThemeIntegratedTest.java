b63f9bb593cbe953113485fb80995044543c896d
taek2222 <oot309@daum.net>
2025-04-30 18:03:01 +0900

    test: 테마 API 테스트 작성

--- src/test/java/roomescape/integrated/ThemeIntegratedTest.java
+++ src/test/java/roomescape/integrated/ThemeIntegratedTest.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,29 @@ import org.springframework.test.annotation.DirtiesContext;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ThemeIntegratedTest {
+
+    @Test
+    @Disabled
+    @DisplayName("테마 페이지를 렌더링한다.")
+    void displayThemeAdmin() {
+        RestAssured.given().log().all()
+                .when().get("/admin/theme")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("전체 테마 데이터를 조회한다.")
+    void readAllTheme() {
+        setUpTheme();
+
+        RestAssured.given().log().all()
+                .when().get("/themes")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+    }
 
     @Test
     @DisplayName("테마 데이터를 추가한다.")
@@ -36,5 +60,26 @@ public class ThemeIntegratedTest {
                         "description", is("우테코 레벨2를 탈출하는 내용입니다."),
                         "thumbnail", is("https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg")
                 );
+    }
+
+    @Test
+    @DisplayName("테마 데이터를 삭제한다.")
+    void deleteTheme() {
+        RestAssured.given().log().all()
+                .when().delete("/themes/1")
+                .then().log().all()
+                .statusCode(204);
+    }
+
+    private void setUpTheme() {
+        Map<String, Object> reservation = new HashMap<>();
+        reservation.put("name", "레벨2 탈출");
+        reservation.put("description", "우테코 레벨2를 탈출하는 내용입니다.");
+        reservation.put("thumbnail", "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(reservation)
+                .when().post("/themes");
     }
 }
