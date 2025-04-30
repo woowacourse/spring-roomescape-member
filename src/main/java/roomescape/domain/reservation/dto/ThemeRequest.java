c5e3609167b0402b729899c0449c3f1c6be3b928
7i.w__k <tizmfns1218@Naver.com>
2025-04-30 14:29:08 +0900

    refactor: 이미지 URL의 길이 제한

--- src/main/java/roomescape/domain/reservation/dto/ThemeRequest.java
+++ src/main/java/roomescape/domain/reservation/dto/ThemeRequest.java
@@ -1,10 +1,11 @@
 package roomescape.domain.reservation.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record ThemeRequest(
         @NotBlank String name,
         @NotBlank String description,
-        @NotBlank String thumbnail) {
+        @Size(max = 255) @NotBlank String thumbnail) {
 
 }
