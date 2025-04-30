8f9b77872df94de6dcf5aead955c7c687ae3c254
GiSeok Kang <ime.rokz@gmail.com>
2025-04-30 14:05:53 +0900

    test: 예약 관련 예외 테스트 추가

---
+++ src/test/java/roomescape/reservation/service/FakeReservationRepository.java
@@ -0,0 +1,71 @@
+package roomescape.reservation.service;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import roomescape.reservation.domain.Reservation;
+import roomescape.reservation.domain.ReservationRepository;
+
+public class FakeReservationRepository implements ReservationRepository {
+    private final List<Reservation> reservations;
+
+    private AtomicLong index = new AtomicLong(0);
+
+    public FakeReservationRepository() {
+        reservations = new ArrayList<>();
+    }
+
+    @Override
+    public List<Reservation> findAll() {
+        return Collections.unmodifiableList(reservations);
+    }
+
+    @Override
+    public boolean existByReservationTimeId(Long timeId) {
+        return reservations.stream()
+                .anyMatch(reservation -> reservation.getTime().getId() == timeId);
+    }
+
+    @Override
+    public boolean existByDateTime(LocalDate date, LocalTime time) {
+        return reservations.stream()
+                .anyMatch(reservation ->
+                        reservation.getTime().getStartAt().equals(time)
+                && reservation.getDate().equals(date));
+    }
+
+    @Override
+    public Long save(Reservation reservation) {
+        long currentIndex = index.incrementAndGet();
+        reservations.add(new Reservation(currentIndex, reservation.getName(), reservation.getDate(), reservation.getTime()));
+        return currentIndex;
+    }
+
+    @Override
+    public int deleteById(Long id) {
+        Optional<Reservation> findReservation = reservations.stream()
+                .filter(reservation -> Objects.equals(reservation.getId(), id))
+                .findAny();
+
+        if(findReservation.isEmpty()){
+            return 0;
+        }
+
+        Reservation reservation = findReservation.get();
+        reservations.remove(reservation);
+        return 1;
+    }
+
+    @Override
+    public Reservation findById(Long id) {
+        return reservations.stream()
+                .filter(reservation -> Objects.equals(reservation.getId(), id))
+                .findAny()
+                .orElseThrow();
+    }
+}
---
+++ src/test/java/roomescape/reservation/service/ReservationServiceTest.java
@@ -0,0 +1,72 @@
+package roomescape.reservation.service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import roomescape.common.util.DateTime;
+import roomescape.reservation.domain.Reservation;
+import roomescape.reservation.domain.ReservationRepository;
+import roomescape.reservation.dto.ReservationRequest;
+import roomescape.reservationTime.domain.ReservationTime;
+import roomescape.reservationTime.domain.ReservationTimeRepository;
+
+class ReservationServiceTest {
+
+    private ReservationService reservationService;
+
+    @BeforeEach
+    void beforeEach() {
+        DateTime dateTime = new DateTime() {
+            @Override
+            public LocalDateTime now() {
+                return LocalDateTime.of(2025, 10, 5, 10, 0);
+            }
+        };
+
+        ReservationTime reservationTime1 = new ReservationTime(null, LocalTime.of(10, 0));
+        ReservationTime reservationTime2 = new ReservationTime(null, LocalTime.of(9, 0));
+
+        ReservationTimeRepository reservationTimeRepository = new FakeReservationTimeRepository();
+        reservationTimeRepository.save(reservationTime1);
+        reservationTimeRepository.save(reservationTime2);
+
+        ReservationRepository reservationRepository = new FakeReservationRepository();
+        reservationRepository.save(new Reservation(null, "홍길동", LocalDate.of(2024, 10, 6), reservationTime1));
+
+        reservationService = new ReservationService(dateTime, reservationRepository, reservationTimeRepository);
+    }
+
+    @DisplayName("지나간 날짜와 시간에 대한 예약을 생성할 수 없다.")
+    @ParameterizedTest
+    @MethodSource
+    void cant_not_reserve_before_now(final LocalDate date, final Long timeId) {
+        Assertions.assertThatThrownBy(
+                        () -> reservationService.createReservation(new ReservationRequest("홍길동", date, timeId)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> cant_not_reserve_before_now() {
+        return Stream.of(
+                Arguments.of(LocalDate.of(2024, 10, 5), 1L),
+                Arguments.of(LocalDate.of(2025, 9, 5), 1L),
+                Arguments.of(LocalDate.of(2025, 10, 4), 1L),
+                Arguments.of(LocalDate.of(2025, 10, 5), 2L)
+        );
+    }
+
+    @DisplayName("중복 예약이 불가하다.")
+    @Test
+    void cant_not_reserve_duplicate() {
+        Assertions.assertThatThrownBy(() -> reservationService.createReservation(
+                        new ReservationRequest("홍길동", LocalDate.of(2024, 10, 6), 1L)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
