3d29311fe9941349ab38ae4296e33d820490871b
GiSeok Kang <ime.rokz@gmail.com>
2025-04-30 16:32:34 +0900

    fix: 도메인 변경에 대한 테스트 코드 수정

--- src/test/java/roomescape/reservationTime/service/ReservationTimeServiceTest.java
+++ src/test/java/roomescape/reservationTime/service/ReservationTimeServiceTest.java
@@ -24,15 +24,15 @@ class ReservationTimeServiceTest {
         Long id = reservationTimeRepository.save(reservationTime1);
         reservationTime1 = reservationTimeRepository.findById(id);
         ReservationRepository reservationRepository = new FakeReservationRepository();
-        reservationRepository.save(new Reservation(null, "홍길동", LocalDate.of(2024, 10, 6), reservationTime1));
+        reservationRepository.save(new Reservation(null, "홍길동", LocalDate.of(2024, 10, 6), reservationTime1, null));
 
-        reservationTimeService = new ReservationTimeService(reservationRepository,reservationTimeRepository);
+        reservationTimeService = new ReservationTimeService(reservationRepository, reservationTimeRepository);
     }
 
     @Test
     @DisplayName("이미 존재하는 예약이 있는 경우 예역 시간을 삭제할 수 없다.")
-    void can_not_delete_when_reservation_exists(){
-        Assertions.assertThatThrownBy(()->reservationTimeService.deleteReservationTimeById(1L))
+    void can_not_delete_when_reservation_exists() {
+        Assertions.assertThatThrownBy(() -> reservationTimeService.deleteReservationTimeById(1L))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
