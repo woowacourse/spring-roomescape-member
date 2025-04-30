be1abee35f600092aced6df4a991fd7a886c5c1e
Firedrago95 <yonghwa95@naver.com>
2025-04-30 11:30:08 +0900

    feat: 테마 호출 클라이언트 코드 수정
    Co-authored-by: theminjunchoi <mj043000@naver.com>

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO reservation_time (start_at)
+VALUES ('10:00');
+INSERT INTO reservation_time (start_at)
+VALUES ('12:00');
+INSERT INTO reservation_time (start_at)
+VALUES ('14:00');
+INSERT INTO reservation_time (start_at)
+VALUES ('16:00');
+INSERT INTO reservation_time (start_at)
+VALUES ('18:00');
+INSERT INTO reservation_time (start_at)
+VALUES ('20:00');
+
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마1', '설명1', '섬네일1');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마2', '설명2', '섬네일2');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마3', '설명3', '섬네일3');
