fdd0fc7218b50848e4c4f1b1607e029e797f4379
mimi <110226866+hongkikii@users.noreply.github.com>
2025-04-30 22:39:21 +0900

    refactor: 불필요 메서드 제거

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -1,24 +1,8 @@
 package roomescape.reservation.domain;
 
 import java.time.LocalDate;
-import java.util.Objects;
 import roomescape.time.domain.Time;
 import roomescape.theme.domain.Theme;
 
 public record Reservation(Long id, String name, LocalDate date, Time time, Theme theme) {
-
-    @Override
-    public boolean equals(Object other) {
-        if (other == null || getClass() != other.getClass()) {
-            return false;
-        }
-        Reservation that = (Reservation) other;
-        return Objects.equals(id, that.id) && Objects.equals(date, that.date)
-                && Objects.equals(time, that.time) && Objects.equals(theme, that.theme);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, date, time, theme);
-    }
 }
