d6336c8451f28ce0f53ef5218f9a3457a2ba7488
Jin409 <sualng123@gmail.com>
2025-04-30 17:14:35 +0900

    feat: 예약 가능한 날짜 전체 조회 기능 구현

---
+++ src/main/java/roomescape/dto/AvailableReservationTimeResponseDto.java
@@ -0,0 +1,19 @@
+package roomescape.dto;
+
+import java.time.LocalTime;
+import roomescape.model.AvailableReservationTime;
+
+public record AvailableReservationTimeResponseDto(
+        Long timeId,
+        LocalTime startAt,
+        boolean alreadyBooked
+) {
+    public static AvailableReservationTimeResponseDto from(AvailableReservationTime availableReservationTime) {
+        return new AvailableReservationTimeResponseDto(
+                availableReservationTime.id(),
+                availableReservationTime.statAt(),
+                availableReservationTime.alreadyBooked()
+        );
+    }
+
+}
