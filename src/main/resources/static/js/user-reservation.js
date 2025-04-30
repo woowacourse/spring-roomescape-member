33b4bb279e9972d4da4c1dd484cafe31773ccf87
sonjh919 <kingjh1125@gmail.com>
2025-04-30 15:48:25 +0900

    feat: 사용자 예약 API 수정

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -36,10 +36,10 @@ function renderTheme(themes) {
   const themeSlots = document.getElementById('theme-slots');
   themeSlots.innerHTML = '';
   themes.forEach(theme => {
-    const name = '';
-    const themeId = '';
+    const name = theme.name;
+    const themeId = theme.id;
     /*
-    TODO: [3단계] 사용자 예약 - 테마 목록 조회 API 호출 후 렌더링
+    - DONE: [3단계] 사용자 예약 - 테마 목록 조회 API 호출 후 렌더링
           response 명세에 맞춰 createSlot 함수 호출 시 값 설정
           createSlot('theme', theme name, theme id) 형태로 호출
     */
@@ -88,10 +88,10 @@ function checkDateAndTheme() {
 
 function fetchAvailableTimes(date, themeId) {
   /*
-  TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출
+  - TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출
         요청 포맷에 맞게 설정
   */
-  fetch('/', { // 예약 가능 시간 조회 API endpoint
+  fetch('/times', { // 예약 가능 시간 조회 API endpoint
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -117,11 +117,11 @@ function renderAvailableTimes(times) {
   }
   times.forEach(time => {
     /*
-    TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출 후 렌더링
+    DONE: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출 후 렌더링
           response 명세에 맞춰 createSlot 함수 호출 시 값 설정
     */
-    const startAt = '';
-    const timeId = '';
+    const startAt = time.startAt;
+    const timeId = time.id;
     const alreadyBooked = false;
 
     const div = createSlot('time', startAt, timeId, alreadyBooked); // createSlot('time', 시작 시간, time id, 예약 여부)
