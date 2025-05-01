33633040886edf4f369cccf7d4cd87957ff3a098
minSsan <msp1125@hanyang.ac.kr>
2025-05-01 16:39:52 +0900

    feat: 예약 가능 시간 API endpoint 변경 (/reservation/times)

--- src/main/java/roomescape/time/controller/user/UserReservationTimeController.java
+++ src/main/java/roomescape/time/controller/user/UserReservationTimeController.java
@@ -1,6 +1,7 @@
 package roomescape.time.controller.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.time.service.dto.response.AvailableReservationTimeResponse;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@RequestMapping("/reservation")
 public class UserReservationTimeController {
     private final ReservationTimeService timeService;
 
@@ -17,7 +19,7 @@ public class UserReservationTimeController {
         this.timeService = timeService;
     }
 
-    @GetMapping("/available-times")
+    @GetMapping("/times")
     public List<AvailableReservationTimeResponse> getAvailableTimes(
             @RequestParam("date") LocalDate date,
             @RequestParam("themeId")Long themeId
--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -101,7 +101,7 @@ function fetchAvailableTimes(date, themeId) {
   TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출
         요청 포맷에 맞게 설정
   */
-  fetch(`/available-times?date=${date}&themeId=${themeId}`, {
+  fetch(`/reservation/times?date=${date}&themeId=${themeId}`, {
     // 예약 가능 시간 조회 API endpoint
     method: "GET",
     headers: {
