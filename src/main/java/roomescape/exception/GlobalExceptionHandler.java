1e6ae2fca2147d7ae144ca5e98a2d91d76aa6c7c
Dompoo <dlckdrms0517@gmail.com>
2025-05-01 13:07:50 +0900

    feat: 예외 응답 발생시 로깅 기능 추가

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,19 +1,25 @@
 package roomescape.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(RootException.class)
     public ResponseEntity<ErrorResponse> handle(RootException e) {
+        logger.warn("Handled RootException: {}", e.getMessage(), e);
         return ResponseEntity.badRequest().body(ErrorResponse.badRequest(e));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handle(Exception e) {
+        logger.error("Handled Exception: {}", e.getMessage(), e);
         return ResponseEntity.internalServerError().body(ErrorResponse.internalServerError(e));
     }
 }
