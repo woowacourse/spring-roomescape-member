b4a0ea0311d6ac89ee6488b2ead9df9f6eee8008
eoehd1ek <eoehd2ek@gmail.com>
2025-05-01 15:29:16 +0900

    feat: 사용자 예약 페이지 응답 구현

---
+++ src/main/java/roomescape/presentation/controller/UserController.java
@@ -0,0 +1,13 @@
+package roomescape.presentation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
+    }
+}
