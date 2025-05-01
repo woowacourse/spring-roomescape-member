96205ae8d39553724ac62779e35eb2063365bf2c
juno <wnsgh732@gmail.com>
2025-05-01 11:31:58 +0900

    feat: DataAccessException 핸들러 추가

--- src/main/java/roomescape/exception_handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception_handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package roomescape.exception_handler;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,6 +28,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotExistException.class)
     public ResponseEntity<Void> handleResourceNotExistException(ResourceNotExistException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<Void> handleDataAccessException(DataAccessException e) {
         return ResponseEntity.notFound().build();
     }
 }
