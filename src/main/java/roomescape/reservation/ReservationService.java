b1fadb2de587863f4905c78df0408d48f922ab9b
changuii <rhljh7410@gmail.com>
2025-05-01 13:11:38 +0900

    refactor: 패키지 및 네이밍 변경

---
+++ src/main/java/roomescape/reservation/ReservationService.java
@@ -0,0 +1,72 @@
+package roomescape.reservation;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import roomescape.globalException.BadRequestException;
+import roomescape.globalException.ConflictException;
+import roomescape.reservation.dto.ReservationRequest;
+import roomescape.reservation.dto.ReservationResponse;
+import roomescape.reservationTime.ReservationTime;
+import roomescape.reservationTime.ReservationTimeRepository;
+
+@Service
+public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+    private final ReservationTimeRepository reservationTimeRepository;
+
+    public ReservationService(
+            @Autowired final ReservationRepository reservationRepository,
+            @Autowired final ReservationTimeRepository reservationTimeRepository
+    ) {
+        this.reservationRepository = reservationRepository;
+        this.reservationTimeRepository = reservationTimeRepository;
+    }
+
+    public ReservationResponse create(final ReservationRequest request) {
+        validateDuplicateDateTime(request.timeId(), request.date());
+        validatePastDateTime(request);
+
+        final Reservation reservation = new Reservation(request.name(), request.date());
+        final long id = reservationRepository.save(reservation, request.timeId(), request.themeId());
+        final Reservation savedReservation = reservationRepository.findById(id);
+        return ReservationResponse.from(savedReservation);
+    }
+
+    public List<ReservationResponse> readAll() {
+        return reservationRepository.findAll().stream()
+                .map(ReservationResponse::from)
+                .toList();
+    }
+
+    public void deleteById(final Long id) {
+        reservationRepository.delete(id);
+    }
+
+    private void validatePastDateTime(final ReservationRequest request) {
+        final LocalDate today = LocalDate.now();
+        final LocalDate reservationDate = request.date();
+        if (reservationDate.isBefore(today)) {
+            throw new BadRequestException("지난 날짜로 예약할 수 없습니다.");
+        }
+        if (reservationDate.isEqual(today)) {
+            validatePastTime(request);
+        }
+    }
+
+    private void validatePastTime(final ReservationRequest request) {
+        final ReservationTime reservationTime = reservationTimeRepository.findById(request.timeId());
+        if (reservationTime.getStartAt().isBefore(LocalTime.now())) {
+            throw new BadRequestException("지난 시간으로 예약할 수 없습니다.");
+        }
+    }
+
+    private void validateDuplicateDateTime(final Long reservationTimeId, final LocalDate date) {
+        if (reservationRepository.existsByReservationTimeAndDate(reservationTimeId, date)) {
+            throw new ConflictException("이미 예약이 존재합니다.");
+        }
+    }
+}
