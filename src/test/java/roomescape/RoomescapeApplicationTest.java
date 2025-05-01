9da08c509f5eba5c47456030ec41e6b5ca289ef5
sonjh919 <kingjh1125@gmail.com>
2025-05-01 15:27:17 +0900

    refactor: import 제거

--- src/test/java/roomescape/RoomescapeApplicationTest.java
+++ src/test/java/roomescape/RoomescapeApplicationTest.java
@@ -1,10 +1,7 @@
 package roomescape;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RoomescapeApplicationTest {
-
 }
