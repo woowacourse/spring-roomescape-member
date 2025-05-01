3fda52e2b2d56beafe428f35a6796602bedacc56
minSsan <msp1125@hanyang.ac.kr>
2025-05-01 20:24:31 +0900

    chore: 뷰 컨트롤러 네이밍 변경 - Reservation 삭제

--- src/main/java/roomescape/view/AdminViewController.java
+++ src/main/java/roomescape/view/AdminViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
-public class AdminReservationViewController {
+public class AdminViewController {
 
     @GetMapping
     public String showAdminWelcome() {
