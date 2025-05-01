a20ceb4fec9233faace8496448f5102a76c0bf3c
threepebbles <threepebbles@naver.com>
2025-05-01 14:51:11 +0900

    chore : 공백 정리

--- src/main/java/roomescape/theme/domain/Theme.java
+++ src/main/java/roomescape/theme/domain/Theme.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @EqualsAndHashCode(of = {"id"})
 public class Theme {
+
     private final Long id;
     private final String name;
     private final String description;
