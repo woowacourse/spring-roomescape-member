2eb41ed2ebb85a7c3ab1c13a9956df28cd4e8c37
GiSeok Kang <ime.rokz@gmail.com>
2025-05-01 15:15:08 +0900

    test: 인기 테마 조회 테스트

--- src/test/java/roomescape/theme/service/ThemeServiceTest.java
+++ src/test/java/roomescape/theme/service/ThemeServiceTest.java
@@ -2,6 +2,7 @@ package roomescape.theme.service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +14,7 @@ import roomescape.reservation.service.FakeReservationRepository;
 import roomescape.reservation.service.FakeThemeRepository;
 import roomescape.theme.domain.Theme;
 import roomescape.theme.domain.ThemeRepository;
+import roomescape.theme.dto.PopularThemeResponse;
 
 class ThemeServiceTest {
 
@@ -20,19 +22,24 @@ class ThemeServiceTest {
 
     @BeforeEach
     void beforeEach() {
-        ThemeRepository themeRepository = new FakeThemeRepository(null);
-        Theme theme = new Theme(null, "테스트", "설명", "썸네일");
-
-        themeRepository.save(theme);
-        theme = themeRepository.findById(1L);
+        Theme theme1 = new Theme(1L, "테스트1", "설명", "썸네일");
+        Theme theme2 = new Theme(2L, "테스트2", "설명", "썸네일");
+        Theme theme3 = new Theme(3L, "테스트3", "설명", "썸네일");
 
         ReservationRepository reservationRepository = new FakeReservationRepository();
-        reservationRepository.save(new Reservation(null, "홍길동", LocalDate.of(2025, 12, 5), null, theme));
+        reservationRepository.save(new Reservation(null, "홍길동1", LocalDate.of(2025, 12, 5), null, theme1));
+        reservationRepository.save(new Reservation(null, "홍길동2", LocalDate.of(2025, 12, 6), null, theme1));
+        reservationRepository.save(new Reservation(null, "홍길동3", LocalDate.of(2025, 12, 4), null, theme3));
+
+        ThemeRepository themeRepository = new FakeThemeRepository(reservationRepository);
+        themeRepository.save(theme1);
+        themeRepository.save(theme2);
+        themeRepository.save(theme3);
 
         DateTime dateTime = new DateTime() {
             @Override
             public LocalDateTime now() {
-                return LocalDateTime.of(2025, 5, 1, 10, 0);
+                return LocalDateTime.of(2025, 12, 7, 10, 0);
             }
         };
 
@@ -41,8 +48,19 @@ class ThemeServiceTest {
 
     @DisplayName("존재하는 예약의 테마는 삭제할 수 없다.")
     @Test
-    void test() {
+    void can_not_remove_exists_reservation() {
         Assertions.assertThatThrownBy(() -> themeService.deleteThemeById(1L))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("인기 테마를 가져올 수 있다.")
+    @Test
+    void can_get_popular_theme() {
+        List<PopularThemeResponse> popularThemes = themeService.getPopularThemes();
+
+        Assertions.assertThat(popularThemes).containsExactly(
+                new PopularThemeResponse("테스트1", "썸네일", "설명"),
+                new PopularThemeResponse("테스트3", "썸네일", "설명")
+        );
     }
 }
