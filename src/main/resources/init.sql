795a4c79c8cfe38bb10bad6fb5fea3e68c3ac04b
GiSeok Kang <ime.rokz@gmail.com>
2025-05-01 12:14:18 +0900

    refactor: 썸네일 경로 수정
    - 이미지 추가

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,6 +1,6 @@
-INSERT INTO theme (name, description, thumbnail) VALUES ('테마1', '재밌음', 'theme1');
-INSERT INTO theme (name, description, thumbnail) VALUES ('테마2', '무서움', 'theme2');
-INSERT INTO theme (name, description, thumbnail) VALUES ('테마3', '놀라움', 'theme3');
+INSERT INTO theme (name, description, thumbnail) VALUES ('테마1', '재밌음', '/image/default.jpg');
+INSERT INTO theme (name, description, thumbnail) VALUES ('테마2', '무서움', '/image/default.jpg');
+INSERT INTO theme (name, description, thumbnail) VALUES ('테마3', '놀라움', '/image/default.jpg');
 
 INSERT INTO reservation_time (start_at) VALUES ('10:00');
 INSERT INTO reservation_time (start_at) VALUES ('11:00');
