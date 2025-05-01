7ba3ac134313cb979e4576d3ce6fac8d6c8c524c
changuii <rhljh7410@gmail.com>
2025-05-01 13:10:38 +0900

    refactor: 패키지 변경 및 메서드를 줄바꿈으로 분류

---
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -0,0 +1,17 @@
+package roomescape.reservation;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ReservationRepository {
+
+    Long save(Reservation reservation, Long reservationTimeId, Long themeId);
+
+    List<Reservation> findAll();
+    Reservation findById(Long id);
+
+    void delete(Long id);
+
+    Boolean existsByReservationTime(Long reservationTime);
+    Boolean existsByReservationTimeAndDate(Long reservationTime, LocalDate date);
+}
