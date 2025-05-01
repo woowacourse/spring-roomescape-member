18fccb95cab9aab0412891a0166518a74629dc18
Chaeyoung714 <202102760@hufs.ac.kr>
2025-05-01 16:24:21 +0900

    chore:  예약 날짜 테이블 형식 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE reservation_time
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL,
+    start_at TIME NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -18,7 +18,7 @@ CREATE TABLE reservation
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    date VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
     time_id BIGINT,
     theme_id BIGINT,
     PRIMARY KEY (id),
