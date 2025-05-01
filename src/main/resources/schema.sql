495715531bf2b76543d3bcdc76583b55aa135941
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-05-01 16:32:11 +0900

    delete(test) : ReservationServiceTest 제거

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -25,3 +25,22 @@ CREATE TABLE reservation
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)            -- 외래키 추가
 );
+
+-- insert into reservation_time (start_at) values ('12:00'); --1
+-- insert into reservation_time (start_at) values ('13:00'); --2
+-- insert into reservation_time (start_at) values ('14:00'); --3
+--
+-- insert into theme (name, description, thumbnail) values ('테마1','설명1','https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+-- insert into theme (name, description, thumbnail) values ('테마2','설명2','https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+-- insert into theme (name, description, thumbnail) values ('테마3','설명3','https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+--
+-- insert into reservation ( name, date, time_id, theme_id) values ('벡터','2025-04-30',1,1);
+-- insert into reservation ( name, date, time_id, theme_id) values ('벡터','2025-04-29',1,1);
+-- insert into reservation ( name, date, time_id, theme_id) values ('벡터','2025-04-28',1,1);
+--
+-- insert into reservation ( name, date, time_id, theme_id) values ('띠용','2025-04-30',2,2);
+-- insert into reservation ( name, date, time_id, theme_id) values ('띠용','2025-04-29',2,2);
+-- insert into reservation ( name, date, time_id, theme_id) values ('띠용','2025-04-22',2,2);
+-- insert into reservation ( name, date, time_id, theme_id) values ('띠용','2025-04-21',2,2);
+--
+-- insert into reservation ( name, date, time_id, theme_id) values ('띠터','2025-04-30',3,3);
