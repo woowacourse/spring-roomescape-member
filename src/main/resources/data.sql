33ad680c85ee14e44afd6b808aa27736c8d8e8ad
taek2222 <oot309@daum.net>
2025-05-01 14:31:06 +0900

    chore: 기본 테스트 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO reservation_time (start_at)
+VALUES ('10:00'),
+       ('12:00'),
+       ('14:00'),
+       ('16:00'),
+       ('18:00'),
+       ('20:00');
+
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마1', '테마1입니다.',
+        'https://static.wikia.nocookie.net/pokemon/images/0/03/%EB%AA%A8%EB%8B%A4%ED%94%BC_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405013619&path-prefix=ko'),
+       ('테마2', '테마2입니다.',
+        'https://static.wikia.nocookie.net/pokemon/images/0/03/%EB%AA%A8%EB%8B%A4%ED%94%BC_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405013619&path-prefix=ko'),
+       ('테마3', '테마3입니다.',
+        'https://static.wikia.nocookie.net/pokemon/images/0/03/%EB%AA%A8%EB%8B%A4%ED%94%BC_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405013619&path-prefix=ko');
