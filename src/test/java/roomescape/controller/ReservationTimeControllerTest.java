83c7174fd71a534b97b6e84376498073ad062020
황재웅 <wodnd0131@naver.com>
2025-05-01 15:39:49 +0900

    test: ReservationTimeControllerTest를 MockMvc로 검증

--- src/test/java/roomescape/controller/ReservationTimeControllerTest.java
+++ src/test/java/roomescape/controller/ReservationTimeControllerTest.java
@@ -1,147 +1,115 @@
 package roomescape.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Time;
+import io.restassured.http.ContentType;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import java.time.LocalTime;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 import roomescape.dto.ReservationTimeRequest;
 import roomescape.dto.ReservationTimeResponse;
-import roomescape.entity.Reservation;
-import roomescape.entity.ReservationTime;
 import roomescape.exceptions.EntityNotFoundException;
-import roomescape.repository.ReservationTimeJDBCDao;
 import roomescape.service.ReservationTimeService;
 
+@WebMvcTest(ReservationTimeController.class)
 public class ReservationTimeControllerTest {
 
-    private static EmbeddedDatabase dataSource;
-    private static NamedParameterJdbcTemplate namedJdbcTemplate;
-    private static ReservationTimeController controller;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @BeforeAll
-    public static void setUpClass() {
-        dataSource = new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .addScript("schema.sql")
-                .build();
-        namedJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
-
-        ReservationTimeService service = new ReservationTimeService(new ReservationTimeJDBCDao(namedJdbcTemplate));
-        controller = new ReservationTimeController(service);
-    }
+    @MockitoBean
+    private ReservationTimeService timeService;
 
     @BeforeEach
-    public void setup() {
-        namedJdbcTemplate.update("delete from reservation_time", new MapSqlParameterSource());
-        namedJdbcTemplate.update("alter table reservation_time alter column id restart with 1",
-                new MapSqlParameterSource());
+    void setUp() {
+        RestAssuredMockMvc.mockMvc(mockMvc);
     }
 
     @Test
     @DisplayName("예약 시간 목록을 조회한다.")
     void readAllReservationTime() {
-        //given
-        ReservationTime reservationTime1 = new ReservationTime(1L, LocalTime.of(15, 0));
-        ReservationTime reservationTime2 = new ReservationTime(2L, LocalTime.of(16, 0));
+        ReservationTimeResponse response1 = new ReservationTimeResponse(1L, LocalTime.of(11, 0));
+        ReservationTimeResponse response2 = new ReservationTimeResponse(2L, LocalTime.of(12, 0));
 
-        List<ReservationTime> reservationTimes = List.of(reservationTime1, reservationTime2);
-        String sql = "insert into reservation_time (start_at) values (:startAt)";
+        List<ReservationTimeResponse> reservationTimeResponses = List.of(
+                response1, response2
+        );
 
-        SqlParameterSource[] batch = reservationTimes.stream()
-                .map(time -> new MapSqlParameterSource("startAt", Time.valueOf(time.startAt())))
-                .toArray(SqlParameterSource[]::new);
+        given(timeService.readAllReservationTime()).willReturn(reservationTimeResponses);
 
-        namedJdbcTemplate.batchUpdate(sql, batch);
-
-        //when
-        List<ReservationTimeResponse> result = controller.readAllReservationTime();
-
-        //then
-        assertThat(result.size()).isEqualTo(2);
-        assertThat(result.getFirst()).isEqualTo(ReservationTimeResponse.from(reservationTime1));
-        assertThat(result.getLast()).isEqualTo(ReservationTimeResponse.from(reservationTime2));
+        RestAssuredMockMvc.given().log().all()
+                .when().get("/times")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(2))
+                .body("[0].id", is(1))
+                .body("[0].startAt", is("11:00"))
+                .body("[1].id", is(2))
+                .body("[1].startAt", is("12:00"));
     }
 
     @Test
-    @DisplayName("예약 시간 관리 페이지 내에서 예약 시간을 추가한다.")
+    @DisplayName("예약 시간 관리 페이지 내에서 예악 시간 추가한다.")
     void postReservationTime() {
-        //given
-        LocalTime fixedTime = LocalTime.of(14, 30);
+        long expectedId = 1L;
+        LocalTime time = LocalTime.of(11, 0);
 
-        ReservationTimeRequest dto = new ReservationTimeRequest(fixedTime);
-        //when
-        controller.postReservationTime(dto);
-        //then
-        String sql = "select * from reservation_time where id = :id";
-        MapSqlParameterSource params = new MapSqlParameterSource("id", 1L);
+        ReservationTimeRequest request = new ReservationTimeRequest(time);
 
-        ReservationTime reservationTime = namedJdbcTemplate.queryForObject(sql, params,
-                (rs, rowNum) -> getReservationTime(rs));
+        ReservationTimeResponse response = new ReservationTimeResponse(expectedId, time);
+        given(timeService.postReservationTime(request)).willReturn(response);
 
-        Assertions.assertNotNull(reservationTime);
-        assertThat(reservationTime).isEqualTo(dto.toEntity(1L));
+        RestAssuredMockMvc.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(201)
+                .body("id", is((int) expectedId))
+                .body("startAt", is(time.toString()));
     }
 
     @Test
     @DisplayName("존재하는 ID로 삭제 요청 시 성공적으로 처리되어야 한다.")
-    void deleteExistingReservationTime() {
-        //given
-        ReservationTime reservationTime1 = new ReservationTime(1L, LocalTime.of(15, 50));
-        ReservationTime reservationTime2 = new ReservationTime(2L, LocalTime.of(15, 55));
+    void deleteExistingTime() {
+        long timeId = 1L;
 
-        List<ReservationTime> reservationTimes = List.of(reservationTime1, reservationTime2);
-        String insertSql = "insert into reservation_time (start_at) values (:startAt)";
+        willDoNothing().given(timeService).deleteReservationTime(timeId);
 
-        SqlParameterSource[] batch = reservationTimes.stream()
-                .map(time -> new MapSqlParameterSource("startAt", Time.valueOf(time.startAt())))
-                .toArray(SqlParameterSource[]::new);
+        RestAssuredMockMvc.given().log().all()
+                .when().delete("/times/" + timeId)
+                .then().log().all()
+                .statusCode(204);
 
-        namedJdbcTemplate.batchUpdate(insertSql, batch);
-        long reservationTimeId = 1L;
-
-        //when
-        controller.deleteReservationTime(reservationTimeId);
-
-        //then
-        String selectSql = "select * from reservation_time where id = :id";
-        MapSqlParameterSource params = new MapSqlParameterSource("id", 1L);
-
-        assertThatThrownBy(() -> namedJdbcTemplate.queryForObject(selectSql, params, Reservation.class))
-                .isInstanceOf(EmptyResultDataAccessException.class);
+        verify(timeService, times(1)).deleteReservationTime(timeId);
     }
 
     @Test
     @DisplayName("존재하지 않는 ID로 삭제 요청 시 404 응답이 반환되어야 한다.")
-    void deleteNonExistingReservationTime() {
-        //given
+    void deleteReservationTime() {
         long nonExistingId = 999L;
 
-        //when & then
-        assertThatThrownBy(() -> controller.deleteReservationTime(nonExistingId))
-                .isInstanceOf(EntityNotFoundException.class)
-                .hasMessage("[ERROR] 예약 시간 데이터를 찾을 수 없습니다:999");
-    }
+        willThrow(new EntityNotFoundException("데이터를 찾을 수 없습니다."))
+                .given(timeService)
+                .deleteReservationTime(nonExistingId);
 
-    private static ReservationTime getReservationTime(ResultSet rs) throws SQLException {
-        return new ReservationTime(
-                rs.getLong("id"),
-                rs.getTime("start_at").toLocalTime()
-        );
+        RestAssuredMockMvc.given().log().all()
+                .when().delete("/times/" + nonExistingId)
+                .then().log().all()
+                .statusCode(404);
+
+        verify(timeService, times(1)).deleteReservationTime(nonExistingId);
     }
 }
