65e292cb1db6daa17c9ca46b76c0b51d91fd5c8a
RIWON <121172065+awrion3@users.noreply.github.com>
2025-05-01 16:09:06 +0900

    chore: 데이터 내용 변경 및 형식 정리

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,110 +1,200 @@
-INSERT INTO reservation_time(start_at) VALUES ('10:00');
-INSERT INTO reservation_time(start_at) VALUES ('11:00');
-INSERT INTO reservation_time(start_at) VALUES ('12:00');
-INSERT INTO reservation_time(start_at) VALUES ('13:00');
-INSERT INTO reservation_time(start_at) VALUES ('14:00');
-INSERT INTO reservation_time(start_at) VALUES ('15:00');
-INSERT INTO reservation_time(start_at) VALUES ('16:00');
-INSERT INTO reservation_time(start_at) VALUES ('17:00');
-INSERT INTO reservation_time(start_at) VALUES ('18:00');
-INSERT INTO reservation_time(start_at) VALUES ('19:00');
-INSERT INTO reservation_time(start_at) VALUES ('20:00');
-INSERT INTO reservation_time(start_at) VALUES ('21:00');
-INSERT INTO reservation_time(start_at) VALUES ('22:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('10:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('11:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('12:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('13:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('14:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('15:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('16:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('17:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('18:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('19:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('20:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('21:00');
+INSERT INTO reservation_time(start_at)
+VALUES ('22:00');
 
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme1', 'description1', 'thumbnail1');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme2', 'description2', 'thumbnail2');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme3', 'description3', 'thumbnail3');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme4', 'description4', 'thumbnail4');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme5', 'description5', 'thumbnail5');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme6', 'description6', 'thumbnail6');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme7', 'description7', 'thumbnail7');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme8', 'description8', 'thumbnail8');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme9', 'description9', 'thumbnail9');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme10', 'description10', 'thumbnail10');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme11', 'description11', 'thumbnail11');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme12', 'description12', 'thumbnail12');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme13', 'description13', 'thumbnail13');
-INSERT INTO theme(name, description, thumbnail) VALUES ('theme14', 'description14', 'thumbnail14');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme1', 'description1', 'thumbnail1');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme2', 'description2', 'thumbnail2');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme3', 'description3', 'thumbnail3');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme4', 'description4', 'thumbnail4');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme5', 'description5', 'thumbnail5');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme6', 'description6', 'thumbnail6');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme7', 'description7', 'thumbnail7');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme8', 'description8', 'thumbnail8');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme9', 'description9', 'thumbnail9');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme10', 'description10', 'thumbnail10');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme11', 'description11', 'thumbnail11');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme12', 'description12', 'thumbnail12');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme13', 'description13', 'thumbnail13');
+INSERT INTO theme(name, description, thumbnail)
+VALUES ('theme14', 'description14', 'thumbnail14');
 
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-20', 1, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-20', 2, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-20', 3, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-20', 4, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-20', 5, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-20', 6, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-20', 7, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-20', 8, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-20', 9, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-20', 10, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-20', 11, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-20', 12, 1);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-20', 1, 1);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-20', 2, 1);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-20', 3, 12);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-20', 4, 12);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-20', 5, 12);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-20', 6, 12);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-20', 7, 12);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-20', 8, 12);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-20', 9, 12);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-20', 10, 12);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-20', 11, 12);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-20', 12, 12);
 
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 1, 11);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 2, 11);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 3, 11);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 4, 11);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 5, 11);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 6, 11);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 7, 11);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 8, 11);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 9, 11);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 10, 11);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-26', 1, 10);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-26', 2, 10);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-26', 3, 10);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-26', 4, 10);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-26', 5, 10);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-26', 6, 10);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-26', 7, 10);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-26', 8, 10);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-26', 9, 10);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-26', 10, 10);
 
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 1, 10);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 2, 10);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 3, 10);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 4, 10);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 5, 10);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 6, 10);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 7, 10);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 8, 10);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 9, 10);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-27', 1, 9);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-27', 2, 9);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-27', 3, 9);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-27', 4, 9);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-27', 5, 9);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-27', 6, 9);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-27', 7, 9);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-27', 8, 9);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-27', 9, 9);
 
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 1, 9);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 2, 9);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 3, 9);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 4, 9);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 5, 9);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 6, 9);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 7, 9);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 8, 9);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-28', 1, 8);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-28', 2, 8);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-28', 3, 8);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-28', 4, 8);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-28', 5, 8);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-28', 6, 8);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-28', 7, 8);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-28', 8, 8);
 
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 1, 8);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 2, 8);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 3, 8);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 4, 8);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 5, 8);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 6, 8);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 7, 8);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-29', 1, 7);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-29', 2, 7);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-29', 3, 7);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-29', 4, 7);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-29', 5, 7);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-29', 6, 7);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-29', 7, 7);
 
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 1, 7);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 2, 7);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 3, 7);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 4, 7);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 5, 7);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 6, 7);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-30', 1, 6);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-30', 2, 6);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-30', 3, 6);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-30', 4, 6);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-30', 5, 6);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-30', 6, 6);
 
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 1, 6);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 2, 6);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 3, 6);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 4, 6);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 5, 6);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-30', 7, 5);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-30', 8, 5);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-30', 9, 5);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-30', 10, 5);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-30', 11, 5);
 
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 1, 5);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 2, 5);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 3, 5);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 4, 5);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-29', 8, 4);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-29', 9, 4);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-29', 10, 4);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-29', 11, 4);
 
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 1, 4);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 2, 4);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 3, 4);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-28', 9, 3);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-28', 10, 3);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-28', 11, 3);
 
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 1, 3);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 2, 3);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-27', 10, 2);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('riwon', '2025-04-27', 11, 2);
 
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-26', 1, 2);
-
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-30', 1, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-30', 2, 1);
-INSERT INTO reservation(name, date, time_id, theme_id) VALUES ('mimi', '2025-04-30', 3, 1);
+INSERT INTO reservation(name, date, time_id, theme_id)
+VALUES ('mimi', '2025-04-26', 11, 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE reservation_time
 (
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
     start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -10,20 +10,18 @@ CREATE TABLE theme
     id          BIGINT       NOT NULL AUTO_INCREMENT,
     name        VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    thumbnail VARCHAR(255) NOT NULL,
+    thumbnail   VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-
 CREATE TABLE reservation
 (
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    date DATE NOT NULL,
-    time_id BIGINT,
-    theme_id BIGINT,                                        -- 컬럼 추가
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL,
+    date     DATE         NOT NULL,
+    time_id  BIGINT,
+    theme_id BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)            -- 외래키 추가
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
-
