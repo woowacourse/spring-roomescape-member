11418275f11368f5dc4692c913f24e2e7c6d6da5
2jin2.1031 <2jin2.1031@gmail.com>
2025-05-01 15:25:42 +0900

    refactor: 로그 출력 로직 제거

--- src/main/java/roomescape/global_exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/global_exception/GlobalExceptionHandler.java
@@ -11,13 +11,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handlerIllegalArgument(CustomException e) {
-        e.printStackTrace();
         return ResponseEntity.status(e.getStatusValue()).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        e.printStackTrace();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("잘못된 형식의 요청입니다."));
     }
 }
