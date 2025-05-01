1d354fd0e049eea27e1b7bc809da68b6a6e89779
jhpark1227 <junhyeok0246@naver.com>
2025-05-01 17:06:40 +0900

    chore: 더미데이터 파일 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,61 @@
+INSERT INTO reservation_time (start_at)
+values ('09:00');
+INSERT INTO reservation_time (start_at)
+values ('10:00');
+INSERT INTO reservation_time (start_at)
+values ('11:00');
+INSERT INTO reservation_time (start_at)
+values ('12:00');
+
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마1', 'desc1', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마2', 'desc2', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마3', 'desc3', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마4', 'desc4', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마5', 'desc5', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마6', 'desc6', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마7', 'desc7', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마8', 'desc8', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마9', 'desc9', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마10', 'desc10', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('테마11', 'desc11', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('포비', '2025-5-3', 1, 1);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('포비', '2025-4-30', 1, 1);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('포라', '2025-4-29', 1, 2);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('포포', '2025-4-28', 2, 1);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('리사', '2025-4-27', 2, 2);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('리사', '2025-4-27', 2, 3);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('리사', '2025-4-27', 2, 4);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('리사', '2025-4-27', 2, 5);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('리사', '2025-4-27', 2, 6);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('리사', '2025-4-27', 2, 7);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('리사', '2025-4-27', 2, 8);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('리사', '2025-4-27', 2, 9);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('리사', '2025-4-27', 2, 11);
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('리사', '2025-4-26', 2, 11);
+
