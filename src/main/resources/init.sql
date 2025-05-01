b674e5e4b8c60ad5abfa211d4cf8b9b8f2758b35
GiSeok Kang <ime.rokz@gmail.com>
2025-05-01 12:08:00 +0900

    chore: 초기 데이터 삽입 sql 파일 생성

---
+++ src/main/resources/init.sql
@@ -0,0 +1,12 @@
+INSERT INTO theme (name, description, thumbnail) VALUES ('테마1', '재밌음', 'theme1');
+INSERT INTO theme (name, description, thumbnail) VALUES ('테마2', '무서움', 'theme2');
+INSERT INTO theme (name, description, thumbnail) VALUES ('테마3', '놀라움', 'theme3');
+
+INSERT INTO reservation_time (start_at) VALUES ('10:00');
+INSERT INTO reservation_time (start_at) VALUES ('11:00');
+INSERT INTO reservation_time (start_at) VALUES ('12:00');
+
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('테스트1', '2025-04-28', 1, 1);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('테스트2', '2025-04-28', 2, 1);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('테스트3', '2025-04-26', 1, 3);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('테스트4', '2025-04-18', 1, 2);
