2ab428689cb79bf249d6197b39362836122abe4b
ryan-dia <cjfdnjs158@naver.com>
2025-05-01 17:16:29 +0900

    refactor: validated 어노테이션 추가
    Co-authored-by: joon6093 <joon6093@naver.com>

--- src/main/java/roomescape/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/reservation/controller/ReservationController.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import roomescape.reservationtime.dto.AvailableReservationTimeResponse;
 @RestController
 @RequestMapping("/reservations")
 @RequiredArgsConstructor
+@Validated
 public class ReservationController {
 
     private final ReservationService reservationService;
