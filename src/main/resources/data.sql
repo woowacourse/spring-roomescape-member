a1342c9854d5624611fbe14cd19769de706a6b72
Chaeyoung714 <202102760@hufs.ac.kr>
2025-05-01 16:24:35 +0900

    chore: 초기 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,8 +8,8 @@ VALUES ('10:00'),
 
 INSERT INTO theme (name, description, thumbnail)
 VALUES ('테마1', '테마1입니다.',
-        'https://static.wikia.nocookie.net/pokemon/images/0/03/%EB%AA%A8%EB%8B%A4%ED%94%BC_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405013619&path-prefix=ko'),
+        'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
        ('테마2', '테마2입니다.',
-        'https://static.wikia.nocookie.net/pokemon/images/0/03/%EB%AA%A8%EB%8B%A4%ED%94%BC_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405013619&path-prefix=ko'),
+        'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
        ('테마3', '테마3입니다.',
-        'https://static.wikia.nocookie.net/pokemon/images/0/03/%EB%AA%A8%EB%8B%A4%ED%94%BC_%EA%B3%B5%EC%8B%9D_%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png/revision/latest?cb=20170405013619&path-prefix=ko');
+        'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
