6fd53aacd19425a52b62ee02f1a3dce4fc8795e6
GiSeok Kang <ime.rokz@gmail.com>
2025-05-01 15:06:57 +0900

    test: 테마와 날짜에 따른 예약 가능 시간 조회 테스트

--- src/test/java/roomescape/reservationTime/service/ReservationTimeServiceTest.java
+++ src/test/java/roomescape/reservationTime/service/ReservationTimeServiceTest.java
@@ -2,6 +2,7 @@ package roomescape.reservationTime.service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +13,9 @@ import roomescape.reservation.service.FakeReservationRepository;
 import roomescape.reservation.service.FakeReservationTimeRepository;
 import roomescape.reservationTime.domain.ReservationTime;
 import roomescape.reservationTime.domain.ReservationTimeRepository;
+import roomescape.reservationTime.dto.TimeConditionRequest;
+import roomescape.reservationTime.dto.TimeConditionResponse;
+import roomescape.theme.domain.Theme;
 
 class ReservationTimeServiceTest {
     private ReservationTimeService reservationTimeService;
@@ -19,20 +23,39 @@ class ReservationTimeServiceTest {
     @BeforeEach
     void beforeEach() {
         ReservationTime reservationTime1 = new ReservationTime(null, LocalTime.of(10, 0));
+        ReservationTime reservationTime2 = new ReservationTime(null, LocalTime.of(11, 0));
+        Theme theme = new Theme(1L, "테마", "테마", "테마");
 
         ReservationTimeRepository reservationTimeRepository = new FakeReservationTimeRepository();
         Long id = reservationTimeRepository.save(reservationTime1);
+        reservationTimeRepository.save(reservationTime2);
+
         reservationTime1 = reservationTimeRepository.findById(id);
         ReservationRepository reservationRepository = new FakeReservationRepository();
-        reservationRepository.save(new Reservation(null, "홍길동", LocalDate.of(2024, 10, 6), reservationTime1, null));
+        reservationRepository.save(new Reservation(null, "홍길동", LocalDate.of(2024, 10, 6), reservationTime1, theme));
 
         reservationTimeService = new ReservationTimeService(reservationRepository, reservationTimeRepository);
     }
 
+    @DisplayName("이미 존재하는 예약이 있는 경우 예약 시간을 삭제할 수 없다.")
     @Test
-    @DisplayName("이미 존재하는 예약이 있는 경우 예역 시간을 삭제할 수 없다.")
     void can_not_delete_when_reservation_exists() {
         Assertions.assertThatThrownBy(() -> reservationTimeService.deleteReservationTimeById(1L))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("예약 가능 시간 조회 테스트")
+    @Test
+    void time_condition_test() {
+        LocalDate localDate = LocalDate.of(2024, 10, 6);
+        Long themeId = 1L;
+
+        List<TimeConditionResponse> responses = reservationTimeService.getTimesWithCondition(
+                new TimeConditionRequest(localDate, themeId));
+
+        Assertions.assertThat(responses).containsExactlyInAnyOrder(
+                new TimeConditionResponse(1L, LocalTime.of(10, 0), true),
+                new TimeConditionResponse(2L, LocalTime.of(11, 0), false)
+                );
     }
 }
