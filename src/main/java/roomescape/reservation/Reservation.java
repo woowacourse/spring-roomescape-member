44e00b1fc86b5ede818e12bdef15531f2d0148f0
changuii <rhljh7410@gmail.com>
2025-05-01 13:09:20 +0900

    refactor: 불필요한 메서드 제거 및 생성자 추가

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -1,10 +1,10 @@
-package roomescape.reservation.domain;
+package roomescape.reservation;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import roomescape.globalException.BadRequestException;
-import roomescape.reservationTime.domain.ReservationTime;
-import roomescape.theme.domain.Theme;
+import roomescape.reservationTime.ReservationTime;
+import roomescape.theme.Theme;
 
 public class Reservation {
 
@@ -22,29 +22,25 @@ public class Reservation {
         this.theme = theme;
     }
 
+    public Reservation(final String name, final LocalDate date) {
+        this(null, name, date, null, null);
+    }
+
     public static Reservation of(String name, LocalDate date, ReservationTime reservationTime, Theme theme) {
         LocalDateTime dateTime = LocalDateTime.of(date, reservationTime.getStartAt());
         validateTense(dateTime);
         return new Reservation(null, name, date, reservationTime, theme);
     }
 
-    private static void validateTense(LocalDateTime dateTime) {
+    private static void validateTense(final LocalDateTime dateTime) {
         if (isPastTense(dateTime)) {
             throw new BadRequestException("과거시점으로 예약을 진행할 수 없습니다.");
         }
     }
 
-    private static boolean isPastTense(LocalDateTime dateTime) {
+    private static boolean isPastTense(final LocalDateTime dateTime) {
         LocalDateTime now = LocalDateTime.now();
         return dateTime.isBefore(now);
-    }
-
-    public boolean isSameDateTime(Reservation compare) {
-        return this.getDateTime().isEqual(compare.getDateTime());
-    }
-
-    public LocalDateTime getDateTime() {
-        return LocalDateTime.of(date, reservationTime.getStartAt());
     }
 
     public Long getId() {
