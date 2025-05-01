148a3fb8e3e7672a4a778cbf99730547225b5e93
jhan0121 <jhan0121@gmail.com>
2025-05-01 15:02:29 +0900

    refactor: DB 스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -19,8 +19,8 @@ CREATE TABLE reservation
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
     date     DATE         NOT NULL,
-    time_id  BIGINT,
-    theme_id BIGINT,
+    time_id  BIGINT       NOT NULL,
+    theme_id BIGINT       NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
