810e1af1f7210b1b7ad579927a3e0601ffafa000
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-05-01 13:41:33 +0900

    test: ThemeServiceImpl 테스트 추가

--- src/test/java/roomescape/RoomescapeApplicationTest.java
+++ src/test/java/roomescape/RoomescapeApplicationTest.java
@@ -1,13 +1,12 @@
-//package roomescape;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-//
-//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-//class RoomescapeApplicationTest {
-//    @Test
-//    void contextLoads() {
-//    }
-//}
-//TODO: 왜 @SpringBootTest는 격리가 안될까..  (2025-04-29, 화, 14:18)
+package roomescape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+class RoomescapeApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
