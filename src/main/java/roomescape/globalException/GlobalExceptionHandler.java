70b73b3e12838ece77f2cdef6299698602f605b5
changuii <rhljh7410@gmail.com>
2025-05-01 13:08:57 +0900

    refactor: 변수 불변 추가 및 stacktrace 출력 제거

--- src/main/java/roomescape/globalException/GlobalExceptionHandler.java
+++ src/main/java/roomescape/globalException/GlobalExceptionHandler.java
@@ -10,14 +10,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponse> handlerIllegalArgument(CustomException e) {
-        e.printStackTrace();
-        return ResponseEntity.status(e.getStatusValue()).body(new ErrorResponse(e.getMessage()));
+    public ResponseEntity<ErrorResponse> handlerIllegalArgument(
+            final CustomException e
+    ) {
+        return ResponseEntity.status(e.getStatusValue())
+                .body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        e.printStackTrace();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("잘못된 형식의 요청입니다."));
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(
+            final HttpMessageNotReadableException e
+    ) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse("잘못된 형식의 요청입니다."));
     }
 }
