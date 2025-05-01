9726e5aeb10dd2f0df375143e8a6b35af5441271
GiSeok Kang <ime.rokz@gmail.com>
2025-05-01 15:34:05 +0900

    remove: 쓸모없는 주석 제거

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -107,10 +107,6 @@ function renderAvailableTimes(times) {
     return;
   }
   times.forEach(time => {
-    /*
-    TODO: [3단계] 사용자 예약 - 예약 가능 시간 조회 API 호출 후 렌더링
-          response 명세에 맞춰 createSlot 함수 호출 시 값 설정
-    */
     const startAt = time.startAt;
     const timeId = time.id;
     const alreadyBooked = time.alreadyBooked;
@@ -149,8 +145,7 @@ function onReservationButtonClick() {
   if (selectedDate && selectedThemeId && selectedTimeId) {
 
     /*
-    TODO: [3단계] 사용자 예약 - 예약 요청 API 호출
-          [5단계] 예약 생성 기능 변경 - 사용자
+    TODO: [5단계] 예약 생성 기능 변경 - 사용자
           request 명세에 맞게 설정
     */
     const reservationData = {
