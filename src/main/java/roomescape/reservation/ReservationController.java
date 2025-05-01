62348575aaa54a4b8f24aa8308165a047627d2cd
changuii <rhljh7410@gmail.com>
2025-05-01 13:09:30 +0900

    refactor: 네이밍 및 패키지 변경

---
+++ src/main/java/roomescape/reservation/ReservationController.java
@@ -0,0 +1,50 @@
+package roomescape.reservation;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.reservation.dto.ReservationRequest;
+import roomescape.reservation.dto.ReservationResponse;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    public ReservationController(
+            @Autowired final ReservationService reservationService
+    ) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationResponse> create(
+            @RequestBody final ReservationRequest request
+    ) {
+        final ReservationResponse response = reservationService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReservationResponse>> readAll() {
+        final List<ReservationResponse> response = reservationService.readAll();
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(
+            @PathVariable("id") final Long id
+    ) {
+        reservationService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
