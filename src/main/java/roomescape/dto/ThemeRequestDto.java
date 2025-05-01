eb2a44a66f3a6b6e6e574de50cf726af1bd4d94e
taek2222 <oot309@daum.net>
2025-05-01 11:53:52 +0900

    feat: DTO 검증 추가

--- src/main/java/roomescape/dto/ThemeRequestDto.java
+++ src/main/java/roomescape/dto/ThemeRequestDto.java
@@ -2,8 +2,28 @@ package roomescape.dto;
 
 import roomescape.domain_entity.Theme;
 
-public record ThemeRequestDto(String name, String description, String thumbnail) {
+public record ThemeRequestDto(
+        String name, String description, String thumbnail
+) {
+    public ThemeRequestDto {
+        validateNotNull(name, description, thumbnail);
+    }
+
+    private void validateNotNull(String name, String description, String thumbnail) {
+        if (name == null) {
+            throw new IllegalArgumentException("잘못된 name 입력입니다.");
+        }
+        if (description == null) {
+            throw new IllegalArgumentException("잘못된 description 입력입니다.");
+        }
+        if (thumbnail == null) {
+            throw new IllegalArgumentException("잘못된 thumbnail 입력입니다.");
+        }
+    }
+
     public Theme toTheme() {
-        return new Theme(name, description, thumbnail);
+        return new Theme(
+                name, description, thumbnail
+        );
     }
 }
