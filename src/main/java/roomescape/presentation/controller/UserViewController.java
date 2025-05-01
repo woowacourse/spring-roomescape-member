255df734c9a649afa744b1fda1767aecef4996b2
saera <yook.saera@gmail.com>
2025-05-01 14:50:09 +0900

    refactor: 사용자 페이지 API 클래스명 변경
    Co-authored-by: YuyoungRhee <bbclara@naver.com>

--- src/main/java/roomescape/presentation/controller/UserViewController.java
+++ src/main/java/roomescape/presentation/controller/UserViewController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeViewController {
+public class UserViewController {
     @GetMapping("/")
     public String redirectReservationPage() {
         return "index";
