351286cc14b34398219613d1c9e3d2ebc8a15f74
junhaa <2171326@hansung.ac.kr>
2025-05-01 14:55:13 +0900

    feat: 서버 내부 예외를 500 코드로 반환하는 기능 구현

--- src/main/java/roomescape/reservation/controller/GlobalExceptionHandler.java
+++ src/main/java/roomescape/reservation/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package roomescape.reservation.controller;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,6 +21,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidFormatException.class)
     public ResponseEntity<String> handleInvalidFormatException(InvalidFormatException exception){
-        return ResponseEntity.badRequest().body("유효하지 않은 형식입니다.");
+        return ResponseEntity.badRequest().body("유효하지 않은 형식입니다.: " + exception.getMessage());
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception exception){
+        return ResponseEntity.internalServerError().body("서버 내부 오류입니다.: " + exception.getMessage());
+    }
+
 }
