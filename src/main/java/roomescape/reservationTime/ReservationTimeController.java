e39943616e396279a6833b3849cbec7dbb29fe34
changuii <rhljh7410@gmail.com>
2025-05-01 13:12:11 +0900

    refactor: 패키지 변경

---
+++ src/main/java/roomescape/reservationTime/ReservationTimeController.java
@@ -0,0 +1,63 @@
+package roomescape.reservationTime;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.reservationTime.dto.AvailableReservationTimeResponse;
+import roomescape.reservationTime.dto.ReservationTimeRequest;
+import roomescape.reservationTime.dto.ReservationTimeResponse;
+
+@RestController
+@RequestMapping("/times")
+public class ReservationTimeController {
+
+    private final ReservationTimeService reservationTimeService;
+
+    public ReservationTimeController(
+            @Autowired final ReservationTimeService reservationTimeService
+    ) {
+        this.reservationTimeService = reservationTimeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationTimeResponse> create(
+            @RequestBody final ReservationTimeRequest request
+    ) {
+        final ReservationTimeResponse response = reservationTimeService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReservationTimeResponse>> findAll() {
+        final List<ReservationTimeResponse> response = reservationTimeService.findAll();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/available-time")
+    public ResponseEntity<List<AvailableReservationTimeResponse>> findAllAvailableTimes(
+            @RequestParam("themeId") final Long themeId,
+            @RequestParam("date") final LocalDate date
+    ) {
+        final List<AvailableReservationTimeResponse> response = reservationTimeService
+                .findAllAvailableTimes(themeId, date);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(
+            @PathVariable("id") final Long id
+    ) {
+        reservationTimeService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
