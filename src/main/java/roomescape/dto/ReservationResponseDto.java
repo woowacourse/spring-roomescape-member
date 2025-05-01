bf15bb188066960925b63998364f99e93dd75b83
Chaeyoung714 <202102760@hufs.ac.kr>
2025-05-01 13:00:55 +0900

    feat: 예약 조회 시 테마 필드 추가

--- src/main/java/roomescape/dto/ReservationResponseDto.java
+++ src/main/java/roomescape/dto/ReservationResponseDto.java
@@ -4,14 +4,15 @@ import java.time.LocalDate;
 import roomescape.domain_entity.Reservation;
 
 public record ReservationResponseDto(
-        long id, String name, LocalDate date, ReservationTimeResponseDto time
+        long id, String name, LocalDate date, ReservationTimeResponseDto time, ThemeResponseDto theme
 ) {
     public static ReservationResponseDto from(Reservation reservation) {
         return new ReservationResponseDto(
                 reservation.getId(),
                 reservation.getName(),
                 reservation.getDate(),
-                ReservationTimeResponseDto.from(reservation.getTime())
+                ReservationTimeResponseDto.from(reservation.getTime()),
+                new ThemeResponseDto(reservation.getTheme())
         );
     }
 }
