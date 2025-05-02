775c8a99e0a61757e9bf1f3692a49b08635e21aa
saera <yook.saera@gmail.com>
2025-05-02 17:46:32 +0900

    fix: 동일한 테마와 일시로 중복 예약 가능한 오류 수정

--- src/main/java/roomescape/domain/Theme.java
+++ src/main/java/roomescape/domain/Theme.java
@@ -1,5 +1,7 @@
 package roomescape.domain;
 
+import java.util.Objects;
+
 public class Theme {
     private final Long id;
     private final String name;
@@ -39,5 +41,18 @@ public class Theme {
 
     public String getThumbnail() {
         return thumbnail;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Theme theme)) {
+            return false;
+        }
+        return Objects.equals(id, theme.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
