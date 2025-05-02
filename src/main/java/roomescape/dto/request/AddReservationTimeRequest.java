4f5c5e52ac9c34ae86b27610257753ff68623f32
sung-june27 <kmymy27@gmail.com>
2025-05-02 17:42:23 +0900

    feat: DTO null 검증 추가

--- src/main/java/roomescape/dto/request/AddReservationTimeRequest.java
+++ src/main/java/roomescape/dto/request/AddReservationTimeRequest.java
@@ -4,7 +4,9 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 import roomescape.domain.ReservationTime;
 
-public record AddReservationTimeRequest(@NotNull LocalTime startAt) {
+public record AddReservationTimeRequest(
+        @NotNull(message = "시간은 비어있을 수 없습니다.") LocalTime startAt
+) {
 
     public ReservationTime toEntity() {
         return new ReservationTime(null, startAt);
