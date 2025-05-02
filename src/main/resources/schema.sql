230378b0e79fff9450dc9dd9312a2c4691d50940
mimi <110226866+hongkikii@users.noreply.github.com>
2025-05-02 20:05:56 +0900

    refactor: 유니크 제약 조건 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE reservation_time
 CREATE TABLE theme
 (
     id          BIGINT       NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
+    name        VARCHAR(255) NOT NULL UNIQUE ,
     description VARCHAR(255) NOT NULL,
     thumbnail VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
