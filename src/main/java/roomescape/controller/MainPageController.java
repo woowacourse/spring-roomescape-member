de452c62d809c8252c7bff4f90fa9f7193fd735f
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-05-02 16:35:33 +0900

    refactor: requestMapping 명시 수정

--- src/main/java/roomescape/controller/MainPageController.java
+++ src/main/java/roomescape/controller/MainPageController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/reservation")
 public class MainPageController {
 
-    @GetMapping("reservation")
+    @GetMapping
     public String mainPage() {
         return "reservation";
     }
