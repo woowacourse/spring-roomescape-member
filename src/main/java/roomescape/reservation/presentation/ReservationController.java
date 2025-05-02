a4e6941ed12b92a934a1294e29e1f3e449ce1c1d
GiSeok Kang <ime.rokz@gmail.com>
2025-05-02 16:26:57 +0900

    refactor: 중복되는 URI @RequestMapping 적용

--- src/main/java/roomescape/reservation/presentation/ReservationController.java
+++ src/main/java/roomescape/reservation/presentation/ReservationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.common.exceptionHandler.dto.ExceptionResponse;
 import roomescape.reservation.service.ReservationService;
@@ -18,6 +19,7 @@ import roomescape.reservation.dto.ReservationRequest;
 import roomescape.reservation.dto.ReservationResponse;
 
 @RestController
+@RequestMapping("/reservations")
 public class ReservationController {
 
     private final ReservationService reservationService;
@@ -26,19 +28,19 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
-    @GetMapping("/reservations")
+    @GetMapping
     public ResponseEntity<List<ReservationResponse>> getReservations() {
         List<ReservationResponse> response = reservationService.getReservations();
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/reservations")
+    @PostMapping
     public ResponseEntity<ReservationResponse> createReservation(@RequestBody final ReservationRequest request) {
         ReservationResponse response = reservationService.createReservation(request);
         return ResponseEntity.created(URI.create("admin/reservation")).body(response);
     }
 
-    @DeleteMapping("/reservations/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReservationById(@PathVariable("id") final Long id) {
         reservationService.deleteReservationById(id);
         return ResponseEntity.noContent().build();
