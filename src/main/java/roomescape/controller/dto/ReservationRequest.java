d6764bf4f6ac4eeb1752741621ba9c48e9b4e062
DEV_WOONG <ywcsuwon@gmail.com>
2025-05-02 14:42:23 +0900

    refactor: 미사용 어노테이션 삭제

--- src/main/java/roomescape/controller/dto/ReservationRequest.java
+++ src/main/java/roomescape/controller/dto/ReservationRequest.java
@@ -2,12 +2,13 @@ package roomescape.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record ReservationRequest(
         @NotBlank String name,
         @NotNull LocalDate date,
-        @NotNull long timeId,
-        @NotNull long themeId
+        long timeId,
+        long themeId
 ) {
 }
