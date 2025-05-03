6b0efffeb84cc200122d5dafa5f68149f3d9b3ba
LJM\eueo8 <eueo8259@gmail.com>
2025-05-03 22:55:23 +0900

    feat: 예외 처리 로직 개선 및 커스텀 예외 핸들링 추가

--- src/main/java/roomescape/common/RoomescapeExceptionHandler.java
+++ src/main/java/roomescape/common/RoomescapeExceptionHandler.java
@@ -6,7 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import roomescape.common.exception.DuplicatedException;
+import roomescape.common.exception.InvalidInputException;
 import roomescape.common.exception.NotFoundException;
+import roomescape.common.exception.ReservationDateException;
 import roomescape.common.exception.ResourceInUseException;
 
 @ControllerAdvice
@@ -14,11 +16,21 @@ public class RoomescapeExceptionHandler {
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<String> handleException(IllegalStateException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("현재 시스템 상태에서는 요청을 처리할 수 없습니다", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleException(IllegalArgumentException ex) {
+        return new ResponseEntity<>("유효하지 않은 값이 전달되었습니다", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ReservationDateException.class)
+    public ResponseEntity<String> handleException(ReservationDateException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidInputException.class)
+    public ResponseEntity<String> handleException(InvalidInputException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
@@ -40,5 +52,10 @@ public class RoomescapeExceptionHandler {
     @ExceptionHandler(ResourceInUseException.class)
     public ResponseEntity<String> handleException(ResourceInUseException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
