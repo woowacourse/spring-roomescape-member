b7bc6efe3a311157549f4bdd180e7b5e9bf9f029
LJM\eueo8 <eueo8259@gmail.com>
2025-05-04 00:32:38 +0900

    feat : @Valid로 터지는 예외 잡는 ExceptionHandler들 추가

--- src/main/java/roomescape/common/RoomescapeExceptionHandler.java
+++ src/main/java/roomescape/common/RoomescapeExceptionHandler.java
@@ -1,8 +1,11 @@
 package roomescape.common;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import java.util.zip.DataFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import roomescape.common.exception.DuplicatedException;
@@ -22,6 +25,25 @@ public class RoomescapeExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleException(IllegalArgumentException ex) {
         return new ResponseEntity<>("유효하지 않은 값이 전달되었습니다", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+        return new ResponseEntity<>("잘못된 요청입니다", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleJsonParseException(HttpMessageNotReadableException ex) {
+        if (ex.getCause() instanceof InvalidFormatException formatEx) {
+            String targetType = formatEx.getTargetType().getSimpleName();
+            if (targetType.equals("LocalDate")) {
+                return new ResponseEntity<>("날짜 형식이 잘못되었습니다. yyyy-MM-dd 형식이어야 합니다.", HttpStatus.BAD_REQUEST);
+            }
+            if (targetType.equals("LocalTime")) {
+                return new ResponseEntity<>("시간 형식이 잘못되었습니다. HH:mm 형식이어야 합니다.", HttpStatus.BAD_REQUEST);
+            }
+        }
+        return new ResponseEntity<>("요청 형식이 잘못되었습니다.", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ReservationDateException.class)
