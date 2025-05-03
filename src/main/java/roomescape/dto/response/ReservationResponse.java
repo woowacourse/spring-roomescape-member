1330d8656a684d926342e1f7df22ed9e4b5e13d9
kjungw <sagakkb@gmail.com>
2025-05-03 14:00:58 +0900

    refactor: 응답 DTO에서 도메인 제거

--- src/main/java/roomescape/dto/response/ReservationResponse.java
+++ src/main/java/roomescape/dto/response/ReservationResponse.java
@@ -2,22 +2,20 @@ package roomescape.dto.response;
 
 import java.time.LocalDate;
 import roomescape.domain.Reservation;
-import roomescape.domain.ReservationTime;
-import roomescape.domain.Theme;
 
 public record ReservationResponse(
         Long id,
         String name,
         LocalDate date,
-        ReservationTime time,
-        Theme theme
+        ReservationTimeResponse time,
+        ThemeResponse theme
 ) {
 
     public ReservationResponse(Reservation reservation) {
         this(reservation.getId(),
                 reservation.getName(),
                 reservation.getDate(),
-                reservation.getTime(),
-                reservation.getTheme());
+                new ReservationTimeResponse(reservation.getTime()),
+                new ThemeResponse(reservation.getTheme()));
     }
 }
