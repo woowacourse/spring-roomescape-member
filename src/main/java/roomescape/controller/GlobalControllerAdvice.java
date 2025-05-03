c9c09529e72ff43962cb834679736becd7a90744
sung-june27 <kmymy27@gmail.com>
2025-05-03 15:04:55 +0900

    feat: 에러 응답 형식 통일

--- src/main/java/roomescape/controller/GlobalControllerAdvice.java
+++ src/main/java/roomescape/controller/GlobalControllerAdvice.java
@@ -1,14 +1,36 @@
 package roomescape.controller;
 
+import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import roomescape.controller.dto.ErrorResponse;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(exception = IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        ErrorResponse response = ErrorResponse.from(e.getMessage());
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(exception = MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorResponse>> handleValidationException(MethodArgumentNotValidException e) {
+        List<ErrorResponse> responses = createValidationErrorMessage(e.getBindingResult());
+
+        return ResponseEntity.badRequest().body(responses);
+    }
+
+    private List<ErrorResponse> createValidationErrorMessage(BindingResult bindingResult) {
+        return bindingResult.getAllErrors()
+                .stream()
+                .map(ObjectError::getDefaultMessage)
+                .map(ErrorResponse::from)
+                .toList();
     }
 }
