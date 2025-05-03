0474a9505d2b7f9181bf283ec75c4d0aa524ceac
LeeJaeHoon <dlwogns3413@naver.com>
2025-05-04 02:17:38 +0900

    feat: ExceptionHandler @RestControllerAdvice로 변경

--- src/main/java/roomescape/controller/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/GlobalExceptionHandler.java
@@ -3,13 +3,13 @@ package roomescape.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.common.BusinessRuleViolationException;
 import roomescape.common.CoreException;
 import roomescape.common.NotFoundEntityException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
