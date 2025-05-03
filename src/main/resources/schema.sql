7d0b70ccc49a5d5cade6de5f86d31fc5490924d9
Minuring <mw9505@gmail.com>
2025-05-03 13:11:48 +0900

    refactor: 도메인 규칙에 따라 DB 스키마 제약조건 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,15 +5,15 @@ DROP TABLE IF EXISTS RESERVATION_TIME;
 CREATE TABLE IF NOT EXISTS reservation_time
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL,
+    start_at TIME NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS theme
 (
     id          BIGINT       NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    name        VARCHAR(10) NOT NULL,
+    description VARCHAR(50) NOT NULL,
     thumbnail VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -21,8 +21,8 @@ CREATE TABLE IF NOT EXISTS theme
 CREATE TABLE IF NOT EXISTS reservation
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    date VARCHAR(255) NOT NULL,
+    name VARCHAR(5) NOT NULL,
+    date DATE NOT NULL,
     time_id BIGINT,
     theme_id BIGINT,
     PRIMARY KEY (id),
