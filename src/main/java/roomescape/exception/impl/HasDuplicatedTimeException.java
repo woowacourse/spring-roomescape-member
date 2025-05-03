d8dbe24d6080cacfff994d7eb59a8fb39ba3aa40
Dompoo <dlckdrms0517@gmail.com>
2025-05-03 11:28:09 +0900

    style: 개행 스타일 일치

--- src/main/java/roomescape/exception/impl/HasDuplicatedTimeException.java
+++ src/main/java/roomescape/exception/impl/HasDuplicatedTimeException.java
@@ -10,4 +10,3 @@ public class HasDuplicatedTimeException extends RootException {
         super(MESSAGE);
     }
 }
-
--- src/main/java/roomescape/exception/impl/ReservationTimeNotFoundException.java
+++ src/main/java/roomescape/exception/impl/ReservationTimeNotFoundException.java
@@ -6,7 +6,6 @@ public class ReservationTimeNotFoundException extends RootException {
 
     private static final String MESSAGE = "해당하는 예약 시간을 찾을 수 없습니다.";
 
-
     public ReservationTimeNotFoundException() {
         super(MESSAGE);
     }
