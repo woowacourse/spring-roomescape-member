6b08601c212a78fef2967ab0419d9b0ade5a22d8
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-05-03 21:41:21 +0900

    test : ReservationH2Dao 테스트 작성

--- src/main/java/roomescape/repository/dao/ReservationH2Dao.java
+++ src/main/java/roomescape/repository/dao/ReservationH2Dao.java
@@ -10,7 +10,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
