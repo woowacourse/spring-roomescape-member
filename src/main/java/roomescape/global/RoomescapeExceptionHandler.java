cc511469570b6375eb217dd032afbddb26837ce2
ljhee92 <weilainib7@gmail.com>
2025-05-03 23:17:21 +0900

    refactor: Validation 예외 처리, 에러 응답 JSON으로 수정

--- src/main/java/roomescape/global/RoomescapeExceptionHandler.java
+++ src/main/java/roomescape/global/RoomescapeExceptionHandler.java
@@ -1,9 +1,15 @@
 package roomescape.global;
 
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import roomescape.presentation.dto.response.ErrorResponse;
 
 @RestControllerAdvice
 public class RoomescapeExceptionHandler {
@@ -14,7 +20,19 @@ public class RoomescapeExceptionHandler {
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        String message = e.getMessage();
+        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, message);
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult()
+                .getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, message);
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 }
---
+++ src/main/java/roomescape/presentation/dto/response/ErrorResponse.java
@@ -0,0 +1,17 @@
+package roomescape.presentation.dto.response;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+        LocalDateTime timestamp,
+        int status,
+        String message
+    ) {
+
+    public static ErrorResponse of(HttpStatus status, String message) {
+        LocalDateTime timestamp = LocalDateTime.now();
+        return new ErrorResponse(timestamp, status.value(), message);
+    }
+}
