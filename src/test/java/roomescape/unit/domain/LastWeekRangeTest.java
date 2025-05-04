28727512ffac603d6c181c7c164a5af2069890d4
HyeonsuLee <leehyeonsu4888@naver.com>
2025-05-04 19:22:12 +0900

    test: LastWeekRange테스트 작성

---
+++ src/test/java/roomescape/unit/domain/LastWeekRangeTest.java
@@ -0,0 +1,37 @@
+package roomescape.unit.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static roomescape.common.Constant.FIXED_CLOCK;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import roomescape.theme.domain.LastWeekRange;
+
+public class LastWeekRangeTest {
+
+    @Test
+    void 시작일은_현재_기준_7일_전이다() {
+        // given
+        LocalDate beforeSevenDays = LocalDate.now(FIXED_CLOCK);
+        LastWeekRange lastWeekRange = new LastWeekRange(beforeSevenDays);
+
+        // when
+        LocalDate start = lastWeekRange.getStart();
+
+        // then
+        assertThat(start).isEqualTo(beforeSevenDays.minusDays(7));
+    }
+
+    @Test
+    void 종료일은_현재_기준_하루_전이다() {
+        // given
+        LocalDate beforeSevenDays = LocalDate.now(FIXED_CLOCK);
+        LastWeekRange lastWeekRange = new LastWeekRange(beforeSevenDays);
+
+        // when
+        LocalDate end = lastWeekRange.getEnd();
+
+        // then
+        assertThat(end).isEqualTo(beforeSevenDays.minusDays(1));
+    }
+}
