275ab3c56aa84b9a9be91a8dbe88de495210de63
kjungw <sagakkb@gmail.com>
2025-05-05 00:02:33 +0900

    refactor: 예외 핸들링 추가

--- src/main/java/roomescape/controller/advice/GlobalAdvice.java
+++ src/main/java/roomescape/controller/advice/GlobalAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.context.MessageSourceResolvable;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -43,13 +44,20 @@ public class GlobalAdvice {
         return ResponseEntity.badRequest().body(String.join("\n", errorMessage));
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException exception) {
+        return ResponseEntity.badRequest().body("형식이 적절하지 않은 요청메세지 입니다.");
+    }
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> illegalArgumentExceptionHandler(IllegalArgumentException exception) {
-        return ResponseEntity.internalServerError().body("서버 내부에서 로직 예외 발생");
+        System.out.println("exception = " + exception);
+        return ResponseEntity.internalServerError().body("서버 내부에서 로직 예외 발생헸습니다.");
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> runtimeExceptionHandler(RuntimeException exception) {
-        return ResponseEntity.internalServerError().body("예상치 못한 예외 발생");
+        System.out.println("exception = " + exception);
+        return ResponseEntity.internalServerError().body("예상치 못한 예외 발생헸습니다.");
     }
 }
