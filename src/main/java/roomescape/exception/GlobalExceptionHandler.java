ec404a155459ab6b38fcd53b0bea3aebee3c913d
CheChe903 <cheche903@naver.com>
2025-05-04 10:38:50 +0900

    refactor(GlobalExceptionHandler): 예외 상황에 맞게 응답 코드 수정

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -11,27 +11,25 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<FailureResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return createBadRequestResponse(ex);
+        return createFailureResponse(ex, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ReservationDuplicateException.class)
     public ResponseEntity<FailureResponse> handleReservationDuplicateException(ReservationDuplicateException ex) {
-        return createBadRequestResponse(ex);
+        return createFailureResponse(ex, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(ReservationExistException.class)
     public ResponseEntity<FailureResponse> handleReservationExistException(ReservationExistException ex) {
-        return createBadRequestResponse(ex);
+        return createFailureResponse(ex, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<FailureResponse> handleNoSuchElementException(NoSuchElementException ex) {
-        return createBadRequestResponse(ex);
+        return createFailureResponse(ex, HttpStatus.NOT_FOUND);
     }
 
-    private ResponseEntity<FailureResponse> createBadRequestResponse(RuntimeException ex) {
-        return new ResponseEntity<>(
-                new FailureResponse(HttpStatus.BAD_REQUEST, ex.getMessage()), HttpStatus.BAD_REQUEST
-        );
+    private ResponseEntity<FailureResponse> createFailureResponse(final RuntimeException ex, HttpStatus status) {
+        return new ResponseEntity<>(new FailureResponse(status, ex.getMessage()), status);
     }
 }
