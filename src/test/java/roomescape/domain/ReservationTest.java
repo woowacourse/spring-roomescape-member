0277b3d2b0b2cd630a859a61f86be9504f8f89f8
taek2222 <oot309@daum.net>
2025-05-04 12:18:45 +0900

    test: 예약 테스트 메서드 개선 및 네이밍 일관성 수정

--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -3,83 +3,82 @@ package roomescape.domain;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+@DisplayName("예약(Reservation) 테스트")
 class ReservationTest {
 
-    private final String TEST_NAME = "moda";
-
     @Test
-    @DisplayName("지난 날짜 예약일 경우 예외가 발생한다")
-    void failIfPastDate() {
-        //given
-        LocalDateTime now = LocalDateTime.now();
-        Reservation reservation = new Reservation(
-                1L,
-                TEST_NAME,
-                now.toLocalDate().minusDays(1),
-                new ReservationTime(now.toLocalTime()),
-                new Theme("moda", "description", "thumbnail")
-        );
+    @DisplayName("이름이 255자 초과인 경우 예외가 발생한다")
+    void validateNameLengthThrowsExceptionWhenExceedsMaxLength() {
+        // given
+        String name = "a".repeat(256);
 
-        //when & then
-        assertThatThrownBy(() -> reservation.validatePastDateTime())
+        // when && then
+        assertThatThrownBy(() -> new Reservation(
+                name,
+                LocalDate.MIN, new ReservationTime(LocalTime.MIDNIGHT), new Theme("moda", "description", "thumbnail")))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("지난 날짜와 시간의 예약은 생성 불가능합니다.");
+                .hasMessage("요청 필드가 최대 제한 길이를 초과했습니다.");
     }
 
-    @Test
-    @DisplayName("같은 날짜와 지난 시간 예약일 경우 예외가 발생한다")
-    void failIfPastTime() {
+    @ParameterizedTest
+    @DisplayName("날짜 또는 시간이 과거인 경우 예외가 발생한다")
+    @MethodSource("createPastDateTimeParameters")
+    void validatePastDateTimeThrowsException(LocalDate date, LocalTime time) {
         //given
-        LocalDateTime now = LocalDateTime.now();
-        Reservation reservation = new Reservation(
-                1L,
-                TEST_NAME,
-                now.toLocalDate(),
-                new ReservationTime(now.toLocalTime().minusMinutes(1)),
-                new Theme("moda", "description", "thumbnail")
-        );
+        Reservation testReservation = createTestReservationByDateAndTime(date, time);
 
-        //when & then
-        assertThatThrownBy(() -> reservation.validatePastDateTime())
+        //when && then
+        assertThatThrownBy(testReservation::validatePastDateTime)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("지난 날짜와 시간의 예약은 생성 불가능합니다.");
+                .hasMessage("지난 날짜와 시간의 예약은 생성 불가능합니다.");
     }
 
-    @Test
-    @DisplayName("같은 날짜와 같은 시간 예약일 경우 예외가 발생하지 않는다.")
-    void successIfSameDateAndSameTime() {
-        //given
+    private static Stream<Arguments> createPastDateTimeParameters() {
         LocalDateTime now = LocalDateTime.now();
-        Reservation reservation = new Reservation(
-                1L,
-                TEST_NAME,
-                now.toLocalDate(),
-                new ReservationTime(now.toLocalTime()),
-                new Theme("moda", "description", "thumbnail")
+        return Stream.of(
+                Arguments.of(now.toLocalDate().minusDays(1), now.toLocalTime()),
+                Arguments.of(now.toLocalDate(), now.toLocalTime().minusMinutes(1))
         );
-
-        //when & then
-        assertThatNoException().isThrownBy(reservation::validatePastDateTime);
     }
 
-    @Test
-    @DisplayName("미래 날짜 예약일 경우 예외가 발생하지 않는다.")
-    void successIfFutureDate() {
-        //given
+    @ParameterizedTest
+    @DisplayName("날짜 또는 시간이 미래 혹은 같은 경우 예외가 발생하지 않는다.")
+    @MethodSource("futureDateTimeParameters")
+    void validatePastDateTimeSuccessWithFuture(LocalDate date, LocalTime time) {
+        // given
+        Reservation testReservation = createTestReservationByDateAndTime(date, time);
+
+        //when && then
+        assertThatNoException()
+                .isThrownBy(testReservation::validatePastDateTime);
+    }
+
+    private static Stream<Arguments> futureDateTimeParameters() {
         LocalDateTime now = LocalDateTime.now();
-        Reservation reservation = new Reservation(
+        return Stream.of(
+                Arguments.of(now.toLocalDate(), now.toLocalTime()),
+                Arguments.of(now.toLocalDate(), now.toLocalTime().plusMinutes(1)),
+                Arguments.of(now.toLocalDate().plusDays(1), now.toLocalTime())
+        );
+    }
+
+    private Reservation createTestReservationByDateAndTime(LocalDate date, LocalTime time) {
+        return new Reservation(
                 1L,
-                TEST_NAME,
-                now.toLocalDate().plusDays(1),
-                new ReservationTime(now.toLocalTime()),
+                "moda",
+                date,
+                new ReservationTime(time),
                 new Theme("moda", "description", "thumbnail")
         );
-
-        //when & then
-        assertThatNoException().isThrownBy(reservation::validatePastDateTime);
     }
 }
