28f281a77546453c43da72ad5f57aa0eaf333c76
Lee Dong Yeop <s01054830455@gmail.com>
2025-05-05 02:21:34 +0900

    test: JdbcThemeDaoImpl 테스트 추가

--- src/main/java/roomescape/domain/Theme.java
+++ src/main/java/roomescape/domain/Theme.java
@@ -1,5 +1,7 @@
 package roomescape.domain;
 
+import java.util.Objects;
+
 public class Theme {
 
     private Long id;
@@ -38,5 +40,21 @@ public class Theme {
 
     public String getThumbnail() {
         return thumbnail;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Theme theme = (Theme) o;
+        return Objects.equals(getId(), theme.getId()) && Objects.equals(getName(), theme.getName())
+                && Objects.equals(getDescription(), theme.getDescription()) && Objects.equals(
+                getThumbnail(), theme.getThumbnail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDescription(), getThumbnail());
     }
 }
