074c974033d03868eb556ffe24969ca2c13633a1
RaZel713 <razel7130@gmail.com>
2025-05-04 17:40:19 +0900

    test: 예약 시간 도메인 테스트 추가

---
+++ src/test/java/roomescape/domain/ReservationTimeTest.java
@@ -0,0 +1,43 @@
+package roomescape.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import roomescape.exception.custom.InvalidInputException;
+
+class ReservationTimeTest {
+
+    @DisplayName("예약 시간 생성시, 예약 시간이 빈 값이면 예외를 던진다")
+    @Test
+    void createReservationTimeTest1() {
+        // when // then
+        assertThatThrownBy(() -> new ReservationTime(null))
+                .isInstanceOf(InvalidInputException.class)
+                .hasMessage("시간은 빈 값이 입력될 수 없습니다");
+    }
+
+    @DisplayName("해당 예약 시간이 현재 시간보다 과거인 경우 true를 반환한다")
+    @Test
+    void validatePastTimeTest1() {
+        // given
+        LocalTime pastTime = LocalTime.now().minusHours(1);
+        ReservationTime reservationTime = new ReservationTime(pastTime);
+
+        // when // then
+        assertThat(reservationTime.validatePastTime()).isTrue();
+    }
+
+    @DisplayName("해당 예약 시간이 현재 시간보다 미래인 경우 false를 반환한다")
+    @Test
+    void validatePastTimeTest2() {
+        // given
+        LocalTime pastTime = LocalTime.now().plusHours(1);
+        ReservationTime reservationTime = new ReservationTime(pastTime);
+
+        // when // then
+        assertThat(reservationTime.validatePastTime()).isFalse();
+    }
+}
