f81b450111362af92bc3ed7a2b48a4fa680b53dd
RIWON <121172065+awrion3@users.noreply.github.com>
2025-05-04 17:30:32 +0900

    refactor: 응답 반환 코드 부분 수정
    - 예외 처리 대상 범위 수정
    - README.md 기능 목록 구조 개선

--- src/main/java/roomescape/common/GlobalExceptionHandler.java
+++ src/main/java/roomescape/common/GlobalExceptionHandler.java
@@ -1,14 +1,13 @@
 package roomescape.common;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(value = IllegalArgumentException.class)
-    public ResponseEntity<String> handleException(IllegalArgumentException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler({IllegalArgumentException.class, NullPointerException.class})
+    public ResponseEntity<String> handleException(Exception exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
     }
 }
