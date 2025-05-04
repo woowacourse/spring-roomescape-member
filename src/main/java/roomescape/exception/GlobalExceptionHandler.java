6a9cc533320a53791edde34c04a45d00f231bedb
jhan0121 <jhan0121@gmail.com>
2025-05-04 19:14:51 +0900

    refactor: 예외 응답 body 형식 변경

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -2,35 +2,41 @@ package roomescape.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.exception.custom.BusinessRuleViolationException;
 import roomescape.exception.custom.ExistedDuplicateValueException;
 import roomescape.exception.custom.InvalidInputException;
 import roomescape.exception.custom.NotFoundValueException;
+import roomescape.exception.dto.ErrorMessageResponse;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     public static final String ERROR_PREFIX = "[ERROR] ";
 
     @ExceptionHandler
-    public ResponseEntity<String> handleExistedDuplicateValueException(ExistedDuplicateValueException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ERROR_PREFIX + e.getMessage());
+    public ResponseEntity<ErrorMessageResponse> handleExistedDuplicateValueException(ExistedDuplicateValueException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new ErrorMessageResponse(ERROR_PREFIX + e.getMessage()));
     }
 
     @ExceptionHandler
-    public ResponseEntity<String> handleInvalidInputException(InvalidInputException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ERROR_PREFIX + e.getMessage());
+    public ResponseEntity<ErrorMessageResponse> handleInvalidInputException(InvalidInputException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorMessageResponse(ERROR_PREFIX + e.getMessage()));
     }
 
     @ExceptionHandler
-    public ResponseEntity<String> handleNotExistedValueException(NotFoundValueException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ERROR_PREFIX + e.getMessage());
+    public ResponseEntity<ErrorMessageResponse> handleNotExistedValueException(NotFoundValueException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorMessageResponse(ERROR_PREFIX + e.getMessage()));
     }
 
     @ExceptionHandler
-    public ResponseEntity<String> handlePharmaceuticalViolationException(BusinessRuleViolationException e) {
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ERROR_PREFIX + e.getMessage());
+    public ResponseEntity<ErrorMessageResponse> handlePharmaceuticalViolationException(
+            BusinessRuleViolationException e) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .body(new ErrorMessageResponse(ERROR_PREFIX + e.getMessage()));
     }
 }
