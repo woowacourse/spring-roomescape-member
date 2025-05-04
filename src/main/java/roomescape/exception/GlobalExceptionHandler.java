97ffde76edf70b42449386df20e28f281479217e
bowook <mbosx316@naver.com>
2025-05-04 21:57:59 +0900

    feat : 예외 핸들러 메서드 추가 및 body에 예외 메시지 담을 수 있도록 기능 추가

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -8,18 +8,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Void> handleIllegalArgumentException(final IllegalArgumentException e) {
-        return ResponseEntity.badRequest().build();
+    @ExceptionHandler({ReservationException.class, ThemeException.class})
+    public ResponseEntity<String> handleExceptions(final Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(DataNotFoundException.class)
-    public ResponseEntity<Void> handleDataNotFoundException(final DataNotFoundException e) {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<String> handleDataNotFoundException(final DataNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+
     @ExceptionHandler(DataExistException.class)
-    public ResponseEntity<Void> handDataExistException(final DataExistException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    public ResponseEntity<String> handDataExistException(final DataExistException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(SaveException.class)
+    public ResponseEntity<String> handleSaveException(final SaveException e) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(e.getMessage());
     }
 }
