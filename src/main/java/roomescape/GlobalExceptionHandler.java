d087f906817105b65e5d3735ccca52009a6a03ed
민경윤 <ruddbs3308@naver.com>
2025-05-04 15:11:23 +0900

    refactor: 불필요한 stack trace 출력 코드 제거

--- src/main/java/roomescape/GlobalExceptionHandler.java
+++ src/main/java/roomescape/GlobalExceptionHandler.java
@@ -16,19 +16,16 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = {ReservationNotFoundException.class, PlayTimeNotFoundException.class, ThemeNotFoundException.class})
     public ResponseEntity<Void> handleNotFoundException(final NoSuchElementException e) {
-        e.printStackTrace();
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(value = {DuplicateReservationException.class, DuplicatePlayTimeException.class})
     public ResponseEntity<Void> handleDuplicateException(final IllegalStateException e) {
-        e.printStackTrace();
         return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 
     @ExceptionHandler(value = IllegalArgumentException.class)
     public ResponseEntity<Void> handleIllegalArgumentException(final IllegalArgumentException e) {
-        e.printStackTrace();
         return ResponseEntity.badRequest().build();
     }
 }
