6caa69a1c2d349254b209bed85e3033e7ccc91ba
seaniiio <cnc4750@naver.com>
2025-05-05 03:34:20 +0900

    refactor: 사용하지 않는 커스텀 예외 제거

--- src/main/java/roomescape/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exceptionhandler/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import roomescape.exception.ForeignKeyConstraintViolationException;
 import roomescape.exception.ResourceNotExistException;
 
 @ControllerAdvice
@@ -14,12 +13,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleNullPointerException(NullPointerException e) {
         return ResponseEntity.internalServerError().body(e.getMessage());
     }
-
-    @ExceptionHandler(ForeignKeyConstraintViolationException.class)
-    public ResponseEntity<String> handleConstraintException(ForeignKeyConstraintViolationException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
-
+    
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
