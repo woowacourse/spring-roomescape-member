0748f14272b304b7886bca180ba37e169f37526a
taek2222 <oot309@daum.net>
2025-05-04 18:10:59 +0900

    test: JdbcReservationDao 테스트 코드 추가 및 테스트 데이터 확장

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,5 +1,18 @@
 INSERT INTO THEME (NAME, DESCRIPTION, THUMBNAIL)
-VALUES ('name', 'description', 'thumbnail');
+VALUES ('name1', 'description1', 'thumbnail1'),
+       ('name2', 'description2', 'thumbnail2'),
+       ('name3', 'description3', 'thumbnail3'),
+       ('name4', 'description4', 'thumbnail4'),
+       ('name5', 'description5', 'thumbnail5'),
+       ('name6', 'description6', 'thumbnail6'),
+       ('name7', 'description7', 'thumbnail7'),
+       ('name8', 'description8', 'thumbnail8'),
+       ('name9', 'description9', 'thumbnail9'),
+       ('name10', 'description10', 'thumbnail10'),
+       ('name11', 'description11', 'thumbnail11');
 
 INSERT INTO RESERVATION_TIME (START_AT)
 VALUES ('10:00');
+
+INSERT INTO RESERVATION (NAME, DATE, TIME_ID, THEME_ID)
+VALUES ('name', '2025-01-01', 1, 1)
