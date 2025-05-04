8a7923e0530209db5a4ba18e4e5ab4c9bd32e21b
taek2222 <oot309@daum.net>
2025-05-04 12:26:46 +0900

    test: 예약 시간 테스트 메서드 개선 및 네이밍 일관성 수정

--- src/test/java/roomescape/domain/ReservationTimeTest.java
+++ src/test/java/roomescape/domain/ReservationTimeTest.java
@@ -6,38 +6,17 @@ import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+@DisplayName("예약 시간(ReservationTime) 테스트")
 class ReservationTimeTest {
-
-    static Stream<Arguments> isFutureReservationTimeReturnsFalse() {
-        return Stream.of(
-                Arguments.of(LocalTime.now().truncatedTo(ChronoUnit.MINUTES)),
-                Arguments.of(LocalTime.now().plusMinutes(1))
-        );
-    }
-
-    @Test
-    @DisplayName("지난 예약 시간인 경우 TRUE 반환한다.")
-    void isPastReservationTimeReturnsTrue() {
-        // given
-        LocalTime now = LocalTime.now();
-        ReservationTime reservationTime = new ReservationTime(1L, now.minusMinutes(1));
-
-        // when
-        boolean result = reservationTime.isPastTime();
-
-        // then
-        assertThat(result).isTrue();
-    }
 
     @ParameterizedTest
     @MethodSource
-    @DisplayName("미래 예약 시간인 경우 FALSE 반환한다.")
-    void isFutureReservationTimeReturnsFalse(LocalTime time) {
+    @DisplayName("예약 시간이 현재 시간보다 이전이면 TRUE, 이후면 FALSE를 반환한다")
+    void isPastTime(LocalTime time, boolean expected) {
         // given
         ReservationTime reservationTime = new ReservationTime(1L, time);
 
@@ -45,6 +24,15 @@ class ReservationTimeTest {
         boolean result = reservationTime.isPastTime();
 
         // then
-        assertThat(result).isFalse();
+        assertThat(result).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> isPastTime() {
+        LocalTime now = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
+        return Stream.of(
+                Arguments.of(now.minusMinutes(1), true),
+                Arguments.of(now, false),
+                Arguments.of(now.plusMinutes(1), false)
+        );
     }
 }
