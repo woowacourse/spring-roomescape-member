3da91ac01760449fde81d6413710427535121d0f
kimdev <kim__dev@hufs.ac.kr>
2025-05-05 23:16:59 +0900

    feat: 예외 발생 시 반환할 응답 객체 생성

--- src/main/java/roomescape/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import roomescape.dto.response.ErrorResponse;
 import roomescape.exception.AssociatedReservationExistsException;
 import roomescape.exception.DuplicateEntityException;
 import roomescape.exception.EntityNotExistException;
@@ -14,31 +15,35 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleUnexpected(RuntimeException exception) {
-        return "예상하지 못한 예외가 발생했다.\n" + exception.getMessage();
+    public ErrorResponse handleUnexpected(RuntimeException exception) {
+        return generateErrorResponse("예상하지 못한 예외가 발생했다.\n" + exception.getMessage());
     }
 
     @ExceptionHandler(value = InvalidInputException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleInvalidInput(InvalidInputException exception) {
-        return exception.getMessage();
+    public ErrorResponse handleInvalidInput(InvalidInputException exception) {
+        return generateErrorResponse(exception.getMessage());
     }
 
     @ExceptionHandler(value = EntityNotExistException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleEntityNotExist(EntityNotExistException exception) {
-        return exception.getMessage();
+    public ErrorResponse handleEntityNotExist(EntityNotExistException exception) {
+        return generateErrorResponse(exception.getMessage());
     }
 
     @ExceptionHandler(value = AssociatedReservationExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public String handleAssociateReservationExists(AssociatedReservationExistsException exception) {
-        return exception.getMessage();
+    public ErrorResponse handleAssociateReservationExists(AssociatedReservationExistsException exception) {
+        return generateErrorResponse(exception.getMessage());
     }
 
     @ExceptionHandler(value = DuplicateEntityException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public String handleDuplicateEntity(DuplicateEntityException exception) {
-        return exception.getMessage();
+    public ErrorResponse handleDuplicateEntity(DuplicateEntityException exception) {
+        return generateErrorResponse(exception.getMessage());
+    }
+
+    private ErrorResponse generateErrorResponse(String message) {
+        return new ErrorResponse(message);
     }
 }
