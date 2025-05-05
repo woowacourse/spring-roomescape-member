4c155e33913f1cdd7f2f10181549e075b6340db7
mimi <110226866+hongkikii@users.noreply.github.com>
2025-05-05 15:05:18 +0900

    style: 개행 추가

--- src/main/java/roomescape/time/dto/TimeResponse.java
+++ src/main/java/roomescape/time/dto/TimeResponse.java
@@ -5,6 +5,7 @@ import java.time.LocalTime;
 import roomescape.time.domain.Time;
 
 public record TimeResponse(Long id, @JsonFormat(pattern = "HH:mm") LocalTime startAt) {
+
     public static TimeResponse from(Time time) {
         return new TimeResponse(time.id(), time.startAt());
     }
