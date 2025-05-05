1037c9cf3220af8e0b74be93dc1709b06a7b43e5
kimdev <kim__dev@hufs.ac.kr>
2025-05-05 20:22:32 +0900

    feat: RuntimeException 예외 처리

--- src/main/java/roomescape/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/handler/GlobalExceptionHandler.java
@@ -10,6 +10,12 @@ import roomescape.exception.InvalidInputException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(value = RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleUnexpected(RuntimeException exception) {
+        return "예상하지 못한 예외가 발생했다.\n" + exception.getMessage();
+    }
+
     @ExceptionHandler(value = InvalidInputException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleInvalidInput(InvalidInputException ex) {
