fa78fe1597d44a5b3fd0fa055807bdcf8f92d653
minSsan <msp1125@hanyang.ac.kr>
2025-05-05 15:47:47 +0900

    feat: GET /reservation/times 파라미터 검증 어노테이션 추가

--- src/main/java/roomescape/time/controller/user/UserReservationTimeController.java
+++ src/main/java/roomescape/time/controller/user/UserReservationTimeController.java
@@ -1,11 +1,12 @@
 package roomescape.time.controller.user;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.time.service.dto.response.ReservationTimeWithBookedResponse;
 import roomescape.time.service.ReservationTimeService;
+import roomescape.time.service.dto.response.ReservationTimeWithBookedResponse;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -21,8 +22,8 @@ public class UserReservationTimeController {
 
     @GetMapping("/times")
     public List<ReservationTimeWithBookedResponse> getAvailableTimes(
-            @RequestParam("date") LocalDate date,
-            @RequestParam("themeId")Long themeId
+            @RequestParam("date") @NotNull LocalDate date,
+            @RequestParam("themeId") @NotNull Long themeId
     ) {
         return timeService.getAllTimesWithBooked(date, themeId);
     }
