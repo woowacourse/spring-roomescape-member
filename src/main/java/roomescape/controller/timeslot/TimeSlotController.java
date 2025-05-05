5553d76450a2050fe42db2830e39cc21a017657b
yeonnhuu <yeonnhuu@gmail.com>
2025-05-05 20:15:56 +0900

    :bug: fix: 예약 가능한 타임 슬롯 목록 조회 에러 수정

--- src/main/java/roomescape/controller/timeslot/TimeSlotController.java
+++ src/main/java/roomescape/controller/timeslot/TimeSlotController.java
@@ -2,8 +2,10 @@ package roomescape.controller.timeslot;
 
 import jakarta.validation.Valid;
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import roomescape.controller.timeslot.dto.AddTimeSlotRequest;
 import roomescape.controller.timeslot.dto.AvailabilityTimeSlotRequest;
 import roomescape.controller.timeslot.dto.TimeSlotResponse;
@@ -50,8 +53,8 @@ public class TimeSlotController {
         return ResponseEntity.notFound().build();
     }
 
-    @GetMapping(value = "/availability")
-    public ResponseEntity<List<AvailabilityTimeSlotResponse>> findAvailabilityTimeSlots(@RequestBody @Valid final AvailabilityTimeSlotRequest request) {
+    @GetMapping("/availability")
+    public ResponseEntity<List<AvailabilityTimeSlotResponse>> findAvailabilityTimeSlots(@Valid final AvailabilityTimeSlotRequest request) {
         var availableTimeSlots = service.findAvailableTimeSlots(request.date(), request.themeId());
         return ResponseEntity.ok(availableTimeSlots);
     }
--- src/test/java/roomescape/Fixtures.java
+++ src/test/java/roomescape/Fixtures.java
@@ -19,12 +19,12 @@ public class Fixtures {
     public static final Theme JUNK_THEME_2 = new Theme(2L, "레벨2", "설명2", "썸네일2");
     public static final Theme JUNK_THEME_3 = new Theme(3L, "레벨3", "설명3", "썸네일3");
 
-    public static final Reservation JUNK_RESERVATION_1 = new Reservation(1L, "예약1", LocalDate.of(2025, 4, 24), JUNK_TIME_SLOT_1, JUNK_THEME_2);
-    public static final Reservation JUNK_RESERVATION_2 = new Reservation(2L, "예약2", LocalDate.of(2025, 4, 24), JUNK_TIME_SLOT_2, JUNK_THEME_2);
-    public static final Reservation JUNK_RESERVATION_3 = new Reservation(3L, "예약3", LocalDate.of(2025, 4, 24), JUNK_TIME_SLOT_3, JUNK_THEME_2);
-    public static final Reservation JUNK_RESERVATION_4 = new Reservation(4L, "예약4", LocalDate.of(2025, 4, 24), JUNK_TIME_SLOT_1, JUNK_THEME_1);
-    public static final Reservation JUNK_RESERVATION_5 = new Reservation(5L, "예약5", LocalDate.of(2025, 4, 25), JUNK_TIME_SLOT_2, JUNK_THEME_3);
-    public static final Reservation JUNK_RESERVATION_6 = new Reservation(6L, "예약6", LocalDate.of(2025, 4, 25), JUNK_TIME_SLOT_3, JUNK_THEME_3);
+    public static final Reservation JUNK_RESERVATION_1 = new Reservation(1L, "예약1", LocalDate.of(2025, 5, 4), JUNK_TIME_SLOT_1, JUNK_THEME_2);
+    public static final Reservation JUNK_RESERVATION_2 = new Reservation(2L, "예약2", LocalDate.of(2025, 5, 4), JUNK_TIME_SLOT_2, JUNK_THEME_2);
+    public static final Reservation JUNK_RESERVATION_3 = new Reservation(3L, "예약3", LocalDate.of(2025, 5, 4), JUNK_TIME_SLOT_3, JUNK_THEME_2);
+    public static final Reservation JUNK_RESERVATION_4 = new Reservation(4L, "예약4", LocalDate.of(2025, 5, 4), JUNK_TIME_SLOT_1, JUNK_THEME_1);
+    public static final Reservation JUNK_RESERVATION_5 = new Reservation(5L, "예약5", LocalDate.of(2025, 5, 5), JUNK_TIME_SLOT_2, JUNK_THEME_3);
+    public static final Reservation JUNK_RESERVATION_6 = new Reservation(6L, "예약6", LocalDate.of(2025, 5, 5), JUNK_TIME_SLOT_3, JUNK_THEME_3);
 
     // TimeSlot to insert
     public static final AddTimeSlotRequest JUNK_TIME_SLOT_REQUEST = new AddTimeSlotRequest(LocalTime.of(16, 0));
@@ -35,7 +35,7 @@ public class Fixtures {
     public static final Theme JUNK_THEME = new Theme(4L, "레벨4", "설명4", "썸네일4");
 
     // Reservation to insert
-    public static final Reservation JUNK_RESERVATION = new Reservation(7L, "예약", LocalDate.of(2025, 4, 26), JUNK_TIME_SLOT_1, JUNK_THEME_1);
+    public static final Reservation JUNK_RESERVATION = new Reservation(7L, "예약", LocalDate.of(2025, 5, 6), JUNK_TIME_SLOT_1, JUNK_THEME_1);
 
 
     public static LocalDate getDateOfTomorrow() {
