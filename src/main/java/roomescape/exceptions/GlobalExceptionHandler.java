5c8ac0b1a95cf628f99367a74e37ec92890b8bcb
황재웅 <wodnd0131@naver.com>
2025-05-05 13:49:44 +0900

    refactor: 불필요한 ProblemDetail에 대한 ResponseEntity 매핑 제거

--- src/main/java/roomescape/exceptions/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exceptions/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,41 +11,40 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ProblemDetail> handleEntityNotFound(EntityNotFoundException e) {
+    public ProblemDetail handleEntityNotFound(EntityNotFoundException e) {
         return createErrorResponse(e, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<ProblemDetail> handleDataIntegrityViolation(DataIntegrityViolationException e) {
+    public ProblemDetail handleDataIntegrityViolation(DataIntegrityViolationException e) {
         return createErrorResponse(e, HttpStatus.BAD_REQUEST, "데이터 무결성 위반이 발생했습니다.");
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ProblemDetail> handleIllegalArgument(IllegalArgumentException e) {
+    public ProblemDetail handleIllegalArgument(IllegalArgumentException e) {
         return createErrorResponse(e, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EntityDuplicateException.class)
-    public ResponseEntity<ProblemDetail> handleEntityDuplicate(EntityDuplicateException e) {
+    public ProblemDetail handleEntityDuplicate(EntityDuplicateException e) {
         return createErrorResponse(e, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ProblemDetail> handleException(Exception e) {
+    public ProblemDetail handleException(Exception e) {
         return createErrorResponse(e, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
     }
 
-    private ResponseEntity<ProblemDetail> createErrorResponse(Exception e, HttpStatus status) {
+    private ProblemDetail createErrorResponse(Exception e, HttpStatus status) {
         return createErrorResponse(e, status, e.getMessage());
     }
 
-    private ResponseEntity<ProblemDetail> createErrorResponse(Exception e, HttpStatus status, String message) {
+    private ProblemDetail createErrorResponse(Exception e, HttpStatus status, String message) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, message);
         problemDetail.setTitle(e.getClass().getSimpleName());
         problemDetail.setProperty("timestamp", LocalDateTime.now());
 
         e.printStackTrace();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(problemDetail);
+        return problemDetail;
     }
 }
