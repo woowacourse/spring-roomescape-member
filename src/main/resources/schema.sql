50e7e4d43f8ac881fd1dd3722f9bf25f5ee68bc3
mimi <110226866+hongkikii@users.noreply.github.com>
2025-05-05 13:48:43 +0900

    refactor: DB 시간 타입 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE reservation_time
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL,
+    start_at TIME NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -21,9 +21,12 @@ CREATE TABLE reservation
     name VARCHAR(255) NOT NULL,
     date DATE NOT NULL,
     time_id BIGINT,
-    theme_id BIGINT,                                        -- 컬럼 추가
+    theme_id BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)            -- 외래키 추가
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
+
+CREATE INDEX idx_reservation_theme_time_date
+ON reservation (theme_id, time_id, date);
 
