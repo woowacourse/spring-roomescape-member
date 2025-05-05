d509647537e5c0193017289202923e1845cce689
LJM\eueo8 <eueo8259@gmail.com>
2025-05-05 18:22:14 +0900

    refactor: CustomException을 이용하여 일괄처리

--- src/main/java/roomescape/common/RoomescapeExceptionHandler.java
+++ src/main/java/roomescape/common/RoomescapeExceptionHandler.java
@@ -1,15 +1,13 @@
 package roomescape.common;
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
-import java.util.zip.DataFormatException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.HttpStatus;
+import roomescape.common.exception.CustomException;
 import roomescape.common.exception.DuplicatedException;
 import roomescape.common.exception.InvalidInputException;
 import roomescape.common.exception.NotFoundException;
@@ -21,68 +19,49 @@ public class RoomescapeExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(RoomescapeExceptionHandler.class);
 
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<String> handleCustomException(CustomException ex) {
+        return new ResponseEntity<>(ex.getMessage(), ex.getStatus());
+    }
+
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<String> handleException(IllegalStateException ex) {
-        return new ResponseEntity<>("현재 시스템 상태에서는 요청을 처리할 수 없습니다", HttpStatus.BAD_REQUEST);
+        throw new CustomException(HttpStatus.BAD_REQUEST, "현재 시스템 상태에서는 요청을 처리할 수 없습니다");
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleException(IllegalArgumentException ex) {
-        return new ResponseEntity<>("유효하지 않은 값이 전달되었습니다", HttpStatus.BAD_REQUEST);
+        throw new CustomException(HttpStatus.BAD_REQUEST, "유효하지 않은 값이 전달되었습니다");
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
-        return new ResponseEntity<>("잘못된 요청입니다", HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> handleJsonParseException(HttpMessageNotReadableException ex) {
-        if (ex.getCause() instanceof InvalidFormatException formatEx) {
-            String targetType = formatEx.getTargetType().getSimpleName();
-            if (targetType.equals("LocalDate")) {
-                return new ResponseEntity<>("날짜 형식이 잘못되었습니다. yyyy-MM-dd 형식이어야 합니다.", HttpStatus.BAD_REQUEST);
-            }
-            if (targetType.equals("LocalTime")) {
-                return new ResponseEntity<>("시간 형식이 잘못되었습니다. HH:mm 형식이어야 합니다.", HttpStatus.BAD_REQUEST);
-            }
-        }
-        return new ResponseEntity<>("요청 형식이 잘못되었습니다.", HttpStatus.BAD_REQUEST);
+        throw new CustomException(HttpStatus.BAD_REQUEST, "잘못된 요청입니다");
     }
 
     @ExceptionHandler(ReservationDateException.class)
     public ResponseEntity<String> handleException(ReservationDateException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        throw new CustomException(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
     @ExceptionHandler(InvalidInputException.class)
     public ResponseEntity<String> handleException(InvalidInputException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(DataFormatException.class)
-    public ResponseEntity<String> handleException(DataFormatException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleException(NotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        throw new CustomException(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
     @ExceptionHandler(DuplicatedException.class)
     public ResponseEntity<String> handleException(DuplicatedException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+        throw new CustomException(HttpStatus.CONFLICT, ex.getMessage());
     }
 
     @ExceptionHandler(ResourceInUseException.class)
     public ResponseEntity<String> handleException(ResourceInUseException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+        throw new CustomException(HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
         logger.error(ex.getMessage());
-        return new ResponseEntity<>("서버 내부 오류가 발생했습니다. 관리자에게 문의하세요.", HttpStatus.INTERNAL_SERVER_ERROR);
+        throw new CustomException(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다. 관리자에게 문의하세요.");
     }
 }
--- src/main/java/roomescape/common/exception/CustomException.java
+++ src/main/java/roomescape/common/exception/CustomException.java
@@ -1,7 +1,23 @@
 package roomescape.common.exception;
 
-public abstract class CustomException extends RuntimeException {
-    public CustomException(String message) {
+import org.springframework.http.HttpStatus;
+
+public class CustomException extends RuntimeException {
+
+    private final HttpStatus status;
+    private final String message;
+
+    public CustomException(HttpStatus status, String message) {
         super(message);
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
