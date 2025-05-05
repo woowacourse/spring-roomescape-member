54b72939ebf16d8516561c0430c62a2bc0052ea4
ppparkta <pppppark2@gmail.com>
2025-05-05 16:45:23 +0900

    refactor: 일반적인 예외 처리
    - 더 일반적인 예외를 전역으로 처리합니다.

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package roomescape.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,6 +23,12 @@ public final class GlobalExceptionHandler {
     @ExceptionHandler(value = ReservationTimeException.class)
     public ResponseEntity<String> handleReservationTimeException(ReservationTimeException exception) {
         return ResponseEntity.badRequest()
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(exception.getMessage());
     }
 }
