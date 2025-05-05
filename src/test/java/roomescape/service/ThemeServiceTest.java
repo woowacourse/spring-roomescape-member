77c59366709532094a40ea6f235cc6659eaa5bac
yeonnhuu <yeonnhuu@gmail.com>
2025-05-05 21:50:04 +0900

    :sparkles: feat: 테마 삭제 예외 테스트 추가

--- src/test/java/roomescape/service/ThemeServiceTest.java
+++ src/test/java/roomescape/service/ThemeServiceTest.java
@@ -1,13 +1,16 @@
 package roomescape.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static roomescape.Fixtures.JUNK_THEME_REQUEST;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import roomescape.Fixtures;
 import roomescape.controller.theme.dto.ThemeResponse;
+import roomescape.exception.RoomescapeException;
 import roomescape.repository.fake.ReservationFakeRepository;
 import roomescape.repository.fake.ThemeFakeRepository;
 
@@ -46,5 +49,22 @@ class ThemeServiceTest {
             () -> assertThat(isRemoved).isTrue(),
             () -> assertThat(themes).doesNotContain(response)
         );
+    }
+
+    @Test
+    @DisplayName("테마를 삭제할 때 해당 테마에 대한 예약이 존재하면 예외 발생")
+    void removeThemeWithReservationException() {
+        // given
+        var reservationRepository = new ReservationFakeRepository();
+        var themeService = new ThemeService(reservationRepository, new ThemeFakeRepository());
+
+        var themeResponse = themeService.add(JUNK_THEME_REQUEST);
+
+        var reservation = Fixtures.getReservationOfTomorrow();
+        reservationRepository.save(reservation);
+
+        // when & then
+        assertThatThrownBy(() -> themeService.removeById(themeResponse.id()))
+                .isInstanceOf(RoomescapeException.class);
     }
 }
