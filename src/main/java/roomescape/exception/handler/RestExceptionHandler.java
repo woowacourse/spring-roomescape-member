ca815af0ba97251ce499584efc66927af3fd4d01
minSsan <msp1125@hanyang.ac.kr>
2025-05-05 15:47:02 +0900

    feat(RestExceptionHandler): RequestBody 검증 오류 핸들링

--- src/main/java/roomescape/exception/handler/RestExceptionHandler.java
+++ src/main/java/roomescape/exception/handler/RestExceptionHandler.java
@@ -2,12 +2,16 @@ package roomescape.exception.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.exception.BadRequestException;
 import roomescape.exception.ConflictException;
 import roomescape.exception.NotFoundException;
+
+import java.util.List;
 
 @RestControllerAdvice(annotations = RestController.class)
 public class RestExceptionHandler {
@@ -24,5 +28,15 @@ public class RestExceptionHandler {
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<String> conflict(ConflictException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> invalidRequest(MethodArgumentNotValidException e) {
+        List<String> messages = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messages);
     }
 }
