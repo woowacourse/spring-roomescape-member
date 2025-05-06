8a4830dcd62c73a441f7c4688dd174b70d568aaf
7i.w__k <tizmfns1218@Naver.com>
2025-05-06 19:09:40 +0900

    feat: 토큰 생성 컴포넌트 추가

---
+++ src/main/java/roomescape/domain/auth/service/JWTProvider.java
@@ -0,0 +1,56 @@
+package roomescape.domain.auth.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTProvider {
+
+    @Value("${auth.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${auth.jwt.expire-length}")
+    private int validityInMilliseconds;
+
+    public String createToken(final String payload) {
+        final Claims claims = Jwts.claims()
+                .setSubject(payload);
+        final Date now = new Date();
+        final Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String getPayload(final String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(final String token) {
+        try {
+            final Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token);
+
+            return !claims.getBody()
+                    .getExpiration()
+                    .before(new Date());
+        } catch (final JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
