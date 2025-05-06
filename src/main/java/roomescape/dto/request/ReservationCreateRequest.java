25283e640d2e95e7d6cd5651755bb01d6dd29404
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-05-06 16:45:18 +0900

    refactor: 사용하지 않는 검증 코드 제거

--- src/main/java/roomescape/dto/request/ReservationCreateRequest.java
+++ src/main/java/roomescape/dto/request/ReservationCreateRequest.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import roomescape.common.exception.InvalidRequestException;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 import roomescape.domain.Theme;
@@ -20,26 +19,7 @@ public record ReservationCreateRequest(
         @NotNull(message = "예약 테마가 올바르지 않습니다")
         Long themeId
 ) {
-    public ReservationCreateRequest {
-//        validate(name, date, timeId, themeId);
-    }
-
     public Reservation toReservation(ReservationTime time, Theme theme) {
         return new Reservation(null, name, date, time, theme);
-    }
-
-    private void validate(String name, LocalDate date, Long timeId, Long themeId) {
-        if (name == null || name.isBlank()) {
-            throw new InvalidRequestException("잘못된 요청입니다.");
-        }
-        if (date == null) {
-            throw new InvalidRequestException("잘못된 요청입니다.");
-        }
-        if (timeId == null) {
-            throw new InvalidRequestException("잘못된 요청입니다.");
-        }
-        if (themeId == null) {
-            throw new InvalidRequestException("잘못된 요청입니다.");
-        }
     }
 }
