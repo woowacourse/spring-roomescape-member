ca13961b54142d04823e132fb437b0bf787cd526
ppparkta <pppppark2@gmail.com>
2025-05-06 21:40:20 +0900

    refactor: 최상위 예외 로깅 추가
    - 최상위 예외 핸들러에 로깅을 추가합니다.

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package roomescape.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,27 +10,34 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public final class GlobalExceptionHandler {
 
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(value = ReservationException.class)
     public ResponseEntity<String> handleReservationException(ReservationException exception) {
-        return ResponseEntity.badRequest()
+        return ResponseEntity
+                .badRequest()
                 .body(exception.getMessage());
     }
 
     @ExceptionHandler(value = ReservationThemeException.class)
     public ResponseEntity<String> handleReservationThemeException(ReservationThemeException exception) {
-        return ResponseEntity.badRequest()
+        return ResponseEntity
+                .badRequest()
                 .body(exception.getMessage());
     }
 
     @ExceptionHandler(value = ReservationTimeException.class)
     public ResponseEntity<String> handleReservationTimeException(ReservationTimeException exception) {
-        return ResponseEntity.badRequest()
+        return ResponseEntity
+                .badRequest()
                 .body(exception.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception exception) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(exception.getMessage());
+        LOG.error("Unexpected error occurred", exception);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("서버 문제가 발생했습니다.");
     }
 }
