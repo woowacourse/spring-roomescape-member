4d5afe846499d6ebd66ba51ab9626f955f460054
RaZel713 <razel7130@gmail.com>
2025-05-06 19:15:00 +0900

    feat: 테마 명, 테마 설명, 썸네일 주소 길이 검증 메서드 추가

--- src/main/java/roomescape/domain/RoomTheme.java
+++ src/main/java/roomescape/domain/RoomTheme.java
@@ -6,6 +6,7 @@ import roomescape.exception.custom.InvalidInputException;
 public class RoomTheme {
 
     private static final int NON_SAVED_STATUS = 0;
+    private static final int MAX_LENGTH = 255;
 
     private final long id;
     private final String name;
@@ -26,6 +27,11 @@ public class RoomTheme {
     }
 
     private void validateInvalidInput(final String name, final String description, final String thumbnail) {
+        validateNotNull(name, description, thumbnail);
+        validateValidLength(name, description, thumbnail);
+    }
+
+    private void validateNotNull(final String name, final String description, final String thumbnail) {
         if (Objects.isNull(name) || name.isBlank()) {
             throw new InvalidInputException("테마 명은 빈 값이 입력될 수 없습니다");
         }
@@ -34,6 +40,18 @@ public class RoomTheme {
         }
         if (Objects.isNull(thumbnail) || thumbnail.isBlank()) {
             throw new InvalidInputException("썸네일 주소는 빈 값이 입력될 수 없습니다");
+        }
+    }
+
+    private void validateValidLength(final String name, final String description, final String thumbnail) {
+        if (name.length() > MAX_LENGTH) {
+            throw new InvalidInputException("테마 명은 255자를 초과할 수 없습니다.");
+        }
+        if (description.length() > MAX_LENGTH) {
+            throw new InvalidInputException("테마 상세 설명은 255자를 초과할 수 없습니다.");
+        }
+        if (thumbnail.length() > MAX_LENGTH) {
+            throw new InvalidInputException("썸네일 주소는 255자를 초과할 수 없습니다.");
         }
     }
 
