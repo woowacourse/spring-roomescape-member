1ecb83eeb801dedc79f7104af10d0d14f99f0a1c
taek2222 <oot309@daum.net>
2025-05-06 13:28:44 +0900

    test: 필드 타입 변경으로 테스트 인자 변경

--- src/test/java/roomescape/application/dto/ReservationTimeResponseTest.java
+++ src/test/java/roomescape/application/dto/ReservationTimeResponseTest.java
@@ -5,16 +5,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import roomescape.domain.ReservationTime;
 
 class ReservationTimeResponseTest {
 
     @Test
     @DisplayName("startAt 필드의 반환 형태는 HH:mm이다.")
     void formatStartAtResponse() {
+        // given
         LocalTime startAt = LocalTime.of(10, 0, 1);
+        ReservationTime reservationTime = new ReservationTime(1L, startAt);
 
-        ReservationTimeResponse reservationTimeResponse = new ReservationTimeResponse(1L, startAt);
+        ReservationTimeResponse reservationTimeResponse = new ReservationTimeResponse(reservationTime);
 
-        assertThat(reservationTimeResponse.getStartAt()).isEqualTo("10:00");
+        // when
+        String result = reservationTimeResponse.startAt();
+
+        // then\
+        assertThat(result).isEqualTo("10:00");
     }
 }
