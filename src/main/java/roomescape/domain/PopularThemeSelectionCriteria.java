25c6418c3f83bd4850ab4d6df073beeccdf276a1
RaZel713 <razel7130@gmail.com>
2025-05-06 17:04:56 +0900

    feat: 인기 테마 선정 기준 도메인의 생성자 파라미터 유효성 검증 메서드 추가

--- src/main/java/roomescape/domain/PopularThemeSelectionCriteria.java
+++ src/main/java/roomescape/domain/PopularThemeSelectionCriteria.java
@@ -1,6 +1,7 @@
 package roomescape.domain;
 
 import java.time.LocalDate;
+import roomescape.exception.custom.InvalidInputException;
 
 public class PopularThemeSelectionCriteria {
 
@@ -10,8 +11,18 @@ public class PopularThemeSelectionCriteria {
     private final LocalDate endDay;
 
     public PopularThemeSelectionCriteria(final LocalDate startBaseDate, final int durationInDays) {
+        validateInvalidInput(startBaseDate, durationInDays);
         this.startDay = calculateDate(startBaseDate, durationInDays + DAY_BEFORE_END);
         this.endDay = calculateDate(startBaseDate, DAY_BEFORE_END);
+    }
+
+    private void validateInvalidInput(final LocalDate startBaseDate, final int durationInDays) {
+        if (startBaseDate == null) {
+            throw new InvalidInputException("시작 기준일은 null이 될 수 없습니다");
+        }
+        if (durationInDays <= 0) {
+            throw new InvalidInputException("인기 테마 선정 기준 기한 일수는 음수나 0이 될 수 없습니다");
+        }
     }
 
     private LocalDate calculateDate(final LocalDate baseDate, final int daysBefore) {
