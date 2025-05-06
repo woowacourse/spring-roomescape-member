a4a50abe52c2c75ed23f663f364b7efa80a2d7ba
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-05-06 18:02:26 +0900

    test: 인기테마 조회 쿼리문 테스트 코드 작성

--- src/test/java/roomescape/dao/JdbcThemeDaoTest.java
+++ src/test/java/roomescape/dao/JdbcThemeDaoTest.java
@@ -2,6 +2,8 @@ package roomescape.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -24,8 +26,9 @@ public class JdbcThemeDaoTest {
 
     @AfterEach
     void clearResourceAfterTest() {
+        jdbcTemplate.update("DELETE FROM RESERVATION");
+        jdbcTemplate.update("DELETE FROM RESERVATION_TIME");
         jdbcTemplate.update("DELETE FROM theme");
-
     }
 
     @DisplayName("테마 저장 테스트")
@@ -77,9 +80,45 @@ public class JdbcThemeDaoTest {
         assertThat(findTheme).isEmpty();
     }
 
-    // TODO : 테스트 코드 작성하기 / 다른 DAO와 연관관계가 있어 패스
     @DisplayName("주어진 날짜를 기준으로 7일 동안 가장 많이 예약된 테마 순서대로 조회할 수 있어야 한다.")
+    @Test
     void find_popular_rank() {
-
+        initData();
+        LocalDate startDate = LocalDate.of(2025, 5, 1);
+        List<Theme> allThemeOfRanks = themeDao.findAllThemeOfRanks(startDate.minusDays(7),
+            startDate);
+        assertThat(allThemeOfRanks.get(0).getId()).isEqualTo(1);
+        assertThat(allThemeOfRanks.get(1).getId()).isEqualTo(2);
+        assertThat(allThemeOfRanks.get(2).getId()).isEqualTo(3);
     }
+
+    private void initData() {
+        String sql1 = "INSERT INTO theme (id, name, description, thumbnail) VALUES (1, '안녕, 자두야', '자두', 'https://jado.com');";
+        String sql2 = "INSERT INTO theme (id, name, description, thumbnail) VALUES (2, '안녕, 도기야', '도가', 'https://dogi.com');";
+        String sql3 = "INSERT INTO theme (id, name, description, thumbnail)VALUES (3, '안녕, 젠슨아', '젠슨', 'https://jenson.com');";
+        String sql4 = "INSERT INTO reservation_time (id, start_at) VALUES (1, '10:00');";
+        String sql5 = "INSERT INTO reservation_time (id, start_at) VALUES (2, '11:00');";
+        String sql6 = "INSERT INTO reservation_time (id, start_at) VALUES (3, '12:00');";
+        String sql7 = "INSERT INTO reservation_time (id, start_at) VALUES (4, '13:00');";
+        String sql8 = "INSERT INTO reservation (id, name, date, time_id, theme_id) VALUES (1, '김덕배', '2025-04-24', 1, 1);";
+        String sql9 = "INSERT INTO reservation (id, name, date, time_id, theme_id)VALUES (2, '김덕배2', '2025-04-25', 2, 1);";
+        String sql10 = "INSERT INTO reservation (id, name, date, time_id, theme_id)VALUES (3, '김덕배3', '2025-04-26', 3, 1);";
+        String sql11 = "INSERT INTO reservation (id, name, date, time_id, theme_id)VALUES (4, '김다배', '2025-04-27', 1, 2);";
+        String sql12 = "INSERT INTO reservation (id, name, date, time_id, theme_id)VALUES (5, '김덕배', '2025-04-28', 1, 2);";
+        String sql13 = "INSERT INTO reservation (id, name, date, time_id, theme_id)VALUES (6, '김덕배', '2025-05-01', 2, 3);";
+        jdbcTemplate.update(sql1);
+        jdbcTemplate.update(sql2);
+        jdbcTemplate.update(sql3);
+        jdbcTemplate.update(sql4);
+        jdbcTemplate.update(sql5);
+        jdbcTemplate.update(sql6);
+        jdbcTemplate.update(sql7);
+        jdbcTemplate.update(sql8);
+        jdbcTemplate.update(sql9);
+        jdbcTemplate.update(sql10);
+        jdbcTemplate.update(sql11);
+        jdbcTemplate.update(sql12);
+        jdbcTemplate.update(sql13);
+    }
+
 }
