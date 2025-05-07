a3603e5f8ee18e76aee9efaceb9ffe5170e0edbd
jhpark1227 <junhyeok0246@naver.com>
2025-05-07 16:40:27 +0900

    feat: 예외 처리 응답 형식 변경

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -9,17 +9,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = CustomException.class)
-    public ResponseEntity<String> handleCustomException(CustomException e) {
-        return ResponseEntity.status(e.getStatus()).body(e.getMessage());
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        ErrorResponse body = new ErrorResponse(e.getMessage());
+        return ResponseEntity.status(e.getStatus()).body(body);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return ResponseEntity.badRequest().body(e.getBindingResult().getAllErrors().getFirst().getDefaultMessage());
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorResponse body = new ErrorResponse(e.getBindingResult().getAllErrors().getFirst().getDefaultMessage());
+        return ResponseEntity.badRequest().body(body);
     }
 
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
-        return ResponseEntity.internalServerError().body("오류가 발생하였습니다. 관리자에게 문의해주세요");
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        e.printStackTrace();
+        ErrorResponse body = new ErrorResponse("오류가 발생하였습니다. 관리자에게 문의해주세요");
+        return ResponseEntity.internalServerError().body(body);
     }
 }
