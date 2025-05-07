0e8dccfb36684562e7741a235ec7a9e392b7c87b
junhaa <2171326@hansung.ac.kr>
2025-05-07 20:41:43 +0900

    refactor: RestControllerAdvice로 변경

--- src/main/java/roomescape/global/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/global/handler/GlobalExceptionHandler.java
@@ -2,38 +2,44 @@ package roomescape.global.handler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+    public String handleIllegalArgumentException(IllegalArgumentException exception) {
         LOGGER.error(exception.getMessage());
-        return ResponseEntity.badRequest().body(exception.getMessage());
+        return exception.getMessage();
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<String> handleIllegalStateException(IllegalStateException exception) {
+    public String handleIllegalStateException(IllegalStateException exception) {
         LOGGER.error(exception.getMessage());
-        return ResponseEntity.badRequest().body(exception.getMessage());
+        return exception.getMessage();
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+    public String handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
         LOGGER.error(exception.getMessage());
-        return ResponseEntity.badRequest().body("요청 형식이 올바르지 않습니다.");
+        return "요청 형식이 올바르지 않습니다.";
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception exception) {
+    public String handleException(Exception exception) {
         LOGGER.error(exception.getMessage());
-        return ResponseEntity.internalServerError().body("서버 내부에서 오류가 발생했습니다.");
+        return "서버 내부에서 오류가 발생했습니다.";
     }
 
 }
