c68301b79d1cb8baa1b4eeb713b3f0a5255c3e41
7i.w__k <tizmfns1218@Naver.com>
2025-05-07 14:12:06 +0900

    feat: User 엔티티 구현

--- src/main/java/roomescape/domain/auth/entity/User.java
+++ src/main/java/roomescape/domain/auth/entity/User.java
@@ -1,21 +1,74 @@
 package roomescape.domain.auth.entity;
 
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
+import roomescape.common.exception.InvalidArgumentException;
 
 @Getter
 public class User {
 
+    private static final int PASSWORD_MAX_LENGTH = 25;
+    private static final String EMAIL_REGEX = "^[a-z+-_]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
+
     private final Long id;
-    private final Name name;
+    private final Name username;
     private final String email;
     private final String password;
 
     @Builder
-    public User(final Long id, final Name name, final String email, final String password) {
+    public User(final Long id, final Name username, final String email, final String password) {
         this.id = id;
-        this.name = name;
+        this.username = username;
         this.email = email;
         this.password = password;
+        validate();
+    }
+
+    private void validate() {
+        if (username == null || email == null || password == null) {
+            throw new InvalidArgumentException("User 필드는 null일 수 없습니다. (id 필드 제외)");
+        }
+
+        validateEmail();
+        validatePassword();
+    }
+
+    private void validateEmail() {
+        if (!email.matches(EMAIL_REGEX)) {
+            throw new InvalidArgumentException("잘못된 이메일입니다.");
+        }
+    }
+
+    private void validatePassword() {
+        if (password.isBlank() || password.length() > PASSWORD_MAX_LENGTH) {
+            throw new InvalidArgumentException("유효하지 않은 패스워드입니다.");
+        }
+    }
+
+    public static User withoutId(final Name name, final String email, final String password) {
+        return new User(null, name, email, password);
+    }
+
+    public boolean existId() {
+        return id != null;
+    }
+
+    public String getName() {
+        return username.getName();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final User user = (User) o;
+        return Objects.equals(id, user.id);
     }
 }
---
+++ src/test/java/roomescape/domain/auth/entity/UserTest.java
@@ -0,0 +1,62 @@
+package roomescape.domain.auth.entity;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import roomescape.common.exception.InvalidArgumentException;
+
+class UserTest {
+
+    @DisplayName("패스워드가 25자 이상이거나 공백이라면 예외를 반환한다.")
+    @NullAndEmptySource
+    @ParameterizedTest
+    @ValueSource(strings = {"aaaaabbbbbcccccdddddeeee26"})
+    void passwordTest1(final String password) {
+        // given
+        final Name name = new Name("꾹");
+        final String email = "231321@naver.com";
+
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            new User(1L, name, email, password);
+        }).isInstanceOf(InvalidArgumentException.class);
+    }
+
+    @DisplayName("이메일이 형식에 맞지 않다면 예외를 반환한다.")
+    @NullAndEmptySource
+    @ParameterizedTest
+    @ValueSource(strings = {"@naver.com", "tdasdsad@", "sdadsa@naver"})
+    void emailTest(final String email) {
+        // given
+        final Name name = new Name("꾹");
+        final String password = "123456";
+
+        // when
+        // then
+        assertThatThrownBy(() -> {
+            new User(1L, name, email, password);
+        }).isInstanceOf(InvalidArgumentException.class);
+    }
+
+    @DisplayName("유저 생성 성공 테스트")
+    @Test
+    void userTest1() {
+        // given
+        final String email = "tizm@naver.com";
+        final String password = "123456";
+        final Name name = new Name("꾹");
+
+        // when
+        // then
+        assertThatCode(() -> {
+            new User(1L, name, email, password);
+        }).doesNotThrowAnyException();
+    }
+
+}
