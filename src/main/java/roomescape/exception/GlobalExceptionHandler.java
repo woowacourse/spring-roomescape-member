f66439f86439beaa454a10b671fb86dd99bc9b29
CheChe903 <cheche903@naver.com>
2025-05-07 20:47:56 +0900

    chore: openai 버전 변경

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 package roomescape.exception;
 
-import io.swagger.v3.oas.annotations.Hidden;
 import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
