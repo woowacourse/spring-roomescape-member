0de3b52baf682a2f92bd212e53265ea08e43fec6
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-05-07 11:00:26 +0900

    feat(controller) : 일반 오류 처리 핸들러에 로깅 추가 및 커스텀 에러 메시지 응답

--- src/main/java/roomescape/controller/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package roomescape.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("[ERROR] " + e.getMessage());
@@ -14,6 +18,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("[ERROR] " + e.getMessage());
+        log.error("[ERROR] " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("[ERROR] " + "오류가 발생했습니다.");
     }
 }
