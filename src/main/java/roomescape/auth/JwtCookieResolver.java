fd560c45898cf65be9a36d7e951c8875b267afa9
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-05-07 18:49:43 +0900

    refactor: setCookie시 덮어씌워지기 때문에 불필요해진 resetCookie 삭제

--- src/main/java/roomescape/auth/JwtCookieResolver.java
+++ src/main/java/roomescape/auth/JwtCookieResolver.java
@@ -7,20 +7,9 @@ import roomescape.exception.auth.AuthenticationException;
 
 public class JwtCookieResolver {
 
-    private static final String EMPTY_TOKEN = "EMPTY";
-
-    //TODO 쿠키 일일이 삭제해야하는지 확인하기 수정
-//    public static void resetToken(HttpServletRequest request) {
-//        String jwtCookieKey = JwtTokenProvider.getCookieKey();
-//        Optional<Cookie> tokenCookie = Arrays.stream(request.getCookies())
-//                .filter((cookie) -> jwtCookieKey.equals(cookie.getName()))
-//                .findAny();
-//        tokenCookie.ifPresent(cookie -> cookie.setMaxAge(0));
-//    }
-
     public static String getTokenFromCookie(HttpServletRequest request) {
         String jwtCookieKey = JwtTokenProvider.getCookieKey();
-        
+
         return Arrays.stream(request.getCookies())
                 .filter((cookie) -> jwtCookieKey.equals(cookie.getName()))
                 .findAny()
