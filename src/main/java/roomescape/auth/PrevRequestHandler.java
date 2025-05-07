144948a7a305189d4a788c03077945dbc51c9708
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-05-07 18:50:09 +0900

    fix: 테스트를 위해 주석처리했던 코드 주석 해제

--- src/main/java/roomescape/auth/PrevRequestHandler.java
+++ src/main/java/roomescape/auth/PrevRequestHandler.java
@@ -4,11 +4,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import org.springframework.web.servlet.HandlerInterceptor;
+import roomescape.domain.member.Role;
 
 public class PrevRequestHandler implements HandlerInterceptor {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final List<RequestMatcher> adminRoleRequestMatcher;
+    //bad code
+    private static final String SUPER_ADMIN = "praisebak@naver.com";
 
     public PrevRequestHandler(JwtTokenProvider jwtTokenProvider, List<RequestMatcher> adminRoleRequestMatcher) {
         this.jwtTokenProvider = jwtTokenProvider;
@@ -20,11 +23,9 @@ public class PrevRequestHandler implements HandlerInterceptor {
             throws Exception {
         boolean isMatchExist = isMatchExist(request);
         if (isMatchExist) {
-//            String token = JwtCookieResolver.getTokenFromCookie(request);
-//            UserInfo userInfo = jwtTokenProvider.resolveToken(token);
-            return true;
-            //TODO
-//            return userInfo.role().equals(Role.ADMIN);
+            String token = JwtCookieResolver.getTokenFromCookie(request);
+            UserInfo userInfo = jwtTokenProvider.resolveToken(token);
+            return userInfo.role().equals(Role.ADMIN) || userInfo.username().equals(SUPER_ADMIN);
         }
         return true;
     }
