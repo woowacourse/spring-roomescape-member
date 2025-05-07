d186266af30cfb4a7533a6f212fe217f5571a92e
Chaeyoung714 <202102760@hufs.ac.kr>
2025-05-07 13:59:58 +0900

    chore: Member 테이블 및 초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,6 +14,14 @@ VALUES ('테마1', '테마1입니다.',
        ('테마3', '테마3입니다.',
         'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
+INSERT INTO member (name, email, password)
+VALUES ('김민준', 'minjun.kim@example.com', 'password123'),
+       ('이서연', 'seoyeon.lee@example.com', 'password456'),
+       ('박지훈', 'jihoon.park@example.com', 'password789'),
+       ('최수아', 'sua.choi@example.com', 'password101'),
+       ('정도윤', 'doyun.jung@example.com', 'password202'),
+       ('강하은', 'haeun.kang@example.com', 'password303');
+
 INSERT INTO reservation (name, date, time_id, theme_id)
 VALUES ('김민준', '2025-05-03', 1, 3),
        ('이서연', '2025-05-03', 3, 3),
