5a65a391a328d0fea4674fa91ab158b37ba898db
kjungw <sagakkb@gmail.com>
2025-05-07 21:25:26 +0900

    test: 로그인 관련 통합테스트 작성

--- src/test/java/roomescape/RoomescapeApplicationTest.java
+++ src/test/java/roomescape/RoomescapeApplicationTest.java
@@ -1,29 +1,39 @@
 package roomescape;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static roomescape.test.fixture.DateFixture.NEXT_DAY;
 import static roomescape.test.fixture.DateFixture.YESTERDAY;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.utility.JwtTokenProvider;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class RoomescapeApplicationTest {
 
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+    @Value("${security.jwt.token.secret-key}")
+    String tokenSecretKey;
 
     @BeforeEach
     void setUp() {
@@ -381,5 +391,117 @@ class RoomescapeApplicationTest {
                 .when().delete("/themes/1")
                 .then().log().all()
                 .statusCode(400);
+    }
+
+    @DisplayName("로그인할 수 있다")
+    @Test
+    void canLogin() {
+        jdbcTemplate.update("INSERT INTO member (name, email, password) VALUES (?,?,?)",
+                "회원", "test@test.com", "ecxewqe!23");
+
+        Map<String, String> params = new HashMap<>();
+        params.put("email", "test@test.com");
+        params.put("password", "ecxewqe!23");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/login")
+                .then().log().all()
+                .statusCode(200)
+                .cookie("access", notNullValue());
+    }
+
+    @DisplayName("이메일에 대한 회원이 존재하지 않을 경우 로그인할 수 없다")
+    @Test
+    void cannotLoginBecauseOfInvalidEmail() {
+        Map<String, String> params = new HashMap<>();
+        params.put("email", "test@test.com");
+        params.put("password", "ecxewqe!23");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/login")
+                .then().log().all()
+                .statusCode(401);
+    }
+
+    @DisplayName("비밀번호가 맞지 않을 경우 로그인할 수 없다")
+    @Test
+    void cannotLoginBecauseOfWrongPassword() {
+        jdbcTemplate.update("INSERT INTO member (name, email, password) VALUES (?,?,?)",
+                "회원", "test@test.com", "ecxewqe!23");
+
+        Map<String, String> params = new HashMap<>();
+        params.put("email", "test@test.com");
+        params.put("password", "wrong_password_123!");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/login")
+                .then().log().all()
+                .statusCode(401);
+    }
+
+    @DisplayName("로그인 여부를 확인할 수 있다")
+    @Test
+    void canCheckLogin() {
+        String accessToken = jwtTokenProvider.makeAccessToken(1L, "회원");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie("access", accessToken)
+                .when().get("/login/check")
+                .then().log().all()
+                .statusCode(200)
+                .body("name", is("회원"));
+    }
+
+    @DisplayName("토큰이 올바르지 않은 경우 예외가 발생한다")
+    @Test
+    void cannotCheckLoginBecauseOfInvalidToken() {
+        String accessToken = jwtTokenProvider.makeAccessToken(1L, "회원");
+        String invalidToken = accessToken + "invalid";
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie("access", invalidToken)
+                .when().get("/login/check")
+                .then().log().all()
+                .statusCode(401);
+    }
+
+    @DisplayName("토큰이 만료된 경우 예외가 발생한다")
+    @Test
+    void cannotCheckLoginBecauseOfTokenExpiration() {
+        Date validity = new Date(new Date().getTime() - 1000);
+        String expirationToken = Jwts.builder()
+                .setSubject(String.valueOf(1L))
+                .claim("name", "회원")
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, tokenSecretKey)
+                .compact();
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie("access", expirationToken)
+                .when().get("/login/check")
+                .then().log().all()
+                .statusCode(401);
+    }
+
+    @DisplayName("로그아웃을 할 수 있다")
+    @Test
+    void canLogout() {
+        String accessToken = jwtTokenProvider.makeAccessToken(1L, "회원");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie("access", accessToken)
+                .when().post("/logout")
+                .then().log().all()
+                .statusCode(200);
     }
 }
