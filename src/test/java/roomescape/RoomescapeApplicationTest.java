885d0a526ff3c2636c408cb7741364a3e5145f6e
dltmddus <tmddus4671@gmail.com>
2025-05-07 16:19:28 +0900

    style: 주석 삭제

--- src/test/java/roomescape/RoomescapeApplicationTest.java
+++ src/test/java/roomescape/RoomescapeApplicationTest.java
@@ -1,10 +1,10 @@
 package roomescape;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// TODO: 이거 왜 됨?
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
 class RoomescapeApplicationTest {
 
     @Test
