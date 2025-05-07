7bab1b7b48adc3e21998a00fb4ba45e44d559b23
Mint <audwl03071@gmail.com>
2025-05-07 19:53:50 +0900

    test: 테스트 추가

---
+++ src/test/java/roomescape/theme/repository/FakeThemeRepositoryTest.java
@@ -0,0 +1,61 @@
+package roomescape.theme.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import roomescape.reservation.fixture.TestFixture;
+import roomescape.theme.domain.Theme;
+
+class FakeThemeRepositoryTest {
+
+    private ThemeRepository themeRepository;
+
+    @BeforeEach
+    void setUp() {
+        themeRepository = new FakeThemeRepository();
+
+        themeRepository.save(TestFixture.makeTheme());
+    }
+
+    @Test
+    void findAll() {
+        List<Theme> themes = themeRepository.findAll();
+
+        assertThat(themes.size()).isEqualTo(1);
+    }
+
+    @Test
+    void save() {
+        themeRepository.save(Theme.of(2L, "theme", "description", "thumbnail"));
+
+        List<Theme> themes = themeRepository.findAll();
+        assertThat(themes.size()).isEqualTo(2);
+    }
+
+    @Test
+    void deleteById() {
+        themeRepository.deleteById(1L);
+
+        List<Theme> themes = themeRepository.findAll();
+        assertThat(themes.size()).isEqualTo(0);
+    }
+
+    @Test
+    void findById() {
+        Optional<Theme> optionalTheme = themeRepository.findById(1L);
+
+        assertThat(optionalTheme.get().getId()).isEqualTo(1L);
+    }
+
+    @Test
+    void findTop10PopularThemesWithinLastWeek() {
+        List<Theme> themes = themeRepository.findTop10PopularThemesWithinLastWeek(
+                LocalDate.now());
+
+        assertThat(themes.size()).isEqualTo(10);
+    }
+}
