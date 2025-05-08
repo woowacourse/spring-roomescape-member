824a987b43ab35c826c211bc6d4e08bc2c33f447
GiSeok Kang <ime.rokz@gmail.com>
2025-05-08 21:41:39 +0900

    feat: 로그인 페이지 기능 구현
    - /login GET 요청 시 로그인 페이지 반환

---
+++ src/main/java/roomescape/login/presentation/LoginPageController.java
@@ -0,0 +1,13 @@
+package roomescape.login.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginPageController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+}
