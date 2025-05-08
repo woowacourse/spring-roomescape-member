bb9d3a3a353a3a8a08f386765975201646176713
egaeng09 <egaeng09@gmail.com>
2025-05-08 16:51:57 +0900

    feat: 로그인 및 회원가입 페이지 정의

--- src/main/java/roomescape/user/admin/controller/AdminController.java
+++ src/main/java/roomescape/user/admin/controller/AdminController.java
@@ -1,4 +1,4 @@
-package roomescape.user.admin.ui;
+package roomescape.user.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
