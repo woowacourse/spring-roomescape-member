753a04f2029608b90c11b89a540b78d4b37e87c8
LJM\eueo8 <eueo8259@gmail.com>
2025-05-08 13:44:38 +0900

    fix: 테이블 생성 오류 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -34,5 +34,5 @@ CREATE TABLE customer
     email    VARCHAR(255) NOT NULL ,
     name     VARCHAR(255) NOT NULL,
     password     VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
