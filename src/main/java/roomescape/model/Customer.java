61ee64d537456e3b652741c7f05468bf6991aed4
LJM\eueo8 <eueo8259@gmail.com>
2025-05-08 13:13:31 +0900

    feat: customer 테이블 id 추가

--- src/main/java/roomescape/model/Customer.java
+++ src/main/java/roomescape/model/Customer.java
@@ -1,17 +1,20 @@
 package roomescape.model;
 
 public class Customer {
-    private String name;
-    private String email;
-    private String password;
+    private final Long id;
+    private final String name;
+    private final String email;
+    private final String password;
 
-    public Customer(String name, String email, String password) {
+    public Customer(Long id, String name, String email, String password) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
     }
 
     public Customer(String email, String password){
+        this.id = null;
         this.email = email;
         this.name = null;
         this.password = password;
@@ -27,5 +30,9 @@ public class Customer {
 
     public String getPassword() {
         return password;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -30,8 +30,9 @@ CREATE TABLE reservation
 
 CREATE TABLE customer
 (
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
     email    VARCHAR(255) NOT NULL ,
     name     VARCHAR(255) NOT NULL,
     password     VARCHAR(255) NOT NULL,
-    PRIMARY KEY (email),
+    PRIMARY KEY (id),
 );
