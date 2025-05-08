d9e2e682a2c058a4795af4aa6103ea74b8a2126d
Jin409 <sualng123@gmail.com>
2025-05-08 13:35:48 +0900

    feat: 토큰 생성 기능 구현

---
+++ src/main/java/roomescape/service/JwtProvider.java
@@ -0,0 +1,33 @@
+package roomescape.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtProvider {
+
+    private static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.token.expire-length}")
+    private long validityInMilliseconds;
+
+    public String createToken(String payload) {
+        Claims claims = Jwts.claims().setSubject(payload);
+        Date now = new Date();
+        Date expiredDate = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiredDate)
+                .signWith(SIGNATURE_ALGORITHM, secretKey)
+                .compact();
+    }
+}
