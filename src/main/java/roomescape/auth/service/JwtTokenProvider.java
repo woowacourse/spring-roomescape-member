320977876383be5a834a75aac114594fd92756ce
minSsan <msp1125@hanyang.ac.kr>
2025-05-08 20:04:45 +0900

    refactor(JwtTokenProvider): JWT 토큰 만료기한 설정

--- src/main/java/roomescape/auth/service/JwtTokenProvider.java
+++ src/main/java/roomescape/auth/service/JwtTokenProvider.java
@@ -6,17 +6,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import roomescape.auth.entity.User;
 
+import java.util.Date;
+
 @Component
 public class JwtTokenProvider {
     @Value("${security.jwt.token.secret-key}")
     private String SECRET_KEY;
     @Value("${security.jwt.token.expire-length}")
-    private long EXPIRE_LENGTH;
+    private long EXPIRE_LENGTH_MILLI;
 
     public String createToken(User user) {
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + EXPIRE_LENGTH_MILLI);
         return Jwts.builder()
                 .subject(user.getId().toString())
                 .claim("email", user.getEmail())
+                .expiration(expiration)
                 .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
                 .compact();
     }
