ccb0cba8aeef784dd8e7f0faf3b55c4aa03265e9
threepebbles <threepebbles@naver.com>
2025-05-08 22:27:54 +0900

    chore(data.sql): 초기 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO members (name, email, password, role)
+VALUES ('어드민이에용', 'admin@admin.com', 'admin', '어드민');
+
+INSERT INTO reservation_times (start_at)
+VALUES ('10:00');
+INSERT INTO reservation_times (start_at)
+VALUES ('11:00');
+INSERT INTO reservation_times (start_at)
+VALUES ('13:00');
+INSERT INTO reservation_times (start_at)
+VALUES ('14:00');
+
+INSERT INTO themes (name, description, thumbnail)
+VALUES ('헤일러의 디버깅 교실', '재밌어요', 'debug.jpg');
+INSERT INTO themes (name, description, thumbnail)
+VALUES ('우가의 레이어드 아키텍처', '우가우가', 'layered.jpg');
+INSERT INTO themes (name, description, thumbnail)
+VALUES ('이름', '설명', 'thumbnail.jpg');
