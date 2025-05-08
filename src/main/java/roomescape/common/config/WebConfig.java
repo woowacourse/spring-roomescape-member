3b34e121e2c8225e07692001f6cfd75f4b4cdbb4
Jin409 <sualng123@gmail.com>
2025-05-08 21:32:13 +0900

    chore: ArgumentResolver 등록

---
+++ src/main/java/roomescape/common/config/WebConfig.java
@@ -0,0 +1,20 @@
+package roomescape.common.config;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.presentation.argument_resolver.LoginMemberArgumentResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
+    }
+}
