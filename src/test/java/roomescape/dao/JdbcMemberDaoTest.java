f18846ec79219cec1009e2b320b068c07ec4aa1b
taek2222 <oot309@daum.net>
2025-05-08 23:16:29 +0900

    test: Optional을 반환하도록 findByEmail 테스트 수정 및 예외 케이스 추가

--- src/test/java/roomescape/dao/JdbcMemberDaoTest.java
+++ src/test/java/roomescape/dao/JdbcMemberDaoTest.java
@@ -2,6 +2,7 @@ package roomescape.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,11 +31,25 @@ class JdbcMemberDaoTest {
         String email = "test@email.com";
 
         // when
-        Member result = jdbcMemberDao.findByEmail(email);
+        Optional<Member> result = jdbcMemberDao.findByEmail(email);
+
+        // then
+        assertThat(result.get())
+                .extracting("id", "name", "email", "password")
+                .containsExactly(1L, "테스트", "test@email.com", "password");
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 이메일로 조회시 빈 Optional을 반환한다")
+    void findMemberByEmailWithNonExistentEmailReturnsEmptyOptional() {
+        // given
+        String email = "null@email.com";
+
+        // when
+        Optional<Member> result = jdbcMemberDao.findByEmail(email);
 
         // then
         assertThat(result)
-                .extracting("id", "name", "email", "password")
-                .containsExactly(1L, "테스트", "test@email.com", "password");
+                .isEmpty();
     }
 }
