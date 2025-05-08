27d02799877bf688ebd9aaabf5ab9f1f196cfcd6
jeyong <joon6093@naver.com>
2025-05-08 20:29:30 +0900

    chore: 디버깅하기 쉽도록 GlobalExceptionHandler에서 로깅하도록 구현

--- src/main/java/roomescape/error/GlobalExceptionHandler.java
+++ src/main/java/roomescape/error/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package roomescape.error;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({ReservationException.class, IllegalArgumentException.class})
+    @ExceptionHandler({ReservationException.class, MemberException.class, IllegalArgumentException.class})
     public ResponseEntity<Object> handleBadRequestException(final Exception ex, final WebRequest request) {
+        log.info("BadRequestException: {}", ex.getMessage());
         final ProblemDetail body = super.createProblemDetail(ex, HttpStatus.BAD_REQUEST, ex.getMessage(), null,
                 null, request);
         return super.handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
@@ -21,6 +24,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Object> handleNotFoundException(final Exception ex, final WebRequest request) {
+        log.info("NotFoundException: {}", ex.getMessage());
         final ProblemDetail body = super.createProblemDetail(ex, HttpStatus.NOT_FOUND, ex.getMessage(), null,
                 null, request);
         return super.handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
@@ -28,6 +32,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleInternalServerError(final Exception ex, final WebRequest request) {
+        log.error("InternalServerError: {}", ex.getMessage(), ex);
         final ProblemDetail body = super.createProblemDetail(ex, HttpStatus.INTERNAL_SERVER_ERROR,
                 "서버에서 알 수 없는 오류가 발생했습니다.", null, null, request);
         return super.handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
