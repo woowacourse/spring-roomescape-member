48a52a2fa3bb65aac6400017b4af40f5cc3a795e
Jin409 <sualng123@gmail.com>
2025-05-08 20:24:36 +0900

    feat: CookieUtils 로 쿠키 관련 로직의 분리

--- src/main/java/roomescape/presentation/controller/AuthController.java
+++ src/main/java/roomescape/presentation/controller/AuthController.java
@@ -1,27 +1,26 @@
 package roomescape.presentation.controller;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.Arrays;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.common.exception.UnauthorizedException;
 import roomescape.dto.request.LoginRequestDto;
 import roomescape.dto.response.MemberResponseDto;
 import roomescape.dto.response.TokenResponseDto;
+import roomescape.presentation.support.CookieUtils;
 import roomescape.service.AuthService;
 
 @RestController
 public class AuthController {
 
-    public static final String COOKIE_NAME_FOR_TOKEN = "token";
     private final AuthService authService;
+    private final CookieUtils cookieUtils;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, CookieUtils cookieUtils) {
         this.authService = authService;
+        this.cookieUtils = cookieUtils;
     }
 
     @PostMapping("/login")
@@ -29,24 +28,12 @@ public class AuthController {
         authService.login(loginRequestDto);
         TokenResponseDto tokenResponseDto = authService.createToken(loginRequestDto.email());
 
-        Cookie cookie = new Cookie(COOKIE_NAME_FOR_TOKEN, tokenResponseDto.token());
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        response.addCookie(cookie);
+        cookieUtils.setCookieForToken(response, tokenResponseDto.token());
     }
 
     @GetMapping("/login/check")
     public MemberResponseDto loginCheck(HttpServletRequest request) {
-        String tokenFromCookie = getTokenFromCookie(request);
+        String tokenFromCookie = cookieUtils.getToken(request);
         return authService.getMemberByToken(tokenFromCookie);
-    }
-
-    private String getTokenFromCookie(HttpServletRequest request) {
-        Cookie foundCookie = Arrays.stream(request.getCookies())
-                .filter(cookie -> cookie.getName().equals(COOKIE_NAME_FOR_TOKEN))
-                .findFirst()
-                .orElseThrow(() -> new UnauthorizedException("권한이 필요한 접근입니다."));
-
-        return foundCookie.getValue();
     }
 }
---
+++ src/main/java/roomescape/presentation/support/CookieUtils.java
@@ -0,0 +1,30 @@
+package roomescape.presentation.support;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import org.springframework.stereotype.Service;
+import roomescape.common.exception.UnauthorizedException;
+
+@Service
+public class CookieUtils {
+
+    public static final String COOKIE_NAME_FOR_TOKEN = "token";
+
+    public void setCookieForToken(HttpServletResponse response, String token) {
+        Cookie cookie = new Cookie(COOKIE_NAME_FOR_TOKEN, token);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
+    public String getToken(HttpServletRequest request) {
+        Cookie foundCookie = Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(COOKIE_NAME_FOR_TOKEN))
+                .findFirst()
+                .orElseThrow(() -> new UnauthorizedException("권한이 필요한 접근입니다."));
+
+        return foundCookie.getValue();
+    }
+}
