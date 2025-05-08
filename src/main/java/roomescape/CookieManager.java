f5789530f62b7e9aa1a6de4126ed3fb722205a1d
supernovaMK <minkikim2002@gmail.com>
2025-05-08 13:11:45 +0900

    feat: 쿠키를 관리하는 기능 구현

---
+++ src/main/java/roomescape/CookieManager.java
@@ -0,0 +1,14 @@
+package roomescape;
+
+import jakarta.servlet.http.Cookie;
+
+public class CookieManager {
+    public static String extractTokenFromCookies(Cookie[] cookies) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                return cookie.getValue();
+            }
+        }
+        return "";
+    }
+}
