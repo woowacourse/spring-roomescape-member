6e0092de783fd0ef7f021f873239e62f1b6bd30b
taek2222 <oot309@daum.net>
2025-05-08 20:27:07 +0900

    refactor: 예외 관련 클래스 패키지 구조 변경

--- src/main/java/roomescape/global/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/global/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package roomescape.global;
+package roomescape.global.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
