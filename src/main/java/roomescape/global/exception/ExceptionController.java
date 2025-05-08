6f1312264f1f6daeea190f051de97e428f5bef06
rladmstn <oz33177351@gmail.com>
2025-05-08 11:18:48 +0900

    feat: Request 예외 통합 핸들러 메서드 추가

--- src/main/java/roomescape/global/exception/ExceptionController.java
+++ src/main/java/roomescape/global/exception/ExceptionController.java
@@ -1,9 +1,9 @@
 package roomescape.global.exception;
 
-import java.time.format.DateTimeParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,9 +14,9 @@ public class ExceptionController {
     private static final String PREFIX = "[ERROR] ";
     private static final Logger log = LoggerFactory.getLogger(ExceptionController.class);
 
-    @ExceptionHandler(DateTimeParseException.class)
-    public ResponseEntity<String> handleDateTimeParseException() {
-        return ResponseEntity.badRequest().body(PREFIX + "시간 형식이 잘못되었습니다.");
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException() {
+        return ResponseEntity.badRequest().body(PREFIX + "요청 데이터 형식이 잘못되었습니다.");
     }
 
     @ExceptionHandler(DuplicateTimeException.class)
@@ -54,6 +54,6 @@ public class ExceptionController {
     public ResponseEntity<String> handleBasicException(Exception e) {
         log.error("error has occurred ) {}", e.getMessage());
         log.error("error class ) {}", e.getClass());
-        return ResponseEntity.internalServerError().body(PREFIX + " 알 수 없는 에러가 발생했습니다.");
+        return ResponseEntity.internalServerError().body(PREFIX + "알 수 없는 에러가 발생했습니다.");
     }
 }
