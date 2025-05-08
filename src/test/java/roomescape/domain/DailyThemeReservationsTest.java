69e38c01ef3859afb6312de36c0b0217c7844296
LeeJaeHoon <dlwogns3413@naver.com>
2025-05-08 11:03:45 +0900

    test: 같은 테마, 같은 날짜에 예약된 시간 계산 로직 테스트

---
+++ src/test/java/roomescape/domain/DailyThemeReservationsTest.java
@@ -0,0 +1,68 @@
+package roomescape.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import roomescape.common.exception.BusinessRuleViolationException;
+
+class DailyThemeReservationsTest {
+
+    @Test
+    void 생성시_같은_테마와_같은_날짜가_아닌_예약이_있으면_예외가_발생한다() {
+        //given
+        List<Reservation> reservations = List.of(
+                new Reservation(
+                        1L,
+                        "test1",
+                        LocalDate.of(2025, 5, 8),
+                        new ReservationTime(1L, LocalTime.of(13, 1)),
+                        new Theme(1L, "theme", "test", "url")),
+                new Reservation(
+                        2L,
+                        "test2",
+                        LocalDate.of(2025, 5, 8),
+                        new ReservationTime(1L, LocalTime.of(13, 3)),
+                        new Theme(2L, "theme", "test", "url"))
+        );
+
+        //when & then
+        assertThatThrownBy(() -> new DailyThemeReservations(reservations, 1L, LocalDate.of(2025, 5, 8)))
+                .isInstanceOf(BusinessRuleViolationException.class)
+                .hasMessage("특정 테마, 특정 날짜에 속한 예약이 아닙니다.");
+    }
+
+    @Test
+    void 예약된_시간을_계산할_수_있다() {
+        //given
+        List<Reservation> reservations = List.of(
+                new Reservation(
+                        1L,
+                        "test1",
+                        LocalDate.of(2025, 5, 8),
+                        new ReservationTime(1L, LocalTime.of(13, 0)),
+                        new Theme(1L, "theme", "test", "url")),
+                new Reservation(
+                        2L,
+                        "test2",
+                        LocalDate.of(2025, 5, 8),
+                        new ReservationTime(2L, LocalTime.of(14, 0)),
+                        new Theme(1L, "theme", "test", "url"))
+        );
+        DailyThemeReservations dailyThemeReservations = new DailyThemeReservations(reservations, 1L,
+                LocalDate.of(2025, 5, 8));
+
+        //when
+        Set<ReservationTime> reservationTimes = dailyThemeReservations.calculateBookedTimes();
+
+        //then
+        assertThat(reservationTimes).isEqualTo(Set.of(
+                new ReservationTime(1L, LocalTime.of(13, 0)),
+                new ReservationTime(2L, LocalTime.of(14, 0))
+        ));
+    }
+}
