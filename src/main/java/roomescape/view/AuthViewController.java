e769ddfb2487741e025394dfeb00d0e8ebf484f8
minSsan <msp1125@hanyang.ac.kr>
2025-05-08 13:48:17 +0900

    feat: 로그인 뷰 렌더링

---
+++ src/main/java/roomescape/view/AuthViewController.java
@@ -0,0 +1,12 @@
+package roomescape.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthViewController {
+    @GetMapping("/login")
+    public String showWelcome() {
+        return "login";
+    }
+}
