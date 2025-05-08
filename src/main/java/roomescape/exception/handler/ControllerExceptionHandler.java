37a1300a245fed638c5f181071e538bf115e9609
AppleMint98 <applemint9821@gmail.com>
2025-05-08 21:19:45 +0900

    feat(ControllerExceptionHandler): 최상위 예외 ExceptionHandler 추가

--- src/main/java/roomescape/exception/handler/ControllerExceptionHandler.java
+++ src/main/java/roomescape/exception/handler/ControllerExceptionHandler.java
@@ -31,4 +31,9 @@ public class ControllerExceptionHandler {
     public ResponseEntity<String> handlePastReservationTimeException(final PastReservationTimeException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(final Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
 }
