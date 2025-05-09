97be4ec97a65eddd2c161a85fe6abd5e090623c9
GiSeok Kang <ime.rokz@gmail.com>
2025-05-09 14:42:11 +0900

    refactor: Admin, Member 클래스 분리
    - Role 삭제
    - Admin 및 Member 클래스로 역할 분리 및 책임 명확하게 변경

--- src/main/java/roomescape/member/domain/Admin.java
+++ src/main/java/roomescape/member/domain/Admin.java
@@ -1,13 +1,13 @@
 package roomescape.member.domain;
 
-public class Member {
+public class Admin {
 
     private final Long id;
     private final String name;
     private final String email;
     private final String password;
 
-    public Member(Long id, String name, String email, String password) {
+    public Admin(Long id, String name, String email, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
