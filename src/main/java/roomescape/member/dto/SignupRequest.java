6c200cf1c53525e0c769907545a195f989007ed4
wogus <ind07152@naver.com>
2025-05-09 15:54:20 +0900

    feat: 회원가입 요청 dto 생성

---
+++ src/main/java/roomescape/member/dto/SignupRequest.java
@@ -0,0 +1,21 @@
+package roomescape.member.dto;
+
+public record SignupRequest(String email, String name, String password) {
+
+    private static final String EMAIL_SIGN = "@";
+
+    public SignupRequest {
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("이메일은 비어있을 수 없습니다.");
+        }
+        if (!email.contains(EMAIL_SIGN)) {
+            throw new IllegalArgumentException("이메일의 형식에 맞게 작성해주세요.");
+        }
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 비어있을 수 없습니다.");
+        }
+        if (password == null || password.isBlank()) {
+            throw new IllegalArgumentException("비밀번호는 비어있을 수 없습니다.");
+        }
+    }
+}
---
+++ src/test/java/roomescape/member/dto/SignupRequestTest.java
@@ -0,0 +1,43 @@
+package roomescape.member.dto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SignupRequestTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("이메일 빈 값 에외 테스트")
+    void email_exception(String email) {
+        assertThatThrownBy(() -> new SignupRequest(email, "a", "a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "a.com", "aasd asd"})
+    @DisplayName("이메일 형식 예외 테스트")
+    void email_format_exception(String email) {
+        assertThatThrownBy(() -> new SignupRequest(email, "a", "a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("이름 빈 값 에외 테스트")
+    void name_exception(String name) {
+        assertThatThrownBy(() -> new SignupRequest("a@naver.com", name, "a"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("비밀번호 빈 값 에외 테스트")
+    void password_exception(String password) {
+        assertThatThrownBy(() -> new SignupRequest("a@naver.com", "a", password))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
