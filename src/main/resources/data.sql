10578db29a9ee0bda7de106a384a6ce9046d4f9b
RaZel713 <razel7130@gmail.com>
2025-05-09 21:17:50 +0900

    refactor: 예약 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,17 +20,17 @@ VALUES ('예시 3',
         '테스트 용 테마 3번 입니다.',
         'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
-INSERT INTO reservation(name, date, time_id, theme_id)
-VALUES ('test1', '2025-04-26', 1, 1);
-
-INSERT INTO reservation(name, date, time_id, theme_id)
-VALUES ('test2', '2025-04-27', 1, 2);
-
-INSERT INTO reservation(name, date, time_id, theme_id)
-VALUES ('test3', '2025-04-28', 1, 1);
-
 INSERT INTO member(role, name, email, password)
 VALUES ('Admin', '어드민', 'admin@email.com', 'password');
 
 INSERT INTO member(role, name, email, password)
 VALUES ('User', '사용자', 'user@email.com', 'password');
+
+INSERT INTO reservation(date, time_id, theme_id, member_id)
+VALUES ('2025-04-26', 1, 1, 1);
+
+INSERT INTO reservation(date, time_id, theme_id, member_id)
+VALUES ('2025-04-27', 1, 2, 1);
+
+INSERT INTO reservation(date, time_id, theme_id, member_id)
+VALUES ('2025-04-28', 1, 1, 1);
