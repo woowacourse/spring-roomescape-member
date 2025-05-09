94c8e5a5ce5326793cafbf11cceea81139f094a3
GiSeok Kang <ime.rokz@gmail.com>
2025-05-09 19:24:04 +0900

    feat: Member와 Admin을 인증하는 ArgumentResolver 추가
    - @LoginMember, @LoginAdmin 애노테이션 생성

---
+++ src/main/java/roomescape/login/infrastructure/LoginAdminArgumentResolver.java
@@ -0,0 +1,40 @@
+package roomescape.login.infrastructure;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import roomescape.common.util.auth.JwtTokenManager;
+import roomescape.login.presentation.annotation.LoginAdmin;
+import roomescape.login.presentation.dto.LoginAdminInfo;
+
+@Component
+public class LoginAdminArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginAdmin.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+
+        if (request.getCookies() == null) {
+            throw new IllegalStateException("인증할 수 없습니다.");
+        }
+
+        String token = Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals("token"))
+                .findAny()
+                .orElseThrow(() -> new IllegalStateException("인증할 수 없습니다."))
+                .getValue();
+
+        return new LoginAdminInfo(JwtTokenManager.getId(token));
+    }
+}
