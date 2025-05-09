9e9b5da0f17b26a5f3ec7c23599d37c6682e58dd
Chaeyoung714 <202102760@hufs.ac.kr>
2025-05-09 15:12:21 +0900

    chore: Member 데이터베이스에 role 필드 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,13 +14,13 @@ VALUES ('테마1', '테마1입니다.',
        ('테마3', '테마3입니다.',
         'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
-INSERT INTO member (name, email, password)
-VALUES ('김민준', 'minjun.kim@example.com', 'password123'),
-       ('이서연', 'seoyeon.lee@example.com', 'password456'),
-       ('박지훈', 'jihoon.park@example.com', 'password789'),
-       ('최수아', 'sua.choi@example.com', 'password101'),
-       ('정도윤', 'doyun.jung@example.com', 'password202'),
-       ('강하은', 'haeun.kang@example.com', 'password303');
+INSERT INTO member (name, email, password, role)
+VALUES ('김민준', 'minjun.kim@example.com', 'password123', 'ADMIN'),
+       ('이서연', 'seoyeon.lee@example.com', 'password456', 'ADMIN'),
+       ('박지훈', 'jihoon.park@example.com', 'password789', 'USER'),
+       ('최수아', 'sua.choi@example.com', 'password101', 'USER'),
+       ('정도윤', 'doyun.jung@example.com', 'password202', 'USER'),
+       ('강하은', 'haeun.kang@example.com', 'password303', 'USER');
 
 INSERT INTO reservation (date, member_id, time_id, theme_id)
 VALUES ('2025-05-03', 1, 1, 3),
