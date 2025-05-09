3355780a72b1f93e4732eb01cc1ff53f01a452e1
RaZel713 <razel7130@gmail.com>
2025-05-09 09:27:48 +0900

    feat: 멤버 테이블 스키마 정의

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,6 +14,16 @@ CREATE TABLE theme
     PRIMARY KEY (id)
 );
 
+CREATE TABLE member
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    role     VARCHAR(255) NOT NULL,
+    name     VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE reservation
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
