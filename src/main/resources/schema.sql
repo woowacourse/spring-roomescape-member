8e37aaaadc3c2eb7697b605da39687cfeb8ddf70
RaZel713 <razel7130@gmail.com>
2025-05-09 20:56:54 +0900

    feat: 예약 테이블 스키마 정의
    - 멤버 테이블을 외래키로 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -26,12 +26,13 @@ CREATE TABLE member
 
 CREATE TABLE reservation
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    name     VARCHAR(255) NOT NULL,
-    date     DATE         NOT NULL,
-    time_id  BIGINT,
-    theme_id BIGINT,
+    id        BIGINT NOT NULL AUTO_INCREMENT,
+    date      DATE   NOT NULL,
+    time_id   BIGINT,
+    theme_id  BIGINT,
+    member_id BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    FOREIGN KEY (member_id) REFERENCES member (id)
 );
