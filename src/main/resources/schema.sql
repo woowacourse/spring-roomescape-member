7a2ac3324ba12926b51dd40cfbcd8b194e4ea5e0
GiSeok Kang <ime.rokz@gmail.com>
2025-05-09 14:44:19 +0900

    feat: admin 테이블 추가
    - admin 테이블 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -32,6 +32,14 @@ CREATE TABLE member
     name     VARCHAR(255) NOT NULL,
     email    VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role     VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE admin
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
