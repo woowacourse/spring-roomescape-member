cd08521fedb61182bb43060363cad53449bcd136
Lee Dong Yeop <s01054830455@gmail.com>
2025-05-09 14:46:07 +0900

    chore: 회원 테이블 email 컬럼 unique key 설정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,9 @@ CREATE TABLE member
     name     VARCHAR(255) NOT NULL,
     email    VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    role VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (email)
 );
 
 
