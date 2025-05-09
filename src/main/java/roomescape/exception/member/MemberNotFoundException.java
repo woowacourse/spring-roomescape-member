e3e7fe1203345a35837dab7e776d8dc43089eb03
supernovaMK <minkikim2002@gmail.com>
2025-05-09 19:53:08 +0900

    feat: 멤버가 없는 경우의 커스텀 예외 구현

---
+++ src/main/java/roomescape/exception/member/MemberNotFoundException.java
@@ -0,0 +1,12 @@
+package roomescape.exception.member;
+
+import org.springframework.http.HttpStatus;
+import roomescape.exception.RoomescapeException;
+
+public class MemberNotFoundException extends RoomescapeException {
+
+    public MemberNotFoundException(Long memberId) {
+        super(HttpStatus.NOT_FOUND, "멤버를 찾을 수 없습니다. id=" + memberId);
+
+    }
+}
