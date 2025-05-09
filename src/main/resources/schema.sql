35ff48dcb3a0e362ce0dcbc319744a55e85fd465
bowook <mbosx316@naver.com>
2025-05-09 21:35:28 +0900

    chore : members 스키마 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,13 @@
+CREATE TABLE IF NOT EXISTS members
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (email)
+);
+
 CREATE TABLE IF NOT EXISTS reservation_times
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
