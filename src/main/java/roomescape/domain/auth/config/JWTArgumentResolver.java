e630a3f33468a874df627abd738f3d97a4fa2eb0
7i.w__k <tizmfns1218@Naver.com>
2025-05-09 16:17:40 +0900

    refactor: JwtArgumentResolver 이름 카멜 케이스 변경

--- src/main/java/roomescape/domain/auth/config/JWTArgumentResolver.java
+++ src/main/java/roomescape/domain/auth/config/JWTArgumentResolver.java
@@ -2,20 +2,23 @@ package roomescape.domain.auth.config;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import roomescape.domain.auth.exception.InvalidAuthorizationException;
 import roomescape.domain.auth.service.AuthService;
 
+@Slf4j
 @Component
-public class JWTArgumentResolver implements HandlerMethodArgumentResolver {
+public class JwtArgumentResolver implements HandlerMethodArgumentResolver {
 
     private final AuthService authService;
 
-    public JWTArgumentResolver(final AuthService authService) {
+    public JwtArgumentResolver(final AuthService authService) {
         this.authService = authService;
     }
 
@@ -28,11 +31,12 @@ public class JWTArgumentResolver implements HandlerMethodArgumentResolver {
     public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer,
                                   final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory)
             throws Exception {
+
         final HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
 
         final Cookie[] cookies = request.getCookies();
         if (cookies == null) {
-            return null;
+            throw new InvalidAuthorizationException("쿠키가 존재하지 않습니다!");
         }
 
         return authService.getLoginUser(cookies);
