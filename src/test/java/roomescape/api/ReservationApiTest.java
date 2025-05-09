f48ef06cd71a44f7febb9ff2144ccf5ea510f13b
jhpark1227 <junhyeok0246@naver.com>
2025-05-09 13:53:29 +0900

    test: API 테스트 리팩터링

--- src/test/java/roomescape/api/ReservationApiTest.java
+++ src/test/java/roomescape/api/ReservationApiTest.java
@@ -1,35 +1,38 @@
 package roomescape.api;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
+import roomescape.auth.Role;
+import roomescape.domain.Member;
+import roomescape.domain.repository.MemberRepository;
+import roomescape.infrastructure.JwtTokenProvider;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @Sql("/sql/data.sql")
 public class ReservationApiTest {
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
     @Test
     void 예약_추가_테스트() {
-        Map<String, Object> body = Map.of(
-                "email", "email1@domain.com",
-                "password", "password1"
+        Member savedMember = memberRepository.save(
+                new Member(null, "name1", "email1@domain.com", "password1", Role.MEMBER)
         );
-        String token = RestAssured.given()
-                .contentType(ContentType.JSON)
-                .body(body)
-                .when().post("/login")
-                .then()
-                .statusCode(200)
-                .extract().cookie("token");
+        String token = tokenProvider.createToken(savedMember.getId().toString(), savedMember.getRole());
 
         Map<String, Object> reservation = new HashMap<>();
         reservation.put("date", "2026-08-05");
@@ -43,30 +46,9 @@ public class ReservationApiTest {
                 .when().post("/reservations")
                 .then().log().all()
                 .statusCode(201)
-                .body("memberName", equalTo("포비"));
+                .body("date", equalTo("2026-08-05"))
+                .body("memberName", equalTo("name1"));
 
-    }
-
-    @Test
-    void 예약_조회_테스트() {
-        Map<String, Object> body = Map.of(
-                "email", "admin@domain.com",
-                "password", "admin"
-        );
-        String token = RestAssured.given()
-                .contentType(ContentType.JSON)
-                .body(body)
-                .when().post("/login")
-                .then()
-                .statusCode(200)
-                .extract().cookie("token");
-
-        RestAssured.given().log().all()
-                .cookie("token", token)
-                .when().get("admin/reservations")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(4));
     }
 
     @Test
--- src/test/java/roomescape/api/ReservationTimeApiTest.java
+++ src/test/java/roomescape/api/ReservationTimeApiTest.java
@@ -1,65 +1,125 @@
 package roomescape.api;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.jdbc.Sql;
+import roomescape.auth.Role;
+import roomescape.domain.Member;
+import roomescape.domain.Reservation;
+import roomescape.domain.ReservationTime;
+import roomescape.domain.Theme;
+import roomescape.domain.repository.MemberRepository;
+import roomescape.domain.repository.ReservationRepository;
+import roomescape.domain.repository.ReservationTimeRepository;
+import roomescape.domain.repository.ThemeRepository;
+import roomescape.dto.response.AvailableTimeResponse;
+import roomescape.dto.response.ReservationTimeResponse;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-@Sql("/sql/data.sql")
 public class ReservationTimeApiTest {
+
+    @Autowired
+    private ReservationTimeRepository reservationTimeRepository;
+    @Autowired
+    private ReservationRepository reservationRepository;
+    @Autowired
+    private ThemeRepository themeRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+
     @Test
     void 예약시간_추가_테스트() {
+        // given
         Map<String, String> params = new HashMap<>();
         params.put("startAt", "10:00");
 
-        RestAssured.given().log().all()
+        // when
+        ReservationTimeResponse response = RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
                 .when().post("/times")
                 .then().log().all()
                 .statusCode(201)
-                .body("startAt", equalTo("10:00"));
+                .extract().as(ReservationTimeResponse.class);
 
+        // then
+        List<ReservationTime> allReservationTimes = reservationTimeRepository.findAll();
+        SoftAssertions soft = new SoftAssertions();
+        soft.assertThat(response.startAt()).isEqualTo(LocalTime.of(10, 0));
+        soft.assertThat(allReservationTimes).hasSize(1);
+        soft.assertAll();
     }
 
     @Test
     void 예약시간_조회_테스트() {
-        RestAssured.given().log().all()
+        // given
+        reservationTimeRepository.create(ReservationTime.createWithoutId(LocalTime.of(10, 0)));
+        // when
+        List<ReservationTimeResponse> response = RestAssured.given().log().all()
                 .when().get("/times")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(4));
+                .extract().jsonPath().getList(".", ReservationTimeResponse.class);
+        // then
+        SoftAssertions soft = new SoftAssertions();
+        soft.assertThat(response).hasSize(1);
+        soft.assertThat(response.getFirst().startAt()).isEqualTo(LocalTime.of(10, 0));
+        soft.assertAll();
+
     }
 
     @Test
     void 예약시간_삭제_테스트() {
+        // given
+        reservationTimeRepository.create(ReservationTime.createWithoutId(LocalTime.of(10, 0)));
+        // when
         RestAssured.given().log().all()
-                .when().delete("/times/3")
+                .when().delete("/times/{timeId}", 1L)
                 .then().log().all()
                 .statusCode(204);
 
-        RestAssured.given().log().all()
-                .when().get("/times")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(3));
+        // then
+        List<ReservationTime> allReservationTimes = reservationTimeRepository.findAll();
+        assertThat(allReservationTimes).hasSize(0);
     }
 
     @Test
     void 가능한_예약시간_조회_테스트() {
-        RestAssured.given().log().all()
+        // when
+        ReservationTime reservationTime1 = reservationTimeRepository.create(
+                ReservationTime.createWithoutId(LocalTime.of(10, 0))
+        );
+        ReservationTime reservationTime2 = reservationTimeRepository.create(
+                ReservationTime.createWithoutId(LocalTime.of(11, 0))
+        );
+        Theme theme = themeRepository.create(Theme.createWithoutId("theme1", "desc", "thumb"));
+        Member member = memberRepository.save(
+                new Member(null, "member1", "email1@domain.com", "password1", Role.MEMBER));
+        reservationRepository.create(
+                Reservation.createWithoutId(member, LocalDate.of(2025, 1, 1), reservationTime1, theme));
+        // when
+        List<AvailableTimeResponse> response = RestAssured.given().log().all()
                 .when().get("/times/available?date=2025-01-01&themeId=1")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(4));
+                .extract().jsonPath().getList(".", AvailableTimeResponse.class);
+        // then
+        SoftAssertions soft = new SoftAssertions();
+        soft.assertThat(response).hasSize(2);
+        soft.assertThat(response.get(0).alreadyBooked()).isTrue();
+        soft.assertThat(response.get(1).alreadyBooked()).isFalse();
+        soft.assertAll();
     }
 }
