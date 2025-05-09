5f50ff8410a781e7ab033ae1e097f5340cf2f12a
Minuring <mw9505@gmail.com>
2025-05-09 22:01:56 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/roomescape/presentation/api/LoginApiController.java
+++ src/main/java/roomescape/presentation/api/LoginApiController.java
@@ -3,13 +3,13 @@ package roomescape.presentation.api;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import java.io.IOException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import roomescape.application.AuthenticationService;
 import roomescape.domain.User;
 import roomescape.presentation.Authenticated;
@@ -17,7 +17,6 @@ import roomescape.presentation.request.LoginRequest;
 import roomescape.presentation.response.UserResponse;
 
 @Controller
-@RequestMapping("/login")
 public class LoginApiController {
 
     private final AuthenticationService authenticationService;
@@ -26,7 +25,7 @@ public class LoginApiController {
         this.authenticationService = authenticationService;
     }
 
-    @PostMapping
+    @PostMapping("/login")
     public ResponseEntity<Void> performLogin(
         @RequestBody @Valid final LoginRequest request,
         final HttpServletResponse response
@@ -41,16 +40,24 @@ public class LoginApiController {
             .build();
     }
 
+    @GetMapping("/login/check")
+    public ResponseEntity<UserResponse> getUser(@Authenticated final User user) {
+        var response = UserResponse.from(user);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/logout")
+    public void performLogout(final HttpServletResponse response) throws IOException {
+        var tokenCookieToExpire = new Cookie("token", "");
+        tokenCookieToExpire.setMaxAge(0);
+        response.addCookie(tokenCookieToExpire);
+        response.sendRedirect("/");
+    }
+
     private Cookie createTokenCookie(final String issuedToken) {
         var tokenCookie = new Cookie("token", issuedToken);
         tokenCookie.setPath("/");
         tokenCookie.setHttpOnly(true);
         return tokenCookie;
-    }
-
-    @GetMapping("/check")
-    public ResponseEntity<UserResponse> getUser(@Authenticated final User user) {
-        var response = UserResponse.from(user);
-        return ResponseEntity.ok(response);
     }
 }
