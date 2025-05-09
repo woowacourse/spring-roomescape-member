ee5e5226f58abdd517750bb8e070ea6cadb46771
GiSeok Kang <ime.rokz@gmail.com>
2025-05-09 15:01:38 +0900

    feat: 로그인 시 Admin, Member를 구분하는 로직 추가
    - /login/check에서 role을 추출하여 구분하여 검증하도록 변경

--- src/main/java/roomescape/login/presentation/LoginController.java
+++ src/main/java/roomescape/login/presentation/LoginController.java
@@ -12,27 +12,49 @@ import org.springframework.web.bind.annotation.RestController;
 import roomescape.common.util.auth.JwtTokenManager;
 import roomescape.login.presentation.dto.LoginCheckResponse;
 import roomescape.login.presentation.dto.LoginRequest;
+import roomescape.member.domain.Admin;
 import roomescape.member.domain.Member;
+import roomescape.member.service.AdminService;
 import roomescape.member.service.MemberService;
 
 @RestController
 public class LoginController {
 
+    private final AdminService adminService;
     private final MemberService memberService;
 
-    public LoginController(MemberService memberService) {
+    public LoginController(AdminService adminService, MemberService memberService) {
+        this.adminService = adminService;
         this.memberService = memberService;
     }
 
     @PostMapping("/login")
     public ResponseEntity<Void> login(@RequestBody LoginRequest request) {
-        Member member = memberService.findByEmailAndPassword(request.email(), request.password());
+        boolean adminExist = adminService.isExistsByEmail(request.email());
+        boolean userExist = memberService.isExistsByEmail(request.email());
 
-        if (member == null) {
+        if (!adminExist && !userExist) {
             return ResponseEntity.status(401).build();
         }
 
-        String token = JwtTokenManager.crateToken(member.getId(), member.getRole().name());
+        String token = null;
+        if (userExist) {
+            Member member = memberService.findByEmail(request.email());
+            if (!member.isSamePassword(request.password())) {
+                return ResponseEntity.status(401).build();
+            }
+
+            token = JwtTokenManager.crateToken(member.getId(), "USER");
+        }
+        if (adminExist) {
+            Admin admin = adminService.findByEmail(request.email());
+            if (!admin.isSamePassword(request.password())) {
+                return ResponseEntity.status(401).build();
+            }
+
+            token = JwtTokenManager.crateToken(admin.getId(), "ADMIN");
+        }
+
         ResponseCookie cookie = ResponseCookie.from("token", token)
                 .httpOnly(true)
                 .path("/")
@@ -55,11 +77,15 @@ public class LoginController {
                 .orElseThrow(() -> new IllegalStateException("인증할 수 없습니다."))
                 .getValue();
 
-        Member member = memberService.findById(JwtTokenManager.getMemberId(token));
-        if (member == null) {
-            return ResponseEntity.status(401).build();
+        Long id = JwtTokenManager.getId(token);
+        String role = JwtTokenManager.getRole(token);
+
+        if (role.equals("ADMIN")) {
+            Admin admin = adminService.findById(id);
+            return ResponseEntity.ok().body(new LoginCheckResponse(admin.getName()));
         }
 
+        Member member = memberService.findById(id);
         return ResponseEntity.ok().body(new LoginCheckResponse(member.getName()));
     }
 }
