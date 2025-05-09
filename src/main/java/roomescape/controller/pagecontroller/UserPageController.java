a99d588da876b3e26bd466c6af24ce771294ab1f
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-05-09 13:52:58 +0900

    feat(controller) : Login 페이지 컨트롤러

--- src/main/java/roomescape/controller/pagecontroller/UserPageController.java
+++ src/main/java/roomescape/controller/pagecontroller/UserPageController.java
@@ -1,4 +1,4 @@
-package roomescape.controller;
+package roomescape.controller.pagecontroller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,5 +14,10 @@ public class UserPageController {
     @GetMapping
     public String bestThemes() {
         return "best-themes";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
