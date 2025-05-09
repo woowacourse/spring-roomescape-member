e44e66dff1fbd0469b0e128335ba7e147e3f83f7
Chaeyoung714 <202102760@hufs.ac.kr>
2025-05-09 16:00:19 +0900

    refactor: 토큰 인증 실패 시 에러메세지 수정

--- src/main/java/roomescape/exception/InvalidAccessTokenException.java
+++ src/main/java/roomescape/exception/InvalidAccessTokenException.java
@@ -3,6 +3,6 @@ package roomescape.exception;
 public class InvalidAccessTokenException extends RuntimeException {
 
     public InvalidAccessTokenException() {
-        super("유효하지 않은 액세스 토큰입니다.");
+        super("권한 인증이 불가능합니다.");
     }
 }
