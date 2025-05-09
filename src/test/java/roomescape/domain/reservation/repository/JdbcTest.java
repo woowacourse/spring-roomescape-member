fb8800a9ac47b220fe265206298e718a77b75d01
7i.w__k <tizmfns1218@Naver.com>
2025-05-09 21:14:39 +0900

    test: 유저 서비스 테스트

--- src/test/java/roomescape/domain/reservation/repository/JdbcTest.java
+++ src/test/java/roomescape/domain/reservation/repository/JdbcTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class JdbcTest {
 
     @Autowired
