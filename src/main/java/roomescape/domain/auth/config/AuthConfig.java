24af329d1e6ea7faf43c4f6a94e1d64121b69a21
7i.w__k <tizmfns1218@Naver.com>
2025-05-09 16:17:00 +0900

    feat: Config에 JwtArgumentResolver 등록

--- src/main/java/roomescape/domain/auth/config/AuthConfig.java
+++ src/main/java/roomescape/domain/auth/config/AuthConfig.java
@@ -1,24 +1,34 @@
 package roomescape.domain.auth.config;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.domain.auth.service.AuthService;
 import roomescape.domain.auth.service.JwtManager;
 
 @Configuration
 public class AuthConfig implements WebMvcConfigurer {
 
     private final JwtManager jwtManager;
+    private final AuthService authService;
 
     @Autowired
-    public AuthConfig(final JwtManager jwtManager) {
+    public AuthConfig(final JwtManager jwtManager, final AuthService authService) {
         this.jwtManager = jwtManager;
+        this.authService = authService;
     }
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(new CheckAdminLoginInterceptor(jwtManager))
                 .addPathPatterns("/admin", "/admin/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new JwtArgumentResolver(authService));
     }
 }
