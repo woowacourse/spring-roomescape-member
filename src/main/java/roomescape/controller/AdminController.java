de2eff3b1acebaf0553fb47cf2b02251790be9be
ChoiDongjun <adong0808@naver.com>
2025-05-09 15:49:26 +0900

    feat: 관리자 예약 생성 기능 추가

---
+++ src/main/java/roomescape/controller/AdminController.java
@@ -0,0 +1,34 @@
+package roomescape.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.controller.request.CreateReservationAdminRequest;
+import roomescape.service.ReservationService;
+import roomescape.service.param.CreateReservationParam;
+import roomescape.service.result.ReservationResult;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class AdminController {
+
+    private final ReservationService reservationService;
+
+    public AdminController(final ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping("/admin/reservations")
+    public ResponseEntity<ReservationResult> createReservation(@RequestBody CreateReservationAdminRequest reservationRequest) {
+        CreateReservationParam createReservationParam = new CreateReservationParam(
+                reservationRequest.memberId(),
+                reservationRequest.reservationDate(),
+                reservationRequest.timeId(),
+                reservationRequest.themeId()
+        );
+        Long reservationId = reservationService.create(createReservationParam, LocalDateTime.now());
+        return ResponseEntity.ok().body(reservationService.findById(reservationId));
+    }
+}
