580c48810af5ec910b65503edfa2b9b5d6444a77
taek2222 <oot309@daum.net>
2025-05-09 13:15:31 +0900

    test: 로그인 쿠키 인증 테스트 추가

--- src/test/java/roomescape/controller/LoginControllerTest.java
+++ src/test/java/roomescape/controller/LoginControllerTest.java
@@ -1,5 +1,7 @@
 package roomescape.controller;
 
+import static org.hamcrest.Matchers.is;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.Map;
@@ -7,6 +9,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -40,6 +43,27 @@ class LoginControllerTest {
                 .header("Keep-Alive", "timeout=60")
                 .header("Set-Cookie", Matchers.containsString("Path=/"))
                 .header("Set-Cookie", Matchers.containsString("HttpOnly"));
+    }
+
+    @Test
+    @DisplayName("사용자는 로그인 쿠키로 인증할 수 있다")
+    void authenticateLoginCookies() {
+        // given
+        Map<String, Object> body = createLoginRequest();
+
+        String token = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(body)
+                .when().post("/login")
+                .cookie("token");
+
+        // when
+        RestAssured.given().log().all()
+                .cookie("token", token)
+                .when().get("/login/check")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body("name", is("테스트"));
     }
 
     private Map<String, Object> createLoginRequest() {
