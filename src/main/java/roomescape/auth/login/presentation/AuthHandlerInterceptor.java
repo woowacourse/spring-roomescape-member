87d52220fad89b9d8bab8cf6234a080c989357b1
GiSeok Kang <ime.rokz@gmail.com>
2025-05-09 22:18:09 +0900

    feat: 토큰 속 role 검증 인터셉터 구현

---
+++ src/main/java/roomescape/auth/login/presentation/AuthHandlerInterceptor.java
@@ -0,0 +1,36 @@
+package roomescape.auth.login.presentation;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import org.springframework.web.servlet.HandlerInterceptor;
+import roomescape.auth.login.infrastructure.JwtTokenManager;
+
+public class AuthHandlerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return false;
+        }
+
+        String token = Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals("token"))
+                .findAny()
+                .orElse(null)
+                .getValue();
+        if (token == null) {
+            return false;
+        }
+
+        String role = JwtTokenManager.getRole(token);
+        if (role.equals("ADMIN")) {
+            return true;
+        }
+
+        return false;
+    }
+}
--- src/main/java/roomescape/common/configuration/WebMvcConfig.java
+++ src/main/java/roomescape/common/configuration/WebMvcConfig.java
@@ -3,7 +3,9 @@ package roomescape.common.configuration;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.auth.login.presentation.AuthHandlerInterceptor;
 import roomescape.auth.login.presentation.argumentresolver.LoginAdminArgumentResolver;
 import roomescape.auth.login.presentation.argumentresolver.LoginMemberArgumentResolver;
 
@@ -14,5 +16,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginAdminArgumentResolver());
         resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthHandlerInterceptor())
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin/login");
     }
 }
