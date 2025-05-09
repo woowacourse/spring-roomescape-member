52eb68e8149e386fe491978d23e2836bdfcfe441
7i.w__k <tizmfns1218@Naver.com>
2025-05-09 21:21:53 +0900

    refactor: 어노테이션을 활용해 처리하도록 변경

--- src/main/java/roomescape/domain/auth/controller/LoginController.java
+++ src/main/java/roomescape/domain/auth/controller/LoginController.java
@@ -1,25 +1,30 @@
 package roomescape.domain.auth.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.auth.config.AuthenticationPrincipal;
 import roomescape.domain.auth.dto.LoginRequest;
+import roomescape.domain.auth.dto.LoginUserDto;
 import roomescape.domain.auth.dto.TokenResponse;
 import roomescape.domain.auth.dto.UserInfoResponse;
 import roomescape.domain.auth.service.AuthService;
 
+@Slf4j
 @RestController
 @RequestMapping("/login")
 public class LoginController {
 
     private final AuthService authService;
 
+    @Autowired
     public LoginController(final AuthService authService) {
         this.authService = authService;
     }
@@ -40,9 +45,9 @@ public class LoginController {
     }
 
     @GetMapping("/check")
-    public ResponseEntity<UserInfoResponse> check(@CookieValue("token") final String token) {
-        final UserInfoResponse userInfo = authService.getUserInfo(token);
+    public ResponseEntity<UserInfoResponse> check(@AuthenticationPrincipal final LoginUserDto loginUserDto) {
+        final UserInfoResponse response = authService.getUserInfo(loginUserDto);
 
-        return ResponseEntity.ok(userInfo);
+        return ResponseEntity.ok(response);
     }
 }
