b0714c63fb97fd78d80b24c6ad877d1742662d48
soeun2537 <soeun2537@gmail.com>
2025-05-09 18:27:12 +0900

    feat: Member Table 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -18,10 +18,20 @@ CREATE TABLE reservation
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
-    date     DATE NOT NULL,
+    date     DATE         NOT NULL,
     time_id  BIGINT,
     theme_id BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
+);
+
+CREATE TABLE member
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role     VARCHAR(50)  NOT NULL,
+    PRIMARY KEY (id),
 );
