90f2b3e136e8390b8170f11c8bbaf5084e890cfd
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-05-09 12:05:48 +0900

    test(service) : 예약 생성 테스트 추가

--- src/test/java/roomescape/service/ReservationServiceTest.java
+++ src/test/java/roomescape/service/ReservationServiceTest.java
@@ -1,6 +1,7 @@
 package roomescape.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
@@ -12,12 +13,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import roomescape.dto.ReservationRequest;
 import roomescape.model.Reservation;
 import roomescape.model.ReservationDateTime;
 import roomescape.model.ReservationTime;
 import roomescape.model.Theme;
 import roomescape.model.UserName;
 import roomescape.repository.ReservationRepository;
+import roomescape.repository.ReservedChecker;
 
 @ExtendWith(MockitoExtension.class)
 class ReservationServiceTest {
@@ -51,6 +54,27 @@ class ReservationServiceTest {
 
         // then
         assertThat(reservationsResult).containsExactly(reservation1, reservation2);
+    }
+
+    @Mock
+    ReservationTimeService reservationTimeService;
+    @Mock
+    ThemeService themeService;
+    @Mock
+    ReservedChecker reservedChecker;
+
+    @Test
+    @DisplayName("예약 생성 조회")
+    void test2() {
+        ReservationTime reservationTime1 = new ReservationTime(1L, LocalTime.of(11, 0));
+        Theme theme1 = new Theme(1L, "테마명1", "테마설명1", "테마썸네일링크1");
+        ReservationRequest reservationRequest = new ReservationRequest("유저명", LocalDate.now().plusDays(1), 1L, 1L);
+
+        when(reservationTimeService.getReservationTimeById(1L)).thenReturn(reservationTime1);
+        when(themeService.getThemeById(1L)).thenReturn(theme1);
+        when(reservedChecker.contains(LocalDate.now().plusDays(1), 1L, 1L)).thenReturn(false);
+
+        assertThatCode(() -> reservationService.addReservation(reservationRequest)).doesNotThrowAnyException();
     }
 
 }
