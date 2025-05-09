2b0a7f37a0c06654816db40935d68913e4334edc
threepebbles <threepebbles@naver.com>
2025-05-09 22:26:50 +0900

    chore(data.sql): 개발 단계에서 브라우저 테스트를 쉽게 하기 위한 초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,10 +9,30 @@ INSERT INTO reservation_times (start_at)
 VALUES ('13:00');
 INSERT INTO reservation_times (start_at)
 VALUES ('14:00');
+INSERT INTO reservation_times (start_at)
+VALUES ('15:00');
 
 INSERT INTO themes (name, description, thumbnail)
-VALUES ('헤일러의 디버깅 교실', '재밌어요', 'debug.jpg');
+VALUES ('a헤일러의 디버깅 교실', '재밌어요', 'debug.jpg');
 INSERT INTO themes (name, description, thumbnail)
-VALUES ('우가의 레이어드 아키텍처', '우가우가', 'layered.jpg');
+VALUES ('b우가의 레이어드 아키텍처', '우가우가', 'layered.jpg');
 INSERT INTO themes (name, description, thumbnail)
-VALUES ('이름', '설명', 'thumbnail.jpg');
+VALUES ('테마명1', '설명1', '썸네일1');
+INSERT INTO themes (name, description, thumbnail)
+VALUES ('테마명2', '설명2', '썸네일2');
+INSERT INTO themes (name, description, thumbnail)
+VALUES ('테마명3', '설명', '썸네일');
+INSERT INTO themes (name, description, thumbnail)
+VALUES ('테마명4', '설명', '썸네일');
+INSERT INTO themes (name, description, thumbnail)
+VALUES ('테마명5', '설명', '썸네일');
+INSERT INTO themes (name, description, thumbnail)
+VALUES ('테마명6', '설명', '썸네일');
+INSERT INTO themes (name, description, thumbnail)
+VALUES ('테마명7', '설명', '썸네일');
+INSERT INTO themes (name, description, thumbnail)
+VALUES ('테마명8', '설명', '썸네일');
+INSERT INTO themes (name, description, thumbnail)
+VALUES ('테마명9', '설명', '썸네일');
+INSERT INTO themes (name, description, thumbnail)
+VALUES ('테마명10', '설명', '썸네일');
