c1e2b44a6aa688ff17bb79846a008908b5ac6197
ryan-dia <cjfdnjs158@naver.com>
2025-05-11 02:20:44 +0900

    feat: 예상치 못한 에러도 잡도록 핸들러 추가

--- src/main/java/roomescape/error/GlobalExceptionHandler.java
+++ src/main/java/roomescape/error/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package roomescape.error;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -10,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ReservationException.class, IllegalArgumentException.class})
@@ -36,6 +38,15 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(TokenCreationException.class)
     public ResponseEntity<Object> handleTokenCreationException(final Exception ex, final WebRequest request) {
         final ProblemDetail body = super.createProblemDetail(ex, HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(),
+                null,
+                null, request);
+        return super.handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleInternalServerException(final Exception ex, final WebRequest request) {
+        log.error(ex.getMessage(), ex);
+        final ProblemDetail body = super.createProblemDetail(ex, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.",
                 null,
                 null, request);
         return super.handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
