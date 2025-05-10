46f53851982bc5e5eef4c8e4308d788b7c9d6a0b
wogus <ind07152@naver.com>
2025-05-10 13:16:55 +0900

    feat: 커스텀 예외 생성
    - 로그인 커스텀 예외 생성
    - 로그인 커스텀 예외 처리

--- src/main/java/roomescape/common/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/common/exceptionHandler/GlobalExceptionHandler.java
@@ -1,16 +1,27 @@
 package roomescape.common.exceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import roomescape.common.exception.LoginException;
 import roomescape.common.exceptionHandler.dto.ExceptionResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final String EXCEPTION_PREFIX = "[ERROR] ";
+
+    @ExceptionHandler(value = LoginException.class)
+    public ResponseEntity<ExceptionResponse> loginFail(
+            final LoginException exception, final HttpServletRequest request
+    ) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                EXCEPTION_PREFIX + exception.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exceptionResponse);
+    }
 
     @ExceptionHandler(value = IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponse> invalidInput(
