3b4a755b5520ecc4f2cb60c3da262f5dd6e6f1f4
ryan-dia <cjfdnjs158@naver.com>
2025-05-11 03:47:03 +0900

    test: 특정 조건 예약 조회 테스트 추가

--- src/test/java/roomescape/reservation/repository/JdbcReservationRepositoryTest.java
+++ src/test/java/roomescape/reservation/repository/JdbcReservationRepositoryTest.java
@@ -52,6 +52,7 @@ class JdbcReservationRepositoryTest {
 
     @Test
     void 예약이_올바르게_생성된다() {
+
         // given
         LocalDate date = LocalDate.of(2025, 7, 1);
         LocalTime time = LocalTime.of(10, 0);
@@ -74,6 +75,7 @@ class JdbcReservationRepositoryTest {
 
     @Test
     void 기존에_날짜와_시간이_같은_예약이_있는지_확인() {
+
         // given
         LocalDate existedDate = LocalDate.of(2025, 7, 1);
         LocalTime existedTime = LocalTime.of(10, 0);
@@ -102,6 +104,7 @@ class JdbcReservationRepositoryTest {
 
     @Test
     void 모든_예약_조회() {
+
         // given
         LocalDate date = LocalDate.of(2025, 7, 1);
         ReservationTime time1 = new ReservationTime(1L, LocalTime.of(10, 0));
@@ -136,7 +139,66 @@ class JdbcReservationRepositoryTest {
     }
 
     @Test
+    void 특정_날짜로_예약_조회() {
+
+        // given
+        LocalDate date1 = LocalDate.of(2999, 7, 1);
+        LocalDate date2 = LocalDate.of(2999, 7, 2);
+        LocalDate date3 = LocalDate.of(2999, 7, 3);
+        ReservationTime time = new ReservationTime(1L, LocalTime.of(10, 0));
+        Theme theme = new Theme(1L, "테마1", "설명1", "썸네일1");
+        Member member = new Member(1L, "유저1", "user1@naver.com", "pwd", MemberRole.MEMBER.name());
+
+        repository.save(new Reservation(null, date1, time, theme, member));
+        repository.save(new Reservation(null, date2, time, theme, member));
+        repository.save(new Reservation(null, date3, time, theme, member));
+
+        // when
+        List<Reservation> reservations = repository.findByCriteria(null, null, date2, null);
+
+        // then
+        SoftAssertions.assertSoftly(soft -> {
+            assertThat(reservations).hasSize(2);
+            assertThat(reservations.get(0).getDate()).isEqualTo(date2);
+            assertThat(reservations.get(1).getDate()).isEqualTo(date3);
+        });
+
+    }
+
+    @Test
+    void 회원_ID로_예약_조회() {
+        // given
+        LocalDate date1 = LocalDate.of(2999, 7, 1);
+        LocalDate date2 = LocalDate.of(2999, 7, 2);
+        LocalDate date3 = LocalDate.of(2999, 7, 3);
+        LocalDate date4 = LocalDate.of(2999, 7, 4);
+        LocalDate date5 = LocalDate.of(2999, 7, 5);
+        ReservationTime time = new ReservationTime(1L, LocalTime.of(10, 0));
+        Theme theme = new Theme(1L, "테마1", "설명1", "썸네일1");
+        Member member1 = new Member(1L, "유저1", "user1@naver.com", "pwd", MemberRole.MEMBER.name());
+        Member member2 = new Member(2L, "유저2", "user2@naver.com", "pwd", MemberRole.MEMBER.name());
+
+        repository.save(new Reservation(null, date1, time, theme, member1));
+        repository.save(new Reservation(null, date2, time, theme, member1));
+        repository.save(new Reservation(null, date3, time, theme, member1));
+        repository.save(new Reservation(null, date4, time, theme, member2));
+        repository.save(new Reservation(null, date5, time, theme, member2));
+
+        // when
+        List<Reservation> reservations = repository.findByCriteria(null, member1.getId(), null, null);
+
+        // then
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(reservations).hasSize(3);
+            soft.assertThat(reservations.get(0).getMember().getId()).isEqualTo(member1.getId());
+            soft.assertThat(reservations.get(1).getMember().getId()).isEqualTo(member1.getId());
+            soft.assertThat(reservations.get(2).getMember().getId()).isEqualTo(member1.getId());
+        });
+    }
+
+    @Test
     void 예약에_예약_시간이_존재하는지_확인() {
+
         // given
         ReservationTime reservationTime = new ReservationTime(1L, LocalTime.of(10, 0));
         Theme theme1 = new Theme(1L, "테마1", "설명1", "썸네일1");
