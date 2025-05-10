36a3d34cb7e8ce5c4c7281468732cd5f21c99edd
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-05-11 02:32:23 +0900

    feat: 유효하지 않은 토큰 예외처리

--- src/main/java/roomescape/auth/infrastructure/JwtTokenProvider.java
+++ src/main/java/roomescape/auth/infrastructure/JwtTokenProvider.java
@@ -1,14 +1,18 @@
 package roomescape.auth.infrastructure;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import roomescape.auth.dto.AuthenticatedMember;
+import roomescape.global.exception.AuthenticationException.InvalidTokenException;
 import roomescape.global.util.NumberParser;
 import roomescape.member.domain.Member;
+import roomescape.member.domain.Role;
 
 @Component
 public class JwtTokenProvider implements TokenProvider {
@@ -30,7 +34,7 @@ public class JwtTokenProvider implements TokenProvider {
                 .subject(member.getId().toString())
                 .expiration(new Date(System.currentTimeMillis() + expiration))
                 .claim(CLAIM_EMAIL_KEY, member.getEmail())
-                .claim(CLAIM_ROLE_KEY, member.getRole())
+                .claim(CLAIM_ROLE_KEY, member.getRole().name())
                 .claim(CLAIM_NAME_KEY, member.getName())
                 .issuer(issure)
                 .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
@@ -40,17 +44,27 @@ public class JwtTokenProvider implements TokenProvider {
     //TODO : JWT 유효성 예외 처리
     @Override
     public AuthenticatedMember resolveAuthenticatedMember(String token) {
-        Claims payload = Jwts.parser()
-                .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
+        Claims payload = extracClaims(token);
 
         return AuthenticatedMember.builder()
                 .id(NumberParser.parseToLong(payload.getSubject()))
                 .email(payload.get(CLAIM_EMAIL_KEY, String.class))
-                .role(payload.get(CLAIM_ROLE_KEY, String.class))
+                .role(Role.findByName(payload.get(CLAIM_ROLE_KEY, String.class)))
                 .name(payload.get(CLAIM_NAME_KEY, String.class))
                 .build();
+    }
+
+    private Claims extracClaims(String token) {
+        try {
+            return Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        } catch (ExpiredJwtException e) {
+            throw new InvalidTokenException("토큰이 만료되었습니다.", e);
+        } catch (JwtException e) {
+            throw new InvalidTokenException("유효하지 않은 토큰입니다..", e);
+        }
     }
 }
