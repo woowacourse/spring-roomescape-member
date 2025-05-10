fa5232989f25c9330968db058db9b300968c87f0
7i.w__k <tizmfns1218@Naver.com>
2025-05-10 22:56:51 +0900

    refactor: JwtProperties 검증 추가

--- src/main/java/roomescape/RoomEscapeApplication.java
+++ src/main/java/roomescape/RoomEscapeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RoomEscapeApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(RoomEscapeApplication.class, args);
     }
 
