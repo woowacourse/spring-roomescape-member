c75210bdb91346e1906c12d55efffa378fb73c5f
RaZel713 <razel7130@gmail.com>
2025-05-10 22:11:52 +0900

    refactor: 응답 DTO 수정

--- src/main/java/roomescape/member/controller/dto/MemberResponse.java
+++ src/main/java/roomescape/member/controller/dto/MemberResponse.java
@@ -2,10 +2,10 @@ package roomescape.member.controller.dto;
 
 import roomescape.member.domain.Member;
 
-public record MemberResponse(long id, String role, String name, String email) {
+public record MemberResponse(long id, String role, String name, String email, String password) {
 
     public static MemberResponse from(final Member member) {
         return new MemberResponse(
-                member.getId(), member.getRole(), member.getName(), member.getEmail());
+                member.getId(), member.getRole(), member.getName(), member.getEmail(), member.getPassword());
     }
 }
