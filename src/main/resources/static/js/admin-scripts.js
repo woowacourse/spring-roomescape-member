65759b0cec37fe53a7e1e885b64dd73c77e8ccd9
GiSeok Kang <ime.rokz@gmail.com>
2025-05-10 21:54:38 +0900

    refactor: 관리자 페이지 회원가입 관련 코드 제거

--- src/main/resources/static/js/admin-scripts.js
+++ src/main/resources/static/js/admin-scripts.js
@@ -88,59 +88,6 @@ function login() {
       });
 }
 
-function signup() {
-  // Redirect to signup page
-  window.location.href = '/signup';
-}
-
-function register(event) {
-  // 폼 데이터 수집
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const name = document.getElementById('name').value;
-
-  // 입력 필드 검증
-  if (!email || !password || !name) {
-    alert('Please fill in all fields.');
-    return; // 필수 입력 필드가 비어있으면 여기서 함수 실행을 중단
-  }
-
-  // 요청 데이터 포맷팅
-  const formData = {
-    email: email,
-    password: password,
-    name: name
-  };
-
-  // AJAX 요청 생성 및 전송
-  fetch('/admins', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(formData)
-  })
-      .then(response => {
-        if (!response.ok) {
-          alert('Signup request failed');
-          throw new Error('Signup request failed');
-        }
-        return response.json(); // 여기서 응답을 JSON 형태로 변환
-      })
-      .then(data => {
-        // 성공적인 응답 처리
-        console.log('Signup successful:', data);
-        window.location.href = '/admin/login';
-      })
-      .catch(error => {
-        // 에러 처리
-        console.error('Error during signup:', error);
-      });
-
-  // 폼 제출에 의한 페이지 리로드 방지
-  event.preventDefault();
-}
-
 function base64DecodeUnicode(str) {
   // Base64 디코딩
   const decodedBytes = atob(str);
