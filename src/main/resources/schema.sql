65334a519fb0625f861b332bb7d35647cd46872a
sonjh919 <kingjh1125@gmail.com>
2025-05-10 15:06:44 +0900

    refactor: email에 unique 속성 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE member
 (
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
-    email       VARCHAR(255) NOT NULL,
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    email       VARCHAR(255) NOT NULL UNIQUE,
     password    VARCHAR(255) NOT NULL,
     name        VARCHAR(255) NOT NULL,
     role        VARCHAR(255) NOT NULL,
@@ -10,8 +10,8 @@ CREATE TABLE member
 
 CREATE TABLE reservation_time
 (
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at TIME NOT NULL,
+    id          BIGINT      NOT NULL AUTO_INCREMENT,
+    start_at    TIME        NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -20,17 +20,17 @@ CREATE TABLE theme
     id          BIGINT       NOT NULL AUTO_INCREMENT,
     name        VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    thumbnail VARCHAR(255) NOT NULL,
+    thumbnail   VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation
 (
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
-    member_id BIGINT,
-    date DATE NOT NULL,
-    time_id BIGINT,
-    theme_id BIGINT,
+    id          BIGINT  NOT NULL AUTO_INCREMENT,
+    member_id   BIGINT,
+    date        DATE    NOT NULL,
+    time_id     BIGINT,
+    theme_id    BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
