672fcb61958485bf537394d4868e5628c78e4116
minSsan <msp1125@hanyang.ac.kr>
2025-05-10 22:36:17 +0900

    chore: 불필요한 import 삭제

--- src/main/java/roomescape/reservation/controller/AdminReservationController.java
+++ src/main/java/roomescape/reservation/controller/AdminReservationController.java
@@ -7,10 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.auth.entity.LoginMember;
 import roomescape.reservation.service.ReservationService;
 import roomescape.reservation.service.dto.request.CreateReservationRequest;
-import roomescape.reservation.service.dto.request.ReservationRequest;
 import roomescape.reservation.service.dto.response.ReservationResponse;
 
 @RestController
