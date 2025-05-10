2e44ad662aa470d008f408158e0fa95b72b03615
wogus <ind07152@naver.com>
2025-05-10 21:19:51 +0900

    test: 예약 관련 테스트 추가
    - 예약 삭제 테스트
    - 예약 조건 조회 테스트

--- src/test/java/roomescape/reservation/service/ReservationServiceTest.java
+++ src/test/java/roomescape/reservation/service/ReservationServiceTest.java
@@ -1,5 +1,6 @@
 package roomescape.reservation.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
@@ -21,7 +22,9 @@ import roomescape.member.domain.Role;
 import roomescape.member.service.FakeMemberRepository;
 import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.domain.ReservationRepository;
+import roomescape.reservation.dto.ReservationConditionRequest;
 import roomescape.reservation.dto.ReservationRequest;
+import roomescape.reservation.dto.ReservationResponse;
 import roomescape.reservationTime.domain.ReservationTime;
 import roomescape.reservationTime.domain.ReservationTimeRepository;
 import roomescape.theme.domain.Theme;
@@ -62,8 +65,12 @@ class ReservationServiceTest {
 
     @BeforeEach
     void beforeEach() {
-        Theme theme = Theme.createWithId(1L, "테스트1", "설명", "localhost:8080");
-        themeRepository.save(theme);
+        Theme theme1 = Theme.createWithId(1L, "테스트1", "설명", "localhost:8080");
+        Theme theme2 = Theme.createWithId(2L, "테스트2", "설명", "localhost:8080");
+        Theme theme3 = Theme.createWithId(3L, "테스트2", "설명", "localhost:8080");
+        themeRepository.save(theme1);
+        themeRepository.save(theme2);
+        themeRepository.save(theme3);
         ReservationTime reservationTime1 = ReservationTime.createWithoutId(LocalTime.of(10, 0));
         ReservationTime reservationTime2 = ReservationTime.createWithoutId(LocalTime.of(9, 0));
         Member member = Member.createWithId(1L, "홍길동", "a", "a", Role.USER);
@@ -72,7 +79,13 @@ class ReservationServiceTest {
         reservationTimeRepository.save(reservationTime2);
         reservationRepository.save(
                 Reservation.createWithoutId(LocalDateTime.of(1999, 11, 2, 20, 10), member, LocalDate.of(2024, 10, 6),
-                        reservationTime1, theme));
+                        reservationTime1, theme1));
+        reservationRepository.save(
+                Reservation.createWithoutId(LocalDateTime.of(1999, 11, 2, 20, 10), member, LocalDate.of(2024, 10, 7),
+                        reservationTime1, theme2));
+        reservationRepository.save(
+                Reservation.createWithoutId(LocalDateTime.of(1999, 11, 2, 20, 10), member, LocalDate.of(2024, 10, 8),
+                        reservationTime1, theme2));
     }
 
     @DisplayName("지나간 날짜와 시간에 대한 예약을 생성할 수 없다.")
@@ -91,4 +104,67 @@ class ReservationServiceTest {
                 new ReservationRequest(LocalDate.of(2024, 10, 6), 1L, 1L), 1L))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("존재하지 않은 예약을 삭제하려고 하면 예외가 발생한다.")
+    void validateExistIdToDelete_test() {
+        assertThatThrownBy(() -> reservationService.deleteReservationById(100L))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("예약을 정상적으로 삭제한다.")
+    void deleteReservationById_test() {
+        // when
+        reservationService.deleteReservationById(1L);
+        // then
+        List<ReservationResponse> reservations = reservationService.getReservations(
+                new ReservationConditionRequest(null, null, null, null));
+        assertThat(reservations).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("조건이 없을 때는 모든 예약을 들고 온다.")
+    void getAllReservations_test() {
+        // given
+        ReservationConditionRequest request = new ReservationConditionRequest(null, null, null, null);
+        // when
+        List<ReservationResponse> reservations = reservationService.getReservations(request);
+        // then
+        assertThat(reservations).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("조건이 있을 경우 조건에 맞는 예약을 들고 온다.")
+    void getConditionalReservations_test(Long memberId, Long themeId, LocalDate dateForm, LocalDate dateTo,
+                                         int expectedCount) {
+        // given
+        ReservationConditionRequest request = new ReservationConditionRequest(memberId, themeId, dateForm, dateTo);
+        // when
+        List<ReservationResponse> reservations = reservationService.getReservations(request);
+        // then
+        assertThat(reservations).hasSize(expectedCount);
+    }
+
+    private static Stream<Arguments> getConditionalReservations_test() {
+        return Stream.of(
+                Arguments.of(1L, null, null, null, 3),
+                Arguments.of(2L, null, null, null, 0),
+                Arguments.of(null, 1L, null, null, 1),
+                Arguments.of(null, 2L, null, null, 2),
+                Arguments.of(null, 3L, null, null, 0),
+                Arguments.of(null, null, LocalDate.of(2024, 10, 6), null, 3),
+                Arguments.of(null, null, LocalDate.of(2024, 10, 7), null, 2),
+                Arguments.of(null, null, LocalDate.of(2024, 10, 8), null, 1),
+                Arguments.of(null, null, LocalDate.of(2024, 10, 9), null, 0),
+                Arguments.of(null, null, null, LocalDate.of(2024, 10, 8), 3),
+                Arguments.of(null, null, null, LocalDate.of(2024, 10, 7), 2),
+                Arguments.of(null, null, null, LocalDate.of(2024, 10, 6), 1),
+                Arguments.of(null, null, null, LocalDate.of(2024, 10, 5), 0),
+                Arguments.of(null, null, LocalDate.of(2024, 10, 6), LocalDate.of(2024, 10, 7), 2)
+        );
+    }
+
+
 }
