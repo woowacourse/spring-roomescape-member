f08d7b862a6523c0abf8ff0a3bf47cbd26baea4c
wogus <ind07152@naver.com>
2025-05-10 18:58:31 +0900

    feat: 어드민 권한 확인 인터셉터 추가

--- src/main/java/roomescape/common/config/ReservationConfig.java
+++ src/main/java/roomescape/common/config/ReservationConfig.java
@@ -3,8 +3,11 @@ package roomescape.common.config;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.common.argumentResolver.LoginArgumentResolver;
+import roomescape.common.interceptor.AdminInterceptor;
+import roomescape.common.util.JwtTokenContainer;
 import roomescape.common.util.TokenCookieManager;
 import roomescape.member.service.LoginService;
 
@@ -13,14 +16,23 @@ public class ReservationConfig implements WebMvcConfigurer {
 
     private final LoginService loginService;
     private final TokenCookieManager tokenCookieManager;
+    private final JwtTokenContainer jwtTokenContainer;
 
-    public ReservationConfig(LoginService loginService, TokenCookieManager tokenCookieManager) {
+    public ReservationConfig(LoginService loginService, TokenCookieManager tokenCookieManager,
+                             JwtTokenContainer jwtTokenContainer) {
         this.loginService = loginService;
         this.tokenCookieManager = tokenCookieManager;
+        this.jwtTokenContainer = jwtTokenContainer;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginArgumentResolver(loginService, tokenCookieManager));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AdminInterceptor(tokenCookieManager,jwtTokenContainer))
+                .addPathPatterns("/admin/**");
     }
 }
---
+++ src/main/java/roomescape/common/interceptor/AdminInterceptor.java
@@ -0,0 +1,43 @@
+package roomescape.common.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.HandlerInterceptor;
+import roomescape.common.exception.LoginException;
+import roomescape.common.util.JwtTokenContainer;
+import roomescape.common.util.TokenCookieManager;
+import roomescape.member.domain.Role;
+
+public class AdminInterceptor implements HandlerInterceptor {
+
+    private final TokenCookieManager tokenCookieManager;
+    private final JwtTokenContainer jwtTokenContainer;
+
+    public AdminInterceptor(TokenCookieManager tokenCookieManager, JwtTokenContainer jwtTokenContainer) {
+        this.tokenCookieManager = tokenCookieManager;
+        this.jwtTokenContainer = jwtTokenContainer;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        try {
+            String token = tokenCookieManager.extractTokenFromCookie(request);
+            Role memberRole = jwtTokenContainer.getMemberRole(token);
+            if (memberRole.equals(Role.ADMIN)) {
+                return true;
+            }
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            return false;
+        } catch (LoginException e) {
+            return redirectedToLogin(response);
+        }
+    }
+
+    private boolean redirectedToLogin(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/login");
+        return false;
+    }
+}
