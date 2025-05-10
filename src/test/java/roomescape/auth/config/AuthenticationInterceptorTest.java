f361e8fdb962a157596a23b2b48db33f9ecd3027
Dompoo <dlckdrms0517@gmail.com>
2025-05-10 16:28:10 +0900

    test: 불일치하는 테스트 클래스명 수정

--- src/test/java/roomescape/auth/config/AuthenticationInterceptorTest.java
+++ src/test/java/roomescape/auth/config/AuthenticationInterceptorTest.java
@@ -14,7 +14,7 @@ import roomescape.exception.auth.NotAuthenticatedException;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
-class AuthTokenInterceptorTest {
+class AuthenticationInterceptorTest {
 
     private HttpServletRequest request;
     private HttpServletResponse response;
