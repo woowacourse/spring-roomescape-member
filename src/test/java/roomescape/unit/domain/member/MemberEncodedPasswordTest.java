a313f36de38770410c8674f78699e1eda2f7041f
HyeonsuLee <leehyeonsu4888@naver.com>
2025-05-11 01:55:24 +0900

    test: 인코딩된 패스워드 테스트 수정

--- src/test/java/roomescape/unit/domain/member/MemberEncodedPasswordTest.java
+++ src/test/java/roomescape/unit/domain/member/MemberEncodedPasswordTest.java
@@ -3,6 +3,7 @@ package roomescape.unit.domain.member;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import roomescape.domain.member.MemberEncodedPassword;
 import roomescape.domain.member.MemberPassword;
 
 class MemberEncodedPasswordTest {
@@ -10,7 +11,7 @@ class MemberEncodedPasswordTest {
     @Test
     void 인코딩된_비밀번호는_null일_수_없다() {
         // when & then
-        assertThatThrownBy(() -> new MemberPassword(null))
+        assertThatThrownBy(() -> new MemberEncodedPassword(null))
                 .isInstanceOf(NullPointerException.class);
     }
 }
