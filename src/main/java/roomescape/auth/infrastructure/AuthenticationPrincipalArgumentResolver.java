b2a85305f779252e1c212113f4d0f5e53e656432
ryan-dia <cjfdnjs158@naver.com>
2025-05-10 16:32:37 +0900

    refactor: 불필요한 로직 제거

--- src/main/java/roomescape/auth/infrastructure/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/roomescape/auth/infrastructure/AuthenticationPrincipalArgumentResolver.java
@@ -1,19 +1,15 @@
 package roomescape.auth.infrastructure;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import roomescape.auth.dto.LoginMember;
 import roomescape.error.NotFoundException;
-import roomescape.error.UnauthorizedException;
 import roomescape.member.domain.Member;
 import roomescape.member.repository.MemberRepository;
 
@@ -21,9 +17,6 @@ import roomescape.member.repository.MemberRepository;
 @RequiredArgsConstructor
 public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
 
-    private static final String TOKEN_COOKIE_NAME = "token";
-
-    private final JwtTokenProvider jwtTokenProvider;
     private final MemberRepository memberRepository;
 
     @Override
@@ -38,35 +31,16 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
             final NativeWebRequest webRequest,
             final WebDataBinderFactory binderFactory
     ) {
-        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
 
-        String token = extractTokenFromCookies(request.getCookies());
-        if (!StringUtils.hasText(token)) {
-            throw new UnauthorizedException("토큰이 없습니다.");
-        }
-
-        final String subject = jwtTokenProvider.getSubject(token);
-        final Long memberId = Long.valueOf(subject);
-
+        final HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        final Long memberId = (Long) request.getAttribute("memberId");
         final Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new NotFoundException("존재하지 않는 멤버입니다."));
-
         return new LoginMember(
                 member.getId(),
                 member.getName(),
                 member.getEmail(),
                 member.getPassword(),
                 member.getRole());
-    }
-
-    private String extractTokenFromCookies(final Cookie[] cookies) {
-        if (cookies == null) {
-            return "";
-        }
-        return Arrays.stream(cookies)
-                .filter(c -> TOKEN_COOKIE_NAME.equals(c.getName()))
-                .findFirst()
-                .map(Cookie::getValue)
-                .orElse("");
     }
 }
