76c17e27779ec73e8b85396a87aaee429409dcdb
taek2222 <oot309@daum.net>
2025-05-10 11:10:51 +0900

    feat: 데이터 초기값에 MEMBER와 reservation 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,4 +15,17 @@ VALUES ('테마1', '테마1입니다.',
         'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
 INSERT INTO MEMBER (NAME, EMAIL, PASSWORD, ROLE)
-VALUES ('어드민', 'admin@email.com', 'password', 'ADMIN');
+VALUES ('어드민', 'admin@email.com', 'password', 'ADMIN'),
+       ('유저', 'user@email.com', 'password', 'USER');
+
+INSERT INTO reservation (date, time_id, theme_id, member_id)
+VALUES ('2025-01-15', 1, 1, 1),
+       ('2025-01-15', 3, 2, 2),
+       ('2025-01-16', 2, 3, 1),
+       ('2025-01-16', 4, 1, 2),
+       ('2025-02-01', 5, 2, 1),
+       ('2025-02-01', 6, 3, 2),
+       ('2025-02-02', 1, 1, 1),
+       ('2025-03-01', 2, 2, 2),
+       ('2025-03-01', 3, 3, 1),
+       ('2025-03-02', 4, 1, 2);
