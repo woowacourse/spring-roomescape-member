d5c95ea3ea8dc506b9e2920ed0d2be090b68835e
wogus <ind07152@naver.com>
2025-05-10 18:25:11 +0900

    test: memberService 모든 회원 조회 테스트

--- src/test/java/roomescape/member/service/MemberServiceTest.java
+++ src/test/java/roomescape/member/service/MemberServiceTest.java
@@ -4,11 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import roomescape.member.domain.Member;
 import roomescape.member.domain.MemberRepository;
 import roomescape.member.domain.Role;
+import roomescape.member.dto.MemberResponse;
 import roomescape.member.dto.SignupRequest;
 import roomescape.member.dto.SignupResponse;
 
@@ -40,5 +42,19 @@ class MemberServiceTest {
         SignupResponse response = memberService.createUser(signupRequest);
         // then
         assertThat(response).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("모든 회원 조회 테스트")
+    void findAllMember() {
+        // given
+        Member member1 = Member.createWithoutId("a", "a@naver.com", "a", Role.USER);
+        Member member2 = Member.createWithoutId("a", "b@naver.com", "a", Role.USER);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        // when
+        List<MemberResponse> allMember = memberService.findAllMember();
+        // then
+        assertThat(allMember).hasSize(2);
     }
 }
