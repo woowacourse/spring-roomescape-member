e90f09c94336e0d6ea243fec224bb4a1a0001596
kimdev <kim__dev@hufs.ac.kr>
2025-05-11 02:31:49 +0900

    feat: 커스텀 예외 처리 구현

--- src/main/java/roomescape/global/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/global/handler/GlobalExceptionHandler.java
@@ -4,12 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import roomescape.auth.exception.InvalidCredentialsException;
 import roomescape.global.dto.response.ErrorResponse;
+import roomescape.global.exception.AlreadyEntityException;
+import roomescape.global.exception.ForbiddenException;
+import roomescape.global.exception.UnauthorizedException;
+import roomescape.member.exception.DuplicateMemberException;
 import roomescape.reservation.exception.AssociatedReservationExistsException;
 import roomescape.reservation.exception.DuplicateReservationException;
 import roomescape.reservation.exception.DuplicateThemeException;
 import roomescape.reservation.exception.DuplicateTimeException;
 import roomescape.global.exception.InvalidInputException;
+import roomescape.reservation.exception.NotCorrectDateTimeException;
 import roomescape.reservation.exception.ThemeNotExistException;
 import roomescape.reservation.exception.TimeNotExistException;
 
@@ -35,16 +41,46 @@ public class GlobalExceptionHandler {
         return generateErrorResponse(exception.getMessage());
     }
 
+    @ExceptionHandler(value = NotCorrectDateTimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleNotcorrectDateTimeExist(NotCorrectDateTimeException exception) {
+        return generateErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(value = InvalidCredentialsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleInvalidCredentials(InvalidCredentialsException exception) {
+        return generateErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(value = UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handleUnauthorized(UnauthorizedException exception) {
+        return generateErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(value = ForbiddenException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponse handleForbidden(ForbiddenException exception) {
+        return generateErrorResponse("접근 권한이 없다.");
+    }
+
     @ExceptionHandler(value = AssociatedReservationExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleAssociateReservationExists(AssociatedReservationExistsException exception) {
         return generateErrorResponse(exception.getMessage());
     }
 
-    @ExceptionHandler(value = {DuplicateReservationException.class, DuplicateThemeException.class, DuplicateTimeException.class})
+    @ExceptionHandler(value = {DuplicateReservationException.class, DuplicateThemeException.class, DuplicateTimeException.class, DuplicateMemberException.class})
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleDuplicateEntity(RuntimeException exception) {
         return generateErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(value = AlreadyEntityException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleAlreadyEntity(AlreadyEntityException exception) {
+        return generateErrorResponse("서버 오류 발생. 관리자에게 문의하시오.");
     }
 
     private ErrorResponse generateErrorResponse(String message) {
