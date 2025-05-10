02ab90f682bd30f869e787b2947942c65121964f
RaZel713 <razel7130@gmail.com>
2025-05-10 21:17:33 +0900

    test: 접근 권한 제어 기능 구현에 따른 도메인 페이지 테스트 수정

--- src/test/java/roomescape/view/AdminPageControllerTest.java
+++ src/test/java/roomescape/view/AdminPageControllerTest.java
@@ -1,6 +1,9 @@
 package roomescape.view;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,10 +13,27 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class AdminPageControllerTest {
 
+    private String token;
+
+    @BeforeEach
+    void setUp() {
+        token = getAdminToken();
+    }
+
+    private String getAdminToken() {
+        return RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(Map.of("email", "admin@email.com", "password", "password"))
+                .when().post("/login")
+                .then().statusCode(200)
+                .extract().response().getDetailedCookies().getValue("token");
+    }
+
     @DisplayName("어드민 페이지를 불러 온다")
     @Test
     void getAdminPageTest() {
         RestAssured.given().log().all()
+                .cookie("token", token)
                 .when().get("/admin")
                 .then().log().all()
                 .statusCode(200);
@@ -23,6 +43,7 @@ class AdminPageControllerTest {
     @Test
     void getAdminReservationPageTest() {
         RestAssured.given().log().all()
+                .cookie("token", token)
                 .when().get("/admin/reservation")
                 .then().log().all()
                 .statusCode(200);
@@ -32,16 +53,17 @@ class AdminPageControllerTest {
     @Test
     void getAdminTimePageTest() {
         RestAssured.given().log().all()
+                .cookie("token", token)
                 .when().get("/admin/time")
                 .then().log().all()
                 .statusCode(200);
     }
 
-
     @DisplayName("어드민 방 테마 관리 페이지를 불러 온다")
     @Test
     void getAdminThemePageTest() {
         RestAssured.given().log().all()
+                .cookie("token", token)
                 .when().get("/admin/theme")
                 .then().log().all()
                 .statusCode(200);
