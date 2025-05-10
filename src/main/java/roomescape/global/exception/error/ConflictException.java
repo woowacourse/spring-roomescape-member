3c8941a73da10f7c4ca3ffdb7686e863d51513f4
junhaa <2171326@hansung.ac.kr>
2025-05-11 00:48:04 +0900

    feat: 커스텀 예외 추가 및 예외 핸들러 변경

---
+++ src/main/java/roomescape/global/exception/error/ConflictException.java
@@ -0,0 +1,8 @@
+package roomescape.global.exception.error;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/roomescape/global/exception/error/ForbiddenException.java
@@ -0,0 +1,9 @@
+package roomescape.global.exception.error;
+
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+
+}
