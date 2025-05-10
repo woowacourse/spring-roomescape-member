dea6c6952992797ef46e431b46b6297ce0340c44
민경윤 <ruddbs3308@naver.com>
2025-05-10 11:13:41 +0900

    feat: 로그인 폼이 있는 페이지를 응답 기능 구현

--- src/main/java/roomescape/presentation/controller/page/UserPageController.java
+++ src/main/java/roomescape/presentation/controller/page/UserPageController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserPageController {
 
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
     @GetMapping("/reservation")
     public String reservation() {
         return "reservation";
