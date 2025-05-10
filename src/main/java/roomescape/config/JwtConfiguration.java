6712ace457a09c2496e63fa8a8600f884413a506
ryan-dia <cjfdnjs158@naver.com>
2025-05-10 16:34:00 +0900

    style: 네이밍 변경(JwtConfig -> JwtConfiguration)

--- src/main/java/roomescape/config/JwtConfiguration.java
+++ src/main/java/roomescape/config/JwtConfiguration.java
@@ -1,9 +1,10 @@
-package roomescape.auth;
+package roomescape.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import roomescape.auth.infrastructure.JwtProperties;
 
 @Configuration
 @EnableConfigurationProperties(JwtProperties.class)
-public class JwtConfig {
+public class JwtConfiguration {
 }
