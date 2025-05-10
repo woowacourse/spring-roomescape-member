5f508070f2da186d8845eeef892443f5ae3e4cd7
CheChe903 <cheche903@naver.com>
2025-05-10 17:43:27 +0900

    docs(LoginController): 토큰 생성 및 토큰으로 멤버 조회 api 명세 추가

--- src/main/java/roomescape/controller/LoginController.java
+++ src/main/java/roomescape/controller/LoginController.java
@@ -1,5 +1,9 @@
 package roomescape.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpCookie;
@@ -30,6 +34,16 @@ public class LoginController {
     }
 
     @PostMapping()
+    @Operation(
+            summary = "토큰 생성",
+            description = "토큰을 생성합니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "토큰 생성 성공"
+                    )
+            }
+    )
     public ResponseEntity<Void> create(
             @RequestBody @Valid final TokenRequest tokenRequest) {
         final TokenResponse tokenResponse = memberService.createToken(tokenRequest);
@@ -44,6 +58,18 @@ public class LoginController {
     }
 
     @GetMapping("/check")
+    @Operation(
+            summary = "토큰에 따른 멤버 조회",
+            description = "토큰에 따른 멤버 정보를 조회합니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "멤버 목록 조회 성공",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = MemberResponse.class))
+                    )
+            }
+    )
     public ResponseEntity<MemberResponse> findInfo(final HttpServletRequest request) {
         final String token = authorizationExtractor.extract(request);
         final MemberResponse memberResponse = memberService.findByToken(token);
