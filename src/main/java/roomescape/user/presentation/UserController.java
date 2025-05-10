3f167c5804a1c81f71ffecfdde3947f469a6f4a8
rladmstn <oz33177351@gmail.com>
2025-05-10 11:41:59 +0900

    feat: 로그아웃 기능 추가

--- src/main/java/roomescape/user/presentation/UserController.java
+++ src/main/java/roomescape/user/presentation/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.user.application.service.AuthService;
 import roomescape.user.domain.User;
@@ -15,8 +14,9 @@ import roomescape.user.presentation.dto.TokenResponse;
 import roomescape.user.presentation.dto.UserResponse;
 
 @RestController
-@RequestMapping("/login")
 public class UserController {
+
+    private static final String SET_COOKIE_KEY = "token";
 
     private final AuthService authService;
 
@@ -24,19 +24,26 @@ public class UserController {
         this.authService = authService;
     }
 
-    @PostMapping
+    @PostMapping("/login")
     public ResponseEntity<Void> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
         TokenResponse token = authService.login(loginRequest);
-        Cookie cookie = new Cookie("token", token.accessToken());
+        Cookie cookie = new Cookie(SET_COOKIE_KEY, token.accessToken());
         cookie.setHttpOnly(true);
         cookie.setPath("/");
         response.addCookie(cookie);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/check")
+    @GetMapping("/login/check")
     public ResponseEntity<UserResponse> checkLogin(User user) {
         return ResponseEntity.ok().body(new UserResponse(user.getName()));
     }
 
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletResponse response) {
+        Cookie logoutCookie = new Cookie(SET_COOKIE_KEY, null);
+        logoutCookie.setMaxAge(0);
+        response.addCookie(logoutCookie);
+        return ResponseEntity.ok().build();
+    }
 }
