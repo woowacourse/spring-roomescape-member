805aed274f67990559541bfc0c8f06d8c3107d0e
kjungw <sagakkb@gmail.com>
2025-05-10 12:24:39 +0900

    refactor: 입력 문자 길이 검증 추가

--- src/main/java/roomescape/dto/request/LoginRequest.java
+++ src/main/java/roomescape/dto/request/LoginRequest.java
@@ -1,11 +1,14 @@
 package roomescape.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 
 public record LoginRequest(
         @NotBlank(message = "[ERROR] 이메일은 빈 값이나 공백값을 허용하지 않습니다.")
+        @Length(max = 255, message = "[ERROR] 255자를 초과한 값은 허용하지 않습니다.")
         String email,
         @NotBlank(message = "[ERROR] 비밀번호는 빈 값이나 공백값을 허용하지 않습니다.")
+        @Length(max = 255, message = "[ERROR] 255자를 초과한 값은 허용하지 않습니다.")
         String password
 ) {
 
--- src/main/java/roomescape/dto/request/MemberCreationRequest.java
+++ src/main/java/roomescape/dto/request/MemberCreationRequest.java
@@ -1,18 +1,21 @@
 package roomescape.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 import roomescape.domain.regex.BaseFormat;
 
 public record MemberCreationRequest(
-        @NotNull(message = "비어있는 이메일로는 회원가입이 불가능합니다.")
+        @NotBlank(message = "비어있는 이메일로는 회원가입이 불가능합니다.")
+        @Length(max = 255, message = "[ERROR] 255자를 초과한 값은 허용하지 않습니다.")
         @Pattern(regexp = BaseFormat.EMAIL, message = "이메일 형식을 제대로 입력해주세요.")
         String email,
         @NotBlank(message = "비어있는 비밀번호로는 회원가입이 불가능합니다.")
+        @Length(max = 255, message = "[ERROR] 255자를 초과한 값은 허용하지 않습니다.")
         @Pattern(regexp = BaseFormat.PASSWORD, message = "영문자, 숫자, 특수기호를 포함한 8자리이상의 비밀번호를 입력해주세요.")
         String password,
         @NotBlank(message = "비어있는 이름로는 회원가입이 불가능합니다.")
+        @Length(max = 255, message = "[ERROR] 255자를 초과한 값은 허용하지 않습니다.")
         String name
 ) {
 
