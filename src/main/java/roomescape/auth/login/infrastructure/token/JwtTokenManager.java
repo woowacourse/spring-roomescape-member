233a808e750a3dd946626d54912d1677fb3062a3
GiSeok Kang <ime.rokz@gmail.com>
2025-05-10 21:33:25 +0900

    refactor: JWT 개인키를 외부 설정으로 변경
    - application.properties에서 @Value를 통해 주입하도록 변경

--- src/main/java/roomescape/auth/login/infrastructure/token/JwtTokenManager.java
+++ src/main/java/roomescape/auth/login/infrastructure/token/JwtTokenManager.java
@@ -3,25 +3,25 @@ package roomescape.auth.login.infrastructure.token;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Jwts.SIG;
 import java.util.Date;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenManager {
 
-    private final static String SECRET_KEY = "92b355b62d70a2b55fbc27f11023a53203415d617022a4f35a4ca1a23679eafe17057643b931b43b72392674338c832bf0e2eed21cf8e80dec11db5543860f0f";
     private final static int EXPIRATION_TIME = 60 * 30;
 
-    public static String crateToken(final Long id, final String role) {
-        SecretKey key = new SecretKeySpec(SECRET_KEY.getBytes(), "HmacSHA256");
+    @Value("${secret.key}")
+    private static String SECRET_KEY;
 
+    public static String crateToken(final Long id, final String role) {
         return Jwts.builder()
                 .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .subject(id.toString())
                 .claim("role", role)
                 .issuedAt(new Date())
-                .signWith(key, SIG.HS256)
+                .signWith(new SecretKeySpec(SECRET_KEY.getBytes(), "HmacSHA256"), SIG.HS256)
                 .compact();
     }
 
