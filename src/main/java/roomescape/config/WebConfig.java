2ecf2a795bee9cb3d819d945532a8f30fde1d6f6
songsunkook <songsunkook@gmail.com>
2025-05-10 15:29:13 +0900

    fix: Admin interceptor가 하위 범위까지 잡도록 수정

--- src/main/java/roomescape/config/WebConfig.java
+++ src/main/java/roomescape/config/WebConfig.java
@@ -25,6 +25,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(adminInterceptor).addPathPatterns("/admin");
+        registry.addInterceptor(adminInterceptor).addPathPatterns("/admin/**");
     }
 }
--- src/main/java/roomescape/controller/view/AdminViewController.java
+++ src/main/java/roomescape/controller/view/AdminViewController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// TODO: admin이 아닌 경우 접속 못하도록 막기
 @Controller
 @RequestMapping("/admin")
 public class AdminViewController {
