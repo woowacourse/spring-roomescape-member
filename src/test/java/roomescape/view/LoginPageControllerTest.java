cab8231faa8543a806416ceea4d0329f3d8e8d94
RaZel713 <razel7130@gmail.com>
2025-05-10 22:01:12 +0900

    test: 로그인 페이지 컨트롤러 테스트 추가

---
+++ src/test/java/roomescape/view/LoginPageControllerTest.java
@@ -0,0 +1,21 @@
+package roomescape.view;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class LoginPageControllerTest {
+
+    @DisplayName("로그인 페이지를 불러 온다")
+    @Test
+    void getLoginPageTest() {
+        RestAssured.given().log().all()
+                .when().get("/login")
+                .then().log().all()
+                .statusCode(200);
+    }
+}
