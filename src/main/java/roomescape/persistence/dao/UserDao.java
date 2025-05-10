71e7d5e744e21838e952a89e368a918841c9699d
민경윤 <ruddbs3308@naver.com>
2025-05-10 16:46:44 +0900

    feat: 데이터베이스에서 principal로 유저를 조회하는 기능 구현

--- src/main/java/roomescape/persistence/dao/UserDao.java
+++ src/main/java/roomescape/persistence/dao/UserDao.java
@@ -1,10 +1,8 @@
 package roomescape.persistence.dao;
 
 import java.util.Optional;
-import org.springframework.stereotype.Repository;
 import roomescape.business.domain.User;
 
-@Repository
 public interface UserDao {
 
     Optional<User> findByPrincipal(String principal);
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USERS (name, email, password) values ('호떡', '호떡@wooteco.com', 'qwe123');
+INSERT INTO USERS (name, email, password, role) values ('호떡', '호떡@wooteco.com', 'qwe123', 'USER');
 
 INSERT INTO THEME (name, description, thumbnail) values ('레벨2 탈출', '우테코 레벨2를 탈출하는 내용입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 INSERT INTO THEME (name, description, thumbnail) values ('레벨3 탈출', '우테코 레벨3를 탈출하는 내용입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
