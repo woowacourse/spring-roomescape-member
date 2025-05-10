7c6560138d037bfa1366b73fac1ddbbc1f6c893a
egaeng09 <egaeng09@gmail.com>
2025-05-10 17:55:58 +0900

    feat: jwt 발급 기능 구현

---
+++ src/main/java/roomescape/member/service/auth/JwtTokenProvider.java
@@ -0,0 +1,21 @@
+package roomescape.member.service.auth;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.*;
+import roomescape.member.domain.Member;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.key}")
+    private String SECRET_KEY;
+
+    public String generateToken(Member member) {
+        return  Jwts.builder()
+                .setSubject(member.getId().toString())
+                .claim("name", member.getName().getValue())
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
