199444e7e7ac3fe18d4abd9b315eb7abba321f1b
LJM\eueo8 <eueo8259@gmail.com>
2025-05-10 13:19:23 +0900

    chore: member 테이블에 role 컬럼 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -34,5 +34,6 @@ CREATE TABLE member
     email    VARCHAR(255) NOT NULL ,
     name     VARCHAR(255) NOT NULL,
     password     VARCHAR(255) NOT NULL,
+    role     VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
