7f03d7dddfc589a8908a5a75b50f92bff1953b85
wogus <ind07152@naver.com>
2025-05-10 18:54:55 +0900

    chore: 어드민 유저 추가

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -8,6 +8,7 @@ INSERT INTO reservation_time (start_at) VALUES ('12:00');
 
 INSERT INTO member (name, email, password, role) VALUES ('코기','ind07152@naver.com','asd','user');
 INSERT INTO member (name, email, password, role) VALUES ('율무','ind07162@naver.com','asd','user');
+INSERT INTO member (name, email, password, role) VALUES ('ADMIN','admin@naver.com','1234','admin');
 
 INSERT INTO reservation (member_id, date, time_id, theme_id) VALUES (1, '2025-04-28', 1, 1);
 INSERT INTO reservation (member_id, date, time_id, theme_id) VALUES (1, '2025-04-28', 2, 1);
