c755e7e1f15f535acf3c7ad09c5fc8ea7fc96554
threepebbles <threepebbles@naver.com>
2025-05-11 01:44:23 +0900

    chore: LocalTime 응답 형식 일관되게 수정

--- src/main/java/roomescape/reservation/ui/dto/response/AvailableReservationTimeResponse.java
+++ src/main/java/roomescape/reservation/ui/dto/response/AvailableReservationTimeResponse.java
@@ -1,9 +1,11 @@
 package roomescape.reservation.ui.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 
 public record AvailableReservationTimeResponse(
         Long timeId,
+        @JsonFormat(pattern = "HH:mm")
         LocalTime startAt,
         boolean alreadyBooked
 ) {
