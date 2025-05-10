d5ab79b5130e2f084554badc88c1b52556cd2844
soeun2537 <soeun2537@gmail.com>
2025-05-11 07:54:20 +0900

    feat: schema.sql 업데이트

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,10 +17,10 @@ CREATE TABLE theme
 CREATE TABLE reservation
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
-    name     VARCHAR(255) NOT NULL,
     date     DATE         NOT NULL,
     time_id  BIGINT,
     theme_id BIGINT,
+    member_id BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
