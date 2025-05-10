e7cb2d5f758fea6dd6147847633d172178367f9b
kimdev <kim__dev@hufs.ac.kr>
2025-05-11 00:44:16 +0900

    feat: 로그인 요청 Dto에서 입력이 없는 경우의 검증 로직 추가

--- src/main/java/roomescape/auth/dto/request/LoginRequest.java
+++ src/main/java/roomescape/auth/dto/request/LoginRequest.java
@@ -1,4 +1,19 @@
 package roomescape.auth.dto.request;
 
+import roomescape.global.exception.InvalidInputException;
+
 public record LoginRequest(String email, String password) {
+
+    public LoginRequest {
+        validateNull(email, password);
+    }
+
+    private void validateNull(String email, String password) {
+        if (email == null) {
+            throw new InvalidInputException("이메일이 입력되지 않았다.");
+        }
+        if (password == null) {
+            throw new InvalidInputException("패스워드가 입력되지 않았다.");
+        }
+    }
 }
