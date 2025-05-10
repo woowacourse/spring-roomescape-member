eab57fff5b558fac351a4e8880b6ea3a3139dca4
Dompoo <dlckdrms0517@gmail.com>
2025-05-11 00:53:49 +0900

    refactor: StartTime VO 추가하여 책임 분리

--- src/main/java/roomescape/business/model/entity/ReservationTime.java
+++ src/main/java/roomescape/business/model/entity/ReservationTime.java
@@ -1,49 +1,38 @@
 package roomescape.business.model.entity;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import roomescape.business.model.vo.Id;
-import roomescape.exception.business.InvalidCreateArgumentException;
+import roomescape.business.model.vo.StartTime;
 
 import java.time.LocalTime;
 
 @ToString
 @EqualsAndHashCode(exclude = "id")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReservationTime {
 
-    private static final LocalTime START_TIME = LocalTime.of(10, 0);
-    private static final LocalTime END_TIME = LocalTime.of(23, 0);
     private static final int MINUTE_INTERVAL = 30;
 
     private final Id id;
-    private final LocalTime startAt;
+    private final StartTime startTime;
 
-    private ReservationTime(final Id id, final LocalTime startAt) {
-        validateTimeAvailable(startAt);
-        this.id = id;
-        this.startAt = startAt;
+    public static ReservationTime create(final LocalTime startTime) {
+        return new ReservationTime(Id.issue(), new StartTime(startTime));
     }
 
-    private static void validateTimeAvailable(final LocalTime time) {
-        if (time.isBefore(START_TIME) || time.isAfter(END_TIME)) {
-            throw new InvalidCreateArgumentException("예약은 10시~23시로만 가능합니다.");
-        }
-    }
-
-    public static ReservationTime create(final LocalTime startAt) {
-        return new ReservationTime(Id.issue(), startAt);
-    }
-
-    public static ReservationTime restore(final String id, final LocalTime startAt) {
-        return new ReservationTime(Id.create(id), startAt);
+    public static ReservationTime restore(final String id, final LocalTime startTime) {
+        return new ReservationTime(Id.create(id), new StartTime(startTime));
     }
 
     public LocalTime startInterval() {
-        return startAt.minusMinutes(MINUTE_INTERVAL);
+        return startTime.minusMinutes(MINUTE_INTERVAL);
     }
 
     public LocalTime endInterval() {
-        return startAt.plusMinutes(MINUTE_INTERVAL);
+        return startTime.plusMinutes(MINUTE_INTERVAL);
     }
 
     public String id() {
@@ -51,6 +40,6 @@ public class ReservationTime {
     }
 
     public LocalTime startAt() {
-        return startAt;
+        return startTime.value();
     }
 }
---
+++ src/main/java/roomescape/business/model/vo/StartTime.java
@@ -0,0 +1,30 @@
+package roomescape.business.model.vo;
+
+import roomescape.exception.business.InvalidCreateArgumentException;
+
+import java.time.LocalTime;
+
+public record StartTime(
+        LocalTime value
+) {
+    private static final LocalTime START_TIME = LocalTime.of(10, 0);
+    private static final LocalTime END_TIME = LocalTime.of(23, 0);
+
+    public StartTime {
+        validateAvailableTime(value);
+    }
+
+    private static void validateAvailableTime(final LocalTime time) {
+        if (time.isBefore(START_TIME) || time.isAfter(END_TIME)) {
+            throw new InvalidCreateArgumentException("시작 시간은 10~23시만 가능합니다.");
+        }
+    }
+
+    public LocalTime plusMinutes(int minutes) {
+        return value.plusMinutes(minutes);
+    }
+
+    public LocalTime minusMinutes(int minutes) {
+        return plusMinutes(-minutes);
+    }
+}
