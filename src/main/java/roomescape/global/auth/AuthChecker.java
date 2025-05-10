b4b2d7b910d3fe77324d143fdd26ceccd78bd09a
sonjh919 <kingjh1125@gmail.com>
2025-05-10 15:27:52 +0900

    refactor: admin 권한 수정

--- src/main/java/roomescape/global/auth/AuthChecker.java
+++ src/main/java/roomescape/global/auth/AuthChecker.java
@@ -6,6 +6,10 @@ import roomescape.member.domain.Role;
 public class AuthChecker {
     public static boolean checkAuthorization(Method method, Role currentUserRole) {
         if (method.isAnnotationPresent(Auth.class)) {
+            if(currentUserRole==Role.ADMIN){
+                return true;
+            }
+
             Auth auth = method.getAnnotation(Auth.class);
             Role requiredRole = auth.value();
             return currentUserRole == requiredRole;
