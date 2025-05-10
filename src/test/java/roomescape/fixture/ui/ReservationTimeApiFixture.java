2a5315152be0ec8498ae4b25a9f2930ec101516c
threepebbles <threepebbles@naver.com>
2025-05-10 21:01:12 +0900

    test: 예약 시간 API 테스트 추가

---
+++ src/test/java/roomescape/fixture/ui/ReservationTimeApiFixture.java
@@ -0,0 +1,21 @@
+package roomescape.fixture.ui;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReservationTimeApiFixture {
+
+    public static Map<String, String> reservationTimeParams1() {
+        final Map<String, String> params = new HashMap<>();
+        params.put("startAt", "10:00");
+
+        return params;
+    }
+
+    public static Map<String, String> reservationTimeParams2() {
+        final Map<String, String> params = new HashMap<>();
+        params.put("startAt", "11:00");
+
+        return params;
+    }
+}
--- src/test/java/roomescape/reservation/ui/ReservationTimeRestControllerTest.java
+++ src/test/java/roomescape/reservation/ui/ReservationTimeRestControllerTest.java
@@ -1,91 +1,105 @@
-//package roomescape.reservation.ui;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.hamcrest.Matchers.is;
-//
-//import io.restassured.RestAssured;
-//import io.restassured.http.ContentType;
-//import java.lang.reflect.Field;
-//import java.util.HashMap;
-//import java.util.Map;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.test.annotation.DirtiesContext;
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-//class ReservationTimeRestControllerTest {
-//
-//    @Autowired
-//    private ReservationTimeRestController reservationTimeRestController;
-//
-//    @Test
-//    void 예약_가능한_시간을_목록에_추가한다() {
-//        final Map<String, String> params = new HashMap<>();
-//        params.put("startAt", "10:00");
-//
-//        RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .body(params)
-//                .when().post("/times")
-//                .then().log().all()
-//                .statusCode(HttpStatus.CREATED.value());
-//    }
-//
-//    @Test
-//    void 예약_가능한_시간을_목록에서_조회한다() {
-//        final Map<String, String> params = new HashMap<>();
-//        params.put("startAt", "10:00");
-//
-//        RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .body(params)
-//                .when().post("/times")
-//                .then().log().all()
-//                .statusCode(HttpStatus.CREATED.value());
-//
-//        RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .when().get("/times")
-//                .then().log().all()
-//                .statusCode(HttpStatus.OK.value())
-//                .body("size()", is(1));
-//    }
-//
-//    @Test
-//    void 예약_가능한_시간을_목록에서_삭제한다() {
-//        final Map<String, String> params = new HashMap<>();
-//        params.put("startAt", "10:00");
-//
-//        final Integer id = RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .body(params)
-//                .when().post("/times")
-//                .then().log().all()
-//                .statusCode(HttpStatus.CREATED.value())
-//                .extract().path("id");
-//
-//        RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .when().delete("/times/{id}", id)
-//                .then().log().all()
-//                .statusCode(HttpStatus.NO_CONTENT.value());
-//    }
-//
-//    @Test
-//    void 컨트롤러는_JdbcTemplate_타입의_필드를_갖고_있지_않다() {
-//        boolean isJdbcTemplateInjected = false;
-//
-//        for (Field field : reservationTimeRestController.getClass().getDeclaredFields()) {
-//            if (field.getType().equals(JdbcTemplate.class)) {
-//                isJdbcTemplateInjected = true;
-//                break;
-//            }
-//        }
-//
-//        assertThat(isJdbcTemplateInjected).isFalse();
-//    }
-//}
+package roomescape.reservation.ui;
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+import roomescape.fixture.ui.LoginApiFixture;
+import roomescape.fixture.ui.MemberApiFixture;
+import roomescape.fixture.ui.ReservationTimeApiFixture;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class ReservationTimeRestControllerTest {
+
+    @Test
+    void 관리자_권한으로_예약_시간을_추가한다() {
+        final Map<String, String> cookies = LoginApiFixture.adminLoginAndGetCookies();
+
+        final Map<String, String> reservationTimeParams = ReservationTimeApiFixture.reservationTimeParams1();
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .body(reservationTimeParams)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    void 회원_권한으로_예약_시간을_추가한다() {
+        final Map<String, String> singUpParams = MemberApiFixture.memberSignUpParams();
+        MemberApiFixture.signUp(singUpParams);
+        final Map<String, String> cookies = LoginApiFixture.memberLoginAndGetCookies(singUpParams);
+
+        final Map<String, String> reservationTimeParams = ReservationTimeApiFixture.reservationTimeParams1();
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .body(reservationTimeParams)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    void 전체_예약_시간을_조회한다() {
+        final Map<String, String> cookies = LoginApiFixture.adminLoginAndGetCookies();
+
+        final Map<String, String> reservationTimeParams = ReservationTimeApiFixture.reservationTimeParams1();
+        final Map<String, String> reservationTimeParams2 = ReservationTimeApiFixture.reservationTimeParams2();
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .body(reservationTimeParams)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .body(reservationTimeParams2)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .when().get("/times")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body("size()", is(2));
+    }
+
+    @Test
+    void 관리자_권한으로_예약_시간을_삭제한다() {
+        final Map<String, String> cookies = LoginApiFixture.adminLoginAndGetCookies();
+
+        final Map<String, String> reservationTimeParams = ReservationTimeApiFixture.reservationTimeParams1();
+
+        final Integer id = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .body(reservationTimeParams)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract().path("id");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .when().delete("/times/{id}", id)
+                .then().log().all()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+}
