2da4fa6eee9712ae0c0735984863094ba9442170
GiSeok Kang <ime.rokz@gmail.com>
2025-05-10 17:15:55 +0900

    refactor: reservation 스키마 수정
    - name 컬럼 제거
    - member_id 컬럼 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE reservation_time
 (
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
     start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -10,20 +10,8 @@ CREATE TABLE theme
     id          BIGINT       NOT NULL AUTO_INCREMENT,
     name        VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    thumbnail VARCHAR(255) NOT NULL,
+    thumbnail   VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
-);
-
-CREATE TABLE reservation
-(
-    id   BIGINT       NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    date VARCHAR(255) NOT NULL,
-    time_id BIGINT,
-    theme_id BIGINT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
 
 CREATE TABLE member
@@ -33,6 +21,19 @@ CREATE TABLE member
     email    VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE reservation
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    date        VARCHAR(255) NOT NULL,
+    time_id     BIGINT,
+    theme_id    BIGINT,
+    member_id   BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    FOREIGN KEY (member_id) REFERENCES member (id)
 );
 
 CREATE TABLE admin
