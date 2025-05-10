3dfc222317aa3965480307c96c5e82b54a19c8f0
saera <yook.saera@gmail.com>
2025-05-11 00:15:56 +0900

    feat: 로그아웃 API 구현

---
+++ src/main/java/roomescape/auth/TokenLogoutController.java
@@ -0,0 +1,20 @@
+package roomescape.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TokenLogoutController {
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> login() {
+        return ResponseEntity
+                .status(HttpStatus.SEE_OTHER)
+                .header("Location", "/")
+                // https://stackoverflow.com/a/5285982
+                .header("Set-Cookie", "token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT")
+                .build();
+    }
+}
---
+++ src/test/java/roomescape/auth/TokenLogoutControllerIntTest.java
@@ -0,0 +1,44 @@
+package roomescape.auth;
+
+import static roomescape.testFixture.Fixture.MEMBER1;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import roomescape.application.dto.MemberDto;
+import roomescape.infrastructure.JwtTokenProvider;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TokenLogoutControllerIntTest {
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void request_logout() {
+        String tokenForAdmin = jwtTokenProvider.createToken(MemberDto.from(MEMBER1));
+
+        RestAssured.given()
+                .redirects()
+                .follow(false)
+                .log().all()
+                .cookie("token", tokenForAdmin)
+                .contentType(ContentType.JSON)
+                .when().post("/logout")
+                .then().log().all()
+                .statusCode(303)
+                .header("Location", "/")
+                .header("Set-Cookie", "token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT");
+    }
+}
