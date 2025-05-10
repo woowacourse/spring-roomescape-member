ad87bdaa59e471bacb86555f6f7c0e3e07346f00
CheChe903 <cheche903@naver.com>
2025-05-10 17:44:15 +0900

    feat(MemberService): 트랜잭션 및 findAll 메서드 추가

--- src/main/java/roomescape/service/MemberService.java
+++ src/main/java/roomescape/service/MemberService.java
@@ -1,8 +1,10 @@
 package roomescape.service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.dao.meber.MemberDao;
 import roomescape.domain.Member;
 import roomescape.dto.request.TokenRequest;
@@ -26,6 +28,7 @@ public class MemberService {
         return new TokenResponse(accessToken);
     }
 
+    @Transactional(readOnly = true)
     public MemberResponse findByToken(final String token) {
         final String email = jwtTokenProvider.getPayload(token);
         final Optional<Member> member = memberDao.findByEmail(email);
@@ -35,11 +38,19 @@ public class MemberService {
         return MemberResponse.from(member.get());
     }
 
+    @Transactional(readOnly = true)
     public Member findById(final Long id) {
         final Optional<Member> member = memberDao.findById(id);
         if (member.isEmpty()) {
             throw new NoSuchElementException("멤버 정보를 찾을 수 없습니다.");
         }
         return member.get();
+    }
+
+    @Transactional(readOnly = true)
+    public List<MemberResponse> findAll() {
+        return memberDao.findAll().stream()
+                .map(MemberResponse::from)
+                .toList();
     }
 }
