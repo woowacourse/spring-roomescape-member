c1d68a9151381bc47260d7c8c38dfbefe116d4aa
bowook <mbosx316@naver.com>
2025-05-12 02:03:09 +0900

    chore : test용 yml파일 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO members (name, email, password, role)
 VALUES ('East', 'east@gmail.com', '1234', 'ADMIN');
 
 INSERT INTO members (name, email, password, role)
-VALUES ('WooGa', 'bowook316@gmail.com', '1234', 'USER');
+VALUES ('WooGa', 'wooga@gmail.com', '1234', 'USER');
 
 INSERT INTO reservation_times (start_at)
 VALUES ('10:00:00'),
