ccccaa68250346f4f58408da91cd622be1c68ee5
Minuring <mw9505@gmail.com>
2025-05-11 16:09:23 +0900

    refactor: 유저 전체 조회를 관리자 컨트롤러로 이동

--- src/main/java/roomescape/presentation/api/AdminApiController.java
+++ src/main/java/roomescape/presentation/api/AdminApiController.java
@@ -2,8 +2,10 @@ package roomescape.presentation.api;
 
 import jakarta.validation.Valid;
 import java.net.URI;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import roomescape.application.ReservationService;
 import roomescape.application.UserService;
 import roomescape.presentation.request.CreateReservationAdminRequest;
 import roomescape.presentation.response.ReservationResponse;
+import roomescape.presentation.response.UserResponse;
 
 @Controller
 @RequestMapping("/admin")
@@ -30,5 +33,12 @@ public class AdminApiController {
         var reservation = reservationService.reserve(user, request.date(), request.timeId(), request.themeId());
         var response = ReservationResponse.from(reservation);
         return ResponseEntity.created(URI.create("reservations/" + reservation.id())).body(response);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        var users = userService.findAllUsers();
+        var response = UserResponse.from(users);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/roomescape/presentation/api/UserApiController.java
+++ src/main/java/roomescape/presentation/api/UserApiController.java
@@ -1,17 +1,12 @@
 package roomescape.presentation.api;
 
 import jakarta.validation.Valid;
-import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import roomescape.application.AuthorizationException;
 import roomescape.application.UserService;
-import roomescape.domain.User;
-import roomescape.presentation.Authenticated;
 import roomescape.presentation.request.SignupRequest;
 import roomescape.presentation.response.UserResponse;
 
@@ -29,17 +24,6 @@ public class UserApiController {
     public ResponseEntity<UserResponse> register(@RequestBody @Valid final SignupRequest request) {
         var user = userService.register(request.email(), request.password(), request.name());
         var response = UserResponse.from(user);
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<UserResponse>> getAllUsers(@Authenticated final User requestedUser) {
-        if (!requestedUser.isAdmin()) {
-            throw new AuthorizationException("관리자만 접근 가능합니다.");
-        }
-
-        var users = userService.findAllUsers();
-        var response = UserResponse.from(users);
         return ResponseEntity.ok(response);
     }
 }
