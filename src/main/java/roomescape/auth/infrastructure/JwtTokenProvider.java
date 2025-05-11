7fdafe8e199ce549bfe0a2de3941ecae3ec3822a
threepebbles <threepebbles@naver.com>
2025-05-12 00:35:55 +0900

    refactor: JWT 파싱 예외 구체적으로 분리 (학습용)

--- src/main/java/roomescape/auth/infrastructure/JwtTokenProvider.java
+++ src/main/java/roomescape/auth/infrastructure/JwtTokenProvider.java
@@ -4,7 +4,10 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.annotation.PostConstruct;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
@@ -96,11 +99,21 @@ public class JwtTokenProvider implements AuthTokenProvider {
                     .build()
                     .parseSignedClaims(token);
             return true;
+        } catch (MalformedJwtException e) {
+            // Header.Payload.Signature 3개 파트로 나누어지지 않은 경우, Base64 디코딩이 불가능한 경우, JSON 형태가 아닌 경우
+            return false;
         } catch (ExpiredJwtException e) {
-            // TODO: 만료된 토큰에 대한 처리
+            // 토큰이 만료된 경우
+            return false;
+        } catch (UnsupportedJwtException e) {
+            // 지원하지 않는 암호화 방식을 사용한 경우 ("alg" 필드로 검증)
+            return false;
+        }
+        catch (SignatureException e) {
+            // 서명이 올바르지 않은 경우(올바른 SecretKey로 서명되지 않은 경우)
             return false;
         } catch (JwtException e) {
-            // TODO: 형식이 잘못된 토큰에 대한 처리
+            // 기타 예외
             return false;
         }
     }
