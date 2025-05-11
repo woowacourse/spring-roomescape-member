0ca486bfad41fac9b715f5a185f6ae0d9de0bf0a
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2025-05-12 02:43:37 +0900

    fix: configurationProperties 설정 변수 삽입 위한 세터추가

--- src/main/java/roomescape/auth/config/JwtProperties.java
+++ src/main/java/roomescape/auth/config/JwtProperties.java
@@ -1,12 +1,14 @@
 package roomescape.auth.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "jwt")
 @Getter
+@Setter
 public class JwtProperties {
 
     private String secretKey;
