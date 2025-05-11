902641689036a625c2f86fb6580aac6dbe329605
bowook <mbosx316@naver.com>
2025-05-11 19:18:16 +0900

    chore : role 컬럼 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
-INSERT INTO members (name, email, password)
-VALUES ('WooGa', 'bowook316@gmail.com', '1234');
+INSERT INTO members (name, email, password, role)
+VALUES ('East', 'east@gmail.com', '1234', 'ADMIN');
+
+INSERT INTO members (name, email, password, role)
+VALUES ('WooGa', 'bowook316@gmail.com', '1234', 'USER');
