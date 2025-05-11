2ab36bd9eadb659e0174ae5058ab33a2095716eb
DEV_WOONG <ywcsuwon@gmail.com>
2025-05-11 12:22:34 +0900

    feat: 쿠키에서 토큰 분리 추가

---
+++ src/main/java/roomescape/auth/util/TokenUtil.java
@@ -0,0 +1,15 @@
+package roomescape.auth.util;
+
+import jakarta.servlet.http.Cookie;
+
+public class TokenUtil {
+
+    public static String extractTokenFromCookie(Cookie[] cookies) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                return cookie.getValue();
+            }
+        }
+        return "";
+    }
+}
