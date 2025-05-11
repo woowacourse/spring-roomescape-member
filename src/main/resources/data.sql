952c9cf10c80ce5255b981c00cf7dfce8363876d
7i.w__k <tizmfns1218@Naver.com>
2025-05-11 18:47:59 +0900

    fix: insert theme Id를 제거하여 auto_increment 동작 수행

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,28 +11,31 @@ values ('14:00');
 
 -- 테마 관련
 
-insert into theme (id, name, description, thumbnail)
-values (1, '테마1', '테마1 입니다.',
+insert into theme (name, description, thumbnail)
+values ('테마1', '테마1 입니다.',
         'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
-insert into theme (id, name, description, thumbnail)
-values (2, '테마2', '테마2 입니다.',
+insert into theme (name, description, thumbnail)
+values ('테마2', '테마2 입니다.',
         'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
-insert into theme (id, name, description, thumbnail)
-values (3, '테마3', '테마3 입니다.',
+insert into theme (name, description, thumbnail)
+values ('테마3', '테마3 입니다.',
         'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
 -- 유저 관련
 
 insert into users (email, name, password, role)
-values ('admin@naver.com', '어드민', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'ROLE_ADMIN');
+values ('admin@naver.com', '어드민',
+        '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'ROLE_ADMIN');
 
-insert into users ( email, name, password, role)
-values ('brown@naver.com', '브라운', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'ROLE_USER');
+insert into users (email, name, password, role)
+values ('brown@naver.com', '브라운',
+        '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'ROLE_USER');
 
-insert into users ( email, name, password, role)
-values ('kkuk@naver.com', '꾹이', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'ROLE_USER');
+insert into users (email, name, password, role)
+values ('kkuk@naver.com', '꾹이', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4',
+        'ROLE_USER');
 
 -- 예약 관련
 
