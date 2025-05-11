5845ffdb8743f7bc99e24bbb6d388a364a290856
Yeongjae Kim <sees77770@gmail.com>
2025-05-11 15:06:24 +0900

    refactor: 요청 dto 검증 메시지 추가

--- src/main/java/roomescape/admin/controller/request/ReserveByAdminRequest.java
+++ src/main/java/roomescape/admin/controller/request/ReserveByAdminRequest.java
@@ -4,9 +4,9 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record ReserveByAdminRequest(
-        @NotNull LocalDate date,
-        @NotNull Long themeId,
-        @NotNull Long timeId,
-        @NotNull Long memberId
+        @NotNull(message = "예약 날짜를 선택해주세요.") LocalDate date,
+        @NotNull(message = "테마를 선택해주세요.") Long themeId,
+        @NotNull(message = "예약 시간을 선택해주세요.") Long timeId,
+        @NotNull(message = "예약할 회원을 선택해주세요.") Long memberId
 ) {
 }
--- src/main/java/roomescape/member/controller/request/SignUpRequest.java
+++ src/main/java/roomescape/member/controller/request/SignUpRequest.java
@@ -3,8 +3,8 @@ package roomescape.member.controller.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record SignUpRequest(
-        @NotBlank String email,
-        @NotBlank String password,
-        @NotBlank String name
+        @NotBlank(message = "이메일을 입력해주세요.") String email,
+        @NotBlank(message = "패스워드를 입력해주세요.") String password,
+        @NotBlank(message = "이름을 입력해주세요.") String name
 ) {
 }
