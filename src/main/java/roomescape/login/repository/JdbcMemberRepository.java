451aa4383deda502658356d815d9b6f896a65f6e
bowook <mbosx316@naver.com>
2025-05-11 19:22:19 +0900

    feat : MemberRepository 기능 추가

--- src/main/java/roomescape/login/repository/JdbcMemberRepository.java
+++ src/main/java/roomescape/login/repository/JdbcMemberRepository.java
@@ -1,10 +1,13 @@
 package roomescape.login.repository;
 
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import roomescape.login.domain.Member;
+import roomescape.login.domain.Role;
 
 @RequiredArgsConstructor
 @Repository
@@ -36,5 +39,63 @@ public class JdbcMemberRepository implements MemberRepository {
         } catch (final EmptyResultDataAccessException e) {
             return Optional.empty();
         }
+    }
+
+    @Override
+    public Optional<Member> findByEmail(final String email) {
+        final String sql = """
+                SELECT id, name, email, password, role
+                FROM members
+                WHERE email = ?
+                """;
+
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new Member(
+                    rs.getLong("id"),
+                    rs.getString("name"),
+                    rs.getString("email"),
+                    rs.getString("password"),
+                    Role.valueOf(rs.getString("role"))
+            ), email));
+        } catch (final EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Member> findById(final Long memberId) {
+        final String sql = """
+                SELECT id, name, email, password, role
+                FROM members
+                WHERE id = ?
+                """;
+
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new Member(
+                    rs.getLong("id"),
+                    rs.getString("name"),
+                    rs.getString("email"),
+                    rs.getString("password"),
+                    Role.valueOf(rs.getString("role"))
+            ), memberId));
+        } catch (final EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Member> findAll() {
+        final String sql = """
+                SELECT id, name, email, password, role
+                FROM members
+                """;
+
+        return jdbcTemplate.query(sql, (rs, rowNum) -> new Member(
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getString("email"),
+                rs.getString("password"),
+                Role.valueOf(rs.getString("role"))
+        ));
     }
 }
