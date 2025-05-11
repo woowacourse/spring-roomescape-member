11208f2f63a09378995075e2a0a2855976cbca1b
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-05-11 21:36:48 +0900

    feat(config, controller) : 관리자 권한 확인 인터셉터 추가

--- src/main/java/roomescape/config/WebMvcConfiguration.java
+++ src/main/java/roomescape/config/WebMvcConfiguration.java
@@ -3,15 +3,26 @@ package roomescape.config;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.controller.AuthenticationPrincipalArgumentResolver;
+import roomescape.controller.CheckAdminInterceptor;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
     private final AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver;
+    private final CheckAdminInterceptor checkAdminInterceptor;
 
-    public WebMvcConfiguration(AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver) {
+    public WebMvcConfiguration(AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver,
+                               CheckAdminInterceptor checkAdminInterceptor) {
         this.authenticationPrincipalArgumentResolver = authenticationPrincipalArgumentResolver;
+        this.checkAdminInterceptor = checkAdminInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(checkAdminInterceptor)
+                .addPathPatterns("/admin/**");
     }
 
     @Override
