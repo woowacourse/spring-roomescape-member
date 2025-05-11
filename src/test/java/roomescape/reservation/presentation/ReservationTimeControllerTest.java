aecd762173029f5e6a79a6dc5109bd1ab2f6ac8e
sonjh919 <kingjh1125@gmail.com>
2025-05-11 17:23:32 +0900

    test: ReservationTimeControllerTest 수정

--- src/test/java/roomescape/reservation/presentation/ReservationTimeControllerTest.java
+++ src/test/java/roomescape/reservation/presentation/ReservationTimeControllerTest.java
@@ -1,133 +1,155 @@
-//package roomescape.reservation.presentation;
-//
-//import static org.hamcrest.Matchers.is;
-//
-//import io.restassured.RestAssured;
-//import io.restassured.http.ContentType;
-//import java.time.LocalDate;
-//import java.time.LocalTime;
-//import java.util.List;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.annotation.DirtiesContext;
-//import roomescape.reservation.presentation.dto.ReservationTimeRequest;
-//import roomescape.reservation.presentation.fixture.ReservationFixture;
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-//public class ReservationTimeControllerTest {
-//    private final ReservationFixture reservationFixture = new ReservationFixture();
-//
-//    @Test
-//    @DisplayName("시간 추가 테스트")
-//    void createTimeTest() {
-//        // given
-//        final ReservationTimeRequest reservationTime = reservationFixture.createReservationTimeRequest(
-//                LocalTime.of(10, 30));
-//
-//        // when-then
-//        RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .body(reservationTime)
-//                .when().post("/times")
-//                .then().log().all()
-//                .statusCode(201);
-//    }
-//
-//    @Test
-//    @DisplayName("중복된 시간 추가는 불가능하다.")
-//    void createTimeDuplicateExceptionTest() {
-//        // given
-//        final ReservationTimeRequest reservationTime = reservationFixture.createReservationTimeRequest(
-//                LocalTime.of(10, 30));
-//
-//        // when
-//        RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .body(reservationTime)
-//                .when().post("/times")
-//                .then().log().all()
-//                .statusCode(201);
-//
-//        // then
-//        RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .body(reservationTime)
-//                .when().post("/times")
-//                .then().log().all()
-//                .statusCode(400);
-//    }
-//
-//    @Test
-//    @DisplayName("시간 조회 테스트")
-//    void getTimesTest() {
-//        // given
-//        reservationFixture.createReservationTime(LocalTime.of(10, 30));
-//
-//        // when-then
-//        RestAssured.given().log().all()
-//                .when().get("/times")
-//                .then().log().all()
-//                .statusCode(200)
-//                .body("size()", is(1));
-//    }
-//
-//    @Test
-//    @DisplayName("예약 가능 시간 조회 테스트")
-//    void getAvailableTimesTest() {
-//        // given
-//        reservationFixture.createReservationTime(LocalTime.of(10, 30));
-//        reservationFixture.createReservationTime(LocalTime.of(11, 30));
-//
-//        reservationFixture.createTheme(
-//                "레벨2 탈출",
-//                "우테코 레벨2를 탈출하는 내용입니다.",
-//                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"
-//        );
-//
-//        reservationFixture.createReservation("브라운", LocalDate.of(2025, 8, 5), 1L, 2L);
-//
-//        // when
-//        RestAssured.given().log().all()
-//                .when().get("/times/available?date=2025-08-05&themeId=1")
-//                .then().log().all()
-//                .statusCode(200)
-//                .body("alreadyBooked", is(List.of(false, true)));
-//    }
-//
-//    @Test
-//    @DisplayName("시간 삭제 테스트")
-//    void deleteTimeTest() {
-//        // given
-//        reservationFixture.createReservationTime(LocalTime.of(10, 30));
-//
-//        // when-then
-//        RestAssured.given().log().all()
-//                .when().delete("/times/1")
-//                .then().log().all()
-//                .statusCode(204);
-//    }
-//
-//    @Test
-//    @DisplayName("예약이 이미 존재하는 시간은 삭제할 수 없다.")
-//    void deleteTimeExceptionTest() {
-//        // given
-//        reservationFixture.createReservationTime(LocalTime.of(10, 30));
-//
-//        reservationFixture.createTheme(
-//                "레벨2 탈출",
-//                "우테코 레벨2를 탈출하는 내용입니다.",
-//                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg"
-//        );
-//
-//        reservationFixture.createReservation("브라운", LocalDate.of(2025, 8, 5), 1L, 1L);
-//
-//        // when-then
-//        RestAssured.given().log().all()
-//                .when().delete("/times/1")
-//                .then().log().all()
-//                .statusCode(400);
-//    }
-//
-//}
+package roomescape.reservation.presentation;
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import roomescape.member.presentation.MemberFixture;
+import roomescape.reservation.presentation.dto.ReservationTimeRequest;
+import roomescape.reservation.presentation.fixture.ReservationFixture;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReservationTimeControllerTest {
+    private final ReservationFixture reservationFixture = new ReservationFixture();
+    private final MemberFixture memberFixture = new MemberFixture();
+
+    @Test
+    @DisplayName("시간 추가 테스트")
+    void createTimeTest() {
+        // given
+        final Map<String, String> cookies = memberFixture.loginAdmin();
+        final ReservationTimeRequest reservationTime = reservationFixture.createReservationTimeRequest(
+                LocalTime.of(10, 30));
+
+        // when-then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .body(reservationTime)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(201);
+    }
+
+    @Test
+    @DisplayName("중복된 시간 추가는 불가능하다.")
+    void createTimeDuplicateExceptionTest() {
+        // given
+        final Map<String, String> cookies = memberFixture.loginAdmin();
+        final ReservationTimeRequest reservationTime = reservationFixture.createReservationTimeRequest(
+                LocalTime.of(10, 30));
+
+        // when
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .body(reservationTime)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(201);
+
+        // then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .body(reservationTime)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(400);
+    }
+
+    @Test
+    @DisplayName("시간 조회 테스트")
+    void getTimesTest() {
+        // given
+        final Map<String, String> cookies = memberFixture.loginAdmin();
+        reservationFixture.createReservationTime(LocalTime.of(10, 30), cookies);
+
+        // when-then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .when().get("/times")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+    }
+
+    @Test
+    @DisplayName("예약 가능 시간 조회 테스트")
+    void getAvailableTimesTest() {
+        // given
+        final Map<String, String> cookies = memberFixture.loginAdmin();
+        reservationFixture.createReservationTime(LocalTime.of(10, 30), cookies);
+        reservationFixture.createReservationTime(LocalTime.of(11, 30), cookies);
+
+        reservationFixture.createTheme(
+                "레벨2 탈출",
+                "우테코 레벨2를 탈출하는 내용입니다.",
+                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg",
+                cookies
+        );
+
+        reservationFixture.createReservation(LocalDate.of(2025, 8, 5), 1L, 2L, cookies);
+
+        // when
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .when().get("/times/available?date=2025-08-05&themeId=1")
+                .then().log().all()
+                .statusCode(200)
+                .body("alreadyBooked", is(List.of(false, true)));
+    }
+
+    @Test
+    @DisplayName("시간 삭제 테스트")
+    void deleteTimeTest() {
+        // given
+        final Map<String, String> cookies = memberFixture.loginAdmin();
+        reservationFixture.createReservationTime(LocalTime.of(10, 30), cookies);
+
+        // when-then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .when().delete("/times/1")
+                .then().log().all()
+                .statusCode(204);
+    }
+
+    @Test
+    @DisplayName("예약이 이미 존재하는 시간은 삭제할 수 없다.")
+    void deleteTimeExceptionTest() {
+        // given
+        final Map<String, String> cookies = memberFixture.loginAdmin();
+        reservationFixture.createReservationTime(LocalTime.of(10, 30), cookies);
+
+        reservationFixture.createTheme(
+                "레벨2 탈출",
+                "우테코 레벨2를 탈출하는 내용입니다.",
+                "https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg",
+                cookies
+        );
+
+        reservationFixture.createReservation(LocalDate.of(2025, 8, 5), 1L, 1L, cookies);
+
+        // when-then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .when().delete("/times/1")
+                .then().log().all()
+                .statusCode(400);
+    }
+
+}
--- src/test/java/roomescape/reservation/presentation/fixture/ReservationFixture.java
+++ src/test/java/roomescape/reservation/presentation/fixture/ReservationFixture.java
@@ -1,48 +1,52 @@
-//package roomescape.reservation.presentation.fixture;
-//
-//import io.restassured.RestAssured;
-//import io.restassured.http.ContentType;
-//import java.time.LocalDate;
-//import java.time.LocalTime;
-//import roomescape.reservation.presentation.dto.ReservationRequest;
-//import roomescape.reservation.presentation.dto.ReservationTimeRequest;
-//import roomescape.reservation.presentation.dto.ThemeRequest;
-//
-//public class ReservationFixture {
-//
-//    public ReservationRequest createReservationRequest(String name, LocalDate date, Long themeId, Long timeId) {
-//        return new ReservationRequest(date, name, themeId, timeId);
-//    }
-//
-//    public ReservationTimeRequest createReservationTimeRequest(LocalTime startAt) {
-//        return new ReservationTimeRequest(startAt);
-//    }
-//
-//    public ThemeRequest createThemeRequest(String name, String description, String thumbnail) {
-//        return new ThemeRequest(name, description, thumbnail);
-//    }
-//
-//    public void createReservation(String name, LocalDate date, Long themeId, Long timeId) {
-//        final ReservationRequest reservationRequest = createReservationRequest(name, date, themeId, timeId);
-//        RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .body(reservationRequest)
-//                .when().post("/reservations");
-//    }
-//
-//    public void createReservationTime(LocalTime startAt) {
-//        final ReservationTimeRequest reservationTimeRequest = createReservationTimeRequest(startAt);
-//        RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .body(reservationTimeRequest)
-//                .when().post("/times");
-//    }
-//
-//    public void createTheme(String name, String description, String thumbnail) {
-//        final ThemeRequest themeRequest = createThemeRequest(name, description, thumbnail);
-//        RestAssured.given().log().all()
-//                .contentType(ContentType.JSON)
-//                .body(themeRequest)
-//                .when().post("/themes");
-//    }
-//}
+package roomescape.reservation.presentation.fixture;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Map;
+import roomescape.reservation.presentation.dto.ReservationRequest;
+import roomescape.reservation.presentation.dto.ReservationTimeRequest;
+import roomescape.reservation.presentation.dto.ThemeRequest;
+
+public class ReservationFixture {
+
+    public ReservationRequest createReservationRequest(LocalDate date, Long themeId, Long timeId) {
+        return new ReservationRequest(date, themeId, timeId);
+    }
+
+    public ReservationTimeRequest createReservationTimeRequest(LocalTime startAt) {
+        return new ReservationTimeRequest(startAt);
+    }
+
+    public ThemeRequest createThemeRequest(String name, String description, String thumbnail) {
+        return new ThemeRequest(name, description, thumbnail);
+    }
+
+    public void createReservation(LocalDate date, Long themeId, Long timeId, Map<String, String> cookies) {
+        final ReservationRequest reservationRequest = createReservationRequest(date, themeId, timeId);
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .body(reservationRequest)
+                .when().post("/reservations");
+    }
+
+    public void createReservationTime(LocalTime startAt, Map<String, String> cookies) {
+        final ReservationTimeRequest reservationTimeRequest = createReservationTimeRequest(startAt);
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .body(reservationTimeRequest)
+                .when().post("/times");
+    }
+
+    public void createTheme(String name, String description, String thumbnail, Map<String, String> cookies) {
+        final ThemeRequest themeRequest = createThemeRequest(name, description, thumbnail);
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookies(cookies)
+                .body(themeRequest)
+                .when().post("/themes");
+    }
+}
