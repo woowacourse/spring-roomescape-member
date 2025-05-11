76d22e5e23b22cf0daddfde40d8be3282ff9fa3a
DEV_WOONG <ywcsuwon@gmail.com>
2025-05-11 12:23:57 +0900

    feat: 로그아웃 구현

--- src/main/java/roomescape/auth/controller/AuthApiController.java
+++ src/main/java/roomescape/auth/controller/AuthApiController.java
@@ -1,18 +1,22 @@
 package roomescape.auth.controller;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.auth.controller.dto.MemberResponse;
 import roomescape.auth.controller.dto.SignupRequest;
 import roomescape.auth.controller.dto.SignupResponse;
 import roomescape.auth.controller.dto.TokenRequest;
 import roomescape.auth.entity.Member;
 import roomescape.auth.service.AuthService;
+import roomescape.auth.util.TokenUtil;
 
 @RestController
 public class AuthApiController {
@@ -25,13 +29,13 @@ public class AuthApiController {
 
 
     @PostMapping("/members")
-    public ResponseEntity<SignupResponse> signup(@RequestBody @Valid SignupRequest signupRequest) {
+    public ResponseEntity<SignupResponse> signup(@RequestBody SignupRequest signupRequest) {
         Member member = authService.register(
                 signupRequest.name(),
                 signupRequest.email(),
                 signupRequest.password()
         );
-        return ResponseEntity.status(HttpStatus.CREATED).body(SignupResponse.from(member));
+        return ResponseEntity.ok().body(SignupResponse.from(member));
     }
 
     @PostMapping("/login")
@@ -47,8 +51,24 @@ public class AuthApiController {
         return ResponseEntity.ok().build();
     }
 
-  /*  @GetMapping("/login/check")
-    public void checkAuthenticationInfo() {
+    @GetMapping("/login/check")
+    public ResponseEntity<?> checkLogin(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        String token = TokenUtil.extractTokenFromCookie(cookies);
+        Member member = authService.findMemberByToken(token);
+        return ResponseEntity.ok(MemberResponse.from(member));
+    }
 
-    }*/
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletResponse response) {
+        ResponseCookie deleteCookie = ResponseCookie.from("token", "")
+                .path("/")
+                .maxAge(0)
+                .httpOnly(true)
+                .build();
+
+        response.setHeader("Set-Cookie", deleteCookie.toString());
+
+        return ResponseEntity.ok().build();
+    }
 }
