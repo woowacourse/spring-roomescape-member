b878301e22a0d09e5e84036561b47ce764f27551
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-05-11 21:33:22 +0900

    fix(model) : Role.of 수정

--- src/main/java/roomescape/model/user/Role.java
+++ src/main/java/roomescape/model/user/Role.java
@@ -4,19 +4,21 @@ import java.util.Arrays;
 
 public enum Role {
     ADMIN("admin"),
-    USER("user");;
+    USER("user");
     private final String role;
 
     Role(String role) {
         this.role = role;
     }
 
-    public String getRole() {
+    public String getValue() {
         return role;
     }
 
     public static Role of(String role) {
-        return Arrays.stream(values()).findFirst()
+        return Arrays.stream(values())
+                .filter(r -> r.role.equals(role))
+                .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("등록되지 않은 역할입니다. : " + role));
     }
 }
