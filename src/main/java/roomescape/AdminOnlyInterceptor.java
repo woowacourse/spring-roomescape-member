e04e17714087c70141c651a1f39b97b8aa957b81
gabean13 <choegabin171@gmail.com>
2025-05-12 03:14:35 +0900

    feat: /admin/**는 admin 권한 들어갈 수 있도록 기능 구현

---
+++ src/main/java/roomescape/AdminOnlyInterceptor.java
@@ -0,0 +1,40 @@
+package roomescape;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.web.servlet.HandlerInterceptor;
+import roomescape.member.domain.Member;
+import roomescape.member.service.MemberService;
+
+public class AdminOnlyInterceptor implements HandlerInterceptor {
+
+    private final MemberService memberService;
+
+    public AdminOnlyInterceptor(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws IOException {
+        String uri = request.getRequestURI();
+        if (!uri.startsWith("/admin")) {
+            return true;
+        }
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if ("token".equals(cookie.getName())) {
+                String token = cookie.getValue();
+                Member member = memberService.findByToken(token);
+                if(member.getRole().getId() == 2L) {
+                    return true;
+                }
+            }
+        }
+        response.sendRedirect("/login");
+        return false;
+    }
+}
--- src/main/java/roomescape/WebMvcConfiguration.java
+++ src/main/java/roomescape/WebMvcConfiguration.java
@@ -3,6 +3,7 @@ package roomescape;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.member.service.MemberService;
 
@@ -18,5 +19,11 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new VisitorArgumentResolver(memberService));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AdminOnlyInterceptor(memberService))
+                .addPathPatterns("/admin/**");
     }
 }
