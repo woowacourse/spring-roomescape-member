323871c653a6638e53917e5cb8d1e6c0e1934d9d
byesol <bsj5967@naver.com>
2025-05-11 19:27:32 +0900

    test(controller): 필터링된 예약 조회 테스트 작성

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -1,6 +1,7 @@
 package roomescape.controller;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static roomescape.controller.ApiTestFixture.createReservationTime;
 import static roomescape.controller.ApiTestFixture.createTheme;
 import static roomescape.controller.ApiTestFixture.loginAndGetToken;
@@ -152,5 +153,108 @@ public class ReservationControllerTest {
         ;
     }
 
+    //    @GetMapping()
+//    public ResponseEntity<List<Reservation>> getReservations(
+//            @RequestParam(value = "memberId", required = false) Long memberId,
+//            @RequestParam(value = "themeId", required = false) Long themeId,
+//            @RequestParam(value = "from", required = false) LocalDate fromDate,
+//            @RequestParam(value = "to", required = false) LocalDate toDate
+//    ) {
+//        if (memberId == null || themeId == null || fromDate == null || toDate == null) {
+//            return ResponseEntity.ok().body(reservationService.getAllReservations());
+//        }
+//        return ResponseEntity.ok()
+//                .body(reservationService.getFilteredReservations(memberId, themeId, fromDate, toDate));
+//    }
+    @Test
+    @DisplayName("조건 없이 조회하면 전체 예약 리스트를 반환한다.")
+    void testGetAllReservations() {
+        ApiTestFixture.createReservationTime();
+        ApiTestFixture.createTheme();
+        token = loginAndGetToken();
+        Map<String, Object> params = new HashMap<>();
+        params.put("memberId", 1);
+        params.put("date", "2222-02-02");
+        params.put("timeId", 1);
+        params.put("themeId", 1);
+        RestAssured.given().log().all()
+                .cookie("token", token)
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/admin/reservations")
+                .then().log().all()
+                .statusCode(201);
+
+        params.put("date", "2222-02-03");
+        RestAssured.given().log().all()
+                .cookie("token", token)
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/admin/reservations")
+                .then().log().all()
+                .statusCode(201);
+
+        // GET 전체 조회
+        RestAssured.given().log().all()
+                .cookie("token", token)
+                .when().get("/admin/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(2));
+    }
+
+    @Test
+    @DisplayName("3건의 예약 중 쿼리 파라미터로 필터링된 예약 하나만 조회한다.")
+    void testGetFilteredReservations() {
+        // 예약 3건 생성: 2건은 검색 범위 밖, 1건만 범위 내
+        ApiTestFixture.createReservationTime();
+        ApiTestFixture.createTheme();
+        token = loginAndGetToken();
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("memberId", 1);
+        params.put("timeId", 1);
+        params.put("themeId", 1);
+
+        params.put("date", "2222-02-02");
+        RestAssured.given().log().all()
+                .cookie("token", token)
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/admin/reservations")
+                .then().statusCode(201);
+
+        // 범위 밖 (memberId 다름)
+        params.put("memberId", 2);
+        params.put("date", "2222-02-03");
+        RestAssured.given().log().all()
+                .cookie("token", token)
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/admin/reservations")
+                .then().statusCode(201);
+
+        // 범위 밖 (date 다름)
+        params.put("memberId", 1);
+        params.put("date", "2222-03-01");
+        RestAssured.given().log().all()
+                .cookie("token", token)
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/admin/reservations")
+                .then().statusCode(201);
+
+        // GET 필터 조회
+        RestAssured.given().log().all()
+                .cookie("token", token)
+                .queryParam("memberId", 1)
+                .queryParam("themeId", 1)
+                .queryParam("from", "2222-02-01")
+                .queryParam("to", "2222-02-28")
+                .when().get("/admin/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(1));
+    }
 
 }
