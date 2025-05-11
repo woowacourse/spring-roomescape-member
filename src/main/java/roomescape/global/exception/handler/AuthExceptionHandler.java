c47fc1c84b64fb4930379c829f32bd45bf91a72d
minSsan <msp1125@hanyang.ac.kr>
2025-05-11 20:05:42 +0900

    feat: ErrorResponse 객체 생성 - 예외 응답 포맷

--- src/main/java/roomescape/global/exception/handler/AuthExceptionHandler.java
+++ src/main/java/roomescape/global/exception/handler/AuthExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import roomescape.global.exception.dto.ErrorResponse;
 import roomescape.global.exception.forbidden.ForbiddenException;
 import roomescape.global.exception.notFound.MemberNotFoundException;
 import roomescape.global.exception.notFound.NotFoundException;
@@ -12,17 +13,23 @@ import roomescape.global.exception.unauthorized.UnauthorizedException;
 @RestControllerAdvice
 public class AuthExceptionHandler {
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<String> unauthorized(UnauthorizedException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    public ResponseEntity<ErrorResponse> unauthorized(UnauthorizedException e) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        ErrorResponse response = new ErrorResponse(status.value(), e.getMessage());
+        return ResponseEntity.status(status).body(response);
     }
 
     @ExceptionHandler(ForbiddenException.class)
-    public ResponseEntity<String> forbidden(ForbiddenException e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    public ResponseEntity<ErrorResponse> forbidden(ForbiddenException e) {
+        HttpStatus status = HttpStatus.FORBIDDEN;
+        ErrorResponse response = new ErrorResponse(status.value(), e.getMessage());
+        return ResponseEntity.status(status).body(response);
     }
 
     @ExceptionHandler(MemberNotFoundException.class)
-    public ResponseEntity<String> notFound(NotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<ErrorResponse> notFound(NotFoundException e) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse response = new ErrorResponse(status.value(), e.getMessage());
+        return ResponseEntity.status(status).body(response);
     }
 }
