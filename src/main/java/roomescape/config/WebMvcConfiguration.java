fc47041275e8841404cd88a00e4f72e096e77a33
supernovaMK <minkikim2002@gmail.com>
2025-05-12 12:33:14 +0900

    refactor: 구현체 사용에서 인터페이스 사용으로 변경

--- src/main/java/roomescape/config/WebMvcConfiguration.java
+++ src/main/java/roomescape/config/WebMvcConfiguration.java
@@ -5,24 +5,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.service.member.MemberService;
 import roomescape.service.member.MemberServiceImpl;
+import roomescape.util.JwtTokenProvider;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-    private final MemberServiceImpl memberServiceImpl;
+    private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public WebMvcConfiguration(MemberServiceImpl memberServiceImpl) {
-        this.memberServiceImpl = memberServiceImpl;
+
+    public WebMvcConfiguration(MemberService memberService,JwtTokenProvider jwtTokenProvider) {
+        this.memberService = memberService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new LoginMemberArgumentResolver(memberServiceImpl));
+        resolvers.add(new LoginMemberArgumentResolver(memberService,jwtTokenProvider));
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AdminInterceptor(memberServiceImpl)).addPathPatterns("/admin/**");
+        registry.addInterceptor(new AdminInterceptor(memberService,jwtTokenProvider)).addPathPatterns("/admin/**");
     }
 }
