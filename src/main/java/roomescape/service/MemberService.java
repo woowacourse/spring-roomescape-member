7a513f8b1006464873f454d2825f644534ef3ecc
AppleMint98 <applemint9821@gmail.com>
2025-05-12 17:06:29 +0900

    feat(MemberService): 멤버 추가 및 조회 로직 구현

---
+++ src/main/java/roomescape/service/MemberService.java
@@ -0,0 +1,29 @@
+package roomescape.service;
+
+import org.springframework.stereotype.Service;
+import roomescape.domain.Member;
+import roomescape.dto.SignUpRequest;
+import roomescape.dto.SignUpResponse;
+import roomescape.exception.exception.DataNotFoundException;
+import roomescape.repository.MemberRepository;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(final MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public SignUpResponse addMember(final SignUpRequest request) {
+        Member member = new Member(request.name(), request.email(), request.password(), "USER");
+        Member saved = memberRepository.save(member);
+        return SignUpResponse.of(saved);
+    }
+
+    public Member findMemberById(final Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(
+                () -> new DataNotFoundException(String.format("[ERROR] %d번에 해당하는 멤버가 없습니다.", memberId)));
+    }
+}
---
+++ src/test/java/roomescape/service/MemberServiceTest.java
@@ -0,0 +1,33 @@
+package roomescape.service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import roomescape.dto.SignUpRequest;
+import roomescape.exception.exception.DataNotFoundException;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles("test")
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @DisplayName("존재하지 않는 멤버번호를 찾으려는 경우 예외를 던진다")
+    @Test
+    void removeReservationTime() {
+        //given
+        memberService.addMember(new SignUpRequest("제프리", "jeffrey@gmail.com", "1234!@#$"));
+        long notExistId = 999;
+
+        //when & then
+        assertThatThrownBy(() -> memberService.findMemberById(notExistId)).isInstanceOf(DataNotFoundException.class)
+                .hasMessage("[ERROR] 999번에 해당하는 멤버가 없습니다.");
+    }
+}
