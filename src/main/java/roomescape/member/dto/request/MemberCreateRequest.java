f06a4926acebbcdc1a71c18f996528aef9dc7751
kimdev <kim__dev@hufs.ac.kr>
2025-05-12 12:55:49 +0900

    refactor: MemberCreateRequest 유효성 검증 로직 추가

--- src/main/java/roomescape/member/dto/request/MemberCreateRequest.java
+++ src/main/java/roomescape/member/dto/request/MemberCreateRequest.java
@@ -1,4 +1,22 @@
 package roomescape.member.dto.request;
 
+import roomescape.global.exception.InvalidInputException;
+
 public record MemberCreateRequest(String email, String password, String name) {
+
+    public MemberCreateRequest {
+        validateNull(email, password, name);
+    }
+
+    private void validateNull(String email, String password, String name) {
+        if (email == null) {
+            throw new InvalidInputException("이메일이 입력되지 않았다.");
+        }
+        if (password == null) {
+            throw new InvalidInputException("패스워드가 입력되지 않았다.");
+        }
+        if (name == null) {
+            throw new InvalidInputException("이름이 입력되지 않았다.");
+        }
+    }
 }
