e8965b82305bc35847d61a3d77ad1e01ce3798ad
KimDaehyeon <daehyeon3351@gmail.com>
2025-05-13 00:59:01 +0900

    feat: 멤버 View 컨트롤러 추가

--- src/main/java/roomescape/member/controller/MemberViewController.java
+++ src/main/java/roomescape/member/controller/MemberViewController.java
@@ -1,14 +1,19 @@
-package roomescape.user.controller;
+package roomescape.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UserViewController {
+public class MemberViewController {
 
     @GetMapping("/reservation")
     public String reservation() {
         return "reservation";
+    }
+
+    @GetMapping("/member/login")
+    public String login() {
+        return "login";
     }
 
     @GetMapping
