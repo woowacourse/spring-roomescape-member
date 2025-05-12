c9431e38e2d3709786fc28fa8304c16a34a28749
seaniiio <cnc4750@naver.com>
2025-05-12 13:50:48 +0900

    refactor: 누락된 `@Validated` 추가

--- src/main/java/roomescape/presentation/controller/AdminController.java
+++ src/main/java/roomescape/presentation/controller/AdminController.java
@@ -2,6 +2,7 @@ package roomescape.presentation.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ public class AdminController {
     }
 
     @PostMapping("/reservations")
-    public ResponseEntity<ReservationResponse> createReservation(@RequestBody AdminReservationRequest request) {
+    public ResponseEntity<ReservationResponse> createReservation(@Validated @RequestBody AdminReservationRequest request) {
         ReservationResponse response = reservationService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
