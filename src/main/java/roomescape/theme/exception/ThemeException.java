aa539d63b332391eb21e5acf048741e683874e06
GiSeok Kang <ime.rokz@gmail.com>
2025-05-12 17:09:30 +0900

    refactor: Theme 관련 예외 처리 로직 개선

---
+++ src/main/java/roomescape/theme/exception/ThemeException.java
@@ -0,0 +1,9 @@
+package roomescape.theme.exception;
+
+import roomescape.common.exception.BusinessException;
+
+public class ThemeException extends BusinessException {
+  public ThemeException(String message) {
+    super(message);
+  }
+}
