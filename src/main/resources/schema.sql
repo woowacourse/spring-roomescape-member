13c261be2264b6f943144ce4cbddbff06001a5d4
HyeonsuLee <leehyeonsu4888@naver.com>
2025-05-12 19:50:12 +0900

    refactor: 테이블 스키마 최적화

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE member
     name        VARCHAR(50) NOT NULL,
     email       VARCHAR(255) NOT NULL,
     password    VARCHAR(255) NOT NULL,
-    role        VARCHAR(255) NOT NULL,
+    role        VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -18,7 +18,7 @@ CREATE TABLE reservation_time
 CREATE TABLE theme
 (
     id          BIGINT       NOT NULL AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
+    name        VARCHAR(50) NOT NULL,
     description VARCHAR(255) NOT NULL,
     thumbnail   VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
