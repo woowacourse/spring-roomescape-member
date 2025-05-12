79816e5ebb7efdc26460e7d14f04423dacb5ae98
kimdev <kim__dev@hufs.ac.kr>
2025-05-12 16:40:33 +0900

    test: 토큰이 필요한 테스트에 토큰 보완

--- src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
@@ -6,12 +6,15 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.global.infrastructure.JwtTokenProvider;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -19,6 +22,17 @@ public class ReservationControllerTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    private String normalAccessToken;
+
+    @BeforeEach
+    void initialize() {
+        String normalMemberEmail = "normal1@normal.com";
+        normalAccessToken = jwtTokenProvider.createToken(normalMemberEmail);
+    }
 
     @Test
     @DisplayName("/reservations 요청 시 예약 정보 조회")
@@ -30,55 +44,55 @@ public class ReservationControllerTest {
             .body("size()", is(21));
     }
 
-    // TODO: 토큰 가지고 요청 테스트
-//    @Test
-//    @DisplayName("예약 관리 페이지 내에서 예약 추가")
-//    void createReservation() {
-//        RestAssured.given().log().all()
-//            .contentType(ContentType.JSON)
-//            .body(getTestParamsWithReservation())
-//            .when().post("/reservations")
-//            .then().log().all()
-//            .statusCode(201)
-//            .body("name", is("사나"));
-//
-//        RestAssured.given().log().all()
-//            .when().get("/reservations")
-//            .then().log().all()
-//            .statusCode(200)
-//            .body("size()", is(4));
-//    }
+    @Test
+    @DisplayName("예약 관리 페이지 내에서 예약 추가")
+    void createReservation() {
+        RestAssured.given().log().all()
+            .contentType(ContentType.JSON)
+            .cookie("token", normalAccessToken)
+            .body(getTestParamsWithReservation())
+            .when().post("/reservations")
+            .then().log().all()
+            .statusCode(201)
+            .body("member.name", is("일반1"));
+
+        RestAssured.given().log().all()
+            .when().get("/reservations")
+            .then().log().all()
+            .statusCode(200)
+            .body("size()", is(22));
+    }
 
 
-    // TODO: 토큰 가지고 요청 테스트
-//    @Test
-//    @DisplayName("예약 관리 페이지 내에서 예약 삭제")
-//    void deleteReservation() {
-//        RestAssured.given().log().all()
-//            .contentType(ContentType.JSON)
-//            .body(getTestParamsWithReservation())
-//            .when().post("/reservations")
-//            .then().log().all()
-//            .statusCode(201)
-//            .body("id", is(4));
-//
-//        RestAssured.given().log().all()
-//            .when().get("/reservations")
-//            .then().log().all()
-//            .statusCode(200)
-//            .body("size()", is(4));
-//
-//        RestAssured.given().log().all()
-//            .when().delete("/reservations/1")
-//            .then().log().all()
-//            .statusCode(204);
-//
-//        RestAssured.given().log().all()
-//            .when().get("/reservations")
-//            .then().log().all()
-//            .statusCode(200)
-//            .body("size()", is(3));
-//    }
+    @Test
+    @DisplayName("예약 관리 페이지 내에서 예약 삭제")
+    void deleteReservation() {
+        RestAssured.given().log().all()
+            .contentType(ContentType.JSON)
+            .cookie("token", normalAccessToken)
+            .body(getTestParamsWithReservation())
+            .when().post("/reservations")
+            .then().log().all()
+            .statusCode(201)
+            .body("id", is(22));
+
+        RestAssured.given().log().all()
+            .when().get("/reservations")
+            .then().log().all()
+            .statusCode(200)
+            .body("size()", is(22));
+
+        RestAssured.given().log().all()
+            .when().delete("/reservations/1")
+            .then().log().all()
+            .statusCode(204);
+
+        RestAssured.given().log().all()
+            .when().get("/reservations")
+            .then().log().all()
+            .statusCode(200)
+            .body("size()", is(21));
+    }
 
     private Map<String, Object> getTestParamsWithReservation() {
         jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES ('15:40')");
@@ -87,7 +101,7 @@ public class ReservationControllerTest {
 
         Map<String, Object> params = new HashMap<>();
         params.put("memberId", 1);
-        params.put("date", "2024-04-26");
+        params.put("date", "2030-04-26");
         params.put("timeId", 1);
         params.put("themeId", 1);
         return params;
