0755721448bf0bb835044717a36bff4f39f480fa
AppleMint98 <applemint9821@gmail.com>
2025-05-12 17:08:28 +0900

    refactor(Reservation): 이름 대신 멤버 객체를 받도록 수정

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -9,34 +9,30 @@ import lombok.Getter;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Reservation {
 
-    private static final String KOREAN_WORDS_REGEX = "^[가-힣]+$";
-    private static final int NAME_MAX_SIZE = 5;
-
     @EqualsAndHashCode.Include
     private final Long id;
-    private final String name;
     private final LocalDate date;
+    private final Member member;
     private final ReservationTime time;
     private final ReservationTheme theme;
 
-    public Reservation(final Long id, final String name, final LocalDate date, final ReservationTime time,
+    public Reservation(final Long id, final LocalDate date, final Member member, final ReservationTime time,
                        final ReservationTheme theme) {
-        validateNameKoreanWords(name);
-        validateNameSize(name);
+
         this.id = id;
-        this.name = name;
         this.date = date;
+        this.member = member;
         this.time = time;
         this.theme = theme;
     }
 
-    public Reservation(final String name, final LocalDate date, final ReservationTime time,
+    public Reservation(final LocalDate date, final Member member, final ReservationTime time,
                        final ReservationTheme theme) {
-        this(null, name, date, time, theme);
+        this(null, date, member, time, theme);
     }
 
     public Reservation assignId(final Long id) {
-        return new Reservation(id, name, date, time, theme);
+        return new Reservation(id, date, member, time, theme);
     }
 
     public boolean isDuplicateReservation(Reservation reservation) {
@@ -45,18 +41,5 @@ public class Reservation {
 
     public LocalDateTime toDateTime() {
         return LocalDateTime.of(date, time.getStartAt());
-    }
-
-    private void validateNameKoreanWords(String name) {
-        if (!name.matches(KOREAN_WORDS_REGEX)) {
-            throw new IllegalArgumentException("[ERROR] 이름은 한국어로만 입력해 주세요.");
-        }
-    }
-
-    private void validateNameSize(String name) {
-        if (name.length() > NAME_MAX_SIZE) {
-            throw new IllegalArgumentException(
-                    "[ERROR] 이름은 " + NAME_MAX_SIZE + "글자 이내로 입력해 주세요. 현재 길이는 " + name.length() + "글자 입니다.");
-        }
     }
 }
