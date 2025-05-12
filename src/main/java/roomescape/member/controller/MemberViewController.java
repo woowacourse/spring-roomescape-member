6f50aed2c82d257f20cdd8fbe8355ae2bcb61c88
BWY <quantumjump10@gmail.com>
2025-05-12 21:23:11 +0900

    refactor: 루트 경로에 대한 명시적 지정

--- src/main/java/roomescape/member/controller/MemberViewController.java
+++ src/main/java/roomescape/member/controller/MemberViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MemberViewController {
 
-    @GetMapping
+    @GetMapping("/")
     public String getMainPage() {
         return "index";
     }
