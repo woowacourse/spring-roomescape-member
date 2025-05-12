9025018e4d3339a0bd6d5049d8f243480ddd834d
AppleMint98 <applemint9821@gmail.com>
2025-05-12 17:13:54 +0900

    refactor(controller): 응답 객체 Object 변경

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -1,7 +1,6 @@
 package roomescape.controller;
 
 import jakarta.validation.Valid;
-import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.ReservationTimeRequest;
-import roomescape.dto.ReservationTimeResponse;
 import roomescape.service.ReservationTimeService;
 
 @RestController
@@ -26,18 +24,18 @@ public class ReservationTimeController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ReservationTimeResponse>> reservationTimeList() {
+    public ResponseEntity<Object> reservationTimeList() {
         return ResponseEntity.ok(reservationTimeService.findReservationTimes());
     }
 
     @PostMapping
-    public ResponseEntity<ReservationTimeResponse> reservationTimeAdd(
+    public ResponseEntity<Object> reservationTimeAdd(
             @Valid @RequestBody ReservationTimeRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(reservationTimeService.addReservationTime(request));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> reservationTimeRemove(@PathVariable(name = "id") long id) {
+    public ResponseEntity<Object> reservationTimeRemove(@PathVariable(name = "id") long id) {
         reservationTimeService.removeReservationTime(id);
         return ResponseEntity.noContent().build();
     }
