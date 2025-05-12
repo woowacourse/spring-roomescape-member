6150b041966290e43f3d61f532bf2a668b18565a
BWY <quantumjump10@gmail.com>
2025-05-12 20:59:24 +0900

    refactor: jwt 토큰을 담은 쿠키를 생성하는 로직을 AuthService로 이동

--- src/main/java/roomescape/auth/service/AuthService.java
+++ src/main/java/roomescape/auth/service/AuthService.java
@@ -8,7 +8,6 @@ import roomescape.auth.dto.TokenRequest;
 import roomescape.auth.dto.TokenWithCookieResponse;
 import roomescape.auth.exception.UnauthorizedException;
 import roomescape.global.security.jwt.JwtTokenProvider;
-import roomescape.global.util.CookieUtil;
 import roomescape.member.entity.Member;
 import roomescape.member.repository.MemberRepository;
 
@@ -34,7 +33,7 @@ public class AuthService {
         }
 
         String accessToken = jwtTokenProvider.createToken(member);
-        ResponseCookie responseCookie = CookieUtil.createJwtCookie(accessToken);
+        ResponseCookie responseCookie = createJwtCookie(accessToken);
         return new TokenWithCookieResponse(accessToken, responseCookie);
     }
 
@@ -42,5 +41,14 @@ public class AuthService {
         Member member = memberRepository.findById(loginMember.id())
                 .orElseThrow(() -> new NoSuchElementException("해당 사용자를 찾을 수 없습니다"));
         return LoginCheckResponse.from(member);
+    }
+
+    private ResponseCookie createJwtCookie(String token) {
+        return ResponseCookie.from("jwt_token", token)
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(600)
+                .build();
     }
 }
