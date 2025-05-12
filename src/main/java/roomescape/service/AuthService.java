21a11bdaa14b9a1c86ad08fd938b11733e920338
AppleMint98 <applemint9821@gmail.com>
2025-05-12 17:07:36 +0900

    feat(AuthService): 로그인 서비스 구현

---
+++ src/main/java/roomescape/service/AuthService.java
@@ -0,0 +1,36 @@
+package roomescape.service;
+
+import org.springframework.stereotype.Service;
+import roomescape.domain.Member;
+import roomescape.dto.LoginRequest;
+import roomescape.exception.exception.DataNotFoundException;
+import roomescape.exception.exception.InvalidPasswordException;
+import roomescape.infrastructure.JwtTokenProvider;
+import roomescape.repository.MemberRepository;
+
+@Service
+public class AuthService {
+
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthService(final MemberRepository memberRepository, final JwtTokenProvider jwtTokenProvider) {
+        this.memberRepository = memberRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    public String login(final LoginRequest request) {
+        Member member = memberRepository.findByEmail(request.email())
+                .orElseThrow(() -> new DataNotFoundException("[ERROR] 이메일에 해당하는 회원을 찾을 수 없습니다."));
+
+        if (!member.checkPassword(request.password())) {
+            throw new InvalidPasswordException("[ERROR] 비밀번호가 일치하지 않습니다.");
+        }
+
+        return jwtTokenProvider.createToken(member.getId(), member.getName(), member.getRole());
+    }
+
+    public String extractName(final String token) {
+        return jwtTokenProvider.getName(token);
+    }
+}
---
+++ src/test/java/roomescape/service/AuthServiceTest.java
@@ -0,0 +1,52 @@
+package roomescape.service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import roomescape.domain.Member;
+import roomescape.dto.LoginRequest;
+import roomescape.exception.exception.DataNotFoundException;
+import roomescape.exception.exception.InvalidPasswordException;
+import roomescape.repository.MemberRepository;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles("test")
+class AuthServiceTest {
+
+    @Autowired
+    private AuthService authService;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void loginWithNotFoundMember() {
+        // given
+        Member member = new Member("제프리", "jeffrey@gmail.com", "1234!@#$", "USER");
+        memberRepository.save(member);
+        LoginRequest loginRequest = new LoginRequest("wilson@gmail.com", "1234!@#$");
+
+        // when & then
+        assertThatThrownBy(() -> authService.login(loginRequest))
+                .isInstanceOf(DataNotFoundException.class)
+                .hasMessage("[ERROR] 이메일에 해당하는 회원을 찾을 수 없습니다.");
+    }
+
+    @Test
+    void loginFailedByIncorrectPassword() {
+        // given
+        Member member = new Member("제프리", "jeffrey@gmail.com", "1234!@#$", "USER");
+        memberRepository.save(member);
+        LoginRequest loginRequest = new LoginRequest("jeffrey@gmail.com", "1234!@");
+
+        // when & then
+        assertThatThrownBy(() -> authService.login(loginRequest))
+                .isInstanceOf(InvalidPasswordException.class)
+                .hasMessage("[ERROR] 비밀번호가 일치하지 않습니다.");
+    }
+
+}
