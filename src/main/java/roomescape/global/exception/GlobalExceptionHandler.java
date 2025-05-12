091c0092e857809ce190786a6a90a8131e34ee69
DongchannN <ldc991104@gmail.com>
2025-05-12 15:37:12 +0900

    refactor: 로깅 시 logger 사용

--- src/main/java/roomescape/global/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package roomescape.global.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,33 +13,36 @@ import roomescape.global.exception.RoomEscapeException.ResourceNotFoundException
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(value = ResourceNotFoundException.class)
     public ResponseEntity<String> handleNotFound(ResourceNotFoundException e) {
-        System.out.println("[ERROR] " + e.getMessage());
+        logger.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(value = BadRequestException.class)
     public ResponseEntity<String> handleBadRequest(BadRequestException e) {
-        System.out.println("[ERROR] " + e.getMessage());
+        logger.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(value = DataAccessException.class)
     public ResponseEntity<String> handleDataAccessError() {
-        System.out.println("[ERROR] " + "데이터베이스 접근에 실패하였습니다.");
+        logger.error("데이터베이스 접근에 실패하였습니다.");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("데이터베이스 접근에 실패하였습니다.");
     }
 
     @ExceptionHandler(value = AuthenticationException.class)
     public ResponseEntity<String> handleAuthenticationError(AuthenticationException e) {
-        System.out.println("[ERROR] " + e.getMessage());
+        logger.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<String> handleUnexpectedError(Exception e) {
-        System.out.println("[ERROR] " + e.getMessage());
+        logger.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
