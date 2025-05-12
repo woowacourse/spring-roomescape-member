72ffef2fd3237ddcfc079071462bad0f536ca947
AppleMint98 <applemint9821@gmail.com>
2025-05-12 17:09:07 +0900

    feat(exception): 인증, 인가 예외 및 핸들러 구현

---
+++ src/main/java/roomescape/exception/exception/InvalidPasswordException.java
@@ -0,0 +1,8 @@
+package roomescape.exception.exception;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}
