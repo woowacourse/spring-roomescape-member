c42a0363879b86cdf61e56cca9800121fa9ff437
kjungw <sagakkb@gmail.com>
2025-05-12 19:49:45 +0900

    refactor: 잘못된 컨벤션 수정

--- src/main/java/roomescape/domain/regex/MemberFormat.java
+++ src/main/java/roomescape/domain/regex/MemberFormat.java
@@ -2,6 +2,6 @@ package roomescape.domain.regex;
 
 public class MemberFormat {
 
-    public final static String EMAIL = "[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+";
-    public final static String PASSWORD = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$";
+    public static final String EMAIL = "[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+";
+    public static final String PASSWORD = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$";
 }
