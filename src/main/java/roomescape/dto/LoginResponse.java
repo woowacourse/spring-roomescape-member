5a938b2c07a4a175313be90e8944798485e909f3
jye326 <jye326@naver.com>
2025-05-12 22:06:47 +0900

    delete(dto) : 미사용 클래스 삭제
    - LoginResponse.java

--- src/main/java/roomescape/dto/LoginResponse.java
+++
@@ -1,7 +0,0 @@
-package roomescape.dto;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-
-public record LoginResponse(@Email String email, @NotNull String password) {
-}
