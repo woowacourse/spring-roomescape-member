9cfecc054468d41b4a105d3f36cfe35cbcc0aba5
2jin2.1031 <2jin2.1031@gmail.com>
2025-05-13 22:36:35 +0900

    refactor: 검색 파라미터 처리 방식에 명시적으로 @ModelAttribute 어노테이션 추가

--- src/main/java/roomescape/admin/controller/AdminController.java
+++ src/main/java/roomescape/admin/controller/AdminController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +36,7 @@ public class AdminController {
 
     @GetMapping("/search")
     public ResponseEntity<List<ReservationResponseDto>> searchReservations(
-            SearchReservationRequestDto searchReservationRequestDto,
+            @ModelAttribute SearchReservationRequestDto searchReservationRequestDto,
             User admin) {
         List<ReservationResponseDto> reservationResponseDtos = adminService.searchReservations(
                 searchReservationRequestDto,
