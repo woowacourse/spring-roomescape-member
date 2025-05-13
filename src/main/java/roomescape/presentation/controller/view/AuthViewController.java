b5f6027b21011bc41896a55c0af06c0b49f0aeea
Jin409 <sualng123@gmail.com>
2025-05-13 20:50:34 +0900

    refactor: 일관성을 위해 `@RequestMapping` 을 이용해 경로를 매핑하도록 수정

--- src/main/java/roomescape/presentation/controller/view/AuthViewController.java
+++ src/main/java/roomescape/presentation/controller/view/AuthViewController.java
@@ -1,11 +1,11 @@
 package roomescape.presentation.controller.view;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/login")
 public class AuthViewController {
-    @GetMapping("/login")
     public String login() {
         return "login";
     }
