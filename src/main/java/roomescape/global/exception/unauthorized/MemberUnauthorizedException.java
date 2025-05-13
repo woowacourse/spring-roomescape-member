c8c449a97d1497632029e15518d1ca908b7d576e
minSsan <msp1125@hanyang.ac.kr>
2025-05-13 21:34:33 +0900

    feat: 401 에러 문구 변경

--- src/main/java/roomescape/global/exception/unauthorized/MemberUnauthorizedException.java
+++ src/main/java/roomescape/global/exception/unauthorized/MemberUnauthorizedException.java
@@ -2,6 +2,6 @@ package roomescape.global.exception.unauthorized;
 
 public class MemberUnauthorizedException extends UnauthorizedException {
     public MemberUnauthorizedException() {
-        super("유저");
+        super("로그인이 필요한 서비스입니다.");
     }
 }
--- src/main/java/roomescape/global/exception/unauthorized/UnauthorizedException.java
+++ src/main/java/roomescape/global/exception/unauthorized/UnauthorizedException.java
@@ -1,7 +1,7 @@
 package roomescape.global.exception.unauthorized;
 
 public class UnauthorizedException extends IllegalArgumentException {
-    public UnauthorizedException(String resource) {
-        super(String.format("등록되어 있지 않은 %s 정보입니다.", resource));
+    public UnauthorizedException(String message) {
+        super(message);
     }
 }
