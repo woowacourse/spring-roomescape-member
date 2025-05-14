73cf3e632f928d8f2e00b5fc3f1c44a7b1f8b513
dltmddus <tmddus4671@gmail.com>
2025-05-14 13:21:04 +0900

    refactor: 변수 추출 삭제하도록 리팩토링

--- src/main/java/roomescape/member/service/MemberService.java
+++ src/main/java/roomescape/member/service/MemberService.java
@@ -2,7 +2,6 @@ package roomescape.member.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
-import roomescape.member.Member;
 import roomescape.member.dao.MemberDao;
 import roomescape.member.dto.response.MemberResponse;
 
@@ -16,9 +15,7 @@ public class MemberService {
     }
 
     public List<MemberResponse> findAll() {
-
-        List<Member> MemberDaoAll = memberDao.findAll();
-        return MemberDaoAll.stream()
+        return memberDao.findAll().stream()
                 .map(MemberResponse::toDto)
                 .toList();
     }
