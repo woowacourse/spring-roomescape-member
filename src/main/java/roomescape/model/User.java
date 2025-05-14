0d6971976893f0eedc3d0fdc3c3d651c1ab42563
geonwoo <kysub99@naver.com>
2025-05-14 23:12:08 +0900

    refactor: 이메일 형식 검증

--- src/main/java/roomescape/model/User.java
+++ src/main/java/roomescape/model/User.java
@@ -1,5 +1,8 @@
 package roomescape.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class User {
     private final Long id;
     private final UserName name;
@@ -10,9 +13,19 @@ public class User {
     public User(Long id, UserName name, String email, String password, Role role) {
         this.id = id;
         this.name = name;
+        if (!isValidEmail(email)) {
+            throw new IllegalArgumentException("유효하지 않은 이메일 형식입니다.");
+        }
         this.email = email;
         this.password = password;
         this.role = role;
+    }
+
+    private boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
     public Long getId() {
