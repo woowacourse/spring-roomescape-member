bf05e2f1758f0748f805fdd88871b76ede23e9e5
ryan-dia <cjfdnjs158@naver.com>
2025-05-14 22:26:32 +0900

    feat: 쿠키를 생성할 때 maxAge 추가
    Co-authored-by: songsunkook <songsunkook@gmail.com>

--- src/main/java/roomescape/auth/controller/AuthController.java
+++ src/main/java/roomescape/auth/controller/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.auth.dto.LoginCheckResponse;
 import roomescape.auth.dto.LoginRequest;
+import roomescape.auth.infrastructure.JwtProperties;
 import roomescape.auth.service.AuthService;
 import roomescape.error.UnauthorizedException;
 
@@ -25,6 +26,7 @@ public class AuthController {
 
     private static final String COOKIE_TOKEN = "token";
     private final AuthService authService;
+    private final JwtProperties jwtProperties;
 
     @PostMapping("/login")
     public void login(@RequestBody @Valid final LoginRequest request, final HttpServletResponse response) {
@@ -35,6 +37,7 @@ public class AuthController {
         final ResponseCookie cookie = ResponseCookie.from(COOKIE_TOKEN, token)
                 .httpOnly(true)
                 .path("/")
+                .maxAge(jwtProperties.getExpireLength())
                 .build();
         log.info("쿠키 생성 완료");
 
