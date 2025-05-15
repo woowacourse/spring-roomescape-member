205b6de7960ac675d5f282e2a4a922bbee02f5c8
2jin2.1031 <2jin2.1031@gmail.com>
2025-05-15 16:03:45 +0900

    refactor: ViewController를 ViewControllerRegistry를 사용하도록 수정

--- src/main/java/roomescape/global/config/WebMvcConfiguration.java
+++ src/main/java/roomescape/global/config/WebMvcConfiguration.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.global.auth.JwtTokenProvider;
 import roomescape.global.config.interceptor.CheckAdminInterceptor;
@@ -30,5 +31,16 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/reservation").setViewName("reservation");
+        registry.addViewController("/admin").setViewName("admin/index");
+        registry.addViewController("/admin/reservation").setViewName("admin/reservation-new");
+        registry.addViewController("/admin/time").setViewName("admin/time");
+        registry.addViewController("/admin/theme").setViewName("admin/theme");
+        registry.addViewController("/").setViewName("index");
+
     }
 }
