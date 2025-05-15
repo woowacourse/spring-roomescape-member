9bed9339c043a532f8496db7209589e11ca7fd9e
AppleMint98 <applemint9821@gmail.com>
2025-05-15 16:59:17 +0900

    feat(domain): 도메인 null값 검증 추가

--- src/main/java/roomescape/domain/Member.java
+++ src/main/java/roomescape/domain/Member.java
@@ -1,11 +1,13 @@
 package roomescape.domain;
 
+import java.util.Objects;
 import lombok.Getter;
 
 @Getter
 public class Member {
 
     private static final String KOREAN_WORDS_REGEX = "^[가-힣]+$";
+    private static final String EMAIL_FORMAT_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
     private static final int NAME_MAX_SIZE = 5;
 
     private final Long id;
@@ -15,8 +17,10 @@ public class Member {
     private final MemberRole role;
 
     public Member(final Long id, final String name, final String email, final String password, final MemberRole role) {
+        validateNotNulls(name, email, password, role);
         validateNameKoreanWords(name);
         validateNameSize(name);
+        validateEmailFormat(email);
         this.id = id;
         this.name = name;
         this.email = email;
@@ -36,6 +40,13 @@ public class Member {
         return this.password.equals(password);
     }
 
+    private void validateNotNulls(String name, String email, String password, MemberRole role) {
+        Objects.requireNonNull(name, "멤버 이름을 입력 해 주세요.");
+        Objects.requireNonNull(email, "이메일을 입력 해 주세요.");
+        Objects.requireNonNull(password, "비밀번호를 입력 해 주세요.");
+        Objects.requireNonNull(role, "역할을 입력 해 주세요.");
+    }
+
     private void validateNameKoreanWords(String name) {
         if (!name.matches(KOREAN_WORDS_REGEX)) {
             throw new IllegalArgumentException("[ERROR] 이름은 한국어로만 입력해 주세요.");
@@ -46,6 +57,12 @@ public class Member {
         if (name.length() > NAME_MAX_SIZE) {
             throw new IllegalArgumentException(
                     "[ERROR] 이름은 " + NAME_MAX_SIZE + "글자 이내로 입력해 주세요. 현재 길이는 " + name.length() + "글자 입니다.");
+        }
+    }
+
+    private void validateEmailFormat(String email) {
+        if (email == null || !email.matches(EMAIL_FORMAT_REGEX)) {
+            throw new IllegalArgumentException("잘못된 이메일 형식입니다: " + email);
         }
     }
 }
--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -2,6 +2,7 @@ package roomescape.domain;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
@@ -18,7 +19,7 @@ public class Reservation {
 
     public Reservation(final Long id, final LocalDate date, final Member member, final ReservationTime time,
                        final ReservationTheme theme) {
-
+        validateNotNulls(date, member, time, theme);
         this.id = id;
         this.date = date;
         this.member = member;
@@ -41,5 +42,12 @@ public class Reservation {
 
     public LocalDateTime toDateTime() {
         return LocalDateTime.of(date, time.getStartAt());
+    }
+
+    private void validateNotNulls(LocalDate date, Member member, ReservationTime time, ReservationTheme theme) {
+        Objects.requireNonNull(date, "예약 날짜를 입력 해 주세요.");
+        Objects.requireNonNull(member, "예약자를 입력 해 주세요.");
+        Objects.requireNonNull(time, "예약 시간을 입력 해 주세요.");
+        Objects.requireNonNull(theme, "예약 테마를 입력 해 주세요.");
     }
 }
