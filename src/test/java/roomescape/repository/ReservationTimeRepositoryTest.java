53aadff961a7b6b1e56cbed014b313d6e54e4da1
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-05-16 15:58:34 +0900

    refactor: 예약 시간 레파지토리 테스트 disabled 애노테이션 제거

--- src/test/java/roomescape/repository/ReservationTimeRepositoryTest.java
+++ src/test/java/roomescape/repository/ReservationTimeRepositoryTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalTime;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +16,6 @@ import roomescape.domain.ReservationTime;
 
 @JdbcTest
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
-@Disabled
 class ReservationTimeRepositoryTest {
 
     ReservationTimeRepository timeRepository;
