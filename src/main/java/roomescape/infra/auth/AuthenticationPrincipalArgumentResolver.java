abe44933a47248b34036711cb11c82b9f04bfac1
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-05-16 16:41:42 +0900

    fix(auth): MemberService 의존성 제거, exception handling 예외 수정
     - MemberService 의존성을 제거하여 코드 간결화
     - BadRequestException을 HttpClientErrorException으로 변경하고 적절한 상태코드 추가

--- src/main/java/roomescape/infra/auth/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/roomescape/infra/auth/AuthenticationPrincipalArgumentResolver.java
@@ -2,22 +2,20 @@ package roomescape.infra.auth;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import org.apache.coyote.BadRequestException;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-import roomescape.service.MemberService;
 
 @Component
 public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
-    private final MemberService memberService;
     private final AuthService authService;
 
-    public AuthenticationPrincipalArgumentResolver(MemberService memberService, AuthService authService) {
-        this.memberService = memberService;
+    public AuthenticationPrincipalArgumentResolver(AuthService authService) {
         this.authService = authService;
     }
 
@@ -28,12 +26,12 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
-                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             return authService.getMemberFromCookies(cookies);
         }
-        throw new BadRequestException("인증 실패");
+        throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED, "인증 실패");
     }
 }
