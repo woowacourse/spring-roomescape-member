3b1971c03cab88c21ad9c1ed1b3f85ac30c4ad03
Lee Dong Yeop <s01054830455@gmail.com>
2025-05-19 19:12:25 +0900

    feat: 토큰 파싱 과정 예외처리 추가

--- src/main/java/roomescape/common/auth/JwtTokenProvider.java
+++ src/main/java/roomescape/common/auth/JwtTokenProvider.java
@@ -20,10 +20,25 @@ public class JwtTokenProvider {
     }
 
     public Long getSubjectFromPayloadBy(final String token) {
-        return Long.valueOf(Jwts.parserBuilder()
-                .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
-                .build()
+        validateToken(token);
+        return Long.valueOf(getSubject(token));
+    }
+
+    private void validateToken(final String token) {
+        try {
+            getParse().parseClaimsJws(token);
+        } catch (final ExpiredJwtException e) {
+            throw new InvalidAuthException("유효시간이 만료된 토큰입니다.");
+        } catch (final MalformedJwtException | SignatureException | UnsupportedJwtException e) {
+            throw new InvalidAuthException("유효하지 않은 토큰입니다.");
+        } catch (final IllegalArgumentException e) {
+            throw new InvalidAuthException("토큰이 비어있습니다.");
+        }
+    }
+
+    private String getSubject(final String token) {
+        return getParse()
                 .parseClaimsJws(token)
-                .getBody().getSubject());
+                .getBody().getSubject();
     }
 }
