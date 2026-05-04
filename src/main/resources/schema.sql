378e09fe8c08ead231b180d8cc4a7b918deca76e
K-iise <kimyungi1004@naver.com>
2026-05-04 16:05:11 +0900

    chore: theme 테이블 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,8 +8,17 @@ CREATE TABLE reservation (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
     date    VARCHAR(255) NOT NULL,
-    time_id BIGINT,
+    theme_id BIGINT NOT NULL,
+    time_id BIGINT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
 );
 
+CREATE TABLE theme (
+    id      BIGINT      NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(20) NOT NULL,
+    description TEXT,
+    url     VARCHAR(255) NOT NULL
+    PRIMARY KEY (id)
+);
