31cf5c34373afd1d84fbef83e7b5952c6d5db432
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-05-04 22:00:18 +0900

    test: 중복된 예약 예외 테스트

--- src/test/java/roomescape/service/ReservationDuplicateTest.java
+++ src/test/java/roomescape/service/ReservationDuplicateTest.java
@@ -1,13 +1,13 @@
 package roomescape.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import roomescape.dto.request.ReservationRequest;
 
@@ -18,23 +18,25 @@ class ReservationDuplicateTest {
     @Autowired
     private ReservationService reservationService;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
     @DisplayName("이미 존재하는 예약 건과 중복으로 예약하면 예외가 발생한다")
     void throwsException_whenDuplicateReservationExists() {
+        jdbcTemplate.update("INSERT INTO reservation_time (id, start_at) VALUES (?, ?)", 1, "10:00");
+        jdbcTemplate.update("INSERT INTO theme (id, name, description, url) VALUES (?, ?, ?, ?)", 1, "테마1", "설명", "url");
 
-        ReservationRequest request = new ReservationRequest("아나키", LocalDate.of(2026, 5, 1), 1L, 1L);
+
+        jdbcTemplate.update("""
+                INSERT INTO reservation (name, date, time_id, theme_id)
+                VALUES (?, ?, ?, ?)
+            """, "브라운", "2026-05-04", 1, 1);
+
+        ReservationRequest request = new ReservationRequest("아나키", LocalDate.of(2026, 5, 4), 1L, 1L);
 
         assertThatThrownBy(() -> reservationService.save(request))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("이미 존재하는 예약 건입니다.");
-    }
-
-    @Test
-    @DisplayName("중복되지 않는 시간에 예약을 하면 통과한다.")
-    void 중복이_없는_정상_예약_테스트() {
-
-        ReservationRequest request = new ReservationRequest("아나키", LocalDate.of(2026, 5, 4), 1L, 1L);
-
-        assertDoesNotThrow(() -> reservationService.save(request));
     }
 }
