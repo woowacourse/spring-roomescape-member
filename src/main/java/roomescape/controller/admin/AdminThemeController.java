1031a245888599acfb634df6999a0a6fa6e0b121
yItWorks <poiu7825@pusan.ac.kr>
2026-05-04 19:49:33 +0900

    fix: request mapping 추가

--- src/main/java/roomescape/controller/admin/AdminThemeController.java
+++ src/main/java/roomescape/controller/admin/AdminThemeController.java
@@ -4,13 +4,15 @@ import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import roomescape.dto.request.ThemeRequest;
 import roomescape.dto.response.ThemeResponse;
 import roomescape.service.ThemeService;
 
-@RestController("/admin/theme")
+@RestController
+@RequestMapping("/admin/theme")
 public class AdminThemeController {
     private final ThemeService themeService;
 
