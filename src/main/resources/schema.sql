a1caf2ca13ae13a26235e302a1bc3af307acb5e4
choubung <choubung@sookmyung.ac.kr>
2026-05-04 16:15:12 +0900

    docs : sql 스키마 업데이트 - theme 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,6 +4,14 @@ CREATE TABLE reservation_time (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE theme (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  image_url VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE reservation (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
