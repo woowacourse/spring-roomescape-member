5dec948a7abce8b971e5be2a3788fdb8c4dc9cfa
sauter001 <lsj457@naver.com>
2026-05-04 16:21:55 +0900

    docs: 테마 목록 조회 API 추가
    Signed-off-by: sauter001 <lsj457@naver.com>

---
+++ src/main/java/roomescape/controller/ThemeController.java
@@ -0,0 +1,12 @@
+package roomescape.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/themes")
+public class ThemeController {
+    @GetMapping
+
+}
