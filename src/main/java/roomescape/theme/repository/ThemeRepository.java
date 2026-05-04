9f9340f4d2017e5dd7b98d1d27f65e8299bb7968
Yoonhojoon <dbsghwns1209@gmail.com>
2026-05-04 15:46:25 +0900

    refactor: ThemeRepository Repository빈 추가

--- src/main/java/roomescape/theme/repository/ThemeRepository.java
+++ src/main/java/roomescape/theme/repository/ThemeRepository.java
@@ -1,8 +1,10 @@
 package roomescape.theme.repository;
 
 import java.util.List;
+import org.springframework.stereotype.Repository;
 import roomescape.theme.domain.Theme;
 
+@Repository
 public interface ThemeRepository {
     List<Theme> findAll();
 
