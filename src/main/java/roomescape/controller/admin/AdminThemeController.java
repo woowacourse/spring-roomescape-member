33c72aa18aa9c2afe205e07da13e190ce527cdcb
yItWorks <poiu7825@pusan.ac.kr>
2026-05-04 19:38:07 +0900

    feat: 관리자 테마 추가 API 추가

---
+++ src/main/java/roomescape/controller/admin/AdminThemeController.java
@@ -0,0 +1,32 @@
+package roomescape.controller.admin;
+
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import roomescape.dto.request.ThemeRequest;
+import roomescape.dto.response.ThemeResponse;
+import roomescape.service.ThemeService;
+
+@RestController("/admin/theme")
+public class AdminThemeController {
+    private final ThemeService themeService;
+
+    public AdminThemeController(ThemeService themeService) {
+        this.themeService = themeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ThemeResponse> createTheme(@RequestBody ThemeRequest request) {
+        ThemeResponse response = themeService.create(request);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(response);
+    }
+}
