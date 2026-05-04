58846a36b9701f1a18a1f55778140b7f661b29ee
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-05-04 20:40:30 +0900

    chore: 초기화 Mock 데이터 설정

---
+++ src/main/resources/mock-reservations.sql
@@ -0,0 +1,71 @@
+-- 🥇 1등 (Theme 1: 버려진 정신병원) - 총 8건
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('포비', '2026-05-03', 1, 1);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('리사', '2026-05-02', 2, 1);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('브리', '2026-05-01', 3, 1);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('제이슨', '2026-04-30', 4, 1);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('브라운', '2026-04-29', 5, 1);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('네오', '2026-04-28', 6, 1);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('라이언', '2026-04-27', 7, 1);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('어피치', '2026-05-03', 8, 1);
+
+-- 🥈 2등 (Theme 7: 오리엔트 특급 살인) - 총 7건
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('포비', '2026-05-02', 3, 7);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('브리', '2026-05-01', 5, 7);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('제이슨', '2026-04-30', 2, 7);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('네오', '2026-04-29', 4, 7);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('라이언', '2026-04-28', 6, 7);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('리사', '2026-04-27', 8, 7);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('브라운', '2026-05-03', 1, 7);
+
+-- 🥉 3등 (Theme 13: 화성 기지 SOS) - 총 6건
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('토미', '2026-05-03', 2, 13);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('제인', '2026-05-02', 4, 13);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('포비', '2026-05-01', 6, 13);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('리사', '2026-04-30', 8, 13);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('브리', '2026-04-29', 1, 13);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('제이슨', '2026-04-28', 3, 13);
+
+-- 🎖️ 4등 (Theme 19: 할머니의 낡은 일기장) - 총 5건
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('앨리스', '2026-05-03', 3, 19);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('밥', '2026-05-02', 5, 19);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('찰리', '2026-05-01', 7, 19);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('데이비드', '2026-04-30', 1, 19);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('이브', '2026-04-29', 2, 19);
+
+-- 🎖️ 공동 5등 (Theme 25: 세기의 금고 털기) - 총 4건
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('포비', '2026-05-03', 4, 25);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('브리', '2026-05-01', 2, 25);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('리사', '2026-04-29', 7, 25);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('네오', '2026-04-27', 5, 25);
+
+-- 🎖️ 공동 5등 (Theme 2: 자정의 해부학 교실) - 총 4건
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('제이슨', '2026-05-02', 1, 2);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('브라운', '2026-04-30', 3, 2);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('라이언', '2026-04-28', 5, 2);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('어피치', '2026-04-27', 4, 2);
+
+-- 🎖️ 공동 7등 (Theme 8: 미제사건 전담반) - 총 3건
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('포비', '2026-05-03', 5, 8);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('리사', '2026-05-01', 8, 8);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('브리', '2026-04-29', 6, 8);
+
+-- 🎖️ 공동 7등 (Theme 14: 타임 파라독스) - 총 3건
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('브라운', '2026-05-02', 6, 14);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('네오', '2026-04-30', 7, 14);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('라이언', '2026-04-28', 8, 14);
+
+-- 🎖️ 공동 7등 (Theme 20: 우체통의 비밀) - 총 3건
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('토미', '2026-05-03', 6, 20);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('제인', '2026-05-01', 4, 20);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('앨리스', '2026-04-29', 3, 20);
+
+-- 🎖️ 10등 (Theme 26: 스파이 미션) - 총 2건
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('밥', '2026-05-02', 7, 26);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('찰리', '2026-04-30', 5, 26);
+
+-- 엑스트라 데이터 (순위권 밖: 각 1건씩) - 총 5건
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('포비', '2026-05-03', 7, 3);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('리사', '2026-05-02', 8, 9);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('브리', '2026-05-01', 1, 15);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('제이슨', '2026-04-30', 6, 21);
+INSERT INTO reservation (name, date, time_id, theme_id) VALUES ('라이언', '2026-04-28', 2, 27);
---
+++ src/main/resources/mock-themes.sql
@@ -0,0 +1,39 @@
+-- 공포 / 스릴러 (6개)
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('버려진 정신병원', '마지막 병동에 남겨진 환자의 진료 기록을 찾아 탈출하라.', 'https://loremflickr.com/400/300/hospital,horror', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('자정의 해부학 교실', '밤 12시, 해부학 실습실에서 들려오는 정체불명의 소리.', 'https://loremflickr.com/400/300/anatomy,dark', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('흉가 체험 생방송', '돌아오지 못한 유명 BJ가 남긴 마지막 카메라 영상을 확인하라.', 'https://loremflickr.com/400/300/haunted,house', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('살인마의 지하실', '납치된 후 눈을 떠보니 지하실. 살인마가 돌아오기 전까지 남은 시간은 60분.', 'https://loremflickr.com/400/300/basement,creepy', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('저주받은 비디오테이프', '재생하는 순간부터 조여오는 저주를 풀고 테이프를 파괴하라.', 'https://loremflickr.com/400/300/vhs,tape', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('피의 여관', '친절했던 여관 주인의 소름 돋는 비밀. 체크아웃은 불가능합니다.', 'https://loremflickr.com/400/300/motel,blood', '01:00:00');
+
+-- 추리 / 미스터리 (6개)
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('오리엔트 특급 살인', '달리는 기차 안에서 발생한 밀실 살인, 숨겨진 유언장을 찾아라.', 'https://loremflickr.com/400/300/train,mystery', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('미제사건 전담반', '공소시효 만료 직전, 15년 전의 사건 파일에서 결정적 단서를 찾아라.', 'https://loremflickr.com/400/300/detective,files', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('괴도 루팡의 예고장', '철통 보안을 자랑하는 루브르 박물관의 다이아몬드를 지켜내라.', 'https://loremflickr.com/400/300/museum,diamond', '01:10:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('기억의 조각', '눈을 떠보니 나는 살인 사건의 유력한 용의자. 내 진짜 기억을 되찾아야 한다.', 'https://loremflickr.com/400/300/memory,puzzle', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('셜록 홈즈의 조수', '런던 뒷골목에서 발생한 연쇄 실종 사건의 진범을 추리하라.', 'https://loremflickr.com/400/300/london,sherlock', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('인형의 집', '완벽하게 세팅된 인형의 집 안, 모든 인형의 시선이 한 곳을 향해 있다.', 'https://loremflickr.com/400/300/doll,creepy', '01:00:00');
+
+-- SF / 판타지 (6개)
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('화성 기지 SOS', '외부 통신 두절, 산소 고갈까지 단 1시간. 생존 포드를 가동하라.', 'https://loremflickr.com/400/300/mars,space', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('타임 파라독스', '멸망을 막기 위해 과거로 돌아갔지만, 나침반이 고장 났다.', 'https://loremflickr.com/400/300/clock,time', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('마법사의 비밀 서재', '봉인된 금서의 주문을 해독하여 마왕의 부활을 막아라.', 'https://loremflickr.com/400/300/magic,library', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('AI의 반란', '인류를 통제하려는 슈퍼컴퓨터의 메인 서버에 잠입해 시스템을 셧다운 하라.', 'https://loremflickr.com/400/300/cyber,ai', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('엘프의 숲', '시들어가는 세계수를 살리기 위해 전설 속 세계수의 눈물을 찾아라.', 'https://loremflickr.com/400/300/forest,fantasy', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('평행우주', '거울 너머의 또 다른 나와 협력하여 붕괴하는 차원을 탈출하라.', 'https://loremflickr.com/400/300/mirror,universe', '01:10:00');
+
+-- 감성 / 드라마 (6개)
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('할머니의 낡은 일기장', '돌아가신 할머니가 남긴 편지를 따라가는 따뜻한 시간 여행.', 'https://loremflickr.com/400/300/diary,vintage', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('우체통의 비밀', '10년 전 첫사랑에게 보냈던, 전달되지 못한 편지의 행방을 찾아라.', 'https://loremflickr.com/400/300/mailbox,letter', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('오르골의 노래', '망가진 오르골을 고치며 잃어버린 유년 시절의 꿈을 되찾는 여정.', 'https://loremflickr.com/400/300/musicbox,antique', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('시한부의 버킷리스트', '남은 시간은 단 하루, 그녀가 가장 원했던 마지막 소원 들어주기.', 'https://loremflickr.com/400/300/sunset,memory', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('꿈을 파는 상점', '당신이 가장 원했던 환상을 보여드립니다. 단, 대가를 지불해야 합니다.', 'https://loremflickr.com/400/300/dream,shop', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('별 헤는 밤', '우주로 떠나 돌아오지 않는 우주비행사 남편이 남긴 마지막 별자리 지도.', 'https://loremflickr.com/400/300/stars,nightsky', '01:00:00');
+
+-- 액션 / 잠입 (6개)
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('세기의 금고 털기', '라스베이거스 최대 카지노의 지하 금고를 털기 위한 완벽한 작전.', 'https://loremflickr.com/400/300/vault,heist', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('스파이 미션', '적국 한가운데 고립되었다. 일급 기밀 문서를 빼내고 헬기 접견지로 향하라.', 'https://loremflickr.com/400/300/spy,helicopter', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('알카트라즈 탈출', '탈옥률 0%의 악명 높은 교도소, 간수의 눈을 피해 바다로 뛰어들어라.', 'https://loremflickr.com/400/300/prison,escape', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('마피아 언더커버', '조직의 보스가 거래하는 현장에 잠입해 핵심 증거품을 빼돌려라.', 'https://loremflickr.com/400/300/mafia,suit', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('좀비 바이러스 연구소', '좀비가 가득한 연구소에서 백신 샘플을 챙겨 옥상으로 대피하라.', 'https://loremflickr.com/400/300/zombie,lab', '01:00:00');
+INSERT INTO theme (name, description, thumbnail_image_url, duration_time) VALUES ('폭탄 해체반', '도심 지하철에 설치된 시한폭탄, 빨간 선일까 파란 선일까? 남은 시간은 10분.', 'https://loremflickr.com/400/300/bomb,timer', '01:00:00');
