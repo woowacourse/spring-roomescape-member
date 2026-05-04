a66839a15685fea11457c6b36c3f30a413db7631
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-05-04 15:46:06 +0900

    feat: 테마 추가 및 삭제 기능 검증 추가

--- src/main/java/roomescape/theme/presentation/dto/ThemeRequest.java
+++ src/main/java/roomescape/theme/presentation/dto/ThemeRequest.java
@@ -1,14 +1,20 @@
 package roomescape.theme.presentation.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 import lombok.Builder;
 import roomescape.theme.domain.Theme;
 
 @Builder
 public record ThemeRequest(
+        @NotBlank
         String name,
+        @NotBlank
         String thumbnailImageUrl,
+        @NotBlank
         String description,
+        @NotNull
         LocalTime durationTime
 ) {
     public static Theme toEntity(ThemeRequest request) {
