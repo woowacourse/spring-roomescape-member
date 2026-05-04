d6b6fa136b16335b25f1fd309182eda01cb9615e
armd482 <armd482@naver.com>
2026-05-04 15:12:02 +0900

    feat: addThemeRequest 구현

---
+++ src/main/java/roomescape/dto/theme/AddThemeRequest.java
@@ -0,0 +1,13 @@
+package roomescape.dto.theme;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record AddThemeRequest(
+        @NotBlank(message = "테마 이름은 반드시 포함되어야 합니다.")
+        String name,
+        @NotBlank(message = "테마 설명은 반드시 포함되어야 합니다.")
+        String description,
+        @NotBlank(message = "썸네일 이미지는 반드시 포함되어야 합니다.")
+        String imageUrl
+) {
+}
