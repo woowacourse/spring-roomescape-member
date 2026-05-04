ab8f54f8073f7e816f8c6d922ae5fda57f910603
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-05-04 20:19:07 +0900

    feat: 스케줄 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,9 +17,20 @@ CREATE TABLE reservation
 
 CREATE TABLE theme
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    description TEXT,
+    id            BIGINT       NOT NULL AUTO_INCREMENT,
+    name          VARCHAR(50)  NOT NULL,
+    description   TEXT,
     thumbnail_url VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE schedule
+(
+    id       BIGINT NOT NULL AUTO_INCREMENT,
+    date     DATE   NOT NULL,
+    time_id  BIGINT,
+    theme_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
 );
