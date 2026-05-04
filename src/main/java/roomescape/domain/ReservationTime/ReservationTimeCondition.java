955852a5608d6536ca7a543527315c55e59879f6
armd482 <armd482@naver.com>
2026-05-04 18:54:01 +0900

    refactor: 값 validate 구현 추가

--- src/main/java/roomescape/domain/ReservationTime/ReservationTimeCondition.java
+++ src/main/java/roomescape/domain/ReservationTime/ReservationTimeCondition.java
@@ -1,4 +1,32 @@
 package roomescape.domain.ReservationTime;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import roomescape.exception.ErrorMessage;
+import roomescape.exception.ReservationCommandException;
+import roomescape.exception.ReservationTimeConditionException;
+
 public record ReservationTimeCondition(String date, long themeId) {
+    public ReservationTimeCondition {
+        validate(date, themeId);
+    }
+
+    private static void validate(String date, long themeId) {
+        validateDate(date);
+        validateThemeId(themeId);
+    }
+
+    private static void validateDate(String date) {
+        try {
+            LocalDate.parse(date);
+        } catch (DateTimeParseException e) {
+            throw new ReservationCommandException(ErrorMessage.INVALID_DATE_FORMAT);
+        }
+    }
+
+    private static void validateThemeId(long themeId) {
+        if(themeId <= 0) {
+            throw new ReservationTimeConditionException(ErrorMessage.INVALID_THEME_ID);
+        }
+    }
 }
