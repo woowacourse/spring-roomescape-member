30c13ca5c23fb735070419d7f486c2b108bca550
zzaekkii <zzaekkii@gmail.com>
2026-05-04 19:57:11 +0900

    feat: 테마 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,16 @@ CREATE TABLE reservation_time
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     start_at TIME NOT NULL,
+    end_at TIME NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE theme
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    thumbnail_url VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -10,8 +20,9 @@ CREATE TABLE reservation
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
     date    DATE NOT NULL,
-    time_id BIGINT,
+    time_id BIGINT NOT NULL,
+    theme_id BIGINT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
-
