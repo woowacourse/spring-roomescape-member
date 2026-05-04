8fb0f4e8c068cf9664faf39c71d11d9243265a88
armd482 <armd482@naver.com>
2026-05-04 16:07:57 +0900

    feat: getAllTheme 구현, addTheme 구현

--- src/main/java/roomescape/controller/ThemeController.java
+++ src/main/java/roomescape/controller/ThemeController.java
@@ -1,6 +1,9 @@
 package roomescape.controller;
 
 import jakarta.validation.Valid;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.Theme.Theme;
 import roomescape.domain.Theme.ThemeCommand;
 import roomescape.dto.theme.AddThemeRequest;
+import roomescape.dto.theme.AddThemeResponse;
+import roomescape.dto.theme.AllThemeResponse;
 import roomescape.service.ThemeService;
 
 @RestController
@@ -22,14 +28,18 @@ public class ThemeController {
     }
 
     @GetMapping()
-    public void getThemes() {
-        /*service*/
+    public ResponseEntity<AllThemeResponse> getThemes() {
+        List<Theme> themes = themeService.getAllTheme();
+
+        return new ResponseEntity<>(new AllThemeResponse(themes), HttpStatus.OK);
     }
 
     @PostMapping()
-    public void addTheme(@RequestBody @Valid AddThemeRequest addThemeRequest) {
+    public ResponseEntity<AddThemeResponse> addTheme(@RequestBody @Valid AddThemeRequest addThemeRequest) {
         ThemeCommand themeCommand = addThemeRequest.from();
-        themeService.addTheme(themeCommand);
+        Theme addedTheme = themeService.addTheme(themeCommand);
+
+        return new ResponseEntity<>(new AddThemeResponse(addedTheme), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
