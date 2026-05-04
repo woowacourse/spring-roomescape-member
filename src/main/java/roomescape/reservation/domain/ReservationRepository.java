c0255e8b1794e51a954e64ea00ca5fa5126558fb
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-05-05 00:39:28 +0900

    refactor: 변수명 수정

--- src/main/java/roomescape/reservation/domain/ReservationRepository.java
+++ src/main/java/roomescape/reservation/domain/ReservationRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface ReservationRepository {
     Reservation save(Reservation reservation);
     List<Reservation> findAll();
-    List<Reservation> findByThemeAndDate(Long aLong, LocalDate date);
+    List<Reservation> findByThemeAndDate(Long themeId, LocalDate date);
     boolean existsByReservationTime(Long timeId);
     boolean existsById(Long id);
     void deleteById(Long id);
