4e0ca39cce1e6d649b9446e632d6f9a2c80ee21f
alstj2384 <ass91160@naver.com>
2026-05-04 19:11:35 +0900

    feat: 테마 도메인 추가

---
+++ src/main/java/roomescape/domain/Theme.java
@@ -0,0 +1,15 @@
+package roomescape.domain;
+
+public class Theme {
+    private final String name;
+    private final String description;
+    private final String thumbnailUrl;
+
+    public Theme(String name, String description, String thumbnailUrl) {
+        this.name = name;
+        this.description = description;
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+
+}
