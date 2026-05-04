c4b016a9292500c539cf5f2a58db97caa653e1ee
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-05-05 00:39:17 +0900

    refactor: dto로 수정

--- src/main/java/roomescape/time/presentation/dto/AvailableReservationTimeResponse.java
+++ src/main/java/roomescape/time/presentation/dto/AvailableReservationTimeResponse.java
@@ -3,17 +3,18 @@ package roomescape.time.presentation.dto;
 import java.util.List;
 import lombok.Builder;
 import roomescape.theme.domain.Theme;
+import roomescape.theme.presentation.dto.ThemeResponse;
 import roomescape.time.domain.ReservationTime;
 
 @Builder
 public record AvailableReservationTimeResponse(
-        Theme theme,
-        List<ReservationTime> times
+        ThemeResponse theme,
+        List<ReservationTimeResponse> times
 ) {
     public static AvailableReservationTimeResponse from(Theme theme, List<ReservationTime> times) {
         return AvailableReservationTimeResponse.builder()
-                .times(times)
-                .theme(theme)
+                .times(times.stream().map(ReservationTimeResponse::from).toList())
+                .theme(ThemeResponse.from(theme))
                 .build();
     }
 }
