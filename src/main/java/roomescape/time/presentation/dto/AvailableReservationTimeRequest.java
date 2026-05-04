1d61791e7d5a8bbb6c978d419d3f61952b2c7bf4
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-05-05 02:01:36 +0900

    refactor: dto 검증 추가

--- src/main/java/roomescape/time/presentation/dto/AvailableReservationTimeRequest.java
+++ src/main/java/roomescape/time/presentation/dto/AvailableReservationTimeRequest.java
@@ -1,9 +1,12 @@
 package roomescape.time.presentation.dto;
 
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record AvailableReservationTimeRequest(
+        @NotNull
         Long themeId,
+        @NotNull
         LocalDate date
 ) {
 }
