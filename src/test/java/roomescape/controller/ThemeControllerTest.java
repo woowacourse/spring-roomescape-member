5d0d6d17652c9ac71635ea4e2eda2e928099516b
yItWorks <poiu7825@pusan.ac.kr>
2026-05-04 20:52:49 +0900

    test: 예약 가능한 시간 조회 API 테스트 추가

--- src/test/java/roomescape/controller/ThemeControllerTest.java
+++ src/test/java/roomescape/controller/ThemeControllerTest.java
@@ -1,10 +1,10 @@
 package roomescape.controller;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
 import static org.hamcrest.Matchers.is;
@@ -14,25 +14,30 @@ import static org.hamcrest.Matchers.is;
 class ThemeControllerTest {
     @Autowired
     private ThemeController themeController;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     void 예약_가능한_시간_조회_API() {
-        // when & then
-        RestAssured.given().log().all()
-                .when().get("/themes/1/available-time?date=2026-05-01")
-                .then().log().all()
-                .statusCode(200)
-                .body("size()", is(4));
-    }
+        // given
+        // 1. 테마 추가
+        jdbcTemplate.execute("INSERT INTO theme (name, description, url) VALUES ('우테코 공포물', '레벨2 미션의 공포', '/horror')");
 
-    @Test
-    @DisplayName("최근 1주동안 예약이 많았던 테마를 조회하는 정상 테스트")
-    void 테마_조회_API() {
+        // 2. 전체 시간대 추가 (10:00, 13:00)
+        jdbcTemplate.execute("INSERT INTO reservation_time (start_at) VALUES ('10:00:00')");
+        jdbcTemplate.execute("INSERT INTO reservation_time (start_at) VALUES ('13:00:00')");
+
+        // 3. 이미 예약된 내역 추가 (10:00 예약됨)
+        jdbcTemplate.update("""
+                    INSERT INTO reservation (name, date, time_id, theme_id) 
+                    VALUES (?, ?, ?, ?)
+                """, "브라운", "2026-05-04", 1, 1);
+
         // when & then
         RestAssured.given().log().all()
-                .when().get("/themes/popular?limit=10")
+                .when().get("/theme/1/available-time?date=2026-05-04")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(7));
+                .body("size()", is(1));
     }
 }
