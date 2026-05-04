cb5f51030ef9ac3db892e20d432f52441a89b754
Giyoul <cla12093@gmail.com>
2026-05-04 18:08:00 +0900

    feat: 관리자 예약 조회 기능

---
+++ src/main/java/roomescape/service/AdminReservationService.java
@@ -0,0 +1,22 @@
+package roomescape.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import roomescape.dto.ReservationResponse;
+import roomescape.repository.ReservationDao;
+
+@Service
+public class AdminReservationService {
+
+    private final ReservationDao reservationDao;
+
+    public AdminReservationService(ReservationDao reservationDao) {
+        this.reservationDao = reservationDao;
+    }
+
+    public List<ReservationResponse> getAllReservations() {
+        return reservationDao.findAll().stream()
+                .map(ReservationResponse::from)
+                .toList();
+    }
+}
