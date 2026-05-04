7342cd04c787263b0c963aab9fa6ae7f23223bcf
Yoonhojoon <dbsghwns1209@gmail.com>
2026-05-04 15:23:08 +0900

    feat: Theme 도메인 생성

---
+++ src/main/java/roomescape/theme/domain/Theme.java
@@ -0,0 +1,31 @@
+package roomescape.theme.domain;
+
+public class Theme {
+  private final Long id;
+  private final String name;
+  private final String description;
+  private final String imageUrl;
+
+  private Theme(Long id, String name, String description, String imageUrl) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,16 @@
 DROP TABLE IF EXISTS reservation;
 DROP TABLE IF EXISTS reservation_time;
 
-CREATE TABLE reservation_time (
+CREATE TABLE reservation_time
+(
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     start_at VARCHAR(255) NOT NULL,
     end_at   VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation (
+CREATE TABLE reservation
+(
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
     date    VARCHAR(255) NOT NULL,
