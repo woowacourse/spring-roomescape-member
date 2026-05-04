3ceb8f15305b114aa060c1164120bf30fc43b5bb
khyej <khyej.h@gmail.com>
2026-05-04 15:29:56 +0900

    chore: schema.sql 테이블 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,27 @@
 CREATE TABLE IF NOT EXISTS reservation_time
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL,
+    id       BIGINT NOT NULL AUTO_INCREMENT,
+    start_at TIME   NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS theme
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description TEXT         NOT NULL,
+    thumbnail   VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS reservation
 (
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255) NOT NULL,
-    date    VARCHAR(255) NOT NULL,
-    time_id BIGINT,
+    id        BIGINT       NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(255) NOT NULL,
+    theme_id  BIGINT       NOT NULL,
+    date      DATE         NOT NULL,
+    time_id   BIGINT       NOT NULL,
     PRIMARY KEY (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id)
 );
-
