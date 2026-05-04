a895a28cf5676500d5d752eaad7f2ddeb30be2ff
yeo-li <parkseongyeol2110@gmail.com>
2026-05-04 19:51:43 +0900

    feat(theme): 사용자 테마 조회 기능 추가

--- src/main/java/roomescape/domain/theme/controller/ThemeController.java
+++ src/main/java/roomescape/domain/theme/controller/ThemeController.java
@@ -24,7 +24,7 @@ public class ThemeController {
         this.themeService = themeService;
     }
 
-    @GetMapping("/admin/themes")
+    @GetMapping("/themes")
     public ResponseEntity<List<ThemeResponseDTO>> getThemes() {
         return ResponseEntity.ok(themeService.getThemes());
     }
