328a1e964db0dc9b40ea365231bde9864cbe97bc
yItWorks <poiu7825@pusan.ac.kr>
2026-05-04 19:49:07 +0900

    test: 관리자 테마 추가 API 테스트 추가

---
+++ src/test/java/roomescape/controller/admin/AdminThemeControllerTest.java
@@ -0,0 +1,33 @@
+package roomescape.controller.admin;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import roomescape.controller.ReservationController;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class AdminThemeControllerTest {
+    @Autowired
+    private ReservationController reservationController;
+
+    @Test
+    void 테마_관리_API() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "공포");
+        params.put("description", "너무무서워");
+        params.put("url", "/horror");
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/admin/theme")
+                .then().log().all()
+                .statusCode(201);
+    }
+}
