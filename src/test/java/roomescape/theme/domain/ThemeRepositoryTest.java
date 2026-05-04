a08c1189cac9abb80d43b679f113910aa6237aac
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-05-04 15:43:32 +0900

    test: 단위 테스트 독립성 부여 및 테마 Service 테스트 작성

--- src/test/java/roomescape/theme/domain/ThemeRepositoryTest.java
+++ src/test/java/roomescape/theme/domain/ThemeRepositoryTest.java
@@ -7,8 +7,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 @SpringBootTest
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 class ThemeRepositoryTest {
 
     @Autowired
