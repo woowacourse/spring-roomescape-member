bae9a79e89f4054407aa391fe66c8df4913bcc8a
K-iise <kimyungi1004@naver.com>
2026-05-04 17:32:29 +0900

    fix: theme 테이블 스키마 순서 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,21 +4,20 @@ CREATE TABLE reservation_time (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE theme (
+    id      BIGINT      NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(100) NOT NULL,
+    description VARCHAR(1000),
+    url     VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE reservation (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
     date    VARCHAR(255) NOT NULL,
-    theme_id BIGINT NOT NULL,
     time_id BIGINT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id)
 );
 
-CREATE TABLE theme (
-    id      BIGINT      NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(20) NOT NULL,
-    description TEXT,
-    url     VARCHAR(255) NOT NULL
-    PRIMARY KEY (id)
-);
