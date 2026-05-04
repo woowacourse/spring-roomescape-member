d06cefb93fa70897f7135527fe4fe345557ded1a
Yoonhojoon <dbsghwns1209@gmail.com>
2026-05-04 15:35:17 +0900

    refactor: Theme withId 추가

--- src/main/java/roomescape/theme/domain/Theme.java
+++ src/main/java/roomescape/theme/domain/Theme.java
@@ -6,11 +6,19 @@ public class Theme {
   private final String description;
   private final String imageUrl;
 
+  public Theme(String name, String description, String imageUrl) {
+    this(null, name, description, imageUrl);
+  }
+
   private Theme(Long id, String name, String description, String imageUrl) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.imageUrl = imageUrl;
+  }
+
+  public Theme withId(Long id) {
+    return new Theme(id, this.name, this.description, this.imageUrl);
   }
 
   public Long getId() {
