0b0958a1e314a776c63164d467d258e2c5c53f20
mvg01 <dkms625@naver.com>
2026-05-04 15:57:34 +0900

    feat: 시작 시간 정각 검증 로직 추가

--- src/main/java/roomescape/model/ReservationTime.java
+++ src/main/java/roomescape/model/ReservationTime.java
@@ -3,12 +3,19 @@ package roomescape.model;
 import java.time.LocalTime;
 
 public class ReservationTime {
-    private Long id;
-    private LocalTime startAt;
+    private final Long id;
+    private final LocalTime startAt;
 
     public ReservationTime(Long id, LocalTime startAt) {
         this.id = id;
         this.startAt = startAt;
+        validateStartAt();
+    }
+
+    private void validateStartAt() {
+        if (startAt.getMinute() != 0) {
+            throw new IllegalArgumentException("[ERROR] 방탈출의 시작 시간은 정각이어야 합니다.");
+        }
     }
 
     public Long getId() {
