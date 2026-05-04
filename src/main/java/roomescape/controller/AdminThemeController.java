019c0d7b0d3b17711ad4a2692a177eb5a9e72935
Giyoul <cla12093@gmail.com>
2026-05-04 18:38:20 +0900

    feat: 테마 정보 추가 기능

---
+++ src/main/java/roomescape/controller/AdminThemeController.java
@@ -0,0 +1,30 @@
+
+package roomescape.controller;
+
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.dto.ThemeRequest;
+import roomescape.service.AdminThemeService;
+
+@RequestMapping("/admin/themes")
+@RestController
+public class AdminThemeController {
+    private final AdminThemeService adminThemeService;
+
+    public AdminThemeController(AdminThemeService adminThemeService){
+        this.adminThemeService = adminThemeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createTheme(@RequestBody ThemeRequest themeRequest) {
+        Long themeId = adminThemeService.save(themeRequest);
+        URI location = URI.create("/themes/" + themeId);
+        return ResponseEntity.created(location).build();
+    }
+
+
+}
