dae070317d2e3fab0f09a65a5412f2e1a3ae211c
snowshower <wonsb25@gmail.com>
2026-05-05 13:18:18 +0900

    refactor: 타임 클래스를 스케줄 클래스로 변경
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- src/main/java/roomescape/schedule/model/Schedule.java
+++ src/main/java/roomescape/schedule/model/Schedule.java
@@ -1,35 +1,64 @@
-package roomescape.model;
+package roomescape.schedule.model;
 
-import java.time.LocalTime;
+import roomescape.theme.model.Theme;
 
-public class ReservationTime {
+import java.time.LocalDateTime;
+
+public class Schedule {
 
     private Long id;
-    private LocalTime startTime;
+    private Theme theme;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
 
-    protected ReservationTime() {}
-
-    public ReservationTime(LocalTime startTime) {
-        this(null, startTime);
+    public Schedule() {
     }
 
-    public ReservationTime(Long id, LocalTime startTime) {
-        validateStartTime(startTime);
+    public Schedule(LocalDateTime startAt, LocalDateTime endAt, Theme theme) {
+        this(null, startAt, endAt, theme);
+    }
+
+    public Schedule(Long id, LocalDateTime startAt, LocalDateTime endAt, Theme theme) {
+        validateStartAt(startAt);
+        validateEndAt(endAt);
+        validateTheme(theme);
         this.id = id;
-        this.startTime = startTime;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.theme = theme;
     }
 
     public Long getId() {
         return id;
     }
 
-    public LocalTime getStartTime() {
-        return startTime;
+    public LocalDateTime getStartAt() {
+        return startAt;
     }
 
-    private void validateStartTime(LocalTime startTime) {
-        if (startTime == null) {
+    public LocalDateTime getEndAt() {
+        return endAt;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    private void validateStartAt(LocalDateTime startAt) {
+        if (startAt == null) {
             throw new IllegalArgumentException("예약 시작 시간은 필수입니다.");
+        }
+    }
+
+    private void validateEndAt(LocalDateTime endAt) {
+        if (endAt == null) {
+            throw new IllegalArgumentException("예약 시작 시간은 필수입니다.");
+        }
+    }
+
+    private void validateTheme(Theme theme) {
+        if (theme == null) {
+            throw new IllegalArgumentException("테마 정보는 필수입니다.");
         }
     }
 }
