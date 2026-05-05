c68edf5e5ef515d2c16c556ff34f51b422cd275c
Jiihyun <cwlgusc@gmail.com>
2026-05-05 14:19:08 +0900

    feat: 인기 테마 조회 API 추가
    - 테스트 미통과

--- src/test/java/roomescape/dao/ThemeDaoTest.java
+++ src/test/java/roomescape/dao/ThemeDaoTest.java
@@ -3,11 +3,14 @@ package roomescape.dao;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 import roomescape.domain.Theme;
 
 @JdbcTest
@@ -59,9 +62,45 @@ class ThemeDaoTest {
     }
 
     @Test
+    @Sql(scripts = {"/data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {"/truncate.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void 인기_테마_10개를_조회한다() {
+        // given
+        LocalDate startDate = LocalDate.of(2026, 4, 29);
+        LocalDate endDate = LocalDate.of(2026, 5, 5);
+
+        // when
+        List<Theme> popularThemes = themeDao.selectPopularThemesByPeriod(startDate, endDate);
+
+        // then
+        assertAll(
+                () -> assertThat(popularThemes.getFirst().getName()).isEqualTo("공포의 저택"),
+                () -> assertThat(popularThemes.get(1).getName()).isEqualTo("사라진 연구소"),
+                () -> assertThat(popularThemes.get(2).getName()).isEqualTo("시간 여행자"),
+                () -> assertThat(popularThemes.get(3).getName()).isEqualTo("감옥 탈출"),
+                () -> assertThat(popularThemes.get(4).getName()).isEqualTo("마법사의 방"),
+                () -> assertThat(popularThemes.get(5).getName()).isEqualTo("좀비 바이러스"),
+                () -> assertThat(popularThemes.get(6).getName()).isEqualTo("해적의 보물"),
+                () -> assertThat(popularThemes.get(7).getName()).isEqualTo("스파이 미션"),
+                () -> assertThat(popularThemes.get(8).getName()).isEqualTo("우주 정거장"),
+                () -> assertThat(popularThemes.get(9).getName()).isEqualTo("고대 유적")
+        );
+    }
+    //1. 공포의 저택       12건
+    //2. 사라진 연구소     10건
+    //3. 시간 여행자       9건
+    //4. 감옥 탈출         8건
+    //5. 마법사의 방       7건
+    //6. 좀비 바이러스     6건
+    //7. 해적의 보물       5건
+    //8. 스파이 미션       4건
+    //9. 우주 정거장       3건
+    //10. 고대 유적        2건
+
+    @Test
     void 아이디에_맞는_테마가_없으면_빈_객체를_반환한다() {
         // when
-        Optional<Theme> foundTheme = themeDao.selectById(1L);
+        Optional<Theme> foundTheme = themeDao.selectById(900L);
 
         // then
         assertThat(foundTheme).isEmpty();
