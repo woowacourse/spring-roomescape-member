beeb79191e187cf7d6028c9302623fe7bdaebcd0
snowshower <wonsb25@gmail.com>
2026-05-05 13:18:35 +0900

    test: 스케줄 클래스 테스트 작성
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- src/test/java/roomescape/schedule/model/ScheduleTest.java
+++ src/test/java/roomescape/schedule/model/ScheduleTest.java
@@ -1,51 +1,49 @@
-//package roomescape.schedule.model;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.assertj.core.api.Assertions.assertThatThrownBy;
-//
-//import java.time.LocalTime;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//class ReservationTimeTest {
-//
-//    @Test
-//    @DisplayName("예약 시간 객체를 성공적으로 생성한다.")
-//    void create_success() {
-//        // given
-//        LocalTime startTime = LocalTime.of(10, 0);
-//
-//        // when
-//        Schedule reservationTime = new Schedule(1L, startTime);
-//
-//        // then
-//        assertThat(reservationTime.getId()).isEqualTo(1L);
-//        assertThat(reservationTime.getStartTime()).isEqualTo(startTime);
-//    }
-//
-//    @Test
-//    @DisplayName("시작 시간이 null이면 객체 생성 시 예외가 발생한다.")
-//    void create_fail_null_time() {
-//        // given
-//        LocalTime startTime = null;
-//
-//        // when & then
-//        assertThatThrownBy(() -> new Schedule(1L, startTime))
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessage("예약 시작 시간은 필수입니다.");
-//    }
-//
-//    @Test
-//    @DisplayName("ID가 없어도 시작 시간이 있으면 객체 생성에 성공한다.")
-//    void create_success_without_id() {
-//        // given
-//        LocalTime startTime = LocalTime.of(13, 0);
-//
-//        // when
-//        Schedule reservationTime = new Schedule(startTime);
-//
-//        // then
-//        assertThat(reservationTime.getId()).isNull();
-//        assertThat(reservationTime.getStartTime()).isEqualTo(startTime);
-//    }
-//}
+package roomescape.schedule.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import roomescape.theme.model.Theme;
+
+class ScheduleTest {
+
+    @Test
+    void 스케줄을_성공적으로_생성한다() {
+        LocalDateTime startAt = LocalDateTime.of(2026,12,10,10, 0);
+        LocalDateTime endAt = LocalDateTime.of(2026,12,10,12, 0);
+        Theme theme = new Theme("테마", "설명", "경로");
+        Boolean isAvailable = true;
+
+        Schedule schedule = new Schedule(1L, startAt, endAt, theme);
+
+        assertThat(schedule.getId()).isEqualTo(1L);
+        assertThat(schedule.getStartAt()).isEqualTo(startAt);
+        assertThat(schedule.getEndAt()).isEqualTo(endAt);
+        assertThat(schedule.getTheme().getName()).isEqualTo(theme.getName());
+    }
+
+    @Test
+    void 시작_시간이_null이면_예외가_발생한다() {
+        LocalDateTime startAt = null;
+        LocalDateTime endAt = LocalDateTime.of(2026,12,10,12, 0);
+        Theme theme = new Theme("테마", "설명", "경로");
+
+        assertThatThrownBy(() -> new Schedule(startAt, endAt, theme))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("예약 시작 시간은 필수입니다.");
+    }
+
+    @Test
+    void ID가_없어도_시작_시간이_있으면_객체_생성에_성공한다() {
+        LocalDateTime startAt = LocalDateTime.of(2026,12,10,10, 0);
+        LocalDateTime endAt = LocalDateTime.of(2026,12,10,12, 0);
+        Theme theme = new Theme("테마", "설명", "경로");
+
+        Schedule schedule = new Schedule(startAt, endAt, theme);
+
+        assertThat(schedule.getId()).isNull();
+        assertThat(schedule.getStartAt()).isEqualTo(startAt);
+    }
+}
