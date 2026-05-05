e5ed8d312ce3189592a60e3a98a448755e5c42c1
snowshower <wonsb25@gmail.com>
2026-05-05 13:23:24 +0900

    test: 테마 저장소 테스트에 @Transactional 어노테이션 추가
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- src/test/java/roomescape/theme/repository/ThemeRepositoryTest.java
+++ src/test/java/roomescape/theme/repository/ThemeRepositoryTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.theme.model.Theme;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class ThemeRepositoryTest {
 
     @Autowired
