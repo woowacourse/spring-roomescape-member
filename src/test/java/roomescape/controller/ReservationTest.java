1547dc29351451af5d6b92986b672ff5d4a93a1b
ghkdtjfxkd <syt1128@gmail.com>
2026-05-05 17:30:40 +0900

    test: 예약 삭제 테스트 추가

--- src/test/java/roomescape/controller/ReservationTest.java
+++ src/test/java/roomescape/controller/ReservationTest.java
@@ -23,18 +23,18 @@ public class ReservationTest {
     @DisplayName("예약 생성 테스트")
     void createReservation() {
 
-            Map<String, Object> params = new HashMap<>();
-            params.put("name", "녀녕");
-            params.put("date", "2025-05-05");
-            params.put("timeId", 1L);
-            params.put("themeId", 2L);
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", "녀녕");
+        params.put("date", "2025-05-05");
+        params.put("timeId", 1L);
+        params.put("themeId", 2L);
 
-            RestAssured.given().log().all()
-                    .contentType(ContentType.JSON)
-                    .body(params)
-                    .when().post("/reservations")
-                    .then().log().all()
-                    .statusCode(201);
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -42,6 +42,7 @@ public class ReservationTest {
                 .when().get("/admin/reservations")
                 .then().log().all()
                 .statusCode(200)
+                .body("size()", is(40))
                 .body("[39].id", is(40))
                 .body("[39].name", is("녀녕"))
                 .body("[39].date", is("2025-05-05"))
@@ -51,5 +52,23 @@ public class ReservationTest {
                 .body("[39].theme.name", is("우주 탐험대"))
                 .body("[39].theme.thumbnailUrl", is("https://picsum.photos/seed/space/400/300"))
                 .body("[39].theme.description", is("은하계를 누비는 우주 탐험"));
+    }
+
+    @Test
+    @Sql(scripts = "/testReservationData.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @DisplayName("예약 삭제 테스트")
+    void deleteReservationTest() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .when().delete("/admin/reservations/1")
+                .then().log().all()
+                .statusCode(204);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .when().get("/admin/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
     }
 }
---
+++ src/test/resources/testReservationData.sql
@@ -0,0 +1,9 @@
+INSERT INTO theme (name, thumbnail_url, description)
+VALUES ('공포의 저택', 'https://picsum.photos/seed/horror/400/300', '어둠 속에 숨겨진 공포를 체험하세요');
+
+
+INSERT INTO reservation_time (start_at)
+VALUES ('10:00');
+
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('user_a', '2026-04-28', 1, 1);
