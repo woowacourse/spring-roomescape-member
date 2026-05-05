c6cc36d78fdb03051f2b50c651eace117298e242
Soojin6943 <sjhan0711@gmail.com>
2026-05-05 20:25:29 +0900

    docs: 단계2 기능 요구사항 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,19 @@
+CREATE TABLE theme (
+                       id      BIGINT       NOT NULL AUTO_INCREMENT,
+                       name    VARCHAR(255) NOT NULL,
+                       description VARCHAR(255),
+                       image VARCHAR(255),
+                       PRIMARY KEY (id)
+);
+
 CREATE TABLE reservation_time (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
+    theme_id BIGINT       NOT NULL,
     start_at VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    is_available BOOLEAN   NOT NULL DEFAULT TRUE,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
 
 CREATE TABLE reservation (
@@ -11,14 +23,6 @@ CREATE TABLE reservation (
     time_id BIGINT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id)
-);
-
-CREATE TABLE theme (
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
-    image VARCHAR(255),
-    PRIMARY KEY (id)
 );
 
 INSERT INTO theme (name, description, image) VALUES ('은하수', '은하수 테마방입니다.', 'http.jpg');
@@ -32,3 +36,6 @@ INSERT INTO theme (name, description, image) VALUES ('보이저', '보이저 테
 INSERT INTO theme (name, description, image) VALUES ('아폴로', '아폴로 테마방입니다.', 'http.jpg');
 INSERT INTO theme (name, description, image) VALUES ('허블', '허블 테마방입니다.', 'http.jpg');
 INSERT INTO theme (name, description, image) VALUES ('안드로메다', '안드로메다 테마방입니다.', 'http.jpg');
+
+INSERT INTO reservation_time (theme_id, start_at, is_available) VALUES (1L, '10:00', true);
+INSERT INTO reservation_time (theme_id, start_at, is_available) VALUES (1L, '11:00', false);
