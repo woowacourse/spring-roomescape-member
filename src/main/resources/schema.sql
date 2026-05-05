88b401f7d05fed7220162ea62b7be6cbe20c80bd
ghkdtjfxkd <syt1128@gmail.com>
2026-05-05 19:21:17 +0900

    fix: CASCADE 적용

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -27,6 +27,6 @@ CREATE TABLE reservation
     time_id  BIGINT       NOT NULL,
     theme_id BIGINT       NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id) ON DELETE CASCADE,
+    FOREIGN KEY (theme_id) REFERENCES theme (id)  ON DELETE CASCADE
 );
