402553885a34741edf7d10087032f0a4da5715d4
snowshower <wonsb25@gmail.com>
2026-05-05 13:22:07 +0900

    feat: 스케줄 컨트롤러 구현
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- src/main/java/roomescape/schedule/controller/ScheduleController.java
+++ src/main/java/roomescape/schedule/controller/ScheduleController.java
@@ -1,45 +1,32 @@
-package roomescape.controller;
+package roomescape.schedule.controller;
 
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.dto.ReservationTimeRequest;
-import roomescape.dto.ReservationTimeResponse;
-import roomescape.service.ReservationTimeService;
+import org.springframework.format.annotation.DateTimeFormat;
+import roomescape.schedule.dto.ScheduleRequest;
+import roomescape.schedule.dto.SchedulesResponse;
+import roomescape.schedule.service.ScheduleService;
 
-import java.util.List;
+import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/times")
-public class ReservationTimeController {
+@RequestMapping("/schedules")
+public class ScheduleController {
 
-    private final ReservationTimeService reservationTimeService;
+    private final ScheduleService scheduleService;
 
-    public ReservationTimeController(ReservationTimeService reservationTimeService) {
-        this.reservationTimeService = reservationTimeService;
-    }
-
-    @PostMapping
-    public ResponseEntity<ReservationTimeResponse> create(@RequestBody @Valid ReservationTimeRequest request) {
-        ReservationTimeResponse response = reservationTimeService.create(request);
-        return ResponseEntity.ok(response);
+    public ScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
     }
 
     @GetMapping
-    public ResponseEntity<List<ReservationTimeResponse>> findAll() {
-        List<ReservationTimeResponse> responses = reservationTimeService.findAll();
+    public ResponseEntity<SchedulesResponse> findAll(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
+            @RequestParam Long themeId) {
+        SchedulesResponse responses = scheduleService.findAll(new ScheduleRequest(date, themeId));
         return ResponseEntity.ok(responses);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        int count = reservationTimeService.delete(id);
-        return ResponseEntity.ok().build();
     }
 }
