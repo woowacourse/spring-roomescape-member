8952ca7d9b6ac1b4319029a518efd6096e3252bb
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-05-05 18:02:11 +0900

    refactor: 더미데이터 추가로 인한 수정

--- src/test/java/roomescape/reservation/infra/JdbcReservationRepositoryTest.java
+++ src/test/java/roomescape/reservation/infra/JdbcReservationRepositoryTest.java
@@ -1,133 +1,78 @@
 package roomescape.reservation.infra;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.List;
-import java.util.Timer;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.domain.ReservationTime;
 import roomescape.reservation.domain.Theme;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Sql(scripts = {"/truncate.sql", "/data.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 class JdbcReservationRepositoryTest {
     @Autowired
     private JdbcReservationRepository reservationRepository;
 
     @Autowired
-    private JdbcReservationTimeRepository timeRepository;
-
-    @Autowired
-    private JdbcThemeRepository themeRepository;
-
-    @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Test
-    @Transactional
     void 예약_저장_레포지토리_테스트() {
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES (?)", "15:40");
-        jdbcTemplate.update("INSERT INTO theme (name, description, thumbnail_url) VALUES (?,?,?)", "꿀잼 방탈출",
-                "재밌는 분위기의 방탈출", "https://example.com/theme_happy.jpg");
-
-        Long timeId = jdbcTemplate.queryForObject("SELECT id FROM reservation_time LIMIT 1", Long.class);
-        Long themeId = jdbcTemplate.queryForObject("SELECT id FROM theme LIMIT 1", Long.class);
-
-        ReservationTime fakeTime = new ReservationTime(timeId, LocalTime.of(15, 40));
-        Theme fakeTheme = new Theme(themeId, "꿀잼 방탈출", "재밌는 분위기의 방탈출", "https://example.com/theme_happy.jpg");
-
-        Reservation reservation = new Reservation(null, "브라운", LocalDate.of(2023, 8, 5), fakeTime, fakeTheme);
+        Reservation reservation = new Reservation(null, "브라운", LocalDate.of(2026, 5, 5),
+                new ReservationTime(2L, LocalTime.of(11, 0)),
+                new Theme(1L, "세기의 도둑", "보안을 뚫고 보석을 훔쳐라", "https://example.com/themes/time.jpg")
+        );
 
         Reservation savedReservation = reservationRepository.save(reservation);
 
-        Long id = jdbcTemplate.queryForObject("SELECT id FROM reservation LIMIT 1", Long.class);
-
-        assertThat(savedReservation.getId()).isEqualTo(id);
         assertThat(savedReservation.getName()).isEqualTo("브라운");
-        assertThat(savedReservation.getDate()).isEqualTo(LocalDate.of(2023, 8, 5));
-        assertThat(savedReservation.getTime().getId()).isEqualTo(timeId);
-        assertThat(savedReservation.getTime().getStartAt()).isEqualTo(LocalTime.of(15, 40));
+        assertThat(savedReservation.getDate()).isEqualTo(LocalDate.of(2026, 5, 5));
+        assertThat(savedReservation.getTime().getId()).isEqualTo(2L);
+        assertThat(savedReservation.getTime().getStartAt()).isEqualTo(LocalTime.of(11, 0));
     }
 
     @Test
-    @Transactional
     void 전체_예약_조회_레포지토리_테스트() {
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES (?)", "15:40");
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES (?)", "16:00");
-        Long firstTimeId = jdbcTemplate.queryForObject(
-                "SELECT id FROM reservation_time WHERE start_at = ?",
-                Long.class,
-                "15:40"
-        );
-        Long secondTimeId = jdbcTemplate.queryForObject(
-                "SELECT id FROM reservation_time WHERE start_at = ?",
-                Long.class,
-                "16:00"
-        );
-
-        jdbcTemplate.update("INSERT INTO theme (name, description, thumbnail_url) VALUES (?,?,?)", "꿀잼 방탈출",
-                "재밌는 분위기의 방탈출", "https://example.com/theme_happy.jpg");
-        Long themeId = jdbcTemplate.queryForObject("SELECT id FROM theme LIMIT 1", Long.class);
-
-        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id, theme_id) VALUES (?, ?, ?, ?)", "브라운", "2023-08-05",
-                firstTimeId,themeId);
-        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id, theme_id) VALUES (?, ?, ?, ?)", "코니", "2023-08-05",
-                secondTimeId,themeId);
-
         List<Reservation> reservations = reservationRepository.findAll();
 
-        assertThat(reservations).hasSize(2);
+        assertThat(reservations).hasSize(3);
         assertThat(reservations)
                 .extracting(Reservation::getName)
-                .containsExactly("브라운", "코니");
+                .containsExactly("kim", "lee", "park");
         assertThat(reservations)
                 .extracting(Reservation::getDate)
-                .containsExactly(LocalDate.of(2023, 8, 5), LocalDate.of(2023, 8, 5));
+                .containsExactly(LocalDate.of(2026, 5, 5), LocalDate.of(2026, 5, 5), LocalDate.of(2026, 5, 6));
         assertThat(reservations)
                 .extracting(reservation -> reservation.getTime().getId())
-                .containsExactly(firstTimeId, secondTimeId);
+                .containsExactly(1L, 3L, 4L);
         assertThat(reservations)
                 .extracting(reservation -> reservation.getTime().getStartAt())
-                .containsExactly(LocalTime.of(15, 40), LocalTime.of(16, 0));
+                .containsExactly(LocalTime.of(10, 0), LocalTime.of(12, 0), LocalTime.of(13, 0));
     }
 
     @Test
-    @Transactional
     void 예약_삭제_레포지토리_테스트() {
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES (?)", "15:40");
-        Long timeId = jdbcTemplate.queryForObject("SELECT id FROM reservation_time LIMIT 1", Long.class);
-        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)", "브라운", "2023-08-05",
-                timeId);
-        Long id = jdbcTemplate.queryForObject("SELECT id FROM reservation LIMIT 1", Long.class);
-
-        reservationRepository.deleteById(id);
+        reservationRepository.deleteById(1L);
         int rowCount = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM reservation", Integer.class);
 
-        assertThat(rowCount).isEqualTo(0);
+        assertThat(rowCount).isEqualTo(2);
     }
 
     @Test
     @DisplayName("이미 예약된 시간을 조회할 수 있다.")
-    void findTimeIdByDateAndThemeId_테스트(){
-        jdbcTemplate.update("INSERT INTO theme (name, description, thumbnail_url) VALUES (?,?,?)", "꿀잼 방탈출",
-                "재밌는 분위기의 방탈출", "https://example.com/theme_happy.jpg");
-        Long themeId = jdbcTemplate.queryForObject("SELECT id FROM theme WHERE name = ?", Long.class, "꿀잼 방탈출");
+    void findTimeIdByDateAndThemeId_테스트() {
+        List<Long> result = reservationRepository.findTimeIdByDateAndThemeId(LocalDate.parse("2026-05-05"), 1L);
 
-        jdbcTemplate.update("INSERT INTO reservation_time (start_at) VALUES (?)", "15:00");
-        Long timeId = jdbcTemplate.queryForObject("SELECT id FROM reservation_time LIMIT 1", Long.class);
-
-        jdbcTemplate.update("INSERT INTO reservation (name, date, time_id, theme_id) VALUES (?, ?, ?, ?)","이삭", "2023-08-05", timeId, themeId);
-
-        List<Long> result = reservationRepository.findTimeIdByDateAndThemeId(LocalDate.parse("2023-08-05"), themeId);
-
-        assertThat(result).containsExactly(timeId);
+        assertThat(result).containsExactly(1L);
     }
 }
--- src/test/java/roomescape/reservation/presentation/ReservationTimeControllerTest.java
+++ src/test/java/roomescape/reservation/presentation/ReservationTimeControllerTest.java
@@ -13,18 +13,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@Sql(scripts = "/truncate.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = {"/truncate.sql", "/data.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class ReservationTimeControllerTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    private void deleteTable(){
+        jdbcTemplate.update("DELETE FROM schedule");
+        jdbcTemplate.update("DELETE FROM reservation");
+    }
+
     @Test
     void 시간_관리_API() {
         Map<String, String> params = new HashMap<>();
-        params.put("startAt", "10:00");
+        params.put("startAt", "14:00");
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -37,7 +41,9 @@ public class ReservationTimeControllerTest {
                 .when().get("/times")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(1));
+                .body("size()", is(5));
+
+        deleteTable();
 
         RestAssured.given().log().all()
                 .when().delete("/times/1")
@@ -47,199 +53,68 @@ public class ReservationTimeControllerTest {
 
     @Test
     void 특정날짜와_테마에_예약_가능_시간을_조회_API(){
-        //1. 시간 초기화
-        Map<String, String> time1 = new HashMap<>();
-        time1.put("startAt", "10:00");
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(time1)
-                .when().post("/times")
-                .then().log().all()
-                .statusCode(201);
-
-        Map<String, String> time2 = new HashMap<>();
-        time2.put("startAt", "11:00");
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(time2)
-                .when().post("/times")
-                .then().log().all()
-                .statusCode(201);
-
-        Map<String, String> time3 = new HashMap<>();
-        time3.put("startAt", "12:00");
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(time3)
-                .when().post("/times")
-                .then().log().all()
-                .statusCode(201);
-
-        //2. 테마 초기화
-        Map<String, String> firstTheme = new HashMap<>();
-        firstTheme.put("name", "꿀잼 방탈출");
-        firstTheme.put("description", "재밌는 분위기의 방탈출");
-        firstTheme.put("thumbnailUrl", "https://example.com/theme_happy.jpg");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(firstTheme)
-                .when().post("/themes")
-                .then().log().all()
-                .statusCode(201);
-
-        //3. 스케줄(일정표) 생성
-        jdbcTemplate.update("INSERT INTO schedule (date, time_id, theme_id) VALUES (?,?,?)", "2023-08-05", 1, 1);
-        jdbcTemplate.update("INSERT INTO schedule (date, time_id, theme_id) VALUES (?,?,?)", "2023-08-05", 2, 1);
-        jdbcTemplate.update("INSERT INTO schedule (date, time_id, theme_id) VALUES (?,?,?)", "2023-08-05", 3, 1);
-
-        //4. 예약 생성
-        Map<String, Object> reservation1 = new HashMap<>();
-        reservation1.put("name", "브라운");
-        reservation1.put("date", "2023-08-05");
-        reservation1.put("timeId", 1);
-        reservation1.put("themeId", 1);
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(reservation1)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(201);
-
-        Map<String, Object> reservation2 = new HashMap<>();
-        reservation2.put("name", "브라운");
-        reservation2.put("date", "2023-08-05");
-        reservation2.put("timeId", 2);
-        reservation2.put("themeId", 1);
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(reservation2)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(201);
-
-        //5. 특정날짜와_테마에_예약_가능_시간을_조회
         Map<String, Object> options = new HashMap<>();
-        options.put("date", "2023-08-05");
+        options.put("date", "2026-05-05");
         options.put("themeId", 1);
 
         RestAssured.given().log().all()
                 .params(options)
                 .when().get("/times/availability")
                 .then().log().all()
-                .body("size()", is(3))
+                .body("size()", is(4))
                 .body("[0].isAvailable", is(false))
-                .body("[1].isAvailable", is(false))
+                .body("[1].isAvailable", is(true))
                 .body("[2].isAvailable", is(true))
+                .body("[3].isAvailable", is(true))
                 .statusCode(200);
     }
 
     @Test
     @DisplayName("예약 가능 시간 조회 및 예약 생성 이후 예약 가능 시간을 재조회를 할 수 있다.")
     void 정상_흐름_테스트(){
-        //1. 시간 초기화
-        Map<String, String> time1 = new HashMap<>();
-        time1.put("startAt", "10:00");
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(time1)
-                .when().post("/times")
-                .then().log().all()
-                .statusCode(201);
-
-        Map<String, String> time2 = new HashMap<>();
-        time2.put("startAt", "11:00");
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(time2)
-                .when().post("/times")
-                .then().log().all()
-                .statusCode(201);
-
-        Map<String, String> time3 = new HashMap<>();
-        time3.put("startAt", "12:00");
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(time3)
-                .when().post("/times")
-                .then().log().all()
-                .statusCode(201);
-
-        //2. 테마 초기화
-        Map<String, String> firstTheme = new HashMap<>();
-        firstTheme.put("name", "꿀잼 방탈출");
-        firstTheme.put("description", "재밌는 분위기의 방탈출");
-        firstTheme.put("thumbnailUrl", "https://example.com/theme_happy.jpg");
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(firstTheme)
-                .when().post("/themes")
-                .then().log().all()
-                .statusCode(201);
-
-        //3. 스케줄(일정표) 생성
-        jdbcTemplate.update("INSERT INTO schedule (date, time_id, theme_id) VALUES (?,?,?)", "2023-08-05", 1, 1);
-        jdbcTemplate.update("INSERT INTO schedule (date, time_id, theme_id) VALUES (?,?,?)", "2023-08-05", 2, 1);
-        jdbcTemplate.update("INSERT INTO schedule (date, time_id, theme_id) VALUES (?,?,?)", "2023-08-05", 3, 1);
-
-        //4. 1차 조회(총 3개의 예약 가능 시간이 반환됨)
         Map<String, Object> options = new HashMap<>();
-        options.put("date", "2023-08-05");
-        options.put("themeId", 1);
+        options.put("date", "2026-05-05");
+        options.put("themeId", 2);
 
         RestAssured.given().log().all()
                 .params(options)
                 .when().get("/times/availability")
                 .then().log().all()
-                .body("size()", is(3))
+                .body("size()", is(4))
                 .body("[0].isAvailable", is(true))
                 .body("[1].isAvailable", is(true))
-                .body("[2].isAvailable", is(true))
+                .body("[2].isAvailable", is(false))
+                .body("[3].isAvailable", is(true))
                 .statusCode(200);
 
         //5. 예약 생성
-        Map<String, Object> reservation1 = new HashMap<>();
-        reservation1.put("name", "브라운");
-        reservation1.put("date", "2023-08-05");
-        reservation1.put("timeId", 1);
-        reservation1.put("themeId", 1);
+        Map<String, Object> reservation = new HashMap<>();
+        reservation.put("name", "브라운");
+        reservation.put("date", "2026-05-05");
+        reservation.put("timeId", 1);
+        reservation.put("themeId", 2);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
-                .body(reservation1)
-                .when().post("/reservations")
-                .then().log().all()
-                .statusCode(201);
-
-        Map<String, Object> reservation2 = new HashMap<>();
-        reservation2.put("name", "브라운");
-        reservation2.put("date", "2023-08-05");
-        reservation2.put("timeId", 2);
-        reservation2.put("themeId", 1);
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(reservation2)
+                .body(reservation)
                 .when().post("/reservations")
                 .then().log().all()
                 .statusCode(201);
 
         //6. 특정날짜와_테마에_예약_가능_시간을_조회
         Map<String, Object> options1 = new HashMap<>();
-        options1.put("date", "2023-08-05");
-        options1.put("themeId", 1);
+        options1.put("date", "2026-05-05");
+        options1.put("themeId", 2);
 
         RestAssured.given().log().all()
                 .params(options1)
                 .when().get("/times/availability")
                 .then().log().all()
-                .body("size()", is(3))
+                .body("size()", is(4))
                 .body("[0].isAvailable", is(false))
-                .body("[1].isAvailable", is(false))
-                .body("[2].isAvailable", is(true))
+                .body("[1].isAvailable", is(true))
+                .body("[2].isAvailable", is(false))
+                .body("[3].isAvailable", is(true))
                 .statusCode(200);
     }
 }
