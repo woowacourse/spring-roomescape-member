230eac7b590b856a2f9b37b7439c6584835fe521
smiinii <sm020320@naver.com>
2026-05-05 14:22:23 +0900

    feat: 스케줄 조회에 테마 소요 시간 필드 추가

--- src/main/java/roomescape/schedule/repository/ScheduleRepository.java
+++ src/main/java/roomescape/schedule/repository/ScheduleRepository.java
@@ -7,6 +7,7 @@ import roomescape.theme.model.Theme;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
@@ -26,7 +27,8 @@ public class ScheduleRepository {
                        t.id AS theme_id, 
                        t.name AS theme_name,
                        t.description,
-                       t.image_url
+                       t.image_url,
+                       t.required_time
                 FROM schedule s
                 INNER JOIN theme t ON s.theme_id = t.id
                 LEFT JOIN reservation r ON s.id = r.schedule_id
@@ -40,7 +42,8 @@ public class ScheduleRepository {
 
         return jdbcTemplate.query(sql, (resultSet, rowNum) -> {
             Theme theme = new Theme(resultSet.getLong("theme_id"), resultSet.getString("theme_name"),
-                    resultSet.getString("description"), resultSet.getString("image_url"));
+                    resultSet.getString("description"), resultSet.getString("image_url"),
+                    resultSet.getObject("required_time", LocalTime.class));
             return new Schedule(
                     resultSet.getLong("schedule_id"),
                     resultSet.getObject("start_at", LocalDateTime.class),
