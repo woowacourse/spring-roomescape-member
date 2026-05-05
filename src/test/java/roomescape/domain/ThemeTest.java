e3371eae35e68285f3781b5b4b7529a3244042af
wontop02 <wjy2002kr@gmail.com>
2026-05-05 15:51:28 +0900

    test: Theme 도메인 검증 테스트 추가

---
+++ src/test/java/roomescape/domain/ThemeTest.java
@@ -0,0 +1,32 @@
+package roomescape.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ThemeTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void nameBlankExceptionTest(String name) {
+        assertThatThrownBy(() -> new Theme(name, "모험 이야기", "url.jpg"))
+                .hasMessage("[ERROR] 이름은 비어 있을 수 없습니다.")
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void descriptionBlankExceptionTest(String description) {
+        assertThatThrownBy(() -> new Theme("피즈의 모험", description, "url.jpg"))
+                .hasMessage("[ERROR] 설명은 비어 있을 수 없습니다.")
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void thumbnailUrlBlankExceptionTest(String thumbnailUrl) {
+        assertThatThrownBy(() -> new Theme("피즈의 모험", "모험 이야기", thumbnailUrl))
+                .hasMessage("[ERROR] 썸네일은 비어 있을 수 없습니다.")
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
