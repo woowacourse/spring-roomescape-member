024f328ec785ffe1b39be08c3fc9e0f86a0ed615
SooHyeon Lee <dalsu4594@naver.com>
2026-05-05 17:20:28 +0900

    feat: time 비즈니스 규칙 추가
    - 영업시간은 10시부터 22시까지 이다

--- src/main/java/roomescape/domain/Time.java
+++ src/main/java/roomescape/domain/Time.java
@@ -8,8 +8,15 @@ public class Time {
 
 
     public Time(Long id, LocalTime startAt) {
+        validate(startAt);
         this.id = id;
         this.startAt = startAt;
+    }
+
+    private void validate(LocalTime startAt) {
+        if (startAt.isBefore(LocalTime.of(10, 0)) || startAt.isAfter(LocalTime.of(22, 0))) {
+            throw new IllegalArgumentException("영업 시간은 10시부터 22시 사이입니다.");
+        }
     }
 
     public Time(LocalTime startAt) {
