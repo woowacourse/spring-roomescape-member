18114643baf29421a7312044a18806fa43f0181d
snowshower <wonsb25@gmail.com>
2026-05-05 13:26:56 +0900

    refactor: 예약 저장소 CRUD 메서드 로직 변경
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- src/main/java/roomescape/reservation/repository/ReservationRepository.java
+++ src/main/java/roomescape/reservation/repository/ReservationRepository.java
@@ -1,61 +1,80 @@
-//package roomescape.repository;
-//
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.support.GeneratedKeyHolder;
-//import org.springframework.jdbc.support.KeyHolder;
-//import org.springframework.stereotype.Repository;
-//import roomescape.model.Reservation;
-//import roomescape.model.ReservationTime;
-//
-//import java.sql.PreparedStatement;
-//import java.time.LocalDate;
-//import java.time.LocalTime;
-//import java.util.List;
-//
-//@Repository
-//public class ReservationRepository {
-//
-//    private final JdbcTemplate jdbcTemplate;
-//
-//    public ReservationRepository(JdbcTemplate jdbcTemplate) {
-//        this.jdbcTemplate = jdbcTemplate;
-//    }
-//
-//    public Long create(Reservation reservation) {
-//        String sql = "INSERT INTO reservation (name, date, time_id) VALUES (?, ?, ?)";
-//
-//        KeyHolder keyHolder = new GeneratedKeyHolder();
-//
-//        jdbcTemplate.update(
-//                connection -> {
-//                    PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
-//                    ps.setString(1, reservation.getName());
-//                    ps.setObject(2, reservation.getDate());
-//                    ps.setLong(3, reservation.getTime().getId());
-//                    return ps;
-//                }, keyHolder);
-//        return keyHolder.getKey().longValue();
-//    }
-//
-//    public List<Reservation> findAll() {
-//        String sql = "SELECT r.id, r.name, r.date, t.id AS time_id, t.start_at AS time_value FROM reservation r INNER JOIN reservation_time t ON r.time_id = t.id";
-//
-//        return jdbcTemplate.query(sql, (resultSet, rowNum) -> {
-//            ReservationTime time = new ReservationTime(
-//                    resultSet.getLong("time_id"),
-//                    resultSet.getObject("time_value", LocalTime.class)
-//            );
-//            return new Reservation(
-//                    resultSet.getLong("id"),
-//                    resultSet.getString("name"),
-//                    resultSet.getObject("date", LocalDate.class),
-//                    time
-//            );
-//        });
-//    }
-//
-//    public int delete(Long id) {
-//        String sql = "delete from reservation where id = ?";
-//        return jdbcTemplate.update(sql, id);
-//    }
-//}
+package roomescape.reservation.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.reservation.model.Reservation;
+import roomescape.schedule.model.Schedule;
+import roomescape.theme.model.Theme;
+import roomescape.user.model.Role;
+import roomescape.user.model.User;
+
+import java.sql.PreparedStatement;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public class ReservationRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Long create(Reservation reservation) {
+        String sql = "INSERT INTO reservation (schedule_id, user_id) VALUES (?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(
+                connection -> {
+                    PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+                    ps.setLong(1, reservation.getSchedule().getId());
+                    ps.setLong(2, reservation.getUser().getId());
+                    return ps;
+                }, keyHolder);
+        return keyHolder.getKey().longValue();
+    }
+
+    public List<Reservation> findAll() {
+        String sql = """
+                SELECT r.id AS reservation_id, 
+                       u.id AS user_id, 
+                       u.name AS user_name, 
+                       u.role AS user_role,
+                       t.id AS theme_id, 
+                       t.name AS theme_name,
+                       s.id AS schedule_id, 
+                       s.start_at AS start_at, 
+                       s.end_at AS end_at
+                FROM reservation r
+                INNER JOIN "USER" u ON r.user_id = u.id
+                INNER JOIN schedule s ON r.schedule_id = s.id
+                INNER JOIN theme t ON s.theme_id = t.id
+                """;
+
+
+        return jdbcTemplate.query(sql, (resultSet, rowNum) -> {
+            User user = new User(
+                    resultSet.getLong("user_id"),
+                    resultSet.getString("user_name"),
+                    Role.valueOf(resultSet.getString("user_role"))
+            );
+
+            Theme theme = new Theme(resultSet.getLong("theme_id"), resultSet.getString("theme_name"), "", "");
+
+            Schedule schedule = new Schedule(resultSet.getLong("schedule_id"),
+                    resultSet.getObject("start_at", LocalDateTime.class),
+                    resultSet.getObject("end_at", LocalDateTime.class), theme);
+
+            return new Reservation(resultSet.getLong("reservation_id"), user, schedule, theme);
+        });
+    }
+
+    public void delete(Long id) {
+        String sql = "delete from reservation where id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+}
