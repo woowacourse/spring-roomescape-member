4a1765c2a4aad15066f9311de5b8e1d76b3647ff
smiinii <sm020320@naver.com>
2026-05-06 16:37:53 +0900

    feat: 예약 생성 기능 추가

--- src/main/java/roomescape/reservation/service/ReservationService.java
+++ src/main/java/roomescape/reservation/service/ReservationService.java
@@ -2,9 +2,15 @@ package roomescape.reservation.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import roomescape.reservation.dto.ReservationIdResponse;
+import roomescape.reservation.dto.ReservationRequest;
 import roomescape.reservation.dto.ReservationsResponse;
 import roomescape.reservation.model.Reservation;
 import roomescape.reservation.repository.ReservationRepository;
+import roomescape.schedule.model.Schedule;
+import roomescape.schedule.repository.ScheduleRepository;
+import roomescape.user.model.User;
+import roomescape.user.repository.UserRepository;
 
 import java.util.List;
 
@@ -13,9 +19,23 @@ import java.util.List;
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private final ScheduleRepository scheduleRepository;
+    private final UserRepository userRepository;
 
-    public ReservationService(ReservationRepository reservationRepository) {
+    public ReservationService(ReservationRepository reservationRepository, ScheduleRepository scheduleRepository, UserRepository userRepository) {
         this.reservationRepository = reservationRepository;
+        this.scheduleRepository = scheduleRepository;
+        this.userRepository = userRepository;
+    }
+
+    public ReservationIdResponse create(ReservationRequest request) {
+        Schedule schedule = scheduleRepository.findById(request.scheduleId());
+        User user = userRepository.findById(request.userId());
+
+        Reservation reservation = new Reservation(user, schedule, schedule.getTheme());
+        Long reservationId = reservationRepository.create(reservation);
+        Reservation savedReservation = new Reservation(reservationId, user, schedule, schedule.getTheme());
+        return ReservationIdResponse.from(savedReservation);
     }
 
     public ReservationsResponse findAll() {
