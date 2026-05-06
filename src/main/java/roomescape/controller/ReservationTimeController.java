0da2113e3a7a57c0db8c94a8a52f7271699c3bdb
Giyoul <cla12093@gmail.com>
2026-05-06 15:49:37 +0900

    test: 예약 시간 api 테스트 추가

--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -1,6 +1,8 @@
 package roomescape.controller;
 
+import java.net.URI;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,17 +22,22 @@ public class ReservationTimeController {
     }
 
     @GetMapping("/times")
-    public List<ReservationTimeResponse> getTimes() {
-        return reservationTimeService.findAll();
+    public ResponseEntity<List<ReservationTimeResponse>> getTimes() {
+        return ResponseEntity.ok(reservationTimeService.findAll());
     }
 
     @PostMapping("/times")
-    public ReservationTimeResponse createTime(@RequestBody ReservationTimeRequest request) {
-        return reservationTimeService.save(request);
+    public ResponseEntity<ReservationTimeResponse> createTime(@RequestBody ReservationTimeRequest request) {
+        final ReservationTimeResponse response = reservationTimeService.save(request);
+        final URI location = URI.create("/times/" + response.id());
+
+        return ResponseEntity.created(location)
+                .body(response);
     }
 
     @DeleteMapping("/times/{id}")
-    public void deleteTime(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteTime(@PathVariable Long id) {
         reservationTimeService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
