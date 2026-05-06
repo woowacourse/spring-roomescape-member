70526bf57d4c36b3b061c5ac241b4e06155bed24
smiinii <sm020320@naver.com>
2026-05-06 16:37:16 +0900

    test: 사용자 생성 서비스 테스트 코드 추가

---
+++ src/test/java/roomescape/user/service/UserServiceTest.java
@@ -0,0 +1,38 @@
+package roomescape.user.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import roomescape.user.dto.UserRequest;
+import roomescape.user.dto.UserResponse;
+import roomescape.user.model.User;
+import roomescape.user.repository.UserRepository;
+
+@SpringBootTest
+@Transactional
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void 유저를_생성하면_DB에_실제로_저장되고_저장된_정보를_반환한다() {
+        UserRequest request = new UserRequest("루크");
+
+        UserResponse response = userService.create(request);
+
+        assertThat(response.getId()).isNotNull();
+        assertThat(response.getName()).isEqualTo("루크");
+
+        User savedUser = userRepository.findById(response.getId());
+        assertThat(savedUser.getName()).isEqualTo("루크");
+        assertThat(savedUser.getRole().name()).isEqualTo("USER");
+    }
+}
