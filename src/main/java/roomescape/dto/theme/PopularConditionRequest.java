d979abcd372b4384f47901e1a8fb6bfb2482d6c5
Chocoding1 <galmeagi2@naver.com>
2026-05-06 17:23:57 +0900

    refactor: 쿼리 파라미터 order 삭제

--- src/main/java/roomescape/dto/theme/PopularConditionRequest.java
+++ src/main/java/roomescape/dto/theme/PopularConditionRequest.java
@@ -12,11 +12,9 @@ public record PopularConditionRequest(
         @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "날짜는 YYYY-MM-DD 형식이여야 합니다.")
         String end_date,
         @NotNull
-        long size,
-        @NotNull
-        String order
+        long size
 ) {
     public PopularThemeCondition to() {
-        return new PopularThemeCondition(start_date, end_date, size, order);
+        return new PopularThemeCondition(start_date, end_date, size);
     }
 }
