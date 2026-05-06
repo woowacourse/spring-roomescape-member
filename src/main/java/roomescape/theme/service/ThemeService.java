4d1318e04accef8926102277187169d1851d11cb
Yoonhojoon <dbsghwns1209@gmail.com>
2026-05-06 16:43:39 +0900

    feat: 예약 가능 시간 기능 구현

--- src/main/java/roomescape/theme/service/ThemeService.java
+++ src/main/java/roomescape/theme/service/ThemeService.java
@@ -1,6 +1,9 @@
 package roomescape.theme.service;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import roomescape.reservation.domain.ReservationTime;
 import roomescape.theme.domain.Theme;
 import roomescape.theme.service.dto.ThemeSaveServiceDto;
 
@@ -8,4 +11,6 @@ public interface ThemeService {
     List<Theme> getThemes();
     Theme save(ThemeSaveServiceDto theme);
     void deleteById(Long id);
+
+    List<ReservationTime> getAvailableTimes(Long themeId, LocalDate date);
 }
