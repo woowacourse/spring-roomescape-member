6e9613ae06b86ce110d66f46792f3f6e1556f29b
Jeshin Kim <jjason0904@cau.ac.kr>
2026-05-06 18:29:05 +0900

    refactor(schema): 스키마 데이터 타입 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE themes
 CREATE TABLE reservation_time
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL UNIQUE,
+    start_at TIME NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
@@ -18,7 +18,7 @@ CREATE TABLE reservation
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
-    date     VARCHAR(255) NOT NULL,
+    date     DATE NOT NULL,
     time_id  BIGINT,
     theme_id BIGINT,
     PRIMARY KEY (id),
--- src/test/resources/schema-test.sql
+++ src/test/resources/schema-test.sql
@@ -1,25 +1,29 @@
-CREATE TABLE IF NOT EXISTS reservation_time (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL UNIQUE,
+CREATE TABLE IF NOT EXISTS themes
+(
+    id          BIGINT        NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255)  NOT NULL UNIQUE,
+    description VARCHAR(255)  NOT NULL,
+    thumbnail   VARCHAR(2048) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS themes (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL UNIQUE,
-    description VARCHAR(255) NOT NULL,
-    thumbnail VARCHAR(2048) NOT NULL,
+CREATE TABLE IF NOT EXISTS reservation_time
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at TIME NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS reservation (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    date VARCHAR(255) NOT NULL,
-    time_id BIGINT,
+CREATE TABLE IF NOT EXISTS reservation
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255) NOT NULL,
+    date     DATE NOT NULL,
+    time_id  BIGINT,
     theme_id BIGINT,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES themes (id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id) ON DELETE CASCADE,
+    FOREIGN KEY (theme_id) REFERENCES themes (id) ON DELETE CASCADE,
     CONSTRAINT unique_reservation UNIQUE (date, time_id, theme_id)
+
 );
