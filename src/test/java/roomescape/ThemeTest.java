c0d3591a72e6bfdc7c7cdec615d0da43a4c98559
sun <sun007021@gmail.com>
2026-05-06 16:46:06 +0900

    test: 인기 테마 조회 테스트 추가

--- src/test/java/roomescape/ThemeTest.java
+++ src/test/java/roomescape/ThemeTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -126,5 +127,47 @@ public class ThemeTest {
                 .extract().jsonPath().getBoolean("[0].isReserved");
 
         assertThat(isReserved).isFalse();
+    }
+
+    @Test
+    @Sql("classpath:data.sql")
+    void 인기_테마_상위_10개를_예약_수_내림차순으로_반환() {
+        List<Map<String, Object>> themes = RestAssured.given().log().all()
+                .when().get("/themes/popular?limit=10")
+                .then().log().all()
+                .statusCode(200)
+                .extract().jsonPath().getList(".");
+
+        assertThat(themes).hasSize(10);
+
+        // 1~6위는 예약 수가 달라 순위 고정
+        assertThat(themes.get(0)).containsEntry("id", 1).containsEntry("reservedCount", 10);
+        assertThat(themes.get(1)).containsEntry("id", 2).containsEntry("reservedCount", 9);
+        assertThat(themes.get(2)).containsEntry("id", 3).containsEntry("reservedCount", 8);
+        assertThat(themes.get(3)).containsEntry("id", 4).containsEntry("reservedCount", 7);
+        assertThat(themes.get(4)).containsEntry("id", 5).containsEntry("reservedCount", 6);
+        assertThat(themes.get(5)).containsEntry("id", 6).containsEntry("reservedCount", 5);
+
+        // 7~8위는 동률(4건), id 오름차순으로 정렬
+        assertThat(themes.get(6)).containsEntry("id", 7).containsEntry("reservedCount", 4);
+        assertThat(themes.get(7)).containsEntry("id", 8).containsEntry("reservedCount", 4);
+
+        // 9~10위는 동률(3건), id 오름차순으로 정렬
+        assertThat(themes.get(8)).containsEntry("id", 9).containsEntry("reservedCount", 3);
+        assertThat(themes.get(9)).containsEntry("id", 10).containsEntry("reservedCount", 3);
+    }
+
+    @Test
+    @Sql("classpath:data.sql")
+    void 인기_테마_조회_시_기간_외_예약은_집계에서_제외() {
+        // 테마 13~15는 기간 외(2026-04-28~29) 예약이 많지만 기간 내 예약은 각 2, 1, 1건
+        // limit=10이면 기간 내 예약 수 기준 상위 10개에 포함되지 않아야 함
+        List<Integer> themeIds = RestAssured.given().log().all()
+                .when().get("/themes/popular?limit=10")
+                .then().log().all()
+                .statusCode(200)
+                .extract().jsonPath().getList("id");
+
+        assertThat(themeIds).doesNotContain(13, 14, 15);
     }
 }
