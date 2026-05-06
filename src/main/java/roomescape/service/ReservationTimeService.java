aa03a9c887b423554d63fd6bded5d8821ff7fc2e
jhk01007 <jhk001007@naver.com>
2026-05-06 14:07:14 +0900

    feat(ReservationTimeService): 특정 날짜 및 테마 id를 가진 예약 정보를 조회하는 기능 추가

--- src/main/java/roomescape/service/ReservationTimeService.java
+++ src/main/java/roomescape/service/ReservationTimeService.java
@@ -2,18 +2,32 @@ package roomescape.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
+import roomescape.domain.ReservationTimeAvailability;
+import roomescape.exception.DomainException;
+import roomescape.exception.ErrorCode;
+import roomescape.repository.ReservationRepository;
 import roomescape.repository.ReservationTimeRepository;
+import roomescape.repository.ThemeRepository;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
 @Service
 public class ReservationTimeService {
     private final ReservationTimeRepository reservationTimeRepository;
+    private final ThemeRepository themeRepository;
+    private final ReservationRepository reservationRepository;
 
-    public ReservationTimeService(ReservationTimeRepository reservationTimeRepository) {
+    public ReservationTimeService(
+            ReservationTimeRepository reservationTimeRepository,
+            ThemeRepository themeRepository,
+            ReservationRepository reservationRepository) {
         this.reservationTimeRepository = reservationTimeRepository;
+        this.themeRepository = themeRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     @Transactional
@@ -31,5 +45,22 @@ public class ReservationTimeService {
     @Transactional
     public void delete(Long id) {
         reservationTimeRepository.deleteById(id);
+    }
+
+    public List<ReservationTimeAvailability> findAvailableTimes(LocalDate date, Long themeId) {
+        themeRepository.findById(themeId)
+                .orElseThrow(() -> new DomainException(ErrorCode.THEME_NOT_FOUND));
+
+        List<Reservation> reservations = reservationRepository.findByDateAndThemeId(date, themeId);
+
+        List<ReservationTime> reservationTimes = reservationTimeRepository.findAll();
+
+        return reservationTimes.stream()
+                .map(reservationTime -> new ReservationTimeAvailability(
+                        reservationTime,
+                        reservations.stream()
+                                .noneMatch(reservation -> reservation.isSameTime(reservationTime))
+                ))
+                .toList();
     }
 }
