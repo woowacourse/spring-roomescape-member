0e349028f947a7b651d145dd76909176f30ba02b
Yoonhojoon <dbsghwns1209@gmail.com>
2026-05-06 19:36:11 +0900

    feat: ReservationResponseDto에 ThemeResponseDto 추가

--- src/main/java/roomescape/reservation/controller/dto/ReservationResponseDto.java
+++ src/main/java/roomescape/reservation/controller/dto/ReservationResponseDto.java
@@ -3,18 +3,22 @@ package roomescape.reservation.controller.dto;
 import java.time.LocalDate;
 
 import roomescape.reservation.domain.Reservation;
+import roomescape.theme.controller.dto.ThemeResponseDto;
 
 public class ReservationResponseDto {
     private final Long id;
     private final String name;
     private final LocalDate date;
     private final ReservationTimeResponseDto time;
+    private final ThemeResponseDto theme;
 
-    public ReservationResponseDto(Long id, String name, LocalDate date, ReservationTimeResponseDto time) {
+    public ReservationResponseDto(Long id, String name, LocalDate date, ReservationTimeResponseDto time,
+                                  ThemeResponseDto theme) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+        this.theme = theme;
     }
 
     public static ReservationResponseDto from(Reservation reservation) {
@@ -22,7 +26,8 @@ public class ReservationResponseDto {
                 reservation.getId(),
                 reservation.getName(),
                 reservation.getDate(),
-                ReservationTimeResponseDto.from(reservation.getTime())
+                ReservationTimeResponseDto.from(reservation.getTime()),
+                ThemeResponseDto.from(reservation.getTheme())
         );
     }
 
@@ -40,5 +45,9 @@ public class ReservationResponseDto {
 
     public ReservationTimeResponseDto getTime() {
         return time;
+    }
+
+    public ThemeResponseDto getTheme() {
+        return theme;
     }
 }
--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -1,6 +1,7 @@
 package roomescape.reservation.domain;
 
 import java.time.LocalDate;
+import roomescape.theme.domain.Theme;
 
 public class Reservation {
     private final Long id;
@@ -8,21 +9,27 @@ public class Reservation {
     private final LocalDate date;
     private final ReservationTime time;
     private final Long themeId;
+    private final Theme theme;
 
     public Reservation(String name, LocalDate date, ReservationTime time, Long themeId) {
-        this(null, name, date, time, themeId);
+        this(null, name, date, time, themeId, null);
     }
 
-    private Reservation(Long id, String name, LocalDate date, ReservationTime time, Long themeId) {
+    private Reservation(Long id, String name, LocalDate date, ReservationTime time, Long themeId, Theme theme) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
         this.themeId = themeId;
+        this.theme = theme;
     }
 
     public Reservation withId(Long id) {
-        return new Reservation(id, this.name, this.date, this.time, this.themeId);
+        return new Reservation(id, this.name, this.date, this.time, this.themeId, this.theme);
+    }
+
+    public Reservation withTheme(Theme theme) {
+        return new Reservation(this.id, this.name, this.date, this.time, this.themeId, theme);
     }
 
     public Long getId() {
@@ -43,5 +50,9 @@ public class Reservation {
 
     public Long getThemeId() {
         return themeId;
+    }
+
+    public Theme getTheme() {
+        return theme;
     }
 }
