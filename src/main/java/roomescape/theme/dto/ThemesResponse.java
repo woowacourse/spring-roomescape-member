39251bb36aea1d14d273bd711ae1811d425e6d49
smiinii <sm020320@naver.com>
2026-05-06 13:45:09 +0900

    feat: 테마 응답 DTO 클래스 추가

---
+++ src/main/java/roomescape/theme/dto/ThemesResponse.java
@@ -0,0 +1,25 @@
+package roomescape.theme.dto;
+
+import roomescape.theme.model.Theme;
+
+import java.util.List;
+
+public class ThemesResponse {
+    private final List<ThemeResponse> themeResponses;
+
+    public ThemesResponse(List<ThemeResponse> themeResponses) {
+        this.themeResponses = themeResponses;
+    }
+
+    public static ThemesResponse from(List<Theme> themes) {
+        List<ThemeResponse> responses = themes.stream()
+                .map(ThemeResponse::from)
+                .toList();
+
+        return new ThemesResponse(responses);
+    }
+
+    public List<ThemeResponse> getThemeResponses() {
+        return themeResponses;
+    }
+}
