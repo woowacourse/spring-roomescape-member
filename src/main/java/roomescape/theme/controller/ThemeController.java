aa80dda3d5e6bcd6294c07f5904ed6a61f793d29
snowshower <wonsb25@gmail.com>
2026-05-06 17:56:57 +0900

    feat: 테마 컨트롤러에 인기 테마 조회 기능 구현
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- src/main/java/roomescape/theme/controller/ThemeController.java
+++ src/main/java/roomescape/theme/controller/ThemeController.java
@@ -3,7 +3,9 @@ package roomescape.theme.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.theme.dto.PopularThemesResponse;
 import roomescape.theme.dto.ThemesResponse;
 import roomescape.theme.service.ThemeService;
 
@@ -20,6 +22,15 @@ public class ThemeController {
     @GetMapping
     public ResponseEntity<ThemesResponse> findAll() {
         ThemesResponse responses = themeService.findAll();
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping(params = "sort")
+    public ResponseEntity<PopularThemesResponse> findPopularThemes(
+            @RequestParam String sort,
+            @RequestParam(defaultValue = "10") int limit,
+            @RequestParam(defaultValue = "7") int days) {
+        PopularThemesResponse responses = themeService.findPopularThemes(sort, limit, days);
         return ResponseEntity.ok(responses);
     }
 }
