2c566bf8e9e3a56a72da2eccc7b75df5f0486a9f
jyt6640 <jyt6640@gmail.com>
2026-05-06 17:52:29 +0900

    feat: create reservationsByDateAndTheme API

--- BE/src/main/java/roomescape/presentation/ReservationController.java
+++ BE/src/main/java/roomescape/presentation/ReservationController.java
@@ -1,6 +1,7 @@
 package roomescape.presentation;
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.application.ReservationService;
 import roomescape.entity.Reservation;
+import roomescape.presentation.dto.AvailableReservationResponse;
 import roomescape.presentation.dto.ReservationRequest;
 import roomescape.presentation.dto.ReservationResponse;
 
@@ -45,11 +48,32 @@ public class ReservationController {
     public ResponseEntity<List<ReservationResponse>> getReservations() {
         List<Reservation> reservations = service.getReservations();
 
-        List<ReservationResponse> response = reservations.stream()
-                .map(ReservationResponse::from)
-                .toList();
+        List<ReservationResponse> response = parseToReservationResponse(reservations);
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AvailableReservationResponse>> getReservationsByDateAndThemeId(
+            @RequestParam LocalDate date,
+            @RequestParam Long themeId
+    ) {
+        List<Reservation> reservations = service.getReservationsByDateAndTheme(date, themeId);
+        List<AvailableReservationResponse> response = parseToAvailableReservationResponse(reservations);
+
+        return ResponseEntity.ok(response);
+    }
+
+    private List<ReservationResponse> parseToReservationResponse(List<Reservation> reservations) {
+        return reservations.stream()
+                .map(ReservationResponse::from)
+                .toList();
+    }
+
+    private List<AvailableReservationResponse> parseToAvailableReservationResponse(List<Reservation> reservations) {
+        return reservations.stream()
+                .map(AvailableReservationResponse::from)
+                .toList();
     }
 
     @DeleteMapping("/{id}")
