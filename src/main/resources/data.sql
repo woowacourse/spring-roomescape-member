d691c972ed90ee3845f115eea9b1cbcb831d384b
minzzun99 <skg06123@naver.com>
2026-05-06 13:53:14 +0900

    chore: 테마 더미 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,7 +13,7 @@ MERGE INTO reservation_time(start_at) KEY (start_at) VALUES
 
 MERGE INTO theme(name, description, thumbnail) KEY (name, description, thumbnail) VALUES
     ('시간조작자 연구소', '반복되는 1시간, 탈출 못하면 다시 시작된다.', '/images/themes/1.png'),
-    ('사라진 개발자', '흔적만 남긴 채 사라진 개발자의 마지막 로그를 추적', '/images/themes/2.png'),
+    ('사라진 개발자', '흔적만 남긴 채 사라진 개발자의 마지막 로그를 추적한다.', '/images/themes/2.png'),
     ('404호의 비밀', '존재하지 않는 방, 들어간 사람은 돌아오지 않는다.', '/images/themes/3.png'),
     ('녹화된 마지막 하루', '누군가의 마지막 하루를 되돌려 탈출해야 한다.', '/images/themes/4.png'),
     ('VIP 전용 금고', '제한 시간 내 금고를 열지 못하면 시스템이 초기화된다.', '/images/themes/5.png'),
