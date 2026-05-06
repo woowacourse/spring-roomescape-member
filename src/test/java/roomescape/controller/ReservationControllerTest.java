61a20e5dd1838cbb38bb58645cde7a4453e6b026
Giyoul <cla12093@gmail.com>
2026-05-06 15:49:02 +0900

    test: 예약 api 테스트 추가

--- src/test/java/roomescape/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/controller/ReservationControllerTest.java
@@ -1,4 +1,62 @@
-import static org.junit.jupiter.api.Assertions.*;
+package roomescape.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationControllerTest {
-  
-}
+    @Autowired
+    private ReservationController reservationController;
+
+    @DisplayName("사용자 예약 추가")
+    @Test
+    void 사용자_예약_추가_API() {
+        Map<String,Object> params = new HashMap<>();
+        params.put("name", "브라운");
+        params.put("date", "2026-05-05");
+        params.put("timeId", 1);
+        params.put("themeId", 1);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201);
+    }
+
+    @DisplayName("사용자 예약 삭제")
+    @Test
+    void 사용자_예약_삭제(){
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .pathParam("id",1)
+                .when().delete("/reservations/{id}")
+                .then().log().all()
+                .statusCode(204);
+    }
+
+    @DisplayName("사용자 예약 조회")
+    @Test
+    void 사용자_예약_조회(){
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .queryParam("username","김철수")
+                .when().get("/reservations")
+                .then().log().all()
+                .statusCode(200);
+
+
+    }
+
+    }
