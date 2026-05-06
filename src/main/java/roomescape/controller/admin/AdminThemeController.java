384c4a566071ed4afb07a914efdc64c5668a9140
yItWorks <poiu7825@pusan.ac.kr>
2026-05-06 15:49:00 +0900

    chore: 사용하지 않는 import문 제거

--- src/main/java/roomescape/controller/admin/AdminThemeController.java
+++ src/main/java/roomescape/controller/admin/AdminThemeController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
