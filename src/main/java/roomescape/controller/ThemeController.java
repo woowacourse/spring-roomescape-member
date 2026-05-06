a5ee6b0c2607007621e6ac7ec250eb6839a40b37
Giyoul <cla12093@gmail.com>
2026-05-06 12:51:47 +0900

    feat: 사용자 테마 시간 조회 기능

--- src/main/java/roomescape/controller/ThemeController.java
+++ src/main/java/roomescape/controller/ThemeController.java
@@ -3,29 +3,41 @@ package roomescape.controller;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.dto.AvailableReservationTimeResponse;
 import roomescape.dto.ThemeResponse;
 import roomescape.service.ThemeService;
 
-@RequestMapping("/theme")
+@RequestMapping("/themes")
 @RestController
 public class ThemeController {
 
-    private final ThemeService userThemeService;
+    private final ThemeService themeService;
 
-    public ThemeController(ThemeService userThemeService) {
-        this.userThemeService = userThemeService;
+    public ThemeController(ThemeService themeService) {
+        this.themeService = themeService;
     }
-    
-    /*
-    사용자 테마 조회 기능	GET /themes	-	200 [{id, name, description, thumbnail_url}]
-    사용자 테마 시간 조회 기능	GET /themes/{id}/times	-	200 {date:"2025-06-06", times:[{id: 1, start_at:"10:00", available:true},...]}
-     */
     
     @GetMapping
     public ResponseEntity<List<ThemeResponse>> getAllThemes() {
-        final List<ThemeResponse> themes = userThemeService.getAllThemes();
+        final List<ThemeResponse> themes = themeService.getAllThemes();
         return ResponseEntity.ok(themes);
     }
+
+    @GetMapping("/{id}/times")
+    public ResponseEntity<List<AvailableReservationTimeResponse>> getReservationTimes(
+            @PathVariable long id,
+            @RequestParam String date) {
+        final List<AvailableReservationTimeResponse> reservationTimeResponses = themeService.getAvailableTimeResponses(id, date);
+        return ResponseEntity.ok(reservationTimeResponses);
+    }
+
+
+    /*
+    사용자 테마 시간 조회 기능	GET /themes/{id}/times	-	200 [{id: 1, start_at:"10:00", available:true},...]
+     */
+
 }
