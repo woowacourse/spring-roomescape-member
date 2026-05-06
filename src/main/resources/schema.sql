5df5a5efe5a32aa83663f585fc0f753b474fadfb
Jeshin Kim <jjason0904@cau.ac.kr>
2026-05-06 18:23:03 +0900

    fix(schema): 스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,8 +22,8 @@ CREATE TABLE reservation
     time_id  BIGINT,
     theme_id BIGINT,
     PRIMARY KEY (id),
-    FOREIGN KEY (time_id) REFERENCES reservation_time (id) CASCADE,
-    FOREIGN KEY (theme_id) REFERENCES themes (id) CASCADE,
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id) ON DELETE CASCADE,
+    FOREIGN KEY (theme_id) REFERENCES themes (id) ON DELETE CASCADE,
     CONSTRAINT unique_reservation UNIQUE (date, time_id, theme_id)
 
 );
