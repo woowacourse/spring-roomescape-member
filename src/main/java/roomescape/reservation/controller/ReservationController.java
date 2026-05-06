8cf74bc73f9631ffd46b0480b7d4af727723fc79
smiinii <sm020320@naver.com>
2026-05-06 16:38:04 +0900

    feat: 예약 생성 컨트롤러 추가

--- src/main/java/roomescape/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/reservation/controller/ReservationController.java
@@ -1,11 +1,17 @@
 package roomescape.reservation.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.reservation.dto.ReservationIdResponse;
+import roomescape.reservation.dto.ReservationRequest;
 import roomescape.reservation.dto.ReservationsResponse;
 import roomescape.reservation.service.ReservationService;
 
@@ -17,6 +23,12 @@ public class ReservationController {
 
     public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationIdResponse> create(@RequestBody @Valid ReservationRequest request) {
+        ReservationIdResponse response = reservationService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
