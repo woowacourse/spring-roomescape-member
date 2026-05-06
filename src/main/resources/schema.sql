cc9baf4a9c4bb759f93e67489d14e78303743b7d
softmoca <acyc0909@gmail.com>
2026-05-06 14:32:56 +0900

    feat : 테마 테이블 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,21 @@
 drop table if exists reservation;
 drop table if exists reservation_time;
+drop table if exists theme;
 
 CREATE TABLE reservation_time (
                                   id       BIGINT       NOT NULL AUTO_INCREMENT,
                                   start_at TIME NOT NULL,
                                   PRIMARY KEY (id)
 );
+
+CREATE TABLE theme (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL ,
+  description   VARCHAR(255) NOT NULL ,
+  thumbnailUrl  VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
 
 CREATE TABLE reservation (
                              id      BIGINT       NOT NULL AUTO_INCREMENT,
