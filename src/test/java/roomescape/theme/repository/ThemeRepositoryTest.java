281a928b35b5d5b9c8abd409d0eab03fe13cf08e
smiinii <sm020320@naver.com>
2026-05-06 13:44:56 +0900

    test: 모든 테마 정보 조회 테스트 코드 추가

--- src/test/java/roomescape/theme/repository/ThemeRepositoryTest.java
+++ src/test/java/roomescape/theme/repository/ThemeRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import roomescape.theme.model.Theme;
 
 import java.time.LocalTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,5 +43,26 @@ class ThemeRepositoryTest {
 
         Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM theme WHERE id = ?", Integer.class, savedId);
         assertThat(count).isEqualTo(0);
+    }
+
+    @Test
+    void 모든_테마_정보를_정상적으로_조회한다() {
+        Theme theme1 = new Theme(1L, "테마1", "설명1", "경로1", LocalTime.of(2, 0));
+        Theme theme2 = new Theme(2L, "테마2", "설명2", "경로2", LocalTime.of(2, 0));
+
+        themeRepository.create(theme1);
+        themeRepository.create(theme2);
+
+        List<Theme> themes = themeRepository.findAll();
+
+        assertThat(themes).isNotNull();
+        assertThat(themes.get(0).getName()).isEqualTo("테마1");
+        assertThat(themes.get(1).getName()).isEqualTo("테마2");
+        assertThat(themes.get(0).getDescription()).isEqualTo("설명1");
+        assertThat(themes.get(1).getDescription()).isEqualTo("설명2");
+        assertThat(themes.get(0).getImageUrl()).isEqualTo("경로1");
+        assertThat(themes.get(1).getImageUrl()).isEqualTo("경로2");
+        assertThat(themes.get(0).getRequiredTime()).isEqualTo(LocalTime.of(2, 0));
+        assertThat(themes.get(1).getRequiredTime()).isEqualTo(LocalTime.of(2, 0));
     }
 }
