218526e64ad5b878757ffc34876692ce81ae2269
JinGoo <zzgg3040@naver.com>
2026-05-06 17:50:17 +0900

    fix: 시간 포멧팅 수정

--- src/main/java/roomescape/dto/reservation/ReservationResponseDto.java
+++ src/main/java/roomescape/dto/reservation/ReservationResponseDto.java
@@ -1,20 +1,28 @@
 package roomescape.dto.reservation;
 
-import roomescape.domain.Reservation;
-import roomescape.domain.ReservationTime;
-
 import java.time.format.DateTimeFormatter;
+import roomescape.domain.Reservation;
+import roomescape.domain.Theme;
 
 public record ReservationResponseDto(
         Long id,
         String name,
         String date,
-        ReservationTime time
+        String time,
+        Theme theme
 ) {
 
     public static ReservationResponseDto from(Reservation reservation) {
         String name = reservation.getName().value();
         String date = reservation.getDate().format(DateTimeFormatter.ISO_LOCAL_DATE);
-        return new ReservationResponseDto(reservation.getId(), name, date, reservation.getTime());
+        String time = reservation.getTime().getStartAt().format(DateTimeFormatter.ofPattern("HH:mm"));
+
+        return new ReservationResponseDto(
+            reservation.getId(),
+            name,
+            date,
+            time,
+            reservation.getTheme()
+        );
     }
 }
