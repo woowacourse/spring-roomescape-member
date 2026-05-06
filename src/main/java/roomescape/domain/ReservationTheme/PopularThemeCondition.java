e0d74ba9a8ab7c2ba689da17ab6468f1b39e4b5b
Chocoding1 <galmeagi2@naver.com>
2026-05-06 17:23:47 +0900

    refactor: 쿼리 파라미터 order 삭제

--- src/main/java/roomescape/domain/ReservationTheme/PopularThemeCondition.java
+++ src/main/java/roomescape/domain/ReservationTheme/PopularThemeCondition.java
@@ -1,4 +1,4 @@
 package roomescape.domain.ReservationTheme;
 
-public record PopularThemeCondition(String startDate, String endDate, long size, String order) {
+public record PopularThemeCondition(String startDate, String endDate, long size) {
 }
