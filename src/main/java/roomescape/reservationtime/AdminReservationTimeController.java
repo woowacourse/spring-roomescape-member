3baebb06d10279065aa04f98575c8a7d934c2d27
Jeshin Kim <jjason0904@cau.ac.kr>
2026-05-06 18:19:28 +0900

    fix(ReservationTimeController): RequestMapping빠진 부분 수정

--- src/main/java/roomescape/reservationtime/AdminReservationTimeController.java
+++ src/main/java/roomescape/reservationtime/AdminReservationTimeController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/admin/times")
 public class AdminReservationTimeController {
     private final AdminReservationTimeService adminReservationTimeService;
 
