6dce755df8b5a8519a9f4fd6f111c1eaabefa1b3
jhk01007 <jhk001007@naver.com>
2026-05-06 16:44:10 +0900

    test: 인기테마 조회 테스트 추가

--- src/test/java/roomescape/repository/JdbcThemeRepositoryTest.java
+++ src/test/java/roomescape/repository/JdbcThemeRepositoryTest.java
@@ -1,15 +1,19 @@
 package roomescape.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 import roomescape.domain.Theme;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.groups.Tuple.tuple;
 
@@ -19,6 +23,21 @@ class JdbcThemeRepositoryTest {
 
     @Autowired
     private JdbcThemeRepository jdbcThemeRepository;
+
+    @Test
+    @DisplayName("Theme를 저장하고 조회한다.")
+    public void saveAndFindById() {
+        Theme theme = jdbcThemeRepository.save(new Theme("kim", "desc1", "thumb1"));
+
+        Optional<Theme> found = jdbcThemeRepository.findById(theme.getId());
+
+        assertThat(found).isPresent();
+        Theme savedTheme = found.get();
+        assertThat(savedTheme.getId()).isEqualTo(theme.getId());
+        assertThat(savedTheme.getName()).isEqualTo("kim");
+        assertThat(savedTheme.getDescription()).isEqualTo("desc1");
+        assertThat(savedTheme.getThumbnail()).isEqualTo("thumb1");
+    }
 
     @Test
     @DisplayName("모든 Theme를 불러온다.")
@@ -42,18 +61,22 @@ class JdbcThemeRepositoryTest {
     }
 
     @Test
-    @DisplayName("Theme를 저장하고 조회한다.")
-    public void saveAndFindById() {
-        Theme theme = jdbcThemeRepository.save(new Theme("kim", "desc1", "thumb1"));
+    @DisplayName("지정된 날짜 기간의 지정된 갯수 만큼의 테마정보를 예약 수를 순서대로 불러온다.")
+    @Sql(scripts = "/popular-theme-data.sql")
+    public void findTopThemesByReservationCount() {
+        // given - @/popular-theme-data.sql
 
-        Optional<Theme> found = jdbcThemeRepository.findById(theme.getId());
+        // when
+        List<Theme> topThemes = jdbcThemeRepository.findTopThemesByReservationCount(
+                LocalDate.of(2026, 4, 29),
+                LocalDate.of(2026, 5, 5),
+                10
+        );
 
-        assertThat(found).isPresent();
-        Theme savedTheme = found.get();
-        assertThat(savedTheme.getId()).isEqualTo(theme.getId());
-        assertThat(savedTheme.getName()).isEqualTo("kim");
-        assertThat(savedTheme.getDescription()).isEqualTo("desc1");
-        assertThat(savedTheme.getThumbnail()).isEqualTo("thumb1");
+        // then
+        assertThat(topThemes).hasSizeLessThanOrEqualTo(10)
+                .extracting(Theme::getId)
+                .containsExactly(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L);
     }
 
     @Test
