bb0f1b94cf87c1ec62e4b7e3bf4d0c915a9d303a
MODUGGAGI <kiroro0814@naver.com>
2026-05-06 18:25:09 +0900

    refactor: 관리자와 사용자 api 분리

---
+++ src/main/java/roomescape/theme/controller/AdminThemeController.java
@@ -0,0 +1,43 @@
+package roomescape.theme.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.theme.dto.ThemeCreateRequest;
+import roomescape.theme.dto.ThemeResponse;
+import roomescape.theme.service.ThemeService;
+
+@RequiredArgsConstructor
+@RequestMapping("/admin/themes")
+@RestController
+public class AdminThemeController {
+
+    private final ThemeService themeService;
+
+    @PostMapping
+    public ResponseEntity<ThemeResponse> create(
+            @RequestBody ThemeCreateRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(themeService.save(request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long id
+    ) {
+        int deletedCount = themeService.delete(id);
+
+        if (deletedCount == 0) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/roomescape/theme/controller/ThemeController.java
+++ src/main/java/roomescape/theme/controller/ThemeController.java
@@ -3,17 +3,11 @@ package roomescape.theme.controller;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.theme.dto.PopularThemeResponse;
-import roomescape.theme.dto.ThemeCreateRequest;
 import roomescape.theme.dto.ThemeResponse;
 import roomescape.theme.service.ThemeService;
 
@@ -32,26 +26,5 @@ public class ThemeController {
     @GetMapping("/popular-top-10")
     public ResponseEntity<List<PopularThemeResponse>> findPopularThemes() {
         return ResponseEntity.ok(themeService.findPopularThemes(LocalDate.now()));
-    }
-
-    @PostMapping
-    public ResponseEntity<ThemeResponse> create(
-            @RequestBody ThemeCreateRequest request
-    ) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(themeService.save(request));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(
-            @PathVariable Long id
-    ) {
-        int deletedCount = themeService.delete(id);
-
-        if (deletedCount == 0) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.noContent().build();
     }
 }
