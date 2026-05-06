30bf6e803870b464ccb8cecbcf046d5aa34477c1
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-05-06 13:52:32 +0900

    feat: 프론트 연결을 위한 cors 설정 파일 작성

---
+++ src/main/java/roomescape/config/WebConfig.java
@@ -0,0 +1,20 @@
+package roomescape.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "http://localhost:*",
+                        "http://127.0.0.1:*"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
