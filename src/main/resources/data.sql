70bc26c57a23ce617d940533c212d304e762f89d
JinGoo <zzgg3040@naver.com>
2026-05-06 21:06:20 +0900

    chore: 방탈출 사진 기본 이미지 URL 저장

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 INSERT INTO theme (name, description, image_url)
-VALUES ('우주 미스터리', '우주선에서 벌어지는 사건을 추적하라', 'https://example.com/space.jpg'),
-       ('좀비 아포칼립스', '좀비 떼를 피해 탈출하라', 'https://example.com/zombie.jpg'),
-       ('마법사의 성', '고대 마법사의 성에서 보물을 찾아라', 'https://example.com/wizard.jpg'),
-       ('탐정 사무소', '의문의 살인 사건을 해결하라', 'https://example.com/detective.jpg'),
-       ('해적선의 보물', '해적선에 숨겨진 보물의 비밀', 'https://example.com/pirate.jpg'),
-       ('시간 여행자', '뒤틀린 시간선을 바로잡아라', 'https://example.com/time.jpg'),
-       ('비밀 실험실', '미친 과학자의 실험실에서 탈출하라', 'https://example.com/lab.jpg'),
-       ('고대 이집트', '파라오의 저주를 풀어라', 'https://example.com/egypt.jpg'),
-       ('유령의 저택', '저택에 깃든 원혼을 달래라', 'https://example.com/ghost.jpg'),
-       ('스파이 미션', '국가 기밀을 회수하라', 'https://example.com/spy.jpg');
+VALUES ('우주 미스터리', '우주선에서 벌어지는 사건을 추적하라', 'https://i.pinimg.com/736x/87/e8/a2/87e8a2af70721d5b886d4343425d1bd4.jpg'),
+       ('좀비 아포칼립스', '좀비 떼를 피해 탈출하라', 'https://i.pinimg.com/736x/87/e8/a2/87e8a2af70721d5b886d4343425d1bd4.jpg'),
+       ('마법사의 성', '고대 마법사의 성에서 보물을 찾아라', 'https://i.pinimg.com/736x/87/e8/a2/87e8a2af70721d5b886d4343425d1bd4.jpg'),
+       ('탐정 사무소', '의문의 살인 사건을 해결하라', 'https://i.pinimg.com/736x/87/e8/a2/87e8a2af70721d5b886d4343425d1bd4.jpg'),
+       ('해적선의 보물', '해적선에 숨겨진 보물의 비밀', 'https://i.pinimg.com/736x/87/e8/a2/87e8a2af70721d5b886d4343425d1bd4.jpg'),
+       ('시간 여행자', '뒤틀린 시간선을 바로잡아라', 'https://i.pinimg.com/736x/87/e8/a2/87e8a2af70721d5b886d4343425d1bd4.jpg'),
+       ('비밀 실험실', '미친 과학자의 실험실에서 탈출하라', 'https://i.pinimg.com/736x/87/e8/a2/87e8a2af70721d5b886d4343425d1bd4.jpg'),
+       ('고대 이집트', '파라오의 저주를 풀어라', 'https://i.pinimg.com/736x/87/e8/a2/87e8a2af70721d5b886d4343425d1bd4.jpg'),
+       ('유령의 저택', '저택에 깃든 원혼을 달래라', 'https://i.pinimg.com/736x/87/e8/a2/87e8a2af70721d5b886d4343425d1bd4.jpg'),
+       ('스파이 미션', '국가 기밀을 회수하라', 'https://i.pinimg.com/736x/87/e8/a2/87e8a2af70721d5b886d4343425d1bd4.jpg');
