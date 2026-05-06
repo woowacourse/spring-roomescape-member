5c86162b7345caa3aebdfa58668e350defefb451
Yoonhojoon <dbsghwns1209@gmail.com>
2026-05-06 14:24:02 +0900

    feat: HoidayServiceImpl 기능 구현

---
+++ src/main/java/roomescape/holiday/service/HolidayService.java
@@ -0,0 +1,14 @@
+package roomescape.holiday.service;
+
+import java.util.List;
+
+import roomescape.holiday.domain.Holiday;
+import roomescape.holiday.service.dto.HolidaySaveServiceDto;
+
+public interface HolidayService {
+    List<Holiday> getAll();
+
+    Holiday create(HolidaySaveServiceDto holiday);
+
+    void delete(Long id);
+}
---
+++ src/main/java/roomescape/holiday/service/dto/HolidaySaveServiceDto.java
@@ -0,0 +1,5 @@
+package roomescape.holiday.service.dto;
+
+import java.time.LocalDate;
+
+public record HolidaySaveServiceDto(LocalDate date) {}
