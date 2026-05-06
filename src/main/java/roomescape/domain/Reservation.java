07369081b670a1267fd6d0054209aec1121dabd5
YEJIN KIM <glow.yejin@gmail.com>
2026-05-06 14:32:39 +0900

    test: ReservationRepository 테스트 추가

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -1,6 +1,7 @@
 package roomescape.domain;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Reservation {
 
@@ -52,5 +53,35 @@ public class Reservation {
 
     public Long getThemeId() {
         return theme.getId();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Reservation that = (Reservation) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name)
+                && Objects.equals(date, that.date) && Objects.equals(time,
+                that.time) && Objects.equals(theme, that.theme);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, date, time, theme);
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date=" + date +
+                ", time=" + time +
+                ", theme=" + theme +
+                '}';
     }
 }
--- src/test/java/roomescape/repository/JdbcReservationRepositoryTest.java
+++ src/test/java/roomescape/repository/JdbcReservationRepositoryTest.java
@@ -1,55 +1,115 @@
 package roomescape.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import roomescape.domain.Reservation;
+import roomescape.domain.ReservationTime;
+import roomescape.domain.Theme;
 
 @SpringBootTest
+@Transactional
 class JdbcReservationRepositoryTest {
 
     @Autowired
-    JdbcReservationTimeRepository jdbcReservationTimeRepository;
+    JdbcReservationRepository reservationRepository;
+    @Autowired
+    JdbcReservationTimeRepository reservationTimeRepository;
+    @Autowired
+    JdbcThemeRepository themeRepository;
 
     @DisplayName("예약시간을 저장한다")
     @Test
     void 예약시간을_저장하면_id를_부여한다() {
         // given
+        ReservationTime reservationTime = reservationTimeRepository.save(
+                new ReservationTime(LocalTime.parse("10:20:30")));
+        Theme theme = themeRepository.save(new Theme("귀신찾기", "귀신을 찾는다", "example.com"));
+        Reservation reservation = new Reservation("루드비코", LocalDate.parse("2026-05-06"),
+                reservationTime,
+                theme);
 
         // when
+        Reservation saved = reservationRepository.save(reservation);
 
         // then
+        assertThat(saved.getId()).isNotNull();
     }
 
     @DisplayName("예약시간을 id로 조회한다")
     @Test
     void 예약시간을_id로_조회한다() {
         // given
+        ReservationTime reservationTime = reservationTimeRepository.save(
+                new ReservationTime(LocalTime.parse("10:20:30")));
+        Theme theme = themeRepository.save(new Theme("귀신찾기", "귀신을 찾는다", "example.com"));
+        Reservation reservation = new Reservation("루드비코", LocalDate.parse("2026-05-06"),
+                reservationTime,
+                theme);
 
         // when
+        Reservation saved = reservationRepository.save(reservation);
+        Optional<Reservation> result = reservationRepository.findById(saved.getId());
 
         // then
+        assertThat(result.get()).isEqualTo(saved);
     }
 
     @DisplayName("저장된 모든 예약시간을 조회한다")
     @Test
     void 저장된_모든_예약시간을_조회한다() {
         // given
+        ReservationTime reservationTime1 = reservationTimeRepository.save(
+                new ReservationTime(LocalTime.parse("10:20:30")));
+        Theme theme = themeRepository.save(new Theme("귀신찾기", "귀신을 찾는다", "example.com"));
+        Reservation rudevicoReservation = new Reservation("루드비코", LocalDate.parse("2026-05-06"),
+                reservationTime1,
+                theme);
+
+        ReservationTime reservationTime2 = reservationTimeRepository.save(
+                new ReservationTime(LocalTime.parse("11:20:30")));
+        Reservation cocoReservation = new Reservation("코코", LocalDate.parse("2026-05-06"),
+                reservationTime2,
+                theme);
 
         // when
+        Reservation savedRudevicoReservation = reservationRepository.save(rudevicoReservation);
+        Reservation savedCocoReservation = reservationRepository.save(cocoReservation);
+
+        List<Reservation> foundReservations = reservationRepository.findAll();
 
         // then
+        assertThat(foundReservations)
+                .hasSize(2)
+                .containsExactlyInAnyOrder(
+                        savedRudevicoReservation, savedCocoReservation
+                );
     }
 
     @DisplayName("id에 해당하는 예약시간을 삭제한다")
     @Test
     void 예약시간을_삭제한다() {
         // given
+        ReservationTime reservationTime = reservationTimeRepository.save(
+                new ReservationTime(LocalTime.parse("10:20:30")));
+        Theme theme = themeRepository.save(new Theme("귀신찾기", "귀신을 찾는다", "example.com"));
+        Reservation reservation = new Reservation("루드비코", LocalDate.parse("2026-05-06"),
+                reservationTime,
+                theme);
 
         // when
+        reservationRepository.delete(reservation.getId());
+        Optional<Reservation> result = reservationRepository.findById(reservation.getId());
 
         // then
+        assertThat(result).isNotPresent();
     }
 }
