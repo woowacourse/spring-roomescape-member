3dae3e96796c00df59045e8b5f2ae831a0ea5eda
yItWorks <poiu7825@pusan.ac.kr>
2026-05-06 11:22:52 +0900

    test: 초기 데이터 파일 추가로 인한 테스트 코드 수정

--- src/test/java/roomescape/controller/ThemeControllerTest.java
+++ src/test/java/roomescape/controller/ThemeControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 
 import static org.hamcrest.Matchers.is;
@@ -15,60 +14,25 @@ import static org.hamcrest.Matchers.is;
 class ThemeControllerTest {
     @Autowired
     private ThemeController themeController;
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     @Test
     void 예약_가능한_시간_조회_API() {
-        // given
-        // 1. 테마 추가
-        jdbcTemplate.execute("INSERT INTO theme (name, description, url) VALUES ('우테코 공포물', '레벨2 미션의 공포', '/horror')");
-
-        // 2. 전체 시간대 추가 (10:00, 13:00)
-        jdbcTemplate.execute("INSERT INTO reservation_time (start_at) VALUES ('10:00:00')");
-        jdbcTemplate.execute("INSERT INTO reservation_time (start_at) VALUES ('13:00:00')");
-
-        // 3. 이미 예약된 내역 추가 (10:00 예약됨)
-        jdbcTemplate.update("""
-                    INSERT INTO reservation (name, date, time_id, theme_id) 
-                    VALUES (?, ?, ?, ?)
-                """, "브라운", "2026-05-04", 1, 1);
-
         // when & then
         RestAssured.given().log().all()
-                .when().get("/theme/1/available-time?date=2026-05-04")
+                .when().get("/theme/1/available-time?date=2026-05-01")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(1));
+                .body("size()", is(4));
     }
 
     @Test
     @DisplayName("최근 1주동안 예약이 많았던 테마를 조회하는 정상 테스트")
     void 테마_조회_API() {
-        // given
-        // 1. 테마 추가
-        jdbcTemplate.execute("INSERT INTO theme (name, description, url) VALUES ('미래 도시', '2050년 서울의 이야기', '/future-city')");
-        jdbcTemplate.execute("INSERT INTO theme (name, description, url) VALUES ('고대 이집트', '파라오의 저주를 풀어라', '/egypt')");
-        jdbcTemplate.execute("INSERT INTO theme (name, description, url) VALUES ('마법 학교', '마법사가 되기 위한 여정', '/magic-school')");
-
-        // 2. 전체 시간대 추가
-        jdbcTemplate.execute("INSERT INTO reservation_time (start_at) VALUES ('10:00:00')");
-        jdbcTemplate.execute("INSERT INTO reservation_time (start_at) VALUES ('11:00:00')");
-        jdbcTemplate.execute("INSERT INTO reservation_time (start_at) VALUES ('12:00:00')");
-
-        // 3. 이미 예약된 내역 추가
-        String sql = """
-                    INSERT INTO reservation (name, date, time_id, theme_id) 
-                    VALUES (?, ?, ?, ?)
-                """;
-        jdbcTemplate.update(sql, "브라운", "2026-05-04", 1, 1);
-        jdbcTemplate.update(sql, "브라운", "2026-05-05", 1, 2);
-
         // when & then
         RestAssured.given().log().all()
-                .when().get("/theme?limit=2")
+                .when().get("/theme?limit=10")
                 .then().log().all()
                 .statusCode(200)
-                .body("size()", is(2));
+                .body("size()", is(10));
     }
 }
--- src/test/java/roomescape/service/ReservationDuplicateTest.java
+++ src/test/java/roomescape/service/ReservationDuplicateTest.java
@@ -24,16 +24,8 @@ class ReservationDuplicateTest {
     @Test
     @DisplayName("이미 존재하는 예약 건과 중복으로 예약하면 예외가 발생한다")
     void throwsException_whenDuplicateReservationExists() {
-        jdbcTemplate.update("INSERT INTO reservation_time (id, start_at) VALUES (?, ?)", 1, "10:00");
-        jdbcTemplate.update("INSERT INTO theme (id, name, description, url) VALUES (?, ?, ?, ?)", 1, "테마1", "설명", "url");
 
-
-        jdbcTemplate.update("""
-                INSERT INTO reservation (name, date, time_id, theme_id)
-                VALUES (?, ?, ?, ?)
-            """, "브라운", "2026-05-04", 1, 1);
-
-        ReservationRequest request = new ReservationRequest("아나키", LocalDate.of(2026, 5, 4), 1L, 1L);
+        ReservationRequest request = new ReservationRequest("아나키", LocalDate.of(2026, 5, 1), 1L, 1L);
 
         assertThatThrownBy(() -> reservationService.save(request))
                 .isInstanceOf(IllegalArgumentException.class)
