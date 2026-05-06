fc0eac9036b78a891a0c6200b7df87037879d589
choubung <choubung@sookmyung.ac.kr>
2026-05-06 16:01:11 +0900

    test(Theme) : 권한에 따른 테마 추가/삭제 인가 테스트 추가

--- src/test/java/roomescape/controller/ThemeControllerTest.java
+++ src/test/java/roomescape/controller/ThemeControllerTest.java
@@ -6,14 +6,18 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import roomescape.domain.ReservationTime;
 import roomescape.domain.Theme;
-import roomescape.dto.theme.PopularThemeResponseDto;
+import roomescape.dto.reservationTime.ReservationTimeRequestDto;
+import roomescape.dto.reservationTime.ReservationTimeResponseDto;
 import roomescape.dto.theme.PopularThemesResponseDto;
 import roomescape.dto.theme.ThemeRequestDto;
 import roomescape.dto.theme.ThemeResponseDto;
@@ -22,6 +26,7 @@ import roomescape.service.ThemeService;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -29,7 +34,6 @@ class ThemeControllerTest {
 
     private static final Theme THEME = new Theme(null, "name", "description", "image-url");
     private static final Theme SAVED_THEME = new Theme(1L, "name", "description", "image-url");
-    private static final ThemeRequestDto THEME_REQUEST = ThemeRequestDto.from(THEME);
     private static final ThemeResponseDto THEME_RESPONSE = ThemeResponseDto.from(THEME);
 
     @LocalServerPort
@@ -41,42 +45,6 @@ class ThemeControllerTest {
     @BeforeEach
     void setPort() {
         RestAssured.port = port;
-    }
-
-    @Test
-    void 테마를_추가한다() {
-        // given
-        when(themeService.addTheme(THEME_REQUEST))
-                .thenReturn(SAVED_THEME);
-
-        // when
-        Response response = RestAssured
-                .given().log().all()
-                .contentType(ContentType.JSON)
-                .body(THEME_REQUEST)
-                .when().post("/themes");
-
-        // then
-        response
-                .then()
-                .statusCode(HttpStatus.CREATED.value());
-
-        ThemeResponseDto responseDto = response.as(ThemeResponseDto.class);
-        assertThat(responseDto.id()).isEqualTo(SAVED_THEME.getId());
-    }
-
-    @Test
-    void 테마를_삭제한다() {
-        // given & when
-        Response response = RestAssured
-                .given().log().all()
-                .pathParam("id", 1)
-                .when().delete("/themes/{id}");
-
-        // then
-        response
-                .then()
-                .statusCode(HttpStatus.NO_CONTENT.value());
     }
 
     @Test
@@ -129,11 +97,97 @@ class ThemeControllerTest {
         assertThat(actualResponse).isEqualTo(expectedResponse);
     }
 
+    @Nested
+    @DisplayName("인가 권한 테스트")
+    class RoleForbidden {
+        @Test
+        void 관리자는_테마를_추가할_수_있다() {
+            // given
+            ThemeRequestDto request = themeRequestDtoFrom(THEME);
+            when(themeService.addTheme(any()))
+                    .thenReturn(SAVED_THEME);
+
+            // when
+            Response response = RestAssured
+                    .given().log().all()
+                    .queryParam("role", "admin")
+                    .contentType(ContentType.JSON)
+                    .body(request)
+                    .when().post("/themes");
+
+            // then
+            response
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+
+            ThemeResponseDto responseDto = response.as(ThemeResponseDto.class);
+            assertThat(responseDto).isEqualTo(ThemeResponseDto.from(SAVED_THEME));
+        }
+
+        @Test
+        void 관리자는_테마를_삭제할_수_있다() {
+            // given & when
+            Response response = RestAssured
+                    .given().log().all()
+                    .queryParam("role", "admin")
+                    .pathParam("id", 1)
+                    .when().delete("/themes/{id}");
+
+            // then
+            response
+                    .then()
+                    .statusCode(HttpStatus.NO_CONTENT.value());
+        }
+
+        @Test
+        void 관리자가_아닌_사용자가_테마를_추가하는_경우_예외가_발생한다() {
+            // given
+            ThemeRequestDto request = themeRequestDtoFrom(THEME);
+
+            // when
+            Response response = RestAssured
+                    .given().log().all()
+                    .queryParam("role", "user")
+                    .contentType(ContentType.JSON)
+                    .body(request)
+                    .when().post("/themes");
+
+
+            // then
+            response
+                    .then()
+                    .statusCode(HttpStatus.FORBIDDEN.value());
+        }
+
+        @Test
+        void 관리자가_아닌_사용자가_테마를_삭제하는_경우_예외가_발생한다() {
+            // given & when
+            Response response = RestAssured
+                    .given().log().all()
+                    .queryParam("role", "user")
+                    .pathParam("id", 1)
+                    .when().delete("/themes/{id}");
+
+            // then
+            response
+                    .then()
+                    .statusCode(HttpStatus.FORBIDDEN.value());
+        }
+    }
+
     private List<Theme> createTenThemes() {
         List<Theme> themes = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             themes.add(new Theme((long) i, "테마" + i, "테마" + i, "테마" + i));
         }
         return themes;
+    }
+
+    private ThemeRequestDto themeRequestDtoFrom(Theme theme) {
+        return new ThemeRequestDto(
+                theme.getName(),
+                theme.getDescription(),
+                theme.getImageUrl()
+        );
     }
 }
