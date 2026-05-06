2583f49616f4803a62a533a8ec2cf671681f5f7b
박병훈 <leafloveu@naver.com>
2026-05-06 15:20:56 +0900

    chore: MissionStepTest 패키지 변경

--- src/test/java/roomescape/missiontest/MissionStep3Test.java
+++ src/test/java/roomescape/missiontest/MissionStep3Test.java
@@ -1,4 +1,4 @@
-package roomescape;
+package roomescape.missiontest;
 
 import static org.hamcrest.Matchers.is;
 
--- src/test/java/roomescape/missiontest/MissionStep4Test.java
+++ src/test/java/roomescape/missiontest/MissionStep4Test.java
@@ -1,4 +1,4 @@
-package roomescape;
+package roomescape.missiontest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
