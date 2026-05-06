7775f1214b8b775e7b3dd123760527ff60063d2c
mvg01 <dkms625@naver.com>
2026-05-06 17:02:56 +0900

    test: ThemeServiceTest 추가

---
+++ src/test/java/roomescape/service/ThemeServiceTest.java
@@ -0,0 +1,29 @@
+package roomescape.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ThemeServiceTest {
+
+    @Autowired
+    private ThemeService themeService;
+
+    @Test
+    void 존재하지_않는_테마를_삭제하는경우_예외가_발생한다() {
+        // then
+        Assertions.assertThatThrownBy(() -> themeService.removeTheme(-1L))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 존재하는_테마를_삭제하는경우_삭제된다() {
+        // then
+        Assertions.assertThatCode(() -> themeService.removeTheme(1L))
+                .doesNotThrowAnyException();
+    }
+}
