24b49900005f69fb1a496aa141587e8c993f52c3
Giyoul <cla12093@gmail.com>
2026-05-06 10:43:10 +0900

    feat: response에 테마 추가하기

--- src/main/java/roomescape/repository/ReservationDao.java
+++ src/main/java/roomescape/repository/ReservationDao.java
@@ -9,6 +9,7 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
+import roomescape.domain.Theme;
 
 @Repository
 public class ReservationDao {
@@ -19,7 +20,8 @@ public class ReservationDao {
             rs.getLong("reservation_id"),
             rs.getString("name"),
             rs.getDate("date").toLocalDate(),
-            new ReservationTime(rs.getLong("time_id"), rs.getTime("time_value").toLocalTime())
+            new ReservationTime(rs.getLong("time_id"), rs.getTime("time_value").toLocalTime()),
+            new Theme(rs.getLong("theme_id"), rs.getString("theme_name"), rs.getString("theme_description"), rs.getString("theme_thumbnail"))
     );
 
     public ReservationDao(JdbcTemplate jdbcTemplate) {
@@ -31,18 +33,22 @@ public class ReservationDao {
 
     public List<Reservation> findAll() {
         String sql = """
-                SELECT r.id AS reservation_id, r.name, r.date, t.id AS time_id, t.start_at AS time_value
+                SELECT r.id AS reservation_id, r.name, r.date,
+                       t.id AS time_id, t.start_at AS time_value,
+                       th.id AS theme_id, th.name AS theme_name, th.description AS theme_description, th.thumbnail_url AS theme_thumbnail
                 FROM reservation AS r
                 INNER JOIN reservation_time AS t ON r.time_id = t.id
+                INNER JOIN theme AS th ON r.theme_id = th.id
                 """;
         return jdbcTemplate.query(sql, reservationRowMapper);
     }
 
-    public Long save(String name, LocalDate date, Long timeId) {
+    public Long save(String name, LocalDate date, Long timeId, Long themeId) {
         return jdbcInsert.executeAndReturnKey(Map.of(
                 "name", name,
                 "date", date,
-                "time_id", timeId
+                "time_id", timeId,
+                "theme_id", themeId
         )).longValue();
     }
 
