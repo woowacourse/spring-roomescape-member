572a0c5e0a506ab63414e6bc44fa2690bd828edc
Jihyun3478 <leejihyun3478@gmail.com>
2026-05-06 11:32:55 +0900

    refactor: 예약된 시간 조회 성능 개선 및 예약 가능 여부 변수명 수정

--- src/main/java/roomescape/dto/ReservationTimeResponse.java
+++ src/main/java/roomescape/dto/ReservationTimeResponse.java
@@ -1,18 +1,17 @@
 package roomescape.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 import roomescape.domain.ReservationTime;
 
 public record ReservationTimeResponse(
         Long id,
         LocalTime startAt,
-        boolean isAvailable
+        boolean isNotReserved
 ) {
-    public static ReservationTimeResponse from(ReservationTime reservationTime, boolean isAvailable) {
+    public static ReservationTimeResponse from(ReservationTime reservationTime, boolean isNotReserved) {
         return new ReservationTimeResponse(
                 reservationTime.getId(),
                 reservationTime.getStartAt(),
-                isAvailable);
+                isNotReserved);
     }
 }
