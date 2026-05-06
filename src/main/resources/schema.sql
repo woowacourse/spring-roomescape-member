d24a0a1997895001dedf479a61e4c8ff0f302574
JunHyung1206 <pjh120610@naver.com>
2026-05-06 14:42:06 +0900

    fix: 스키마 DATE, TIME 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE theme
 CREATE TABLE reservation_time
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL,
+    start_at TIME NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -23,7 +23,7 @@ CREATE TABLE reservation
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
-    date     VARCHAR(255) NOT NULL,
+    date     DATE NOT NULL,
     time_id  BIGINT       NOT NULL,
     theme_id BIGINT       NOT NULL,
     PRIMARY KEY (id),
