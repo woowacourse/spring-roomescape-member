29c499914a60f7d2a3d8a972e3dfb3c65c03b88e
Giyoul <cla12093@gmail.com>
2026-05-06 15:49:02 +0900

    test: 예약 api 테스트 추가

--- src/test/java/roomescape/reservation/ReservationTest.java
+++ src/test/java/roomescape/reservation/ReservationTest.java
@@ -8,13 +8,36 @@ import java.time.LocalTime;
 import org.junit.jupiter.api.Test;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
+import roomescape.domain.Theme;
 
 class ReservationTest {
 
+    private final ReservationTime time;
+    private final Theme theme;
+
+    public ReservationTest() {
+        this.time = new ReservationTime(1L, LocalTime.of(15, 40));
+        this.theme = new Theme(1L, "공포의 저택", "버려진 저택에서 탈출하라! 어둠 속에 숨겨진 비밀을 밝혀야 살 수 있다.",
+                "https://picsum.photos/seed/haunted/400/250");
+    }
+
+    //    private final Long id;
+    //    private final String name;
+    //    private final LocalDate date;
+    //    private final ReservationTime time;
+    //    private final Theme theme;
+
+    //public class Theme {
+    //    private final Long id;
+    //    private final String name;
+    //    private final String description;
+    //    private final String thumbnailUrl;
+
+    // '공포의 저택', '버려진 저택에서 탈출하라! 어둠 속에 숨겨진 비밀을 밝혀야 살 수 있다.', 'https://picsum.photos/seed/haunted/400/250');
+
     @Test
     void 예약_생성() {
-        ReservationTime time = new ReservationTime(1L, LocalTime.of(15, 40));
-        Reservation reservation = new Reservation(1L, "브라운", LocalDate.of(2023, 8, 5), time);
+        Reservation reservation = new Reservation(1L, "브라운", LocalDate.of(2023, 8, 5), time, theme);
 
         assertThat(reservation.getId()).isEqualTo(1L);
         assertThat(reservation.getName()).isEqualTo("브라운");
@@ -26,7 +49,7 @@ class ReservationTest {
     void 이름이_null이면_예외() {
         ReservationTime time = new ReservationTime(1L, LocalTime.of(15, 40));
 
-        assertThatThrownBy(() -> new Reservation(1L, null, LocalDate.of(2023, 8, 5), time))
+        assertThatThrownBy(() -> new Reservation(1L, null, LocalDate.of(2023, 8, 5), time, theme))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -34,7 +57,7 @@ class ReservationTest {
     void 이름이_공백이면_예외() {
         ReservationTime time = new ReservationTime(1L, LocalTime.of(15, 40));
 
-        assertThatThrownBy(() -> new Reservation(1L, "   ", LocalDate.of(2023, 8, 5), time))
+        assertThatThrownBy(() -> new Reservation(1L, "   ", LocalDate.of(2023, 8, 5), time, theme))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -42,13 +65,13 @@ class ReservationTest {
     void 날짜가_null이면_예외() {
         ReservationTime time = new ReservationTime(1L, LocalTime.of(15, 40));
 
-        assertThatThrownBy(() -> new Reservation(1L, "브라운", null, time))
+        assertThatThrownBy(() -> new Reservation(1L, "브라운", null, time, theme))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 시간이_null이면_예외() {
-        assertThatThrownBy(() -> new Reservation(1L, "브라운", LocalDate.of(2023, 8, 5), null))
+        assertThatThrownBy(() -> new Reservation(1L, "브라운", LocalDate.of(2023, 8, 5), null, theme))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
