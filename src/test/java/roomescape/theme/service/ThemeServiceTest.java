64deb9a6951c59b4b9d0147ebbe8e942a150863a
smiinii <sm020320@naver.com>
2026-05-06 13:45:18 +0900

    test: 테마 전체 조회 서비스 테스트 코드 추가

--- src/test/java/roomescape/theme/service/ThemeServiceTest.java
+++ src/test/java/roomescape/theme/service/ThemeServiceTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import roomescape.theme.dto.ThemeRequest;
 import roomescape.theme.dto.ThemeResponse;
+import roomescape.theme.dto.ThemesResponse;
+import roomescape.theme.model.Theme;
 
 import java.time.LocalTime;
 
@@ -38,5 +40,22 @@ class ThemeServiceTest {
         ThemeResponse response = themeService.create(request);
 
         assertDoesNotThrow(() -> themeService.delete(response.getId()));
+    }
+
+    @Test
+    void 테마를_전체_조회한다() {
+        Theme theme1 = new Theme(1L, "테마1", "설명1", "경로1", LocalTime.of(2, 0));
+        Theme theme2 = new Theme(2L, "테마2", "설명2", "경로2", LocalTime.of(2, 0));
+
+        ThemeRequest theme1Request = new ThemeRequest(theme1.getName(), theme1.getDescription(), theme1.getImageUrl(), theme1.getRequiredTime());
+        ThemeRequest theme2Request = new ThemeRequest(theme2.getName(), theme2.getDescription(), theme2.getImageUrl(), theme2.getRequiredTime());
+
+        themeService.create(theme1Request);
+        themeService.create(theme2Request);
+
+        ThemesResponse response = themeService.findAll();
+
+        assertThat(response).isNotNull();
+        assertThat(response.getThemeResponses()).hasSize(2);
     }
 }
