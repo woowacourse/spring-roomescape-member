85b7af7cf2801d5d44e935d7019e88a56520e6a3
mvg01 <dkms625@naver.com>
2026-05-06 18:51:11 +0900

    test: ReservationTimeControllerTest 테스트 추가

---
+++ src/test/java/roomescape/controller/ReservationTimeController.java
@@ -0,0 +1,61 @@
+package roomescape.controller;
+
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.time.LocalTime;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.annotation.DirtiesContext;
+import roomescape.dto.ThemeRequest;
+import roomescape.dto.TimeRequest;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReservationTimeController {
+
+    @Test
+    public void 전체_시간_조회_API() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .when().get("/times")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(14));
+    }
+
+    @Test
+    public void 테마_별_예약가능한_시간_조회_API() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .when().get("/times/2?date=2026-05-04")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(12));
+    }
+
+    @Test
+    public void 예약_가능한_시간_삭제_API() {
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .when().delete("/times/1")
+                .then().log().all()
+                .statusCode(204);
+    }
+
+    @Test
+    public void 예약_가능한_시간_추가_API() {
+        TimeRequest timeRequest = new TimeRequest(LocalTime.of(8,0));
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(timeRequest)
+                .when().post("/times")
+                .then().log().all()
+                .statusCode(201)
+                .body("size()", is(2));
+    }
+}
