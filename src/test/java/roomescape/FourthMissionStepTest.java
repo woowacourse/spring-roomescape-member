7ee20d4b5754c98103df85c29dab7b4463dd6f15
woohyeon <woohyeon6885@gmail.com>
2026-05-06 11:11:05 +0900

    refactor(test): @DirtyContext 제거

--- src/test/java/roomescape/FourthMissionStepTest.java
+++ src/test/java/roomescape/FourthMissionStepTest.java
@@ -1,18 +1,15 @@
 package roomescape;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.lang.reflect.Field;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.DirtiesContext;
 import roomescape.reservation.controller.ReservationController;
 
-import java.lang.reflect.Field;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class FourthMissionStepTest {
 
     @Autowired
