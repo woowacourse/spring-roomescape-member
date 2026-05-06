9a98ac93be8614e3654b363ddd4ad103b446b366
Sumin <new.sumyang@gmail.com>
2026-05-06 17:10:46 +0900

    refactor: 오탈자 수정

--- src/main/java/roomescape/support/exception/ThemeErrorCode.java
+++ src/main/java/roomescape/support/exception/ThemeErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum ThemeErrorCode implements ErrorCode {
     INVALID_THEME(HttpStatus.BAD_REQUEST, "테마는 필수입니다."),
     THEME_NOT_EXIST(HttpStatus.NOT_FOUND, "존재하지 않는 테마 입니다."),
-    THEME_IN_USE(HttpStatus.CONFLICT, "이미 얘약이 존재하는 테마는 삭제할 수 없습니다."),
+    THEME_IN_USE(HttpStatus.CONFLICT, "이미 예약이 존재하는 테마는 삭제할 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
