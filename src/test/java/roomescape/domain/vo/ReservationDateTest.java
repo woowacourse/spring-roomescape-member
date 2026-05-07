dd769cedbe4955a0a476fc31cf0a4d1358dae373
choubung <choubung@sookmyung.ac.kr>
2026-05-07 17:03:41 +0900

    test : ReservationDate 테스트

---
+++ src/test/java/roomescape/domain/vo/ReservationDateTest.java
@@ -0,0 +1,11 @@
+package roomescape.domain.vo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationDateTest {
+    @Test
+    void 오늘_이전의_날짜로_저장시_예외가_발생한다 () {
+    }
+}
