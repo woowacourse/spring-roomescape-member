c59e14e70faeac286e5127e503e013918f9df467
Jinriro <riro12.hanyang.ac.kr>
2026-05-07 16:28:24 +0900

    refactor: 검증 위치 올바르게 변경

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -1,8 +1,9 @@
 package roomescape.reservation.domain;
 
-import java.time.LocalDate;
 import roomescape.theme.domain.Theme;
 import roomescape.time.domain.ReservationTime;
+
+import java.time.LocalDate;
 
 public class Reservation {
 
@@ -15,6 +16,7 @@ public class Reservation {
     private final Theme theme;
 
     public Reservation(Long id, String name, LocalDate date, ReservationTime time, Theme theme) {
+        validateNameSize(name);
         this.id = id;
         this.name = name;
         this.date = date;
@@ -23,11 +25,10 @@ public class Reservation {
     }
 
     public static Reservation create(String name, LocalDate date, ReservationTime time, Theme theme) {
-        validateNameSize(name);
         return new Reservation(null, name, date, time, theme);
     }
 
-    private static void validateNameSize(String name) {
+    private void validateNameSize(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(String.format("이름은 10글자 이하여야 합니다. (현재 이름의 글자 수: %d)", name.length()));
         }
