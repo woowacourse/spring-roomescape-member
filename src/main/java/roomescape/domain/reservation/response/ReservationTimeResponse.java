4a4c34b0bb0f67fe034771d086ff72a6aade9e76
박병훈 <leafloveu@naver.com>
2026-05-07 11:44:39 +0900

    fix(time): LocalTime 출력 "HH:mm" 양식으로 변경

--- src/main/java/roomescape/domain/reservation/response/ReservationTimeResponse.java
+++ src/main/java/roomescape/domain/reservation/response/ReservationTimeResponse.java
@@ -1,10 +1,14 @@
 package roomescape.domain.reservation.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 import roomescape.domain.reservation.entity.ReservationTime;
 
-public record ReservationTimeResponse(Long id, LocalTime startAt) {
-
+public record ReservationTimeResponse(
+        Long id,
+        @JsonFormat(pattern = "HH:mm")
+        LocalTime startAt
+) {
     public static ReservationTimeResponse from(ReservationTime reservationTime) {
         return new ReservationTimeResponse(
                 reservationTime.getId(),
