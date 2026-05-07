44c9c89aa409c819b0b7c2f265a84b9b18aad878
Jinriro <riro12.hanyang.ac.kr>
2026-05-07 09:43:45 +0900

    refactor: schema.sql varchar 길이 조정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,13 +8,13 @@ CREATE TABLE theme (
     id                 BIGINT           NOT NULL AUTO_INCREMENT,
     name               VARCHAR(255)     NOT NULL UNIQUE,
     description        VARCHAR(255)     NOT NULL,
-    thumbnail_url      VARCHAR(255)     NOT NULL,
+    thumbnail_url      VARCHAR(1024)     NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation (
     id                 BIGINT          NOT NULL AUTO_INCREMENT,
-    name               VARCHAR(255)    NOT NULL,
+    name               VARCHAR(50)    NOT NULL,
     reservation_date   DATE            NOT NULL,
     time_id BIGINT                     NOT NULL,
     theme_id BIGINT                    NOT NULL,
