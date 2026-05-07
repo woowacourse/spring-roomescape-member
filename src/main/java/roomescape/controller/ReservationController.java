1df1fd26c148ba3ca90a8a3905026c6b1efb7683
HyoYoonNam <dev.rudevico@gmail.com>
2026-05-07 17:13:23 +0900

    refactor: ReservationController가 RestController 애노테이션 사용

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -2,18 +2,17 @@ package roomescape.controller;
 
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.ReservationRequestDTO;
 import roomescape.dto.ReservationResponseDTO;
 import roomescape.service.ReservationService;
 
-@Controller
+@RestController
 @RequestMapping("/reservations")
 public class ReservationController {
 
@@ -24,7 +23,6 @@ public class ReservationController {
     }
 
     @PostMapping
-    @ResponseBody
     public ResponseEntity<Void> add(@RequestBody ReservationRequestDTO request) {
         ReservationResponseDTO saved = reservationService.addReservation(request);
         return ResponseEntity
