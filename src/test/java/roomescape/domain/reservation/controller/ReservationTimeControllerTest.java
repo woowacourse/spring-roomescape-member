c37c2d747c619bf91fe91008d5f7a4f4abc443e2
박병훈 <leafloveu@naver.com>
2026-05-07 13:35:04 +0900

    test: test-schema drop table 추가 및 random port 지정 추가

--- src/test/java/roomescape/domain/reservation/controller/ReservationTimeControllerTest.java
+++ src/test/java/roomescape/domain/reservation/controller/ReservationTimeControllerTest.java
@@ -7,14 +7,24 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationTimeControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
 
     @Test
     @DisplayName("예약 시간 목록을 조회한다.")
