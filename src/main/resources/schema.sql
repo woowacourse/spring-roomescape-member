62da980a31a2a9a790bd79ea4362fa8e457e7af3
choubung <choubung@sookmyung.ac.kr>
2026-05-07 15:18:19 +0900

    chore : 스키마 타입 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,11 +13,11 @@ CREATE TABLE IF NOT EXISTS theme (
 );
 
 CREATE TABLE IF NOT EXISTS reservation (
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255) NOT NULL,
-    res_date VARCHAR(255) NOT NULL,
-    time_id BIGINT       NOT NULL,
-    theme_id BIGINT      NOT NULL,
+    id       BIGINT        NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(255)  NOT NULL,
+    res_date DATE          NOT NULL,
+    time_id  BIGINT        NOT NULL,
+    theme_id BIGINT        NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
