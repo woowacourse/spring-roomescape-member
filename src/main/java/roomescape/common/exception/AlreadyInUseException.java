ba6a547e89548967854f54f283658b0a7a1078ee
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-05-07 10:20:03 +0900

    refactor: 공통 예외 정의

---
+++ src/main/java/roomescape/common/exception/AlreadyInUseException.java
@@ -0,0 +1,7 @@
+package roomescape.common.exception;
+
+public class AlreadyInUseException extends RuntimeException {
+    public AlreadyInUseException(String message) {
+        super(message);
+    }
+}
