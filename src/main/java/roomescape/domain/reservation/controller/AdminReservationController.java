afc667f04a3fa69e3fca9fa01ad1f1ccd22fd560
everypine <kenton.lim@gmail.com>
2026-05-07 13:51:56 +0900

    feat(api-spec): 예약 생성 api를 관리자용과 사용자용으로 분리

--- src/main/java/roomescape/domain/reservation/controller/AdminReservationController.java
+++ src/main/java/roomescape/domain/reservation/controller/AdminReservationController.java
@@ -6,8 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.domain.reservation.dto.request.ReservationCreateRequestDto;
+import roomescape.domain.reservation.dto.response.ReservationCreateResponseDto;
 import roomescape.domain.reservation.dto.response.ReservationResponseDto;
 import roomescape.domain.reservation.service.ReservationService;
 
@@ -24,6 +28,12 @@ public class AdminReservationController {
     @GetMapping()
     public ResponseEntity<List<ReservationResponseDto>> getReservations() {
         return ResponseEntity.ok(reservationService.getReservations());
+    }
+
+    @PostMapping()
+    public ResponseEntity<ReservationCreateResponseDto> saveReservation(
+        @RequestBody ReservationCreateRequestDto requestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(reservationService.saveReservation(requestDto));
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/roomescape/domain/reservation/controller/ReservationController.java
+++ src/main/java/roomescape/domain/reservation/controller/ReservationController.java
@@ -1,18 +1,13 @@
 package roomescape.domain.reservation.controller;
 
-import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.reservation.dto.request.ReservationCreateRequestDto;
 import roomescape.domain.reservation.dto.response.ReservationCreateResponseDto;
-import roomescape.domain.reservation.dto.response.ReservationResponseDto;
 import roomescape.domain.reservation.service.ReservationService;
 
 @RestController
