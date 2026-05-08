d50e3a61a809327d53c918fa8d4fcdbd556634c8
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-05-08 13:32:09 +0900

    style: schema.sql 정렬

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,18 +8,17 @@ CREATE TABLE theme(
 );
 
 CREATE TABLE reservation_time (
-                                  id       BIGINT       NOT NULL AUTO_INCREMENT,
-                                  start_at VARCHAR(255) NOT NULL,
-                                  PRIMARY KEY (id)
+                    id       BIGINT       NOT NULL AUTO_INCREMENT,
+                    start_at VARCHAR(255) NOT NULL,
+                    PRIMARY KEY (id)
 );
 CREATE TABLE reservation (
-                             id      BIGINT       NOT NULL AUTO_INCREMENT,
-                             name    VARCHAR(255) NOT NULL,
-                             date    VARCHAR(255) NOT NULL,
-                             time_id BIGINT,
-                             theme_id BIGINT,
-                             PRIMARY KEY (id),
-                             FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-                             FOREIGN KEY (theme_id) REFERENCES theme (id)
-
+                    id      BIGINT       NOT NULL AUTO_INCREMENT,
+                    name    VARCHAR(255) NOT NULL,
+                    date    VARCHAR(255) NOT NULL,
+                    time_id BIGINT,
+                    theme_id BIGINT,
+                    PRIMARY KEY (id),
+                    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+                    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
