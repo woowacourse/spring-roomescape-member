fcded5858b4e869a7d11b5143fb0beb4f6e42e79
Huiyeongkim <huiyeong9619@naver.com>
2026-05-08 17:48:32 +0900

    refactor: api controller 패키지 구조 정리

--- src/test/java/roomescape/admin/MissionStep4Test.java
+++ src/test/java/roomescape/admin/MissionStep4Test.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
-import roomescape.controller.ReservationRestController;
+import roomescape.controller.api.ReservationRestController;
 
 import java.lang.reflect.Field;
 
