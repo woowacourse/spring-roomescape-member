1c00f142302a75681881f5d06fc66c73f4524489
armd482 <armd482@naver.com>
2026-05-08 16:31:20 +0900

    refactor: Long타입으로 변경

--- src/main/java/roomescape/dto/Reservation/AddReservationRequest.java
+++ src/main/java/roomescape/dto/Reservation/AddReservationRequest.java
@@ -12,9 +12,9 @@ public record AddReservationRequest(
         @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "날짜는 YYYY-MM-DD 형식이여야 합니다.")
         String date,
         @NotNull(message = "시간 ID가 반드시 포함되어야 합니다.")
-        long timeId,
-        @NotNull
-        long themeId
+        Long timeId,
+        @NotNull(message = "테마 ID가 반드시 포함되어야 합니다.")
+        Long themeId
 ) {
     public ReservationCommand to() {
         return new ReservationCommand(name, date, timeId, themeId);
