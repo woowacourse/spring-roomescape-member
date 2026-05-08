aa1e4dbf72c725296882a25993ed73de6e3110b7
sauter001 <lsj457@naver.com>
2026-05-08 16:47:12 +0900

    fix: 예약 중복 방지를 위한 date, time_id, theme_id 조합에 유니크 제약 조건 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -24,5 +24,6 @@ CREATE TABLE reservation
     theme_id BIGINT       NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-    FOREIGN KEY (theme_id) REFERENCES theme (id)
+    FOREIGN KEY (theme_id) REFERENCES theme (id),
+    UNIQUE (date, time_id, theme_id)
 );
