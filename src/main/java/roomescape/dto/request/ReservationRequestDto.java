d48bb1979b6fdde74ccffc66285829f6a29394ea
SooHyeon Lee <dalsu4594@naver.com>
2026-05-10 05:04:51 +0900

    feat: ReservationRequestDto themeId, TimeId @Positive 검증 추가

--- src/main/java/roomescape/dto/request/ReservationRequestDto.java
+++ src/main/java/roomescape/dto/request/ReservationRequestDto.java
@@ -2,6 +2,7 @@ package roomescape.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
@@ -10,7 +11,7 @@ public record ReservationRequestDto(
         @NotBlank @Size(max = 20, message = "이름은 20자 이하여야 합니다")
         String name,
         @NotNull LocalDate date,
-        @NotNull Long timeId,
-        @NotNull Long themeId
+        @NotNull @Positive Long timeId,
+        @NotNull @Positive Long themeId
 ) {
 }
