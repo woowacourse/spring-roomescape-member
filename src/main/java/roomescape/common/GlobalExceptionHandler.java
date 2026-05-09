8908712228a7d7c1b90589498f368a5cc1dac8dc
toctoce <toctoce@naver.com>
2026-05-09 19:59:46 +0900

    feat: 에러 핸들러 추가
    - IllegalArgumentException, HttpMessageNotReadableException 에러 핸들러를 추가함.
    - 공통 에러 핸들러를 추가함.

--- src/main/java/roomescape/common/GlobalExceptionHandler.java
+++ src/main/java/roomescape/common/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package roomescape.common;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.reservation.exception.ReservationDuplicatedException;
@@ -24,4 +25,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("작업을 처리할 수 없습니다. : %s".formatted(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("예상하지 못한 오류가 발생했습니다. : %s".formatted(e.getMessage()));
+    }
 }
