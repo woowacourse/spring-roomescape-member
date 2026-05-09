c7dc47552a5a4438c0db0f833900f77a72419410
Jinriro <riro12.hanyang.ac.kr>
2026-05-09 20:46:50 +0900

    chore: NPE 발생하는 문제 해결

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -5,7 +5,6 @@ import roomescape.time.domain.ReservationTime;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 public class Reservation {
 
@@ -19,9 +18,10 @@ public class Reservation {
 
     public Reservation(Long id, String name, LocalDate date, ReservationTime time, Theme theme) {
         validateName(name);
-        Objects.requireNonNull(date, "예약 날짜는 필수입니다.");
-        Objects.requireNonNull(time, "예약 시간은 필수입니다.");
-        Objects.requireNonNull(theme, "예약 테마는 필수입니다.");
+        validateNotNull(date, "예약 날짜는 필수입니다.");
+        validateNotNull(time, "예약 시간은 필수입니다.");
+        validateNotNull(theme, "예약 테마는 필수입니다.");
+
         this.id = id;
         this.name = name;
         this.date = date;
@@ -34,7 +34,12 @@ public class Reservation {
     }
 
     public void validateNotPast(LocalDateTime now) {
-        Objects.requireNonNull(now, "현재 시각(now)은 필수입니다.");
+        validateNotNull(now, "현재 시각(now)은 필수입니다.");
+
+        if (time.getStartAt() == null) {
+            throw new IllegalStateException("예약 시간 데이터가 올바르지 않습니다. (startAt is null)");
+        }
+
         LocalDateTime reservationDateTime = LocalDateTime.of(date, time.getStartAt());
         if (reservationDateTime.isBefore(now)) {
             throw new IllegalArgumentException(
@@ -51,6 +56,12 @@ public class Reservation {
             throw new IllegalArgumentException(
                     String.format("이름은 %d글자 이하여야 합니다. (현재 이름의 글자 수: %d)", MAX_NAME_LENGTH, name.length())
             );
+        }
+    }
+
+    private void validateNotNull(Object obj, String message) {
+        if (obj == null) {
+            throw new IllegalArgumentException(message);
         }
     }
 
