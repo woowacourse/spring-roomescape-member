7dd0b2e09a6de86b3401c68deba3f8d26694c453
kkb <gimgyubin1225@gmail.com>
2026-05-09 18:19:00 +0900

    refactor(GlobalExceptionHandler): 예외 관련 로그를 warn, error level로 변경

--- src/main/java/roomescape/global/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/global/exception/GlobalExceptionHandler.java
@@ -14,21 +14,21 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
-        log.info(e.getMessage());
+        log.error(e.getMessage());
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
-        log.info(e.getMessage());
+        log.warn(e.getMessage());
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse("현재 다른 데이터에서 참조 중이어서 삭제할 수 없습니다."));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.info(e.getMessage());
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponse("일시적인 서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요."));
     }
