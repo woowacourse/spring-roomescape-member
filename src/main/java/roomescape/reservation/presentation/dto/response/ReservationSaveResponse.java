2993a4d7df747e564d70ead36e9fa5a11923cbd0
조상준 <juncho12011201@gmail.com>
2026-05-09 17:06:36 +0900

    refactor: 응답/요청값에 date, time 포멧 일관성 있도록 설정

--- src/main/java/roomescape/reservation/presentation/dto/response/ReservationSaveResponse.java
+++ src/main/java/roomescape/reservation/presentation/dto/response/ReservationSaveResponse.java
@@ -1,5 +1,6 @@
 package roomescape.reservation.presentation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import roomescape.reservation.domain.Reservation;
@@ -10,7 +11,7 @@ import roomescape.reservation.presentation.dto.response.dto.TimeInformation;
 public record ReservationSaveResponse(@NotNull Long id,
                                       @NotNull Long themeId,
                                       @NotNull String name,
-                                      @NotNull LocalDate date,
+                                      @JsonFormat(pattern = "yyyy-MM-dd") @NotNull LocalDate date,
                                       @NotNull TimeInformation time) {
     public static ReservationSaveResponse of(ReservationTime time, Theme theme, Reservation reservation) {
         return new ReservationSaveResponse(
