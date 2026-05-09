9348aefef017b21e566d117ea861a0b2f6a74631
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-05-09 16:26:54 +0900

    fix: 500 응답에 내부 예외 메시지가 노출되던 문제 수정

--- src/main/java/roomescape/common/advice/GlobalExceptionHandler.java
+++ src/main/java/roomescape/common/advice/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package roomescape.common.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidation(MethodArgumentNotValidException e) {
@@ -20,6 +24,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleUnhandledException(Exception e) {
-        return ResponseEntity.internalServerError().body("서버 내부에서 예상치 못한 문제가 발생했습니다. " + e.getMessage());
+        log.error("Unhandled exception", e);
+        return ResponseEntity.internalServerError()
+                .body("서버 내부에서 예상치 못한 문제가 발생했습니다.");
     }
 }
