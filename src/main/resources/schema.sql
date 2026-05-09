6aa75682f6194c4a8c48111e2025e0287f9b8505
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-05-09 16:07:58 +0900

    chore(schema.sql): 시간/날짜 관련 스키마 타입 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,19 +3,19 @@ CREATE TABLE theme(
                     name VARCHAR(255) NOT NULL,
                     thumbnail_image_url VARCHAR(255) NOT NULL,
                     description VARCHAR(255) NOT NULL,
-                    duration_time VARCHAR(255) NOT NULL,
+                    duration_time TIME NOT NULL,
                     PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation_time (
-                    id       BIGINT       NOT NULL AUTO_INCREMENT,
-                    start_at VARCHAR(255) NOT NULL,
+                    id       BIGINT NOT NULL AUTO_INCREMENT,
+                    start_at TIME   NOT NULL,
                     PRIMARY KEY (id)
 );
 CREATE TABLE reservation (
                     id      BIGINT       NOT NULL AUTO_INCREMENT,
                     name    VARCHAR(255) NOT NULL,
-                    date    VARCHAR(255) NOT NULL,
+                    date    DATE         NOT NULL,
                     time_id BIGINT,
                     theme_id BIGINT,
                     PRIMARY KEY (id),
