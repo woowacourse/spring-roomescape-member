c358bd4bb0fdf2d90e33255979f708a4514d6e80
toctoce <toctoce@naver.com>
2026-05-09 20:18:37 +0900

    fix: 에러 핸들러가 String을 반환하던 것을 ErrorResponse를 반환하도록 수정

--- src/main/java/roomescape/common/GlobalExceptionHandler.java
+++ src/main/java/roomescape/common/GlobalExceptionHandler.java
@@ -1,40 +1,51 @@
 package roomescape.common;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.common.exception.NotFoundException;
+import roomescape.common.payload.ErrorResponse;
 import roomescape.reservation.exception.ReservationDuplicatedException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleNotFound(NotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFound(NotFoundException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(ReservationDuplicatedException.class)
-    public ResponseEntity<String> handleReservationAlreadyExists(ReservationDuplicatedException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleReservationAlreadyExists(ReservationDuplicatedException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return new ErrorResponse("작업을 처리할 수 없습니다. : %s".formatted(e.getMessage()));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body("작업을 처리할 수 없습니다. : %s".formatted(e.getMessage()));
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return new ErrorResponse("작업을 처리할 수 없습니다. : %s".formatted(e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleAllException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("예상하지 못한 오류가 발생했습니다. : %s".formatted(e.getMessage()));
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleAllException(Exception e) {
+        return new ErrorResponse("예상하지 못한 오류가 발생했습니다. : %s".formatted(e.getMessage()));
     }
 }
---
+++ src/main/java/roomescape/common/payload/ErrorResponse.java
@@ -0,0 +1,4 @@
+package roomescape.common.payload;
+
+public record ErrorResponse(String message) {
+}
