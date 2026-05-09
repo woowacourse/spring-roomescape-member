c8163c8962db9b2838ac84a6a6e7039e228221bd
choubung <choubung@sookmyung.ac.kr>
2026-05-10 00:55:41 +0900

    test(vo) : ReservationDate 오늘 이전 날짜로 예약 시도시 예외 발생 테스트

--- src/test/java/roomescape/domain/vo/ReservationDateTest.java
+++ src/test/java/roomescape/domain/vo/ReservationDateTest.java
@@ -2,10 +2,16 @@ package roomescape.domain.vo;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 class ReservationDateTest {
     @Test
-    void 오늘_이전의_날짜로_저장시_예외가_발생한다 () {
+    void 오늘_이전의_날짜로_저장시_예외가_발생한다() {
+        // when & then
+        assertThatCode(() -> ReservationDate.createForSave(LocalDate.now().minusDays(2)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("오늘 이후의 날짜만 선택할 수 있습니다.");
     }
 }
