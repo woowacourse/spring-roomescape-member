e2838c592f39ce6e09546cfc66a2a18aa42c25a0
koreaioi <koreaioi@gachon.ac.kr>
2026-05-09 17:44:33 +0900

    feat: ReservationDate 정상 작동 테스트 추가

--- src/test/java/roomescape/date/domain/ReservationDateTest.java
+++ src/test/java/roomescape/date/domain/ReservationDateTest.java
@@ -9,6 +9,84 @@ import java.time.LocalDate;
 class ReservationDateTest {
 
     @Test
+    @DisplayName("유효한 날짜로 ReservationDate를 등록할 수 있다.")
+    void create_with_valid_field() {
+        // given
+        LocalDate validDate = LocalDate.of(2099, 1, 1);
+
+        // when & then
+        Assertions.assertThatCode(() -> ReservationDate.create(validDate))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("오늘 날짜로 ReservationDate를 등록할 수 있다.")
+    void create_with_today() {
+        // given
+        LocalDate today = LocalDate.now();
+
+        // when & then
+        Assertions.assertThatCode(() -> ReservationDate.create(today))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("유효한 날짜로 ReservationDate를 생성하면 입력한 날짜를 그대로 유지한다.")
+    void create_compare_date_field() {
+        // given
+        LocalDate validDate = LocalDate.of(2099, 1, 1);
+
+        // when
+        ReservationDate reservationDate = ReservationDate.create(validDate);
+
+        // then
+        Assertions.assertThat(reservationDate.date())
+                .isEqualTo(validDate);
+    }
+
+    @Test
+    @DisplayName("DB로부터 유효한 값을 가져오면 ReservationDate를 생성할 수 있다.")
+    void load() {
+        // given
+        Long loadValidId = 1L;
+        LocalDate loadValidDate = LocalDate.of(2099, 1, 1);
+
+        // when & then
+        Assertions.assertThatCode(() -> {
+            ReservationDate.load(loadValidId, loadValidDate);
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("등록, 로드한 ReservationDate는 id를 제외한 모든 필드가 일치한다.")
+    void creat_compare_load() {
+        // given
+        Long loadValidId = 1L;
+        LocalDate loadValidDate = LocalDate.of(2099, 1, 1);
+        ReservationDate createdDate = ReservationDate.create(loadValidDate);
+        ReservationDate loadedDate = ReservationDate.load(loadValidId, loadValidDate);
+
+        // when & then
+        Assertions.assertThat(createdDate)
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(loadedDate);
+    }
+
+    @Test
+    @DisplayName("DB에서 로드할 때 ID가 null이면 예외가 발생한다.")
+    void load_null_id() {
+        // given
+        Long nullId = null;
+        LocalDate validDate = LocalDate.of(2099, 1, 1);
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> ReservationDate.load(nullId, validDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("예약날짜 ID는 필수입니다.");
+    }
+
+    @Test
     @DisplayName("등록할 날짜가 null이면 예외가 발생한다.")
     void create_null_date() {
         // given
