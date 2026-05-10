cda4165f93bb9227566eb57839d26e6b86c78b42
Minjae Kang <rkdalswoals@gmail.com>
2026-05-10 19:12:08 +0900

    refactor(theme): 커스텀 예외 적용

--- src/main/java/roomescape/domain/Theme.java
+++ src/main/java/roomescape/domain/Theme.java
@@ -1,6 +1,7 @@
 package roomescape.domain;
 
 import lombok.Getter;
+import roomescape.global.exception.InvalidThemeException;
 
 @Getter
 public class Theme {
@@ -31,24 +32,24 @@ public class Theme {
 
     private void validate(String name, String description, String imageUrl) {
         if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("테마 이름은 비어있을 수 없습니다.");
+            throw new InvalidThemeException("테마 이름은 비어있을 수 없습니다.");
         }
         if (name.length() > MAX_VARCHAR_LENGTH) {
-            throw new IllegalArgumentException("테마 이름은 " + MAX_VARCHAR_LENGTH + "자를 초과할 수 없습니다.");
+            throw new InvalidThemeException("테마 이름은 " + MAX_VARCHAR_LENGTH + "자를 초과할 수 없습니다.");
         }
 
         if (description == null || description.isBlank()) {
-            throw new IllegalArgumentException("테마 설명은 비어있을 수 없습니다.");
+            throw new InvalidThemeException("테마 설명은 비어있을 수 없습니다.");
         }
         if (description.length() > MAX_VARCHAR_LENGTH) {
-            throw new IllegalArgumentException("테마 설명은 " + MAX_VARCHAR_LENGTH + "자를 초과할 수 없습니다.");
+            throw new InvalidThemeException("테마 설명은 " + MAX_VARCHAR_LENGTH + "자를 초과할 수 없습니다.");
         }
 
         if (imageUrl == null || imageUrl.isBlank()) {
-            throw new IllegalArgumentException("테마 이미지 URL은 비어있을 수 없습니다.");
+            throw new InvalidThemeException("테마 이미지 URL은 비어있을 수 없습니다.");
         }
         if (imageUrl.length() > MAX_URL_LENGTH) {
-            throw new IllegalArgumentException("이미지 URL은 " + MAX_URL_LENGTH + "자를 초과할 수 없습니다.");
+            throw new InvalidThemeException("이미지 URL은 " + MAX_URL_LENGTH + "자를 초과할 수 없습니다.");
         }
     }
 }
