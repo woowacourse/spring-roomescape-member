87e3e4fa04fa0025eee5cb38ba6b65b7d5d839d7
sun <sun007021@gmail.com>
2026-05-11 02:11:20 +0900

    feat: db start_at 컬럼 스키마 varchar -> TIME 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE theme
 CREATE TABLE reservation_time
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL,
+    start_at TIME NOT NULL,
     PRIMARY KEY (id)
 );
 
