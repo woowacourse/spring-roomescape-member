33c47219b11ea1ec602ee1d5d80c5f957054a05c
choiyoung69 <choiyoung609@gmail.com>
2026-05-10 22:48:16 +0900

    refactor: reservation의 동등성 비교 기준 변경

--- src/main/java/roomescape/reservation/domain/Reservation.java
+++ src/main/java/roomescape/reservation/domain/Reservation.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 
 @Builder
 @Getter
-@EqualsAndHashCode(of = {"name", "date", "themeId", "timeId"})
+@EqualsAndHashCode(of = {"date", "themeId", "timeId"})
 public class Reservation {
 
     private final Long id;
