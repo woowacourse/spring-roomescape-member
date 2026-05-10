46bd60b41af4db9d50f788a414635145a694b853
gleaming9 <gleaming9@khu.ac.kr>
2026-05-11 00:38:59 +0900

    refactor: 입력에 크기 제약 조건 추가

--- src/main/java/roomescape/reservation/dto/ReservationCreateRequest.java
+++ src/main/java/roomescape/reservation/dto/ReservationCreateRequest.java
@@ -3,11 +3,13 @@ package roomescape.reservation.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record ReservationCreateRequest(
         @NotBlank(message = "예약자 이름은 비어 있을 수 없습니다.")
+        @Size(max = 255, message = "예약자 이름은 255자를 넘을 수 없습니다.")
         String name,
 
         @NotNull(message = "예약 날짜는 비어 있을 수 없습니다.")
