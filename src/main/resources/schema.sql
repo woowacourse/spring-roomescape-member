9d46c3afd54c8999a769d8717616453c9f3d60e1
kangrae <kangrae0501@gmail.com>
2026-05-10 15:44:42 +0900

    refactor: LocalDate를 DATE 타입으로 저장하도록 scheme수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS reservation
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
-    date     VARCHAR(255) NOT NULL,
+    date     DATE         NOT NULL,
     time_id  BIGINT       NOT NULL,
     theme_id BIGINT       NOT NULL,
 
