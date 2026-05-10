84e1543404400d39a44f1b4e94ac96d9574a8b0b
Minjae Kang <rkdalswoals@gmail.com>
2026-05-10 19:12:53 +0900

    refactor(exception): 요청 본문 예외 핸들러 추가

--- src/main/java/roomescape/global/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/global/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package roomescape.global.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.global.exception.dto.ErrorResponse;
@@ -25,6 +26,12 @@ public class GlobalExceptionHandler {
         ErrorCode errorCode = ErrorCode.REFERENCED_DATA;
         return ResponseEntity.status(errorCode.getStatus())
                 .body(new ErrorResponse(errorCode.getCode(), "현재 다른 데이터에서 참조 중이어서 삭제할 수 없습니다."));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse("INVALID_REQUEST", "요청 본문 형식이 올바르지 않습니다."));
     }
 
     @ExceptionHandler(Exception.class)
