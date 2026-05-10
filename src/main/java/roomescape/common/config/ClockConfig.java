df478e63c998e089669ad1b44b8a37936ac6156b
jhk01007 <jhk001007@naver.com>
2026-05-10 15:03:58 +0900

    refactor(ClockConfig): @Configuration으로 변경

--- src/main/java/roomescape/common/config/ClockConfig.java
+++ src/main/java/roomescape/common/config/ClockConfig.java
@@ -1,12 +1,12 @@
 package roomescape.common.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
 import java.time.ZoneId;
 
-@Component
+@Configuration
 public class ClockConfig {
 
     @Bean
