ba43c4047ebdf9e3755e896ebee62f27918fdb39
kangrae <kangrae0501@gmail.com>
2026-05-11 11:05:59 +0900

    fix: test에 fixedClock 빈이 주입되도록 수정

--- src/test/java/roomescape/theme/service/ThemeServiceTest.java
+++ src/test/java/roomescape/theme/service/ThemeServiceTest.java
@@ -7,13 +7,11 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import roomescape.theme.entity.Theme;
 import roomescape.theme.exception.ThemeNotFoundException;
 import roomescape.theme.payload.ThemeRequest;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class ThemeServiceTest {
 
