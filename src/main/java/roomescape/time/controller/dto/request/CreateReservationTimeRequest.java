5ee5fd75cc7268073407d09b9558d1de22d626d2
kkb <gimgyubin1225@gmail.com>
2026-05-11 16:30:58 +0900

    chore: dto 클래스명 오타 수정
    - CreateResrvationTimeRequest -> CreateReservationTimeRequest

--- src/main/java/roomescape/time/controller/dto/request/CreateReservationTimeRequest.java
+++ src/main/java/roomescape/time/controller/dto/request/CreateReservationTimeRequest.java
@@ -2,5 +2,5 @@ package roomescape.time.controller.dto.request;
 
 import java.time.LocalTime;
 
-public record CreateResrvationTimeRequest(LocalTime startAt) {
+public record CreateReservationTimeRequest(LocalTime startAt) {
 }
