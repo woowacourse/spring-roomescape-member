d62e5eafc64aa67f6f277626c3d2e5ae326f44e8
rin <chaerin0916@gmail.com>
2026-05-11 21:10:09 +0900

    feat: reservation 테이블에서 외래키에 NOT NULL 제약 조건 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -25,9 +25,9 @@ CREATE TABLE reservation
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
-    date_id  BIGINT,
-    time_id  BIGINT,
-    theme_id BIGINT,
+    date_id  BIGINT NOT NULL,
+    time_id  BIGINT NOT NULL,
+    theme_id BIGINT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (date_id) REFERENCES reservation_date (id),
