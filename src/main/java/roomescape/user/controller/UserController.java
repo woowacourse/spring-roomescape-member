391e9a6ec9c1f2a38d80109a9c7bc042bfc986e2
snowshower <wonsb25@gmail.com>
2026-05-11 10:47:58 +0900

    fix: 사용자 생성 API의 누락된 @RequestBody 어노테이션 추가
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- src/main/java/roomescape/user/controller/UserController.java
+++ src/main/java/roomescape/user/controller/UserController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.user.dto.UserRequest;
@@ -21,7 +22,7 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserResponse> create(@Valid UserRequest request) {
+    public ResponseEntity<UserResponse> create(@RequestBody @Valid UserRequest request) {
         UserResponse response = userService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
