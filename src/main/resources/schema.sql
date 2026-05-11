8917e043c2ceb31b892f6c0ed1ee1bd9becab8dc
Huiyeongkim <huiyeong9619@naver.com>
2026-05-11 18:52:52 +0900

    refactor: startAt TIME으로 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE reservation_time
 (
-    id         BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at   VARCHAR(255) NOT NULL,
+    id         BIGINT NOT NULL AUTO_INCREMENT,
+    start_at   TIME   NOT NULL,
     created_at TIMESTAMP DEFAULT current_timestamp,
     updated_at TIMESTAMP DEFAULT current_timestamp,
     PRIMARY KEY (id)
