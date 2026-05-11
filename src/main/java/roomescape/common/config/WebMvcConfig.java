776d7397ad69eb39cba15a657e22c629d3c0f2aa
dahxxn <dahyeonhan4529@gmail.com>
2026-05-11 22:38:39 +0900

    refactor: PageController를 WebMvcConfig로 대체

---
+++ src/main/java/roomescape/common/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package roomescape.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("user");
+        registry.addViewController("/reservation-lookup").setViewName("reservation-lookup");
+        registry.addViewController("/admin-page").setViewName("admin");
+    }
+}
