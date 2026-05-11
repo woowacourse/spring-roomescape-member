83955f255a22248d4e8da34c39f079916c20eb5d
SeungGyu <phillipskl123@gmail.com>
2026-05-11 14:19:25 +0900

    refactor: 예외에서 HTTP 상태 의존 제거

--- src/main/java/roomescape/reservation/exception/InvalidReservationTimeException.java
+++ src/main/java/roomescape/reservation/exception/InvalidReservationTimeException.java
@@ -1,9 +1,5 @@
 package roomescape.reservation.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidReservationTimeException extends RuntimeException {
 
     public InvalidReservationTimeException(Long timeId) {
