2651ba35a9f0bea020e65fa55c15d8fc3bf3cf46
woohyeon <woohyeon6885@gmail.com>
2026-05-11 23:41:26 +0900

    refactor(application.properties): 타임존을 설정 파일로 분리

--- src/main/java/roomescape/config/ClockConfig.java
+++ src/main/java/roomescape/config/ClockConfig.java
@@ -2,6 +2,7 @@ package roomescape.config;
 
 import java.time.Clock;
 import java.time.ZoneId;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,8 +11,11 @@ import org.springframework.context.annotation.Profile;
 @Profile("!test")
 public class ClockConfig {
 
+    @Value("${spring.clock.zone}")
+    private String clockZone;
+
     @Bean
     public Clock clock() {
-        return Clock.system(ZoneId.of("Asia/Seoul"));
+        return Clock.system(ZoneId.of(clockZone));
     }
 }
