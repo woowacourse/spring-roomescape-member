16247e7f17e05b30af575c97c2d6dfa0c1194ef2
haeyoon1 <hynam1220@naver.com>
2026-05-12 20:47:03 +0900

    refactor: 에러 호출 부 변경

--- src/main/java/roomescape/admin/theme/AdminThemeService.java
+++ src/main/java/roomescape/admin/theme/AdminThemeService.java
@@ -39,7 +39,7 @@ public class AdminThemeService {
 
     private void validateDuplicateTheme(String name) {
         if (adminThemeRepository.existsByName(name)) {
-            throw new IllegalArgumentException("[ERROR] 이미 존재하는 테마 이름입니다.");
+            throw new RoomescapeException(ErrorCode.DUPLICATE_RESERVATION_NAME);
         }
     }
 
