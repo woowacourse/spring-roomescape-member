55b2aa87ec1954131401a254b2ab37db0738df78
Sumin <new.sumyang@gmail.com>
2026-05-12 19:47:19 +0900

    refactor: 에러 포맷팅 누락 해결

--- src/main/java/roomescape/support/exception/ErrorResponse.java
+++ src/main/java/roomescape/support/exception/ErrorResponse.java
@@ -8,9 +8,13 @@ public record ErrorResponse(
     String message
 ) {
 
+    public static ResponseEntity<ErrorResponse> of(HttpStatus httpStatus, RoomescapeException exception) {
+        return ResponseEntity.status(httpStatus)
+            .body(new ErrorResponse(exception.getErrorCode().getCode(), exception.getMessage()));
+    }
+
     public static ResponseEntity<ErrorResponse> of(HttpStatus httpStatus, ErrorCode errorCode) {
         return ResponseEntity.status(httpStatus)
             .body(new ErrorResponse(errorCode.getCode(), errorCode.getMessage()));
     }
-
 }
--- src/main/java/roomescape/support/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/support/exception/GlobalExceptionHandler.java
@@ -10,22 +10,22 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException exception) {
-        return ErrorResponse.of(HttpStatus.BAD_REQUEST, exception.getErrorCode());
+        return ErrorResponse.of(HttpStatus.BAD_REQUEST, exception);
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException exception) {
-        return ErrorResponse.of(HttpStatus.NOT_FOUND, exception.getErrorCode());
+        return ErrorResponse.of(HttpStatus.NOT_FOUND, exception);
     }
 
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<ErrorResponse> handleConflictException(ConflictException exception) {
-        return ErrorResponse.of(HttpStatus.CONFLICT, exception.getErrorCode());
+        return ErrorResponse.of(HttpStatus.CONFLICT, exception);
     }
 
     @ExceptionHandler(InternalServerException.class)
     public ResponseEntity<ErrorResponse> handleInternalServerException(InternalServerException exception) {
-        return ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, exception.getErrorCode());
+        return ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, exception);
     }
 
     @ExceptionHandler(Exception.class)
