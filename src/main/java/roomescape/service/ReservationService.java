ead78cb162b809da8ebdc16c3e86529de93468dc
2Jaeheon <jaeheon0826@jbnu.ac.kr>
2026-05-12 15:34:45 +0900

    refactor: 예외 처리를 위한 기존 예외 로직 제거
    Signed-off-by: 2Jaeheon <jaeheon0826@jbnu.ac.kr>

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,17 +1,15 @@
 package roomescape.service;
 
+import java.util.List;
+import java.util.Objects;
 import org.springframework.stereotype.Service;
-import roomescape.service.command.ReservationSaveCommand;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
 import roomescape.domain.Theme;
 import roomescape.repository.ReservationRepository;
 import roomescape.repository.ReservationTimeRepository;
 import roomescape.repository.ThemeRepository;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Objects;
+import roomescape.service.command.ReservationSaveCommand;
 
 @Service
 public class ReservationService {
@@ -37,23 +35,15 @@ public class ReservationService {
     }
 
     public Reservation saveUserReservation(ReservationSaveCommand command) {
-        if (isPast(command)) {
-            throw new IllegalArgumentException("지난 날짜는 예약할 수 없습니다.");
-        }
-
         return saveReservation(command);
-    }
-
-    private boolean isPast(ReservationSaveCommand command) {
-        return Objects.nonNull(command.date()) && command.date().isBefore(LocalDate.now());
     }
 
     public Reservation saveReservation(ReservationSaveCommand command) {
         ReservationTime reservationTime = reservationTimeRepository.findById(command.timeId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 예약 시간입니다."));
-
         Theme theme = themeRepository.findById(command.themeId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 테마입니다."));
+
         Reservation reservation = new Reservation(null, command.name(), command.date(), reservationTime, theme);
 
         return reservationRepository.addReservation(reservation);
