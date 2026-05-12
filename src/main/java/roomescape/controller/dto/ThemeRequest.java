cfdafc718166de21a88b4ea34f6cfb9932d2e302
nn98 <jkllhgb@gmail.com>
2026-05-12 18:49:50 +0900

    feat(valid): Request DTO Valid 추가

--- src/main/java/roomescape/controller/dto/ThemeRequest.java
+++ src/main/java/roomescape/controller/dto/ThemeRequest.java
@@ -1,4 +1,10 @@
 package roomescape.controller.dto;
 
-public record ThemeRequest(String name, String description, String thumbnailUrl) {
+import jakarta.validation.constraints.NotBlank;
+
+public record ThemeRequest(
+        @NotBlank(message = "이름은 필수입니다.") String name,
+        @NotBlank(message = "이름은 필수입니다.") String description,
+        @NotBlank(message = "이름은 필수입니다.") String thumbnailUrl
+) {
 }
