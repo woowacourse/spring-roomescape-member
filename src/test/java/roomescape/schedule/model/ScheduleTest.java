a2c51af75d3ad1cc9f92b7cb5dc430b2c2b17c27
smiinii <sm020320@naver.com>
2026-05-12 21:29:29 +0900

    test: 스케줄 테스트에서 불필요한 예외 테스트 제거

--- src/test/java/roomescape/schedule/model/ScheduleTest.java
+++ src/test/java/roomescape/schedule/model/ScheduleTest.java
@@ -25,15 +25,6 @@ class ScheduleTest {
     }
 
     @Test
-    void 시작_시간이_null이면_예외가_발생한다() {
-        Theme theme = new Theme("테마", "설명", "경로", LocalTime.of(2, 0));
-
-        assertThatThrownBy(() -> new Schedule(null, theme))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("예약 시작 시간은 필수입니다.");
-    }
-
-    @Test
     void 테마가_null이면_예외가_발생한다() {
         LocalDateTime startAt = LocalDateTime.of(2026, 12, 10, 10, 0);
 
@@ -61,26 +52,6 @@ class ScheduleTest {
 
         assertThat(schedule.getStartAt().toLocalTime()).isEqualTo(LocalTime.of(10, 0));
         assertThat(schedule.getEndAt().toLocalTime()).isEqualTo(LocalTime.of(12, 0));
-    }
-
-    @Test
-    void 오전_10시_이전에_예약을_하면_예외가_발생한다() {
-        LocalDateTime startAt = LocalDateTime.of(2026, 12, 10, 9, 59);
-        Theme theme = new Theme("테마", "설명", "경로", LocalTime.of(2, 0));
-
-        assertThatThrownBy(() -> new Schedule(startAt, theme))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("오전 10시 이전에는 예약이 불가능합니다.");
-    }
-
-    @Test
-    void 오후_8시_이후에_종료되는_스케줄이면_예외가_발생한다() {
-        LocalDateTime startAt = LocalDateTime.of(2026, 12, 10, 18, 1);
-        Theme theme = new Theme("테마", "설명", "경로", LocalTime.of(2, 0));
-
-        assertThatThrownBy(() -> new Schedule(startAt, theme))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("오후 8시 이후에는 예약이 불가능합니다.");
     }
 
     @Test
