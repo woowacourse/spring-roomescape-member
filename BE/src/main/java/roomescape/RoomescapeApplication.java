3da527078e2ab449d7287de4872fb43879ebf54f
kdongsu5509 <kod66170@gmail.com>
2026-05-12 17:39:41 +0900

    feat : change TimeZone
    서버 시간을 한국 시간으로 변경하는 작업 수행

--- BE/src/main/java/roomescape/RoomescapeApplication.java
+++ BE/src/main/java/roomescape/RoomescapeApplication.java
@@ -1,11 +1,13 @@
 package roomescape;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RoomescapeApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(RoomescapeApplication.class, args);
     }
 
