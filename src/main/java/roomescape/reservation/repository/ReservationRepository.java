d9c00ac88db85d3d85d8fd1983636e3179f58814
snowshower <wonsb25@gmail.com>
2026-05-13 00:29:29 +0900

    feat: 사용자 본인 예약 목록 조회 기능 추가
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- src/main/java/roomescape/reservation/repository/ReservationRepository.java
+++ src/main/java/roomescape/reservation/repository/ReservationRepository.java
@@ -92,4 +92,45 @@ public class ReservationRepository {
         Integer count = jdbcTemplate.queryForObject(sql, Integer.class, scheduleId);
         return count != null && count > 0;
     }
+
+    public List<Reservation> findAllByUserId(Long id) {
+        String sql = """
+                SELECT r.id AS reservation_id, 
+                       u.id AS user_id, 
+                       u.name AS user_name, 
+                       u.role AS user_role,
+                       t.id AS theme_id, 
+                       t.description AS theme_description,
+                       t.image_url AS theme_image,         
+                       t.required_time AS theme_required_time,
+                       t.name AS theme_name,
+                       s.id AS schedule_id, 
+                       s.start_at AS start_at, 
+                       s.end_at AS end_at
+                FROM reservation r
+                INNER JOIN "USER" u ON r.user_id = u.id
+                INNER JOIN schedule s ON r.schedule_id = s.id
+                INNER JOIN theme t ON s.theme_id = t.id
+                WHERE r.user_id=?
+                """;
+
+        return jdbcTemplate.query(sql, (resultSet, rowNum) -> {
+            User user = new User(
+                    resultSet.getLong("user_id"),
+                    resultSet.getString("user_name"),
+                    Role.valueOf(resultSet.getString("user_role"))
+            );
+
+            Theme theme = new Theme(resultSet.getLong("theme_id"), resultSet.getString("theme_name"),
+                    resultSet.getString("description"), resultSet.getString("image_url"),
+                    resultSet.getObject("required_time", LocalTime.class));
+
+            Schedule schedule = new Schedule(resultSet.getLong("schedule_id"),
+                    resultSet.getObject("start_at", LocalDateTime.class),
+                    theme);
+
+            return new Reservation(
+                    resultSet.getLong("reservation_id"), user, schedule, theme);
+        }, id);
+    }
 }
