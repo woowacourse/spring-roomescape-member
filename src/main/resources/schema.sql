d2d61778ff32f9f48bb2deaca708dbeaed50d1db
minzzun99 <skg06123@naver.com>
2026-05-12 15:14:03 +0900

    feat: 테마 설명, 썸네일 URL 필수 입력으로 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS reservation_time (
 CREATE TABLE IF NOT EXISTS theme (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
-    thumbnail VARCHAR(255),
+    description VARCHAR(255) NOT NULL,
+    thumbnail VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE(name)
 );
