60617e1dbce162ab9a071dbec2b02bbb733d91dc
kdongsu5509 <kod66170@gmail.com>
2026-05-12 20:24:43 +0900

    refactor: rename error reason

--- BE/src/main/java/roomescape/global/exception/ErrorCode.java
+++ BE/src/main/java/roomescape/global/exception/ErrorCode.java
@@ -9,6 +9,7 @@ public enum ErrorCode {
     THEME_NOT_FOUND("테마를 찾을 수 없습니다. 테마 ID를 확인해주세요."),
 
     // Bad Request - 400
+    REQUEST_NAME_EMPTY("요청 이름이 비어있습니다. 이름을 입력해주세요"),
     RESERVATION_NAME_EMPTY("예약자의 이름이 비어있습니다. 이름을 입력해주세요."),
     RESERVATION_DATE_NULL("예약 날짜가 비어있습니다. 날짜를 선택해주세요."),
     RESERVATION_ID_NULL("예약 목록 ID가 비어있습니다. 요청 형식을 확인해주세요."),
@@ -23,7 +24,7 @@ public enum ErrorCode {
     THEME_ALREADY_USED("사용 중인 테마는 삭제할 수 없습니다. 관련 예약을 먼저 삭제해주세요."),
 
     // Unprocessable Entity - 422
-    PAST_DATE_OR_TIME("지나간 날짜나 시간은 예약할 수 없습니다. 현재 이후의 시간을 선택해주세요.");
+    ILLEGAL_PAST_DATE("지나간 날짜나 시간으로는 예약을 생성하거나 삭제할 수 없습니다. 현재 이후의 시간을 선택해주세요.");
 
     private final String message;
 
