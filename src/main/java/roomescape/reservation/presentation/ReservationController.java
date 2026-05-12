7b19ae98c16a0c72f4fd703026ba34d3903d2655
juntae6942 <juntae6942@o.cnu.ac.kr>
2026-05-12 15:43:07 +0900

    refactor: RequestBody 어노테이션 추가

--- src/main/java/roomescape/reservation/presentation/ReservationController.java
+++ src/main/java/roomescape/reservation/presentation/ReservationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,7 +46,7 @@ public class ReservationController {
     }
 
     @PostMapping
-    public ResponseEntity<ReservationResponse> createReservation(@Valid ReservationRequest request) {
+    public ResponseEntity<ReservationResponse> createReservation(@Valid @RequestBody ReservationRequest request) {
         ReservationResponse response = ReservationResponse.from(reservationService.addReservation(request.toCommand()));
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
@@ -57,7 +58,7 @@ public class ReservationController {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<ReservationResponse> changeReservation(@PathVariable Long id, @Valid ReservationChangeRequest request) {
+    public ResponseEntity<ReservationResponse> changeReservation(@PathVariable Long id, @Valid @RequestBody ReservationChangeRequest request) {
         Reservation reservation = reservationService.changeReservation(id, request.toCommand());
         ReservationResponse response = ReservationResponse.from(reservation);
         return ResponseEntity.ok(response);
