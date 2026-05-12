f31ac264626002fdd801d5106ec26deb2b907443
Jeshin Kim <jjason0904@cau.ac.kr>
2026-05-12 13:33:29 +0900

    refactor(ReservationTimeRequest): DateTimeFormat 어긋날경우 500띄우던 것 400 뜨도록 변경

--- src/main/java/roomescape/reservationtime/controller/ReservationTimeRequest.java
+++ src/main/java/roomescape/reservationtime/controller/ReservationTimeRequest.java
@@ -1,9 +1,11 @@
 package roomescape.reservationtime.controller;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 
 public record ReservationTimeRequest(
         @NotNull(message = "시작 시간을 입력해주세요")
+        @JsonFormat(pattern = "HH:mm:ss")
         LocalTime startAt) {
 }
