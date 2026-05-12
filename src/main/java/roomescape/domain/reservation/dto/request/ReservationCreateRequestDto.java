97a6e7c1a98e73fcc5feb7d00b2863fa48cb160d
everypine <kenton.lim@gmail.com>
2026-05-12 16:10:58 +0900

    feat(reservation): 예약 생성 요청의 이름 필드의 유효성 검증 추가

--- src/main/java/roomescape/domain/reservation/dto/request/ReservationCreateRequestDto.java
+++ src/main/java/roomescape/domain/reservation/dto/request/ReservationCreateRequestDto.java
@@ -7,5 +7,13 @@ public record ReservationCreateRequestDto(String name,
                                           @JsonFormat(pattern = "yyyy-MM-dd") LocalDate date,
                                           Long timeId, Long themeId) {
 
+    public ReservationCreateRequestDto {
+        validateName(name);
+    }
 
+    private static void validateName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 빈 문자열일 수 없습니다.");
+        }
+    }
 }
---
+++ src/test/java/roomescape/domain/reservation/dto/request/ReservationCreateRequestDtoTest.java
@@ -0,0 +1,44 @@
+package roomescape.domain.reservation.dto.request;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class ReservationCreateRequestDtoTest {
+
+    @Nested
+    @DisplayName("생성 테스트")
+    class Constructor {
+
+        @Test
+        @DisplayName("정상 테스트")
+        void 성공() {
+            String name = "이름";
+            LocalDate date = LocalDate.of(2026, 1, 1);
+            Long timeId = 1L;
+            Long themeId = 1L;
+
+            assertThatCode(() -> new ReservationCreateRequestDto(name, date, timeId, themeId))
+                .doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("빈 문자열인 경우 예외가 발생한다.")
+        void 실패1() {
+            String name = "";
+            LocalDate date = LocalDate.of(2026, 1, 1);
+            Long timeId = 1L;
+            Long themeId = 1L;
+
+            assertThatThrownBy(() -> new ReservationCreateRequestDto(name, date, timeId, themeId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 빈 문자열일 수 없습니다.");
+        }
+    }
+
+}
