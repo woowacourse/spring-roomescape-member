27c72dd8a13a8f68db6b09c847633f5c4757d480
quddaz <quddnddl35@naver.com>
2026-05-12 15:05:13 +0900

    test : ReservationAdminApiTest 다시 정의

---
+++ src/test/java/roomescape/acceptancetest/reservation/ReservationAdminApiTest.java
@@ -0,0 +1,43 @@
+package roomescape.acceptancetest.reservation;
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import roomescape.acceptancetest.RoomecapeAcceptanceTest;
+import roomescape.acceptancetest.fixture.AcceptanceTestFixture;
+
+@RoomecapeAcceptanceTest
+class ReservationAdminApiTest {
+
+    @Autowired
+    private AcceptanceTestFixture acceptanceTestFixture;
+
+    @Test
+    @DisplayName("예약 조회")
+    void findReservations() {
+        RestAssured.given().log().all()
+                .when().get("/admin/reservations")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @Test
+    @DisplayName("예약 삭제")
+    void deleteReservation() {
+        acceptanceTestFixture.createTheme();
+        acceptanceTestFixture.createReservationTime("15:40", 1L);
+        acceptanceTestFixture.createReservation("브라운", acceptanceTestFixture.reservationDate(), 1L);
+
+        RestAssured.given().log().all()
+                .when().delete("/admin/reservations/1")
+                .then().log().all()
+                .statusCode(204);
+    }
+
+}
