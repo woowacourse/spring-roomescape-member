e697ab007b9d1209041778df03184c41438d4ecc
haeyoon1 <hynam1220@naver.com>
2026-05-12 19:40:16 +0900

    feat: 에러 코드 enum 생성 - httpStatus - message

---
+++ src/main/java/roomescape/exception/ErrorCode.java
@@ -0,0 +1,36 @@
+package roomescape.exception;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    //400 - BAD_REQUEST
+    INVALID_RESERVATION_REQUEST(HttpStatus.BAD_REQUEST, "유효하지 않은 입력값 입니다."),
+
+    //404 - NOT_FOUND
+    TIME_ID_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 time id를 찾을 수 없습니다"),
+    THEME_ID_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 theme id를 찾을 수 없습니다"),
+    RESERVATION_ID_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 reservation id를 찾을 수 없습니다"),
+
+    //409 - CONFLICT
+    RESERVATION_TIME_PASSED(HttpStatus.CONFLICT, "이미 지난 날짜의 예약은 생성할 수 없습니다."),
+    DUPLICATE_RESERVATION(HttpStatus.CONFLICT, "이미 선택된 예약입니다"),
+    TIME_DELETE_NOT_ALLOWED(HttpStatus.CONFLICT, "예약이 존재하는 시간을 삭제할 수 없습니다."),
+    DUPLICATE_RESERVATION_NAME(HttpStatus.CONFLICT, "해당 이름의 예약이 이미 존재합니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+
+    ErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
