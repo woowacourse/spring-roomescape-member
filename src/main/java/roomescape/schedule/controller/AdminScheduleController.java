19e13b16e4a87799c457960d59e063e3449669c3
snowshower <wonsb25@gmail.com>
2026-05-12 22:43:42 +0900

    feat: 관리자 스케줄 삭제 API 추가
    Signed-off-by: snowshower <wonsb25@gmail.com>

---
+++ src/main/java/roomescape/schedule/controller/AdminScheduleController.java
@@ -0,0 +1,25 @@
+package roomescape.schedule.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.schedule.service.ScheduleService;
+
+@RestController
+@RequestMapping("/admin/schedules")
+public class AdminScheduleController {
+
+    private final ScheduleService scheduleService;
+
+    public AdminScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        scheduleService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
