cfef218947beebdc69ffdcc3786952ab3ffe9c5e
kkb <gimgyubin1225@gmail.com>
2026-05-12 21:16:25 +0900

    feat: 도메인 식별자 유효성 검증 로직 추가
    - id값이 음수이거나 null인 경우 예외를 발생시킨다.

--- src/main/java/roomescape/reservation/controller/dto/CreateReservationRequest.java
+++ src/main/java/roomescape/reservation/controller/dto/CreateReservationRequest.java
@@ -3,6 +3,7 @@ package roomescape.reservation.controller.dto;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -16,9 +17,11 @@ public record CreateReservationRequest(
         LocalDate date,
 
         @NotNull(message = "시간 정보는 필수입니다.")
+        @Positive(message = "식별자는 양수여야 합니다.")
         Long timeId,
 
         @NotNull(message = "테마 정보는 필수입니다.")
+        @Positive(message = "식별자는 양수여야 합니다.")
         Long themeId
 ) {
 }
--- src/test/java/roomescape/reservation/domain/ReservationTest.java
+++ src/test/java/roomescape/reservation/domain/ReservationTest.java
@@ -6,6 +6,8 @@ import java.time.LocalTime;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import roomescape.theme.domain.Theme;
 import roomescape.time.domain.ReservationTime;
 
@@ -14,14 +16,14 @@ class ReservationTest {
     @Test
     void 식별자가_같은_경우_동등한_객체로_판단한다() {
         // given
-        ReservationTime reservationTimeA = new ReservationTime(1L, LocalTime.of(10,1));
-        ReservationTime reservationTimeB = new ReservationTime(2L, LocalTime.of(10,2));
+        ReservationTime reservationTimeA = new ReservationTime(1L, LocalTime.of(10, 1));
+        ReservationTime reservationTimeB = new ReservationTime(2L, LocalTime.of(10, 2));
         Theme themeA = new Theme(1L, "A", "hello", "/resources/image/...");
         Theme themeB = new Theme(2L, "B", "hello", "/resources/image/...");
 
         // when
-        Reservation reservationA = new Reservation(1L, "name1", LocalDate.of(2026,1,1), reservationTimeA, themeA);
-        Reservation reservationB = new Reservation(1L, "name2", LocalDate.of(2026,1,2), reservationTimeB, themeB);
+        Reservation reservationA = new Reservation(1L, "name1", LocalDate.of(2026, 1, 1), reservationTimeA, themeA);
+        Reservation reservationB = new Reservation(1L, "name2", LocalDate.of(2026, 1, 2), reservationTimeB, themeB);
 
         //then
         Assertions.assertThat(reservationA).isEqualTo(reservationB);
@@ -32,12 +34,33 @@ class ReservationTest {
         //given
         String name = "a".repeat(51);
         ReservationTime reservationTime = new ReservationTime(1L, LocalTime.now());
-        LocalDate now = LocalDate.now();
-
-        Theme theme = new Theme(null, "hello", "world", "/resources/image/...");
+        Theme theme = new Theme(1L, "hello", "world", "/resources/image/...");
 
         //when & then
-        Assertions.assertThatThrownBy(() -> new Reservation(1L, name, now, reservationTime, theme))
+        Assertions.assertThatThrownBy(() -> new Reservation(1L, name, LocalDate.now(), reservationTime, theme))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {-1L, 0L})
+    void 식별자가_0보다_작은_경우_예외가_발생한다(Long id) {
+        //given
+        ReservationTime reservationTime = new ReservationTime(1L, LocalTime.now());
+        Theme theme = new Theme(1L, "hello", "world", "/resources/image/...");
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> new Reservation(id, "userA", LocalDate.now(), reservationTime, theme))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 식별자가_null_인_경우_예외가_발생한다() {
+        //given
+        ReservationTime reservationTime = new ReservationTime(1L, LocalTime.now());
+        Theme theme = new Theme(1L, "hello", "world", "/resources/image/...");
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> new Reservation(null, "userA", LocalDate.now(), reservationTime, theme))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
