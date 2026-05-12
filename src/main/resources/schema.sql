b5729f03757e2a12d42a52b9a4d845da24ff7838
armd482 <armd482@naver.com>
2026-05-12 16:18:03 +0900

    refactor: reservation 테이블 날짜, theme, time 중복 제약 조건 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,13 +13,14 @@ CREATE TABLE theme (
 );
 
 CREATE TABLE reservation (
-     id      BIGINT       NOT NULL AUTO_INCREMENT,
-     name    VARCHAR(255) NOT NULL,
-     date    DATE NOT NULL,
-     time_id BIGINT NOT NULL,
-     theme_id BIGINT NOT NULL,
-     created_at DATE DEFAULT CURRENT_DATE,
-     PRIMARY KEY (id),
-     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
-     FOREIGN KEY (theme_id) REFERENCES theme (id)
+    id      BIGINT       NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(255) NOT NULL,
+    date    DATE NOT NULL,
+    time_id BIGINT NOT NULL,
+    theme_id BIGINT NOT NULL,
+    created_at DATE DEFAULT CURRENT_DATE,
+    PRIMARY KEY (id),
+    UNIQUE (date, theme_id, time_id),
+    FOREIGN KEY (time_id) REFERENCES reservation_time (id),
+    FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
