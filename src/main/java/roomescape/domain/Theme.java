b39e3d27f64cbccc32f626db9a6bcbb3e56bbc77
yItWorks <poiu7825@pusan.ac.kr>
2026-05-13 17:49:22 +0900

    feat: 테마 유효성 검사 로직 추가

--- src/main/java/roomescape/domain/Theme.java
+++ src/main/java/roomescape/domain/Theme.java
@@ -1,5 +1,7 @@
 package roomescape.domain;
 
+import java.util.Objects;
+
 public class Theme {
     private Long id;
     private String name;
@@ -7,10 +9,23 @@ public class Theme {
     private String url;
 
     public Theme(Long id, String name, String description, String url) {
+        validate(name, description, url);
         this.id = id;
         this.name = name;
         this.description = description;
         this.url = url;
+    }
+
+    private void validate(String name, String description, String url) {
+        validateName(name);
+        Objects.requireNonNull(description, "테마 설명이 비어 있습니다.");
+        Objects.requireNonNull(url, "테마 썸네일 주소가 비어 있습니다.");
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("테마 이름이 비어 있습니다.");
+        }
     }
 
     public Long getId() {
