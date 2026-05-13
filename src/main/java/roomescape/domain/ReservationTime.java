e1a0a55c429066948d3558566390c9dff1d85340
zzaekkii <zzaekkii@gmail.com>
2026-05-13 21:16:07 +0900

    refactor(time): 필수값 검증 추가

--- src/main/java/roomescape/domain/ReservationTime.java
+++ src/main/java/roomescape/domain/ReservationTime.java
@@ -13,6 +13,7 @@ public class ReservationTime {
 
     private ReservationTime(final Long id, final LocalTime startAt, final LocalTime endAt) {
         validateStartAt(startAt);
+        validateEndAt(endAt);
 
         this.id = id;
         this.startAt = startAt;
@@ -35,6 +36,12 @@ public class ReservationTime {
     private void validateStartAt(final LocalTime startAt) {
         if (startAt == null) {
             throw new IllegalArgumentException("예약 시작 시간을 입력해야 합니다.");
+        }
+    }
+
+    private void validateEndAt(final LocalTime endAt) {
+        if (endAt == null) {
+            throw new IllegalArgumentException("예약 종료 시간을 입력해야 합니다.");
         }
     }
 }
