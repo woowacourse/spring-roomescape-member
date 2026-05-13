87a11550a16aec351d67169cdbbb48f89f2424d0
yItWorks <poiu7825@pusan.ac.kr>
2026-05-13 17:32:12 +0900

    test: 예약 유효성 검사 테스트 추가

--- src/test/java/roomescape/domain/ReservationTest.java
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -1,62 +1,89 @@
 package roomescape.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static roomescape.config.FixedClockConfig.TODAY;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullSource;
 
 class ReservationTest {
 
     @Test
-    @DisplayName("올바른 정보로 예약을 생성하면 성공한다")
-    void create_reservation_success() {
-        // given
+    @DisplayName("올바른 정보로 예약을 생성하면 성공한다.")
+    void 정상_예약_테스트() {
         String name = "아나키";
-        LocalDate date = LocalDate.of(2026, 5, 4);
+        LocalDate date = LocalDate.parse(TODAY);
         ReservationTime time = new ReservationTime(1L, LocalTime.of(10, 0));
         Theme theme = new Theme(1L, "공포", "너무무서워", "/horror");
 
-        // when
-        Reservation reservation = new Reservation(name, date, time, theme);
-
-        // then
-        assertThat(reservation.getName()).isEqualTo(name);
-        assertThat(reservation.getDate()).isEqualTo(date);
-        assertThat(reservation.getTime()).isEqualTo(time);
+        assertDoesNotThrow(() -> new Reservation(name, date, time, theme));
     }
 
-    @ParameterizedTest
-    @NullSource
-    @DisplayName("예약자 이름이 null이면 예외가 발생한다")
-    void validate_name_fail(String name) {
-        // given
-        LocalDate date = LocalDate.now();
+    @Test
+    @DisplayName("예약자 이름이 null 이면 예외가 발생한다.")
+    void 이름이_null_예외_테스트() {
+        String name = null;
+        LocalDate date = LocalDate.parse(TODAY);
         ReservationTime time = new ReservationTime(1L, LocalTime.of(10, 0));
         Theme theme = new Theme(1L, "공포", "너무무서워", "/horror");
 
-        // when & then
         assertThatThrownBy(() -> new Reservation(name, date, time, theme))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("예약자명이 유효하지 않습니다.");
+                .hasMessage("예약자 이름은 비어 있을 수 없습니다.");
     }
 
-    @ParameterizedTest
-    @NullSource
-    @DisplayName("예약 날짜가 null이면 예외가 발생한다")
-    void validate_date_fail(LocalDate date) {
-        // given
-        String name = "아나키";
+    @Test
+    @DisplayName("예약자 이름이 빈칸이면 예외가 발생한다.")
+    void 이름이_빈칸_예외_테스트() {
+        String name = "";
+        LocalDate date = LocalDate.parse(TODAY);
         ReservationTime time = new ReservationTime(1L, LocalTime.of(10, 0));
         Theme theme = new Theme(1L, "공포", "너무무서워", "/horror");
 
-        // when & then
         assertThatThrownBy(() -> new Reservation(name, date, time, theme))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("예약 날짜가 유효하지 않습니다.");
+                .hasMessage("예약자 이름은 비어 있을 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("예약 날짜가 null 이면 예외가 발생한다.")
+    void 날짜가_null_예외_테스트() {
+        String name = "아나키";
+        LocalDate date = null;
+        ReservationTime time = new ReservationTime(1L, LocalTime.of(10, 0));
+        Theme theme = new Theme(1L, "공포", "너무무서워", "/horror");
+
+        assertThatThrownBy(() -> new Reservation(name, date, time, theme))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("예약 날짜가 비어 있습니다.");
+    }
+
+    @Test
+    @DisplayName("예약 시간이 null 이면 예외가 발생한다.")
+    void 시간이_null_예외_테스트() {
+        String name = "아나키";
+        LocalDate date = LocalDate.parse(TODAY);
+        ReservationTime time = null;
+        Theme theme = new Theme(1L, "공포", "너무무서워", "/horror");
+
+        assertThatThrownBy(() -> new Reservation(name, date, time, theme))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("시간이 비어 있습니다.");
+    }
+
+    @Test
+    @DisplayName("테마가 null 이면 예외가 발생한다.")
+    void 테마가_null_예외_테스트() {
+        String name = "아나키";
+        LocalDate date = LocalDate.parse(TODAY);
+        ReservationTime time = new ReservationTime(1L, LocalTime.of(10, 0));
+        Theme theme = null;
+
+        assertThatThrownBy(() -> new Reservation(name, date, time, theme))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("테마가 비어 있습니다.");
     }
 }
