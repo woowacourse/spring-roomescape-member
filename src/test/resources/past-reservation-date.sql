519f7822d9ed9100835171aedc5bcdbd1ce43f9f
koreaioi <koreaioi@gachon.ac.kr>
2026-05-14 02:04:20 +0900

    refactor: 격리된 환경에서 실행되기 위해 데이터 DELETE 수행

--- src/test/resources/past-reservation-date.sql
+++ src/test/resources/past-reservation-date.sql
@@ -1,2 +1,8 @@
+DELETE FROM reservation;
+DELETE FROM reservation_date;
+DELETE FROM reservation_time;
+DELETE FROM theme;
+
+
 INSERT INTO reservation_date (date, is_active)
 VALUES (DATEADD('DAY', -1, CURRENT_DATE), TRUE);
--- src/test/resources/past-reservation-time.sql
+++ src/test/resources/past-reservation-time.sql
@@ -1,2 +1,8 @@
+DELETE FROM reservation;
+DELETE FROM reservation_date;
+DELETE FROM reservation_time;
+DELETE FROM theme;
+
+
 INSERT INTO reservation_time (start_at, is_active)
 VALUES ('10:00:00', TRUE);
