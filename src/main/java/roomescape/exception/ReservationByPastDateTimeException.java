e6cfe023ae031a37bf97f10e4f6f5f12325d4b3b
HyoYoonNam <dev.rudevico@gmail.com>
2026-05-13 16:48:47 +0900

    refactor: 과거 시점에 예약에 대한 예외가 다음 행동을 유도하도록 변경

--- src/main/java/roomescape/exception/ReservationByPastDateTimeException.java
+++ src/main/java/roomescape/exception/ReservationByPastDateTimeException.java
@@ -5,8 +5,8 @@ import java.time.format.DateTimeFormatter;
 
 public class ReservationByPastDateTimeException extends IllegalArgumentException {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm");
-    private static final String ERROR_MESSAGE_FORMAT = "과거 시점에 대한 예약은 불가합니다. 현재=%s, 예약 요청 시점=%s";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final String ERROR_MESSAGE_FORMAT = "과거 시점에 대한 예약은 불가합니다. 다른 날짜나 시간을 선택해 주세요. 현재=%s, 예약 요청 시점=%s";
 
     public ReservationByPastDateTimeException(LocalDateTime now, LocalDateTime targetDateTime) {
         super(ERROR_MESSAGE_FORMAT.formatted(now.format(FORMATTER), targetDateTime.format(FORMATTER)));
