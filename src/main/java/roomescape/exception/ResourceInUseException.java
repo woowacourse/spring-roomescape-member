8b63b522033bd85c879a3564bec400abdb6ec5ea
Jeshin Kim <jjason0904@cau.ac.kr>
2026-05-13 20:01:07 +0900

    refactor(ResourceInUseException): 남아있던 http 상태코드 삭제

--- src/main/java/roomescape/exception/ResourceInUseException.java
+++ src/main/java/roomescape/exception/ResourceInUseException.java
@@ -1,15 +1,7 @@
 package roomescape.exception;
 
-import org.springframework.http.HttpStatus;
-
 public class ResourceInUseException extends ApiException {
-    private final HttpStatus status = HttpStatus.CONFLICT;
-
     public ResourceInUseException(String message) {
         super(message);
-    }
-
-    public HttpStatus getStatus() {
-        return status;
     }
 }
