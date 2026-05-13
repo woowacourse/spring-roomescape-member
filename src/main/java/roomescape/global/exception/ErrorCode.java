8ef9829f2b4f814e90b8585e64ff954df799ef28
sinequanonn <dleck288@gmail.com>
2026-05-13 11:09:24 +0900

    refactor: 에러 코드 이름 enum name()으로 통일

--- src/main/java/roomescape/global/exception/ErrorCode.java
+++ src/main/java/roomescape/global/exception/ErrorCode.java
@@ -4,21 +4,19 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
 
-    INVALID_INPUT("INVALID_INPUT", HttpStatus.BAD_REQUEST, "입력값이 적절하지 않습니다."),
-    INVALID_REQUEST_FORMAT("INVALID_REQUEST_FORMAT", HttpStatus.BAD_REQUEST, "요청 형식이 올바르지 않습니다."),;
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "입력값이 적절하지 않습니다."),
+    INVALID_REQUEST_FORMAT(HttpStatus.BAD_REQUEST, "요청 형식이 올바르지 않습니다."),;
 
-    private final String errorCode;
     private final HttpStatus status;
     private final String message;
 
-    ErrorCode(final String errorCode, final HttpStatus status, final String message) {
-        this.errorCode = errorCode;
+    ErrorCode(final HttpStatus status, final String message) {
         this.status = status;
         this.message = message;
     }
 
     public String errorCode() {
-        return errorCode;
+        return this.name();
     }
 
     public HttpStatus status() {
