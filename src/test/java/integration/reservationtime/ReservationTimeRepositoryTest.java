1226a69385d9e0691d66276df8aada8928e7a977
jihwankim128 <jihwan1535@nate.com>
2026-05-13 18:35:39 +0900

    refactor: 테스트 패키지 분리

--- src/test/java/integration/reservationtime/ReservationTimeRepositoryTest.java
+++ src/test/java/integration/reservationtime/ReservationTimeRepositoryTest.java
@@ -1,4 +1,4 @@
-package integration.reservation;
+package integration.reservationtime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
