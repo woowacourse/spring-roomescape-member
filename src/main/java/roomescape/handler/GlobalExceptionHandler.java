0fba3564c8590afe2c8730d1ceeae6142b32f9ce
K-iise <kimyungi1004@naver.com>
2026-05-13 12:05:28 +0900

    fix: DTO 검증 오류 응답을 리스트로 응답하도록 변경

--- src/main/java/roomescape/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package roomescape.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,12 +14,16 @@ import roomescape.exception.ErrorCode;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(MethodArgumentNotValidException e,
-                                                                        HttpServletRequest request) {
-        ErrorCode error = ErrorCode.valueOf(e.getBindingResult().getFieldError().getDefaultMessage());
-        ErrorResponse errorResponse = new ErrorResponse(
-                error.getCode(), request.getRequestURI(), error.getMessage(), error.getAction()
-        );
-        return ResponseEntity.status(error.getStatus()).body(errorResponse);
+    public ResponseEntity<List<ErrorResponse>> handleIllegalArgumentException(MethodArgumentNotValidException e,
+                                                                              HttpServletRequest request) {
+        List<ErrorResponse> errors = new ArrayList<>();
+        e.getBindingResult().getFieldErrors().forEach((fieldError) -> {
+            ErrorCode error = ErrorCode.valueOf(fieldError.getDefaultMessage());
+            ErrorResponse errorResponse = new ErrorResponse(
+                    error.getCode(), request.getRequestURI(), error.getMessage(), error.getAction()
+            );
+            errors.add(errorResponse);
+        });
+        return ResponseEntity.status(e.getStatusCode()).body(errors);
     }
 }
