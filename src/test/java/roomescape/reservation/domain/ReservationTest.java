ea9ae46736e8d6658565a279c19da6ad3fe6f866
kkb <gimgyubin1225@gmail.com>
2026-05-14 00:00:06 +0900

    feat(reservation): 예약 생성 시 이름이 비어있는 경우 예외 발생

--- src/test/java/roomescape/reservation/domain/ReservationTest.java
+++ src/test/java/roomescape/reservation/domain/ReservationTest.java
@@ -8,6 +8,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import roomescape.global.exception.validation.InvalidNameException;
 import roomescape.global.exception.validation.InvalidNameLengthException;
 import roomescape.theme.domain.Theme;
 import roomescape.time.domain.ReservationTime;
@@ -28,6 +29,18 @@ class ReservationTest {
 
         //then
         Assertions.assertThat(reservationA).isEqualTo(reservationB);
+    }
+
+    @Test
+    void 이름이_비어있는_경우_예외가_발생한다() {
+        //given
+        String name = "";
+        ReservationTime reservationTime = new ReservationTime(1L, LocalTime.now());
+        Theme theme = new Theme(1L, "hello", "world", "/resources/image/...");
+
+        //when & then
+        Assertions.assertThatThrownBy(() -> new Reservation(1L, name, LocalDate.now(), reservationTime, theme))
+                .isInstanceOf(InvalidNameException.class);
     }
 
     @Test
