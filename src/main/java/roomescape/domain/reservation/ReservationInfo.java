f5efe507d168c52a5c61ce773fb85042c2527bc6
armd482 <armd482@naver.com>
2026-05-13 14:19:45 +0900

    refactor: 생성자에 대한 예외 처리 방식 변경

--- src/main/java/roomescape/domain/reservation/ReservationInfo.java
+++ src/main/java/roomescape/domain/reservation/ReservationInfo.java
@@ -7,22 +7,51 @@ import roomescape.domain.theme.Theme;
 import roomescape.exception.InvalidRequestValueException;
 
 public record ReservationInfo(long id, String name, LocalDate date, ReservationTime time, Theme reservationTheme) {
+    private static final String INVALID_NAME_NULL = "이름은 필수입니다.";
     private static final String INVALID_DATE_NULL = "날짜는 필수입니다.";
     private static final String INVALID_DATE_FORMAT = "유효하지 않은 날짜입니다.";
+    private static final String INVALID_TIME_NULL = "시간 정보는 필수입니다.";
+    private static final String INVALID_THEME_NULL = "테마 정보는 필수입니다.";
 
-    public ReservationInfo(long id, String name, String date, ReservationTime time, Theme theme) {
-        this(id, name, validateDate(date), time, theme);
+    public ReservationInfo {
+        validateName(name);
+        validateDateValue(date);
+        validateObjects(time, reservationTheme);
     }
 
-    public static LocalDate validateDate(String date) {
-        if(date == null || date.isBlank()) {
+    public ReservationInfo(long id, String name, String date, ReservationTime time, Theme theme) {
+        this(id, name, parseDate(date), time, theme);
+    }
+
+    private static void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new InvalidRequestValueException(INVALID_NAME_NULL);
+        }
+    }
+
+    private static LocalDate parseDate(String date) {
+        if (date == null || date.isBlank()) {
             throw new InvalidRequestValueException(INVALID_DATE_NULL);
         }
-
         try {
             return LocalDate.parse(date);
         } catch (DateTimeParseException e) {
             throw new InvalidRequestValueException(INVALID_DATE_FORMAT);
+        }
+    }
+
+    private static void validateDateValue(LocalDate date) {
+        if (date == null) {
+            throw new InvalidRequestValueException(INVALID_DATE_NULL);
+        }
+    }
+
+    private static void validateObjects(ReservationTime time, Theme theme) {
+        if (time == null) {
+            throw new InvalidRequestValueException(INVALID_TIME_NULL);
+        }
+        if (theme == null) {
+            throw new InvalidRequestValueException(INVALID_THEME_NULL);
         }
     }
 }
