2ac27a30fa50fed2e4dac64d76c22d89bf8464d5
sun <sun007021@gmail.com>
2026-05-13 22:37:05 +0900

    refactor: 글로벌 예외 핸들러 동작 베이스 예외로 추상화 해 중복 코드 구조 제거

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -7,13 +7,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(DuplicateReservationException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalState(DuplicateReservationException e) {
-        return ResponseEntity.status(e.getStatus()).body(new ErrorResponse(e.getMessage()));
-    }
-
-    @ExceptionHandler(InvalidReservationDateTimeException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalState(InvalidReservationDateTimeException e) {
-        return ResponseEntity.status(e.getStatus()).body(new ErrorResponse(e.getMessage()));
+    @ExceptionHandler(RoomescapeBaseException.class)
+    public ResponseEntity<ErrorResponse> handleRoomescapeException(RoomescapeBaseException e) {
+        return ResponseEntity.status(e.getStatus())
+                .body(new ErrorResponse(e.getMessage()));
     }
 }
