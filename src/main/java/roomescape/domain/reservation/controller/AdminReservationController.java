e2833949ba5e4c724fe1873cf35bea46c5d4816d
박병훈 <leafloveu@naver.com>
2026-05-13 19:21:13 +0900

    feat: 어드민 예약 수정 기능 추가

--- src/main/java/roomescape/domain/reservation/controller/AdminReservationController.java
+++ src/main/java/roomescape/domain/reservation/controller/AdminReservationController.java
@@ -5,12 +5,14 @@ import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.reservation.request.ReservationCreateRequest;
+import roomescape.domain.reservation.request.ReservationUpdateRequest;
 import roomescape.domain.reservation.response.ReservationResponse;
 import roomescape.domain.reservation.response.ReservationsResponse;
 import roomescape.domain.reservation.service.ReservationService;
@@ -38,9 +40,19 @@ public class AdminReservationController {
                 .body(response);
     }
 
+    // TODO: 예약 수정 추가 patch
+    @PatchMapping("/{reservationId}")
+    public ResponseEntity<ReservationResponse> update(
+            @PathVariable Long reservationId,
+            @RequestBody @Valid ReservationUpdateRequest request
+    ) {
+        ReservationResponse response = reservationService.updateReservation(reservationId, request);
+        return ResponseEntity.ok(response);
+    }
+
     @DeleteMapping("/{reservationId}")
     public ResponseEntity<Void> deleteById(@PathVariable Long reservationId) {
-        reservationService.deleteReservationBy(reservationId);
+        reservationService.deleteReservationById(reservationId);
         return ResponseEntity.noContent().build();
     }
 }
