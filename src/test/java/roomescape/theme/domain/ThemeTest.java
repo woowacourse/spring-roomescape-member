80061800d01ec173b1575d79b3084bb4e568c459
kkb <gimgyubin1225@gmail.com>
2026-05-14 00:12:22 +0900

    test: 식별자 유효성 검증 테스트 추가

--- src/test/java/roomescape/theme/domain/ThemeTest.java
+++ src/test/java/roomescape/theme/domain/ThemeTest.java
@@ -2,8 +2,29 @@ package roomescape.theme.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import roomescape.global.exception.validation.InvalidIdException;
 
 class ThemeTest {
+
+    @ParameterizedTest
+    @ValueSource(longs = {-1L, 0L})
+    void 식별자가_0보다_작은_경우_예외가_발생한다(Long id) {
+        // when & then
+        Assertions.assertThatThrownBy(() -> new Theme(id, "hello", "world", "/resources/image/..."))
+                .isInstanceOf(InvalidIdException.class);
+    }
+
+    @Test
+    void 식별자가_null_인_경우_예외가_발생한다() {
+        //given
+        Long id = null;
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> new Theme(id, "hello", "world", "/resources/image/..."))
+                .isInstanceOf(InvalidIdException.class);
+    }
 
     @Test
     void 식별자가_같은_경우_동등한_객체로_판단한다() {
--- src/test/java/roomescape/time/domain/ReservationTimeTest.java
+++ src/test/java/roomescape/time/domain/ReservationTimeTest.java
@@ -3,6 +3,9 @@ package roomescape.time.domain;
 import java.time.LocalTime;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import roomescape.global.exception.validation.InvalidIdException;
 
 class ReservationTimeTest {
     @Test
@@ -13,5 +16,23 @@ class ReservationTimeTest {
 
         // when & then
         Assertions.assertThat(reservationTimeA).isEqualTo(reservationTimeB);
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {-1L, 0L})
+    void 식별자가_0보다_작은_경우_예외가_발생한다(Long id) {
+        // when & then
+        Assertions.assertThatThrownBy(() -> new ReservationTime(id, LocalTime.of(10,1)))
+                .isInstanceOf(InvalidIdException.class);
+    }
+
+    @Test
+    void 식별자가_null_인_경우_예외가_발생한다() {
+        //given
+        Long id = null;
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> new ReservationTime(id, LocalTime.of(10,1)))
+                .isInstanceOf(InvalidIdException.class);
     }
 }
