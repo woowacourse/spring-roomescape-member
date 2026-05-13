41c5c42e11215229d69dcf5d0f3093b9b173bc82
kkb <gimgyubin1225@gmail.com>
2026-05-14 02:45:50 +0900

    test(ReservationDaoTest): 예약 저장 테스트 추가

--- src/test/java/roomescape/reservation/repository/dao/ReservationDaoTest.java
+++ src/test/java/roomescape/reservation/repository/dao/ReservationDaoTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.jdbc.Sql;
 import roomescape.reservation.repository.entity.ReservationEntity;
 
@@ -21,9 +22,42 @@ class ReservationDaoTest {
 
     ReservationDao reservationDao;
 
+    private static final RowMapper<ReservationEntity> reservationRowMapper = (rs, rowNum) ->
+            new ReservationEntity(
+                    rs.getLong("id"),
+                    rs.getString("name"),
+                    rs.getObject("date", LocalDate.class),
+                    rs.getLong("time_id"),
+                    rs.getLong("theme_id"),
+                    rs.getBoolean("is_cancelled")
+            );
+
     @BeforeEach
     void setup() {
         reservationDao = new ReservationDao(jdbcTemplate);
+    }
+
+    @Test
+    void 예약을_저장한다() {
+        //given
+        String name = "user";
+        LocalDate date = LocalDate.now();
+        Long timeId = 1L;
+        Long themeId = 1L;
+
+        //when
+        Long savedId = reservationDao.insert(name, date, timeId, themeId);
+
+        //then
+        ReservationEntity reservationEntity = jdbcTemplate.queryForObject(
+                "SELECT * FROM reservation WHERE id = ? AND is_deleted = FALSE;",
+                reservationRowMapper, savedId);
+
+        ReservationEntity expected = new ReservationEntity(savedId, name, date, timeId, themeId, false);
+
+        Assertions.assertThat(reservationEntity)
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
     }
 
     @Test
