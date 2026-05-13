874bd62dc177cf82020ef8508baa58bea51a3a71
Jiihyun <cwlgusc@gmail.com>
2026-05-14 00:34:18 +0900

    refactor: 테마 관련 에러 http 상태 변경

--- src/main/java/roomescape/exception/code/ThemeErrorCode.java
+++ src/main/java/roomescape/exception/code/ThemeErrorCode.java
@@ -7,7 +7,7 @@ public enum ThemeErrorCode implements ErrorCode {
 
     THEME_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 테마입니다."),
     THEME_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 테마입니다."),
-    THEME_HAS_RESERVATION(HttpStatus.BAD_REQUEST, "해당 테마를 지닌 예약이 존재합니다."),
+    THEME_HAS_RESERVATION(HttpStatus.CONFLICT, "해당 테마를 지닌 예약이 존재합니다."),
     ;
 
     private final HttpStatus httpStatus;
