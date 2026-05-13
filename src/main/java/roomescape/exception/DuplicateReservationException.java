16d86bae2c0313cd6f8688bf8930db14f52fe8d8
sun <sun007021@gmail.com>
2026-05-13 19:18:26 +0900

    style: 중복 예약 메세지 구체화

--- src/main/java/roomescape/exception/DuplicateReservationException.java
+++ src/main/java/roomescape/exception/DuplicateReservationException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public class DuplicateReservationException extends RoomescapeBaseException {
     public DuplicateReservationException() {
-        super(HttpStatus.CONFLICT, "해당 날짜·시간·테마에 이미 예약이 존재합니다.");
+        super(HttpStatus.CONFLICT, "해당 날짜·시간·테마에 이미 예약이 존재합니다. 다른 날짜·시간·테마를 선택해주세요.");
     }
 }
