e675df471bfe76c70c1700da8c9e93d382c3a2c8
Jinriro <riro12.hanyang.ac.kr>
2026-05-13 10:35:09 +0900

    refactor: 400 과 422 분리를 위한 커스텀 예외 생성

---
+++ src/main/java/roomescape/exception/BusinessRuleViolationException.java
@@ -0,0 +1,8 @@
+package roomescape.exception;
+
+public class BusinessRuleViolationException extends RuntimeException {
+
+    public BusinessRuleViolationException(String message) {
+        super(message);
+    }
+}
