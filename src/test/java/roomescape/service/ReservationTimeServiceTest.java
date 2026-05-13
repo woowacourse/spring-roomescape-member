b73e461358017cbb17fe034e05fb372fa58f91be
Huiyeongkim <huiyeong9619@naver.com>
2026-05-13 12:53:09 +0900

    refactor: 예약 시간 test 코드 추가

---
+++ src/test/java/roomescape/service/ReservationTimeServiceTest.java
@@ -0,0 +1,60 @@
+package roomescape.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import roomescape.domain.reservationtime.ReservationTime;
+import roomescape.domain.reservationtime.ReservationTimeRequest;
+import roomescape.domain.reservationtime.ReservationTimeResponse;
+import roomescape.repository.ReservationTimeQueryingDao;
+import roomescape.repository.ReservationTimeUpdatingDao;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class ReservationTimeServiceTest {
+
+    @Mock
+    ReservationTimeUpdatingDao reservationTimeUpdatingDao;
+
+    @Mock
+    ReservationTimeQueryingDao reservationTimeQueryingDao;
+
+    @InjectMocks
+    ReservationTimeService reservationTimeService;
+
+    @Test
+    void 시간_생성_성공() {
+        // when
+        LocalTime time = LocalTime.of(10, 0);
+        ReservationTimeRequest request = new ReservationTimeRequest(time);
+
+        // given
+        ReservationTimeResponse reservationTimeResponse = reservationTimeService.create(request);
+
+        // then
+        Assertions.assertEquals(time, reservationTimeResponse.getStartAt());
+    }
+
+    @Test
+    void 시간_조회_성공() {
+        // when
+        LocalTime time = LocalTime.of(10, 0);
+        ReservationTime reservationTime = new ReservationTime(1L, time);
+
+        // given
+        when(reservationTimeQueryingDao.findAllReservationTime(null, null))
+                .thenReturn(List.of(reservationTime));
+        List<ReservationTimeResponse> reservationTimeResponses = reservationTimeService.read(null, null);
+
+        // then
+        Assertions.assertEquals(time, reservationTimeResponses.getFirst().getStartAt());
+    }
+}
