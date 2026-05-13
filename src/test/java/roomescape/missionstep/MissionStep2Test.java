7ad79b4fa38c71a08bb36752093955e3d2ea0b24
Yeji Kim <yejikim.erica@gmail.com>
2026-05-13 14:36:28 +0900

    test: API 테스트의 역직렬화 대상을 도메인에서 DTO로 변경
    - API 테스트가 실제 컨트롤러의 응답 스펙과 일치하도록 검증 객체를 DTO로 변경함.
    - 불필요한 도메인 의존성을 제거함.

--- src/test/java/roomescape/missionstep/MissionStep2Test.java
+++ src/test/java/roomescape/missionstep/MissionStep2Test.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import roomescape.ClearDbTest;
-import roomescape.domain.Reservation;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import roomescape.service.dto.response.ReservationResponse;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,11 +39,11 @@ public class MissionStep2Test {
         jdbcTemplate.update("INSERT INTO theme (name, description, thumbnail_url) VALUES (?, ?, ?)", "링", "공포 테마", "http:~");
         jdbcTemplate.update("INSERT INTO reservation (name, date, time_id, theme_id) VALUES (?, ?, ?, ?)", "브라운", "2026-08-05", "1", "1");
 
-        List<Reservation> reservations = RestAssured.given().log().all()
+        List<ReservationResponse> reservations = RestAssured.given().log().all()
                 .when().get("/admin/reservations")
                 .then().log().all()
                 .statusCode(200).extract()
-                .jsonPath().getList(".", Reservation.class);
+                .jsonPath().getList(".", ReservationResponse.class);
 
         Integer count = jdbcTemplate.queryForObject("SELECT count(1) from reservation", Integer.class);
 
