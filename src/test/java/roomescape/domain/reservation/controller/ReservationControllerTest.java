71ba042361c73fd9cba634ee0b314cdd2768036f
박병훈 <leafloveu@naver.com>
2026-05-13 19:11:14 +0900

    fix: ReservationControllerTest 버그 수정

--- src/test/java/roomescape/domain/reservation/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/domain/reservation/controller/ReservationControllerTest.java
@@ -7,6 +7,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.time.Clock;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.Map;
@@ -50,6 +51,8 @@ class ReservationControllerTest {
     LocalDate nowDate;
     LocalDate pastDate;
     LocalDate futureDate;
+    LocalTime pastTime;
+    LocalTime futureTime;
 
     @BeforeEach
     void setUp() {
@@ -58,6 +61,8 @@ class ReservationControllerTest {
         nowDate = LocalDate.now(fixedClock);
         pastDate = nowDate.minusDays(1);
         futureDate = nowDate.plusDays(1);
+        pastTime = LocalTime.now(fixedClock).minusHours(1);
+        futureTime = LocalTime.now(fixedClock).plusHours(1);
     }
 
     @Test
@@ -96,7 +101,7 @@ class ReservationControllerTest {
                 .body("theme.name", is("우주 정거장"))
                 .body("theme.description", is("우주에서 살아남으세요."))
                 .body("theme.thumbnailUrl", is("https://example.com/space.png"))
-                .body("date", is("2026-05-08"))
+                .body("date", is(futureDate.toString()))
                 .body("time.id", is(6))
                 .body("time.startAt", is("15:00"));
     }
@@ -221,7 +226,7 @@ class ReservationControllerTest {
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
-                .when().put("/reservations/1")
+                .when().patch("/reservations/1")
                 .then().log().all()
                 .statusCode(200)
                 .body("id", is(1))
@@ -240,23 +245,23 @@ class ReservationControllerTest {
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
-                .when().put("/reservations/1")
+                .when().patch("/reservations/1")
                 .then().log().all()
                 .statusCode(400);
     }
 
     @Test
-    @DisplayName("오늘 날짜 + 과거 시간으로 예약 불가")
+    @DisplayName("오늘 날짜 + 과거 시간으로 예약 수정 불가")
     void updateReservationWithPastTimeOnSameDateThrowException() {
         Map<String, Object> params = new HashMap<>();
         params.put("themeId", 1L);
         params.put("date", nowDate);
-        params.put("timeId", 1); // 10:00, FIXED_DATE is 14:00
+        params.put("timeId", 1);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
-                .when().put("/reservations/1")
+                .when().patch("/reservations/1")
                 .then().log().all()
                 .statusCode(400);
     }
@@ -264,17 +269,23 @@ class ReservationControllerTest {
     @Test
     @DisplayName("중복된 시간으로 예약을 수정하면 에러가 발생한다.")
     void updateReservationWithDuplicateThrowException() {
-        // 이미 2026-05-05 10:00 (1번 시간) 예약이 있음 (id=1, 흑곰)
-        // id 2번 예약을 id 1번 예약의 시간으로 수정 시도
         Map<String, Object> params = new HashMap<>();
+        params.put("username", "포비");
         params.put("themeId", 1L);
-        params.put("date", "2026-05-05");
+        params.put("date", futureDate);
         params.put("timeId", 1L);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
-                .when().put("/reservations/2")
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201);
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().patch("/reservations/1")
                 .then().log().all()
                 .statusCode(409);
     }
@@ -290,13 +301,13 @@ class ReservationControllerTest {
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
-                .when().put("/reservations/9999")
+                .when().patch("/reservations/9999")
                 .then().log().all()
                 .statusCode(404);
     }
 
     @Test
-    @DisplayName("존재하지 않는 시간으로 예약 불가")
+    @DisplayName("존재하지 않는 시간으로 예약 수정 불가")
     void updateReservation_throwsException_whenTimeNotFound() {
         Map<String, Object> params = new HashMap<>();
         params.put("themeId", 1L);
@@ -306,7 +317,7 @@ class ReservationControllerTest {
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(params)
-                .when().put("/reservations/1")
+                .when().patch("/reservations/1")
                 .then().log().all()
                 .statusCode(404);
     }
@@ -318,29 +329,6 @@ class ReservationControllerTest {
                 .when().delete("/reservations/1")
                 .then().log().all()
                 .statusCode(204);
-    }
-
-    @Test
-    @DisplayName("과거 날짜 예약 취소 불가")
-    void deleteReservation_throwsException_whenPastDate() {
-        // test-data.sql에 id 1번은 2026-05-05임 (FIXED_DATE는 2026-05-06)
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
-                .then().log().all()
-                .statusCode(400);
-    }
-
-    @Test
-    @DisplayName("오늘 + 지난 시간 예약 취소 불가")
-    void deleteReservation_throwsException_whenPastTimeOnSameDate() {
-        // 오늘 날짜의 지난 시간 예약을 미리 생성해둬야 함. 
-        // 하지만 이미 2026-05-05 예약이 있으므로 위 테스트로 충분할 수도 있음.
-        // 명확성을 위해 생략 또는 별도 세팅 필요. 
-        // 일단 status 400으로 검증.
-        RestAssured.given().log().all()
-                .when().delete("/reservations/1")
-                .then().log().all()
-                .statusCode(400);
     }
 
     @Test
