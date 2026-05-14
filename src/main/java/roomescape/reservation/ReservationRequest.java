f3b7220a7561a414863393e88c5586bb834958ee
구민서 <minseo12379@gmail.com>
2026-05-14 21:39:38 +0900

    feat(ReservationRequest): 예약 요청 입력값 유효성 검사 제약 사항 추가 및 메시지 개선

--- src/main/java/roomescape/reservation/ReservationRequest.java
+++ src/main/java/roomescape/reservation/ReservationRequest.java
@@ -3,17 +3,23 @@ package roomescape.reservation;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import jakarta.validation.constraints.Positive;
 import java.time.LocalDate;
 
 
 public record ReservationRequest(
         @NotBlank(message = "이름은 비어있을 수 없습니다.")
         String name,
-        @NotNull(message = "날짜를 입력해주세요.")
+
+        @NotNull(message = "날짜는 필수 입력 항목입니다.")
         LocalDate date,
-        @NotNull(message = "예약 시간 정보가 필요합니다.")
+
+        @NotNull(message = "시간 ID는 필수 입력 항목입니다.")
+        @Positive(message = "시간 ID는 양수여야 합니다.")
         Long timeId,
-        @NotNull(message = "테마 정보가 필요합니다.")
+
+        @NotNull(message = "테마 ID는 필수 입력 항목입니다.")
+        @Positive(message = "테마 ID는 양수여야 합니다.")
         Long themeId
 ) {
 }
