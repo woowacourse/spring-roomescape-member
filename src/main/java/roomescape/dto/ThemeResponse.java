557dc5831a97f4ef8e247744b82e9a5615608c0d
mvg01 <dkms625@naver.com>
2026-05-14 16:07:41 +0900

    refactor: get 메서드네이밍 수정

--- src/main/java/roomescape/dto/ThemeResponse.java
+++ src/main/java/roomescape/dto/ThemeResponse.java
@@ -5,7 +5,7 @@ import roomescape.model.Theme;
 public record ThemeResponse(Long id, String name, String description, String url) {
 
     public static ThemeResponse from(Theme theme) {
-        return new ThemeResponse(theme.getId(), theme.getName(), theme.getDescription(), theme.getUrl());
+        return new ThemeResponse(theme.id(), theme.name(), theme.description(), theme.url());
     }
 
 }
