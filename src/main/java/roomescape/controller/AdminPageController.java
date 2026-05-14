62b9c1e314b6bec7534e000d2b3d71e067202040
Yeji Kim <yejikim.erica@gmail.com>
2026-05-14 18:06:48 +0900

    refactor: PageController의 RequestMapping 양식 통일
    - RequestMapping을 "/page"로 통일함.

--- src/main/java/roomescape/controller/AdminPageController.java
+++ src/main/java/roomescape/controller/AdminPageController.java
@@ -2,12 +2,14 @@ package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 관리자 화면 서빙 컨트롤러.
  * /admin GET 요청을 정적 HTML 파일로 포워딩합니다.
  */
 @Controller
+@RequestMapping("/page")
 public class AdminPageController {
 
     @GetMapping("/admin")
