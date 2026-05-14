f6720be0a15380688e73ff81baa39dec05650e71
quddaz <quddnddl35@naver.com>
2026-05-14 15:28:18 +0900

    refactor : 페이지 수정

---
+++ src/main/java/roomescape/global/controller/HomePageController.java
@@ -0,0 +1,14 @@
+package roomescape.global.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping("/")
+    public String read() {
+        return "index";
+    }
+
+}
