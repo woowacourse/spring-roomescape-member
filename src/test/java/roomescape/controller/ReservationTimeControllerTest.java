f524099f9205fc760ad6a23f3936616f9f781d4b
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-05-14 16:09:11 +0900

    test: 전체 시간 조회 테스트 추가

---
+++ src/test/java/roomescape/controller/ReservationTimeControllerTest.java
@@ -0,0 +1,21 @@
+package roomescape.controller;
+
+import static org.hamcrest.Matchers.is;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ReservationTimeControllerTest {
+    @Test
+    void 전체_시간_조회_API() {
+        RestAssured.given().log().all()
+                .when().get("/times")
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(9)); // data.sql에 9개 삽입
+    }
+}
