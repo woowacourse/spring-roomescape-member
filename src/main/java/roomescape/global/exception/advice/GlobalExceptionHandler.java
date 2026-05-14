5e44f2f7a3cdcb778e5ada9de47493144f543c03
woohyeon <woohyeon6885@gmail.com>
2026-05-14 15:52:28 +0900

    refactor(globalExceptionHandler): request 파라미터 누락 예외 핸들러 추가

--- src/main/java/roomescape/global/exception/advice/GlobalExceptionHandler.java
+++ src/main/java/roomescape/global/exception/advice/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package roomescape.global.exception.advice;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomescape.global.exception.base.BusinessException;
@@ -8,11 +9,20 @@ import roomescape.global.exception.base.BusinessException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneralException(Exception e) {
         return ResponseEntity
                 .internalServerError()
                 .body(new ErrorResponse("서버 내부 예외가 발생했습니다."));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<?> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException e
+    ) {
+        return ResponseEntity
+                .badRequest()
+                .body("필수 요청 파라미터가 누락되었습니다.");
     }
 
     @ExceptionHandler(BusinessException.class)
