e413cdcdda36b4457b565dd2bb0e8b199938a1d5
yItWorks <poiu7825@pusan.ac.kr>
2026-05-14 13:41:23 +0900

    test: 사용자 이름으로 예약 조회 테스트 추가

--- src/test/java/roomescape/apitest/users/ReservationApiTest.java
+++ src/test/java/roomescape/apitest/users/ReservationApiTest.java
@@ -15,14 +15,17 @@ import org.springframework.test.annotation.DirtiesContext;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationApiTest {
+    private final String userName = "브라운";
+    private final Long timeId = 1L;
+    private final Long themeId = 1L;
 
     @Test
     void 예약_사용자_API() {
         Map<String, Object> reservation = new HashMap<>();
-        reservation.put("name", "브라운");
+        reservation.put("name", userName);
         reservation.put("date", FUTURE_DATE);
-        reservation.put("timeId", 1);
-        reservation.put("themeId", 1);
+        reservation.put("timeId", timeId);
+        reservation.put("themeId", themeId);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -36,6 +39,12 @@ class ReservationApiTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("size()", is(25));
+
+        RestAssured.given().log().all()
+                .when().get("/reservations?userName=" + userName)
+                .then().log().all()
+                .statusCode(200)
+                .body("size()", is(25));
     }
 
     @Test
@@ -43,8 +52,8 @@ class ReservationApiTest {
     void 요청_이름_null_테스트() {
         Map<String, Object> params = new HashMap<>();
         params.put("date", FUTURE_DATE);
-        params.put("timeId", 1);
-        params.put("themeId", 1);
+        params.put("timeId", timeId);
+        params.put("themeId", themeId);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -58,9 +67,9 @@ class ReservationApiTest {
     @DisplayName("예약 날짜가 null이면 상태코드 400을 반환한다.")
     void 요청_날짜_null_테스트() {
         Map<String, Object> params = new HashMap<>();
-        params.put("name", "브라운");
-        params.put("timeId", 1);
-        params.put("themeId", 1);
+        params.put("name", userName);
+        params.put("timeId", timeId);
+        params.put("themeId", themeId);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -74,10 +83,10 @@ class ReservationApiTest {
     @DisplayName("예약 날짜의 형식이 올바르지 않으면 상태코드 400을 반환한다.")
     void 요청_날짜_형식_불일치_테스트() {
         Map<String, Object> params = new HashMap<>();
-        params.put("name", "브라운");
+        params.put("name", userName);
         params.put("date", "26-01-01");
-        params.put("timeId", 1);
-        params.put("themeId", 1);
+        params.put("timeId", timeId);
+        params.put("themeId", themeId);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -91,9 +100,9 @@ class ReservationApiTest {
     @DisplayName("시간 식별자가 null이면 상태코드 400을 반환한다.")
     void 요청_시간_식별자_null_테스트() {
         Map<String, Object> params = new HashMap<>();
-        params.put("name", "브라운");
+        params.put("name", userName);
         params.put("date", FUTURE_DATE);
-        params.put("themeId", 1);
+        params.put("themeId", themeId);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
@@ -107,9 +116,9 @@ class ReservationApiTest {
     @DisplayName("테마 식별자가 null이면 상태코드 400을 반환한다.")
     void 요청_테마_식별자_null_테스트() {
         Map<String, Object> params = new HashMap<>();
-        params.put("name", "브라운");
+        params.put("name", userName);
         params.put("date", FUTURE_DATE);
-        params.put("timeId", 1);
+        params.put("timeId", timeId);
 
         RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
