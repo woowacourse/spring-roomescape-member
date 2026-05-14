f691c77715fff3ba727de58f3bf758fcc43437da
심호경 <ghrudtla@naver.com>
2026-05-14 14:44:55 +0900

    refactor: 시간 기준을 명시적인 Clock으로 관리

--- src/main/java/roomescape/config/TimeConfig.java
+++ src/main/java/roomescape/config/TimeConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
+import java.time.ZoneId;
 
 @Configuration
 public class TimeConfig {
 
     @Bean
     public Clock clock() {
-        return Clock.systemDefaultZone();
+        return Clock.system(ZoneId.of("Asia/Seoul"));
     }
 }
