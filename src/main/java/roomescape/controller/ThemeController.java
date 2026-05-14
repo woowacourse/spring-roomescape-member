1b98ce62fe5c1a46b4f3c7c4a91a9ca3915a35ab
심호경 <ghrudtla@naver.com>
2026-05-14 15:03:04 +0900

    refactor: wildcard import 제거

--- src/main/java/roomescape/controller/ThemeController.java
+++ src/main/java/roomescape/controller/ThemeController.java
@@ -4,7 +4,14 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.Theme;
 import roomescape.dto.PopularThemeResponse;
 import roomescape.dto.ReservationTimeStatusResponse;
