13308d719681e89f9a9b61e1534028b252609c99
mvg01 <dkms625@naver.com>
2026-05-14 19:31:55 +0900

    test: Clock 시간 고정으로 시간 의존 테스트 안정화

--- src/test/java/roomescape/service/ReservationServiceTest.java
+++ src/test/java/roomescape/service/ReservationServiceTest.java
@@ -1,10 +1,15 @@
 package roomescape.service;
 
+import java.time.Clock;
 import java.time.LocalDate;
+import java.time.ZoneOffset;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.annotation.Transactional;
 import roomescape.dto.ReservationRequest;
 import roomescape.exception.RoomescapeException;
@@ -15,6 +20,9 @@ public class ReservationServiceTest {
 
     @Autowired
     private ReservationService reservationService;
+
+    @Autowired
+    private Clock clock;
 
     @Test
     void 존재하지_않는_예약을_삭제할_경우_예외가_발생한다() {
@@ -30,7 +38,7 @@ public class ReservationServiceTest {
 
     @Test
     void 이름이_빈_예약을_추가할_경우_예외가_발생한다() {
-        LocalDate date = LocalDate.now().plusDays(1);
+        LocalDate date = LocalDate.now(clock).plusDays(1);
         ReservationRequest reservationRequest = new ReservationRequest(null, date, 4L, 6L);
 
         Assertions.assertThatThrownBy(() -> reservationService.register(reservationRequest))
@@ -47,7 +55,7 @@ public class ReservationServiceTest {
 
     @Test
     void 시간_아이디가_빈_예약을_추가할_경우_예외가_발생한다() {
-        LocalDate date = LocalDate.now().plusDays(1);
+        LocalDate date = LocalDate.now(clock).plusDays(1);
         ReservationRequest reservationRequest = new ReservationRequest("포비", date, null, 6L);
 
         Assertions.assertThatThrownBy(() -> reservationService.register(reservationRequest))
@@ -56,7 +64,7 @@ public class ReservationServiceTest {
 
     @Test
     void 테마_아이디가_빈_예약을_추가할_경우_예외가_발생한다() {
-        LocalDate date = LocalDate.now().plusDays(1);
+        LocalDate date = LocalDate.now(clock).plusDays(1);
         ReservationRequest reservationRequest = new ReservationRequest("포비", date, 4L, null);
 
         Assertions.assertThatThrownBy(() -> reservationService.register(reservationRequest))
@@ -65,7 +73,7 @@ public class ReservationServiceTest {
 
     @Test
     void 존재하는_예약을_추가할_경우_예외가_발생한다() {
-        LocalDate date = LocalDate.now().plusDays(3);
+        LocalDate date = LocalDate.now(clock).plusDays(3);
         ReservationRequest reservationRequest = new ReservationRequest("구바", date, 8L, 2L);
 
         Assertions.assertThatThrownBy(() -> reservationService.register(reservationRequest))
@@ -74,7 +82,7 @@ public class ReservationServiceTest {
 
     @Test
     void 존재하지_않는_예약을_정상적으로_추가할_수_있다() {
-        ReservationRequest reservationRequest = new ReservationRequest("무빙", LocalDate.now().plusDays(2), 2L, 2L);
+        ReservationRequest reservationRequest = new ReservationRequest("무빙", LocalDate.now(clock).plusDays(2), 2L, 2L);
 
         Assertions.assertThatCode(() -> reservationService.register(reservationRequest))
                 .doesNotThrowAnyException();
@@ -82,7 +90,7 @@ public class ReservationServiceTest {
 
     @Test
     void 이미_지난_날짜의_예약은_불가능하다() {
-        ReservationRequest reservationRequest = new ReservationRequest("무빙", LocalDate.now().minusDays(1), 2L, 2L);
+        ReservationRequest reservationRequest = new ReservationRequest("무빙", LocalDate.now(clock).minusDays(1), 2L, 2L);
 
         Assertions.assertThatThrownBy(() -> reservationService.register(reservationRequest))
                 .isInstanceOf(RoomescapeException.class);
@@ -90,9 +98,21 @@ public class ReservationServiceTest {
 
     @Test
     void 오늘_예약이고_이미_지난_시간의_예약은_불가능하다() {
-        ReservationRequest reservationRequest = new ReservationRequest("무빙", LocalDate.now(), 1L, 2L);
+        ReservationRequest reservationRequest = new ReservationRequest("무빙", LocalDate.now(clock), 1L, 2L);
 
         Assertions.assertThatThrownBy(() -> reservationService.register(reservationRequest))
                 .isInstanceOf(RoomescapeException.class);
+    }
+    
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        @Primary
+        public Clock clock() {
+            return Clock.fixed(
+                    LocalDate.now().atTime(14, 0).toInstant(ZoneOffset.UTC),
+                    ZoneOffset.UTC
+            );
+        }
     }
 }
