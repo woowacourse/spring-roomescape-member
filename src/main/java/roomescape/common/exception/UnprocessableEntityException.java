fedee30444ca8f55b0faa7845a5e5b2f448bfea8
yItWorks <poiu7825@pusan.ac.kr>
2026-05-14 16:54:52 +0900

    feat: 커스텀 에러 추가

---
+++ src/main/java/roomescape/common/exception/UnprocessableEntityException.java
@@ -0,0 +1,7 @@
+package roomescape.common.exception;
+
+public class UnprocessableEntityException extends RuntimeException {
+    public UnprocessableEntityException(String message) {
+        super(message);
+    }
+}
