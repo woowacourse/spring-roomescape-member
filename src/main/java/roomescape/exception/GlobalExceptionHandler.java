98b923c9ef6796aad697724182b8eb4743f78d1c
Jeshin Kim <jjason0904@cau.ac.kr>
2026-05-15 17:52:07 +0900

    refactor(GlobalExceptionHandler): ResponseEntityExceptionHandler 상속으로 표준 예외 커버하도록 변경

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,105 +1,89 @@
 package roomescape.exception;
 
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(MissingRequestHeaderException.class)
-    public ProblemDetail handleMissingRequestHeaderException(MissingRequestHeaderException e) {
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
-                "필수 헤더가 누락되었습니다: " + e.getHeaderName());
-        pd.setProperty("code", "MISSING_HEADER");
-        return pd;
-    }
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ProblemDetail handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
-                "잘못된 형식의 값입니다: " + e.getName());
-        pd.setProperty("code", "INVALID_PARAMETER_TYPE");
-        return pd;
-    }
+    private static final Map<Class<?>, String> STANDARD_CODES = Map.of(
+            MissingRequestHeaderException.class, "MISSING_HEADER",
+            MethodArgumentTypeMismatchException.class, "INVALID_PARAMETER_TYPE",
+            HttpMessageNotReadableException.class, "INVALID_REQUEST_BODY",
+            MethodArgumentNotValidException.class, "VALIDATION_FAILED"
+    );
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ProblemDetail handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
-                "요청 본문을 읽을 수 없습니다. 입력 형식을 확인해주세요.");
-        pd.setProperty("code", "INVALID_REQUEST_BODY");
-        return pd;
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ProblemDetail handleValidationException(MethodArgumentNotValidException e) {
-        String message = e.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .findFirst()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .orElse("유효하지 않은 요청입니다.");
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, message);
-        pd.setProperty("code", "VALIDATION_FAILED");
-        return pd;
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        if (body instanceof ProblemDetail pd) {
+            String code = STANDARD_CODES.getOrDefault(ex.getClass(), "INTERNAL_ERROR");
+            pd.setProperty("code", code);
+        }
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ProblemDetail handleNotFoundException(NotFoundException e) {
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+        ProblemDetail pd = ProblemDetail.forStatusAndDetail(org.springframework.http.HttpStatus.NOT_FOUND, e.getMessage());
         pd.setProperty("code", e.getCode());
         return pd;
     }
 
     @ExceptionHandler(EntityInUseException.class)
     public ProblemDetail handleEntityInUseException(EntityInUseException e) {
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
+        ProblemDetail pd = ProblemDetail.forStatusAndDetail(org.springframework.http.HttpStatus.CONFLICT, e.getMessage());
         pd.setProperty("code", e.getCode());
         return pd;
     }
 
     @ExceptionHandler(OwnershipViolationException.class)
     public ProblemDetail handleOwnershipViolationException(OwnershipViolationException e) {
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, e.getMessage());
+        ProblemDetail pd = ProblemDetail.forStatusAndDetail(org.springframework.http.HttpStatus.FORBIDDEN, e.getMessage());
         pd.setProperty("code", e.getCode());
         return pd;
     }
 
     @ExceptionHandler(DuplicateException.class)
     public ProblemDetail handleDuplicateException(DuplicateException e) {
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
+        ProblemDetail pd = ProblemDetail.forStatusAndDetail(org.springframework.http.HttpStatus.CONFLICT, e.getMessage());
         pd.setProperty("code", e.getCode());
         return pd;
     }
 
     @ExceptionHandler(InvalidInputException.class)
     public ProblemDetail handleInvalidInputException(InvalidInputException e) {
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        ProblemDetail pd = ProblemDetail.forStatusAndDetail(org.springframework.http.HttpStatus.BAD_REQUEST, e.getMessage());
         pd.setProperty("code", e.getCode());
         return pd;
     }
 
     @ExceptionHandler(BusinessRuleException.class)
     public ProblemDetail handleBusinessRuleException(BusinessRuleException e) {
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+        ProblemDetail pd = ProblemDetail.forStatusAndDetail(org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
         pd.setProperty("code", e.getCode());
         return pd;
     }
 
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleUnhandledException(Exception e) {
-        logger.error("서버 내부 오류 발생", e);
-        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
+        log.error("서버 내부 오류 발생", e);
+        return ProblemDetail.forStatusAndDetail(org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR,
                 "서버 내부 오류가 발생했습니다. 관리자에게 문의해주세요.");
-        return pd;
     }
 }
