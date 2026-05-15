840e2ab2564559fbee11c970f042e26a0c9e007a
choiyoung69 <choiyoung609@gmail.com>
2026-05-16 08:57:04 +0900

    chore: import 문 추가

--- src/main/java/roomescape/reservation/presentation/controller/ReservationController.java
+++ src/main/java/roomescape/reservation/presentation/controller/ReservationController.java
@@ -8,15 +8,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.reservation.application.dto.ReservationCreateCommand;
+import roomescape.reservation.application.dto.ReservationUpdateCommand;
 import roomescape.reservation.application.service.ReservationService;
 import roomescape.reservation.presentation.dto.ReservationCreateRequest;
 import roomescape.reservation.presentation.dto.ReservationResponse;
+import roomescape.reservation.presentation.dto.ReservationUpdateRequest;
 
 @RequiredArgsConstructor
 @RequestMapping("/reservations")
