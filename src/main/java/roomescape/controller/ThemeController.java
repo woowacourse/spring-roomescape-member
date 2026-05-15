30a057993962468e241e2d01d38fc2a629cbfc8e
dongheeHan <ark182818@gmail.com>
2026-05-15 17:56:36 +0900

    refactor: 사용자 컨트롤러에서 관리자 기능(POST/DELETE) 제거

--- src/main/java/roomescape/controller/ThemeController.java
+++ src/main/java/roomescape/controller/ThemeController.java
@@ -2,16 +2,12 @@ package roomescape.controller;
 
 import java.util.List;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import roomescape.controller.dto.ThemeCreateRequest;
 import roomescape.controller.dto.ThemeFamousFindRequest;
 import roomescape.controller.dto.ThemeResponse;
 import roomescape.service.ThemeService;
@@ -26,18 +22,6 @@ public class ThemeController {
         this.themeService = themeService;
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ThemeResponse create(@RequestBody ThemeCreateRequest request) {
-        return ThemeResponse.toDto(themeService.create(request));
-    }
-
-    @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public ThemeResponse find(@PathVariable Long id) {
-        return ThemeResponse.toDto(themeService.find(id));
-    }
-
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ThemeResponse> findAll() {
@@ -46,17 +30,17 @@ public class ThemeController {
                 .toList();
     }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ThemeResponse find(@PathVariable Long id) {
+        return ThemeResponse.toDto(themeService.find(id));
+    }
+
     @GetMapping("/famous")
     @ResponseStatus(HttpStatus.OK)
     public List<ThemeResponse> findFamous(@ModelAttribute ThemeFamousFindRequest request) {
         return themeService.findFamous(request).stream()
                 .map(ThemeResponse::toDto)
                 .toList();
-    }
-
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable long id) {
-        themeService.delete(id);
     }
 }
