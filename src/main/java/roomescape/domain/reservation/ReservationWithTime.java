085fbd6202a1c906fbcad880061194719213fca2
armd482 <armd482@naver.com>
2026-05-15 13:21:26 +0900

    refactor: reservationWithTime객체 생성

--- src/main/java/roomescape/domain/reservation/ReservationWithTime.java
+++ src/main/java/roomescape/domain/reservation/ReservationWithTime.java
@@ -3,18 +3,38 @@ package roomescape.domain.reservation;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import roomescape.domain.reservationTime.ReservationTime;
-import roomescape.domain.theme.Theme;
 import roomescape.exception.InvalidRequestValueException;
 
-public record ReservationWithTime(String name, LocalDate date, ReservationTime time, Theme reservationTheme) {
+public record ReservationWithTime(long id, String name, LocalDate date, ReservationTime reservationTime, long themeId) {
+    private static final String CANNOT_UPDATE_SAME_VALUE = "기존 정보와 동일하여 수정할 내용이 없습니다.";
     private static final String CANNOT_UPDATE_PAST_RESERVATION = "이미 지난 예약은 수정할 수 없습니다.";
+
+    public void validateUpdateValue(ReservationCommand reservationCommand) {
+        validDateReservationDateTime();
+        validateEqualValue(reservationCommand);
+    }
+
+    private void validateEqualValue(ReservationCommand reservationCommand) {
+        if(isEqualValue(reservationCommand)) {
+            throw new InvalidRequestValueException(CANNOT_UPDATE_SAME_VALUE);
+        }
+    }
 
     public void validDateReservationDateTime() {
         LocalDateTime now = LocalDateTime.now();
-        LocalDateTime reservationTime = LocalDateTime.of(date, time.startAt());
+        LocalDateTime reservationTime = LocalDateTime.of(date, reservationTime().startAt());
 
         if(reservationTime.isBefore(now)) {
             throw new InvalidRequestValueException(CANNOT_UPDATE_PAST_RESERVATION);
         }
+    }
+
+    private boolean isEqualValue(ReservationCommand reservationCommand) {
+        return (
+                reservationCommand.name().equals(name) &&
+                        reservationCommand.date().equals(date) &&
+                        reservationCommand.timeId() == reservationTime().id() &&
+                        reservationCommand.themeId() == themeId
+        );
     }
 }
