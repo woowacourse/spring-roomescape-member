2374f828677d4a636441dd918de9e304f57e54f3
SooHyeon Lee <dalsu4594@naver.com>
2026-05-15 15:21:56 +0900

    test: Time 단위 테스트 추가

--- src/test/java/roomescape/domain/TimeTest.java
+++ src/test/java/roomescape/domain/TimeTest.java
@@ -1,5 +1,26 @@
 package roomescape.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.time.LocalTime;
+
 class TimeTest {
 
+    @ParameterizedTest
+    @CsvSource(value = {"09:59", "22:01"})
+    void 오전_10시_오후_10시가_아니면_예외_처리한다(LocalTime value) {
+        Assertions.assertThatThrownBy(() -> Time.create(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"10:00", "22:00"})
+    void 오전_10시_오후_10시면_정상_생성된다 (LocalTime value) {
+        Time time = Time.create(value);
+
+        Assertions.assertThat(time.getStartAt()).isEqualTo(value);
+    }
 }
