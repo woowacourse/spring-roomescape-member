90c2f093dd5cc8aacc5c270f5de0458d97ae1424
심호경 <ghrudtla@naver.com>
2026-05-15 11:05:55 +0900

    refactor: 파라미터 검증 추가

--- src/main/java/roomescape/controller/GlobalExceptionHandler.java
+++ src/main/java/roomescape/controller/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package roomescape.controller;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,5 +26,27 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EmptyResultDataAccessException.class)
     public ResponseEntity<String> handleEmptyResultDataAccessException(EmptyResultDataAccessException e) {
         return ResponseEntity.badRequest().body("존재하지 않는 데이터입니다.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .orElse("입력값이 올바르지 않습니다.");
+
+        return ResponseEntity.badRequest().body(message);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+        String message = e.getConstraintViolations()
+                .stream()
+                .findFirst()
+                .map(ConstraintViolation::getMessage)
+                .orElse("요청 파라미터가 올바르지 않습니다.");
+        return ResponseEntity.badRequest().body(message);
     }
 }
--- src/main/java/roomescape/dto/ReservationTimeRequest.java
+++ src/main/java/roomescape/dto/ReservationTimeRequest.java
@@ -8,8 +8,8 @@ import jakarta.validation.constraints.NotNull;
 import roomescape.domain.ReservationTime;
 
 public record ReservationTimeRequest(
-        @JsonFormat(pattern = "HH:mm")
         @NotNull(message = "예약 시간은 필수입니다.")
+        @JsonFormat(pattern = "HH:mm")
         LocalTime startAt
 ) {
     public ReservationTime toEntity() {
