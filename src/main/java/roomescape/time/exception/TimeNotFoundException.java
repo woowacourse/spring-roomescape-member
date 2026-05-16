117ed0b8e0364d4bf50ac01fa6095c589a3b7193
dahye <dahye9011@naver.com>
2026-05-16 17:00:57 +0900

    fix: 예외 메시지 수정

--- src/main/java/roomescape/time/exception/TimeNotFoundException.java
+++ src/main/java/roomescape/time/exception/TimeNotFoundException.java
@@ -4,7 +4,7 @@ public class TimeNotFoundException extends RuntimeException {
     private final Long id;
 
     public TimeNotFoundException(Long id) {
-        super("해당 시간에 예약이 존재하여 삭제할 수 없습니다. id=" + id);
+        super("예약 시간이 존재하지 않습니다. id=" + id);
         this.id = id;
     }
 
