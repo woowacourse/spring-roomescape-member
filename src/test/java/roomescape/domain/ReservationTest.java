8e6656425a8604688e31bd9d295366b2deb842b2
YEJIN KIM <glow.yejin@gmail.com>
2026-05-16 16:12:07 +0900

    test: Reservation 테스트 추가

---
+++ src/test/java/roomescape/domain/ReservationTest.java
@@ -0,0 +1,77 @@
+package roomescape.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import org.junit.jupiter.api.Test;
+import roomescape.exception.ReservationErrorCode;
+import roomescape.exception.RoomEscapeException;
+
+class ReservationTest {
+
+    @Test
+    void 이름이_비어있으면_예외가_발생한다() {
+        ReservationTime time = ReservationTime.create(LocalTime.parse("10:00"));
+        Theme theme = Theme.create("귀신찾기", "귀신을 찾는다", "https://image.png");
+
+        assertThatThrownBy(() -> Reservation.create("", LocalDate.parse("2026-08-05"), time, theme))
+                .isInstanceOf(RoomEscapeException.class)
+                .extracting("errorCode")
+                .isEqualTo(ReservationErrorCode.INVALID_NAME);
+    }
+
+    @Test
+    void 날짜가_null이면_예외가_발생한다() {
+        ReservationTime time = ReservationTime.create(LocalTime.parse("10:00"));
+        Theme theme = Theme.create("귀신찾기", "귀신을 찾는다", "https://image.png");
+
+        assertThatThrownBy(() -> Reservation.create("네오", null, time, theme))
+                .isInstanceOf(RoomEscapeException.class)
+                .extracting("errorCode")
+                .isEqualTo(ReservationErrorCode.INVALID_DATE);
+    }
+
+    @Test
+    void 시간이_null이면_예외가_발생한다() {
+        Theme theme = Theme.create("귀신찾기", "귀신을 찾는다", "https://image.png");
+
+        assertThatThrownBy(
+                () -> Reservation.create("네오", LocalDate.parse("2026-08-05"), null, theme))
+                .isInstanceOf(RoomEscapeException.class)
+                .extracting("errorCode")
+                .isEqualTo(ReservationErrorCode.INVALID_TIME);
+    }
+
+    @Test
+    void 테마가_null이면_예외가_발생한다() {
+        ReservationTime time = ReservationTime.create(LocalTime.parse("10:00"));
+
+        assertThatThrownBy(
+                () -> Reservation.create("네오", LocalDate.parse("2026-08-05"), time, null))
+                .isInstanceOf(RoomEscapeException.class)
+                .extracting("errorCode")
+                .isEqualTo(ReservationErrorCode.INVALID_THEME);
+    }
+
+
+    @Test
+    void 과거_예약시간이면_예외가_발생한다() {
+        ReservationTime time = ReservationTime.create(LocalTime.parse("10:00"));
+        Theme theme = Theme.create("귀신찾기", "귀신을 찾는다", "https://image.png");
+        Reservation reservation = Reservation.create(
+                "브라운",
+                LocalDate.parse("2026-05-06"),
+                time,
+                theme
+        );
+
+        assertThatThrownBy(() -> reservation.validateNotPastTime(
+                LocalDateTime.parse("2026-05-07T00:00:00")
+        ))
+                .isInstanceOf(RoomEscapeException.class)
+                .extracting("errorCode")
+                .isEqualTo(ReservationErrorCode.RESERVATION_PAST_TIME);
+    }
+}
