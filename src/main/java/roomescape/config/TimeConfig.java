5b33913fe7252da7ccd8fbe1817dc70401ab6804
YEJIN KIM <glow.yejin@gmail.com>
2026-05-16 16:52:34 +0900

    refactor: config 패키지 생성

--- src/main/java/roomescape/config/TimeConfig.java
+++ src/main/java/roomescape/config/TimeConfig.java
@@ -1,6 +1,7 @@
-package roomescape;
+package roomescape.config;
 
 import java.time.Clock;
+import java.time.ZoneId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,6 @@ public class TimeConfig {
 
     @Bean
     public Clock clock() {
-        return Clock.systemUTC();
+        return Clock.system(ZoneId.of("Asia/Seoul"));
     }
 }
