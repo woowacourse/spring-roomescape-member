eb6beb84d421dba2bbcb0f0f25e0391c33ac8774
sinequanonn <dleck288@gmail.com>
2026-05-17 03:55:00 +0900

    remove: 읽기 전용 커스텀 예외 삭제

--- src/main/java/roomescape/global/exception/ReadOnlyViolationException.java
+++
@@ -1,7 +0,0 @@
-package roomescape.global.exception;
-
-public class ReadOnlyViolationException extends RuntimeException {
-    public ReadOnlyViolationException(String message) {
-        super(message);
-    }
-}
