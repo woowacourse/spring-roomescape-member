b0b7fc3b1c8cb0ea4280f0f03d3fbf28d072c469
quddaz <quddnddl35@naver.com>
2026-05-16 15:50:21 +0900

    feat : swagger 추가

--- src/main/java/roomescape/reservation/controller/ReservationAdminController.java
+++ src/main/java/roomescape/reservation/controller/ReservationAdminController.java
@@ -1,5 +1,7 @@
 package roomescape.reservation.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import roomescape.reservation.controller.dto.ReservationResponse;
 import roomescape.reservation.service.ReservationService;
 
+@Tag(name = "admin-reservation", description = "예약 관리자 API")
 @RestController
 @RequestMapping("/admin/reservations")
 @RequiredArgsConstructor
@@ -20,6 +23,7 @@ public class ReservationAdminController {
     private final ReservationService reservationService;
 
     @GetMapping
+    @Operation(summary = "전체 예약 조회", description = "전체 예약 정보를 가져옵니다.")
     public List<ReservationResponse> read() {
         return reservationService.getAll().stream()
                 .map(ReservationResponse::from)
@@ -28,6 +32,7 @@ public class ReservationAdminController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "예약 삭제", description = "예약을 삭제합니다.")
     public void delete(@PathVariable Long id) {
         reservationService.deleteById(id);
     }
