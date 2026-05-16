5127a090800c939ab837fc1781c46ddfcb2f1871
armd482 <armd482@naver.com>
2026-05-16 12:44:28 +0900

    refactor: 에러 handler 종류 및 log 추가

--- src/main/java/roomescape/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/handler/GlobalExceptionHandler.java
@@ -2,9 +2,12 @@ package roomescape.exception.handler;
 
 import static roomescape.exception.HttpStatusMapper.STATUS_MAP;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,6 +16,8 @@ import roomescape.exception.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler({CustomException.class})
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException customException) {
         HttpStatus httpStatus = STATUS_MAP.get(customException.getClass());
@@ -25,21 +30,27 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException exception) {
-        String errorMessage = exception.getBindingResult().getAllErrors().getFirst().getDefaultMessage();
-
-        return getResponse(HttpStatus.BAD_REQUEST, errorMessage);
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException 발생: ", e);
+        return getResponse(HttpStatus.BAD_REQUEST, "올바르지 않은 요청 파라미터 형식입니다.");
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<ErrorResponse> handleDataIntegrityViolation(DataIntegrityViolationException e) {
-        return getResponse(HttpStatus.BAD_REQUEST, "이미 존재하는 데이터이거나 제약 조건을 위반했습니다.");
+    public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        log.error("DataIntegrityViolationException 발생: ", e);
+        return getResponse(HttpStatus.BAD_REQUEST, "요청하신 처리를 완료할 수 없습니다. 데이터 제약 조건을 확인해 주세요.");
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.error("HttpMessageNotReadableException 발생: ", e);
+        return getResponse(HttpStatus.BAD_REQUEST, "요청하신 JSON 데이터의 포맷이 잘못되었습니다.");
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> handleRuntimeException() {
-        return getResponse(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러");
-    }
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        log.error("RuntimeException 발생: ", e);
+        return getResponse(HttpStatus.INTERNAL_SERVER_ERROR, "시스템 내부 오류가 발생했습니다. 관리자에게 문의하세요.");    }
 
     private ResponseEntity<ErrorResponse> getResponse(HttpStatus httpStatus, String message) {
         return new ResponseEntity<>(new ErrorResponse(httpStatus.value(), message), httpStatus);
