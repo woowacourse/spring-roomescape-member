5abdb8f160c1ece7e516d49389a74a8db92b7b5a
jyt6640 <jyt6640@gmail.com>
2026-05-16 15:41:47 +0900

    test: 과거 일정 예약 수정 및 취소 테스트 추가

--- BE/src/test/java/roomescape/reservation/domain/DefaultReservationPolicyTest.java
+++ BE/src/test/java/roomescape/reservation/domain/DefaultReservationPolicyTest.java
@@ -9,32 +9,57 @@ import org.junit.jupiter.api.Test;
 import roomescape.global.exception.customException.BusinessException;
 import roomescape.reservationTime.domain.ReservationTime;
 
-class                                DefaultReservationPolicyTest {
+class DefaultReservationPolicyTest {
 
     private final ReservationPolicy reservationPolicy = new DefaultReservationPolicy();
 
     @Test
-    @DisplayName("미래 일정이면 예외가 발생하지 않는다")
-    void pastDateTime_success() {
+    @DisplayName("미래 일정이면 예약 생성 시 예외가 발생하지 않는다")
+    void validateCreatableDateTime_success() {
         // given
         LocalDateTime futureDateTime = LocalDateTime.now().plusMinutes(1);
         ReservationTime reservationTime = ReservationTime.create(futureDateTime.toLocalTime());
 
         // when & then
-        assertThatCode(() -> reservationPolicy.pastDateTime(futureDateTime.toLocalDate(), reservationTime))
+        assertThatCode(() -> reservationPolicy.validateCreatableDateTime(futureDateTime.toLocalDate(), reservationTime))
                 .doesNotThrowAnyException();
     }
 
     @Test
-    @DisplayName("지난 일정이면 예외가 발생한다")
-    void pastDateTime_fail_with_past_date_time() {
+    @DisplayName("지난 일정이면 예약 생성 시 예외가 발생한다")
+    void validateCreatableDateTime_fail_with_past_date_time() {
         // given
         LocalDateTime pastDateTime = LocalDateTime.now().minusMinutes(1);
         ReservationTime reservationTime = ReservationTime.create(pastDateTime.toLocalTime());
 
         // when & then
-        assertThatThrownBy(() -> reservationPolicy.pastDateTime(pastDateTime.toLocalDate(), reservationTime))
+        assertThatThrownBy(() -> reservationPolicy.validateCreatableDateTime(pastDateTime.toLocalDate(), reservationTime))
                 .isInstanceOf(BusinessException.class)
                 .hasMessageContaining("지난 일정으로 예약할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("미래 일정이면 예약 수정 및 취소 시 예외가 발생하지 않는다")
+    void validateModifiableDateTime_success() {
+        // given
+        LocalDateTime futureDateTime = LocalDateTime.now().plusMinutes(1);
+        ReservationTime reservationTime = ReservationTime.create(futureDateTime.toLocalTime());
+
+        // when & then
+        assertThatCode(() -> reservationPolicy.validateModifiableDateTime(futureDateTime.toLocalDate(), reservationTime))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("지난 일정이면 예약 수정 및 취소 시 예외가 발생한다")
+    void validateModifiableDateTime_fail_with_past_date_time() {
+        // given
+        LocalDateTime pastDateTime = LocalDateTime.now().minusMinutes(1);
+        ReservationTime reservationTime = ReservationTime.create(pastDateTime.toLocalTime());
+
+        // when & then
+        assertThatThrownBy(() -> reservationPolicy.validateModifiableDateTime(pastDateTime.toLocalDate(), reservationTime))
+                .isInstanceOf(BusinessException.class)
+                .hasMessageContaining("지난 일정의 예약은 수정 및 취소할 수 없습니다.");
     }
 }
