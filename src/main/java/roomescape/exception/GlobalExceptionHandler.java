0c672b0240121d349aefff2a113d4bd9c3055ad0
구민서 <minseo12379@gmail.com>
2026-05-16 18:11:37 +0900

    feat(GlobalExceptionHandler): HttpMessageNotReadableException 예외 처리 구현

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -48,6 +49,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleMissingParams(MissingServletRequestParameterException e) {
         String errorMessage = String.format("필수 요청 파라미터(%s)가 누락되었습니다.", e.getParameterName());
         ErrorResponse response = new ErrorResponse(errorMessage);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        ErrorResponse response = new ErrorResponse("요청 본문이 올바르지 않습니다. JSON 형식을 확인해주세요.");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
