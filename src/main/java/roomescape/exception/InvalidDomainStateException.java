ad0625012a15798849263a7ece6f8d2954654c42
Jinriro <riro12.hanyang.ac.kr>
2026-05-17 17:16:17 +0900

    refactor: domain 형식 검증 실패 시 HTTP 상태 코드를 400으로 변경

--- src/main/java/roomescape/exception/InvalidDomainStateException.java
+++ src/main/java/roomescape/exception/InvalidDomainStateException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 public class InvalidDomainStateException extends RoomescapeException {
 
     public InvalidDomainStateException(String message) {
-        super(HttpStatus.INTERNAL_SERVER_ERROR, "INVALID_DOMAIN_STATE", message);
+        super(HttpStatus.BAD_REQUEST, "INVALID_DOMAIN_STATE", message);
     }
 }
