7c653d2788dfa532cd04eed3378676ad8044a17a
gleaming9 <gleaming9@khu.ac.kr>
2026-05-17 17:33:03 +0900

    refactor: 예외 처리 방식을 ExceptionHandler로 통일

--- src/main/java/roomescape/global/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/global/exception/GlobalExceptionHandler.java
@@ -2,56 +2,34 @@ package roomescape.global.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     private static final String SERVER_ERROR_MESSAGE = "서버 내부에서 문제가 발생했습니다.";
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatusCode status,
-                                                                  WebRequest request) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, "입력값이 올바르지 않습니다.");
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ProblemDetail> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_REQUEST,
+                "입력값이 올바르지 않습니다."
+        );
         problemDetail.setProperty("errors", exception.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(error -> new ValidationError(error.getField(), error.getDefaultMessage()))
                 .toList());
 
-        return handleExceptionInternal(exception, problemDetail, headers, status, request);
-    }
-
-    @Override
-    protected ProblemDetail createProblemDetail(Exception exception,
-                                                HttpStatusCode status,
-                                                String defaultDetail,
-                                                String detailMessageCode,
-                                                Object[] detailMessageArguments,
-                                                WebRequest request) {
-        if (status.is5xxServerError()) {
-            return ProblemDetail.forStatusAndDetail(status, SERVER_ERROR_MESSAGE);
-        }
-        return super.createProblemDetail(
-                exception,
-                status,
-                defaultDetail,
-                detailMessageCode,
-                detailMessageArguments,
-                request
-        );
+        return ResponseEntity
+                .badRequest()
+                .body(problemDetail);
     }
 
     @ExceptionHandler(InvalidRequestException.class)
@@ -71,12 +49,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(InfrastructureException.class)
     public ResponseEntity<ProblemDetail> handleInfrastructureException(InfrastructureException exception) {
-        return problem(HttpStatus.INTERNAL_SERVER_ERROR, SERVER_ERROR_MESSAGE);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ProblemDetail> handleAllUncaughtException(Exception exception) {
-        log.error("Unexpected exception occurred", exception);
         return problem(HttpStatus.INTERNAL_SERVER_ERROR, SERVER_ERROR_MESSAGE);
     }
 
