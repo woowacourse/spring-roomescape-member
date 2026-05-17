15334ddaa6edfbeec3ca561d0d47378c6e7caf1a
Soojin6943 <sjhan0711@gmail.com>
2026-05-17 13:54:14 +0900

    fix(test): 날짜 하드코딩으로 인해 깨지던 테스트 해결

--- src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.is;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
@@ -24,9 +25,11 @@ public class ReservationControllerTest {
 
     @Test
     void 예약추가_예약존재_성공() {
+        String tomorrow = LocalDate.now().plusDays(1).toString();
+
         Map<String, Object> params = new HashMap<>();
         params.put("name", "초록");
-        params.put("date", "2026-05-20");
+        params.put("date", tomorrow);
         params.put("timeId", 7L);
         params.put("themeId", 2L);
 
@@ -40,9 +43,11 @@ public class ReservationControllerTest {
 
     @Test
     void 예약_추가_시간없음_실패() {
+        String tomorrow = LocalDate.now().plusDays(1).toString();
+
         Map<String, Object> params = new HashMap<>();
         params.put("name", "초록");
-        params.put("date", "2026-05-20");
+        params.put("date", tomorrow);
         params.put("timeId", 15L);
         params.put("themeId", 2L);
 
@@ -56,8 +61,10 @@ public class ReservationControllerTest {
 
     @Test
     void 예약_변경_성공() {
+        String tomorrow = LocalDate.now().plusDays(1).toString();
+
         Map<String, Object> params = new HashMap<>();
-        params.put("date", "2026-05-16");
+        params.put("date", tomorrow);
         params.put("timeId", 7L);
 
         RestAssured.given().log().all()
@@ -70,14 +77,15 @@ public class ReservationControllerTest {
         RestAssured.given().log().all()
                 .when().get("/api/reservations?name=도우너")
                 .then().log().all()
-                .body("reservations[1].date", is("2026-05-16"))
+                .body("reservations[1].date", is(tomorrow))
                 .body("reservations[1].time.id", is(7));
     }
 
     @Test
     void 과거로_예약_변경시_실패_테스트() {
+        String yesterday = LocalDate.now().minusDays(1).toString();
         Map<String, Object> params = new HashMap<>();
-        params.put("date", "2026-05-14");
+        params.put("date", yesterday);
         params.put("timeId", 7L);
 
         RestAssured.given().log().all()
