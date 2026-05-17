b46634609e0ab36d06d5b0b43b7864fb56d404f0
wonsang.choi <wonsang.choi@e4net.net>
2026-05-17 11:06:40 +0900

    fix: 어드민 API URL 대문자 오탈자 수정

--- src/main/java/roomescape/admin/controller/AdminReservationTimeController.java
+++ src/main/java/roomescape/admin/controller/AdminReservationTimeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.reservation.service.ReservationTimeService;
 
-@RequestMapping("/admin/Times")
+@RequestMapping("/admin/times")
 @RestController
 @RequiredArgsConstructor
 public class AdminReservationTimeController {
