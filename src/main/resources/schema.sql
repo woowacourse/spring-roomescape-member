e08fe42bb74d3a19f8b54fc01cf577952cc46ab1
wonsang.choi <wonsang.choi@e4net.net>
2026-05-17 11:05:48 +0900

    fix: reservation 테이블 인덱스 컬럼순서 조정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS reservation
     time_id  BIGINT       NOT NULL,
     theme_id BIGINT       NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT unique_reservation_date_time_theme UNIQUE (date, time_id, theme_id),
+    CONSTRAINT unique_reservation_date_time_theme UNIQUE (theme_id, date, time_id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
