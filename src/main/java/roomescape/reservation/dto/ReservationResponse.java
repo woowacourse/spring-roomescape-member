79bee7c1231253fb367059038cd18becca038a9e
gleaming9 <gleaming9@khu.ac.kr>
2026-05-17 17:39:37 +0900

    style: 형식 통일

--- src/main/java/roomescape/reservation/dto/ReservationResponse.java
+++ src/main/java/roomescape/reservation/dto/ReservationResponse.java
@@ -4,8 +4,13 @@ import roomescape.reservation.domain.Reservation;
 import roomescape.reservationtime.dto.ReservationTimeResponse;
 import roomescape.theme.dto.ThemeResponse;
 
-public record ReservationResponse(Long id, String name, String date, ReservationTimeResponse time,
-                                  ThemeResponse theme) {
+public record ReservationResponse(
+        Long id,
+        String name,
+        String date,
+        ReservationTimeResponse time,
+        ThemeResponse theme
+) {
 
     public static ReservationResponse from(Reservation reservation) {
         return new ReservationResponse(
