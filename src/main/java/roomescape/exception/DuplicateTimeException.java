9c7c8cb610e81f116148f77a2f5214e1364c1d00
Andrew Kim <38521355+nn98@users.noreply.github.com>
2026-05-17 14:34:57 +0900

    refactor(feedback01): DuplicateReservationException 상속 오류

--- src/main/java/roomescape/exception/DuplicateTimeException.java
+++ src/main/java/roomescape/exception/DuplicateTimeException.java
@@ -2,6 +2,6 @@ package roomescape.exception;
 
 public class DuplicateTimeException extends RoomescapeException {
     public DuplicateTimeException(String time) {
-        super("DUPLICATE_TIME_SLOT", "이미 등록된 예약 시간입니다. (" + time + ")");
+        super("DUPLICATE_TIME_SLOT", "이미 등록된 예약대 시간입니다. (" + time + ")");
     }
 }
--- src/main/java/roomescape/exception/ProblemDetailsAdvice.java
+++ src/main/java/roomescape/exception/ProblemDetailsAdvice.java
@@ -3,22 +3,20 @@ package roomescape.exception;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @RestControllerAdvice
-public class ProblemDetailsAdvice extends ResponseEntityExceptionHandler {
+public class ProblemDetailsAdvice {
 
     private final Map<Class<? extends RoomescapeException>, HttpStatus> exceptionHttpStatusMap = new ConcurrentHashMap<>();
 
@@ -30,6 +28,7 @@ public class ProblemDetailsAdvice extends ResponseEntityExceptionHandler {
         exceptionHttpStatusMap.put(PastReservationControlException.class, HttpStatus.BAD_REQUEST);
         exceptionHttpStatusMap.put(PastTimeException.class, HttpStatus.BAD_REQUEST);
         exceptionHttpStatusMap.put(ResourceInUseException.class, HttpStatus.CONFLICT);
+        exceptionHttpStatusMap.put(DuplicateReservationException.class, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(RoomescapeException.class)
@@ -65,35 +64,8 @@ public class ProblemDetailsAdvice extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
-            HttpRequestMethodNotSupportedException ex,
-            HttpHeaders headers,
-            HttpStatusCode status,
-            WebRequest request) {
-        String detailMessage = "해당 경로에서는 " + ex.getMethod() + " 요청을 지원하지 않습니다. API 명세를 확인해 주세요.";
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.METHOD_NOT_ALLOWED, detailMessage);
-        problemDetail.setProperty("code", "METHOD_NOT_ALLOWED");
-        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(problemDetail);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleMissingServletRequestParameter(
-            MissingServletRequestParameterException ex,
-            HttpHeaders headers,
-            HttpStatusCode status,
-            WebRequest request) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "필수 파라미터가 누락되었습니다.");
-        problemDetail.setProperty("code", "MISSING_PARAMETER");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex,
-            HttpHeaders headers,
-            HttpStatusCode status,
-            WebRequest request) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ProblemDetail> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "요청 값이 유효하지 않습니다.");
         problemDetail.setProperty("code", "INVALID_REQUEST_BODY");
         problemDetail.setProperty("errors", extractErrors(ex));
