cba1dd5c36dc3867f79dc8f472ecebea2347959e
choiyoung69 <choiyoung609@gmail.com>
2026-05-18 15:05:36 +0900

    feat: reservation 중복 예약 시 db에서 에러 반환하고, 이를 도메인 에러로 변환

--- src/main/java/roomescape/reservation/infra/JdbcReservationRepository.java
+++ src/main/java/roomescape/reservation/infra/JdbcReservationRepository.java
@@ -3,11 +3,14 @@ package roomescape.reservation.infra;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import roomescape.global.RoomEscapeException;
+import roomescape.reservation.application.exception.ReservationErrorCode;
 import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.domain.repository.ReservationDetail;
 import roomescape.reservation.domain.repository.ReservationRepository;
@@ -82,8 +85,12 @@ public class JdbcReservationRepository implements ReservationRepository {
                 .addValue("theme_id", reservation.getThemeId())
                 .addValue("time_id", reservation.getTimeId());
 
-        Long id = jdbcInsert.executeAndReturnKey(params).longValue();
-        return reservation.withId(id);
+        try {
+            Long id = jdbcInsert.executeAndReturnKey(params).longValue();
+            return reservation.withId(id);
+        } catch (DuplicateKeyException e) {
+            throw new RoomEscapeException(ReservationErrorCode.DUPLICATE_RESERVATION);
+        }
     }
 
     @Override
