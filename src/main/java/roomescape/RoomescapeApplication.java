26e563551221f77366298b9bdb4b55611f74f256
SeungGyu <phillipskl123@gmail.com>
2026-05-18 17:19:05 +0900

    refactor: Clock 설정 분리 및 시간대 명시

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -2,19 +2,11 @@ package roomescape;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.Clock;
 
 @SpringBootApplication
 public class RoomescapeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeApplication.class, args);
-    }
-
-    @Bean
-    public Clock clock() {
-        return Clock.systemDefaultZone();
     }
 }
