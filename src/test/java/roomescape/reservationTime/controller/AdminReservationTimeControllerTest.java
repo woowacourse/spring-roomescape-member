e9cb78f41be6e0094e091e4aad290b274d36936b
Soojin6943 <sjhan0711@gmail.com>
2026-05-18 12:52:35 +0900

    refactor(test): DEFINDED_PORT에서 RANDOM_PORT로 변경

--- src/test/java/roomescape/reservationTime/controller/AdminReservationTimeControllerTest.java
+++ src/test/java/roomescape/reservationTime/controller/AdminReservationTimeControllerTest.java
@@ -8,9 +8,10 @@ import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class AdminReservationTimeControllerTest {
 
--- src/test/java/roomescape/theme/controller/ThemeControllerTest.java
+++ src/test/java/roomescape/theme/controller/ThemeControllerTest.java
@@ -5,9 +5,10 @@ import static org.hamcrest.Matchers.is;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ThemeControllerTest {
 
