5f92d135400d2e1b0ad2928a70800953a910c950
dahye <dahye9011@naver.com>
2026-05-18 13:33:16 +0900

    과거 예약 ErrorCode 추가 및 PastReservationException 생성자 ErrorCode 주입 수정

--- src/main/java/roomescape/reservation/exception/PastReservationException.java
+++ src/main/java/roomescape/reservation/exception/PastReservationException.java
@@ -6,19 +6,19 @@ import roomescape.error.BusinessException;
 import roomescape.error.ErrorCode;
 
 public class PastReservationException extends BusinessException {
-    private PastReservationException() {
-        super(HttpStatus.BAD_REQUEST, ErrorCode.PAST_RESERVATION);
+    private PastReservationException(ErrorCode errorCode) {
+        super(HttpStatus.BAD_REQUEST, errorCode);
     }
 
     public static PastReservationException pastReservation() {
-        return new PastReservationException();
+        return new PastReservationException(ErrorCode.PAST_RESERVATION_CREATE);
     }
 
     public static PastReservationException pastCancel() {
-        return new PastReservationException();
+        return new PastReservationException(ErrorCode.PAST_RESERVATION_CANCEL);
     }
 
     public static PastReservationException pastUpdate() {
-        return new PastReservationException();
+        return new PastReservationException(ErrorCode.PAST_RESERVATION_UPDATE);
     }
 }
