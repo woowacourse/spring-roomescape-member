081e0d194e04c7f3f8fbd375b6fc266555d7accb
나의찬 <cab07036@naver.com>
2026-05-18 15:08:19 +0900

    fix: @Validated를 통한 파라미터 @NotBlank 검증

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -6,11 +6,13 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import roomescape.controller.dto.*;
 import roomescape.domain.Reservation;
 import roomescape.service.ReservationService;
 
+@Validated
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
