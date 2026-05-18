b9204e5b959dd1566b52e5f7d58ed4c8f64f7520
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2026-05-18 17:11:16 +0900

    refactor(reservation): findByThemeAndDate 파라미터 이름을 themeId로 변경

--- src/main/java/roomescape/reservation/domain/ReservationRepository.java
+++ src/main/java/roomescape/reservation/domain/ReservationRepository.java
@@ -9,7 +9,7 @@ public interface ReservationRepository {
     Reservation update(Reservation reservation);
     List<Reservation> findAll();
     List<Reservation> findByName(String name);
-    List<Reservation> findByThemeAndDate(Long aLong, LocalDate date);
+    List<Reservation> findByThemeAndDate(Long themeId, LocalDate date);
     Optional<Reservation> findById(Long id);
     boolean existsByReservationTime(Long timeId);
     boolean existsByDateAndTimeAndTheme(LocalDate date, Long timeId, Long themeId);
