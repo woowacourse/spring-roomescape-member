8f6ea6a109374e41d0fb376a1094e79a9d99301e
jyt6640 <jyt6640@gmail.com>
2026-05-19 13:37:25 +0900

    refactor: 예약 정책 삭제

--- BE/src/main/java/roomescape/reservation/domain/ReservationPolicy.java
+++
@@ -1,9 +0,0 @@
-package roomescape.reservation.domain;
-
-import java.time.LocalDate;
-import roomescape.reservationTime.domain.ReservationTime;
-
-public interface ReservationPolicy {
-    void validateCreatableDateTime(LocalDate date, ReservationTime time);
-    void validateModifiableDateTime(LocalDate date, ReservationTime time);
-}
